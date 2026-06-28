3a8b634cf7a072d33502a71ec0ed499a3a3c8e7c
dginovker <danielginovker@gmail.com>
2026-06-28 01:43:32 -0700

    Match game client random variable accessors

---
+++ src/game/game_client_random_variable.cpp
@@ -0,0 +1,42 @@
+#include "game_client_random_variable.h"
+
+GameClientRandomVariable::GameClientRandomVariable()
+{
+    distribution = CONSTANT;
+    minimum = 0.0f;
+    maximum = 0.0f;
+}
+
+GameClientRandomVariable::DistributionType GameClientRandomVariable::getDistributionType() const
+{
+    return distribution;
+}
+
+float GameClientRandomVariable::getMinimumValue() const
+{
+    return minimum;
+}
+
+float GameClientRandomVariable::getMaximumValue() const
+{
+    return maximum;
+}
+
+void GameClientRandomVariable::setRange(float minimum, float maximum, DistributionType distribution)
+{
+    this->minimum = minimum;
+    this->maximum = maximum;
+    this->distribution = distribution;
+}
+
+bool operator==(const GameClientRandomVariable &left, const GameClientRandomVariable &right)
+{
+    return left.distribution == right.distribution &&
+        left.minimum == right.minimum &&
+        left.maximum == right.maximum;
+}
+
+bool operator!=(const GameClientRandomVariable &left, const GameClientRandomVariable &right)
+{
+    return !(left == right);
+}
---
+++ src/game/game_client_random_variable.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class GameClientRandomVariable {
+public:
+    enum DistributionType {
+        CONSTANT = 0,
+    };
+
+    GameClientRandomVariable();
+
+    DistributionType getDistributionType() const;
+    float getMinimumValue() const;
+    float getMaximumValue() const;
+    void setRange(float minimum, float maximum, DistributionType distribution);
+
+    DistributionType distribution;
+    float minimum;
+    float maximum;
+};
+
+bool operator==(const GameClientRandomVariable &left, const GameClientRandomVariable &right);
+bool operator!=(const GameClientRandomVariable &left, const GameClientRandomVariable &right);
