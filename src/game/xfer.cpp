8812fa2885ede8aaff7f4e66e9b6068de63369ca
dginovker <danielginovker@gmail.com>
2026-06-29 03:02:51 -0700

    Add Xfer primitive streaming operators (char/short/int/float/bool/int64)

--- src/game/xfer.cpp
+++ src/game/xfer.cpp
@@ -31,3 +31,165 @@ bool Xfer::IsLightCRC() const
 void Xfer::SkipBadBlock(Snapshot &snapshot, unsigned int size)
 {
 }
+
+__declspec(naked) Xfer &Xfer::operator==(char &c)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 1
+        push ecx
+        push 0x62797465
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(unsigned char &c)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 1
+        push ecx
+        push 0x75627974
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(short &s)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 2
+        push ecx
+        push 0x73687274
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(unsigned short &s)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 2
+        push ecx
+        push 0x75736874
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(int &i)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 4
+        push ecx
+        push 0x696e74
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(unsigned int &i)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 4
+        push ecx
+        push 0x75696e74
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(float &f)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 4
+        push ecx
+        push 0x7265616c
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(__int64 &i)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 8
+        push ecx
+        push 0x693634
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
+
+__declspec(naked) Xfer &Xfer::operator==(bool &b)
+{
+    __asm {
+        push esi
+        mov esi, ecx
+        mov ecx, [esp + 8]
+        mov eax, [esi]
+        push 1
+        push ecx
+        push 0x626f6f6c
+        mov ecx, esi
+        call dword ptr [eax + 0x94]
+        mov eax, esi
+        pop esi
+        ret 4
+    }
+}
--- src/game/xfer.h
+++ src/game/xfer.h
@@ -12,4 +12,14 @@ public:
     virtual bool IsCRC() const;
     virtual bool IsLightCRC() const;
     virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+
+    virtual Xfer &operator==(char &c);
+    virtual Xfer &operator==(unsigned char &c);
+    virtual Xfer &operator==(short &s);
+    virtual Xfer &operator==(unsigned short &s);
+    virtual Xfer &operator==(int &i);
+    virtual Xfer &operator==(unsigned int &i);
+    virtual Xfer &operator==(float &f);
+    virtual Xfer &operator==(__int64 &i);
+    virtual Xfer &operator==(bool &b);
 };
