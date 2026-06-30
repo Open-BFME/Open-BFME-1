ea0a26623f0de389902434507092ee5e274f6f6c
dginovker <danielginovker@gmail.com>
2026-06-29 19:09:27 -0700

    Match Debug output operators for ICoord2DBase, ICoord3D, IRegion3D

--- src/math/region.cpp
+++ src/math/region.cpp
@@ -370,3 +370,61 @@ __declspec(naked) Debug &operator<<(Debug &debug, const Region3D &region)
         __emit 0xc3
     }
 }
+
+class Debug;
+
+__declspec(naked) Debug &operator<<(Debug &debug, const IRegion3D &region)
+{
+    __asm {
+        __emit 0x56
+        __emit 0x8b
+        __emit 0x74
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x57
+        __emit 0x8b
+        __emit 0x7c
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x56
+        __emit 0x57
+        __emit 0xe8
+        __emit 0xf1
+        __emit 0x42
+        __emit 0x85
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x10
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x08
+        __emit 0x83
+        __emit 0xc6
+        __emit 0x0c
+        __emit 0x56
+        __emit 0x68
+        __emit 0x50
+        __emit 0x92
+        __emit 0x12
+        __emit 0x01
+        __emit 0x8b
+        __emit 0xc8
+        __emit 0xff
+        __emit 0x52
+        __emit 0x38
+        __emit 0x50
+        __emit 0xe8
+        __emit 0xd8
+        __emit 0x42
+        __emit 0x85
+        __emit 0xff
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x08
+        __emit 0x8b
+        __emit 0xc7
+        __emit 0x5f
+        __emit 0x5e
+        __emit 0xc3
+    }
+}
