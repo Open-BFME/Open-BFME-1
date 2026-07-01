a6d4d27b9a9af12811d53b9658ba6da548f73b12
dginovker <danielginovker@gmail.com>
2026-07-01 12:41:41 -0700

    Translate ortho velocity assignment

--- src/game/fx_particle_system_bulk.cpp
+++ src/game/fx_particle_system_bulk.cpp
@@ -23034,100 +23034,10 @@ OrthoEmissionVelocityInfo &OrthoEmissionVelocityInfo::operator=(const OrthoEmiss
 }
 
 // ??4OrthoEmissionVelocityModuleTemplate@FXParticleSystem@@QAEAAV01@ABV01@@Z
-__declspec(naked) OrthoEmissionVelocityModuleTemplate &OrthoEmissionVelocityModuleTemplate::operator=(const OrthoEmissionVelocityModuleTemplate &that)
+OrthoEmissionVelocityModuleTemplate &OrthoEmissionVelocityModuleTemplate::operator=(const OrthoEmissionVelocityModuleTemplate &that)
 {
-    __asm {
-        __emit 0x8b
-        __emit 0xc1
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x51
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xd2
-        __emit 0x56
-        __emit 0x8d
-        __emit 0x4a
-        __emit 0x04
-        __emit 0x57
-        __emit 0x8b
-        __emit 0x39
-        __emit 0x8d
-        __emit 0x70
-        __emit 0x0c
-        __emit 0x89
-        __emit 0x3e
-        __emit 0x8b
-        __emit 0x79
-        __emit 0x04
-        __emit 0x89
-        __emit 0x7e
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x49
-        __emit 0x08
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x4a
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x39
-        __emit 0x8d
-        __emit 0x70
-        __emit 0x18
-        __emit 0x89
-        __emit 0x3e
-        __emit 0x8b
-        __emit 0x79
-        __emit 0x04
-        __emit 0x89
-        __emit 0x7e
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x49
-        __emit 0x08
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x08
-        __emit 0x83
-        __emit 0xc2
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0x32
-        __emit 0x8d
-        __emit 0x48
-        __emit 0x24
-        __emit 0x89
-        __emit 0x31
-        __emit 0x8b
-        __emit 0x72
-        __emit 0x04
-        __emit 0x89
-        __emit 0x71
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x52
-        __emit 0x08
-        __emit 0x5f
-        __emit 0x89
-        __emit 0x51
-        __emit 0x08
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
+    OrthoEmissionVelocityInfo::operator=(that);
+    return *this;
 }
 
 OutwardEmissionVelocityInfo &OutwardEmissionVelocityInfo::operator=(const OutwardEmissionVelocityInfo &that)
