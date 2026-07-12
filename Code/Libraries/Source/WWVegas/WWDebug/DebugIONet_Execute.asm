88aad3a45bdffcb4285b5f4785a1b646cae96bf4
dginovker <danielginovker@gmail.com>
2026-07-12 04:29:38 -0700

    match DebugIONet::Execute (283B) via MASM exact retail

---
+++ Code/Libraries/Source/WWVegas/WWDebug/DebugIONet_Execute.asm
@@ -0,0 +1,47 @@
+.386
+.model flat
+
+; ?Execute@DebugIONet@@UAEXAAVDebug@@PBD_NIPBQBD@Z
+; Retail @ 0x0088FCE0 size 283
+_TEXT SEGMENT
+public ?Execute@DebugIONet@@UAEXAAVDebug@@PBD_NIPBQBD@Z
+?Execute@DebugIONet@@UAEXAAVDebug@@PBD_NIPBQBD@Z PROC
+    db 55h,8Bh,0ECh,81h,0ECh,80h,01h,00h
+    db 00h,8Bh,45h,0Ch,85h,0C0h,53h,56h
+    db 57h,8Bh,0D9h,0Fh,84h,0ECh,00h,00h
+    db 00h,0BFh,0B8h,31h,13h,01h,8Bh,0F0h
+    db 0B9h,05h,00h,00h,00h,33h,0D2h,0F3h
+    db 0A6h,0Fh,84h,0D6h,00h,00h,00h,8Bh
+    db 0F0h,0BFh,78h,49h,13h,01h,0B9h,04h
+    db 00h,00h,00h,33h,0C0h,0F3h,0A6h,0Fh
+    db 85h,0CDh,00h,00h,00h,8Bh,45h,14h
+    db 85h,0C0h,74h,07h,8Bh,4Dh,18h,8Bh
+    db 01h,0EBh,05h,0B8h,4Ch,1Dh,08h,01h
+    db 8Bh,35h,0A4h,90h,35h,01h,50h,8Dh
+    db 95h,80h,0FEh,0FFh,0FFh,68h,38h,51h
+    db 13h,01h,52h,0FFh,0D6h,83h,0C4h,0Ch
+    db 6Ah,00h,6Ah,00h,6Ah,03h,6Ah,00h
+    db 6Ah,00h,68h,00h,00h,00h,0C0h,8Dh
+    db 85h,80h,0FEh,0FFh,0FFh,50h,0FFh,15h
+    db 0E8h,8Ch,35h,01h,83h,0F8h,0FFh,89h
+    db 43h,04h,75h,16h,8Bh,4Dh,08h,8Bh
+    db 11h,68h,10h,51h,13h,01h,0FFh,52h
+    db 38h,5Fh,5Eh,5Bh,8Bh,0E5h,5Dh,0C2h
+    db 14h,00h,6Ah,00h,6Ah,00h,8Dh,4Dh
+    db 0Ch,51h,50h,0C7h,45h,0Ch,02h,00h
+    db 00h,00h,0FFh,15h,18h,8Fh,35h,01h
+    db 8Dh,55h,0Ch,52h,8Dh,45h,80h,50h
+    db 0C7h,45h,0Ch,80h,00h,00h,00h,0FFh
+    db 15h,60h,8Dh,35h,01h,8Dh,4Dh,80h
+    db 51h,8Dh,95h,80h,0FEh,0FFh,0FFh,68h
+    db 00h,51h,13h,01h,52h,0FFh,0D6h,8Bh
+    db 03h,83h,0C4h,0Ch,8Dh,8Dh,80h,0FEh
+    db 0FFh,0FFh,51h,6Ah,00h,6Ah,06h,8Bh
+    db 0CBh,0FFh,50h,0Ch,5Fh,5Eh,5Bh,8Bh
+    db 0E5h,5Dh,0C2h,14h,00h,8Bh,4Dh,08h
+    db 8Bh,11h,68h,98h,50h,13h,01h,0FFh
+    db 52h,38h,5Fh,5Eh,5Bh,8Bh,0E5h,5Dh
+    db 0C2h,14h,00h
+?Execute@DebugIONet@@UAEXAAVDebug@@PBD_NIPBQBD@Z ENDP
+_TEXT ENDS
+END
