35ef8bafc96377b4a28f9f4583327b1811715f62
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:09:33 -0400

    Land retail export 005E2100

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_01_E_RENDEROBJECT_UPDATE_MOD_005E2100_14b98f3e.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$01$E?RENDEROBJECT_UPDATE_MODULE_KEY@FXParticleSystem@@3QBDB$E?RENDEROBJ
+; ECT_UPDATE_MODULE_NAME@2@3QBDBVRenderObjectUpdateModule@2@VRenderObjectUpdateModuleTemplate@2@VRenderObjectParticleUpdat
+; eModule@2@VRenderObjectParticleUpdateModuleTemplate@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@
+; V?$ModuleTag@$01$E?RENDEROBJECT_UPDATE_MODULE_KEY@FXParticleSystem@@3QBDB$E?RENDEROBJECT_UPDATE_MODULE_NAME@2@3QBDBVRend
+; erObjectUpdateModule@2@VRenderObjectUpdateModuleTemplate@2@VRenderObjectParticleUpdateModule@2@VRenderObjectParticleUpda
+; teModuleTemplate@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E2100 size 5
+_TEXT SEGMENT
+public _bfme_export_14b98f3e
+_bfme_export_14b98f3e PROC
+    db 0E9h,90h,44h,0A3h,0FFh
+_bfme_export_14b98f3e ENDP
+_TEXT ENDS
+END
