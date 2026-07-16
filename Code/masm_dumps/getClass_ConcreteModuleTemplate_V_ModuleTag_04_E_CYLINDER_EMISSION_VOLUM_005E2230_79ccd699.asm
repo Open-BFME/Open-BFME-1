a8f7a01c015d72ceb8e58591e87cd8b30b0e1ec8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:11:58 -0400

    Land retail export 005E2230

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_04_E_CYLINDER_EMISSION_VOLUM_005E2230_79ccd699.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$04$E?CYLINDER_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?CYLI
+; NDER_EMISSION_VOLUME_MODULE_NAME@2@3QBDBVCylinderEmissionVolumeModule@2@VCylinderEmissionVolumeModuleTemplate@2@V?$Defau
+; ltParticleModule@$04@2@V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModu
+; leClass@V?$ModuleTag@$04$E?CYLINDER_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?CYLINDER_EMISSION_VOLUME_MODULE
+; _NAME@2@3QBDBVCylinderEmissionVolumeModule@2@VCylinderEmissionVolumeModuleTemplate@2@V?$DefaultParticleModule@$04@2@V?$D
+; efaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E2230 size 5
+_TEXT SEGMENT
+public _bfme_export_79ccd699
+_bfme_export_79ccd699 PROC
+    db 0E9h,44h,12h,0A5h,0FFh
+_bfme_export_79ccd699 ENDP
+_TEXT ENDS
+END
