5016e29d0a01f863d16341390f6a10830bff85b4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:10:31 -0400

    Land retail export 005E21C0

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_03_E_HEMISPHERICAL_EMISSION__005E21C0_7c4219c1.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$03$E?HEMISPHERICAL_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QBDB
+; $E?HEMISPHERICAL_EMISSION_VELOCITY_MODULE_NAME@2@3QBDBVHemisphericalEmissionVelocityModule@2@VHemisphericalEmissionVeloc
+; ityModuleTemplate@2@V?$DefaultParticleModule@$03@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@FXParticle
+; System@@UBEABV?$ConcreteModuleClass@V?$ModuleTag@$03$E?HEMISPHERICAL_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QBD
+; B$E?HEMISPHERICAL_EMISSION_VELOCITY_MODULE_NAME@2@3QBDBVHemisphericalEmissionVelocityModule@2@VHemisphericalEmissionVelo
+; cityModuleTemplate@2@V?$DefaultParticleModule@$03@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E21C0 size 5
+_TEXT SEGMENT
+public _bfme_export_7c4219c1
+_bfme_export_7c4219c1 PROC
+    db 0E9h,2Ch,8Fh,0A6h,0FFh
+_bfme_export_7c4219c1 ENDP
+_TEXT ENDS
+END
