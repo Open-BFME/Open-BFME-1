095a32329d3c4d171c352d0eae5d746ed8222585
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:22:29 -0400

    Land retail export 005E46C0

---
+++ Code/masm_dumps/createTemplate_ConcreteModuleClass_V_ModuleTag_07_E_LIFE_EVENT_MODULE_KE_005E46C0_20024dd4.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?createTemplate@?$ConcreteModuleClass@V?$ModuleTag@$07$E?LIFE_EVENT_MODULE_KEY@FXParticleSystem@@3QBDB$E?LIFE_EVENT_MODU
+; LE_NAME@2@3QBDBVLifeEventModule@2@VLifeEventModuleTemplate@2@VParticleLifeEventModule@2@VParticleLifeEventModuleTemplate
+; @2@@FXParticleSystem@@@FXParticleSystem@@UBEPAVLifeEventModuleTemplate@2@PAVINI@@@Z
+; Retail @ 0x005E46C0 size 126
+_TEXT SEGMENT
+public _bfme_export_20024dd4
+_bfme_export_20024dd4 PROC
+    db 6Ah,0FFh,68h,43h,0B8h,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,6Ah
+    db 24h,0E8h,52h,0D8h,29h,00h,8Bh,0F0h
+    db 83h,0C4h,04h,89h,74h,24h,04h,85h
+    db 0F6h,0C7h,44h,24h,10h,00h,00h,00h
+    db 00h,74h,1Dh,8Bh,0CEh,0E8h,2Ah,69h
+    db 0A6h,0FFh,0C7h,06h,24h,14h,11h,01h
+    db 0C7h,46h,04h,20h,14h,11h,01h,0C7h
+    db 46h,0Ch,0Ch,14h,11h,01h,0EBh,02h
+    db 33h,0F6h,89h,74h,24h,04h,8Bh,44h
+    db 24h,18h,50h,8Bh,0CEh,0C7h,44h,24h
+    db 14h,01h,00h,00h,00h,0E8h,29h,0AEh
+    db 0A5h,0FFh,8Bh,4Ch,24h,08h,8Bh,0C6h
+    db 5Eh,64h,89h,0Dh,00h,00h,00h,00h
+    db 83h,0C4h,10h,0C2h,04h,00h
+_bfme_export_20024dd4 ENDP
+_TEXT ENDS
+END
