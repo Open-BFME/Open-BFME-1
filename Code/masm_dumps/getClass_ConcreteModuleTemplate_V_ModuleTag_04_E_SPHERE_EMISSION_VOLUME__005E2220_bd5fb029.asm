4c0a9c53e8784d87cbce797024b73699e1a1d8de
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:11:44 -0400

    Land retail export 005E2220

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_04_E_SPHERE_EMISSION_VOLUME__005E2220_bd5fb029.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$04$E?SPHERE_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?SPHERE
+; _EMISSION_VOLUME_MODULE_NAME@2@3QBDBVSphereEmissionVolumeModule@2@VSphereEmissionVolumeModuleTemplate@2@V?$DefaultPartic
+; leModule@$04@2@V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@
+; V?$ModuleTag@$04$E?SPHERE_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?SPHERE_EMISSION_VOLUME_MODULE_NAME@2@3QBD
+; BVSphereEmissionVolumeModule@2@VSphereEmissionVolumeModuleTemplate@2@V?$DefaultParticleModule@$04@2@V?$DefaultParticleMo
+; duleTemplate@$04@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E2220 size 5
+_TEXT SEGMENT
+public _bfme_export_bd5fb029
+_bfme_export_bd5fb029 PROC
+    db 0E9h,33h,3Eh,0A4h,0FFh
+_bfme_export_bd5fb029 ENDP
+_TEXT ENDS
+END
