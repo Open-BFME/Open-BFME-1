6f1c2593f3f3be41a969a440c5aa988f7af993c8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:08:52 -0400

    Land retail export 005E1F40

---
+++ Code/masm_dumps/1_ConcreteModuleTemplate_V_ModuleTag_07_E_TERRAIN_COLLISION_MODULE_KEY_F_005E1F40_4b62ea43.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ??1?$ConcreteModuleTemplate@V?$ModuleTag@$07$E?TERRAIN_COLLISION_MODULE_KEY@FXParticleSystem@@3QBDB$E?TERRAIN_COLLISION_
+; MODULE_NAME@2@3QBDBVTerrainCollisionModule@2@VTerrainCollisionModuleTemplate@2@VParticleTerrainCollisionModule@2@VPartic
+; leTerrainCollisionModuleTemplate@2@@FXParticleSystem@@@FXParticleSystem@@UAE@XZ
+; Retail @ 0x005E1F40 size 5
+_TEXT SEGMENT
+public _bfme_export_4b62ea43
+_bfme_export_4b62ea43 PROC
+    db 0E9h,0CBh,64h,0A5h,0FFh
+_bfme_export_4b62ea43 ENDP
+_TEXT ENDS
+END
