a8bcdc065083657ebb4c4402c4191efdea56d2d2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:21:49 -0400

    Land retail export 005E2240

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_04_E_LIGHTNING_EMISSION_MODU_005E2240_e7512f05.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$04$E?LIGHTNING_EMISSION_MODULE_KEY@FXParticleSystem@@3QBDB$E?LIGHTNING_
+; EMISSION_MODULE_NAME@2@3QBDBVLightningEmissionModule@2@VLightningEmissionModuleTemplate@2@V?$DefaultParticleModule@$04@2
+; @V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@V?$ModuleTag@$
+; 04$E?LIGHTNING_EMISSION_MODULE_KEY@FXParticleSystem@@3QBDB$E?LIGHTNING_EMISSION_MODULE_NAME@2@3QBDBVLightningEmissionMod
+; ule@2@VLightningEmissionModuleTemplate@2@V?$DefaultParticleModule@$04@2@V?$DefaultParticleModuleTemplate@$04@2@@FXPartic
+; leSystem@@@2@XZ
+; Retail @ 0x005E2240 size 5
+_TEXT SEGMENT
+public _bfme_export_e7512f05
+_bfme_export_e7512f05 PROC
+    db 0E9h,0B9h,1Eh,0A5h,0FFh
+_bfme_export_e7512f05 ENDP
+_TEXT ENDS
+END
