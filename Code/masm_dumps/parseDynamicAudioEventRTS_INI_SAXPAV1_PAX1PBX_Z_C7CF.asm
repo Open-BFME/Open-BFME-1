09adf18f2eeb45770e531f5391fb226e49f81bbb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:21:51 -0400

    Match INI dynamic audio event parser

---
+++ Code/masm_dumps/parseDynamicAudioEventRTS_INI_SAXPAV1_PAX1PBX_Z_C7CF.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0040C7CF size 5; canonical body @ 004BD640
+_TEXT SEGMENT
+public ?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z
+?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 06ch, 00eh, 00bh, 000h
+?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
