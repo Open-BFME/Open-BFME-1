be4ca0b08f6ba0101eb1fb67711da20a5cd9923a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:58:17 -0400

    Match INI index list parser

---
+++ Code/masm_dumps/parseIndexList_INI_SAXPAV1_PAX1PBX_Z_851050.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?parseIndexList@INI@@SAXPAV1@PAX1PBX@Z
+; Ghidra FUN_00c51050 @ RVA 00851050 size 91
+_TEXT SEGMENT
+public ?parseIndexList@INI@@SAXPAV1@PAX1PBX@Z
+?parseIndexList@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 08bh, 044h, 024h, 004h, 083h, 0ech, 008h, 056h
+    db 08bh, 0b0h, 014h, 004h, 000h, 000h, 056h, 06ah
+    db 000h, 0ffh, 015h, 0d8h, 094h, 035h, 001h, 083h
+    db 0c4h, 008h, 085h, 0c0h, 075h, 024h, 056h, 068h
+    db 050h, 003h, 013h, 001h, 08dh, 04ch, 024h, 00ch
+    db 06ah, 003h, 051h, 0e8h, 080h, 0f5h, 0ffh, 0ffh
+    db 083h, 0c4h, 010h, 068h, 030h, 0fch, 01dh, 001h
+    db 08dh, 054h, 024h, 008h, 052h, 0e8h, 06eh, 05ch
+    db 01ah, 000h, 08bh, 04ch, 024h, 01ch, 051h, 050h
+    db 0e8h, 043h, 0f9h, 0ffh, 0ffh, 08bh, 054h, 024h
+    db 020h, 083h, 0c4h, 008h, 089h, 002h, 05eh, 083h
+    db 0c4h, 008h, 0c3h
+?parseIndexList@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
