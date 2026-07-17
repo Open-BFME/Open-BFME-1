91f6a72fa91b89cdae08314486c7b5b21ce5a778
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 06:08:18 -0400

    Match object filter INI parser

---
+++ Code/masm_dumps/iniParseObjectFilter_YAXPAVINI_PAX1PBX_Z_39865.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?iniParseObjectFilter@@YAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 00439865 size 5; Ghidra body FUN_0079F470
+_TEXT SEGMENT
+public ?iniParseObjectFilter@@YAXPAVINI@@PAX1PBX@Z
+?iniParseObjectFilter@@YAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 006h, 05ch, 036h, 000h
+?iniParseObjectFilter@@YAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
