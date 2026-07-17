8e798a63b2044f59140a2e49e08e89d3250be408
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:49:20 -0400

    Match dictionary pair clear

---
+++ Code/GameEngine/Source/Common/Dict_DictPair_clear.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?clear@DictPair@Dict@@QAEXXZ
+; Retail @ 0x000680D0 size 64
+_TEXT SEGMENT
+public ?clear@DictPair@Dict@@QAEXXZ
+?clear@DictPair@Dict@@QAEXXZ PROC
+    db 08Bh,001h,025h,0FFh,000h,000h,000h,083h
+    db 0F8h,004h,077h,00Eh,0FFh,024h,085h,0FCh
+    db 080h,046h,000h,0C7h,041h,004h,000h,000h
+    db 000h,000h,0C3h,083h,0C1h,004h,0E9h,04Dh
+    db 0F8h,081h,000h,083h,0C1h,004h,0E9h,0D5h
+    db 000h,082h,000h,090h,0E3h,080h,046h,000h
+    db 0E3h,080h,046h,000h,0E3h,080h,046h,000h
+    db 0EBh,080h,046h,000h,0F3h,080h,046h,000h
+?clear@DictPair@Dict@@QAEXXZ ENDP
+_TEXT ENDS
+END
