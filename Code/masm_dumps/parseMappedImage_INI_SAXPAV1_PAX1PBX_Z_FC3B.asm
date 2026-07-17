134acff22847b11ae3f50f58a680f286a2fc1008
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:15:15 -0400

    Match INI mapped image parser

---
+++ Code/masm_dumps/parseMappedImage_INI_SAXPAV1_PAX1PBX_Z_FC3B.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseMappedImage@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0040FC3B size 5; canonical body @ 004BAAA0
+_TEXT SEGMENT
+public ?parseMappedImage@INI@@SAXPAV1@PAX1PBX@Z
+?parseMappedImage@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 60h, 0aeh, 0ah, 00h
+?parseMappedImage@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
