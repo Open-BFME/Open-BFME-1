47df58785e55f91827743386f7ad6644d6f74c5b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:18:23 -0400

    Match quick match start button update

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/WOLQuickMatchMenu_UpdateStartButton.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+
+; ?UpdateStartButton@@YAXXZ
+; Exact 197 retail bytes @ 0x00505570; BFME keeps menu controls on an object.
+_TEXT SEGMENT
+public ?UpdateStartButton@@YAXXZ
+?UpdateStartButton@@YAXXZ PROC
+    db 051h,056h,08Bh,0F1h,08Bh,086h,044h,002h
+    db 000h,000h,085h,0C0h,00Fh,084h,09Eh,000h
+    db 000h,000h,08Bh,08Eh,06Ch,002h,000h,000h
+    db 085h,0C9h,00Fh,084h,090h,000h,000h,000h
+    db 08Bh,08Eh,084h,002h,000h,000h,085h,0C9h
+    db 00Fh,084h,082h,000h,000h,000h,08Dh,04Ch
+    db 024h,004h,051h,050h,0E8h,0B1h,061h,0B3h
+    db 0FFh,08Bh,054h,024h,00Ch,08Bh,086h,044h
+    db 002h,000h,000h,052h,050h,0E8h,0B7h,063h
+    db 0B3h,0FFh,08Bh,00Dh,0E8h,070h,02Fh,001h
+    db 083h,0C4h,010h,050h,0E8h,0CAh,038h,0B2h
+    db 0FFh,085h,0C0h,074h,010h,08Bh,08Eh,06Ch
+    db 002h,000h,000h,06Ah,001h,0E8h,021h,04Ch
+    db 0B4h,0FFh,05Eh,059h,0C3h,08Bh,08Eh,084h
+    db 002h,000h,000h,053h,057h,051h,0E8h,06Ch
+    db 0B2h,0B0h,0FFh,08Bh,0D8h,083h,0C4h,004h
+    db 033h,0FFh,085h,0DBh,07Eh,01Bh,08Bh,096h
+    db 084h,002h,000h,000h,06Ah,000h,057h,052h
+    db 0E8h,07Fh,03Ah,0B0h,0FFh,083h,0C4h,00Ch
+    db 085h,0C0h,075h,017h,047h,03Bh,0FBh,07Ch
+    db 0E5h,08Bh,08Eh,06Ch,002h,000h,000h,06Ah
+    db 000h,0E8h,0DDh,04Bh,0B4h,0FFh,05Fh,05Bh
+    db 05Eh,059h,0C3h,08Bh,08Eh,06Ch,002h,000h
+    db 000h,06Ah,001h,0E8h,0CBh,04Bh,0B4h,0FFh
+    db 05Fh,05Bh,05Eh,059h,0C3h
+?UpdateStartButton@@YAXXZ ENDP
+_TEXT ENDS
+END
