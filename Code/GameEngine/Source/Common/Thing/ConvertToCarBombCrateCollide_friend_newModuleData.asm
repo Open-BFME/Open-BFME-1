4f9bfb85ef022cd5ebf7da486652885e1ff3b433
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 18:13:04 -0400

    Match car bomb collide module factory

---
+++ Code/GameEngine/Source/Common/Thing/ConvertToCarBombCrateCollide_friend_newModuleData.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?friend_newModuleData@ConvertToCarBombCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+; Retail @ 0x00125FD0 size 129
+_TEXT SEGMENT
+public ?friend_newModuleData@ConvertToCarBombCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@ConvertToCarBombCrateCollide@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0ABh,018h,000h,001h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,056h
+    db 06Ah,060h,0E8h,041h,0BFh,075h,000h,08Bh
+    db 0F0h,083h,0C4h,004h,089h,074h,024h,008h
+    db 033h,0DBh,03Bh,0F3h,089h,05Ch,024h,014h
+    db 074h,01Fh,08Bh,0CEh,0E8h,0C1h,0E1h,0F1h
+    db 0FFh,0C7h,006h,040h,0E9h,008h,001h,089h
+    db 05Eh,054h,088h,05Eh,058h,088h,05Eh,059h
+    db 0C7h,046h,05Ch,00Ah,000h,000h,000h,0EBh
+    db 002h,033h,0F6h,08Bh,04Ch,024h,01Ch,03Bh
+    db 0CBh,0C7h,044h,024h,014h,0FFh,0FFh,0FFh
+    db 0FFh,074h,00Bh,068h,051h,027h,041h,000h
+    db 056h,0E8h,0F2h,0C0h,072h,000h,08Bh,04Ch
+    db 024h,00Ch,08Bh,0C6h,05Eh,05Bh,064h,089h
+    db 00Dh,000h,000h,000h,000h,083h,0C4h,010h
+    db 0C3h
+?friend_newModuleData@ConvertToCarBombCrateCollide@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
