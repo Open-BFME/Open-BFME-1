8d3a40a9b274c8b69944781b3b6fb90f0a75f05b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:16:31 -0400

    Match INI translated label parser

---
+++ Code/masm_dumps/parseAndTranslateLabel_INI_SAXPAV1_PAX1PBX_Z_32C45.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseAndTranslateLabel@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00432C45 size 5; canonical body @ 004BA9B0
+_TEXT SEGMENT
+public ?parseAndTranslateLabel@INI@@SAXPAV1@PAX1PBX@Z
+?parseAndTranslateLabel@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 66h, 7dh, 08h, 00h
+?parseAndTranslateLabel@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
