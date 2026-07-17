b700f0d6a8ad3a48fc99909a01dc0ade80f81745
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:35:15 -0400

    Match INI upgrade template parser

---
+++ Code/masm_dumps/parseUpgradeTemplate_INI_SAXPAV1_PAX1PBX_Z_4A39A.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseUpgradeTemplate@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0044A39A size 5; canonical body @ 004BB040
+_TEXT SEGMENT
+public ?parseUpgradeTemplate@INI@@SAXPAV1@PAX1PBX@Z
+?parseUpgradeTemplate@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0a1h, 0ch, 07h, 00h
+?parseUpgradeTemplate@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
