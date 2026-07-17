51e1661650f17bb3d937a9765d83ac42a3c694ee
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:54:10 -0400

    Match locomotor set destructor

---
+++ Code/GameEngine/Source/GameLogic/Object/LocomotorSet_destructor.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ??1LocomotorSet@@QAE@XZ
+; Retail @ 0x009F2800 size 115
+_TEXT SEGMENT
+public ??1LocomotorSet@@QAE@XZ
+??1LocomotorSet@@QAE@XZ PROC
+    db 06Ah,0FFh,068h,03Fh,01Eh,006h,001h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,008h
+    db 056h,08Bh,0F1h,057h,089h,074h,024h,00Ch
+    db 0C7h,006h,0F8h,057h,014h,001h,0C7h,046h
+    db 008h,0E8h,057h,014h,001h,08Bh,07Eh,00Ch
+    db 085h,0FFh,0C7h,044h,024h,018h,001h,000h
+    db 000h,000h,074h,010h,08Bh,0CFh,0E8h,02Dh
+    db 031h,000h,000h,057h,0E8h,067h,0F6h,0E8h
+    db 0FFh,083h,0C4h,004h,08Bh,0CEh,0C7h,046h
+    db 008h,044h,037h,007h,001h,0C7h,044h,024h
+    db 018h,0FFh,0FFh,0FFh,0FFh,0E8h,0DEh,0F1h
+    db 0FAh,0FFh,08Bh,04Ch,024h,010h,05Fh,05Eh
+    db 064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,014h,0C3h
+??1LocomotorSet@@QAE@XZ ENDP
+_TEXT ENDS
+END
