1729e4ae070230e0adf5a760a21010912d582da0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 16:43:02 -0400

    Match WOL quick match shutdown

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/WOLQuickMatchMenu_Shutdown.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ?WOLQuickMatchMenuShutdown@@YAXPAVWindowLayout@@PAX@Z
+; Exact 147 retail bytes @ 0x00507E10; BFME callback register convention
+_TEXT SEGMENT
+public ?WOLQuickMatchMenuShutdown@@YAXPAVWindowLayout@@PAX@Z
+?WOLQuickMatchMenuShutdown@@YAXPAVWindowLayout@@PAX@Z PROC
+    db 08Bh,015h,020h,048h,02Fh,001h,08Bh,092h
+    db 094h,002h,000h,000h,056h,08Bh,0F1h,08Bh
+    db 00Dh,094h,071h,02Fh,001h,08Bh,001h,052h
+    db 0FFh,090h,0E8h,000h,000h,000h,08Bh,00Dh
+    db 024h,0D5h,02Eh,001h,08Bh,001h,0FFh,050h
+    db 038h,084h,0C0h,075h,00Bh,08Bh,00Dh,020h
+    db 048h,02Fh,001h,0E8h,0C5h,09Fh,0B2h,0FFh
+    db 08Bh,04Ch,024h,008h,033h,0C0h,0A3h,020h
+    db 048h,02Fh,001h,038h,001h,0A3h,010h,048h
+    db 02Fh,001h,0A3h,00Ch,048h,02Fh,001h,0C6h
+    db 005h,000h,048h,02Fh,001h,001h,074h,00Bh
+    db 08Bh,0CEh,0E8h,004h,010h,0B4h,0FFh,05Eh
+    db 0C2h,004h,000h,08Bh,00Dh,058h,04Bh,02Fh
+    db 001h,0E8h,070h,05Eh,0B3h,0FFh,051h,089h
+    db 064h,024h,00Ch,08Bh,0CCh,068h,0C0h,042h
+    db 010h,001h,0E8h,031h,00Dh,038h,000h,08Bh
+    db 00Dh,030h,033h,02Fh,001h,0E8h,092h,0C3h
+    db 0B0h,0FFh,0E8h,0EDh,0FCh,0B2h,0FFh,05Eh
+    db 0C2h,004h,000h
+?WOLQuickMatchMenuShutdown@@YAXPAVWindowLayout@@PAX@Z ENDP
+_TEXT ENDS
+END
