7f354baa9e3d1677ee54efdfea52c4f5e98df3b9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 06:02:13 -0400

    Match BoneFX particle parser

---
+++ Code/masm_dumps/parseParticleSystem_BoneFXUpdateModuleData_SAXPAVINI_PAX1PBX_Z_16A04.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseParticleSystem@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 00416A04 size 5; Ghidra body FUN_00687E40
+_TEXT SEGMENT
+public ?parseParticleSystem@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseParticleSystem@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 037h, 014h, 027h, 000h
+?parseParticleSystem@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
