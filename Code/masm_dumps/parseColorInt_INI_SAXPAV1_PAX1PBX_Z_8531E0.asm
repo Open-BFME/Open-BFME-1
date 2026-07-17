704ffec207bce75827fb08d13b4c57675812f97f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:14:19 -0400

    Match INI packed color parser

---
+++ Code/masm_dumps/parseColorInt_INI_SAXPAV1_PAX1PBX_Z_8531E0.asm
@@ -0,0 +1,37 @@
+.386
+.model flat
+
+; ?parseColorInt@INI@@SAXPAV1@PAX1PBX@Z
+; Retail @ 00C531E0 size 409
+_TEXT SEGMENT
+public ?parseColorInt@INI@@SAXPAV1@PAX1PBX@Z
+?parseColorInt@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 83h, 0ech, 28h, 53h, 8bh, 5ch, 24h, 30h, 55h, 8bh, 2dh, 0d8h, 94h, 35h, 01h, 56h
+    db 57h, 0c7h, 44h, 24h, 18h, 28h, 2dh, 0fh, 01h, 0c7h, 44h, 24h, 1ch, 24h, 2dh, 0fh
+    db 01h, 0c7h, 44h, 24h, 20h, 20h, 2dh, 0fh, 01h, 0c7h, 44h, 24h, 24h, 50h, 0beh, 12h
+    db 01h, 33h, 0ffh, 8bh, 83h, 1ch, 04h, 00h, 00h, 85h, 0c0h, 75h, 06h, 8bh, 83h, 14h
+    db 04h, 00h, 00h, 50h, 6ah, 00h, 0ffh, 0d5h, 83h, 0c4h, 08h, 85h, 0c0h, 75h, 13h, 83h
+    db 0ffh, 03h, 0fh, 8ch, 0a0h, 00h, 00h, 00h, 0c7h, 44h, 0bch, 28h, 0ffh, 00h, 00h, 00h
+    db 0ebh, 44h, 8bh, 4ch, 0bch, 18h, 51h, 50h, 0ffh, 15h, 3ch, 93h, 35h, 01h, 83h, 0c4h
+    db 08h, 85h, 0c0h, 0fh, 85h, 0a7h, 00h, 00h, 00h, 8bh, 0b3h, 1ch, 04h, 00h, 00h, 85h
+    db 0f6h, 75h, 06h, 8bh, 0b3h, 14h, 04h, 00h, 00h, 56h, 6ah, 00h, 0ffh, 0d5h, 83h, 0c4h
+    db 08h, 85h, 0c0h, 0fh, 84h, 0afh, 00h, 00h, 00h, 50h, 0e8h, 0a1h, 0f3h, 0ffh, 0ffh, 83h
+    db 0c4h, 04h, 89h, 44h, 0bch, 28h, 8bh, 44h, 0bch, 28h, 85h, 0c0h, 0fh, 8ch, 0bah, 00h
+    db 00h, 00h, 3dh, 0ffh, 00h, 00h, 00h, 0fh, 8fh, 0afh, 00h, 00h, 00h, 47h, 83h, 0ffh
+    db 04h, 0fh, 8ch, 6ch, 0ffh, 0ffh, 0ffh, 0fh, 0b6h, 54h, 24h, 34h, 0fh, 0b6h, 44h, 24h
+    db 28h, 0fh, 0b6h, 4ch, 24h, 2ch, 0c1h, 0e2h, 08h, 0bh, 0d0h, 0fh, 0b6h, 44h, 24h, 30h
+    db 0c1h, 0e2h, 08h, 0bh, 0d1h, 8bh, 4ch, 24h, 44h, 5fh, 5eh, 0c1h, 0e2h, 08h, 0bh, 0d0h
+    db 5dh, 89h, 11h, 5bh, 83h, 0c4h, 28h, 0c3h, 8bh, 54h, 0bch, 18h, 52h, 68h, 88h, 08h
+    db 13h, 01h, 8dh, 44h, 24h, 18h, 6ah, 03h, 50h, 0e8h, 12h, 0d3h, 0ffh, 0ffh, 83h, 0c4h
+    db 10h, 68h, 30h, 0fch, 1dh, 01h, 8dh, 4ch, 24h, 14h, 51h, 0e8h, 00h, 3ah, 1ah, 00h
+    db 8bh, 54h, 0bch, 18h, 52h, 68h, 78h, 08h, 13h, 01h, 8dh, 44h, 24h, 18h, 6ah, 03h
+    db 50h, 0e8h, 0eah, 0d2h, 0ffh, 0ffh, 83h, 0c4h, 10h, 68h, 30h, 0fch, 1dh, 01h, 8dh, 4ch
+    db 24h, 14h, 51h, 0e8h, 0d8h, 39h, 1ah, 00h, 56h, 68h, 50h, 03h, 13h, 01h, 8dh, 54h
+    db 24h, 18h, 6ah, 03h, 52h, 0e8h, 0c6h, 0d2h, 0ffh, 0ffh, 83h, 0c4h, 10h, 68h, 30h, 0fch
+    db 1dh, 01h, 8dh, 44h, 24h, 14h, 50h, 0e8h, 0b4h, 39h, 1ah, 00h, 8bh, 4ch, 0bch, 28h
+    db 8bh, 54h, 0bch, 18h, 51h, 52h, 68h, 0f0h, 2ch, 0fh, 01h, 8dh, 44h, 24h, 1ch, 6ah
+    db 03h, 50h, 0e8h, 99h, 0d2h, 0ffh, 0ffh, 83h, 0c4h, 14h, 68h, 30h, 0fch, 1dh, 01h, 8dh
+    db 4ch, 24h, 14h, 51h, 0e8h, 87h, 39h, 1ah, 00h
+?parseColorInt@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
