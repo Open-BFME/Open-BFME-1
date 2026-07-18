78431f9c63de14ebb870723c03cea58045dd88bc
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 07:24:01 -0400

    Match MetaMap record lookup

---
+++ Code/masm_dumps/MetaEvent_MetaMap_getMetaMapRec.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?getMetaMapRec@MetaMap@@IAEPAVMetaMapRec@@W4Type@GameMessage@@@Z
+; Exact 111 retail bytes @ 0x009B7110; uses the non-polymorphic 0x24-byte MetaMapRec layout.
+_TEXT SEGMENT
+public ?getMetaMapRec@MetaMap@@IAEPAVMetaMapRec@@W4Type@GameMessage@@@Z
+?getMetaMapRec@MetaMap@@IAEPAVMetaMapRec@@W4Type@GameMessage@@@Z PROC
+    db 53h,55h,8Bh,6Ch,24h,0Ch,8Bh,0D9h
+    db 8Bh,43h,08h,57h,33h,0FFh,3Bh,0C7h
+    db 74h,0Bh,39h,68h,04h,74h,52h,8Bh
+    db 00h,3Bh,0C7h,75h,0F5h,56h,6Ah,24h
+    db 0E8h,0FBh,0ADh,2Ch,00h,83h,0C4h,04h
+    db 3Bh,0C7h,74h,0Ah,89h,78h,1Ch,89h
+    db 78h,20h,8Bh,0F0h,0EBh,02h,33h,0F6h
+    db 8Dh,4Eh,1Ch,89h,6Eh,04h,89h,7Eh
+    db 08h,89h,7Eh,0Ch,89h,7Eh,10h,89h
+    db 7Eh,14h,0C7h,46h,18h,06h,00h,00h
+    db 00h,0E8h,6Ah,10h,2Dh,00h,8Dh,4Eh
+    db 20h,0E8h,62h,10h,2Dh,00h,8Bh,43h
+    db 08h,89h,06h,89h,73h,08h,8Bh,0C6h
+    db 5Eh,5Fh,5Dh,5Bh,0C2h,04h,00h
+?getMetaMapRec@MetaMap@@IAEPAVMetaMapRec@@W4Type@GameMessage@@@Z ENDP
+_TEXT ENDS
+END
