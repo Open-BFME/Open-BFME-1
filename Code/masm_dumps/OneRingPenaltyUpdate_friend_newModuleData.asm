445199a384201aff35aef0f1065aa975648bf858
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 12:39:31 -0400

    Match OneRingPenaltyUpdate module-data factory

---
+++ Code/masm_dumps/OneRingPenaltyUpdate_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@OneRingPenaltyUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ 0x0051A460; allocates, constructs, and parses OneRingPenaltyUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@OneRingPenaltyUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@OneRingPenaltyUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,02Bh,0F6h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,024h,0E8h,0B2h,07Ah,076h,000h,083h,0C4h
+    db 004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h,000h,074h
+    db 00Bh,08Bh,0C8h,0E8h,034h,0A2h,0F0h,0FFh,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch
+    db 024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,07Ch
+    db 077h,043h,000h,056h,0E8h,077h,07Ch,073h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh
+    db 064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@OneRingPenaltyUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
