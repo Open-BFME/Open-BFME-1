a74feadc134103665a654b82719553fc5ea367c0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 14:50:29 -0400

    Match W3D rope draw data factory

---
+++ Code/masm_dumps/W3DRopeDraw_friend_newModuleData.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?friend_newModuleData@W3DRopeDraw@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 47 retail bytes @ RVA 0x00113BE0; W3D module registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@W3DRopeDraw@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@W3DRopeDraw@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h,06Ah,008h,0E8h,048h,0E3h,076h,000h,083h,0C4h,004h,085h,0C0h,074h,00Ah,0C7h
+    db 000h,0B8h,096h,008h,001h,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch,024h,008h,085h
+    db 0C9h,074h,008h,06Ah,000h,056h,0E8h,095h,0E4h,073h,000h,08Bh,0C6h,05Eh,0C3h
+?friend_newModuleData@W3DRopeDraw@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
