fece5cba3b0935cba7f5df68350cb0b47e46ab0e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 06:53:27 -0400

    Match true MetaMapRec destructor

---
+++ Code/masm_dumps/MetaEvent_MetaMapRec_retail_destructor.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1MetaMapRec@@QAE@XZ
+; Exact 77 retail bytes @ 0x009B6FC0; retail MetaMapRec is non-polymorphic and 0x24 bytes.
+_TEXT SEGMENT
+public ??1MetaMapRec@@QAE@XZ
+??1MetaMapRec@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,0BBh,91h,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,89h,74h,24h,04h,8Dh,4Eh,20h
+    db 0C7h,44h,24h,10h,00h,00h,00h,00h
+    db 0E8h,0E3h,11h,2Dh,00h,8Dh,4Eh,1Ch
+    db 0C7h,44h,24h,10h,0FFh,0FFh,0FFh,0FFh
+    db 0E8h,0D3h,11h,2Dh,00h,8Bh,4Ch,24h
+    db 08h,5Eh,64h,89h,0Dh,00h,00h,00h
+    db 00h,83h,0C4h,10h,0C3h
+??1MetaMapRec@@QAE@XZ ENDP
+_TEXT ENDS
+END
