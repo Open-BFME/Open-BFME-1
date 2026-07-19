cffc30aedfe55401006fe6a2fc85eca9ce9fbe36
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 08:33:17 -0400

    Correct LaserUpdate data owner

---
+++ Code/masm_dumps/LaserUpdate_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@LaserUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ 0x00118DF0; allocates and initializes LaserUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@LaserUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@LaserUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 00Bh, 0F2h, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 06Ah, 01Ch, 0E8h, 022h, 091h, 076h, 000h, 083h, 0C4h
+    db 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 074h
+    db 00Bh, 08Bh, 0C8h, 0E8h, 041h, 052h, 0F1h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch
+    db 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h, 00Bh, 068h, 092h
+    db 031h, 041h, 000h, 056h, 0E8h, 0E7h, 092h, 073h, 000h, 08Bh, 04Ch, 024h, 008h, 08Bh, 0C6h, 05Eh, 064h
+    db 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@LaserUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
