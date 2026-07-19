fe96f56a88c4f1e25e2994ed029e50b1392fd62e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 17:03:40 -0400

    Match SidesList constructor

---
+++ Code/masm_dumps/SidesList_ctor.asm
@@ -0,0 +1,34 @@
+.386
+.model flat
+
+; ??0SidesList@@QAE@XZ
+; Exact 365 retail bytes @ 0x0019EA80; GameEngine init calls thunk 0x00032EC5.
+_TEXT SEGMENT
+public ??0SidesList@@QAE@XZ
+??0SidesList@@QAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0D6h, 07Bh, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 053h, 056h, 08Bh, 0F1h, 057h, 089h, 074h, 024h, 00Ch, 0E8h
+    db 08Ch, 02Fh, 080h, 000h, 033h, 0DBh, 089h, 05Ch, 024h, 018h, 0C7h, 046h, 008h, 044h, 037h, 007h
+    db 001h, 0C7h, 006h, 004h, 0C2h, 009h, 001h, 0C7h, 046h, 008h, 0F0h, 0C1h, 009h, 001h, 06Ah, 00Ch
+    db 0C6h, 044h, 024h, 01Ch, 001h, 089h, 05Eh, 00Ch, 0E8h, 073h, 0FAh, 068h, 000h, 089h, 000h, 089h
+    db 040h, 004h, 089h, 046h, 00Ch, 06Ah, 020h, 0C6h, 044h, 024h, 020h, 002h, 089h, 05Eh, 010h, 0E8h
+    db 05Ch, 0FAh, 068h, 000h, 089h, 046h, 010h, 089h, 05Eh, 014h, 088h, 018h, 08Bh, 046h, 010h, 089h
+    db 058h, 004h, 08Bh, 046h, 010h, 089h, 040h, 008h, 08Bh, 046h, 010h, 089h, 040h, 00Ch, 06Ah, 020h
+    db 0C6h, 044h, 024h, 024h, 003h, 089h, 05Eh, 01Ch, 0E8h, 033h, 0FAh, 068h, 000h, 089h, 046h, 01Ch
+    db 089h, 05Eh, 020h, 088h, 018h, 08Bh, 04Eh, 01Ch, 089h, 059h, 004h, 08Bh, 046h, 01Ch, 089h, 040h
+    db 008h, 08Bh, 046h, 01Ch, 083h, 0C4h, 00Ch, 089h, 040h, 00Ch, 068h, 090h, 055h, 042h, 000h, 068h
+    db 08Eh, 069h, 044h, 000h, 06Ah, 020h, 06Ah, 018h, 08Dh, 056h, 02Ch, 052h, 0C6h, 044h, 024h, 02Ch
+    db 004h, 089h, 05Eh, 028h, 0E8h, 09Bh, 083h, 085h, 000h, 068h, 090h, 055h, 042h, 000h, 068h, 08Eh
+    db 069h, 044h, 000h, 06Ah, 020h, 06Ah, 018h, 08Dh, 086h, 030h, 003h, 000h, 000h, 050h, 0C6h, 044h
+    db 024h, 02Ch, 005h, 089h, 09Eh, 02Ch, 003h, 000h, 000h, 0E8h, 076h, 083h, 085h, 000h, 08Dh, 0BEh
+    db 030h, 006h, 000h, 000h, 08Bh, 0CFh, 0C6h, 044h, 024h, 018h, 006h, 0E8h, 012h, 09Ch, 0E6h, 0FFh
+    db 08Dh, 09Eh, 04Ch, 006h, 000h, 000h, 08Bh, 0CBh, 0C6h, 044h, 024h, 018h, 007h, 0E8h, 000h, 09Ch
+    db 0E6h, 0FFh, 0C6h, 044h, 024h, 018h, 008h, 068h, 039h, 039h, 042h, 000h, 068h, 02Ah, 09Ch, 043h
+    db 000h, 06Ah, 020h, 06Ah, 01Ch, 08Dh, 08Eh, 06Ch, 006h, 000h, 000h, 051h, 0C6h, 086h, 068h, 006h
+    db 000h, 000h, 001h, 0E8h, 02Ch, 083h, 085h, 000h, 08Bh, 0CEh, 0C6h, 044h, 024h, 018h, 009h, 0E8h
+    db 0E8h, 08Fh, 0E7h, 0FFh, 08Bh, 0CFh, 0E8h, 09Ah, 0F3h, 0E8h, 0FFh, 08Bh, 0CBh, 0E8h, 093h, 0F3h
+    db 0E8h, 0FFh, 08Bh, 04Ch, 024h, 010h, 05Fh, 0C6h, 086h, 068h, 006h, 000h, 000h, 001h, 08Bh, 0C6h
+    db 05Eh, 05Bh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??0SidesList@@QAE@XZ ENDP
+_TEXT ENDS
+END
