05dd2f6f755e9c4c71e21b566adf409a5db7834a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 12:13:06 -0400

    Match LevelUpUpgrade data factory

---
+++ Code/masm_dumps/LevelUpUpgrade_friend_newModuleData.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?friend_newModuleData@LevelUpUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 72 retail bytes @ 0x0012B320; registration ordinal 0x9C proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@LevelUpUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@LevelUpUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 078h, 0E8h, 008h, 06Ch, 075h, 000h, 08Bh, 0F0h, 083h, 0C4h, 004h, 085h, 0F6h, 074h
+    db 01Eh, 08Dh, 04Eh, 008h, 0E8h, 06Dh, 03Dh, 0EEh, 0FFh, 0C7h, 006h, 0E8h, 0F3h, 008h, 001h, 0C7h
+    db 046h, 070h, 000h, 000h, 000h, 000h, 0C7h, 046h, 074h, 000h, 000h, 000h, 000h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 008h, 085h, 0C9h, 074h, 00Bh, 068h, 0F8h, 0CFh, 041h, 000h, 056h, 0E8h
+    db 0CCh, 06Dh, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@LevelUpUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
