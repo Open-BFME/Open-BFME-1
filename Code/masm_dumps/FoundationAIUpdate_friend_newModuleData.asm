c61467631f85464a1cc6a4f2818425ff09b218f1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 11:17:15 -0400

    Match FoundationAIUpdate data factory

---
+++ Code/masm_dumps/FoundationAIUpdate_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@FoundationAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 57 retail bytes @ 0x00122E30; registration ordinal 0x8E proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@FoundationAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@FoundationAIUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 00Ch, 0E8h, 0F8h, 0F0h, 075h, 000h, 083h, 0C4h, 004h, 085h, 0C0h, 074h, 011h, 0C7h
+    db 000h, 0A0h, 0AEh, 008h, 001h, 0C7h, 040h, 008h, 000h, 000h, 000h, 000h, 08Bh, 0F0h, 0EBh, 002h
+    db 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 085h, 0C9h, 074h, 00Bh, 068h, 0E6h, 031h, 042h, 000h, 056h
+    db 0E8h, 0CBh, 0F2h, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@FoundationAIUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
