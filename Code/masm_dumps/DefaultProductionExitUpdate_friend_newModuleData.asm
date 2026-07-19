382d3641c6a76cd004d2093ea3695c1d737021a4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 10:29:45 -0400

    Match DefaultProductionExitUpdate data factory

---
+++ Code/masm_dumps/DefaultProductionExitUpdate_friend_newModuleData.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?friend_newModuleData@DefaultProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 70 retail bytes @ 0x00123910; registration ordinal 0x75 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@DefaultProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@DefaultProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 020h, 0E8h, 018h, 0E6h, 075h, 000h, 033h, 0D2h, 083h, 0C4h, 004h, 03Bh, 0C2h, 074h
+    db 01Ch, 0C7h, 000h, 090h, 0C9h, 008h, 001h, 089h, 050h, 008h, 089h, 050h, 00Ch, 089h, 050h, 010h
+    db 089h, 050h, 014h, 089h, 050h, 018h, 089h, 050h, 01Ch, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh
+    db 04Ch, 024h, 008h, 03Bh, 0CAh, 074h, 00Bh, 068h, 087h, 036h, 042h, 000h, 056h, 0E8h, 0DEh, 0E7h
+    db 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@DefaultProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
