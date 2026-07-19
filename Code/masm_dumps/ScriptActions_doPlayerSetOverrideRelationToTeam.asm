b19ed75c4dbb7154a69a0c041955cd4f97f9f149
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:22:29 -0400

    Match player-team relation override action

---
+++ Code/masm_dumps/ScriptActions_doPlayerSetOverrideRelationToTeam.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?doPlayerSetOverrideRelationToTeam@ScriptActions@@IAEXABVAsciiString@@0H@Z
+; Exact 102 retail bytes @ 0x002F84C0; executeAction dispatch thunk proves ownership.
+_TEXT SEGMENT
+public ?doPlayerSetOverrideRelationToTeam@ScriptActions@@IAEXABVAsciiString@@0H@Z
+?doPlayerSetOverrideRelationToTeam@ScriptActions@@IAEXABVAsciiString@@0H@Z PROC
+    db 08Bh, 044h, 024h, 004h, 08Bh, 000h, 085h, 0C0h, 056h, 074h, 005h, 083h, 0C0h, 008h, 0EBh, 005h
+    db 0B8h, 08Bh, 038h, 007h, 001h, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 050h, 0E8h, 0F6h, 028h, 0D4h
+    db 0FFh, 08Bh, 00Dh, 048h, 0D7h, 02Eh, 001h, 050h, 0E8h, 099h, 070h, 0D3h, 0FFh, 08Bh, 054h, 024h
+    db 00Ch, 06Ah, 000h, 051h, 089h, 064h, 024h, 010h, 08Bh, 0CCh, 052h, 08Bh, 0F0h, 0E8h, 05Eh, 0F6h
+    db 058h, 000h, 08Bh, 00Dh, 06Ch, 007h, 02Fh, 001h, 08Bh, 001h, 0FFh, 050h, 044h, 085h, 0F6h, 074h
+    db 011h, 085h, 0C0h, 074h, 00Dh, 08Bh, 04Ch, 024h, 010h, 051h, 050h, 08Bh, 0CEh, 0E8h, 0B4h, 0C2h
+    db 0D1h, 0FFh, 05Eh, 0C2h, 00Ch, 000h
+?doPlayerSetOverrideRelationToTeam@ScriptActions@@IAEXABVAsciiString@@0H@Z ENDP
+_TEXT ENDS
+END
