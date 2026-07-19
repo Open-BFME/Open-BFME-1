abf7fb298fde5f0d99fdb64f3ea0ccf5229e4b5a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 10:58:02 -0400

    Match HijackerUpdate data factory

---
+++ Code/masm_dumps/HijackerUpdate_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@HijackerUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 58 retail bytes @ 0x00126D50; registration ordinal 0x84 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@HijackerUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@HijackerUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 010h, 0E8h, 0D8h, 0B1h, 075h, 000h, 033h, 0D2h, 083h, 0C4h, 004h, 03Bh, 0C2h, 074h
+    db 010h, 0C7h, 000h, 0E0h, 0E7h, 008h, 001h, 089h, 050h, 008h, 089h, 050h, 00Ch, 08Bh, 0F0h, 0EBh
+    db 002h, 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 03Bh, 0CAh, 074h, 00Bh, 068h, 0DAh, 00Bh, 042h, 000h
+    db 056h, 0E8h, 0AAh, 0B3h, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@HijackerUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
