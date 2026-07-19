85cf60ffec889686a65013b2efb2db73ebcbfc2b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:35:50 -0400

    Match Radar destructor

---
+++ Code/masm_dumps/Radar_destructor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??1Radar@@UAE@XZ
+; Exact 118 retail bytes @ 0x00107620; both Radar vtables and deleting wrappers prove ownership.
+_TEXT SEGMENT
+public ??1Radar@@UAE@XZ
+??1Radar@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0F5h, 0CEh, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 008h, 056h, 08Bh, 0F1h, 057h, 08Dh, 07Eh, 004h, 089h
+    db 074h, 024h, 00Ch, 0C7h, 006h, 0ACh, 088h, 008h, 001h, 0C7h, 007h, 080h, 088h, 008h, 001h, 0C7h
+    db 044h, 024h, 018h, 002h, 000h, 000h, 000h, 0E8h, 01Ah, 053h, 0F2h, 0FFh, 068h, 015h, 05Dh, 042h
+    db 000h, 06Ah, 040h, 06Ah, 050h, 08Dh, 046h, 028h, 050h, 0C6h, 044h, 024h, 028h, 001h, 0E8h, 003h
+    db 0F7h, 08Eh, 000h, 08Bh, 0CFh, 0C6h, 044h, 024h, 018h, 000h, 0E8h, 0C1h, 0A3h, 089h, 000h, 08Bh
+    db 04Ch, 024h, 010h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Fh, 05Eh, 064h, 089h, 00Dh, 000h, 000h
+    db 000h, 000h, 083h, 0C4h, 014h, 0C3h
+??1Radar@@UAE@XZ ENDP
+_TEXT ENDS
+END
