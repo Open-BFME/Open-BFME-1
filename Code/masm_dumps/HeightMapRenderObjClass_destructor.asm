dfa20f74c5491ef95e82f87c37234149d61bf4a1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:27:06 -0400

    Match HeightMap render object destructor

---
+++ Code/masm_dumps/HeightMapRenderObjClass_destructor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??1HeightMapRenderObjClass@@UAE@XZ
+; Exact 116 retail bytes @ 0x006D33B0; constructor vtables and deleting-dtor chain prove ownership.
+_TEXT SEGMENT
+public ??1HeightMapRenderObjClass@@UAE@XZ
+??1HeightMapRenderObjClass@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 078h, 0A7h, 004h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 0C7h, 006h, 088h
+    db 0DCh, 011h, 001h, 0C7h, 046h, 008h, 080h, 0DCh, 011h, 001h, 0C7h, 086h, 0C8h, 000h, 000h, 000h
+    db 074h, 0DCh, 011h, 001h, 0C7h, 086h, 0CCh, 000h, 000h, 000h, 060h, 0DCh, 011h, 001h, 0C7h, 044h
+    db 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 0D9h, 0F1h, 094h, 0FFh, 08Bh, 0CEh, 0C7h, 005h, 008h
+    db 080h, 02Fh, 001h, 000h, 000h, 000h, 000h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h
+    db 038h, 0F4h, 092h, 0FFh, 08Bh, 04Ch, 024h, 008h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h
+    db 083h, 0C4h, 010h, 0C3h
+??1HeightMapRenderObjClass@@UAE@XZ ENDP
+_TEXT ENDS
+END
