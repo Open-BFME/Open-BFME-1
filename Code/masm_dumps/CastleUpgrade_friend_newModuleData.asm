5ea31bf176cf4620a81d4be0f8697e9d3d1b122c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 12:20:22 -0400

    Match CastleUpgrade data factory

---
+++ Code/masm_dumps/CastleUpgrade_friend_newModuleData.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?friend_newModuleData@CastleUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 65 retail bytes @ 0x0012BA80; registration ordinal 0xAB proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@CastleUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@CastleUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 074h, 0E8h, 0A8h, 064h, 075h, 000h, 08Bh, 0F0h, 083h, 0C4h, 004h, 085h, 0F6h, 074h
+    db 017h, 08Dh, 04Eh, 008h, 0E8h, 00Dh, 036h, 0EEh, 0FFh, 0C7h, 006h, 0F8h, 0F5h, 008h, 001h, 0C7h
+    db 046h, 070h, 000h, 000h, 000h, 000h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 085h, 0C9h
+    db 074h, 00Bh, 068h, 06Ch, 0D5h, 040h, 000h, 056h, 0E8h, 073h, 066h, 072h, 000h, 08Bh, 0C6h, 05Eh
+    db 0C3h
+?friend_newModuleData@CastleUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
