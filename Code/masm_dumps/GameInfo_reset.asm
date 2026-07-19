95c4b95e93e41b54b5f622328bb00f10a471228e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:13:13 -0400

    Match GameInfo reset

---
+++ Code/masm_dumps/GameInfo_reset.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?reset@GameInfo@@UAEXXZ
+; Exact 168 retail bytes @ 0x0061F440; vtable callers use thunk 0x00006CD0.
+_TEXT SEGMENT
+public ?reset@GameInfo@@UAEXXZ
+?reset@GameInfo@@UAEXXZ PROC
+    db 06Ah, 0FFh, 068h, 028h, 0F2h, 003h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 0A1h, 040h, 070h, 02Ah, 001h, 053h, 055h, 056h, 08Bh, 0F1h
+    db 057h, 033h, 0DBh, 068h, 05Ch, 076h, 011h, 001h, 08Dh, 04Ch, 024h, 014h, 089h, 046h, 008h, 088h
+    db 05Eh, 00Ch, 088h, 05Eh, 00Dh, 089h, 05Eh, 010h, 0E8h, 043h, 097h, 026h, 000h, 08Dh, 054h, 024h
+    db 010h, 08Dh, 04Eh, 03Ch, 052h, 089h, 05Ch, 024h, 020h, 0E8h, 002h, 088h, 026h, 000h, 083h, 0CFh
+    db 0FFh, 08Dh, 04Ch, 024h, 010h, 089h, 07Ch, 024h, 01Ch, 0E8h, 0A2h, 084h, 026h, 000h, 089h, 05Eh
+    db 048h, 0FFh, 015h, 00Ch, 08Eh, 035h, 001h, 089h, 07Eh, 050h, 089h, 046h, 04Ch, 088h, 05Eh, 00Eh
+    db 089h, 05Eh, 040h, 089h, 05Eh, 044h, 08Dh, 07Eh, 014h, 0BDh, 008h, 000h, 000h, 000h, 08Bh, 0FFh
+    db 08Bh, 007h, 03Bh, 0C3h, 074h, 006h, 08Bh, 0C8h, 08Bh, 001h, 0FFh, 010h, 083h, 0C7h, 004h, 04Dh
+    db 075h, 0EEh, 08Bh, 04Ch, 024h, 014h, 05Fh, 089h, 05Eh, 004h, 05Eh, 05Dh, 05Bh, 064h, 089h, 00Dh
+    db 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?reset@GameInfo@@UAEXXZ ENDP
+_TEXT ENDS
+END
