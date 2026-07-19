df1f1117618a8abc6d80ddb2a7ebe48e9f55cd2a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:45:40 -0400

    Match TeamFactory initTeam

---
+++ Code/masm_dumps/TeamFactory_initTeam.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?initTeam@TeamFactory@@QAEXABVAsciiString@@0_NPAVDict@@@Z
+; Exact 220 retail bytes @ 0x000F8170; the unique public thunk is at 0x00039E0F.
+_TEXT SEGMENT
+public ?initTeam@TeamFactory@@QAEXABVAsciiString@@0_NPAVDict@@@Z
+?initTeam@TeamFactory@@QAEXABVAsciiString@@0_NPAVDict@@@Z PROC
+    db 064h, 0A1h, 000h, 000h, 000h, 000h, 06Ah, 0FFh, 068h, 05Bh, 0C0h, 0FFh, 000h, 050h, 08Bh, 044h
+    db 024h, 010h, 064h, 089h, 025h, 000h, 000h, 000h, 000h, 053h, 055h, 08Bh, 06Ch, 024h, 01Ch, 056h
+    db 057h, 050h, 055h, 08Bh, 0F1h, 0E8h, 09Fh, 088h, 0F4h, 0FFh, 085h, 0C0h, 00Fh, 085h, 095h, 000h
+    db 000h, 000h, 08Bh, 045h, 000h, 085h, 0C0h, 074h, 005h, 083h, 0C0h, 008h, 0EBh, 005h, 0B8h, 08Bh
+    db 038h, 007h, 001h, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 050h, 0E8h, 018h, 02Ch, 0F4h, 0FFh, 08Bh
+    db 00Dh, 048h, 0D7h, 02Eh, 001h, 050h, 0E8h, 0BBh, 073h, 0F3h, 0FFh, 08Bh, 0F8h, 085h, 0FFh, 075h
+    db 009h, 08Bh, 00Dh, 048h, 0D7h, 02Eh, 001h, 08Bh, 079h, 014h, 068h, 078h, 002h, 000h, 000h, 0E8h
+    db 04Ch, 09Dh, 078h, 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 024h, 085h, 0C0h, 08Bh, 05Ch, 024h
+    db 028h, 0C7h, 044h, 024h, 018h, 000h, 000h, 000h, 000h, 074h, 01Fh, 08Bh, 056h, 018h, 08Bh, 04Ch
+    db 024h, 020h, 042h, 052h, 089h, 056h, 018h, 08Bh, 054h, 024h, 030h, 052h, 053h, 057h, 051h, 055h
+    db 056h, 08Bh, 0C8h, 0E8h, 07Bh, 0A3h, 0F2h, 0FFh, 0EBh, 002h, 033h, 0C0h, 084h, 0DBh, 0C7h, 044h
+    db 024h, 018h, 0FFh, 0FFh, 0FFh, 0FFh, 074h, 00Fh, 08Dh, 050h, 014h, 052h, 083h, 0C0h, 010h, 050h
+    db 08Bh, 0CEh, 0E8h, 025h, 04Fh, 0F1h, 0FFh, 08Bh, 04Ch, 024h, 010h, 05Fh, 05Eh, 05Dh, 064h, 089h
+    db 00Dh, 000h, 000h, 000h, 000h, 05Bh, 083h, 0C4h, 00Ch, 0C2h, 010h, 000h
+?initTeam@TeamFactory@@QAEXABVAsciiString@@0_NPAVDict@@@Z ENDP
+_TEXT ENDS
+END
