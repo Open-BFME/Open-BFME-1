8a74b4914486698109be31db2c38734af7f73831
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:09:09 -0400

    Match named-unit sound action

---
+++ Code/masm_dumps/ScriptActions_doSoundPlayFromNamed.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?doSoundPlayFromNamed@ScriptActions@@IAEXABVAsciiString@@0@Z
+; Exact 165 retail bytes @ 0x002EE880; executeAction dispatch calls thunk 0x00045336.
+_TEXT SEGMENT
+public ?doSoundPlayFromNamed@ScriptActions@@IAEXABVAsciiString@@0@Z
+?doSoundPlayFromNamed@ScriptActions@@IAEXABVAsciiString@@0@Z PROC
+    db 064h, 0A1h, 000h, 000h, 000h, 000h, 08Bh, 00Dh, 06Ch, 007h, 02Fh, 001h, 08Bh, 054h, 024h, 008h
+    db 06Ah, 0FFh, 068h, 078h, 05Dh, 001h, 001h, 050h, 064h, 089h, 025h, 000h, 000h, 000h, 000h, 08Bh
+    db 001h, 083h, 0ECh, 070h, 056h, 052h, 0FFh, 050h, 068h, 08Bh, 0F0h, 085h, 0F6h, 074h, 064h, 08Bh
+    db 046h, 074h, 050h, 08Bh, 084h, 024h, 088h, 000h, 000h, 000h, 050h, 08Dh, 04Ch, 024h, 00Ch, 0E8h
+    db 058h, 0ADh, 0D4h, 0FFh, 06Ah, 001h, 08Dh, 04Ch, 024h, 008h, 0C7h, 084h, 024h, 080h, 000h, 000h
+    db 000h, 000h, 000h, 000h, 000h, 0E8h, 02Ch, 099h, 0D1h, 0FFh, 08Bh, 0CEh, 0E8h, 043h, 01Fh, 0D3h
+    db 0FFh, 085h, 0C0h, 074h, 00Dh, 08Bh, 040h, 024h, 050h, 08Dh, 04Ch, 024h, 008h, 0E8h, 096h, 0C3h
+    db 0D4h, 0FFh, 08Bh, 00Dh, 068h, 0D6h, 02Eh, 001h, 08Bh, 011h, 08Dh, 044h, 024h, 004h, 050h, 0FFh
+    db 052h, 044h, 08Dh, 04Ch, 024h, 004h, 0C7h, 044h, 024h, 07Ch, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 022h
+    db 086h, 0D3h, 0FFh, 08Bh, 04Ch, 024h, 074h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 07Ch, 0C2h, 008h, 000h
+?doSoundPlayFromNamed@ScriptActions@@IAEXABVAsciiString@@0@Z ENDP
+_TEXT ENDS
+END
