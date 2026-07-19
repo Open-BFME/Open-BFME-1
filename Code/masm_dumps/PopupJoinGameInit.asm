e4ba46e86cfb0fb16389d5b8d2d43689423fd401
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 17:16:55 -0400

    Match popup join game initializer

---
+++ Code/masm_dumps/PopupJoinGameInit.asm
@@ -0,0 +1,43 @@
+.386
+.model flat
+
+; ?PopupJoinGameInit@@YAXPAVWindowLayout@@PAX@Z
+; Exact 510 retail bytes @ 0x004D7300; FunctionLexicon thunk 0x0003E24D proves ownership.
+_TEXT SEGMENT
+public ?PopupJoinGameInit@@YAXPAVWindowLayout@@PAX@Z
+?PopupJoinGameInit@@YAXPAVWindowLayout@@PAX@Z PROC
+    db 06Ah, 0FFh, 068h, 0B8h, 0A8h, 002h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 008h, 056h, 057h, 068h, 008h, 001h, 010h, 001h, 08Dh
+    db 04Ch, 024h, 00Ch, 0E8h, 098h, 018h, 03Bh, 000h, 08Bh, 044h, 024h, 008h, 085h, 0C0h, 0C7h, 044h
+    db 024h, 018h, 000h, 000h, 000h, 000h, 074h, 005h, 083h, 0C0h, 008h, 0EBh, 005h, 0B8h, 08Bh, 038h
+    db 007h, 001h, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 050h, 0E8h, 089h, 03Ah, 0B6h, 0FFh, 083h, 0CFh
+    db 0FFh, 08Dh, 04Ch, 024h, 008h, 0A3h, 044h, 03Fh, 02Fh, 001h, 089h, 07Ch, 024h, 018h, 0E8h, 0DDh
+    db 005h, 03Bh, 000h, 08Bh, 015h, 044h, 03Fh, 02Fh, 001h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 08Bh
+    db 001h, 052h, 06Ah, 000h, 0FFh, 090h, 0DCh, 000h, 000h, 000h, 068h, 0D8h, 000h, 010h, 001h, 08Dh
+    db 04Ch, 024h, 00Ch, 0A3h, 050h, 03Fh, 02Fh, 001h, 0E8h, 033h, 018h, 03Bh, 000h, 08Bh, 044h, 024h
+    db 008h, 085h, 0C0h, 0C7h, 044h, 024h, 018h, 001h, 000h, 000h, 000h, 074h, 005h, 083h, 0C0h, 008h
+    db 0EBh, 005h, 0B8h, 08Bh, 038h, 007h, 001h, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 050h, 0E8h, 024h
+    db 03Ah, 0B6h, 0FFh, 08Dh, 04Ch, 024h, 008h, 0A3h, 048h, 03Fh, 02Fh, 001h, 089h, 07Ch, 024h, 018h
+    db 0E8h, 07Bh, 005h, 03Bh, 000h, 08Bh, 015h, 048h, 03Fh, 02Fh, 001h, 08Bh, 00Dh, 040h, 01Bh, 02Fh
+    db 001h, 08Bh, 001h, 052h, 08Bh, 015h, 050h, 03Fh, 02Fh, 001h, 052h, 0FFh, 090h, 0DCh, 000h, 000h
+    db 000h, 051h, 089h, 064h, 024h, 00Ch, 08Bh, 0CCh, 068h, 054h, 06Eh, 033h, 001h, 0A3h, 054h, 03Fh
+    db 02Fh, 001h, 0E8h, 009h, 010h, 03Bh, 000h, 0A1h, 054h, 03Fh, 02Fh, 001h, 050h, 0E8h, 072h, 054h
+    db 0B3h, 0FFh, 083h, 0C4h, 008h, 068h, 0ACh, 000h, 010h, 001h, 08Dh, 04Ch, 024h, 010h, 0E8h, 0ADh
+    db 017h, 03Bh, 000h, 08Bh, 044h, 024h, 00Ch, 085h, 0C0h, 0C7h, 044h, 024h, 018h, 002h, 000h, 000h
+    db 000h, 074h, 005h, 083h, 0C0h, 008h, 0EBh, 005h, 0B8h, 08Bh, 038h, 007h, 001h, 08Bh, 00Dh, 000h
+    db 0D6h, 02Eh, 001h, 050h, 0E8h, 09Eh, 039h, 0B6h, 0FFh, 08Dh, 04Ch, 024h, 00Ch, 08Bh, 0F0h, 089h
+    db 07Ch, 024h, 018h, 0E8h, 0F8h, 004h, 03Bh, 000h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 0A1h, 050h
+    db 03Fh, 02Fh, 001h, 08Bh, 011h, 056h, 050h, 0FFh, 092h, 0DCh, 000h, 000h, 000h, 051h, 089h, 064h
+    db 024h, 00Ch, 08Bh, 0CCh, 068h, 054h, 06Eh, 033h, 001h, 08Bh, 0F8h, 0E8h, 090h, 00Fh, 03Bh, 000h
+    db 057h, 0E8h, 0F9h, 04Ch, 0B5h, 0FFh, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 083h, 0C4h, 008h, 068h
+    db 084h, 000h, 010h, 001h, 0E8h, 04Eh, 039h, 0B6h, 0FFh, 08Bh, 00Dh, 094h, 071h, 02Fh, 001h, 08Bh
+    db 031h, 0A3h, 04Ch, 03Fh, 02Fh, 001h, 0FFh, 096h, 0D0h, 000h, 000h, 000h, 08Bh, 00Dh, 094h, 071h
+    db 02Fh, 001h, 050h, 0FFh, 096h, 09Ch, 000h, 000h, 000h, 085h, 0C0h, 074h, 018h, 051h, 08Bh, 0CCh
+    db 089h, 064h, 024h, 00Ch, 051h, 08Bh, 0C8h, 0E8h, 0F8h, 052h, 0B5h, 0FFh, 057h, 0E8h, 0ADh, 04Ch
+    db 0B5h, 0FFh, 083h, 0C4h, 008h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 0A1h, 050h, 03Fh, 02Fh, 001h
+    db 08Bh, 011h, 050h, 0FFh, 092h, 0B0h, 000h, 000h, 000h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 0A1h
+    db 050h, 03Fh, 02Fh, 001h, 08Bh, 011h, 050h, 0FFh, 092h, 0ECh, 000h, 000h, 000h, 08Bh, 04Ch, 024h
+    db 010h, 05Fh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 05Eh, 083h, 0C4h, 014h, 0C3h
+?PopupJoinGameInit@@YAXPAVWindowLayout@@PAX@Z ENDP
+_TEXT ENDS
+END
