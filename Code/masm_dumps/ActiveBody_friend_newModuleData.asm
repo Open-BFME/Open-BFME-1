6e47f3afe9261fe06752b79788ff291197f2ed24
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 12:58:23 -0400

    Correct ActiveBody data factory owner

---
+++ Code/masm_dumps/ActiveBody_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@ActiveBody@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ 0x0011F420; shared by Active/Highlander/Immortal/Oathbreaker registrations.
+_TEXT SEGMENT
+public ?friend_newModuleData@ActiveBody@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@ActiveBody@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 06Bh, 008h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 06Ah, 05Ch, 0E8h, 0F2h, 02Ah, 076h, 000h, 083h, 0C4h
+    db 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 074h
+    db 00Bh, 08Bh, 0C8h, 0E8h, 0C0h, 073h, 0F1h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch
+    db 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h, 00Bh, 068h, 055h
+    db 023h, 041h, 000h, 056h, 0E8h, 0B7h, 02Ch, 073h, 000h, 08Bh, 04Ch, 024h, 008h, 08Bh, 0C6h, 05Eh
+    db 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@ActiveBody@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
