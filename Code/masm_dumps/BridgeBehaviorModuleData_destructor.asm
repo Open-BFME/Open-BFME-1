386456c6aef85f0ca537e6757aab9a02222c85fb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:08:51 -0400

    Match BridgeBehavior module data destructor

---
+++ Code/masm_dumps/BridgeBehaviorModuleData_destructor.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1BridgeBehaviorModuleData@@UAE@XZ
+; Exact 147 retail bytes @ 0x001F5440; constructor vtable and deleting-dtor xrefs prove ownership.
+_TEXT SEGMENT
+public ??1BridgeBehaviorModuleData@@UAE@XZ
+??1BridgeBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0FEh, 0ACh, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 053h, 056h, 08Bh, 0F1h, 057h, 089h, 074h, 024h, 00Ch, 0C7h
+    db 006h, 0A8h, 027h, 00Ah, 001h, 08Dh, 05Eh, 010h, 08Bh, 0CBh, 0C7h, 044h, 024h, 018h, 002h, 000h
+    db 000h, 000h, 0E8h, 076h, 095h, 0E2h, 0FFh, 08Dh, 07Eh, 014h, 08Bh, 0CFh, 0E8h, 078h, 0E0h, 0E0h
+    db 0FFh, 08Bh, 0CFh, 0C6h, 044h, 024h, 018h, 001h, 0E8h, 06Ch, 0E0h, 0E0h, 0FFh, 08Bh, 03Fh, 085h
+    db 0FFh, 074h, 00Bh, 06Ah, 014h, 057h, 0E8h, 055h, 091h, 063h, 000h, 083h, 0C4h, 008h, 08Bh, 0CBh
+    db 0C6h, 044h, 024h, 018h, 000h, 0E8h, 043h, 095h, 0E2h, 0FFh, 08Bh, 01Bh, 085h, 0DBh, 074h, 00Bh
+    db 06Ah, 014h, 053h, 0E8h, 038h, 091h, 063h, 000h, 083h, 0C4h, 008h, 08Bh, 04Ch, 024h, 010h, 05Fh
+    db 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 05Bh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1BridgeBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
