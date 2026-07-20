41164301ebf4553f0db62f05866967413d4e6824
dginovker <danielginovker@gmail.com>
2026-07-19 23:01:40 -0700

    Match ParticleUplinkCannonUpdate ModuleData destructor
    Exact retail MASM at 0x2A26C0/115; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/ParticleUplinkCannonUpdateModuleData_destructor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??1ParticleUplinkCannonUpdateModuleData@@UAE@XZ
+; Exact 115 retail bytes @ 0x002A26C0; factory 0x11CC80 ctor 0x2A2520 vtable 0x10C1AC8 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1ParticleUplinkCannonUpdateModuleData@@UAE@XZ
+??1ParticleUplinkCannonUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 07Fh, 026h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 08Eh, 0B0h
+    db 000h, 000h, 000h, 0C7h, 044h, 024h, 010h, 003h, 000h, 000h, 000h, 0E8h, 050h, 052h, 05Eh, 000h
+    db 08Dh, 08Eh, 0ACh, 000h, 000h, 000h, 0C6h, 044h, 024h, 010h, 002h, 0E8h, 040h, 052h, 05Eh, 000h
+    db 08Dh, 08Eh, 0A0h, 000h, 000h, 000h, 0C6h, 044h, 024h, 010h, 001h, 0E8h, 033h, 0D6h, 0D7h, 0FFh
+    db 08Dh, 04Eh, 008h, 0C6h, 044h, 024h, 010h, 000h, 0E8h, 0E4h, 07Ch, 0D7h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1ParticleUplinkCannonUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
