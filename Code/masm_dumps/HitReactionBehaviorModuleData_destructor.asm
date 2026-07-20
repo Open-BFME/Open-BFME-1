63a851fb08a5b1da2d6ba9d86df1e997b59afde0
dginovker <danielginovker@gmail.com>
2026-07-19 22:38:10 -0700

    Match HitReactionBehavior ModuleData destructor
    Exact retail MASM at 0x291EB0/80; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/HitReactionBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1HitReactionBehaviorModuleData@@UAE@XZ
+; Exact 80 retail bytes @ 0x00291EB0; factory 0x11D000 ctor 0x291950 vtable 0x10BE5A0 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1HitReactionBehaviorModuleData@@UAE@XZ
+??1HitReactionBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0C3h, 019h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 01Ch
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 024h, 085h, 0D8h, 0FFh, 08Dh, 04Eh, 018h
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 017h, 085h, 0D8h, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1HitReactionBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
