ef57cb88e8b7d56c1e320354865b71ef0f7e71d2
dginovker <danielginovker@gmail.com>
2026-07-19 22:14:10 -0700

    Match SpawnBehavior ModuleData destructor
    Exact retail MASM at 0x12AA50/80; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/SpawnBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1SpawnBehaviorModuleData@@UAE@XZ
+; Exact 80 retail bytes @ 0x0012AA50; factory 0x12AAC0 ctor 0x12A970 vtable 0x108F230 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1SpawnBehaviorModuleData@@UAE@XZ
+??1SpawnBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0B3h, 022h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 058h
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 000h, 00Fh, 0EFh, 0FFh, 08Dh, 04Eh, 020h
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 028h, 0C0h, 0EFh, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1SpawnBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
