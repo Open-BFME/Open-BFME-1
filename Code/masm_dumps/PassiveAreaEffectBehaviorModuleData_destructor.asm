32a0ea25f2b89d49b0748573a51b8b463a3f9750
dginovker <danielginovker@gmail.com>
2026-07-19 22:37:36 -0700

    Match PassiveAreaEffectBehavior ModuleData destructor
    Exact retail MASM at 0x202870/80; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/PassiveAreaEffectBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1PassiveAreaEffectBehaviorModuleData@@UAE@XZ
+; Exact 80 retail bytes @ 0x00202870; factory 0x117650 ctor 0x2027A0 vtable 0x10A5350 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1PassiveAreaEffectBehaviorModuleData@@UAE@XZ
+??1PassiveAreaEffectBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0F3h, 0B9h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 020h
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 064h, 07Bh, 0E1h, 0FFh, 08Dh, 04Eh, 014h
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 008h, 042h, 0E2h, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1PassiveAreaEffectBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
