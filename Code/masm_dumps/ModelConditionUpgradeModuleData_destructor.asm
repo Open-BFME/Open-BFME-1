08d576f8d1bca667dcb89f72c59d44ae1f7050d9
dginovker <danielginovker@gmail.com>
2026-07-19 22:14:39 -0700

    Match ModelConditionUpgrade ModuleData destructor
    Exact retail MASM at 0x2D6B20/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/ModelConditionUpgradeModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1ModelConditionUpgradeModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x002D6B20; factory 0x11DD90 ctor 0x2D6A20 vtable 0x10CD4C0 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1ModelConditionUpgradeModuleData@@UAE@XZ
+??1ModelConditionUpgradeModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0C8h, 04Ah, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 030h, 04Eh, 0D4h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1ModelConditionUpgradeModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
