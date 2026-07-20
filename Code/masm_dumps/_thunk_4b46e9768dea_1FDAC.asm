54ceef0191a1a9fcb1de6d88637e010acc9064a9
dginovker <danielginovker@gmail.com>
2026-07-19 23:37:32 -0700

    Open-BFME5: reverse engineer ScriptEngine::doFreezeTime thunk (11,219 -> 11,220)

---
+++ Code/masm_dumps/_thunk_4b46e9768dea_1FDAC.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?doFreezeTime@ScriptEngine@@QAEXXZ
+; Retail @ 0x01fdac size 5
+_TEXT SEGMENT
+public ?doFreezeTime@ScriptEngine@@QAEXXZ
+?doFreezeTime@ScriptEngine@@QAEXXZ PROC
+    db 0E9h, 04Fh, 071h, 031h, 000h
+?doFreezeTime@ScriptEngine@@QAEXXZ ENDP
+_TEXT ENDS
+END
