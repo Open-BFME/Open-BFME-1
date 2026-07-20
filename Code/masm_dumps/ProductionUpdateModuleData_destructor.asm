add2e51bd1163aa820a8e44902a4a95962aa16d8
dginovker <danielginovker@gmail.com>
2026-07-19 23:02:13 -0700

    Match ProductionUpdate ModuleData destructor
    Exact retail MASM at 0x29E260/126; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/ProductionUpdateModuleData_destructor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??1ProductionUpdateModuleData@@UAE@XZ
+; Exact 126 retail bytes @ 0x0029E260; factory 0x11AC90 ctor 0x29FB70 vtable 0x10C0F68 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1ProductionUpdateModuleData@@UAE@XZ
+??1ProductionUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 05Eh, 023h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 057h, 089h, 074h, 024h, 008h, 0C7h, 006h
+    db 068h, 00Fh, 00Ch, 001h, 08Bh, 07Eh, 044h, 085h, 0FFh, 0C7h, 044h, 024h, 014h, 002h, 000h, 000h
+    db 000h, 074h, 01Ah, 08Dh, 047h, 004h, 050h, 0FFh, 015h, 054h, 08Eh, 035h, 001h, 085h, 0C0h, 07Fh
+    db 00Ch, 085h, 0FFh, 074h, 008h, 08Bh, 017h, 06Ah, 001h, 08Bh, 0CFh, 0FFh, 012h, 08Dh, 04Eh, 040h
+    db 0C6h, 044h, 024h, 014h, 001h, 0E8h, 086h, 096h, 05Eh, 000h, 08Dh, 04Eh, 01Ch, 0C6h, 044h, 024h
+    db 014h, 000h, 0E8h, 092h, 0DAh, 0D9h, 0FFh, 08Bh, 04Ch, 024h, 00Ch, 0C7h, 006h, 044h, 037h, 007h
+    db 001h, 05Fh, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1ProductionUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
