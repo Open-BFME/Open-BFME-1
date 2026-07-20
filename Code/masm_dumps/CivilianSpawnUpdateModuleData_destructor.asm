e783828adfd4101aaa1fcb07de759df9a03f8291
dginovker <danielginovker@gmail.com>
2026-07-19 22:48:23 -0700

    Match CivilianSpawnUpdate ModuleData destructor
    Exact retail MASM at 0x28A530/80; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/CivilianSpawnUpdateModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1CivilianSpawnUpdateModuleData@@UAE@XZ
+; Exact 80 retail bytes @ 0x0028A530; factory 0x116B60 ctor 0x28A480 vtable 0x10BCE48 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1CivilianSpawnUpdateModuleData@@UAE@XZ
+??1CivilianSpawnUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0A3h, 012h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 014h
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 055h, 0C5h, 0D9h, 0FFh, 08Dh, 04Eh, 00Ch
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 097h, 0FEh, 0D8h, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1CivilianSpawnUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
