43c3274bc0c4a4f1d27c2bedeb7df6168770a088
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 20:03:07 -0400

    Match quick-match color population

---
+++ Code/masm_dumps/populateQMColorComboBox.asm
@@ -0,0 +1,37 @@
+.386
+.model flat
+
+; ?populateQMColorComboBox@@YAXAAVQuickMatchPreferences@@@Z
+; Exact 406 retail bytes @ 0x00505DB0; thunk and multiplayer-color population loop prove ownership.
+_TEXT SEGMENT
+public ?populateQMColorComboBox@@YAXAAVQuickMatchPreferences@@@Z
+?populateQMColorComboBox@@YAXAAVQuickMatchPreferences@@@Z PROC
+    db 06Ah, 0FFh, 064h, 0A1h, 000h, 000h, 000h, 000h, 068h, 0F8h, 0E1h, 002h, 001h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 014h, 053h, 055h, 056h, 08Bh, 0F1h, 08Bh, 00Dh, 0FCh
+    db 0D5h, 02Eh, 001h, 08Bh, 051h, 03Ch, 08Dh, 041h, 03Ch, 033h, 0DBh, 03Bh, 0D3h, 057h, 075h, 005h
+    db 08Bh, 049h, 034h, 089h, 008h, 08Bh, 038h, 089h, 07Ch, 024h, 01Ch, 089h, 05Ch, 024h, 010h, 08Bh
+    db 096h, 05Ch, 002h, 000h, 000h, 052h, 089h, 05Ch, 024h, 030h, 0E8h, 005h, 012h, 0B0h, 0FFh, 08Bh
+    db 00Dh, 0FCh, 0D5h, 02Eh, 001h, 083h, 0C4h, 004h, 06Ah, 0FFh, 0E8h, 0B3h, 0C0h, 0B1h, 0FFh, 08Bh
+    db 040h, 010h, 08Bh, 00Dh, 07Ch, 014h, 02Fh, 001h, 050h, 08Bh, 001h, 051h, 08Bh, 0D4h, 089h, 064h
+    db 024h, 020h, 053h, 068h, 050h, 040h, 010h, 001h, 052h, 0FFh, 050h, 028h, 08Bh, 086h, 05Ch, 002h
+    db 000h, 000h, 050h, 0E8h, 000h, 095h, 0B2h, 0FFh, 08Bh, 08Eh, 05Ch, 002h, 000h, 000h, 06Ah, 0FFh
+    db 050h, 051h, 0E8h, 0D4h, 071h, 0B1h, 0FFh, 083h, 0C4h, 018h, 033h, 0EDh, 03Bh, 0FBh, 00Fh, 08Eh
+    db 0B2h, 000h, 000h, 000h, 08Bh, 00Dh, 0FCh, 0D5h, 02Eh, 001h, 055h, 0E8h, 062h, 0C0h, 0B1h, 0FFh
+    db 08Bh, 0F8h, 03Bh, 0FBh, 00Fh, 084h, 08Fh, 000h, 000h, 000h, 08Dh, 054h, 024h, 018h, 052h, 08Bh
+    db 0CFh, 0E8h, 0BAh, 079h, 0B2h, 0FFh, 08Bh, 000h, 03Bh, 0C3h, 0C6h, 044h, 024h, 02Ch, 001h, 074h
+    db 005h, 083h, 0C0h, 008h, 0EBh, 005h, 0B8h, 08Bh, 038h, 007h, 001h, 08Bh, 00Dh, 07Ch, 014h, 02Fh
+    db 001h, 08Bh, 011h, 053h, 050h, 08Dh, 044h, 024h, 01Ch, 050h, 0FFh, 052h, 028h, 050h, 08Dh, 04Ch
+    db 024h, 014h, 0C6h, 044h, 024h, 030h, 002h, 0E8h, 084h, 026h, 038h, 000h, 08Dh, 04Ch, 024h, 014h
+    db 0C6h, 044h, 024h, 02Ch, 001h, 0E8h, 016h, 023h, 038h, 000h, 08Dh, 04Ch, 024h, 018h, 088h, 05Ch
+    db 024h, 02Ch, 0E8h, 079h, 01Ah, 038h, 000h, 08Bh, 047h, 010h, 050h, 051h, 08Dh, 054h, 024h, 018h
+    db 089h, 064h, 024h, 028h, 08Bh, 0CCh, 052h, 0E8h, 024h, 025h, 038h, 000h, 08Bh, 086h, 05Ch, 002h
+    db 000h, 000h, 050h, 0E8h, 050h, 094h, 0B2h, 0FFh, 08Bh, 08Eh, 05Ch, 002h, 000h, 000h, 055h, 050h
+    db 051h, 0E8h, 025h, 071h, 0B1h, 0FFh, 083h, 0C4h, 018h, 08Bh, 044h, 024h, 01Ch, 045h, 03Bh, 0E8h
+    db 00Fh, 08Ch, 04Eh, 0FFh, 0FFh, 0FFh, 08Bh, 04Ch, 024h, 034h, 053h, 0E8h, 03Dh, 03Fh, 0B1h, 0FFh
+    db 08Bh, 096h, 05Ch, 002h, 000h, 000h, 050h, 052h, 0E8h, 0A6h, 0DAh, 0B3h, 0FFh, 083h, 0C4h, 00Ch
+    db 08Dh, 04Ch, 024h, 010h, 0C7h, 044h, 024h, 02Ch, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 09Fh, 022h, 038h
+    db 000h, 08Bh, 04Ch, 024h, 024h, 05Fh, 05Eh, 05Dh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 05Bh
+    db 083h, 0C4h, 020h, 0C2h, 004h, 000h
+?populateQMColorComboBox@@YAXAAVQuickMatchPreferences@@@Z ENDP
+_TEXT ENDS
+END
