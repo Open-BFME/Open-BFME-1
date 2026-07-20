8fbfbdaa0e7490d8700d2f886918e6d009382339
dginovker <danielginovker@gmail.com>
2026-07-19 22:34:43 -0700

    Match UnpauseSpecialPowerUpgrade ModuleData destructor
    Exact retail MASM at 0x2D9A10/67; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/UnpauseSpecialPowerUpgradeModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1UnpauseSpecialPowerUpgradeModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x002D9A10; factory 0x11D7C0 ctor 0x2D99B0 vtable 0x10CE498 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1UnpauseSpecialPowerUpgradeModuleData@@UAE@XZ
+??1UnpauseSpecialPowerUpgradeModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0B8h, 04Dh, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 040h, 01Fh, 0D4h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1UnpauseSpecialPowerUpgradeModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
