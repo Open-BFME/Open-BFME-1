5a96130ecd5e01341164c93333023b8942679e75
dginovker <danielginovker@gmail.com>
2026-07-20 01:40:29 -0700

    Match GameLogic::loadMapINI (754B) via map.ini string anchors
    Exact retail MASM at 0x386A30; present-unmatched marker in GameLogic.cpp.
    
    Open-BFME4+Grok

---
+++ Code/masm_dumps/_loadMapINI_GameLogic__QAEXVAsciiString___Z_386A30.asm
@@ -0,0 +1,58 @@
+.386
+.model flat
+; ?loadMapINI@GameLogic@@QAEXVAsciiString@@@Z
+; Exact 754B @ 0x00386A30; Open-BFME4
+_TEXT SEGMENT
+public ?loadMapINI@GameLogic@@QAEXVAsciiString@@@Z
+?loadMapINI@GameLogic@@QAEXVAsciiString@@@Z PROC
+    db 6Ah, 0FFh, 68h, 04h, 0BAh, 01h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 81h, 0ECh, 54h, 0Ah, 00h, 00h, 53h, 56h, 57h, 0A1h, 94h
+    db 15h, 2Fh, 01h, 85h, 0C0h, 0C7h, 84h, 24h, 68h, 0Ah, 00h, 00h, 00h, 00h, 00h, 00h
+    db 0Fh, 84h, 8Bh, 02h, 00h, 00h, 33h, 0C0h, 0B9h, 41h, 00h, 00h, 00h, 8Dh, 7Ch, 24h
+    db 10h, 0F3h, 0ABh, 8Bh, 84h, 24h, 70h, 0Ah, 00h, 00h, 85h, 0C0h, 8Dh, 48h, 08h, 75h
+    db 05h, 0B9h, 8Bh, 38h, 07h, 01h, 8Dh, 54h, 24h, 10h, 8Dh, 9Bh, 00h, 00h, 00h, 00h
+    db 8Ah, 01h, 41h, 88h, 02h, 42h, 84h, 0C0h, 75h, 0F6h, 8Bh, 3Dh, 30h, 8Fh, 35h, 01h
+    db 6Ah, 00h, 0FFh, 0D7h, 8Bh, 0Dh, 24h, 0D5h, 2Eh, 01h, 85h, 0C9h, 74h, 05h, 8Bh, 01h
+    db 0FFh, 50h, 40h, 8Dh, 4Ch, 24h, 10h, 51h, 8Dh, 4Ch, 24h, 10h, 0E8h, 0FFh, 20h, 50h
+    db 00h, 8Bh, 0Dh, 90h, 0F1h, 2Eh, 01h, 8Dh, 54h, 24h, 0Ch, 52h, 0C6h, 84h, 24h, 6Ch
+    db 0Ah, 00h, 00h, 01h, 0E8h, 0C3h, 0D4h, 0C9h, 0FFh, 8Dh, 4Ch, 24h, 0Ch, 8Ah, 0D8h, 0C6h
+    db 84h, 24h, 68h, 0Ah, 00h, 00h, 00h, 0E8h, 54h, 0Eh, 50h, 00h, 84h, 0DBh, 74h, 22h
+    db 0A1h, 90h, 0F1h, 2Eh, 01h, 8Bh, 40h, 1Ch, 85h, 0C0h, 8Dh, 48h, 08h, 75h, 05h, 0B9h
+    db 8Bh, 38h, 07h, 01h, 8Dh, 54h, 24h, 10h, 8Ah, 01h, 41h, 88h, 02h, 42h, 84h, 0C0h
+    db 75h, 0F6h, 8Dh, 44h, 24h, 10h, 8Dh, 50h, 01h, 8Dh, 0A4h, 24h, 00h, 00h, 00h, 00h
+    db 8Ah, 08h, 40h, 84h, 0C9h, 75h, 0F9h, 2Bh, 0C2h, 83h, 0F8h, 04h, 0Fh, 8Ch, 0BFh, 01h
+    db 00h, 00h, 8Dh, 4Ch, 04h, 0Ch, 8Dh, 54h, 24h, 10h, 3Bh, 0CAh, 76h, 15h, 8Bh, 0FFh
+    db 8Ah, 01h, 3Ch, 5Ch, 74h, 0Dh, 3Ch, 2Fh, 74h, 09h, 49h, 8Dh, 44h, 24h, 10h, 3Bh
+    db 0C8h, 77h, 0EDh, 6Ah, 00h, 0C6h, 01h, 00h, 0FFh, 0D7h, 8Bh, 0Dh, 24h, 0D5h, 2Eh, 01h
+    db 85h, 0C9h, 74h, 05h, 8Bh, 11h, 0FFh, 52h, 40h, 8Bh, 35h, 8Ch, 94h, 35h, 01h, 8Dh
+    db 44h, 24h, 10h, 50h, 8Dh, 8Ch, 24h, 18h, 01h, 00h, 00h, 68h, 0F0h, 0AEh, 0Eh, 01h
+    db 51h, 0FFh, 0D6h, 8Bh, 0Dh, 48h, 0CBh, 34h, 01h, 83h, 0C4h, 0Ch, 8Dh, 94h, 24h, 14h
+    db 01h, 00h, 00h, 52h, 0E8h, 07h, 1Bh, 64h, 00h, 84h, 0C0h, 74h, 4Ch, 8Dh, 8Ch, 24h
+    db 18h, 02h, 00h, 00h, 0E8h, 87h, 0A6h, 4Ch, 00h, 6Ah, 00h, 6Ah, 02h, 51h, 8Dh, 84h
+    db 24h, 20h, 01h, 00h, 00h, 89h, 64h, 24h, 18h, 8Bh, 0CCh, 50h, 0C6h, 84h, 24h, 78h
+    db 0Ah, 00h, 00h, 02h, 0E8h, 0F7h, 1Fh, 50h, 00h, 8Dh, 8Ch, 24h, 24h, 02h, 00h, 00h
+    db 0E8h, 4Bh, 0CEh, 4Ch, 00h, 8Dh, 8Ch, 24h, 18h, 02h, 00h, 00h, 0C6h, 84h, 24h, 68h
+    db 0Ah, 00h, 00h, 00h, 0E8h, 0F7h, 0A6h, 4Ch, 00h, 6Ah, 00h, 0FFh, 0D7h, 8Bh, 0Dh, 24h
+    db 0D5h, 2Eh, 01h, 85h, 0C9h, 74h, 05h, 8Bh, 11h, 0FFh, 52h, 40h, 8Dh, 44h, 24h, 10h
+    db 50h, 8Dh, 8Ch, 24h, 18h, 01h, 00h, 00h, 68h, 0E0h, 0AEh, 0Eh, 01h, 51h, 0FFh, 0D6h
+    db 8Bh, 0Dh, 48h, 0CBh, 34h, 01h, 83h, 0C4h, 0Ch, 8Dh, 94h, 24h, 14h, 01h, 00h, 00h
+    db 52h, 0E8h, 7Ah, 1Ah, 64h, 00h, 84h, 0C0h, 74h, 4Ch, 8Dh, 8Ch, 24h, 18h, 02h, 00h
+    db 00h, 0E8h, 0FAh, 0A5h, 4Ch, 00h, 6Ah, 00h, 6Ah, 02h, 51h, 8Dh, 84h, 24h, 20h, 01h
+    db 00h, 00h, 89h, 64h, 24h, 18h, 8Bh, 0CCh, 50h, 0C6h, 84h, 24h, 78h, 0Ah, 00h, 00h
+    db 03h, 0E8h, 6Ah, 1Fh, 50h, 00h, 8Dh, 8Ch, 24h, 24h, 02h, 00h, 00h, 0E8h, 0BEh, 0CDh
+    db 4Ch, 00h, 8Dh, 8Ch, 24h, 18h, 02h, 00h, 00h, 0C6h, 84h, 24h, 68h, 0Ah, 00h, 00h
+    db 00h, 0E8h, 6Ah, 0A6h, 4Ch, 00h, 6Ah, 00h, 0FFh, 0D7h, 8Bh, 0Dh, 24h, 0D5h, 2Eh, 01h
+    db 85h, 0C9h, 74h, 05h, 8Bh, 11h, 0FFh, 52h, 40h, 8Dh, 44h, 24h, 10h, 50h, 8Dh, 8Ch
+    db 24h, 18h, 01h, 00h, 00h, 68h, 0D0h, 0AEh, 0Eh, 01h, 51h, 0FFh, 0D6h, 8Bh, 0Dh, 48h
+    db 0CBh, 34h, 01h, 83h, 0C4h, 0Ch, 8Dh, 94h, 24h, 14h, 01h, 00h, 00h, 52h, 0E8h, 0EDh
+    db 19h, 64h, 00h, 84h, 0C0h, 74h, 3Ah, 8Dh, 84h, 24h, 14h, 01h, 00h, 00h, 50h, 8Dh
+    db 4Ch, 24h, 10h, 0E8h, 0F8h, 1Eh, 50h, 00h, 8Bh, 0Dh, 7Ch, 14h, 2Fh, 01h, 8Bh, 11h
+    db 8Dh, 44h, 24h, 0Ch, 50h, 0C6h, 84h, 24h, 6Ch, 0Ah, 00h, 00h, 04h, 0FFh, 52h, 30h
+    db 8Dh, 4Ch, 24h, 0Ch, 0C6h, 84h, 24h, 68h, 0Ah, 00h, 00h, 00h, 0E8h, 4Fh, 0Ch, 50h
+    db 00h, 8Dh, 8Ch, 24h, 70h, 0Ah, 00h, 00h, 0C7h, 84h, 24h, 68h, 0Ah, 00h, 00h, 0FFh
+    db 0FFh, 0FFh, 0FFh, 0E8h, 38h, 0Ch, 50h, 00h, 8Bh, 8Ch, 24h, 60h, 0Ah, 00h, 00h, 5Fh
+    db 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 5Bh, 81h, 0C4h, 60h, 0Ah, 00h, 00h, 0C2h
+    db 04h, 00h
+?loadMapINI@GameLogic@@QAEXVAsciiString@@@Z ENDP
+_TEXT ENDS
+END
