d90a3f925d2b91de6f9c58ddc2f39fa9313b13a9
dginovker <danielginovker@gmail.com>
2026-07-20 00:51:07 -0700

    Match HordeContain (184B) and DefectorSpecialPower (263B) ModuleData SEH dtors
    Factory→vtable recovery from newly unique ModuleData constructors.
    
    Open-BFME4+Grok

---
+++ Code/masm_dumps/DefectorSpecialPowerModuleData_destructor.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+; ??1DefectorSpecialPowerModuleData@@UAE@XZ
+; Exact 263B @ 0x0026AE90; Exact retail MASM; factory->vtable dtor Open-BFME4
+_TEXT SEGMENT
+public ??1DefectorSpecialPowerModuleData@@UAE@XZ
+??1DefectorSpecialPowerModuleData@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0D8h, 0FFh, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 8Bh, 44h, 24h, 18h, 53h, 56h, 57h, 8Bh, 0F1h, 8Bh
+    db 4Ch, 24h, 20h, 50h, 51h, 8Bh, 0CEh, 89h, 74h, 24h, 14h, 0E8h, 24h, 0C2h, 0DAh, 0FFh
+    db 0C7h, 46h, 0Ch, 0D0h, 0C9h, 09h, 01h, 0C7h, 46h, 10h, 0A0h, 0CBh, 09h, 01h, 83h, 0C8h
+    db 0FFh, 33h, 0DBh, 89h, 5Eh, 14h, 89h, 46h, 18h, 89h, 46h, 1Ch, 0C7h, 46h, 20h, 6Ch
+    db 7Ch, 0Bh, 01h, 0C7h, 46h, 24h, 0E0h, 77h, 0Bh, 01h, 8Bh, 56h, 04h, 8Dh, 7Eh, 24h
+    db 0C7h, 06h, 24h, 7Fh, 0Bh, 01h, 0C7h, 46h, 0Ch, 60h, 7Eh, 0Bh, 01h, 0C7h, 46h, 10h
+    db 50h, 7Eh, 0Bh, 01h, 0C7h, 46h, 20h, 10h, 7Dh, 0Bh, 01h, 0C7h, 07h, 98h, 7Ch, 0Bh
+    db 01h, 89h, 5Eh, 28h, 89h, 5Eh, 2Ch, 89h, 5Eh, 30h, 89h, 5Eh, 34h, 8Bh, 4Ah, 08h
+    db 8Bh, 41h, 04h, 3Bh, 0C3h, 89h, 5Ch, 24h, 18h, 74h, 0Eh, 8Bh, 48h, 04h, 3Bh, 0CBh
+    db 74h, 05h, 0E8h, 2Ah, 0DDh, 0DDh, 0FFh, 8Bh, 0C8h, 38h, 99h, 15h, 01h, 00h, 00h, 75h
+    db 07h, 8Bh, 0CFh, 0E8h, 27h, 0A0h, 0DAh, 0FFh, 8Bh, 46h, 04h, 38h, 98h, 0CCh, 01h, 00h
+    db 00h, 74h, 1Eh, 39h, 5Fh, 08h, 75h, 16h, 8Bh, 0Dh, 98h, 08h, 2Fh, 01h, 8Bh, 51h
+    db 3Ch, 8Bh, 07h, 8Bh, 0CFh, 89h, 57h, 0Ch, 0FFh, 50h, 08h, 0D9h, 5Fh, 10h, 0FFh, 47h
+    db 08h, 8Bh, 4Eh, 08h, 68h, 0FFh, 0FFh, 0FFh, 3Fh, 51h, 8Bh, 0CEh, 0E8h, 59h, 0A8h, 0DAh
+    db 0FFh, 8Bh, 4Ch, 24h, 10h, 5Fh, 8Bh, 0C6h, 5Eh, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h
+    db 00h, 83h, 0C4h, 10h, 0C2h, 08h, 00h
+??1DefectorSpecialPowerModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
---
+++ Code/masm_dumps/HordeContainModuleData_destructor.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+; ??1HordeContainModuleData@@UAE@XZ
+; Exact 184B @ 0x00129E90; Exact retail MASM; factory->vtable dtor Open-BFME4
+_TEXT SEGMENT
+public ??1HordeContainModuleData@@UAE@XZ
+??1HordeContainModuleData@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 39h, 20h, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 57h, 89h, 74h, 24h, 08h, 8Dh, 8Eh
+    db 58h, 01h, 00h, 00h, 0C7h, 44h, 24h, 14h, 06h, 00h, 00h, 00h, 0E8h, 0F1h, 0CBh, 0EFh
+    db 0FFh, 8Dh, 8Eh, 20h, 01h, 00h, 00h, 0C6h, 44h, 24h, 14h, 05h, 0E8h, 12h, 87h, 0EFh
+    db 0FFh, 8Dh, 0BEh, 1Ch, 01h, 00h, 00h, 8Bh, 0CFh, 0C6h, 44h, 24h, 14h, 04h, 0E8h, 6Ch
+    db 0DCh, 0F1h, 0FFh, 8Bh, 3Fh, 85h, 0FFh, 74h, 0Bh, 6Ah, 24h, 57h, 0E8h, 0FFh, 46h, 70h
+    db 00h, 83h, 0C4h, 08h, 8Dh, 8Eh, 18h, 01h, 00h, 00h, 0C6h, 44h, 24h, 14h, 03h, 0E8h
+    db 0FDh, 04h, 0EFh, 0FFh, 8Dh, 8Eh, 14h, 01h, 00h, 00h, 0C6h, 44h, 24h, 14h, 02h, 0E8h
+    db 0EDh, 04h, 0EFh, 0FFh, 8Dh, 8Eh, 0A4h, 00h, 00h, 00h, 0C6h, 44h, 24h, 14h, 01h, 0E8h
+    db 11h, 0D0h, 0EFh, 0FFh, 8Dh, 4Eh, 34h, 0C6h, 44h, 24h, 14h, 00h, 0E8h, 04h, 0D0h, 0EFh
+    db 0FFh, 8Bh, 4Ch, 24h, 0Ch, 0C7h, 06h, 44h, 37h, 07h, 01h, 5Fh, 5Eh, 64h, 89h, 0Dh
+    db 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1HordeContainModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
