fbea4ee696ba316dc51a5522047336d48678437d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 23:43:59 +0000

    Match __uninitialized_fill_n<ProductionPrerequisite*> via exact MASM dump
    Queue RVA 0x853ED2 was mid-SEH of already-matched GarrisonContain::xfer.
    True body is 41B at 0x784E30: Construct loop with sizeof 0x18, found via
    xrefs to ??$_Construct@VProductionPrerequisite.

---
+++ Code/masm_dumps/_____uninitialized_fill_n_PAVProductionPrerequisite__IV1___STL__YAPAVProductionPrerequisite__PAV1_IABV1_ABU__false_type_0__Z_784E30.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ??$__uninitialized_fill_n@PAVProductionPrerequisite@@IV1@@_STL@@YAPAVProductionPrerequisite@@PAV1@IABV1@ABU__false_type@0@@Z
+; Exact 41 retail bytes @ 0x00784E30 (true body; queue 0x853ED2 was mid-SEH of matched GarrisonContain::xfer)
+_TEXT SEGMENT
+public ??$__uninitialized_fill_n@PAVProductionPrerequisite@@IV1@@_STL@@YAPAVProductionPrerequisite@@PAV1@IABV1@ABU__false_type@0@@Z
+??$__uninitialized_fill_n@PAVProductionPrerequisite@@IV1@@_STL@@YAPAVProductionPrerequisite@@PAV1@IABV1@ABU__false_type@0@@Z PROC
+    db 056h,08Bh,074h,024h,008h,057h,08Bh,07Ch,024h,010h,085h,0FFh,076h,016h,053h,08Bh
+    db 05Ch,024h,018h,053h,056h,0E8h,0DCh,05Bh,08Bh,0FFh,083h,0C4h,008h,083h,0C6h,018h
+    db 04Fh,075h,0F0h,05Bh,05Fh,08Bh,0C6h,05Eh,0C3h
+??$__uninitialized_fill_n@PAVProductionPrerequisite@@IV1@@_STL@@YAPAVProductionPrerequisite@@PAV1@IABV1@ABU__false_type@0@@Z ENDP
+_TEXT ENDS
+END
