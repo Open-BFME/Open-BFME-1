9c0f66cce3409ca3d9183a75de77462e9e44d481
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 11:01:59 +0000

    match joinGame exact 359B MASM at 0x4D7580
    Queue RVA was off-by-one (0x4D7581); true SEH body is 359B. C++ remains blocked by GameSpyInfo vtable slots and GameSpyStagingRoom field layout.

---
+++ Code/masm_dumps/joinGame.asm
@@ -0,0 +1,34 @@
+.386
+.model flat
+
+; ?joinGame@@YAXVAsciiString@@@Z
+; Exact 359 retail bytes @ 0x004D7580; true start after int3 pad (queue listed 0x4D7581).
+_TEXT SEGMENT
+public ?joinGame@@YAXVAsciiString@@@Z
+?joinGame@@YAXVAsciiString@@@Z PROC
+    db 06Ah, 0FFh, 068h, 0EEh, 0A8h, 002h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 081h, 0ECh, 098h, 001h, 000h, 000h, 053h, 056h, 08Bh, 00Dh, 094h
+    db 071h, 02Fh, 001h, 08Bh, 031h, 033h, 0DBh, 08Bh, 0C6h, 089h, 09Ch, 024h, 0A8h, 001h, 000h, 000h
+    db 0FFh, 090h, 0D0h, 000h, 000h, 000h, 08Bh, 00Dh, 094h, 071h, 02Fh, 001h, 050h, 0FFh, 096h, 09Ch
+    db 000h, 000h, 000h, 08Bh, 0F0h, 03Bh, 0F3h, 075h, 01Bh, 06Ah, 005h, 0E8h, 0C4h, 098h, 0B6h, 0FFh
+    db 053h, 0E8h, 054h, 0C0h, 0B2h, 0FFh, 083h, 0C4h, 008h, 089h, 01Dh, 050h, 03Fh, 02Fh, 001h, 0E9h
+    db 0D5h, 000h, 000h, 000h, 08Dh, 04Ch, 024h, 00Ch, 0E8h, 00Fh, 0FCh, 0B3h, 0FFh, 08Dh, 04Ch, 024h
+    db 008h, 051h, 08Bh, 0CEh, 0C6h, 084h, 024h, 0ACh, 001h, 000h, 000h, 001h, 0C7h, 044h, 024h, 010h
+    db 00Bh, 000h, 000h, 000h, 0E8h, 0ABh, 051h, 0B5h, 0FFh, 08Bh, 000h, 03Bh, 0C3h, 0C6h, 084h, 024h
+    db 0A8h, 001h, 000h, 000h, 002h, 074h, 005h, 083h, 0C0h, 008h, 0EBh, 005h, 0B8h, 08Ch, 038h, 007h
+    db 001h, 033h, 0C9h, 066h, 039h, 018h, 074h, 007h, 041h, 066h, 039h, 01Ch, 048h, 075h, 0F9h, 08Dh
+    db 014h, 048h, 052h, 050h, 08Dh, 04Ch, 024h, 024h, 0E8h, 038h, 0D4h, 0B2h, 0FFh, 08Dh, 04Ch, 024h
+    db 008h, 0C6h, 084h, 024h, 0A8h, 001h, 000h, 000h, 001h, 0E8h, 082h, 00Bh, 03Bh, 000h, 08Bh, 086h
+    db 01Ch, 004h, 000h, 000h, 089h, 084h, 024h, 0F0h, 000h, 000h, 000h, 08Bh, 084h, 024h, 0B0h, 001h
+    db 000h, 000h, 03Bh, 0C3h, 08Dh, 048h, 008h, 075h, 005h, 0B9h, 08Bh, 038h, 007h, 001h, 08Bh, 0C1h
+    db 08Dh, 070h, 001h, 08Ah, 010h, 040h, 03Ah, 0D3h, 075h, 0F9h, 02Bh, 0C6h, 003h, 0C1h, 050h, 051h
+    db 08Dh, 04Ch, 024h, 030h, 0E8h, 00Eh, 03Ch, 0B5h, 0FFh, 08Bh, 00Dh, 0C8h, 071h, 02Fh, 001h, 08Bh
+    db 011h, 08Dh, 044h, 024h, 00Ch, 050h, 0FFh, 052h, 018h, 06Ah, 005h, 0E8h, 0F4h, 097h, 0B6h, 0FFh
+    db 083h, 0C4h, 004h, 08Dh, 04Ch, 024h, 00Ch, 089h, 01Dh, 050h, 03Fh, 02Fh, 001h, 088h, 09Ch, 024h
+    db 0A8h, 001h, 000h, 000h, 0E8h, 01Ch, 0F5h, 0B3h, 0FFh, 08Dh, 08Ch, 024h, 0B0h, 001h, 000h, 000h
+    db 0C7h, 084h, 024h, 0A8h, 001h, 000h, 000h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 070h, 002h, 03Bh, 000h
+    db 08Bh, 08Ch, 024h, 0A0h, 001h, 000h, 000h, 05Eh, 05Bh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h
+    db 081h, 0C4h, 0A4h, 001h, 000h, 000h, 0C3h
+?joinGame@@YAXVAsciiString@@@Z ENDP
+_TEXT ENDS
+END
