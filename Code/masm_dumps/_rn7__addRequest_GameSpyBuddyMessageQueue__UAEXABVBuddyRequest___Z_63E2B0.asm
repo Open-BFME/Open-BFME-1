4b68212a40d6ebac0b27cf2fbd58e3923f47f8d4
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 20:42:02 +0000

    Match GameSpyBuddyMessageQueue::addRequest at true body 0x63E2B0.
    Queue RVA 0x63E39F was a one-byte pad mispair onto already-matched
    addResponse (0x63E3A0). Vtable slot 6 confirms addRequest @ 0x63E2B0/185B.
    C++ blocked by sizeof(BuddyRequest) retail 0x2B8 vs ZH 0x208; land exact
    bytes via MASM like sibling addResponse/getRequest.

---
+++ Code/masm_dumps/_rn7__addRequest_GameSpyBuddyMessageQueue__UAEXABVBuddyRequest___Z_63E2B0.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+; ?addRequest@GameSpyBuddyMessageQueue@@UAEXABVBuddyRequest@@@Z
+; Exact 185B @ 0x63E2B0; vtable slot6 GameSpyBuddyMessageQueue; queue 0x63E39F was addResponse mispair
+_TEXT SEGMENT
+public ?addRequest@GameSpyBuddyMessageQueue@@UAEXABVBuddyRequest@@@Z
+?addRequest@GameSpyBuddyMessageQueue@@UAEXABVBuddyRequest@@@Z PROC
+    db 6Ah, 0FFh, 68h, 0E8h, 19h, 04h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 56h, 8Bh, 0F1h, 6Ah, 0FFh, 8Dh, 46h, 04h
+    db 50h, 8Dh, 4Ch, 24h, 0Ch, 0E8h, 0D6h, 0D0h, 39h, 00h, 8Ah, 44h, 24h, 08h, 84h, 0C0h
+    db 0C7h, 44h, 24h, 14h, 00h, 00h, 00h, 00h, 74h, 23h, 8Dh, 4Ch, 24h, 04h, 0C7h, 44h
+    db 24h, 14h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 05h, 0D1h, 39h, 00h, 5Eh, 8Bh, 4Ch, 24h, 08h
+    db 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 14h, 0C2h, 04h, 00h, 8Bh, 4Eh, 2Ch
+    db 8Dh, 46h, 14h, 57h, 8Bh, 78h, 10h, 81h, 0E9h, 0B8h, 02h, 00h, 00h, 3Bh, 0F9h, 74h
+    db 18h, 85h, 0FFh, 74h, 0Bh, 8Bh, 74h, 24h, 20h, 0B9h, 0AEh, 00h, 00h, 00h, 0F3h, 0A5h
+    db 81h, 40h, 10h, 0B8h, 02h, 00h, 00h, 0EBh, 0Ch, 8Bh, 54h, 24h, 20h, 52h, 8Bh, 0C8h
+    db 0E8h, 81h, 88h, 9Dh, 0FFh, 8Dh, 4Ch, 24h, 08h, 0C7h, 44h, 24h, 18h, 0FFh, 0FFh, 0FFh
+    db 0FFh, 0E8h, 0AAh, 0D0h, 39h, 00h, 8Bh, 4Ch, 24h, 10h, 5Fh, 5Eh, 64h, 89h, 0Dh, 00h
+    db 00h, 00h, 00h, 83h, 0C4h, 14h, 0C2h, 04h, 00h
+?addRequest@GameSpyBuddyMessageQueue@@UAEXABVBuddyRequest@@@Z ENDP
+_TEXT ENDS
+END
