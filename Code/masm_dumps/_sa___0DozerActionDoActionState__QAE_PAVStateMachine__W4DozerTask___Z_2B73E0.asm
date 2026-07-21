b8fe91992ba6193ff5d569b57ae9c58a1043e2e2
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 12:21:18 +0000

    Match ??0DozerActionDoActionState at true body 0x2B73E0
    Structural queue RVA 0x2B755E was a mid-inlined fragment inside the
    already-matched DozerActionStateMachine ctor. Standalone COMDAT is 60B
    at 0x2B73E0 (thin AsciiString@0x888BC0 + State thunk); MASM dump matches
    retail bytes.

---
+++ Code/masm_dumps/_sa___0DozerActionDoActionState__QAE_PAVStateMachine__W4DozerTask___Z_2B73E0.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+; ??0DozerActionDoActionState@@QAE@PAVStateMachine@@W4DozerTask@@@Z
+; Exact 60B @ 0x2B73E0; structural queue pointed at mid-inlined fragment 0x2B755E inside already-matched DozerActionStateMachine ctor; true standalone body; thin AsciiString@0x888BC0 + State@thunk; Open-BFME fleet
+_TEXT SEGMENT
+public ??0DozerActionDoActionState@@QAE@PAVStateMachine@@W4DozerTask@@@Z
+??0DozerActionDoActionState@@QAE@PAVStateMachine@@W4DozerTask@@@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 74h, 6Eh, 0Ch, 01h
+    db 0E8h, 0CBh, 17h, 5Dh, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 0B1h, 0C1h, 0D4h
+    db 0FFh, 8Bh, 4Ch, 24h, 10h, 0C7h, 06h, 20h, 6Eh, 0Ch, 01h, 89h, 4Eh, 24h, 0C7h, 46h
+    db 28h, 00h, 00h, 00h, 00h, 8Bh, 0C6h, 5Eh, 59h, 0C2h, 08h, 00h
+??0DozerActionDoActionState@@QAE@PAVStateMachine@@W4DozerTask@@@Z ENDP
+_TEXT ENDS
+END
