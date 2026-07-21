7511943786a56e88301b9d837e1eb82fecc5d115
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 13:07:20 +0000

    match ReleaseResources@W3DProjectedShadowManager at true 0x7B1450
    Drift candidate 0x7B4E74 is mid-destructor; standalone body is 58B at
    0x7B1450 (texture-manager invalidate + release decal IB/VB). Exact MASM dump.

---
+++ Code/masm_dumps/ReleaseResources_W3DProjectedShadowManager_QAEXXZ_007B1450_28d95344.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+; ?ReleaseResources@W3DProjectedShadowManager@@QAEXXZ
+; Exact 58B @ 0x007B1450; true body (drift 0x7B4E74 was mid-dtor)
+_TEXT SEGMENT
+public ?ReleaseResources@W3DProjectedShadowManager@@QAEXXZ
+?ReleaseResources@W3DProjectedShadowManager@@QAEXXZ PROC
+    db 8Bh, 89h, 4Ch, 02h, 00h, 00h, 56h, 0E8h, 0E8h, 0CEh, 86h, 0FFh, 0A1h, 08h, 6Eh, 30h
+    db 01h, 33h, 0F6h, 3Bh, 0C6h, 74h, 06h, 8Bh, 08h, 50h, 0FFh, 51h, 08h, 0A1h, 04h, 6Eh
+    db 30h, 01h, 3Bh, 0C6h, 74h, 06h, 8Bh, 10h, 50h, 0FFh, 52h, 08h, 89h, 35h, 08h, 6Eh
+    db 30h, 01h, 89h, 35h, 04h, 6Eh, 30h, 01h, 5Eh, 0C3h
+?ReleaseResources@W3DProjectedShadowManager@@QAEXXZ ENDP
+_TEXT ENDS
+END
