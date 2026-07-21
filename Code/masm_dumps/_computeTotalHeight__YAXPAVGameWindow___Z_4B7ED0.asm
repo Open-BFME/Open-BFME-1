2581b8d4aaf3e646265a9c93beaa11cdca44c676
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 13:59:18 +0000

    Match computeTotalHeight (357B @ 0x4B7ED0)
    MASM exact retail dump. C++ blocked by BFME ListboxData field offsets
    (+4 vs ZH), ListEntryRow.height dword store, and DisplayString::getSize
    vtable slot 0x3c.

---
+++ Code/masm_dumps/_computeTotalHeight__YAXPAVGameWindow___Z_4B7ED0.asm
@@ -0,0 +1,34 @@
+.386
+.model flat
+
+; ?computeTotalHeight@@YAXPAVGameWindow@@@Z
+; Exact 357B retail @ 0x004B7ED0; BFME ListboxData/DisplayString layout
+_TEXT SEGMENT
+public ?computeTotalHeight@@YAXPAVGameWindow@@@Z
+?computeTotalHeight@@YAXPAVGameWindow@@@Z PROC
+    db 83h,0ECh,14h,53h,56h,57h,8Bh,7Ch,24h,24h,33h,0DBh,8Bh,0CFh,89h,5Ch
+    db 24h,14h,0E8h,51h,0E6h,0B8h,0FFh,8Bh,0CFh,8Bh,0F0h,0E8h,0A1h,0CAh,0B7h,0FFh
+    db 66h,39h,5Eh,2Ch,89h,44h,24h,18h,89h,5Ch,24h,1Ch,0Fh,8Eh,0E8h,00h
+    db 00h,00h,33h,0FFh,55h,0EBh,09h,8Dh,0A4h,24h,00h,00h,00h,00h,8Bh,0FFh
+    db 8Bh,46h,18h,39h,5Ch,38h,08h,0Fh,84h,0B0h,00h,00h,00h,33h,0EDh,66h
+    db 39h,5Eh,02h,89h,5Ch,24h,14h,0Fh,8Eh,7Eh,00h,00h,00h,8Dh,49h,00h
+    db 8Bh,4Eh,18h,33h,0C0h,89h,44h,24h,10h,8Bh,54h,39h,08h,8Dh,0Ch,1Ah
+    db 8Bh,11h,83h,0FAh,01h,0Fh,85h,0B8h,00h,00h,00h,8Bh,4Ch,24h,28h,0E8h
+    db 86h,0BEh,0B6h,0FFh,0F6h,0C4h,40h,74h,1Bh,8Bh,44h,24h,1Ch,8Bh,80h,84h
+    db 01h,00h,00h,8Bh,0Dh,40h,1Bh,2Fh,01h,8Bh,11h,50h,0FFh,92h,0Ch,01h
+    db 00h,00h,0EBh,1Fh,8Bh,46h,18h,8Bh,4Ch,38h,08h,8Bh,4Ch,19h,08h,85h
+    db 0C9h,74h,0Ch,8Bh,11h,8Dh,44h,24h,10h,50h,6Ah,00h,0FFh,52h,3Ch,8Bh
+    db 44h,24h,10h,3Bh,44h,24h,14h,7Eh,04h,89h,44h,24h,14h,0Fh,0BFh,46h
+    db 02h,45h,83h,0C3h,18h,3Bh,0E8h,7Ch,87h,33h,0DBh,8Bh,4Eh,18h,8Bh,54h
+    db 24h,14h,89h,54h,39h,04h,8Bh,46h,18h,8Bh,4Ch,38h,04h,8Bh,54h,24h
+    db 18h,03h,0C7h,8Dh,4Ch,0Ah,01h,89h,4Ch,24h,18h,89h,08h,8Bh,44h,24h
+    db 20h,0Fh,0BFh,4Eh,2Ch,40h,83h,0C7h,0Ch,3Bh,0C1h,89h,44h,24h,20h,0Fh
+    db 8Ch,2Bh,0FFh,0FFh,0FFh,8Bh,7Ch,24h,28h,5Dh,8Bh,54h,24h,14h,6Ah,01h
+    db 57h,89h,56h,28h,0E8h,27h,0FBh,0FFh,0FFh,83h,0C4h,08h,5Fh,5Eh,5Bh,83h
+    db 0C4h,14h,0C3h,83h,0FAh,02h,75h,8Bh,8Bh,49h,14h,85h,0C9h,7Eh,08h,8Dh
+    db 41h,01h,0E9h,7Ch,0FFh,0FFh,0FFh,8Bh,4Ch,24h,1Ch,8Bh,81h,84h,01h,00h
+    db 00h,8Bh,0Dh,40h,1Bh,2Fh,01h,8Bh,11h,50h,0FFh,92h,0Ch,01h,00h,00h
+    db 0E9h,5Eh,0FFh,0FFh,0FFh
+?computeTotalHeight@@YAXPAVGameWindow@@@Z ENDP
+_TEXT ENDS
+END
