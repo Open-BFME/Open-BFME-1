c0961e8ce76fbeaffc2de705ad8440b15cf28547
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 19:59:24 +0000

    Match ??0Radar@@QAE@XZ at true body 0x107FE0/237 via MASM.
    Queue RVA 0x9F271F was inter-function pad/epilogue; real ctor is the only
    other store of the dual Radar vtables already owned by the matched dtor.

---
+++ Code/masm_dumps/Radar_constructor.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ??0Radar@@QAE@XZ
+; Exact 237 retail bytes @ 0x00107FE0; dual Radar vtables match dtor; queue 0x9F271F was unrelated pad.
+_TEXT SEGMENT
+public ??0Radar@@QAE@XZ
+??0Radar@@QAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 49h, 0CFh, 0FFh, 00h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 53h, 56h, 8Bh, 0F1h, 57h, 89h, 74h, 24h, 0Ch, 0C7h
+    db 06h, 44h, 37h, 07h, 01h, 8Dh, 7Eh, 04h, 33h, 0DBh, 8Bh, 0CFh, 89h, 5Ch, 24h, 18h
+    db 0E8h, 1Bh, 9Ah, 89h, 00h, 68h, 15h, 5Dh, 42h, 00h, 68h, 0D7h, 76h, 43h, 00h, 6Ah
+    db 40h, 6Ah, 50h, 8Dh, 46h, 28h, 50h, 0C6h, 44h, 24h, 2Ch, 01h, 0C7h, 06h, 0ACh, 88h
+    db 08h, 01h, 0C7h, 07h, 80h, 88h, 08h, 01h, 0E8h, 0A7h, 0EEh, 8Eh, 00h, 0B8h, 01h, 00h
+    db 0FFh, 0FFh, 89h, 86h, 54h, 14h, 00h, 00h, 89h, 86h, 58h, 14h, 00h, 00h, 0B8h, 0FFh
+    db 0FFh, 00h, 00h, 8Bh, 0CEh, 0C6h, 44h, 24h, 18h, 02h, 89h, 9Eh, 38h, 14h, 00h, 00h
+    db 89h, 5Eh, 10h, 89h, 5Eh, 14h, 88h, 5Eh, 0Ch, 88h, 5Eh, 0Dh, 89h, 5Eh, 18h, 89h
+    db 5Eh, 1Ch, 89h, 5Eh, 20h, 89h, 5Eh, 24h, 89h, 9Eh, 3Ch, 14h, 00h, 00h, 89h, 9Eh
+    db 40h, 14h, 00h, 00h, 89h, 9Eh, 44h, 14h, 00h, 00h, 89h, 9Eh, 48h, 14h, 00h, 00h
+    db 89h, 9Eh, 4Ch, 14h, 00h, 00h, 89h, 9Eh, 50h, 14h, 00h, 00h, 89h, 86h, 5Ch, 14h
+    db 00h, 00h, 89h, 86h, 60h, 14h, 00h, 00h, 88h, 9Eh, 64h, 14h, 00h, 00h, 89h, 9Eh
+    db 68h, 14h, 00h, 00h, 0E8h, 6Eh, 0Bh, 0F2h, 0FFh, 8Bh, 4Ch, 24h, 10h, 5Fh, 8Bh, 0C6h
+    db 5Eh, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??0Radar@@QAE@XZ ENDP
+_TEXT ENDS
+END
