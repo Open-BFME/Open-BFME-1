9d44ab136c2a0672f19fc6b81710d5fb46bb4f38
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 20:30:51 +0000

    Match ??1W3DModelDrawModuleData at true body 0x213750 (76B).
    Queue RVA 0x9F2761 was inside already-matched ControlBar ctor. True body
    found via friend_newModuleData@W3DModelDraw -> ctor 0x2136E0 -> vt 0xCA8598.
    Shrink false 6838B RespawnBody dtor dump that swallowed this range (true
    first-function size 1803B to int3 gap).

---
+++ Code/masm_dumps/W3DModelDrawModuleData_destructor.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+; ??1W3DModelDrawModuleData@@UAE@XZ
+; Exact 76B @ 0x00213750; factory friend_newModuleData@W3DModelDraw@0x11F9C0->ctor 0x2136E0 vt 0xCA8598->body; queue 0x9F2761 was INSIDE ControlBar ctor
+_TEXT SEGMENT
+public ??1W3DModelDrawModuleData@@UAE@XZ
+??1W3DModelDrawModuleData@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0A8h, 0C7h, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 89h, 74h, 24h, 04h, 8Dh, 4Eh, 5Ch
+    db 0C7h, 44h, 24h, 10h, 00h, 00h, 00h, 00h, 0E8h, 84h, 6Ch, 0E0h, 0FFh, 8Bh, 0CEh, 0C7h
+    db 44h, 24h, 10h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 27h, 93h, 0DFh, 0FFh, 8Bh, 4Ch, 24h, 08h
+    db 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1W3DModelDrawModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END
