42b818862ea0df6181435bf35a995839493d82fe
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 19:27:16 +0000

    match DifficultySelectSystem via exact MASM at true body 0x4C72C0/331
    Queue RVA 0x79CE5C was a misplaced mid-epilogue. Identity from the GUI
    callback name table (DifficultySelectSystem -> thunk 0x4B38F -> body).
    C++ emits SEH and an OptionPreferences stack object; retail has neither.

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/DifficultySelect_System.asm
@@ -0,0 +1,56 @@
+.386
+.model flat
+
+; ?DifficultySelectSystem@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z
+; Exact 331 retail bytes @ 0x004C72C0; identity from GUI callback name table
+; "DifficultySelectSystem" @ 0xC8734C -> thunk 0x4B38F -> body 0x4C72C0.
+; Queue RVA 0x79CE5C was mid-epilogue of unrelated fn (misplaced drift vote).
+; C++ emits SEH + OptionPreferences stack object; retail has neither.
+_TEXT SEGMENT
+public ?DifficultySelectSystem@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z
+?DifficultySelectSystem@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z PROC
+    db 8Bh,44h,24h,08h,83h,0F8h,17h,56h
+    db 77h,2Dh,74h,12h,48h,0Fh,84h,31h
+    db 01h,00h,00h,48h,0Fh,84h,2Ah,01h
+    db 00h,00h,33h,0C0h,5Eh,0C3h,83h,7Ch
+    db 24h,10h,01h,0Fh,85h,1Bh,01h,00h
+    db 00h,8Bh,44h,24h,14h,0C6h,00h,01h
+    db 0B8h,01h,00h,00h,00h,5Eh,0C3h,3Dh
+    db 08h,40h,00h,00h,74h,04h,33h,0C0h
+    db 5Eh,0C3h,8Bh,4Ch,24h,10h,0E8h,99h
+    db 15h,0B5h,0FFh,3Bh,05h,48h,39h,2Fh
+    db 01h,75h,56h,8Bh,0Dh,78h,69h,2Bh
+    db 01h,51h,8Bh,0Dh,6Ch,07h,2Fh,01h
+    db 0E8h,96h,0B3h,0B4h,0FFh,8Bh,4Ch,24h
+    db 08h,0E8h,0C7h,8Fh,0B7h,0FFh,8Bh,0F0h
+    db 8Bh,16h,8Bh,0CEh,0FFh,52h,20h,8Bh
+    db 06h,6Ah,01h,8Bh,0CEh,0FFh,50h,04h
+    db 8Bh,0Dh,78h,69h,2Bh,01h,51h,51h
+    db 8Bh,0Dh,0B0h,4Ch,2Fh,01h,8Bh,0D4h
+    db 89h,64h,24h,14h,52h,0E8h,55h,32h
+    db 0B5h,0FFh,0E8h,0F5h,17h,0B7h,0FFh,83h
+    db 0C4h,08h,0B8h,01h,00h,00h,00h,5Eh
+    db 0C3h,3Bh,05h,50h,39h,2Fh,01h,75h
+    db 4Fh,51h,89h,64h,24h,10h,8Bh,0CCh
+    db 68h,50h,6Eh,33h,01h,0E8h,0DEh,07h
+    db 3Ch,00h,8Bh,0Dh,0B0h,4Ch,2Fh,01h
+    db 0E8h,7Eh,0C7h,0B4h,0FFh,8Bh,0Dh,40h
+    db 1Bh,2Fh,01h,8Bh,74h,24h,08h,8Bh
+    db 01h,56h,0FFh,90h,0F0h,00h,00h,00h
+    db 8Bh,0CEh,0E8h,4Eh,8Fh,0B7h,0FFh,8Bh
+    db 0F0h,8Bh,16h,8Bh,0CEh,0FFh,52h,20h
+    db 8Bh,06h,6Ah,01h,8Bh,0CEh,0FFh,50h
+    db 04h,0B8h,01h,00h,00h,00h,5Eh,0C3h
+    db 3Bh,05h,58h,39h,2Fh,01h,75h,11h
+    db 0C7h,05h,78h,69h,2Bh,01h,00h,00h
+    db 00h,00h,0B8h,01h,00h,00h,00h,5Eh
+    db 0C3h,3Bh,05h,5Ch,39h,2Fh,01h,75h
+    db 11h,0C7h,05h,78h,69h,2Bh,01h,01h
+    db 00h,00h,00h,0B8h,01h,00h,00h,00h
+    db 5Eh,0C3h,3Bh,05h,60h,39h,2Fh,01h
+    db 75h,0Ah,0C7h,05h,78h,69h,2Bh,01h
+    db 02h,00h,00h,00h,0B8h,01h,00h,00h
+    db 00h,5Eh,0C3h
+?DifficultySelectSystem@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z ENDP
+_TEXT ENDS
+END
