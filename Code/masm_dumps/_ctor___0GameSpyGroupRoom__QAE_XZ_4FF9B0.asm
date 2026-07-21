7f0d2184b5d0aa56921749f0e5c879a56b332d7e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 19:27:30 +0000

    match ??0GameSpyGroupRoom@@QAE@XZ at true body 0x4FF9B0 (106B)
    Queued 0x888A27 was INSIDE nextToken@StringBase@G; true default ctor is
    SEH + zero Ascii/Unicode + set(TheEmpty*) + five int zeros, called from
    GroupRoom map operator[] via ILT 0x1DCC3. Exact MASM dump; C++ inlines set.

---
+++ Code/masm_dumps/_ctor___0GameSpyGroupRoom__QAE_XZ_4FF9B0.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+; ??0GameSpyGroupRoom@@QAE@XZ
+; Exact 106B @ 0x4FF9B0; true body (queued 0x888A27 was INSIDE nextToken@StringBase@G);
+; map::operator[] GroupRoom @0x6357D0 calls ILT 0x1DCC3->here; Ascii+Unicode empty + 5 ints
+_TEXT SEGMENT
+public ??0GameSpyGroupRoom@@QAE@XZ
+??0GameSpyGroupRoom@@QAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 43h, 0D8h, 02h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 53h, 56h, 8Bh, 0F1h, 33h, 0DBh, 57h, 89h, 74h, 24h
+    db 0Ch, 89h, 1Eh, 8Dh, 7Eh, 04h, 89h, 5Ch, 24h, 18h, 89h, 1Fh, 68h, 50h, 6Eh, 33h
+    db 01h, 0C6h, 44h, 24h, 1Ch, 01h, 0E8h, 0A5h, 82h, 38h, 00h, 68h, 54h, 6Eh, 33h, 01h
+    db 8Bh, 0CFh, 0E8h, 39h, 8Bh, 38h, 00h, 8Bh, 4Ch, 24h, 10h, 89h, 5Eh, 18h, 89h, 5Eh
+    db 14h, 89h, 5Eh, 10h, 89h, 5Eh, 0Ch, 89h, 5Eh, 08h, 5Fh, 8Bh, 0C6h, 5Eh, 5Bh, 64h
+    db 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??0GameSpyGroupRoom@@QAE@XZ ENDP
+_TEXT ENDS
+END
