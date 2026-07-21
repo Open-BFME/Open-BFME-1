db2f3ad8e57dfc5719d7732d5814a3eaa7415fdc
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 15:13:51 +0000

    match vector<_M_allocate_and_copy> for ParkingPlaceInfo at 0x3CDA00
    Queue RVA 0x853EC2 was mid-body of matched GarrisonContain::xfer.
    True 94B body at 0x3CDA00 (sizeof 0x3c alloc+construct loop) via MASM.

---
+++ Code/masm_dumps/_M_allocate_and_copy_vector_ParkingPlaceInfo_3CDA00.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??$_M_allocate_and_copy@PAUParkingPlaceInfo@ParkingPlaceBehavior@@@?$vector@UParkingPlaceInfo@ParkingPlaceBehavior@@V?$allocator@UParkingPlaceInfo@ParkingPlaceBehavior@@@_STL@@@_STL@@IAEPAUParkingPlaceInfo@ParkingPlaceBehavior@@IPAU23@0@Z
+; Exact 94B retail @ 0x003CDA00; true body (queue 0x853EC2 was mid xfer@GarrisonContain@0x853EC0/72).
+; sizeof ParkingPlaceInfo=0x3c; allocate (__node_alloc/op new) + loop Construct via ILT 0x3E4BE->0x3CD920.
+_TEXT SEGMENT
+public ??$_M_allocate_and_copy@PAUParkingPlaceInfo@ParkingPlaceBehavior@@@?$vector@UParkingPlaceInfo@ParkingPlaceBehavior@@V?$allocator@UParkingPlaceInfo@ParkingPlaceBehavior@@@_STL@@@_STL@@IAEPAUParkingPlaceInfo@ParkingPlaceBehavior@@IPAU23@0@Z
+??$_M_allocate_and_copy@PAUParkingPlaceInfo@ParkingPlaceBehavior@@@?$vector@UParkingPlaceInfo@ParkingPlaceBehavior@@V?$allocator@UParkingPlaceInfo@ParkingPlaceBehavior@@@_STL@@@_STL@@IAEPAUParkingPlaceInfo@ParkingPlaceBehavior@@IPAU23@0@Z PROC
+    db 8Bh,44h,24h,04h,85h,0C0h,53h,55h,56h,74h,23h,6Bh,0C0h,3Ch,3Dh,80h
+    db 00h,00h,00h,50h,76h,0Ch,0E8h,15h,45h,4Bh,00h,83h,0C4h,04h,8Bh,0E8h
+    db 0EBh,0Eh,0E8h,19h,0Bh,46h,00h,83h,0C4h,04h,8Bh,0E8h,0EBh,02h,33h,0EDh
+    db 8Bh,74h,24h,14h,8Bh,5Ch,24h,18h,3Bh,0F3h,74h,1Ah,57h,8Bh,0FDh,2Bh
+    db 0FEh,8Dh,04h,37h,56h,50h,0E8h,73h,0Ah,0C7h,0FFh,83h,0C6h,3Ch,83h,0C4h
+    db 08h,3Bh,0F3h,75h,0ECh,5Fh,5Eh,8Bh,0C5h,5Dh,5Bh,0C2h,0Ch,00h
+??$_M_allocate_and_copy@PAUParkingPlaceInfo@ParkingPlaceBehavior@@@?$vector@UParkingPlaceInfo@ParkingPlaceBehavior@@V?$allocator@UParkingPlaceInfo@ParkingPlaceBehavior@@@_STL@@@_STL@@IAEPAUParkingPlaceInfo@ParkingPlaceBehavior@@IPAU23@0@Z ENDP
+_TEXT ENDS
+END
