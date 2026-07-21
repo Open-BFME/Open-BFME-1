a0e534b8323deb11b398e446faa954e2148c8a99
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 13:01:07 +0000

    Match ??1PartitionManager@@UAE@XZ at 0x9F2800 (115B MASM).
    Queue RVA 0x9F27CE was int3 pad; true body is 0x9F2800 (already claimed as
    mislabeled TeamFactory dtor). Identity from dual vtables and getName returning
    "PartitionManager". Land as ICF alias of those exact retail bytes.

---
+++ Code/masm_dumps/PartitionManager_dtor.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ??1PartitionManager@@UAE@XZ
+; Exact 115 retail bytes @ 0x009F2800. Identity: primary/secondary vtables
+; adj. to getName returning "PartitionManager" @ VA 0x0114581C; scalar dtor @ 0x9F2900.
+; Same bytes as mislabeled ??1TeamFactory row (ICF alias).
+_TEXT SEGMENT
+public ??1PartitionManager@@UAE@XZ
+??1PartitionManager@@UAE@XZ PROC
+    db 6Ah,0FFh,68h,3Fh,1Eh,06h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h
+    db 56h,8Bh,0F1h,57h,89h,74h,24h,0Ch
+    db 0C7h,06h,0F8h,57h,14h,01h,0C7h,46h
+    db 08h,0E8h,57h,14h,01h,8Bh,7Eh,0Ch
+    db 85h,0FFh,0C7h,44h,24h,18h,01h,00h
+    db 00h,00h,74h,10h,8Bh,0CFh,0E8h,2Dh
+    db 31h,00h,00h,57h,0E8h,67h,0F6h,0E8h
+    db 0FFh,83h,0C4h,04h,8Bh,0CEh,0C7h,46h
+    db 08h,44h,37h,07h,01h,0C7h,44h,24h
+    db 18h,0FFh,0FFh,0FFh,0FFh,0E8h,0DEh,0F1h
+    db 0FAh,0FFh,8Bh,4Ch,24h,10h,5Fh,5Eh
+    db 64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,14h,0C3h
+??1PartitionManager@@UAE@XZ ENDP
+_TEXT ENDS
+END
