6f783cd9fae8e4efc3a87f007f736634065d47fe
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 11:27:11 +0000

    Match ??0DozerPrimaryIdleState@@QAE@PAVStateMachine@@@Z at 0x2B7680
    Structural queue RVA 0x2B7945 was a mid-inlined fragment inside the already-
    matched DozerPrimaryStateMachine ctor. True standalone body is 57B at
    0x2B7680 (thin AsciiString@0x888BC0 + State base); land via MASM exact dump.

---
+++ Code/masm_dumps/_sa___0DozerPrimaryIdleState__QAE_PAVStateMachine___Z_2B7680.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+; ??0DozerPrimaryIdleState@@QAE@PAVStateMachine@@@Z
+; Exact 57B @ 0x2B7680; structural queue pointed at mid-inlined fragment 0x2B7945 inside already-matched DozerPrimaryStateMachine ctor; true standalone body; thin AsciiString@0x888BC0 + State@thunk; Open-BFME fleet
+_TEXT SEGMENT
+public ??0DozerPrimaryIdleState@@QAE@PAVStateMachine@@@Z
+??0DozerPrimaryIdleState@@QAE@PAVStateMachine@@@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 54h, 6Fh, 0Ch, 01h
+    db 0E8h, 2Bh, 15h, 5Dh, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 11h, 0BFh, 0D4h
+    db 0FFh, 33h, 0C0h, 89h, 46h, 24h, 89h, 46h, 28h, 88h, 46h, 2Ch, 0C7h, 06h, 00h, 6Fh
+    db 0Ch, 01h, 8Bh, 0C6h, 5Eh, 59h, 0C2h, 04h, 00h
+??0DozerPrimaryIdleState@@QAE@PAVStateMachine@@@Z ENDP
+_TEXT ENDS
+END
