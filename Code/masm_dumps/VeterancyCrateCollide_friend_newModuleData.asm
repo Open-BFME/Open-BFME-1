c457b0888f3c631ad905b7c69d91279a2afed110
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 10:55:11 +0000

    Match friend_newModuleData@VeterancyCrateCollide at 0x125FD0.
    ModuleFactory registration ordinal 0xC6 binds the VeterancyCrateCollide
    data-factory thunk to the 129B body already claimed as ConvertToCarBomb;
    land as ICF alias. Queue drift RVA 0x12B63B is unreferenced int3 pad.

---
+++ Code/masm_dumps/VeterancyCrateCollide_friend_newModuleData.asm
@@ -0,0 +1,31 @@
+.386
+.model flat
+
+; ?friend_newModuleData@VeterancyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 129 retail bytes @ RVA 0x00125FD0; ModuleFactory registration ordinal 0xC6
+; (string "VeterancyCrateCollide") binds data factory thunk 0x74aa -> this body.
+; ICF-identical to ConvertToCarBombCrateCollide friend_newModuleData at same RVA.
+; Queue drift RVA 0x0012B63B is int3 pad (unreferenced); true body is 0x00125FD0.
+_TEXT SEGMENT
+public ?friend_newModuleData@VeterancyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@VeterancyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0ABh,018h,000h,001h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,056h
+    db 06Ah,060h,0E8h,041h,0BFh,075h,000h,08Bh
+    db 0F0h,083h,0C4h,004h,089h,074h,024h,008h
+    db 033h,0DBh,03Bh,0F3h,089h,05Ch,024h,014h
+    db 074h,01Fh,08Bh,0CEh,0E8h,0C1h,0E1h,0F1h
+    db 0FFh,0C7h,006h,040h,0E9h,008h,001h,089h
+    db 05Eh,054h,088h,05Eh,058h,088h,05Eh,059h
+    db 0C7h,046h,05Ch,00Ah,000h,000h,000h,0EBh
+    db 002h,033h,0F6h,08Bh,04Ch,024h,01Ch,03Bh
+    db 0CBh,0C7h,044h,024h,014h,0FFh,0FFh,0FFh
+    db 0FFh,074h,00Bh,068h,051h,027h,041h,000h
+    db 056h,0E8h,0F2h,0C0h,072h,000h,08Bh,04Ch
+    db 024h,00Ch,08Bh,0C6h,05Eh,05Bh,064h,089h
+    db 00Dh,000h,000h,000h,000h,083h,0C4h,010h
+    db 0C3h
+?friend_newModuleData@VeterancyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
