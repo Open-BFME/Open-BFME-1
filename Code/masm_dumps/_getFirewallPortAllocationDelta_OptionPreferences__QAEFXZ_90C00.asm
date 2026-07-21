054daeb37dbd78590072c577a17924c20bfe4d42
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 21:46:45 +0000

    Match getFirewallPortAllocationDelta@OptionPreferences via exact MASM
    True body is 95B at 0x90C00 (FirewallPortAllocationDelta string-anchor);
    queue RVA 0x66FEF3 was write-path interior, not the getter. C++ blocked by
    fat AsciiString(const char*) vs retail thin ctor at 0x888BC0.

---
+++ Code/masm_dumps/_getFirewallPortAllocationDelta_OptionPreferences__QAEFXZ_90C00.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+; ?getFirewallPortAllocationDelta@OptionPreferences@@QAEFXZ
+; Exact 95B @ 0x90C00; string-anchor FirewallPortAllocationDelta true body (queue 0x66FEF3 was interior of write-path)
+_TEXT SEGMENT
+public ?getFirewallPortAllocationDelta@OptionPreferences@@QAEFXZ
+?getFirewallPortAllocationDelta@OptionPreferences@@QAEFXZ PROC
+    db 051h, 056h, 057h, 08Bh, 0F1h, 068h, 03Ch, 0A1h, 007h, 001h, 08Dh, 04Ch, 024h, 00Ch, 0E8h, 0ADh
+    db 07Fh, 07Fh, 000h, 08Dh, 044h, 024h, 008h, 083h, 0C6h, 004h, 050h, 08Bh, 0CEh, 0E8h, 08Ah, 0A2h
+    db 0F7h, 0FFh, 08Dh, 04Ch, 024h, 008h, 08Bh, 0F8h, 0E8h, 013h, 06Dh, 07Fh, 000h, 03Bh, 03Eh, 075h
+    db 011h, 08Bh, 00Dh, 0C8h, 0D5h, 02Eh, 001h, 066h, 08Bh, 081h, 020h, 00Bh, 000h, 000h, 05Fh, 05Eh
+    db 059h, 0C3h, 08Bh, 07Fh, 014h, 085h, 0FFh, 08Dh, 047h, 008h, 075h, 005h, 0B8h, 08Bh, 038h, 007h
+    db 001h, 050h, 0FFh, 015h, 084h, 093h, 035h, 001h, 083h, 0C4h, 004h, 05Fh, 05Eh, 059h, 0C3h
+?getFirewallPortAllocationDelta@OptionPreferences@@QAEFXZ ENDP
+_TEXT ENDS
+END
