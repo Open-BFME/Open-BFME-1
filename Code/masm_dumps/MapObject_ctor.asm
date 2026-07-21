4d00ae0bf1bdb9400af56109dc5bc5aebe1b9a43
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 11:52:37 +0000

    Match MapObject::MapObject at true body 0x88160/386
    Queue RVA 0x88120/565 was a 21B helper plus padding into the
    SEH constructor. Land exact retail bytes via MASM; identity from
    ret 0x20 arg frame, normalizeAngle, 0xFF00 color, Dict@+0x24 and
    matched MapObject field accessors.

---
+++ Code/masm_dumps/MapObject_ctor.asm
@@ -0,0 +1,39 @@
+.386
+.model flat
+
+; ??0MapObject@@QAE@UCoord3D@@VAsciiString@@MHPBVDict@@PBVThingTemplate@@@Z
+; Exact 386 retail bytes @ 0x00088160
+; Identity: SEH ctor ret 0x20 (Coord3D+AsciiString+Real+Int+Dict*+ThingTemplate*);
+; normalizeAngle@0x991C; m_color=0xFF00; Dict@+0x24; towers/render/shadow zeros;
+; matches ZH MapObject::MapObject. Queue RVA 0x88120 was a 21B helper, not this body.
+_TEXT SEGMENT
+public ??0MapObject@@QAE@UCoord3D@@VAsciiString@@MHPBVDict@@PBVThingTemplate@@@Z
+??0MapObject@@QAE@UCoord3D@@VAsciiString@@MHPBVDict@@PBVThingTemplate@@@Z PROC
+    db 06Ah,0FFh,068h,0B1h,055h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,055h,056h,057h,08Bh,0F9h,033h,0F6h,089h,07Ch
+    db 024h,010h,089h,077h,004h,08Dh,05Fh,014h,0C7h,007h,0E0h,0C7h,007h,001h,089h,074h
+    db 024h,01Ch,089h,033h,08Dh,06Fh,024h,056h,08Bh,0CDh,0C6h,044h,024h,020h,001h,0E8h
+    db 029h,0ADh,0F7h,0FFh,089h,077h,05Ch,08Bh,04Ch,024h,024h,08Bh,054h,024h,028h,08Dh
+    db 047h,008h,089h,008h,08Bh,04Ch,024h,02Ch,089h,050h,004h,089h,048h,008h,08Bh,044h
+    db 024h,030h,08Dh,074h,024h,030h,0C6h,044h,024h,01Ch,003h,0E8h,070h,0FFh,0FFh,0FFh
+    db 050h,08Bh,0CBh,0C6h,044h,024h,020h,004h,0E8h,0B3h,0FAh,07Fh,000h,08Bh,0CEh,0C6h
+    db 044h,024h,01Ch,003h,0E8h,057h,0F7h,07Fh,000h,08Bh,044h,024h,034h,08Bh,054h,024h
+    db 040h,050h,089h,057h,018h,0E8h,022h,017h,0F8h,0FFh,0D9h,05Fh,01Ch,08Bh,044h,024h
+    db 040h,08Bh,04Ch,024h,03Ch,0BEh,001h,000h,000h,000h,033h,0DBh,083h,0C4h,004h,03Bh
+    db 0C3h,089h,04Fh,020h,089h,077h,054h,089h,077h,058h,074h,00Dh,050h,08Bh,0CDh,0E8h
+    db 042h,0D3h,0FBh,0FFh,0E9h,086h,000h,000h,000h,06Ah,064h,0B9h,0F8h,077h,02Ah,001h
+    db 0E8h,0CFh,010h,0F8h,0FFh,050h,08Bh,0CDh,0E8h,0CDh,087h,0F8h,0FFh,056h,0B9h,010h
+    db 078h,02Ah,001h,0E8h,0BCh,010h,0F8h,0FFh,050h,08Bh,0CDh,0E8h,014h,0EDh,0F7h,0FFh
+    db 053h,0B9h,018h,078h,02Ah,001h,0E8h,0A9h,010h,0F8h,0FFh,050h,08Bh,0CDh,0E8h,001h
+    db 0EDh,0F7h,0FFh,053h,0B9h,020h,078h,02Ah,001h,0E8h,096h,010h,0F8h,0FFh,050h,08Bh
+    db 0CDh,0E8h,0EEh,0ECh,0F7h,0FFh,056h,0B9h,040h,078h,02Ah,001h,0E8h,083h,010h,0F8h
+    db 0FFh,050h,08Bh,0CDh,0E8h,0DBh,0ECh,0F7h,0FFh,056h,0B9h,070h,078h,02Ah,001h,0E8h
+    db 070h,010h,0F8h,0FFh,050h,08Bh,0CDh,0E8h,0C8h,0ECh,0F7h,0FFh,053h,0B9h,028h,078h
+    db 02Ah,001h,0E8h,05Dh,010h,0F8h,0FFh,050h,08Bh,0CDh,0E8h,0B5h,0ECh,0F7h,0FFh,08Bh
+    db 04Ch,024h,014h,089h,05Fh,02Ch,089h,05Fh,030h,0C7h,047h,028h,000h,0FFh,000h,000h
+    db 089h,05Fh,034h,089h,05Fh,038h,089h,05Fh,03Ch,089h,05Fh,040h,089h,05Fh,044h,08Bh
+    db 0C7h,05Fh,05Eh,05Dh,05Bh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C2h
+    db 020h,000h
+??0MapObject@@QAE@UCoord3D@@VAsciiString@@MHPBVDict@@PBVThingTemplate@@@Z ENDP
+_TEXT ENDS
+END
