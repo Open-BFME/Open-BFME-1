842f3e70b0d3e7bbb66d8a55d127cc7ea5547436
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 23:52:55 +0000

    Match ??0ParticleSystemFXNugget@@QAE@XZ at 0x429F80 (350B MASM)
    Queue RVA 0x6004EF was interior of matched RenderObjectUpdateModuleInfo
    FXParticleSystem ctor. True body via FX type table ParticleSystem parse
    (new 0x128 + ctor); BFME-expanded layout, exact retail dump.

---
+++ Code/masm_dumps/ParticleSystemFXNugget_ctor_429F80.asm
@@ -0,0 +1,37 @@
+.386
+.model flat
+
+; ??0ParticleSystemFXNugget@@QAE@XZ
+; Exact 350 retail bytes @ 0x00429F80
+; True body via FX type table ParticleSystem -> ILT 0x42D4D -> parse 0x42CB80
+; parse: global new(0x128)+ctor ILT 0x2ADC4 -> this body; FieldParse Name@+0xB4..
+; Queue 0x6004EF was INSIDE matched ??0RenderObjectUpdateModuleInfo@FXParticleSystem
+; BFME rewrite (sizeof 0x128, many bone/target fields) — not ZH 180B C++ ctor
+_TEXT SEGMENT
+public ??0ParticleSystemFXNugget@@QAE@XZ
+??0ParticleSystemFXNugget@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,30h,14h,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,53h,55h,56h,8Bh,0F1h,57h,89h,74h,24h,10h
+    db 0E8h,3Bh,87h,0C1h,0FFh,33h,0DBh,8Dh,8Eh,0B4h,00h,00h,00h,0C7h,06h,20h
+    db 35h,0Fh,01h,89h,5Ch,24h,1Ch,89h,19h,89h,9Eh,0C8h,00h,00h,00h,89h
+    db 9Eh,0CCh,00h,00h,00h,89h,9Eh,0D0h,00h,00h,00h,8Dh,0BEh,0C8h,00h,00h
+    db 00h,8Dh,0AEh,0D4h,00h,00h,00h,89h,5Dh,00h,89h,5Dh,04h,89h,5Dh,08h
+    db 89h,9Eh,0E0h,00h,00h,00h,89h,9Eh,0E4h,00h,00h,00h,89h,9Eh,0E8h,00h
+    db 00h,00h,89h,9Eh,0FCh,00h,00h,00h,89h,9Eh,04h,01h,00h,00h,89h,9Eh
+    db 08h,01h,00h,00h,0C6h,44h,24h,1Ch,04h,0E8h,32h,0D9h,45h,00h,53h,53h
+    db 53h,8Bh,0CFh,0C7h,86h,0B8h,00h,00h,00h,01h,00h,00h,00h,0E8h,36h,0CCh
+    db 0BDh,0FFh,53h,53h,53h,8Bh,0CDh,0E8h,2Ch,0CCh,0BDh,0FFh,53h,68h,00h,00h
+    db 80h,0BFh,68h,00h,00h,80h,0BFh,8Dh,8Eh,0E0h,00h,00h,00h,0E8h,16h,0CCh
+    db 0BDh,0FFh,8Bh,4Ch,24h,14h,5Fh,89h,9Eh,0C4h,00h,00h,00h,89h,9Eh,0C0h
+    db 00h,00h,00h,89h,9Eh,0BCh,00h,00h,00h,88h,9Eh,0F8h,00h,00h,00h,88h
+    db 9Eh,0F9h,00h,00h,00h,88h,9Eh,00h,01h,00h,00h,89h,9Eh,0F4h,00h,00h
+    db 00h,89h,9Eh,0F0h,00h,00h,00h,89h,9Eh,0ECh,00h,00h,00h,88h,9Eh,0Ch
+    db 01h,00h,00h,88h,9Eh,18h,01h,00h,00h,89h,9Eh,24h,01h,00h,00h,89h
+    db 9Eh,20h,01h,00h,00h,89h,9Eh,1Ch,01h,00h,00h,88h,9Eh,19h,01h,00h
+    db 00h,88h,9Eh,1Ah,01h,00h,00h,88h,9Eh,1Bh,01h,00h,00h,88h,9Eh,01h
+    db 01h,00h,00h,0C7h,46h,04h,08h,00h,00h,00h,0C7h,86h,10h,01h,00h,00h
+    db 00h,00h,80h,3Fh,0C7h,86h,14h,01h,00h,00h,0FFh,0FFh,0FFh,0FFh,8Bh,0C6h
+    db 5Eh,5Dh,5Bh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h,0C3h
+??0ParticleSystemFXNugget@@QAE@XZ ENDP
+_TEXT ENDS
+END
