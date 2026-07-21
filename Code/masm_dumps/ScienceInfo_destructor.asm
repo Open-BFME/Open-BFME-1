39f248a20b08b6a5972a22b7a34d5ef4538af2d2
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 21:01:59 +0000

    Match ??1ScienceInfo@@MAE@XZ at true body 0xE8520 (127B MASM)
    Queue RVA 0x6F4C1A was misplaced (mid-function / W3DDisplayString region).
    True dtor recovered via Science FieldParse table and parseScienceDefinition
    inlined ctor (sizeof 0x30, vtbl 0x1084CC0); scalar deleting dtor at 0xE84F0
    calls through ILT 0x36A4D into this body.

---
+++ Code/masm_dumps/ScienceInfo_destructor.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1ScienceInfo@@MAE@XZ
+; Retail @ 0x000E8520 size 127
+; Identity: ScienceInfo FieldParse@0xC84D40 (name@0x10 desc@0x14 prereq@0x18 cost@0x24 costMP@0x28 grant@0x2c);
+; ctor in parseScienceDefinition@0xE8630 inlines sizeof 0x30 + vtbl 0x1084CC0; ??_G@0xE84F0 -> ILT 0x36A4D -> this body
+_TEXT SEGMENT
+public ??1ScienceInfo@@MAE@XZ
+??1ScienceInfo@@MAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 5Eh, 0B3h, 0FFh, 00h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 89h, 74h, 24h, 04h, 0C7h, 06h, 0C0h
+    db 4Ch, 08h, 01h, 8Dh, 4Eh, 18h, 0C7h, 44h, 24h, 10h, 02h, 00h, 00h, 00h, 0E8h, 05h
+    db 0E6h, 0F2h, 0FFh, 8Dh, 4Eh, 14h, 0C6h, 44h, 24h, 10h, 01h, 0E8h, 70h, 0FCh, 79h, 00h
+    db 8Dh, 4Eh, 10h, 0C6h, 44h, 24h, 10h, 00h, 0E8h, 63h, 0FCh, 79h, 00h, 8Bh, 4Eh, 04h
+    db 85h, 0C9h, 0C7h, 44h, 24h, 10h, 0FFh, 0FFh, 0FFh, 0FFh, 0C7h, 06h, 0B0h, 0FCh, 07h, 01h
+    db 74h, 06h, 8Bh, 01h, 6Ah, 01h, 0FFh, 10h, 8Bh, 4Ch, 24h, 08h, 0C7h, 46h, 04h, 00h
+    db 00h, 00h, 00h, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1ScienceInfo@@MAE@XZ ENDP
+_TEXT ENDS
+END
