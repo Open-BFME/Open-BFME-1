bb92e494ef4e133c01998cfdb5c59a5e1d3fc5a9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 00:18:32 +0000

    Match __uninitialized_fill_n<ModelConditionInfo*> via exact MASM dump
    Queue RVA 0x853EE2 was mid-SEH of already-matched GarrisonContain::xfer.
    True body is 44B at 0x774820: Construct loop with sizeof 0xBC, found via
    Construct@0x13A760 (copy@0x772E10) and parseConditionState imul*0xBC.

---
+++ Code/masm_dumps/_____uninitialized_fill_n_PAUModelConditionInfo__IU1___STL__YAPAUModelConditionInfo__PAU1_IABU1_ABU__false_type_0__Z_774820.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??$__uninitialized_fill_n@PAUModelConditionInfo@@IU1@@_STL@@YAPAUModelConditionInfo@@PAU1@IABU1@ABU__false_type@0@@Z
+; Exact 44 retail bytes @ 0x00774820 (true body; queue 0x853EE2 was mid-SEH of matched GarrisonContain::xfer)
+; Construct loop sizeof 0xBC via Construct@0x13A760 -> copy@0x772E10; sizeof proven by imul in parseConditionState
+_TEXT SEGMENT
+public ??$__uninitialized_fill_n@PAUModelConditionInfo@@IU1@@_STL@@YAPAUModelConditionInfo@@PAU1@IABU1@ABU__false_type@0@@Z
+??$__uninitialized_fill_n@PAUModelConditionInfo@@IU1@@_STL@@YAPAUModelConditionInfo@@PAU1@IABU1@ABU__false_type@0@@Z PROC
+    db 056h,08Bh,074h,024h,008h,057h,08Bh,07Ch,024h,010h,085h,0FFh,076h,019h,053h,08Bh
+    db 05Ch,024h,018h,053h,056h,0E8h,094h,02Dh,08Ch,0FFh,083h,0C4h,008h,081h,0C6h,0BCh
+    db 000h,000h,000h,04Fh,075h,0EDh,05Bh,05Fh,08Bh,0C6h,05Eh,0C3h
+??$__uninitialized_fill_n@PAUModelConditionInfo@@IU1@@_STL@@YAPAUModelConditionInfo@@PAU1@IABU1@ABU__false_type@0@@Z ENDP
+_TEXT ENDS
+END
