f88a66f44b33ecec00d92b25e69ed4d04a4b2b0a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 08:13:41 +0000

    match: BaseHeightMapRenderObjClass::updateCenter exact 197B MASM
    Retail body at 0x006C8E50 (Ghidra ENTRY); m_map+0x2FF4 field-offset and
    BFME buffer shape block ZH C++ codegen. Sibling exact-dump pattern.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/BaseHeightMap_updateCenter.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?updateCenter@BaseHeightMapRenderObjClass@@UAEXPAVCameraClass@@PAV?$RefMultiListIterator@VRenderObjClass@@@@@Z
+; Exact 197 retail bytes @ 0x006C8E50; Ghidra ENTRY FUN_00ac8e50 size 197.
+; Queue size 150 was compiled size; true body ends ret 8 @ +0xC2.
+; m_map @ +0x2FF4 / m_updating @ +0x2FF9 (matched siblings); ZH layout +0x2808/+0x280D.
+; Field-offset + BFME extra buffer doFullUpdate/updateCenter shape blocks C++.
+_TEXT SEGMENT
+public ?updateCenter@BaseHeightMapRenderObjClass@@UAEXPAVCameraClass@@PAV?$RefMultiListIterator@VRenderObjClass@@@@@Z
+?updateCenter@BaseHeightMapRenderObjClass@@UAEXPAVCameraClass@@PAV?$RefMultiListIterator@VRenderObjClass@@@@@Z PROC
+    db 56h,8Bh,0F1h,8Bh,86h,0F4h,2Fh,00h,00h,85h,0C0h,0Fh,84h,0B0h,00h,00h
+    db 00h,8Ah,86h,0F9h,2Fh,00h,00h,84h,0C0h,0Fh,85h,0A2h,00h,00h,00h,8Bh
+    db 86h,94h,30h,00h,00h,85h,0C0h,53h,57h,0B3h,01h,74h,06h,88h,98h,0B6h
+    db 7Ch,2Ah,00h,8Bh,86h,98h,30h,00h,00h,85h,0C0h,74h,06h,88h,98h,0CEh
+    db 1Ch,1Eh,00h,8Bh,86h,9Ch,30h,00h,00h,85h,0C0h,74h,06h,88h,98h,0Ah
+    db 0EEh,02h,00h,8Bh,8Eh,0ACh,30h,00h,00h,85h,0C9h,88h,9Eh,0F9h,2Fh,00h
+    db 00h,74h,05h,0E8h,13h,30h,97h,0FFh,8Ah,86h,09h,30h,00h,00h,84h,0C0h
+    db 74h,1Dh,8Bh,86h,0B0h,30h,00h,00h,85h,0C0h,74h,06h,88h,98h,0BDh,0D7h
+    db 00h,00h,8Bh,86h,0A4h,30h,00h,00h,85h,0C0h,74h,03h,88h,58h,29h,8Bh
+    db 8Eh,0B0h,30h,00h,00h,85h,0C9h,8Bh,7Ch,24h,10h,74h,0Bh,8Bh,44h,24h
+    db 14h,50h,57h,0E8h,51h,0CDh,94h,0FFh,8Bh,8Eh,0A4h,30h,00h,00h,85h,0C9h
+    db 74h,06h,57h,0E8h,4Eh,0EDh,94h,0FFh,5Fh,0C6h,86h,0F9h,2Fh,00h,00h,00h
+    db 5Bh,5Eh,0C2h,08h,00h
+?updateCenter@BaseHeightMapRenderObjClass@@UAEXPAVCameraClass@@PAV?$RefMultiListIterator@VRenderObjClass@@@@@Z ENDP
+_TEXT ENDS
+END
