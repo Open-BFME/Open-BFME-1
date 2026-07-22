5c115f01870792ed0d27f46a44cdbddb69a8210d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 05:21:41 +0000

    match getMaxCellHeight@BaseHeightMapRenderObjClass (290B MASM @ 0x006C6CB0)
    Queue RVA 0x008B440D was mid foreign epilogue; true body uses m_map@+0x2FF4
    like matched siblings. Field-offset drift blocks C++; exact retail dump.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/BaseHeightMap_getMaxCellHeight.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+
+; ?getMaxCellHeight@BaseHeightMapRenderObjClass@@QBEMMM@Z
+; Exact 290 retail bytes @ 0x006C6CB0; queue RVA 0x008B440D was MISPLACED
+; (mid foreign epilogue). m_map @ +0x2FF4 (matched siblings); ZH layout +0x2808.
+; Retail uses m_map only (no TheTerrainVisual/getLogicHeightMap). Field-offset blocks C++.
+_TEXT SEGMENT
+public ?getMaxCellHeight@BaseHeightMapRenderObjClass@@QBEMMM@Z
+?getMaxCellHeight@BaseHeightMapRenderObjClass@@QBEMMM@Z PROC
+    db 083h,0ECh,008h,053h,055h,08Bh,0E9h,08Bh,09Dh,0F4h,02Fh,000h,000h,085h,0DBh,075h
+    db 00Eh,0D9h,005h,050h,053h,007h,001h,05Dh,05Bh,083h,0C4h,008h,0C2h,008h,000h,0D9h
+    db 044h,024h,014h,08Bh,085h,0F4h,02Fh,000h,000h,0D8h,00Dh,078h,02Eh,00Ch,001h,056h
+    db 057h,08Bh,078h,010h,0E8h,04Fh,001h,033h,000h,0D9h,044h,024h,020h,0D8h,00Dh,078h
+    db 02Eh,00Ch,001h,08Bh,0F7h,02Bh,0F0h,0E8h,03Ch,001h,033h,000h,02Bh,0F8h,085h,0F6h
+    db 08Bh,0CFh,07Dh,002h,033h,0F6h,085h,0C9h,07Dh,002h,033h,0C9h,08Bh,043h,008h,08Dh
+    db 050h,0FFh,03Bh,0F2h,07Ch,003h,08Dh,070h,0FEh,08Bh,043h,00Ch,08Dh,050h,0FFh,03Bh
+    db 0CAh,07Ch,003h,08Dh,048h,0FEh,08Bh,095h,0F4h,02Fh,000h,000h,08Bh,052h,008h,08Bh
+    db 043h,024h,08Bh,0FAh,00Fh,0AFh,0F9h,003h,0FEh,00Fh,0B7h,05Ch,078h,002h,041h,00Fh
+    db 0AFh,0CAh,089h,05Ch,024h,01Ch,003h,0CEh,00Fh,0B7h,054h,048h,002h,0DBh,044h,024h
+    db 01Ch,00Fh,0B7h,00Ch,048h,0D8h,00Dh,03Ch,065h,00Fh,001h,089h,054h,024h,020h,00Fh
+    db 0B7h,014h,078h,0D9h,05Ch,024h,01Ch,0DBh,044h,024h,020h,089h,04Ch,024h,010h,089h
+    db 054h,024h,014h,0D8h,00Dh,03Ch,065h,00Fh,001h,05Fh,05Eh,0D9h,05Ch,024h,018h,0DBh
+    db 044h,024h,008h,0D8h,00Dh,03Ch,065h,00Fh,001h,0D9h,05Ch,024h,008h,0DBh,044h,024h
+    db 00Ch,0D8h,00Dh,03Ch,065h,00Fh,001h,0D8h,054h,024h,014h,0DFh,0E0h,0F6h,0C4h,041h
+    db 074h,006h,0DDh,0D8h,0D9h,044h,024h,014h,0D8h,054h,024h,018h,0DFh,0E0h,0F6h,0C4h
+    db 041h,074h,006h,0DDh,0D8h,0D9h,044h,024h,018h,0D8h,054h,024h,008h,0DFh,0E0h,0F6h
+    db 0C4h,041h,074h,006h,0DDh,0D8h,0D9h,044h,024h,008h,05Dh,05Bh,083h,0C4h,008h,0C2h
+    db 008h,000h
+?getMaxCellHeight@BaseHeightMapRenderObjClass@@QBEMMM@Z ENDP
+_TEXT ENDS
+END
