051609981ee8289cbbf93580b0147aab5c866a6f
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 02:10:29 +0000

    match hashtable::resize(Relationship) via exact MASM @ 0x9EE4D0
    Queue RVA 0x853E06 was INSIDE another function; true body is 281B at
    0x9EE4D0 (direct call from matched _M_insert@0x9EE9B0). C++ emits SEH
    prologue (331B) vs retail frame-less shape — same MASM path as sibling
    _M_insert.

---
+++ Code/masm_dumps/___resize___hashtable_U__pair___CBIW4Relationship_9EE4D0.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+; ?resize@?$hashtable@U?$pair@$$CBIW4Relationship@@@_STL@@IU?$hash@I@2@U?$_Select1st@U?$pair@$$CBIW4Relationship@@@_STL@@@2@U?$equal_to@I@2@V?$allocator@U?$pair@$$CBIW4Relationship@@@_STL@@@2@@_STL@@QAEXI@Z
+; Exact 281B @ 0x9EE4D0; via _M_insert@0x9EE9B0 call; SEH-blocked C++ Open-BFME+Grok
+_TEXT SEGMENT
+public ?resize@?$hashtable@U?$pair@$$CBIW4Relationship@@@_STL@@IU?$hash@I@2@U?$_Select1st@U?$pair@$$CBIW4Relationship@@@_STL@@@2@U?$equal_to@I@2@V?$allocator@U?$pair@$$CBIW4Relationship@@@_STL@@@2@@_STL@@QAEXI@Z
+?resize@?$hashtable@U?$pair@$$CBIW4Relationship@@@_STL@@IU?$hash@I@2@U?$_Select1st@U?$pair@$$CBIW4Relationship@@@_STL@@@2@U?$equal_to@I@2@V?$allocator@U?$pair@$$CBIW4Relationship@@@_STL@@@2@@_STL@@QAEXI@Z PROC
+    db 083h, 0ECh, 018h, 08Bh, 044h, 024h, 01Ch, 053h, 08Bh, 059h, 004h, 056h, 08Bh, 071h, 008h, 057h
+    db 08Dh, 079h, 004h, 02Bh, 0F3h, 0C1h, 0FEh, 002h, 03Bh, 0C6h, 089h, 074h, 024h, 014h, 00Fh, 086h
+    db 0ECh, 000h, 000h, 000h, 06Ah, 000h, 089h, 044h, 024h, 02Ch, 08Bh, 044h, 024h, 02Ch, 050h, 08Dh
+    db 04Ch, 024h, 030h, 051h, 068h, 0E0h, 058h, 007h, 001h, 068h, 070h, 058h, 007h, 001h, 0E8h, 046h
+    db 071h, 062h, 0FFh, 083h, 0C4h, 014h, 03Dh, 0E0h, 058h, 007h, 001h, 0BBh, 0FBh, 0FFh, 0FFh, 0FFh
+    db 074h, 002h, 08Bh, 018h, 03Bh, 0DEh, 089h, 05Ch, 024h, 00Ch, 00Fh, 086h, 0B0h, 000h, 000h, 000h
+    db 055h, 08Dh, 054h, 024h, 02Ch, 052h, 08Bh, 0CFh, 0C7h, 044h, 024h, 018h, 000h, 000h, 000h, 000h
+    db 0E8h, 0E3h, 0B6h, 061h, 0FFh, 050h, 08Dh, 044h, 024h, 018h, 050h, 053h, 08Dh, 04Ch, 024h, 028h
+    db 0E8h, 047h, 01Eh, 063h, 0FFh, 08Bh, 06Ch, 024h, 01Ch, 033h, 0DBh, 085h, 0F6h, 076h, 03Ch, 090h
+    db 08Bh, 00Fh, 08Bh, 00Ch, 099h, 085h, 0C9h, 074h, 02Dh, 08Bh, 017h, 08Dh, 034h, 09Ah, 08Bh, 0FFh
+    db 08Bh, 041h, 004h, 033h, 0D2h, 0F7h, 074h, 024h, 010h, 08Bh, 001h, 089h, 006h, 08Bh, 044h, 095h
+    db 000h, 089h, 001h, 089h, 04Ch, 095h, 000h, 08Bh, 00Fh, 08Dh, 034h, 099h, 08Bh, 00Eh, 085h, 0C9h
+    db 075h, 0DEh, 08Bh, 074h, 024h, 018h, 043h, 03Bh, 0DEh, 072h, 0C5h, 08Bh, 00Fh, 085h, 0C9h, 08Bh
+    db 054h, 024h, 020h, 089h, 02Fh, 089h, 057h, 004h, 08Bh, 054h, 024h, 024h, 08Bh, 047h, 008h, 089h
+    db 057h, 008h, 05Dh, 074h, 02Bh, 02Bh, 0C1h, 0C1h, 0F8h, 002h, 0C1h, 0E0h, 002h, 03Dh, 080h, 000h
+    db 000h, 000h, 076h, 012h, 051h, 0E8h, 0E6h, 038h, 0E9h, 0FFh, 083h, 0C4h, 004h, 05Fh, 05Eh, 05Bh
+    db 083h, 0C4h, 018h, 0C2h, 004h, 000h, 050h, 051h, 0E8h, 013h, 000h, 0E4h, 0FFh, 083h, 0C4h, 008h
+    db 05Fh, 05Eh, 05Bh, 083h, 0C4h, 018h, 0C2h, 004h, 000h
+?resize@?$hashtable@U?$pair@$$CBIW4Relationship@@@_STL@@IU?$hash@I@2@U?$_Select1st@U?$pair@$$CBIW4Relationship@@@_STL@@@2@U?$equal_to@I@2@V?$allocator@U?$pair@$$CBIW4Relationship@@@_STL@@@2@@_STL@@QAEXI@Z ENDP
+_TEXT ENDS
+END
