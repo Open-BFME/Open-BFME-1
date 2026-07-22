d44b54a80a0ed329aee37999b7585f2423d7e867
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 05:13:31 +0000

    Match hashtable::resize AsciiString/AudioEventInfo at 0xB87C0.
    Queue RVA 0x853DD8 was mid-body of INI-parse; true 418B frameless-SEH
    body found via _M_insert@0xB8AE0. MASM exact (C++ SEH-blocked).

---
+++ Code/masm_dumps/___resize___hashtable_AsciiString_AudioEventInfo_B87C0.asm
@@ -0,0 +1,40 @@
+.386
+.model flat
+; ?resize@?$hashtable@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@VAsciiString@@U?$hash@VAsciiString@@@rts@@U?$_Select1st@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@@2@U?$equal_to@VAsciiString@@@5@V?$allocator@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@@2@@_STL@@QAEXI@Z
+; Exact 418B @ 0x000B87C0; queue 0x853DD8 MISPLACED inside INI-parse @0x853CB0.
+; True body via _M_insert@0xB8AE0 -> ILT 0x3AB93; sibling bkt_num_key 0x103F7->0xB8670; node 0x0C.
+; Frameless-SEH shape; C++ emits EBP-frame 377B (SEH-blocked). object-symbol trunc 240 for ML A2043.
+; Open-BFME+Grok structural
+_TEXT SEGMENT
+public ?resize@?$hashtable@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@VAsciiString@@U?$hash@VAsciiString@@@rts@@U?$_Select1st@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@@2@U?$equal_to@VAsciiString@@@5@V?$allocator@U?$pair@$$CBVAs
+?resize@?$hashtable@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@VAsciiString@@U?$hash@VAsciiString@@@rts@@U?$_Select1st@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@@2@U?$equal_to@VAsciiString@@@5@V?$allocator@U?$pair@$$CBVAs PROC
+    db 06Ah, 0FFh, 068h, 058h, 08Ah, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 018h, 08Bh, 044h, 024h, 028h, 055h, 056h, 08Bh, 071h
+    db 004h, 057h, 08Bh, 079h, 008h, 08Dh, 069h, 004h, 02Bh, 0FEh, 0C1h, 0FFh, 002h, 03Bh, 0C7h, 089h
+    db 07Ch, 024h, 014h, 00Fh, 086h, 055h, 001h, 000h, 000h, 053h, 089h, 044h, 024h, 038h, 08Bh, 044h
+    db 024h, 038h, 033h, 0DBh, 053h, 050h, 08Dh, 04Ch, 024h, 040h, 051h, 068h, 0E0h, 058h, 007h, 001h
+    db 068h, 070h, 058h, 007h, 001h, 0E8h, 03Fh, 0CEh, 0F5h, 0FFh, 083h, 0C4h, 014h, 03Dh, 0E0h, 058h
+    db 007h, 001h, 075h, 00Bh, 0BEh, 0FBh, 0FFh, 0FFh, 0FFh, 089h, 074h, 024h, 010h, 0EBh, 008h, 08Bh
+    db 010h, 089h, 054h, 024h, 010h, 08Bh, 0F2h, 03Bh, 0F7h, 00Fh, 086h, 00Eh, 001h, 000h, 000h, 08Dh
+    db 044h, 024h, 038h, 050h, 08Bh, 0CDh, 089h, 05Ch, 024h, 018h, 0E8h, 0D9h, 013h, 0F5h, 0FFh, 050h
+    db 08Dh, 04Ch, 024h, 018h, 051h, 056h, 08Dh, 04Ch, 024h, 028h, 0E8h, 03Dh, 07Bh, 0F6h, 0FFh, 03Bh
+    db 0FBh, 089h, 05Ch, 024h, 030h, 00Fh, 086h, 084h, 000h, 000h, 000h, 0EBh, 003h, 08Dh, 049h, 000h
+    db 08Bh, 045h, 000h, 08Bh, 034h, 098h, 085h, 0F6h, 074h, 070h, 08Dh, 09Bh, 000h, 000h, 000h, 000h
+    db 08Dh, 056h, 004h, 052h, 08Dh, 04Ch, 024h, 018h, 0E8h, 0D3h, 0F2h, 07Ch, 000h, 08Bh, 04Ch, 024h
+    db 014h, 085h, 0C9h, 074h, 005h, 083h, 0C1h, 008h, 0EBh, 005h, 0B9h, 08Bh, 038h, 007h, 001h, 08Ah
+    db 001h, 033h, 0FFh, 084h, 0C0h, 074h, 010h, 00Fh, 0BEh, 0C0h, 08Dh, 03Ch, 0BFh, 003h, 0F8h, 08Ah
+    db 041h, 001h, 041h, 084h, 0C0h, 075h, 0F0h, 08Dh, 04Ch, 024h, 014h, 0E8h, 080h, 0F0h, 07Ch, 000h
+    db 033h, 0D2h, 08Bh, 0C7h, 0F7h, 074h, 024h, 010h, 08Bh, 00Eh, 08Bh, 045h, 000h, 089h, 00Ch, 098h
+    db 08Bh, 044h, 024h, 01Ch, 08Bh, 00Ch, 090h, 089h, 00Eh, 089h, 034h, 090h, 08Bh, 045h, 000h, 08Bh
+    db 034h, 098h, 085h, 0F6h, 075h, 09Ah, 08Bh, 07Ch, 024h, 018h, 043h, 03Bh, 0DFh, 072h, 081h, 08Bh
+    db 04Dh, 000h, 085h, 0C9h, 08Bh, 054h, 024h, 01Ch, 08Bh, 044h, 024h, 020h, 089h, 055h, 000h, 08Bh
+    db 054h, 024h, 024h, 089h, 045h, 004h, 08Bh, 045h, 008h, 089h, 055h, 008h, 0C7h, 044h, 024h, 030h
+    db 0FFh, 0FFh, 0FFh, 0FFh, 074h, 037h, 02Bh, 0C1h, 0C1h, 0F8h, 002h, 0C1h, 0E0h, 002h, 03Dh, 080h
+    db 000h, 000h, 000h, 076h, 01Eh, 051h, 0E8h, 085h, 095h, 07Ch, 000h, 083h, 0C4h, 004h, 05Bh, 05Fh
+    db 05Eh, 05Dh, 08Bh, 04Ch, 024h, 018h, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 024h
+    db 0C2h, 004h, 000h, 050h, 051h, 0E8h, 0A6h, 05Ch, 077h, 000h, 083h, 0C4h, 008h, 05Bh, 08Bh, 04Ch
+    db 024h, 024h, 05Fh, 05Eh, 05Dh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 024h, 0C2h
+    db 004h, 000h
+?resize@?$hashtable@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@VAsciiString@@U?$hash@VAsciiString@@@rts@@U?$_Select1st@U?$pair@$$CBVAsciiString@@PAUAudioEventInfo@@@_STL@@@2@U?$equal_to@VAsciiString@@@5@V?$allocator@U?$pair@$$CBVAs ENDP
+_TEXT ENDS
+END
