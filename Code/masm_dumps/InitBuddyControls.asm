409be6213bdcebcc2498be2d836d516ad138a486
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 07:41:26 +0000

    Match InitBuddyControls via exact MASM at true body 0x4EA9D0
    Queue RVA 0x8CC0A was misplaced; retail body is 776B at 0x4EA9D0
    (Diplomacy/WOLBuddy TextEntryChat string xrefs + SEH). C++ blocked by
    BFME AsciiString +8 payload vs ZH +4 and regalloc; all REL32 callees resolved.

---
+++ Code/masm_dumps/InitBuddyControls.asm
@@ -0,0 +1,60 @@
+.386
+.model flat
+
+; ?InitBuddyControls@@YAXH@Z
+; Exact 776 retail bytes @ 0x004EA9D0; string xrefs Diplomacy/WOLBuddy TextEntryChat prove ownership.
+_TEXT SEGMENT
+public ?InitBuddyControls@@YAXH@Z
+?InitBuddyControls@@YAXH@Z PROC
+    db 64h, 0A1h, 00h, 00h, 00h, 00h, 6Ah, 0FFh, 68h, 40h, 0BCh, 02h, 01h, 50h, 0A1h, 94h
+    db 71h, 2Fh, 01h, 64h, 89h, 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 1Ch, 56h, 57h, 33h
+    db 0FFh, 3Bh, 0C7h, 0Fh, 84h, 0A3h, 02h, 00h, 00h, 8Bh, 44h, 24h, 34h, 83h, 0CEh, 0FFh
+    db 3Bh, 0C6h, 0Fh, 84h, 94h, 02h, 00h, 00h, 3Bh, 0C7h, 0Fh, 84h, 38h, 01h, 00h, 00h
+    db 83h, 0F8h, 01h, 0Fh, 85h, 0AEh, 02h, 00h, 00h, 68h, 4Ch, 21h, 10h, 01h, 8Dh, 4Ch
+    db 24h, 0Ch, 0E8h, 99h, 0E1h, 39h, 00h, 8Bh, 0Dh, 00h, 0D6h, 2Eh, 01h, 8Dh, 44h, 24h
+    db 08h, 50h, 0C7h, 44h, 24h, 30h, 03h, 00h, 00h, 00h, 0E8h, 9Eh, 0F7h, 0B5h, 0FFh, 8Dh
+    db 4Ch, 24h, 08h, 0A3h, 74h, 42h, 2Fh, 01h, 89h, 74h, 24h, 2Ch, 0E8h, 0EFh, 0CEh, 39h
+    db 00h, 0A1h, 74h, 42h, 2Fh, 01h, 8Bh, 0Dh, 40h, 1Bh, 2Fh, 01h, 8Bh, 11h, 50h, 57h
+    db 0FFh, 92h, 0DCh, 00h, 00h, 00h, 68h, 28h, 21h, 10h, 01h, 8Dh, 4Ch, 24h, 10h, 0A3h
+    db 70h, 42h, 2Fh, 01h, 0E8h, 47h, 0E1h, 39h, 00h, 8Dh, 4Ch, 24h, 0Ch, 51h, 8Bh, 0Dh
+    db 00h, 0D6h, 2Eh, 01h, 0C7h, 44h, 24h, 30h, 04h, 00h, 00h, 00h, 0E8h, 4Ch, 0F7h, 0B5h
+    db 0FFh, 8Dh, 4Ch, 24h, 0Ch, 0A3h, 6Ch, 42h, 2Fh, 01h, 89h, 74h, 24h, 2Ch, 0E8h, 9Dh
+    db 0CEh, 39h, 00h, 68h, 00h, 21h, 10h, 01h, 8Dh, 4Ch, 24h, 14h, 0E8h, 0Fh, 0E1h, 39h
+    db 00h, 8Bh, 0Dh, 00h, 0D6h, 2Eh, 01h, 8Dh, 54h, 24h, 10h, 52h, 0C7h, 44h, 24h, 30h
+    db 05h, 00h, 00h, 00h, 0E8h, 14h, 0F7h, 0B5h, 0FFh, 8Dh, 4Ch, 24h, 10h, 0A3h, 64h, 42h
+    db 2Fh, 01h, 89h, 74h, 24h, 2Ch, 0E8h, 65h, 0CEh, 39h, 00h, 8Bh, 15h, 6Ch, 42h, 2Fh
+    db 01h, 8Bh, 0Dh, 40h, 1Bh, 2Fh, 01h, 8Bh, 01h, 52h, 57h, 0FFh, 90h, 0DCh, 00h, 00h
+    db 00h, 8Bh, 15h, 64h, 42h, 2Fh, 01h, 8Bh, 0Dh, 40h, 1Bh, 2Fh, 01h, 52h, 0A3h, 68h
+    db 42h, 2Fh, 01h, 8Bh, 01h, 57h, 0FFh, 90h, 0DCh, 00h, 00h, 00h, 51h, 89h, 64h, 24h
+    db 24h, 8Bh, 0CCh, 68h, 54h, 6Eh, 33h, 01h, 0A3h, 60h, 42h, 2Fh, 01h, 0E8h, 0DEh, 0D8h
+    db 39h, 00h, 0A1h, 70h, 42h, 2Fh, 01h, 50h, 0E8h, 47h, 1Dh, 0B2h, 0FFh, 83h, 0C4h, 08h
+    db 0C6h, 05h, 78h, 42h, 2Fh, 01h, 01h, 8Bh, 4Ch, 24h, 24h, 64h, 89h, 0Dh, 00h, 00h
+    db 00h, 00h, 5Fh, 5Eh, 83h, 0C4h, 28h, 0C3h, 68h, 0D8h, 20h, 10h, 01h, 8Dh, 4Ch, 24h
+    db 18h, 0E8h, 6Ah, 0E0h, 39h, 00h, 8Bh, 44h, 24h, 14h, 3Bh, 0C7h, 89h, 7Ch, 24h, 2Ch
+    db 74h, 05h, 83h, 0C0h, 08h, 0EBh, 05h, 0B8h, 8Bh, 38h, 07h, 01h, 8Bh, 0Dh, 00h, 0D6h
+    db 2Eh, 01h, 50h, 0E8h, 5Fh, 02h, 0B5h, 0FFh, 8Dh, 4Ch, 24h, 14h, 0A3h, 74h, 42h, 2Fh
+    db 01h, 89h, 74h, 24h, 2Ch, 0E8h, 0B6h, 0CDh, 39h, 00h, 0A1h, 74h, 42h, 2Fh, 01h, 8Bh
+    db 0Dh, 40h, 1Bh, 2Fh, 01h, 8Bh, 11h, 50h, 57h, 0FFh, 92h, 0DCh, 00h, 00h, 00h, 68h
+    db 0ACh, 20h, 10h, 01h, 8Dh, 4Ch, 24h, 1Ch, 0A3h, 70h, 42h, 2Fh, 01h, 0E8h, 0Eh, 0E0h
+    db 39h, 00h, 8Bh, 44h, 24h, 18h, 3Bh, 0C7h, 0C7h, 44h, 24h, 2Ch, 01h, 00h, 00h, 00h
+    db 74h, 05h, 83h, 0C0h, 08h, 0EBh, 05h, 0B8h, 8Bh, 38h, 07h, 01h, 8Bh, 0Dh, 00h, 0D6h
+    db 2Eh, 01h, 50h, 0E8h, 0FFh, 01h, 0B5h, 0FFh, 8Dh, 4Ch, 24h, 18h, 0A3h, 6Ch, 42h, 2Fh
+    db 01h, 89h, 74h, 24h, 2Ch, 0E8h, 56h, 0CDh, 39h, 00h, 68h, 80h, 20h, 10h, 01h, 8Dh
+    db 4Ch, 24h, 20h, 0E8h, 0C8h, 0DFh, 39h, 00h, 8Bh, 44h, 24h, 1Ch, 3Bh, 0C7h, 0C7h, 44h
+    db 24h, 2Ch, 02h, 00h, 00h, 00h, 74h, 05h, 83h, 0C0h, 08h, 0EBh, 05h, 0B8h, 8Bh, 38h
+    db 07h, 01h, 8Bh, 0Dh, 00h, 0D6h, 2Eh, 01h, 50h, 0E8h, 0B9h, 01h, 0B5h, 0FFh, 8Dh, 4Ch
+    db 24h, 1Ch, 0A3h, 64h, 42h, 2Fh, 01h, 89h, 74h, 24h, 2Ch, 0E8h, 10h, 0CDh, 39h, 00h
+    db 0A1h, 6Ch, 42h, 2Fh, 01h, 8Bh, 0Dh, 40h, 1Bh, 2Fh, 01h, 8Bh, 11h, 50h, 57h, 0FFh
+    db 92h, 0DCh, 00h, 00h, 00h, 8Bh, 0Dh, 40h, 1Bh, 2Fh, 01h, 0A3h, 68h, 42h, 2Fh, 01h
+    db 0A1h, 64h, 42h, 2Fh, 01h, 8Bh, 11h, 50h, 57h, 0FFh, 92h, 0DCh, 00h, 00h, 00h, 51h
+    db 89h, 64h, 24h, 24h, 8Bh, 0CCh, 68h, 54h, 6Eh, 33h, 01h, 0A3h, 60h, 42h, 2Fh, 01h
+    db 0E8h, 8Bh, 0D7h, 39h, 00h, 8Bh, 0Dh, 70h, 42h, 2Fh, 01h, 51h, 0E8h, 0F3h, 1Bh, 0B2h
+    db 0FFh, 83h, 0C4h, 08h, 0C6h, 05h, 78h, 42h, 2Fh, 01h, 01h, 8Bh, 4Ch, 24h, 24h, 64h
+    db 89h, 0Dh, 00h, 00h, 00h, 00h, 5Fh, 5Eh, 83h, 0C4h, 28h, 0C3h, 89h, 3Dh, 74h, 42h
+    db 2Fh, 01h, 89h, 3Dh, 70h, 42h, 2Fh, 01h, 89h, 3Dh, 6Ch, 42h, 2Fh, 01h, 89h, 3Dh
+    db 64h, 42h, 2Fh, 01h, 89h, 3Dh, 68h, 42h, 2Fh, 01h, 89h, 3Dh, 60h, 42h, 2Fh, 01h
+    db 0C6h, 05h, 78h, 42h, 2Fh, 01h, 00h, 8Bh, 4Ch, 24h, 24h, 5Fh, 64h, 89h, 0Dh, 00h
+    db 00h, 00h, 00h, 5Eh, 83h, 0C4h, 28h, 0C3h
+?InitBuddyControls@@YAXH@Z ENDP
+_TEXT ENDS
+END
