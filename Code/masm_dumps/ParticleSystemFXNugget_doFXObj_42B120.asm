cd999124e2cd8316fdc47a6e4e0fa892f2be0ce2
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 15:46:30 +0000

    match ParticleSystemFXNugget::doFXObj as exact 281B MASM
    True body at 0x42B120 via class vtbl slot2; queue RVA 0xA9A536 was misplaced.

---
+++ Code/masm_dumps/ParticleSystemFXNugget_doFXObj_42B120.asm
@@ -0,0 +1,31 @@
+.386
+.model flat
+
+; ?doFXObj@ParticleSystemFXNugget@@UBEXPBVObject@@0@Z
+; Exact 281B @ 0x0042B120; true body via ParticleSystemFXNugget vtbl slot2
+; (ctor stores vtbl 0x010F3520); queue 0xA9A536 was mid-body garbage (misplaced)
+; BFME m_ricochet@+0x101; Object pos@+0x38; reallyDoFX both arms; size 281 not queue 277
+_TEXT SEGMENT
+public ?doFXObj@ParticleSystemFXNugget@@UBEXPBVObject@@0@Z
+?doFXObj@ParticleSystemFXNugget@@UBEXPBVObject@@0@Z PROC
+    db 83h,0ECh,30h,55h,56h,8Bh,74h,24h,3Ch,85h,0F6h,8Bh,0E9h,0Fh,84h,0FEh
+    db 00h,00h,00h,8Ah,85h,01h,01h,00h,00h,84h,0C0h,57h,8Bh,7Ch,24h,44h
+    db 0Fh,84h,0D9h,00h,00h,00h,85h,0FFh,0Fh,84h,0D1h,00h,00h,00h,0D9h,46h
+    db 3Ch,53h,0D8h,67h,3Ch,8Dh,5Eh,38h,0D9h,03h,68h,61h,55h,44h,00h,0D8h
+    db 67h,38h,6Ah,03h,6Ah,10h,0D9h,0F3h,8Dh,44h,24h,1Ch,50h,0D9h,5Ch,24h
+    db 54h,0E8h,0E6h,0FCh,0BDh,0FFh,0D9h,44h,24h,44h,0D9h,0FFh,57h,56h,8Dh,4Ch
+    db 24h,18h,51h,53h,8Bh,0CDh,0C7h,44h,24h,28h,00h,00h,00h,00h,0C7h,44h
+    db 24h,2Ch,00h,00h,00h,00h,0C7h,44h,24h,38h,00h,00h,00h,00h,0C7h,44h
+    db 24h,3Ch,00h,00h,00h,00h,0C7h,44h,24h,48h,00h,00h,80h,3Fh,0C7h,44h
+    db 24h,4Ch,00h,00h,00h,00h,0D9h,5Ch,24h,58h,0D9h,44h,24h,54h,0D9h,0FEh
+    db 0D9h,05h,50h,53h,07h,01h,0D8h,0C9h,0D9h,44h,24h,58h,0D9h,0C0h,0D8h,0C2h
+    db 0D9h,5Ch,24h,20h,0D9h,44h,24h,58h,0D8h,0Dh,50h,53h,07h,01h,0D9h,54h
+    db 24h,54h,0D8h,0E3h,0D9h,5Ch,24h,24h,0D9h,0CAh,0D8h,44h,24h,54h,0D9h,5Ch
+    db 24h,30h,0D9h,0C9h,0D8h,0E1h,0D9h,5Ch,24h,34h,0D9h,44h,24h,54h,0D8h,0C1h
+    db 0D9h,5Ch,24h,40h,0D9h,44h,24h,54h,0D8h,0E1h,0D9h,5Ch,24h,44h,0DDh,0D8h
+    db 0E8h,0F0h,3Eh,0BEh,0FFh,5Bh,5Fh,5Eh,5Dh,83h,0C4h,30h,0C2h,08h,00h,57h
+    db 56h,8Dh,56h,08h,52h,83h,0C6h,38h,56h,8Bh,0CDh,0E8h,0D5h,3Eh,0BEh,0FFh
+    db 5Fh,5Eh,5Dh,83h,0C4h,30h,0C2h,08h,00h
+?doFXObj@ParticleSystemFXNugget@@UBEXPBVObject@@0@Z ENDP
+_TEXT ENDS
+END
