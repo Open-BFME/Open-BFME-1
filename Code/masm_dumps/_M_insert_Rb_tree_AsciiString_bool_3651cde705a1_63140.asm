40a3038e14f954397c6ed118e3908a6fc32e962d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 16:39:37 +0000

    Match map<AsciiString,bool> _M_insert as MASM at true body 0x63140
    Queue RVA 0x64E69B was inside already-matched map Coord3D operator[]; true
    183B body recovered via matched map bool operator[] call chain.

---
+++ Code/masm_dumps/_M_insert_Rb_tree_AsciiString_bool_3651cde705a1_63140.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ?_M_insert@?$_Rb_tree@VAsciiString@@U?$pair@$$CBVAsciiString@@_N@_STL@@U?$_Select1st@U?$pair@$$CBVAsciiString@@_N@_STL@@@3@U?$less@VAsciiString@@@3@V?$allocator@U?$pair@$$CBVAsciiString@@_N@_STL@@@3@@_STL@@AAE?AU?$_Rb_tree_iterator@U?$pair@$$CBVAsciiString@@_N@_STL@@U?$_Nonconst_traits@U?$pair@$$CBVAsciiString@@_N@_STL@@@2@@2@PAU_Rb_tree_node_base@2@0ABU?$pair@$$CBVAsciiString@@_N@2@0@Z
+; Exact 183B _M_insert map<AsciiString,bool> true body @ 0x63140;
+; queue 0x64E69B was INSIDE already-matched map<AsciiString,Coord3D>::operator[] @0x64E690/229.
+; Identity: map bool op[]@0x63960->insert@0x633E0->thunk 0x235B5->body;
+; node push 0x18; compare@0x220C5; allocate@0x82E540; _Rebalance@0x82C9D0; ret 0x14.
+_TEXT SEGMENT
+public _bfme_export_3651cde705a1
+_bfme_export_3651cde705a1 PROC
+    db 053h,08Bh,05Ch,024h,010h,055h,08Bh,06Ch,024h,018h,056h,08Bh,0F1h,03Bh,01Eh,057h
+    db 074h,046h,08Bh,044h,024h,024h,085h,0C0h,075h,017h,08Bh,044h,024h,018h,085h,0C0h
+    db 075h,036h,08Dh,043h,010h,050h,08Bh,0CDh,0E8h,058h,0EFh,0FBh,0FFh,085h,0C0h,07Ch
+    db 027h,06Ah,018h,0E8h,0C8h,0B3h,07Ch,000h,08Bh,0F8h,08Dh,04Fh,010h,055h,051h,0E8h
+    db 0C9h,02Ch,0FAh,0FFh,089h,07Bh,00Ch,08Bh,006h,08Bh,048h,00Ch,083h,0C4h,00Ch,03Bh
+    db 0D9h,075h,036h,089h,078h,00Ch,0EBh,031h,06Ah,018h,0E8h,0A1h,0B3h,07Ch,000h,08Bh
+    db 0F8h,08Dh,057h,010h,055h,052h,0E8h,0A2h,02Ch,0FAh,0FFh,089h,07Bh,008h,08Bh,006h
+    db 083h,0C4h,00Ch,03Bh,0D8h,075h,00Ah,089h,078h,004h,08Bh,006h,089h,078h,00Ch,0EBh
+    db 008h,03Bh,058h,008h,075h,003h,089h,078h,008h,033h,0C0h,089h,05Fh,004h,089h,047h
+    db 008h,089h,047h,00Ch,08Bh,00Eh,083h,0C1h,004h,051h,057h,0E8h,0F0h,097h,07Ch,000h
+    db 08Bh,046h,004h,083h,0C4h,008h,040h,089h,046h,004h,08Bh,044h,024h,014h,089h,038h
+    db 05Fh,05Eh,05Dh,05Bh,0C2h,014h,000h
+_bfme_export_3651cde705a1 ENDP
+_TEXT ENDS
+END
