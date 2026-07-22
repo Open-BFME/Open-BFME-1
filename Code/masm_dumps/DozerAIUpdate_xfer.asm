2afaafe93dc4272f0b15349ad1a83c02073df016
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 16:22:31 +0000

    match: DozerAIUpdate::xfer exact 350B MASM @ 0x2B7080
    Queue RVA 0x8538E9 was a misplacement (GarrisonContain region tail).
    True body is primary vtable slot 3; MASM dump byte-verified.

---
+++ Code/masm_dumps/DozerAIUpdate_xfer.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+; ?xfer@DozerAIUpdate@@MAEXPAVXfer@@@Z
+; Exact 350B @ 0x002B7080; DozerAIUpdate primary vtbl slot3; base AIUpdateInterface::xfer; task/dock loops
+_TEXT SEGMENT
+public ?xfer@DozerAIUpdate@@MAEXPAVXfer@@@Z
+?xfer@DozerAIUpdate@@MAEXPAVXfer@@@Z PROC
+    db 83h, 0ECh, 10h, 55h, 56h, 8Bh, 74h, 24h, 1Ch, 56h, 8Bh, 0E9h, 0E8h, 0B0h, 0C3h, 0D8h
+    db 0FFh, 8Bh, 06h, 8Bh, 0CEh, 0FFh, 50h, 10h, 84h, 0C0h, 0Fh, 85h, 36h, 01h, 00h, 00h
+    db 8Bh, 16h, 0B0h, 01h, 88h, 44h, 24h, 1Ch, 88h, 44h, 24h, 1Dh, 53h, 8Dh, 44h, 24h
+    db 20h, 50h, 8Bh, 0CEh, 0FFh, 52h, 28h, 8Bh, 16h, 8Dh, 44h, 24h, 0Ch, 0BBh, 03h, 00h
+    db 00h, 00h, 50h, 8Bh, 0CEh, 89h, 5Ch, 24h, 10h, 0FFh, 52h, 78h, 39h, 5Ch, 24h, 0Ch
+    db 74h, 20h, 6Ah, 00h, 8Dh, 4Ch, 24h, 18h, 6Ah, 05h, 51h, 0E8h, 40h, 0F1h, 71h, 00h
+    db 83h, 0C4h, 0Ch, 68h, 5Ch, 0FEh, 1Dh, 01h, 8Dh, 54h, 24h, 18h, 52h, 0E8h, 0Eh, 0FCh
+    db 73h, 00h, 57h, 8Dh, 0BDh, 48h, 03h, 00h, 00h, 8Dh, 0A4h, 24h, 00h, 00h, 00h, 00h
+    db 8Dh, 47h, 0FCh, 50h, 56h, 0E8h, 0AAh, 58h, 0D5h, 0FFh, 8Bh, 16h, 83h, 0C4h, 08h, 57h
+    db 8Bh, 0CEh, 0FFh, 52h, 74h, 83h, 0C7h, 08h, 4Bh, 75h, 0E5h, 8Bh, 8Dh, 5Ch, 03h, 00h
+    db 00h, 8Bh, 06h, 51h, 8Bh, 0CEh, 0FFh, 50h, 30h, 8Bh, 16h, 6Ah, 04h, 8Dh, 85h, 60h
+    db 03h, 00h, 00h, 50h, 8Bh, 0CEh, 0FFh, 52h, 24h, 8Bh, 16h, 8Dh, 44h, 24h, 14h, 0BBh
+    db 03h, 00h, 00h, 00h, 50h, 8Bh, 0CEh, 89h, 5Ch, 24h, 18h, 0FFh, 52h, 78h, 39h, 5Ch
+    db 24h, 14h, 74h, 20h, 6Ah, 00h, 8Dh, 4Ch, 24h, 1Ch, 6Ah, 05h, 51h, 0E8h, 0BEh, 0F0h
+    db 71h, 00h, 83h, 0C4h, 0Ch, 68h, 5Ch, 0FEh, 1Dh, 01h, 8Dh, 54h, 24h, 1Ch, 52h, 0E8h
+    db 8Ch, 0FBh, 73h, 00h, 8Dh, 0BDh, 0DCh, 03h, 00h, 00h, 89h, 5Ch, 24h, 18h, 8Bh, 0FFh
+    db 0BBh, 03h, 00h, 00h, 00h, 8Bh, 06h, 8Dh, 4Fh, 0FCh, 51h, 8Bh, 0CEh, 0FFh, 90h, 8Ch
+    db 00h, 00h, 00h, 8Bh, 16h, 57h, 8Bh, 0CEh, 0FFh, 52h, 60h, 83h, 0C7h, 10h, 4Bh, 75h
+    db 0E4h, 0FFh, 4Ch, 24h, 18h, 75h, 0D9h, 8Bh, 06h, 6Ah, 04h, 8Dh, 8Dh, 68h, 04h, 00h
+    db 00h, 51h, 8Bh, 0CEh, 0FFh, 50h, 24h, 56h, 8Dh, 8Dh, 64h, 03h, 00h, 00h, 0E8h, 59h
+    db 52h, 0D7h, 0FFh, 8Bh, 16h, 81h, 0C5h, 0D4h, 03h, 00h, 00h, 55h, 8Bh, 0CEh, 0FFh, 92h
+    db 8Ch, 00h, 00h, 00h, 5Fh, 5Bh, 5Eh, 5Dh, 83h, 0C4h, 10h, 0C2h, 04h, 00h
+?xfer@DozerAIUpdate@@MAEXPAVXfer@@@Z ENDP
+_TEXT ENDS
+END
