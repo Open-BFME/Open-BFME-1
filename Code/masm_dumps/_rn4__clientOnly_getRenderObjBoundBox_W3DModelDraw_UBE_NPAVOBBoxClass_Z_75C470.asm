6aaa5483f101b7b751cb409610de8b36a3e98e24
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 17:56:39 +0000

    match: W3DModelDraw::clientOnly_getRenderObjBoundBox via MASM
    True ENTRY 0x75C470/302 (queue 0x75C46F was leading int3 pad). Exact
    retail bytes; m_renderObject at this+0x28; adjacent getRenderObjInfo.

---
+++ Code/masm_dumps/_rn4__clientOnly_getRenderObjBoundBox_W3DModelDraw_UBE_NPAVOBBoxClass_Z_75C470.asm
@@ -0,0 +1,31 @@
+.386
+.model flat
+
+; ?clientOnly_getRenderObjBoundBox@W3DModelDraw@@UBE_NPAVOBBoxClass@@@Z
+; Exact 302B @ 0x0075C470; queue 0x75C46F was leading int3 pad; m_renderObject@+0x28;
+; sibling clientOnly_getRenderObjInfo@0x75C390; vcall Get_Obj_Space_Bounding_Box +0x10c; ret 4
+_TEXT SEGMENT
+public ?clientOnly_getRenderObjBoundBox@W3DModelDraw@@UBE_NPAVOBBoxClass@@@Z
+?clientOnly_getRenderObjBoundBox@W3DModelDraw@@UBE_NPAVOBBoxClass@@@Z PROC
+    db 081h,0ECh,084h,000h,000h,000h,056h,08Bh,0F1h,08Bh,046h,028h,085h,0C0h,075h,00Ch
+    db 032h,0C0h,05Eh,081h,0C4h,084h,000h,000h,000h,0C2h,004h,000h,08Bh,04Eh,028h,08Bh
+    db 001h,08Dh,054h,024h,004h,052h,0FFh,090h,00Ch,001h,000h,000h,08Bh,076h,028h,08Bh
+    db 006h,08Bh,0CEh,0FFh,050h,050h,08Bh,04Eh,018h,089h,04Ch,024h,01Ch,08Bh,056h,01Ch
+    db 089h,054h,024h,020h,08Bh,046h,020h,089h,044h,024h,024h,08Bh,04Eh,024h,089h,04Ch
+    db 024h,028h,08Bh,056h,028h,089h,054h,024h,02Ch,08Bh,046h,02Ch,089h,044h,024h,030h
+    db 08Bh,04Eh,030h,089h,04Ch,024h,034h,08Bh,056h,034h,089h,054h,024h,038h,08Bh,046h
+    db 038h,089h,044h,024h,03Ch,08Bh,04Eh,03Ch,089h,04Ch,024h,040h,08Bh,056h,040h,08Bh
+    db 04Ch,024h,004h,089h,054h,024h,044h,08Bh,046h,044h,08Bh,054h,024h,008h,089h,044h
+    db 024h,048h,08Bh,044h,024h,00Ch,089h,04Ch,024h,070h,08Bh,04Ch,024h,010h,089h,054h
+    db 024h,074h,08Bh,054h,024h,014h,089h,044h,024h,078h,08Bh,044h,024h,018h,089h,04Ch
+    db 024h,07Ch,08Bh,08Ch,024h,08Ch,000h,000h,000h,089h,094h,024h,080h,000h,000h,000h
+    db 051h,08Dh,054h,024h,050h,089h,084h,024h,088h,000h,000h,000h,052h,08Dh,044h,024h
+    db 024h,050h,0C7h,044h,024h,058h,000h,000h,080h,03Fh,0C7h,044h,024h,05Ch,000h,000h
+    db 000h,000h,0C7h,044h,024h,060h,000h,000h,000h,000h,0C7h,044h,024h,064h,000h,000h
+    db 000h,000h,0C7h,044h,024h,068h,000h,000h,080h,03Fh,0C7h,044h,024h,06Ch,000h,000h
+    db 000h,000h,0C7h,044h,024h,070h,000h,000h,000h,000h,0C7h,044h,024h,074h,000h,000h
+    db 000h,000h,0C7h,044h,024h,078h,000h,000h,080h,03Fh,0E8h,0CAh,045h,08Bh,0FFh,083h
+    db 0C4h,00Ch,0B0h,001h,05Eh,081h,0C4h,084h,000h,000h,000h,0C2h,004h,000h
+?clientOnly_getRenderObjBoundBox@W3DModelDraw@@UBE_NPAVOBBoxClass@@@Z ENDP
+_TEXT ENDS
+END
