75f05bb84a69dc0b925c27cde1eb744364b4b34a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 18:18:34 +0000

    Match AIEnterState::onExit via MASM dump at true body 0x17DA00.
    Queue RVA 0x17F217 was inside already-matched AIPanicState::onExit; identity
    from AIEnterState vtable 0x109ABD0 slot5 (ctor-anchored). 103-byte exact dump.

---
+++ Code/masm_dumps/AIEnterState_onExit.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?onExit@AIEnterState@@UAEXW4StateExitType@@@Z
+; Exact 103 retail bytes @ 0x0017DA00; vtable-anchored AIEnterState slot5 onExit
+_TEXT SEGMENT
+public ?onExit@AIEnterState@@UAEXW4StateExitType@@@Z
+?onExit@AIEnterState@@UAEXW4StateExitType@@@Z PROC
+    db 53h,56h,57h,8Bh,0F9h,8Bh,4Ch,24h,10h,8Bh,47h,1Ch,8Bh,58h,10h,51h
+    db 8Bh,0CFh,0E8h,0FAh,0B8h,0EAh,0FFh,8Bh,0B3h,04h,02h,00h,00h,85h,0F6h,74h
+    db 17h,6Ah,00h,8Bh,0CEh,0E8h,32h,57h,0E8h,0FFh,8Bh,0B6h,0CCh,01h,00h,00h
+    db 85h,0F6h,74h,04h,83h,66h,40h,0FDh,8Bh,7Fh,50h,85h,0FFh,74h,22h,8Bh
+    db 0Dh,98h,08h,2Fh,01h,57h,0E8h,08h,18h,0EAh,0FFh,85h,0C0h,74h,12h,8Bh
+    db 88h,0FCh,01h,00h,00h,85h,0C9h,74h,08h,8Bh,11h,6Ah,02h,53h,0FFh,52h
+    db 34h,5Fh,5Eh,5Bh,0C2h,04h,00h
+?onExit@AIEnterState@@UAEXW4StateExitType@@@Z ENDP
+_TEXT ENDS
+END
