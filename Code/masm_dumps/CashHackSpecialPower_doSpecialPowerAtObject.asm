730eaede55b6010708baae8cd74137c2f8a586b7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 02:41:23 +0000

    match CashHackSpecialPower::doSpecialPowerAtObject via MASM (439B @ 0x258800)
    True body starts at 0x258800 (queue was 0x258809 mid-SEH). Identity: GUI:AddCash
    and GUI:LoseCash, CashHack TU cluster. C++ blocked by Object/Player/InGameUI layout.

---
+++ Code/masm_dumps/CashHackSpecialPower_doSpecialPowerAtObject.asm
@@ -0,0 +1,39 @@
+.386
+.model flat
+
+; ?doSpecialPowerAtObject@CashHackSpecialPower@@UAEXPAVObject@@I@Z
+; Exact 439B @ 0x00258800; identity: GUI:AddCash+GUI:LoseCash; cluster with getModuleNameKey@0x2584C0 ModuleData@0x258D70; Object/Player/InGameUI layout blocks C++
+_TEXT SEGMENT
+public ?doSpecialPowerAtObject@CashHackSpecialPower@@UAEXPAVObject@@I@Z
+?doSpecialPowerAtObject@CashHackSpecialPower@@UAEXPAVObject@@I@Z PROC
+    db 06Ah, 0FFh, 064h, 0A1h, 000h, 000h, 000h, 000h, 068h, 058h, 0EFh, 000h, 001h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 010h, 053h, 055h, 056h, 08Bh, 0F1h, 08Bh, 046h, 0F8h
+    db 08Bh, 088h, 0A4h, 001h, 000h, 000h, 085h, 0C9h, 057h, 00Fh, 085h, 073h, 001h, 000h, 000h, 08Bh
+    db 05Ch, 024h, 030h, 085h, 0DBh, 00Fh, 084h, 067h, 001h, 000h, 000h, 08Bh, 044h, 024h, 034h, 050h
+    db 053h, 08Bh, 0CEh, 0E8h, 030h, 0F2h, 0DEh, 0FFh, 08Bh, 07Eh, 0F8h, 08Bh, 0CBh, 0E8h, 0D2h, 07Fh
+    db 0DCh, 0FFh, 08Bh, 0CFh, 08Dh, 068h, 048h, 0E8h, 0C8h, 07Fh, 0DCh, 0FFh, 083h, 0C0h, 048h, 085h
+    db 0EDh, 089h, 044h, 024h, 010h, 00Fh, 084h, 037h, 001h, 000h, 000h, 085h, 0C0h, 00Fh, 084h, 02Fh
+    db 001h, 000h, 000h, 08Bh, 04Dh, 004h, 089h, 04Ch, 024h, 034h, 08Dh, 04Eh, 0F0h, 0E8h, 03Bh, 022h
+    db 0DDh, 0FFh, 08Bh, 04Ch, 024h, 034h, 089h, 044h, 024h, 030h, 03Bh, 0C1h, 08Dh, 044h, 024h, 030h
+    db 072h, 004h, 08Dh, 044h, 024h, 034h, 08Bh, 030h, 085h, 0F6h, 00Fh, 086h, 002h, 001h, 000h, 000h
+    db 06Ah, 001h, 056h, 08Bh, 0CDh, 0E8h, 0EAh, 08Fh, 0DEh, 0FFh, 08Bh, 04Ch, 024h, 010h, 06Ah, 001h
+    db 056h, 0E8h, 0B7h, 0F4h, 0DCh, 0FFh, 08Bh, 0CFh, 0E8h, 067h, 07Fh, 0DCh, 0FFh, 056h, 08Dh, 088h
+    db 048h, 003h, 000h, 000h, 0E8h, 095h, 01Bh, 0DEh, 0FFh, 033h, 0EDh, 089h, 06Ch, 024h, 030h, 08Bh
+    db 00Dh, 07Ch, 014h, 02Fh, 001h, 08Bh, 011h, 056h, 051h, 08Bh, 0C4h, 089h, 064h, 024h, 03Ch, 055h
+    db 068h, 014h, 040h, 008h, 001h, 050h, 089h, 06Ch, 024h, 03Ch, 0FFh, 052h, 028h, 08Dh, 04Ch, 024h
+    db 038h, 051h, 0E8h, 099h, 008h, 063h, 000h, 0D9h, 047h, 040h, 08Bh, 047h, 03Ch, 08Bh, 057h, 038h
+    db 08Bh, 00Dh, 08Ch, 014h, 02Fh, 001h, 083h, 0C4h, 00Ch, 089h, 044h, 024h, 018h, 068h, 000h, 0FFh
+    db 000h, 0FFh, 0D8h, 005h, 0E0h, 077h, 009h, 001h, 08Dh, 044h, 024h, 018h, 050h, 08Dh, 044h, 024h
+    db 038h, 0D9h, 05Ch, 024h, 024h, 089h, 054h, 024h, 01Ch, 08Bh, 011h, 050h, 0FFh, 092h, 078h, 001h
+    db 000h, 000h, 08Bh, 00Dh, 07Ch, 014h, 02Fh, 001h, 08Bh, 011h, 056h, 051h, 08Bh, 0C4h, 089h, 064h
+    db 024h, 03Ch, 055h, 068h, 0F4h, 039h, 00Bh, 001h, 050h, 0FFh, 052h, 028h, 08Dh, 04Ch, 024h, 038h
+    db 051h, 0E8h, 03Ah, 008h, 063h, 000h, 0D9h, 043h, 040h, 08Bh, 043h, 03Ch, 08Bh, 053h, 038h, 08Bh
+    db 00Dh, 08Ch, 014h, 02Fh, 001h, 083h, 0C4h, 00Ch, 089h, 044h, 024h, 018h, 068h, 000h, 000h, 0FFh
+    db 0FFh, 0D8h, 005h, 05Ch, 061h, 008h, 001h, 08Dh, 044h, 024h, 018h, 050h, 08Dh, 044h, 024h, 038h
+    db 0D9h, 05Ch, 024h, 024h, 089h, 054h, 024h, 01Ch, 08Bh, 011h, 050h, 0FFh, 092h, 078h, 001h, 000h
+    db 000h, 08Dh, 04Ch, 024h, 030h, 0C7h, 044h, 024h, 028h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 02Eh, 0F8h
+    db 062h, 000h, 08Bh, 04Ch, 024h, 020h, 05Fh, 05Eh, 05Dh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h
+    db 05Bh, 083h, 0C4h, 01Ch, 0C2h, 008h, 000h
+?doSpecialPowerAtObject@CashHackSpecialPower@@UAEXPAVObject@@I@Z ENDP
+_TEXT ENDS
+END
