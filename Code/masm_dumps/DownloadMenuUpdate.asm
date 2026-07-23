46b904d5e320aff7ed8d92238690c520d7bf18b9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 19:34:45 +0000

    Recover DownloadMenuUpdate retail body

---
+++ Code/masm_dumps/DownloadMenuUpdate.asm
@@ -0,0 +1,34 @@
+.386
+.model flat
+
+; ?DownloadMenuUpdate@@YAXPAVWindowLayout@@PAX@Z
+; Certified retail body @ RVA 0x004C7E40, size 363.
+_TEXT SEGMENT
+public ?DownloadMenuUpdate@@YAXPAVWindowLayout@@PAX@Z
+?DownloadMenuUpdate@@YAXPAVWindowLayout@@PAX@Z PROC
+    db 6Ah, 0FFh, 68h, 70h, 97h, 02h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 0Ch, 33h, 0C9h, 89h, 4Ch, 24h, 08h, 0A1h, 94h
+    db 39h, 2Fh, 01h, 85h, 0C0h, 53h, 56h, 74h, 24h, 50h, 8Dh, 44h, 24h, 10h, 50h, 0E8h
+    db 0CFh, 0Bh, 0B5h, 0FFh, 8Bh, 00h, 83h, 0C4h, 08h, 85h, 0C0h, 0B9h, 01h, 00h, 00h, 00h
+    db 74h, 0Bh, 66h, 83h, 78h, 04h, 00h, 74h, 04h, 8Ah, 0D9h, 0EBh, 02h, 32h, 0DBh, 0F6h
+    db 0C1h, 01h, 74h, 09h, 8Dh, 4Ch, 24h, 0Ch, 0E8h, 33h, 03h, 3Ch, 00h, 84h, 0DBh, 0Fh
+    db 84h, 0F5h, 00h, 00h, 00h, 6Ah, 00h, 0FFh, 15h, 0F0h, 94h, 35h, 01h, 8Bh, 0Dh, 0A8h
+    db 39h, 2Fh, 01h, 83h, 0C4h, 04h, 3Bh, 0C1h, 0Fh, 8Eh, 0DCh, 00h, 00h, 00h, 0A3h, 0A8h
+    db 39h, 2Fh, 01h, 0C7h, 44h, 24h, 08h, 00h, 00h, 00h, 00h, 0A1h, 0ACh, 39h, 2Fh, 01h
+    db 85h, 0C0h, 0C7h, 44h, 24h, 1Ch, 00h, 00h, 00h, 00h, 74h, 5Ah, 83h, 0F8h, 01h, 7Dh
+    db 0Ah, 0B8h, 01h, 00h, 00h, 00h, 0A3h, 0ACh, 39h, 2Fh, 01h, 99h, 0B9h, 3Ch, 00h, 00h
+    db 00h, 0F7h, 0F9h, 8Bh, 0C8h, 8Bh, 0F2h, 0B8h, 89h, 88h, 88h, 88h, 0F7h, 0E9h, 03h, 0D1h
+    db 0C1h, 0FAh, 05h, 56h, 8Bh, 0C2h, 51h, 8Bh, 0Dh, 7Ch, 14h, 2Fh, 01h, 0C1h, 0E8h, 1Fh
+    db 03h, 0C2h, 8Bh, 11h, 50h, 51h, 8Bh, 0C4h, 89h, 64h, 24h, 20h, 6Ah, 00h, 68h, 8Ch
+    db 0E3h, 0Fh, 01h, 50h, 0FFh, 52h, 28h, 8Dh, 4Ch, 24h, 18h, 51h, 0E8h, 5Fh, 12h, 3Ch
+    db 00h, 83h, 0C4h, 14h, 0EBh, 34h, 8Bh, 0Dh, 7Ch, 14h, 2Fh, 01h, 8Bh, 11h, 6Ah, 00h
+    db 68h, 70h, 0E3h, 0Fh, 01h, 8Dh, 44h, 24h, 18h, 50h, 0FFh, 52h, 28h, 50h, 8Dh, 4Ch
+    db 24h, 0Ch, 0C6h, 44h, 24h, 20h, 01h, 0E8h, 0D4h, 05h, 3Ch, 00h, 8Dh, 4Ch, 24h, 10h
+    db 0C6h, 44h, 24h, 1Ch, 00h, 0E8h, 66h, 02h, 3Ch, 00h, 51h, 8Dh, 54h, 24h, 0Ch, 89h
+    db 64h, 24h, 10h, 8Bh, 0CCh, 52h, 0E8h, 85h, 04h, 3Ch, 00h, 0A1h, 94h, 39h, 2Fh, 01h
+    db 50h, 0E8h, 0E9h, 41h, 0B6h, 0FFh, 83h, 0C4h, 08h, 8Dh, 4Ch, 24h, 08h, 0C7h, 44h, 24h
+    db 1Ch, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 36h, 02h, 3Ch, 00h, 8Bh, 4Ch, 24h, 14h, 5Eh, 64h
+    db 89h, 0Dh, 00h, 00h, 00h, 00h, 5Bh, 83h, 0C4h, 18h, 0C3h
+?DownloadMenuUpdate@@YAXPAVWindowLayout@@PAX@Z ENDP
+_TEXT ENDS
+END
