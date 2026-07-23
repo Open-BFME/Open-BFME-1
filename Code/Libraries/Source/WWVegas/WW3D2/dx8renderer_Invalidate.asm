7e011ca70da89d0de8bc19991572ad304018a4ed
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 04:23:07 +0000

    Match DX8MeshRenderer Invalidate

---
+++ Code/Libraries/Source/WWVegas/WW3D2/dx8renderer_Invalidate.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ?Invalidate@DX8MeshRendererClass@@QAEX_N@Z
+; Exact 289 retail bytes @ 0x00947BD0.
+_TEXT SEGMENT
+public ?Invalidate@DX8MeshRendererClass@@QAEX_N@Z
+?Invalidate@DX8MeshRendererClass@@QAEX_N@Z PROC
+    db 0A1h,0E4h,0B0h,34h,01h,53h,55h,33h,0DBh,3Bh,0C3h,56h,8Bh,0E9h,74h,36h
+    db 8Bh,88h,0BCh,00h,00h,00h,3Bh,0CBh,8Dh,90h,0BCh,00h,00h,00h,74h,22h
+    db 8Bh,0B0h,0C0h,00h,00h,00h,05h,0C0h,00h,00h,00h,89h,31h,8Bh,00h,3Bh
+    db 0C3h,74h,08h,8Bh,0Ah,89h,88h,0BCh,00h,00h,00h,89h,1Ah,0A1h,0E4h,0B0h
+    db 34h,01h,3Bh,0C3h,75h,0CAh,8Bh,45h,14h,57h,33h,0FFh,3Bh,0C3h,7Eh,3Ch
+    db 8Bh,55h,0Ch,8Bh,34h,0BAh,8Bh,0CEh,0E8h,43h,45h,09h,00h,3Bh,0C3h,74h
+    db 13h,8Bh,10h,6Ah,01h,8Bh,0C8h,0FFh,12h,8Bh,0CEh,0E8h,30h,45h,09h,00h
+    db 3Bh,0C3h,75h,0EDh,8Bh,45h,0Ch,8Bh,0Ch,0B8h,3Bh,0CBh,74h,06h,8Bh,11h
+    db 6Ah,01h,0FFh,12h,8Bh,45h,14h,47h,3Bh,0F8h,7Ch,0C4h,8Bh,75h,18h,3Bh
+    db 0F3h,5Fh,74h,2Eh,8Bh,0CEh,0E8h,05h,45h,09h,00h,3Bh,0C3h,74h,13h,8Bh
+    db 10h,6Ah,01h,8Bh,0C8h,0FFh,12h,8Bh,0CEh,0E8h,0F2h,44h,09h,00h,3Bh,0C3h
+    db 75h,0EDh,8Bh,4Dh,18h,3Bh,0CBh,74h,06h,8Bh,01h,6Ah,01h,0FFh,10h,89h
+    db 5Dh,18h,38h,5Ch,24h,10h,75h,37h,6Ah,18h,0E8h,91h,0A2h,0F3h,0FFh,83h
+    db 0C4h,04h,3Bh,0C3h,74h,24h,8Dh,48h,04h,89h,59h,08h,89h,59h,04h,89h
+    db 19h,89h,59h,0Ch,89h,59h,10h,89h,09h,89h,48h,08h,89h,58h,10h,89h
+    db 58h,0Ch,0C7h,00h,0FCh,0CFh,13h,01h,0EBh,02h,33h,0C0h,89h,45h,18h,8Bh
+    db 45h,10h,8Dh,4Dh,08h,99h,83h,0E2h,03h,03h,0C2h,0C1h,0F8h,02h,85h,0C0h
+    db 89h,59h,0Ch,7Eh,06h,8Bh,11h,53h,0FFh,52h,04h,5Eh,5Dh,5Bh,0C2h,04h
+    db 00h
+?Invalidate@DX8MeshRendererClass@@QAEX_N@Z ENDP
+_TEXT ENDS
+END
