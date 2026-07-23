28994b82002b291374b176422fa505268b8b0e98
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:51:12 +0000

    Recover MeshModelClass read_prelit_material

---
+++ Code/masm_dumps/read_prelit_material_MeshModelClass_96FC30.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?read_prelit_material@MeshModelClass@@IAE?AW4WW3DErrorType@@AAVChunkLoadClass@@PAVMeshLoadContextClass@@@Z
+; Exact 249B retail span @ 0x0096FC30; certified recovery packet 66.
+_TEXT SEGMENT
+public ?read_prelit_material@MeshModelClass@@IAE?AW4WW3DErrorType@@AAVChunkLoadClass@@PAVMeshLoadContextClass@@@Z
+?read_prelit_material@MeshModelClass@@IAE?AW4WW3DErrorType@@AAVChunkLoadClass@@PAVMeshLoadContextClass@@@Z PROC
+    db 53h, 55h, 56h, 8Bh, 74h, 24h, 10h, 57h, 8Bh, 0F9h, 8Bh, 0CEh, 0E8h, 0FFh, 17h, 07h
+    db 00h, 8Bh, 6Ch, 24h, 18h, 3Bh, 85h, 88h, 00h, 00h, 00h, 0Fh, 85h, 99h, 00h, 00h
+    db 00h, 8Bh, 0CEh, 0E8h, 28h, 17h, 07h, 00h, 84h, 0C0h, 0Fh, 84h, 8Ah, 00h, 00h, 00h
+    db 8Bh, 0CEh, 0B3h, 01h, 0E8h, 0D7h, 17h, 07h, 00h, 83h, 0C0h, 0D8h, 83h, 0F8h, 10h, 77h
+    db 5Eh, 0Fh, 0B6h, 80h, 14h, 0FDh, 0D6h, 00h, 0FFh, 24h, 85h, 0FCh, 0FCh, 0D6h, 00h, 6Ah
+    db 10h, 8Dh, 5Dh, 78h, 53h, 8Bh, 0CEh, 0E8h, 34h, 19h, 07h, 00h, 83h, 0F8h, 10h, 74h
+    db 04h, 32h, 0DBh, 0EBh, 3Ah, 8Bh, 13h, 8Bh, 8Fh, 9Ch, 00h, 00h, 00h, 89h, 11h, 0B3h
+    db 01h, 0EBh, 2Ch, 55h, 56h, 8Bh, 0CFh, 0E8h, 84h, 0EEh, 0FFh, 0FFh, 0EBh, 1Fh, 55h, 56h
+    db 8Bh, 0CFh, 0E8h, 0A9h, 0EDh, 0FFh, 0FFh, 0EBh, 14h, 55h, 56h, 8Bh, 0CFh, 0E8h, 7Eh, 0EFh
+    db 0FFh, 0FFh, 0EBh, 09h, 55h, 56h, 8Bh, 0CFh, 0E8h, 93h, 0FEh, 0FFh, 0FFh, 8Ah, 0D8h, 8Bh
+    db 0CEh, 0E8h, 0Ah, 17h, 07h, 00h, 80h, 0FBh, 01h, 75h, 18h, 8Bh, 0CEh, 0E8h, 9Eh, 16h
+    db 07h, 00h, 84h, 0C0h, 0Fh, 85h, 76h, 0FFh, 0FFh, 0FFh, 5Fh, 5Eh, 5Dh, 0B0h, 01h, 5Bh
+    db 0C2h, 08h, 00h, 5Fh, 5Eh, 5Dh, 8Ah, 0C3h, 5Bh, 0C2h, 08h, 00h, 7Fh, 0FCh, 0D6h, 00h
+    db 0AEh, 0FCh, 0D6h, 00h, 0A3h, 0FCh, 0D6h, 00h, 0B9h, 0FCh, 0D6h, 00h, 0C4h, 0FCh, 0D6h, 00h
+    db 0CFh, 0FCh, 0D6h, 00h, 00h, 01h, 02h, 05h, 05h, 05h, 05h, 05h, 03h, 05h, 05h, 05h
+    db 05h, 05h, 05h, 05h, 04h, 0CCh, 0CCh, 0CCh, 0CCh
+?read_prelit_material@MeshModelClass@@IAE?AW4WW3DErrorType@@AAVChunkLoadClass@@PAVMeshLoadContextClass@@@Z ENDP
+_TEXT ENDS
+END
