a384602418887778e371652467153c3fbf4d8dae
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:48:02 +0000

    Recover ThingTemplate parsePrerequisites retail body

---
+++ Code/masm_dumps/parsePrerequisites_ThingTemplate_001485B0.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?parsePrerequisites@ThingTemplate@@KAXPAVINI@@PAX1PBX@Z
+; Exact 63 retail bytes @ 0x001485B0
+_TEXT SEGMENT
+public ?parsePrerequisites@ThingTemplate@@KAXPAVINI@@PAX1PBX@Z
+?parsePrerequisites@ThingTemplate@@KAXPAVINI@@PAX1PBX@Z PROC
+    db 056h,08Bh,074h,024h,00Ch,057h,08Bh,07Ch,024h,00Ch,083h,07Fh,008h,002h,075h,019h
+    db 08Bh,086h,0C8h,002h,000h,000h,08Bh,096h,0C4h,002h,000h,000h,08Dh,08Eh,0C4h,002h
+    db 000h,000h,050h,052h,0E8h,0DAh,085h,0ECh,0FFh,068h,0F8h,049h,009h,001h,081h,0C6h
+    db 0C4h,002h,000h,000h,056h,08Bh,0CFh,0E8h,0B4h,09Ah,070h,000h,05Fh,05Eh,0C3h
+?parsePrerequisites@ThingTemplate@@KAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
