b7f54481eef97111fbdf99687accd4e20b2982d7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 17:07:04 +0000

    Recover LightPulseFXNugget parse

---
+++ Code/masm_dumps/LightPulseFXNugget_parse_42BE30.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?parse@LightPulseFXNugget@@SAXPAVINI@@PAX1PBX@Z
+; Certified retail body @ 0x0042BE30, size 224.
+_TEXT SEGMENT
+public ?parse@LightPulseFXNugget@@SAXPAVINI@@PAX1PBX@Z
+?parse@LightPulseFXNugget@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 06ah, 0ffh, 068h, 04eh, 016h, 002h, 001h, 064h, 0a1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 081h, 0ech, 088h, 000h, 000h, 000h, 053h, 056h, 057h, 068h, 0d0h
+    db 000h, 000h, 000h, 0e8h, 0d8h, 060h, 045h, 000h, 083h, 0c4h, 004h, 089h, 044h, 024h, 00ch, 033h
+    db 0dbh, 03bh, 0c3h, 089h, 09ch, 024h, 09ch, 000h, 000h, 000h, 074h, 00bh, 08bh, 0c8h, 0e8h, 002h
+    db 053h, 0bfh, 0ffh, 08bh, 0f8h, 0ebh, 002h, 033h, 0ffh, 0c7h, 084h, 024h, 09ch, 000h, 000h, 000h
+    db 0ffh, 0ffh, 0ffh, 0ffh, 089h, 09ch, 024h, 090h, 000h, 000h, 000h, 033h, 0c0h, 08dh, 049h, 000h
+    db 089h, 05ch, 084h, 050h, 089h, 05ch, 084h, 010h, 040h, 083h, 0f8h, 010h, 07ch, 0f2h, 053h, 068h
+    db 078h, 026h, 00fh, 001h, 08dh, 04ch, 024h, 018h, 0e8h, 073h, 04ah, 042h, 000h, 053h, 068h, 080h
+    db 024h, 00fh, 001h, 08dh, 04ch, 024h, 018h, 0e8h, 064h, 04ah, 042h, 000h, 08bh, 08ch, 024h, 0a4h
+    db 000h, 000h, 000h, 08dh, 044h, 024h, 010h, 050h, 057h, 0e8h, 042h, 05ah, 042h, 000h, 08bh, 08ch
+    db 024h, 0a8h, 000h, 000h, 000h, 08bh, 071h, 004h, 06ah, 00ch, 0e8h, 061h, 026h, 040h, 000h, 08dh
+    db 048h, 008h, 083h, 0c4h, 004h, 03bh, 0cbh, 074h, 002h, 089h, 039h, 08bh, 04eh, 004h, 089h, 030h
+    db 089h, 048h, 004h, 089h, 001h, 08bh, 08ch, 024h, 094h, 000h, 000h, 000h, 05fh, 089h, 046h, 004h
+    db 05eh, 064h, 089h, 00dh, 000h, 000h, 000h, 000h, 05bh, 081h, 0c4h, 094h, 000h, 000h, 000h, 0c3h
+?parse@LightPulseFXNugget@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
