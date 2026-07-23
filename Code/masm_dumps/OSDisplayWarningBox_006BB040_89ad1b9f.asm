5c5730e2e864b5d6380770ed1a45168cbc2c0b3d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:45:13 +0000

    Recover OSDisplayWarningBox retail body

---
+++ Code/masm_dumps/OSDisplayWarningBox_006BB040_89ad1b9f.asm
@@ -0,0 +1,44 @@
+.386
+.model flat
+
+; ?OSDisplayWarningBox@@YA?AW4OSDisplayButtonType@@VAsciiString@@0II@Z
+; Certified packet 60: exact 509 retail bytes @ 0x006BB040
+; SHA-256: 89ad1b9f238caa0cfe2a26d9a0a1e181e8d82ead4a19e761fdea541639c79901
+_TEXT SEGMENT
+public ?OSDisplayWarningBox@@YA?AW4OSDisplayButtonType@@VAsciiString@@0II@Z
+?OSDisplayWarningBox@@YA?AW4OSDisplayButtonType@@VAsciiString@@0II@Z PROC
+    db 6Ah,0FFh,68h,10h,96h,04h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,14h,53h,56h,0A1h,7Ch,14h,2Fh,01h,33h
+    db 0DBh,3Bh,0C3h,0C7h,44h,24h,24h,01h,00h,00h,00h,75h,34h,8Dh,4Ch,24h
+    db 2Ch,88h,5Ch,24h,24h,0E8h,0C6h,0C8h,1Ch,00h,8Dh,4Ch,24h,30h,0C7h,44h
+    db 24h,24h,0FFh,0FFh,0FFh,0FFh,0E8h,0B5h,0C8h,1Ch,00h,0B8h,00h,00h,00h,80h
+    db 8Bh,4Ch,24h,1Ch,64h,89h,0Dh,00h,00h,00h,00h,5Eh,5Bh,83h,0C4h,20h
+    db 0C3h,53h,51h,8Dh,44h,24h,34h,89h,64h,24h,20h,8Bh,0CCh,50h,0E8h,0ADh
+    db 0CAh,1Ch,00h,8Bh,0Dh,7Ch,14h,2Fh,01h,8Bh,11h,8Dh,44h,24h,14h,50h
+    db 0FFh,52h,24h,53h,51h,8Dh,54h,24h,38h,89h,64h,24h,20h,8Bh,0CCh,52h
+    db 0C6h,44h,24h,30h,02h,0E8h,86h,0CAh,1Ch,00h,8Bh,0Dh,7Ch,14h,2Fh,01h
+    db 8Bh,01h,8Dh,54h,24h,10h,52h,0FFh,50h,24h,8Bh,44h,24h,38h,8Bh,4Ch
+    db 24h,34h,50h,51h,8Dh,44h,24h,18h,89h,5Ch,24h,18h,0E8h,0CFh,0FEh,0FFh
+    db 0FFh,0A0h,28h,0D5h,2Eh,01h,83h,0C4h,08h,3Ah,0C3h,74h,4Ah,8Bh,44h,24h
+    db 0Ch,3Bh,0C3h,8Dh,48h,08h,75h,05h,0B9h,8Ch,38h,07h,01h,8Bh,44h,24h
+    db 08h,3Bh,0C3h,74h,18h,8Bh,54h,24h,10h,52h,51h,83h,0C0h,08h,50h,53h
+    db 0FFh,15h,40h,90h,35h,01h,8Bh,0F0h,0E9h,0A6h,00h,00h,00h,8Bh,54h,24h
+    db 10h,52h,51h,0B8h,8Ch,38h,07h,01h,50h,53h,0FFh,15h,40h,90h,35h,01h
+    db 8Bh,0F0h,0E9h,8Ch,00h,00h,00h,89h,5Ch,24h,18h,89h,5Ch,24h,14h,8Dh
+    db 44h,24h,0Ch,50h,8Dh,4Ch,24h,1Ch,0C6h,44h,24h,28h,05h,0E8h,0DEh,0DEh
+    db 1Ch,00h,8Dh,4Ch,24h,08h,51h,8Dh,4Ch,24h,18h,0E8h,0D0h,0DEh,1Ch,00h
+    db 8Bh,15h,38h,0D2h,2Eh,01h,6Ah,03h,53h,53h,53h,53h,6Ah,0FEh,52h,0FFh
+    db 15h,84h,90h,35h,01h,8Bh,44h,24h,18h,3Bh,0C3h,8Dh,48h,08h,75h,05h
+    db 0B9h,8Bh,38h,07h,01h,8Bh,44h,24h,14h,3Bh,0C3h,74h,05h,83h,0C0h,08h
+    db 0EBh,05h,0B8h,8Bh,38h,07h,01h,8Bh,54h,24h,10h,52h,51h,50h,53h,0FFh
+    db 15h,3Ch,90h,35h,01h,8Dh,4Ch,24h,14h,8Bh,0F0h,0C6h,44h,24h,24h,04h
+    db 0E8h,6Bh,0C7h,1Ch,00h,8Dh,4Ch,24h,18h,0C6h,44h,24h,24h,03h,0E8h,5Dh
+    db 0C7h,1Ch,00h,8Dh,4Ch,24h,08h,0C6h,44h,24h,24h,02h,0E8h,0DFh,0CFh,1Ch
+    db 00h,8Dh,4Ch,24h,0Ch,0C6h,44h,24h,24h,01h,0E8h,0D1h,0CFh,1Ch,00h,8Dh
+    db 4Ch,24h,2Ch,88h,5Ch,24h,24h,0E8h,34h,0C7h,1Ch,00h,8Dh,4Ch,24h,30h
+    db 0C7h,44h,24h,24h,0FFh,0FFh,0FFh,0FFh,0E8h,23h,0C7h,1Ch,00h,83h,0FEh,01h
+    db 0B8h,01h,00h,00h,00h,74h,05h,0B8h,02h,00h,00h,00h,8Bh,4Ch,24h,1Ch
+    db 5Eh,64h,89h,0Dh,00h,00h,00h,00h,5Bh,83h,0C4h,20h,0C3h
+?OSDisplayWarningBox@@YA?AW4OSDisplayButtonType@@VAsciiString@@0II@Z ENDP
+_TEXT ENDS
+END
