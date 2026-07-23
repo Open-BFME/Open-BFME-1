f5ccaf9f8bf1fe6c9df468a951e3096d9f015660
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 04:43:04 +0000

    Match AIPlayer destructor with retail MASM

---
+++ Code/masm_dumps/AIPlayer_destructor.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ??1AIPlayer@@MAE@XZ
+; Exact 87B @ 0x001613C0; AIPlayer vtable slot 0 -> deleting wrapper -> this body.
+_TEXT SEGMENT
+public ??1AIPlayer@@MAE@XZ
+??1AIPlayer@@MAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0F8h, 53h, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 89h, 74h, 24h, 04h, 0C7h, 06h, 0B0h
+    db 68h, 09h, 01h, 68h, 50h, 0Eh, 56h, 00h, 0C7h, 44h, 24h, 14h, 00h, 00h, 00h, 00h
+    db 0E8h, 0E8h, 83h, 0EEh, 0FFh, 68h, 50h, 0Eh, 56h, 00h, 8Bh, 0CEh, 0E8h, 1Fh, 17h, 0EBh
+    db 0FFh, 8Bh, 4Ch, 24h, 08h, 0C7h, 06h, 44h, 37h, 07h, 01h, 5Eh, 64h, 89h, 0Dh, 00h
+    db 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1AIPlayer@@MAE@XZ ENDP
+_TEXT ENDS
+END
