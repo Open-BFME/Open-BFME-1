018e636f5def65308e6a6fcfb0dc366dceb8cac0
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:29:40 +0000

    Recover W3DLeftHUDDraw retail body

---
+++ Code/masm_dumps/W3DLeftHUDDraw_799DC0.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+; ?W3DLeftHUDDraw@@YAXPAVGameWindow@@PAVWinInstanceData@@@Z
+; Exact 271B @ 0x00799DC0; retail SHA-256 3342a48b2ce3ca581ae492d350b092e8d68c5110987dcdff022f0af25c25668e
+_TEXT SEGMENT
+public ?W3DLeftHUDDraw@@YAXPAVGameWindow@@PAVWinInstanceData@@@Z
+?W3DLeftHUDDraw@@YAXPAVGameWindow@@PAVWinInstanceData@@@Z PROC
+    db 8Bh, 0Dh, 8Ch, 14h, 2Fh, 01h, 0A1h, 48h, 0D7h, 2Eh, 01h, 8Bh, 11h, 83h, 0ECh, 14h
+    db 56h, 57h, 8Bh, 78h, 0Ch, 0FFh, 92h, 3Ch, 01h, 00h, 00h, 8Bh, 0F0h, 85h, 0F6h, 74h
+    db 7Bh, 8Bh, 7Ch, 24h, 20h, 8Dh, 44h, 24h, 10h, 50h, 8Dh, 4Ch, 24h, 10h, 51h, 8Bh
+    db 0CFh, 0E8h, 55h, 5Bh, 89h, 0FFh, 8Dh, 54h, 24h, 18h, 52h, 8Dh, 44h, 24h, 18h, 50h
+    db 8Bh, 0CFh, 0E8h, 0B5h, 0D0h, 89h, 0FFh, 8Bh, 44h, 24h, 10h, 8Bh, 7Ch, 24h, 18h, 8Bh
+    db 0Dh, 70h, 12h, 2Fh, 01h, 8Bh, 11h, 03h, 0F8h, 8Bh, 44h, 24h, 0Ch, 89h, 7Ch, 24h
+    db 08h, 8Bh, 7Ch, 24h, 14h, 0DBh, 44h, 24h, 08h, 6Ah, 0FFh, 83h, 0ECh, 10h, 0D9h, 5Ch
+    db 24h, 0Ch, 03h, 0F8h, 89h, 7Ch, 24h, 1Ch, 0DBh, 44h, 24h, 1Ch, 0D9h, 5Ch, 24h, 08h
+    db 0DBh, 44h, 24h, 24h, 0D9h, 5Ch, 24h, 04h, 0DBh, 44h, 24h, 20h, 0D9h, 1Ch, 24h, 56h
+    db 0FFh, 92h, 0E0h, 00h, 00h, 00h, 5Fh, 5Eh, 83h, 0C4h, 14h, 0C3h, 0A1h, 0E4h, 0F0h, 2Eh
+    db 01h, 8Ah, 48h, 0Dh, 84h, 0C9h, 75h, 12h, 8Ah, 48h, 0Ch, 84h, 0C9h, 75h, 5Ah, 8Bh
+    db 0CFh, 0E8h, 8Eh, 0BCh, 87h, 0FFh, 84h, 0C0h, 74h, 4Fh, 8Bh, 74h, 24h, 20h, 8Dh, 4Ch
+    db 24h, 10h, 51h, 8Dh, 54h, 24h, 10h, 52h, 8Bh, 0CEh, 0E8h, 0BCh, 5Ah, 89h, 0FFh, 8Dh
+    db 44h, 24h, 18h, 50h, 8Dh, 4Ch, 24h, 18h, 51h, 8Bh, 0CEh, 0E8h, 1Ch, 0D0h, 89h, 0FFh
+    db 8Bh, 44h, 24h, 18h, 8Bh, 0Dh, 0E4h, 0F0h, 2Eh, 01h, 8Bh, 11h, 83h, 0C0h, 0FEh, 6Ah
+    db 0FFh, 50h, 8Bh, 44h, 24h, 1Ch, 83h, 0C0h, 0FEh, 50h, 8Bh, 44h, 24h, 1Ch, 40h, 50h
+    db 8Bh, 44h, 24h, 1Ch, 40h, 50h, 0FFh, 52h, 1Ch, 5Fh, 5Eh, 83h, 0C4h, 14h, 0C3h
+?W3DLeftHUDDraw@@YAXPAVGameWindow@@PAVWinInstanceData@@@Z ENDP
+_TEXT ENDS
+END
