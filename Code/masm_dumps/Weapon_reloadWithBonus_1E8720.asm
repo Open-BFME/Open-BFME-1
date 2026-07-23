3e1b8f2f3fc41a9484b1b4e0b842b70f2f38da0b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:00:48 +0000

    Recover Weapon reloadWithBonus retail body

---
+++ Code/masm_dumps/Weapon_reloadWithBonus_1E8720.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+
+; ?reloadWithBonus@Weapon@@IAEXPBVObject@@ABVWeaponBonus@@_N@Z
+; Exact 371 retail bytes @ RVA 0x001E8720; reviewed Weapon reload body.
+_TEXT SEGMENT
+public ?reloadWithBonus@Weapon@@IAEXPBVObject@@ABVWeaponBonus@@_N@Z
+?reloadWithBonus@Weapon@@IAEXPBVObject@@ABVWeaponBonus@@_N@Z PROC
+    db 53h,8Bh,5Ch,24h,08h,56h,8Bh,0F1h,8Bh,46h,04h,8Bh,88h,0ACh,04h,00h
+    db 00h,57h,33h,0FFh,3Bh,0CFh,7Eh,64h,8Dh,88h,0E8h,04h,00h,00h,0E8h,6Fh
+    db 8Ch,0E5h,0FFh,84h,0C0h,74h,34h,8Bh,46h,08h,8Bh,0Dh,98h,08h,2Fh,01h
+    db 50h,0E8h,0FDh,6Ah,0E3h,0FFh,3Bh,0C7h,74h,08h,8Bh,88h,0FCh,01h,00h,00h
+    db 0EBh,02h,33h,0C9h,3Bh,0CFh,74h,13h,8Bh,46h,04h,8Bh,11h,05h,0E8h,04h
+    db 00h,00h,50h,0FFh,92h,00h,01h,00h,00h,0EBh,03h,8Bh,46h,14h,8Bh,4Eh
+    db 04h,3Bh,81h,0ACh,04h,00h,00h,75h,13h,8Dh,8Bh,64h,02h,00h,00h,0E8h
+    db 0E0h,35h,0E3h,0FFh,84h,0C0h,0Fh,84h,0F1h,00h,00h,00h,8Bh,46h,04h,8Bh
+    db 90h,0ACh,04h,00h,00h,8Dh,88h,0E8h,04h,00h,00h,89h,56h,14h,0E8h,0FFh
+    db 8Bh,0E5h,0FFh,84h,0C0h,74h,34h,8Bh,46h,08h,8Bh,0Dh,98h,08h,2Fh,01h
+    db 50h,0E8h,8Dh,6Ah,0E3h,0FFh,3Bh,0C7h,74h,08h,8Bh,88h,0FCh,01h,00h,00h
+    db 0EBh,02h,33h,0C9h,3Bh,0CFh,74h,13h,8Bh,46h,04h,8Bh,11h,05h,0E8h,04h
+    db 00h,00h,50h,0FFh,92h,00h,01h,00h,00h,0EBh,03h,8Bh,46h,14h,3Bh,0C7h
+    db 77h,07h,0C7h,46h,14h,0FFh,0FFh,0FFh,7Fh,8Bh,46h,10h,55h,0BDh,03h,00h
+    db 00h,00h,3Bh,0C5h,74h,03h,89h,6Eh,10h,8Ah,44h,24h,1Ch,84h,0C0h,74h
+    db 06h,89h,7Ch,24h,1Ch,0EBh,11h,8Bh,44h,24h,18h,8Bh,4Eh,04h,50h,0E8h
+    db 8Bh,4Eh,0E4h,0FFh,89h,44h,24h,1Ch,8Bh,0Dh,98h,08h,2Fh,01h,0DBh,44h
+    db 24h,1Ch,8Bh,41h,3Ch,85h,0C0h,89h,44h,24h,1Ch,0DBh,44h,24h,1Ch,89h
+    db 46h,28h,7Dh,06h,0D8h,05h,58h,53h,07h,01h,0DEh,0C1h,0E8h,0E7h,0E5h,80h
+    db 00h,81h,0C3h,64h,02h,00h,00h,8Bh,0CBh,89h,46h,18h,0E8h,13h,35h,0E3h
+    db 0FFh,84h,0C0h,74h,20h,57h,8Bh,0CBh,0E8h,7Ch,40h,0E5h,0FFh,85h,0C0h,74h
+    db 0Eh,39h,68h,10h,8Bh,56h,18h,89h,50h,18h,74h,03h,89h,68h,10h,47h
+    db 83h,0FFh,04h,7Ch,0E0h,8Bh,0CEh,0E8h,0FDh,7Ch,0E5h,0FFh,5Dh,5Fh,5Eh,5Bh
+    db 0C2h,0Ch,00h
+?reloadWithBonus@Weapon@@IAEXPBVObject@@ABVWeaponBonus@@_N@Z ENDP
+_TEXT ENDS
+END
