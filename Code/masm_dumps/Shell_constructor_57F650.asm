18c92339a4dfefa12ea929e38a5064ae41bc7d76
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:44:06 +0000

    Recover Shell default constructor

---
+++ Code/masm_dumps/Shell_constructor_57F650.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+; ??0Shell@@QAE@XZ
+; Exact 259B @ 0x0057F650; certified retail Shell default constructor
+_TEXT SEGMENT
+public ??0Shell@@QAE@XZ
+??0Shell@@QAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 09h, 68h, 03h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 53h, 56h, 8Bh, 0F1h, 89h, 74h, 24h, 08h
+    db 0E8h, 0BBh, 23h, 42h, 00h, 33h, 0DBh, 8Dh, 4Eh, 54h, 0C7h, 06h, 58h, 0B5h, 10h, 01h
+    db 89h, 5Ch, 24h, 18h, 89h, 19h, 89h, 5Eh, 48h, 33h, 0C0h, 89h, 46h, 08h, 89h, 46h
+    db 0Ch, 89h, 46h, 10h, 89h, 46h, 14h, 89h, 46h, 18h, 89h, 46h, 1Ch, 89h, 46h, 20h
+    db 89h, 46h, 24h, 89h, 46h, 28h, 89h, 46h, 2Ch, 89h, 46h, 30h, 89h, 46h, 34h, 89h
+    db 46h, 38h, 89h, 46h, 3Ch, 89h, 46h, 40h, 89h, 46h, 44h, 53h, 68h, 1Ch, 30h, 07h
+    db 01h, 0C6h, 44h, 24h, 20h, 01h, 88h, 5Eh, 4Eh, 88h, 5Eh, 4Fh, 88h, 5Eh, 4Ch, 88h
+    db 5Eh, 4Dh, 88h, 5Eh, 50h, 0E8h, 46h, 86h, 30h, 00h, 6Ah, 34h, 0C6h, 46h, 58h, 01h
+    db 88h, 5Eh, 59h, 0E8h, 48h, 28h, 30h, 00h, 83h, 0C4h, 04h, 89h, 44h, 24h, 0Ch, 3Bh
+    db 0C3h, 0C6h, 44h, 24h, 18h, 02h, 74h, 09h, 8Bh, 0C8h, 0E8h, 8Eh, 7Bh, 0A9h, 0FFh, 0EBh
+    db 02h, 33h, 0C0h, 6Ah, 08h, 0C6h, 44h, 24h, 1Ch, 01h, 89h, 46h, 5Ch, 0E8h, 1Eh, 28h
+    db 30h, 00h, 83h, 0C4h, 04h, 89h, 44h, 24h, 0Ch, 3Bh, 0C3h, 0C6h, 44h, 24h, 18h, 03h
+    db 74h, 09h, 8Bh, 0C8h, 0E8h, 9Dh, 0BCh, 0ACh, 0FFh, 0EBh, 02h, 33h, 0C0h, 8Bh, 4Ch, 24h
+    db 10h, 89h, 46h, 60h, 89h, 5Eh, 68h, 89h, 5Eh, 6Ch, 89h, 5Eh, 48h, 0C7h, 46h, 64h
+    db 01h, 00h, 00h, 00h, 8Bh, 0C6h, 5Eh, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h
+    db 0C4h, 14h, 0C3h
+??0Shell@@QAE@XZ ENDP
+_TEXT ENDS
+END
