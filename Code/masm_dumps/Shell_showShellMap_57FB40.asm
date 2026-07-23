2cdab2b255f52198099d000e8e2db306709ef9c8
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:54:22 +0000

    Recover Shell showShellMap retail body

---
+++ Code/masm_dumps/Shell_showShellMap_57FB40.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+; ?showShellMap@Shell@@QAEX_N@Z
+; Exact 289B @ 0x0057FB40; certified retail Shell::showShellMap
+_TEXT SEGMENT
+public ?showShellMap@Shell@@QAEX_N@Z
+?showShellMap@Shell@@QAEX_N@Z PROC
+    db 056h, 08Bh, 0F1h, 08Ah, 04Ch, 024h, 008h, 084h, 0C9h, 074h, 016h, 0A1h, 0C8h, 0D5h, 02Eh, 001h
+    db 08Ah, 090h, 0B4h, 00Bh, 000h, 000h, 084h, 0D2h, 075h, 007h, 0B8h, 001h, 000h, 000h, 000h, 0EBh
+    db 002h, 033h, 0C0h, 088h, 046h, 04Eh, 08Bh, 015h, 0C8h, 0D5h, 02Eh, 001h, 08Bh, 082h, 080h, 00Bh
+    db 000h, 000h, 085h, 0C0h, 074h, 00Bh, 066h, 083h, 078h, 004h, 000h, 00Fh, 085h, 0DAh, 000h, 000h
+    db 000h, 0A1h, 098h, 008h, 02Fh, 001h, 085h, 0C0h, 00Fh, 084h, 0CDh, 000h, 000h, 000h, 084h, 0C9h
+    db 0C6h, 046h, 04Fh, 000h, 00Fh, 084h, 081h, 000h, 000h, 000h, 0A1h, 0C8h, 0D5h, 02Eh, 001h, 08Ah
+    db 088h, 0B4h, 00Bh, 000h, 000h, 084h, 0C9h, 074h, 072h, 08Bh, 00Dh, 070h, 012h, 02Fh, 001h, 0E8h
+    db 07Bh, 09Dh, 0A9h, 0FFh, 08Bh, 0CEh, 0E8h, 081h, 03Dh, 0ACh, 0FFh, 08Bh, 00Dh, 098h, 008h, 02Fh
+    db 001h, 08Bh, 081h, 00Ch, 001h, 000h, 000h, 083h, 0F8h, 004h, 074h, 049h, 083h, 0F8h, 008h, 074h
+    db 00Dh, 08Bh, 00Dh, 0ECh, 0D5h, 02Eh, 001h, 08Bh, 011h, 06Ah, 01Dh, 0FFh, 052h, 034h, 0A1h, 0C8h
+    db 0D5h, 02Eh, 001h, 08Dh, 088h, 0B0h, 00Bh, 000h, 000h, 051h, 08Dh, 088h, 084h, 00Bh, 000h, 000h
+    db 0E8h, 070h, 03Bh, 0A8h, 0FFh, 06Ah, 000h, 0E8h, 0BFh, 0ECh, 0AAh, 0FFh, 08Bh, 00Dh, 0ECh, 0D5h
+    db 02Eh, 001h, 08Bh, 011h, 083h, 0C4h, 004h, 06Ah, 01Eh, 0FFh, 052h, 034h, 06Ah, 004h, 08Bh, 0C8h
+    db 0E8h, 05Bh, 017h, 0A8h, 0FFh, 0B0h, 001h, 05Eh, 0C2h, 004h, 000h, 08Bh, 00Dh, 070h, 012h, 02Fh
+    db 001h, 0E8h, 009h, 09Dh, 0A9h, 0FFh, 08Bh, 00Dh, 068h, 0D6h, 02Eh, 001h, 085h, 0C9h, 074h, 010h
+    db 08Bh, 056h, 064h, 08Bh, 001h, 052h, 0FFh, 050h, 04Ch, 0C7h, 046h, 064h, 001h, 000h, 000h, 000h
+    db 0A1h, 098h, 008h, 02Fh, 001h, 083h, 0B8h, 00Ch, 001h, 000h, 000h, 004h, 075h, 00Dh, 08Bh, 00Dh
+    db 0ECh, 0D5h, 02Eh, 001h, 08Bh, 011h, 06Ah, 01Dh, 0FFh, 052h, 034h, 032h, 0C0h, 05Eh, 0C2h, 004h
+    db 000h
+?showShellMap@Shell@@QAEX_N@Z ENDP
+_TEXT ENDS
+END
