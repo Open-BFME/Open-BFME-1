304286ed8df19e62c30ace5d32dd9d56364a209d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:15:31 +0000

    Recover InGameUI setRadiusCursor retail body

---
+++ Code/masm_dumps/setRadiusCursor_InGameUI_43DBE0.asm
@@ -0,0 +1,39 @@
+.386
+.model flat
+; ?setRadiusCursor@InGameUI@@UAEXW4RadiusCursorType@@PBVSpecialPowerTemplate@@W4WeaponSlotType@@@Z
+; Certified exact 461B retail body @ RVA 0x0043DBE0
+_TEXT SEGMENT
+public ?setRadiusCursor@InGameUI@@UAEXW4RadiusCursorType@@PBVSpecialPowerTemplate@@W4WeaponSlotType@@@Z
+?setRadiusCursor@InGameUI@@UAEXW4RadiusCursorType@@PBVSpecialPowerTemplate@@W4WeaponSlotType@@@Z PROC
+    db 83h, 0ECh, 0Ch, 53h, 55h, 8Bh, 6Ch, 24h, 18h, 56h, 57h, 8Bh, 0F9h, 3Bh, 0AFh, 94h
+    db 12h, 00h, 00h, 0Fh, 84h, 88h, 01h, 00h, 00h, 8Dh, 8Fh, 84h, 12h, 00h, 00h, 0E8h
+    db 0A5h, 0CCh, 0C0h, 0FFh, 85h, 0EDh, 0C7h, 87h, 94h, 12h, 00h, 00h, 00h, 00h, 00h, 00h
+    db 0Fh, 84h, 8Dh, 01h, 00h, 00h, 8Bh, 87h, 30h, 02h, 00h, 00h, 85h, 0C0h, 74h, 3Fh
+    db 8Bh, 40h, 10h, 83h, 0F8h, 1Fh, 75h, 17h, 0A1h, 48h, 0D7h, 2Eh, 01h, 85h, 0C0h, 74h
+    db 2Eh, 8Bh, 48h, 0Ch, 85h, 0C9h, 74h, 27h, 0E8h, 0DFh, 56h, 0BEh, 0FFh, 0EBh, 1Ah, 83h
+    db 0F8h, 24h, 75h, 1Bh, 0A1h, 48h, 0D7h, 2Eh, 01h, 85h, 0C0h, 74h, 12h, 8Bh, 48h, 0Ch
+    db 85h, 0C9h, 74h, 0Bh, 0E8h, 11h, 7Eh, 0BCh, 0FFh, 8Bh, 0F0h, 85h, 0F6h, 75h, 32h, 8Bh
+    db 07h, 8Bh, 0CFh, 0FFh, 90h, 0F0h, 00h, 00h, 00h, 85h, 0C0h, 0Fh, 84h, 32h, 01h, 00h
+    db 00h, 8Bh, 17h, 8Bh, 0CFh, 0FFh, 92h, 04h, 01h, 00h, 00h, 85h, 0C0h, 0Fh, 84h, 20h
+    db 01h, 00h, 00h, 8Bh, 0B0h, 0FCh, 00h, 00h, 00h, 85h, 0F6h, 0Fh, 84h, 12h, 01h, 00h
+    db 00h, 8Bh, 0CEh, 0E8h, 8Ch, 2Bh, 0BEh, 0FFh, 8Bh, 0D8h, 85h, 0DBh, 0Fh, 84h, 01h, 01h
+    db 00h, 00h, 8Dh, 45h, 0FEh, 83h, 0F8h, 32h, 0Fh, 87h, 0F5h, 00h, 00h, 00h, 0Fh, 0B6h
+    db 80h, 0C0h, 0DDh, 83h, 00h, 0FFh, 24h, 85h, 0B0h, 0DDh, 83h, 00h, 8Bh, 4Ch, 24h, 28h
+    db 51h, 8Dh, 8Eh, 64h, 02h, 00h, 00h, 0E8h, 1Dh, 0ECh, 0BFh, 0FFh, 85h, 0C0h, 0Fh, 84h
+    db 0CFh, 00h, 00h, 00h, 8Bh, 40h, 04h, 0D9h, 40h, 2Ch, 0D8h, 40h, 28h, 0EBh, 43h, 8Bh
+    db 54h, 24h, 28h, 52h, 8Dh, 8Eh, 64h, 02h, 00h, 00h, 0E8h, 0FAh, 0EBh, 0BFh, 0FFh, 85h
+    db 0C0h, 0Fh, 84h, 0ACh, 00h, 00h, 00h, 8Bh, 40h, 04h, 8Bh, 88h, 10h, 05h, 00h, 00h
+    db 89h, 4Ch, 24h, 28h, 0EBh, 20h, 56h, 0E8h, 86h, 5Bh, 0C0h, 0FFh, 83h, 0C4h, 04h, 0EBh
+    db 11h, 8Bh, 4Ch, 24h, 24h, 85h, 0C9h, 0Fh, 84h, 86h, 00h, 00h, 00h, 0E8h, 0DFh, 49h
+    db 0BDh, 0FFh, 0D9h, 5Ch, 24h, 28h, 0D9h, 44h, 24h, 28h, 0D8h, 1Dh, 50h, 53h, 07h, 01h
+    db 0DFh, 0E0h, 0F6h, 0C4h, 41h, 7Bh, 6Ch, 8Bh, 54h, 24h, 28h, 8Dh, 87h, 84h, 12h, 00h
+    db 00h, 50h, 53h, 8Dh, 4Ch, 6Dh, 00h, 52h, 8Dh, 44h, 24h, 1Ch, 0C1h, 0E1h, 04h, 50h
+    db 8Dh, 8Ch, 39h, 94h, 08h, 00h, 00h, 0C7h, 44h, 24h, 20h, 00h, 00h, 00h, 00h, 0C7h
+    db 44h, 24h, 24h, 00h, 00h, 00h, 00h, 0C7h, 44h, 24h, 28h, 00h, 00h, 00h, 00h, 0E8h
+    db 0B2h, 9Ah, 0BFh, 0FFh, 8Bh, 0CFh, 89h, 0AFh, 94h, 12h, 00h, 00h, 0E8h, 4Ah, 7Ch, 0C0h
+    db 0FFh, 8Ah, 44h, 24h, 2Ch, 8Bh, 8Fh, 88h, 12h, 00h, 00h, 0F6h, 0D8h, 1Bh, 0C0h, 25h
+    db 0EBh, 0EBh, 00h, 00h, 05h, 14h, 14h, 0FFh, 0FFh, 85h, 0C9h, 74h, 06h, 50h, 0E8h, 83h
+    db 0D7h, 0BEh, 0FFh, 5Fh, 5Eh, 5Dh, 5Bh, 83h, 0C4h, 0Ch, 0C2h, 10h, 00h
+?setRadiusCursor@InGameUI@@UAEXW4RadiusCursorType@@PBVSpecialPowerTemplate@@W4WeaponSlotType@@@Z ENDP
+_TEXT ENDS
+END
