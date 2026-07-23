670ca9e8cb9f1c307e08b12957f760c894031ec6
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 06:21:07 +0000

    Recover ModuleFactory interface mask lookup

---
+++ Code/masm_dumps/findModuleInterfaceMask_ModuleFactory_00129100.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+; ?findModuleInterfaceMask@ModuleFactory@@QAEHABVAsciiString@@W4ModuleType@@@Z
+; Certified 84B retail body @ 0x00129100.
+_TEXT SEGMENT
+public ?findModuleInterfaceMask@ModuleFactory@@QAEHABVAsciiString@@W4ModuleType@@@Z
+?findModuleInterfaceMask@ModuleFactory@@QAEHABVAsciiString@@W4ModuleType@@@Z PROC
+    db 056h,08Bh,0F1h,08Bh,04Ch,024h,008h,08Bh,001h,085h,0C0h,074h,041h,066h,083h,078h
+    db 004h,000h,074h,03Ah,08Bh,044h,024h,00Ch,050h,051h,0E8h,01Ah,023h,0F2h,0FFh,083h
+    db 0C4h,008h,08Dh,04Ch,024h,008h,051h,08Dh,054h,024h,010h,083h,0C6h,00Ch,052h,08Bh
+    db 0CEh,089h,044h,024h,010h,0E8h,01Ah,02Fh,0EEh,0FFh,08Bh,044h,024h,00Ch,03Bh,006h
+    db 074h,00Ch,083h,0C0h,014h,074h,007h,08Bh,040h,00Ch,05Eh,0C2h,008h,000h,033h,0C0h
+    db 05Eh,0C2h,008h,000h
+?findModuleInterfaceMask@ModuleFactory@@QAEHABVAsciiString@@W4ModuleType@@@Z ENDP
+_TEXT ENDS
+END
