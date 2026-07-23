27af11ede306e1fce23cb8ee7f0c6d2856f2bc71
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:09:19 +0000

    Recover ControlBar special power shortcut animation

---
+++ Code/masm_dumps/_animateSpecialPowerShortcut_ControlBar__QAEX_N_Z_49D3F0.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?animateSpecialPowerShortcut@ControlBar@@QAEX_N@Z
+; Exact 138B retail body @ 0x0049D3F0; certified packet 52.
+_TEXT SEGMENT
+public ?animateSpecialPowerShortcut@ControlBar@@QAEX_N@Z
+?animateSpecialPowerShortcut@ControlBar@@QAEX_N@Z PROC
+    db 056h,08Bh,0F1h,08Bh,086h,0FCh,000h,000h,000h,085h,0C0h,074h,079h,08Bh,046h,010h
+    db 085h,0C0h,074h,072h,08Bh,086h,0F4h,000h,000h,000h,085h,0C0h,074h,068h,057h,033h
+    db 0FFh,085h,0C0h,07Eh,060h,053h,08Dh,09Eh,0CCh,000h,000h,000h,08Dh,064h,024h,000h
+    db 08Bh,00Bh,0E8h,011h,091h,0BAh,0FFh,085h,0C0h,075h,014h,08Bh,086h,0F4h,000h,000h
+    db 000h,047h,083h,0C3h,004h,03Bh,0F8h,07Ch,0E7h,05Bh,05Fh,05Eh,0C2h,004h,000h,08Ah
+    db 044h,024h,010h,084h,0C0h,08Bh,04Eh,010h,074h,025h,08Bh,001h,0FFh,050h,010h,08Bh
+    db 08Eh,0FCh,000h,000h,000h,06Ah,000h,068h,0F4h,001h,000h,000h,06Ah,001h,06Ah,001h
+    db 051h,08Bh,04Eh,010h,0E8h,0B9h,07Eh,0BAh,0FFh,05Bh,05Fh,05Eh,0C2h,004h,000h,0E8h
+    db 0F1h,0A6h,0B8h,0FFh,05Bh,05Fh,05Eh,0C2h,004h,000h
+?animateSpecialPowerShortcut@ControlBar@@QAEX_N@Z ENDP
+_TEXT ENDS
+END
