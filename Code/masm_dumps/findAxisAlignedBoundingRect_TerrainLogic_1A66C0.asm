adfcd8ac88e779952ddc596128762fb6f08a3cbe
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 04:36:29 +0000

    Match TerrainLogic water bounding rect

---
+++ Code/masm_dumps/findAxisAlignedBoundingRect_TerrainLogic_1A66C0.asm
@@ -0,0 +1,54 @@
+.386
+.model flat
+
+; ?findAxisAlignedBoundingRect@TerrainLogic@@IAEXPBVWaterHandle@@PAURegion3D@@@Z
+; Exact 668 retail bytes @ 0x001A66C0
+; setWaterHeight@0x001AC560 calls thunk 0x00033F41 -> this body
+_TEXT SEGMENT
+public ?findAxisAlignedBoundingRect@TerrainLogic@@IAEXPBVWaterHandle@@PAURegion3D@@@Z
+?findAxisAlignedBoundingRect@TerrainLogic@@IAEXPBVWaterHandle@@PAURegion3D@@@Z PROC
+    db 81h,0ECh,84h,00h,00h,00h,55h,57h,8Bh,0BCh,24h,90h,00h,00h,00h,33h
+    db 0EDh,3Bh,0FDh,0Fh,84h,78h,02h,00h,00h,56h,8Bh,0B4h,24h,98h,00h,00h
+    db 00h,3Bh,0F5h,0Fh,84h,67h,02h,00h,00h,8Bh,01h,8Dh,54h,24h,18h,52h
+    db 0FFh,50h,20h,0D9h,44h,24h,24h,81h,0FFh,0D4h,0F4h,2Eh,01h,0D8h,05h,50h
+    db 0C3h,09h,01h,0D9h,1Eh,0D9h,44h,24h,28h,0D8h,05h,50h,0C3h,09h,01h,0D9h
+    db 5Eh,04h,0D9h,44h,24h,18h,0D8h,25h,50h,0C3h,09h,01h,0D9h,5Eh,0Ch,0D9h
+    db 44h,24h,1Ch,0D8h,25h,50h,0C3h,09h,01h,0D9h,5Eh,10h,0Fh,85h,65h,01h
+    db 00h,00h,68h,0AEh,0DEh,40h,00h,6Ah,04h,6Ah,0Ch,8Dh,44h,24h,3Ch,50h
+    db 0E8h,17h,47h,0E6h,0FFh,8Bh,0Dh,14h,70h,2Fh,01h,8Bh,11h,8Dh,44h,24h
+    db 0Ch,50h,8Dh,44h,24h,18h,50h,8Dh,44h,24h,18h,50h,57h,0FFh,52h,44h
+    db 0D9h,44h,24h,10h,0D8h,4Ch,24h,0Ch,89h,6Ch,24h,30h,89h,6Ch,24h,34h
+    db 0E8h,0C3h,06h,85h,00h,0D9h,44h,24h,14h,0D8h,4Ch,24h,0Ch,89h,44h,24h
+    db 3Ch,89h,6Ch,24h,40h,89h,44h,24h,48h,0E8h,0AAh,06h,85h,00h,8Bh,0Dh
+    db 14h,70h,2Fh,01h,89h,44h,24h,4Ch,89h,44h,24h,58h,8Dh,44h,24h,60h
+    db 50h,89h,6Ch,24h,58h,8Bh,11h,57h,0FFh,52h,3Ch,0D9h,46h,10h,8Bh,0Eh
+    db 8Bh,56h,04h,8Bh,46h,0Ch,89h,4Ch,24h,14h,89h,54h,24h,10h,89h,44h
+    db 24h,0Ch,8Dh,4Ch,24h,34h,0BAh,04h,00h,00h,00h,0EBh,03h,8Dh,49h,00h
+    db 0DBh,41h,0FCh,0DBh,01h,0DBh,41h,04h,0D9h,0C2h,0D9h,44h,24h,68h,0D8h,0CAh
+    db 0D9h,44h,24h,64h,0D8h,0CCh,0DEh,0C1h,0D9h,44h,24h,60h,0D8h,0CDh,0DEh,0C1h
+    db 0D8h,44h,24h,6Ch,0D9h,5Ch,24h,18h,0D9h,44h,24h,70h,0D8h,0C9h,0D9h,44h
+    db 24h,78h,0D8h,0CBh,0DEh,0C1h,0D9h,44h,24h,74h,0D8h,0CCh,0DEh,0C1h,0D8h,44h
+    db 24h,7Ch,0D9h,54h,24h,1Ch,0DDh,0DCh,0DDh,0D8h,0DDh,0D8h,0DDh,0D8h,0D9h,44h
+    db 24h,18h,0D8h,5Ch,24h,14h,0DFh,0E0h,0F6h,0C4h,05h,7Ah,08h,8Bh,44h,24h
+    db 18h,89h,44h,24h,14h,0D9h,44h,24h,18h,0D8h,5Ch,24h,0Ch,0DFh,0E0h,0F6h
+    db 0C4h,41h,75h,08h,8Bh,44h,24h,18h,89h,44h,24h,0Ch,0D8h,54h,24h,10h
+    db 0DFh,0E0h,0F6h,0C4h,05h,7Ah,04h,0D9h,54h,24h,10h,0D8h,0D9h,0DFh,0E0h,0F6h
+    db 0C4h,41h,75h,06h,0DDh,0D8h,0D9h,44h,24h,1Ch,83h,0C1h,0Ch,4Ah,0Fh,85h
+    db 5Ch,0FFh,0FFh,0FFh,8Bh,4Ch,24h,14h,0D9h,5Eh,10h,8Bh,54h,24h,10h,8Bh
+    db 44h,24h,0Ch,89h,0Eh,89h,56h,04h,89h,46h,0Ch,5Eh,5Fh,5Dh,81h,0C4h
+    db 84h,00h,00h,00h,0C2h,08h,00h,8Bh,0Fh,8Bh,41h,14h,53h,33h,0DBh,3Bh
+    db 0C5h,89h,44h,24h,10h,0Fh,8Eh,0A4h,00h,00h,00h,0EBh,03h,8Dh,49h,00h
+    db 3Bh,0DDh,8Bh,0Fh,8Bh,0C3h,7Dh,02h,33h,0C0h,8Bh,51h,14h,3Bh,0C2h,7Ch
+    db 03h,8Dh,42h,0FFh,8Dh,14h,40h,8Bh,41h,10h,0DBh,04h,90h,8Dh,0Ch,90h
+    db 0D8h,16h,0DFh,0E0h,0F6h,0C4h,05h,7Ah,04h,0D9h,1Eh,0EBh,02h,0DDh,0D8h,0DBh
+    db 01h,0D8h,56h,0Ch,0DFh,0E0h,0F6h,0C4h,41h,75h,05h,0D9h,5Eh,0Ch,0EBh,02h
+    db 0DDh,0D8h,0DBh,41h,04h,0D8h,56h,04h,0DFh,0E0h,0F6h,0C4h,05h,7Ah,05h,0D9h
+    db 5Eh,04h,0EBh,02h,0DDh,0D8h,0DBh,41h,04h,0D8h,56h,10h,0DFh,0E0h,0F6h,0C4h
+    db 41h,75h,05h,0D9h,5Eh,10h,0EBh,02h,0DDh,0D8h,0DBh,41h,08h,0D8h,56h,08h
+    db 0DFh,0E0h,0F6h,0C4h,05h,7Ah,05h,0D9h,5Eh,08h,0EBh,02h,0DDh,0D8h,0DBh,41h
+    db 08h,0D8h,56h,14h,0DFh,0E0h,0F6h,0C4h,41h,75h,05h,0D9h,5Eh,14h,0EBh,02h
+    db 0DDh,0D8h,8Bh,44h,24h,10h,43h,3Bh,0D8h,0Fh,8Ch,61h,0FFh,0FFh,0FFh,5Bh
+    db 5Eh,5Fh,5Dh,81h,0C4h,84h,00h,00h,00h,0C2h,08h,00h
+?findAxisAlignedBoundingRect@TerrainLogic@@IAEXPBVWaterHandle@@PAURegion3D@@@Z ENDP
+_TEXT ENDS
+END
