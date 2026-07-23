5ae4417efab1bbbcc159fa6bbe5300341fc6350a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 04:39:42 +0000

    Match ShareBuffer unsigned short destructor

---
+++ Code/masm_dumps/ShareBuffer_unsigned_short_destructor.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+; ??1?$ShareBufferClass@G@@UAE@XZ
+; Exact 29B @ 0x009240D0; ctor 0x923E50 stores vtable 0x113C110,
+; whose scalar-deleting destructor 0x9240B0 calls this body.
+_TEXT SEGMENT
+public ??1?$ShareBufferClass@G@@UAE@XZ
+??1?$ShareBufferClass@G@@UAE@XZ PROC
+    db 56h, 8Bh, 0F1h, 8Bh, 46h, 08h, 50h, 0C7h, 06h, 10h, 0C1h, 13h, 01h, 0E8h, 0Eh, 0DEh
+    db 0F5h, 0FFh, 83h, 0C4h, 04h, 0C7h, 06h, 0ACh, 35h, 11h, 01h, 5Eh, 0C3h
+??1?$ShareBufferClass@G@@UAE@XZ ENDP
+_TEXT ENDS
+END
