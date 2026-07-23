d5f9fa09b08d7da63ba0f18931a00da1ea84fc02
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 17:17:11 +0000

    Recover ToppleUpdate onCollide retail body

---
+++ Code/masm_dumps/onCollide_ToppleUpdate_UAEXPAVObject_PBUCoord3D_1_Z_002B12C0_e8785e1b.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?onCollide@ToppleUpdate@@UAEXPAVObject@@PBUCoord3D@@1@Z
+; Exact 161 retail bytes @ 0x002B12C0
+; SHA-256 e8785e1bacdd68f43b7ed8db67c515f1a9957f6e0fd9ad890a919711e3a93870
+_TEXT SEGMENT
+public ?onCollide@ToppleUpdate@@UAEXPAVObject@@PBUCoord3D@@1@Z
+?onCollide@ToppleUpdate@@UAEXPAVObject@@PBUCoord3D@@1@Z PROC
+    db 83h,0ECh,0Ch,56h,8Bh,74h,24h,14h,85h,0F6h,57h,8Bh,0F9h,0Fh,84h,86h
+    db 00h,00h,00h,53h,8Bh,0CEh,0E8h,0B5h,39h,0D6h,0FFh,8Bh,4Fh,0E8h,8Ah,0D8h
+    db 0E8h,3Eh,88h,0D6h,0FFh,3Ah,0D8h,5Bh,7Eh,6Fh,8Bh,47h,0E8h,8Bh,48h,38h
+    db 83h,0C0h,38h,89h,4Ch,24h,08h,0D9h,40h,04h,0D9h,44h,24h,08h,8Bh,0CEh
+    db 0D8h,66h,38h,0C7h,44h,24h,10h,00h,00h,00h,00h,0D9h,5Ch,24h,08h,0D8h
+    db 66h,3Ch,0D9h,5Ch,24h,0Ch,0E8h,0ADh,34h,0D5h,0FFh,0D9h,5Ch,24h,18h,0D9h
+    db 05h,50h,53h,07h,01h,0D9h,44h,24h,18h,0DAh,0E9h,0DFh,0E0h,0F6h,0C4h,44h
+    db 7Ah,13h,8Bh,8Eh,14h,02h,00h,00h,85h,0C9h,74h,09h,0E8h,87h,34h,0D5h
+    db 0FFh,0D9h,5Ch,24h,18h,8Bh,54h,24h,18h,8Bh,4Fh,0E8h,6Ah,00h,52h,8Dh
+    db 44h,24h,10h,50h,0E8h,78h,0Ah,0D8h,0FFh,5Fh,5Eh,83h,0C4h,0Ch,0C2h,0Ch
+    db 00h
+?onCollide@ToppleUpdate@@UAEXPAVObject@@PBUCoord3D@@1@Z ENDP
+_TEXT ENDS
+END
