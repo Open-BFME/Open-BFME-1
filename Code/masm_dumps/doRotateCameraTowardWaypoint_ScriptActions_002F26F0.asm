33c8f1da967bc2cd83dca0e22b2d0d3f50ed5ee7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:02:29 +0000

    Recover ScriptActions waypoint camera rotation

---
+++ Code/masm_dumps/doRotateCameraTowardWaypoint_ScriptActions_002F26F0.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?doRotateCameraTowardWaypoint@ScriptActions@@IAEXABVAsciiString@@MMM_N@Z
+; Exact 116 retail bytes @ 0x002F26F0
+_TEXT SEGMENT
+public ?doRotateCameraTowardWaypoint@ScriptActions@@IAEXABVAsciiString@@MMM_N@Z
+?doRotateCameraTowardWaypoint@ScriptActions@@IAEXABVAsciiString@@MMM_N@Z PROC
+    db 051h,08Bh,044h,024h,008h,053h,056h,057h,051h,089h,064h,024h,010h,08Bh,0CCh,050h
+    db 0E8h,05Bh,054h,059h,000h,08Bh,00Dh,0CCh,0F4h,02Eh,001h,08Bh,011h,0FFh,052h,07Ch
+    db 08Bh,0F0h,085h,0F6h,074h,047h,08Bh,044h,024h,024h,0D9h,044h,024h,020h,0D8h,00Dh
+    db 068h,05Ch,007h,001h,08Bh,03Dh,000h,016h,02Fh,001h,08Bh,01Fh,050h,083h,0ECh,008h
+    db 0D9h,05Ch,024h,004h,0D9h,044h,024h,028h,0D8h,00Dh,068h,05Ch,007h,001h,0D9h,01Ch
+    db 024h,0D9h,044h,024h,024h,0D8h,00Dh,068h,05Ch,007h,001h,0E8h,0E8h,046h,070h,000h
+    db 050h,083h,0C6h,00Ch,056h,08Bh,0CFh,0FFh,093h,0D0h,000h,000h,000h,05Fh,05Eh,05Bh
+    db 059h,0C2h,014h,000h
+?doRotateCameraTowardWaypoint@ScriptActions@@IAEXABVAsciiString@@MMM_N@Z ENDP
+_TEXT ENDS
+END
