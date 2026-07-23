d20b9dadce5d25780113dbd485afc0b833795aad
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:56:49 +0000

    Recover AIAttackSquadState constructor

---
+++ Code/masm_dumps/AIAttackSquadState_constructor_171330.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+; ??0AIAttackSquadState@@QAE@PAVStateMachine@@PAVAttackExitConditionsInterface@@@Z
+; Exact 54B @ 0x00171330; certified packet 96
+_TEXT SEGMENT
+public ??0AIAttackSquadState@@QAE@PAVStateMachine@@PAVAttackExitConditionsInterface@@@Z
+??0AIAttackSquadState@@QAE@PAVStateMachine@@PAVAttackExitConditionsInterface@@@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 84h, 7Eh, 09h, 01h
+    db 0E8h, 7Bh, 78h, 71h, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 61h, 22h, 0E9h
+    db 0FFh, 33h, 0C0h, 89h, 46h, 24h, 88h, 46h, 28h, 0C7h, 06h, 30h, 7Eh, 09h, 01h, 8Bh
+    db 0C6h, 5Eh, 59h, 0C2h, 08h, 00h
+??0AIAttackSquadState@@QAE@PAVStateMachine@@PAVAttackExitConditionsInterface@@@Z ENDP
+_TEXT ENDS
+END
