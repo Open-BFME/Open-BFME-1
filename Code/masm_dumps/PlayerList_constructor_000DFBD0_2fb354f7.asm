e1c2ff179aed823d544790d6660e4a9fd4ac2d7b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:13:01 +0000

    Recover PlayerList constructor retail body

---
+++ Code/masm_dumps/PlayerList_constructor_000DFBD0_2fb354f7.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ??0PlayerList@@QAE@XZ
+; Exact 162 retail bytes @ 0x000DFBD0; certified packet 217
+_TEXT SEGMENT
+public ??0PlayerList@@QAE@XZ
+??0PlayerList@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,0AEh,0A5h,0FFh,00h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h,55h,56h,57h,8Bh,0F9h,89h,7Ch,24h
+    db 0Ch,0E8h,3Ah,1Eh,8Ch,00h,33h,0F6h,89h,74h,24h,1Ch,0C7h,47h,08h,44h
+    db 37h,07h,01h,0C6h,44h,24h,1Ch,01h,0C7h,07h,7Ch,41h,08h,01h,0C7h,47h
+    db 08h,68h,41h,08h,01h,89h,77h,0Ch,89h,77h,10h,8Dh,6Fh,14h,8Bh,0FFh
+    db 68h,0A4h,06h,00h,00h,0E8h,06h,23h,7Ah,00h,83h,0C4h,04h,89h,44h,24h
+    db 10h,85h,0C0h,0C6h,44h,24h,1Ch,02h,74h,0Ah,56h,8Bh,0C8h,0E8h,01h,1Dh
+    db 0F6h,0FFh,0EBh,02h,33h,0C0h,89h,45h,00h,46h,83h,0C5h,04h,83h,0FEh,20h
+    db 0C6h,44h,24h,1Ch,01h,7Ch,0C9h,8Bh,0CFh,0E8h,32h,12h,0F5h,0FFh,8Bh,4Ch
+    db 24h,14h,8Bh,0C7h,5Fh,5Eh,5Dh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h
+    db 14h,0C3h
+??0PlayerList@@QAE@XZ ENDP
+_TEXT ENDS
+END
