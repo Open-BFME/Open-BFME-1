d90d526d32338c6cf5e273fb85bd8faf87c7bf25
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:00:09 +0000

    Recover SpecialPowerTemplate destructor retail body

---
+++ Code/masm_dumps/dtor_SpecialPowerTemplate_MAE_XZ_000EB5D0_9e3fc815.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1SpecialPowerTemplate@@MAE@XZ
+; Exact 146 retail bytes @ 0x000EB5D0; certified packet 148
+_TEXT SEGMENT
+public ??1SpecialPowerTemplate@@MAE@XZ
+??1SpecialPowerTemplate@@MAE@XZ PROC
+    db 06Ah,0FFh,068h,03Ch,0B5h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,08Bh,0F1h,089h,074h,024h,004h,0C7h,006h,0C0h
+    db 05Ch,008h,001h,08Dh,08Eh,018h,001h,000h,000h,0C7h,044h,024h,010h,003h,000h,000h,000h
+    db 0E8h,03Ah,0C3h,079h,000h,08Dh,08Eh,094h,000h,000h,000h,0C6h,044h,024h,010h,002h
+    db 0E8h,01Fh,0B9h,0F3h,0FFh,08Dh,04Eh,024h,0C6h,044h,024h,010h,001h,0E8h,012h,0B9h
+    db 0F3h,0FFh,08Dh,04Eh,00Ch,0C6h,044h,024h,010h,000h,0E8h,010h,0C3h,079h,000h,08Bh
+    db 04Eh,004h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,0C7h,006h,0B0h,0FCh
+    db 007h,001h,074h,006h,08Bh,001h,06Ah,001h,0FFh,010h,08Bh,04Ch,024h,008h,0C7h,046h
+    db 004h,000h,000h,000h,000h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h
+    db 0C3h
+??1SpecialPowerTemplate@@MAE@XZ ENDP
+_TEXT ENDS
+END
