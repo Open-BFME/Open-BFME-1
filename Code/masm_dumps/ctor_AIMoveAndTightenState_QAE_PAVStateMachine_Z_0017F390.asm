3cf7353e65438922c16460a01e2b4ed3653b3fa8
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:42:22 +0000

    Recover AIMoveAndTightenState constructor

---
+++ Code/masm_dumps/ctor_AIMoveAndTightenState_QAE_PAVStateMachine_Z_0017F390.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??0AIMoveAndTightenState@@QAE@PAVStateMachine@@@Z
+; Exact 54B retail body at RVA 0x0017F390.
+_TEXT SEGMENT
+public ??0AIMoveAndTightenState@@QAE@PAVStateMachine@@@Z
+??0AIMoveAndTightenState@@QAE@PAVStateMachine@@@Z PROC
+    db 51h,56h,51h,8Bh,0F1h,89h,64h,24h,08h,8Bh,0CCh,68h,48h,0A3h,09h,01h
+    db 0E8h,1Bh,98h,70h,00h,8Bh,44h,24h,10h,50h,8Bh,0CEh,0E8h,0D1h,2Dh,0EBh
+    db 0FFh,33h,0C0h,88h,46h,54h,89h,46h,50h,0C7h,06h,0F0h,0A2h,09h,01h
+    db 8Bh,0C6h,5Eh,59h,0C2h,04h,00h
+??0AIMoveAndTightenState@@QAE@PAVStateMachine@@@Z ENDP
+_TEXT ENDS
+END
