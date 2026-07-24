7a48e8486b73b7e80b5b01ba46ae3811af387fa3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 11:20:11 +0000

    Recover InGameUI beginAreaSelectHint

---
+++ Code/masm_dumps/beginAreaSelectHint_InGameUI_0043AC30_packet318.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?beginAreaSelectHint@InGameUI@@UAEXPBVGameMessage@@@Z
+; Retail @ 0x0043AC30 size 51
+_TEXT SEGMENT
+public ?beginAreaSelectHint@InGameUI@@UAEXPBVGameMessage@@@Z
+?beginAreaSelectHint@InGameUI@@UAEXPBVGameMessage@@@Z PROC
+    db 056h,08Bh,0F1h,08Bh,04Ch,024h,08h,085h,0C9h,0C6h,046h,020h,01h,074h,020h,06Ah
+    db 00h,0E8h,09Dh,0AEh,0C0h,0FFh,08Bh,08h,083h,0C6h,024h,089h,0Eh,08Bh,050h,04h
+    db 089h,056h,04h,08Bh,048h,08h,089h,04Eh,08h,08Bh,050h,0Ch,089h,056h,0Ch,05Eh
+    db 0C2h,04h,00h
+?beginAreaSelectHint@InGameUI@@UAEXPBVGameMessage@@@Z ENDP
+_TEXT ENDS
+END
