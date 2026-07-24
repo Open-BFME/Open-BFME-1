e3a312eb2710d00248845b9b0f6ce34e1c871c2f
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:08:52 +0000

    Recover TerrainLogic alignOnTerrain retail body

---
+++ Code/masm_dumps/alignOnTerrain_TerrainLogic_UAE_AW4PathfindLayerEnum_MABUCoord3D_NAAVMatrix3D_Z_001A8BD0_ae4e706b.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?alignOnTerrain@TerrainLogic@@UAE?AW4PathfindLayerEnum@@MABUCoord3D@@_NAAVMatrix3D@@@Z
+; Exact 117 retail bytes @ 0x001A8BD0; certified packet 196
+_TEXT SEGMENT
+public ?alignOnTerrain@TerrainLogic@@UAE?AW4PathfindLayerEnum@@MABUCoord3D@@_NAAVMatrix3D@@@Z
+?alignOnTerrain@TerrainLogic@@UAE?AW4PathfindLayerEnum@@MABUCoord3D@@_NAAVMatrix3D@@@Z PROC
+    db 083h,0ECh,00Ch,053h,056h,057h,08Bh,07Ch,024h,020h,057h,06Ah,000h,08Bh,0F1h,0E8h
+    db 091h,03Ah,0E7h,0FFh,08Bh,057h,004h,06Ah,001h,08Dh,04Ch,024h,010h,051h,08Bh,00Fh
+    db 08Bh,0D8h,08Bh,006h,053h,052h,051h,08Bh,0CEh,0FFh,050h,01Ch,0D9h,05Ch,024h,020h
+    db 083h,0FBh,001h,07Eh,00Eh,0D9h,044h,024h,020h,0D8h,005h,0D4h,08Ah,009h,001h,0D9h,05Ch
+    db 024h,020h,08Bh,074h,024h,028h,08Bh,044h,024h,01Ch,056h,08Dh,054h,024h,010h,052h
+    db 057h,050h,0E8h,0C9h,08Dh,0E5h,0FFh,08Ah,044h,024h,034h,083h,0C4h,010h,084h,0C0h
+    db 08Bh,0C3h,074h,007h,08Bh,04Ch,024h,020h,089h,04Eh,02Ch,05Fh,05Eh,05Bh,083h,0C4h
+    db 00Ch,0C2h,010h,000h
+?alignOnTerrain@TerrainLogic@@UAE?AW4PathfindLayerEnum@@MABUCoord3D@@_NAAVMatrix3D@@@Z ENDP
+_TEXT ENDS
+END
