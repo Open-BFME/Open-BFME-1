95d83d1868deb94c2b49ff1f0847d676691bf940
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:01:33 +0000

    Recover DisplayString getText retail body

---
+++ Code/masm_dumps/DisplayString_getText_410080.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+
+; ?getText@DisplayString@@UAE?AVUnicodeString@@XZ
+; Retail @ 0x00410080 size 32
+_TEXT SEGMENT
+public ?getText@DisplayString@@UAE?AVUnicodeString@@XZ
+?getText@DisplayString@@UAE?AVUnicodeString@@XZ PROC
+    db 51h, 56h, 8Bh, 74h, 24h, 0Ch, 83h, 0C1h, 04h, 51h, 8Bh, 0CEh, 0C7h, 44h, 24h, 08h
+    db 00h, 00h, 00h, 00h, 0E8h, 67h, 83h, 47h, 00h, 8Bh, 0C6h, 5Eh, 59h, 0C2h, 04h, 00h
+?getText@DisplayString@@UAE?AVUnicodeString@@XZ ENDP
+_TEXT ENDS
+END
