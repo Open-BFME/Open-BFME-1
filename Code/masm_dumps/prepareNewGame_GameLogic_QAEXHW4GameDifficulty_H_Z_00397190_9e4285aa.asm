40f398ae0c277c43ffae820908a2b3fbe0352e6d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:20:28 +0000

    Recover GameLogic prepareNewGame retail body

---
+++ Code/masm_dumps/prepareNewGame_GameLogic_QAEXHW4GameDifficulty_H_Z_00397190_9e4285aa.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?prepareNewGame@GameLogic@@QAEXHW4GameDifficulty@@H@Z
+; Exact 234 retail bytes @ 0x00397190; certified packet 219
+; SHA-256 9e4285aae608dfc21d9a51be758f7c3009ca5f1388ed72633e4fe430fda42019
+_TEXT SEGMENT
+public ?prepareNewGame@GameLogic@@QAEXHW4GameDifficulty@@H@Z
+?prepareNewGame@GameLogic@@QAEXHW4GameDifficulty@@H@Z PROC
+    db 56h,8Bh,0F1h,8Bh,0Dh,6Ch,07h,2Fh,01h,6Ah,01h,0E8h,1Bh,0B5h,0C7h,0FFh
+    db 8Bh,44h,24h,0Ch,8Bh,0Dh,98h,08h,2Fh,01h,89h,81h,9Ch,00h,00h,00h
+    db 8Bh,86h,0A4h,00h,00h,00h,85h,0C0h,75h,36h,51h,89h,64h,24h,10h,8Bh
+    db 0CCh,68h,60h,5Dh,07h,01h,0E8h,0F5h,19h,4Fh,00h,8Bh,0Dh,40h,1Bh,2Fh
+    db 01h,8Bh,11h,0FFh,52h,6Ch,89h,86h,0A4h,00h,00h,00h,8Bh,10h,6Ah,00h
+    db 8Bh,0C8h,0FFh,52h,10h,8Bh,8Eh,0A4h,00h,00h,00h,8Bh,01h,0FFh,50h,14h
+    db 8Bh,8Eh,0A4h,00h,00h,00h,8Bh,49h,08h,68h,80h,00h,00h,00h,0E8h,21h
+    db 09h,0C9h,0FFh,8Bh,54h,24h,08h,0A1h,98h,08h,2Fh,01h,89h,90h,0Ch,01h
+    db 00h,00h,8Bh,15h,0C8h,0D5h,2Eh,01h,8Bh,8Ah,84h,0Bh,00h,00h,85h,0C9h
+    db 8Dh,82h,84h,0Bh,00h,00h,74h,21h,66h,83h,79h,04h,00h,74h,1Ah,50h
+    db 8Dh,4Ah,08h,0E8h,58h,0Ah,4Fh,00h,8Bh,0Dh,0C8h,0D5h,2Eh,01h,81h,0C1h
+    db 84h,0Bh,00h,00h,0E8h,0F7h,06h,4Fh,00h,8Bh,4Ch,24h,10h,89h,8Eh,8Ch
+    db 00h,00h,00h,8Bh,15h,98h,08h,2Fh,01h,83h,0BAh,0Ch,01h,00h,00h,04h
+    db 74h,0Dh,8Bh,0Dh,58h,4Bh,2Fh,01h,6Ah,01h,0E8h,7Eh,0B6h,0CAh,0FFh,0C6h
+    db 86h,0A0h,00h,00h,00h,00h,5Eh,0C2h,0Ch,00h
+?prepareNewGame@GameLogic@@QAEXHW4GameDifficulty@@H@Z ENDP
+_TEXT ENDS
+END
