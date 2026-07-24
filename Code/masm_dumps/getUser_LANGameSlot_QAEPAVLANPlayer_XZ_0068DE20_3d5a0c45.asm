1457f277639e2c68a40dba920b34d41384238bd7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:29:29 +0000

    Recover LANGameSlot getUser

---
+++ Code/masm_dumps/getUser_LANGameSlot_QAEPAVLANPlayer_XZ_0068DE20_3d5a0c45.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?getUser@LANGameSlot@@QAEPAVLANPlayer@@XZ
+; Retail @ 0x0068DE20 size 145
+_TEXT SEGMENT
+public ?getUser@LANGameSlot@@QAEPAVLANPlayer@@XZ
+?getUser@LANGameSlot@@QAEPAVLANPlayer@@XZ PROC
+    db 6Ah,0FFh,68h,78h,6Ch,04h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh,0F1h,0E8h,8Dh,9Bh,99h,0FFh,84h,0C0h
+    db 74h,5Dh,8Bh,46h,30h,8Bh,4Eh,34h,57h,89h,46h,58h,8Bh,46h,64h,8Dh
+    db 54h,24h,08h,89h,4Eh,5Ch,52h,8Bh,0CEh,89h,46h,50h,0E8h,0AAh,0C3h,9Ah
+    db 0FFh,8Dh,7Eh,44h,50h,8Bh,0CFh,0C7h,44h,24h,18h,00h,00h,00h,00h,0E8h
+    db 0BCh,0A6h,1Fh,00h,8Dh,4Ch,24h,08h,0C7h,44h,24h,14h,0FFh,0FFh,0FFh,0FFh
+    db 0E8h,4Bh,0A3h,1Fh,00h,8Bh,0C7h,5Fh,0C7h,46h,54h,00h,00h,00h,00h,5Eh
+    db 8Bh,4Ch,24h,04h,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h,0C3h,8Bh
+    db 4Ch,24h,08h,33h,0C0h,5Eh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h
+    db 0C3h
+?getUser@LANGameSlot@@QAEPAVLANPlayer@@XZ ENDP
+_TEXT ENDS
+END
