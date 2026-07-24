6b24e24d7b0713104f818e1e0c757702803e0626
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:28:36 +0000

    Recover ScriptList xfer retail body

---
+++ Code/masm_dumps/xfer_ScriptList_MAEXPAVXfer_Z_003556A0_76e94c51.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+
+; ?xfer@ScriptList@@MAEXPAVXfer@@@Z
+; Exact 400 retail bytes @ 0x003556A0; certified packet 106
+_TEXT SEGMENT
+public ?xfer@ScriptList@@MAEXPAVXfer@@@Z
+?xfer@ScriptList@@MAEXPAVXfer@@@Z PROC
+    db 83h,0ECh,08h,53h,55h,56h,8Bh,74h,24h,18h,8Bh,06h,57h,8Bh,0F9h,8Dh
+    db 4Ch,24h,10h,51h,8Bh,0CEh,0C6h,44h,24h,14h,01h,0C6h,44h,24h,15h,02h
+    db 0FFh,50h,28h,8Bh,57h,30h,8Bh,6Fh,2Ch,8Bh,06h,2Bh,0D5h,8Dh,4Ch,24h
+    db 1Ch,0C1h,0FAh,02h,51h,8Bh,0CEh,89h,54h,24h,20h,0FFh,50h,7Ch,8Bh,6Fh
+    db 48h,33h,0DBh,83h,0FDh,0FFh,74h,3Eh,0EBh,06h,8Dh,9Bh,00h,00h,00h,00h
+    db 8Bh,47h,38h,8Bh,16h,8Dh,6Ch,0ADh,00h,0C1h,0E5h,02h,8Bh,4Ch,28h,10h
+    db 83h,0C1h,04h,51h,8Bh,0CEh,0FFh,52h,30h,8Bh,44h,24h,1Ch,05h,0FFh,0FFh
+    db 00h,00h,66h,3Bh,0C3h,89h,44h,24h,1Ch,74h,0Bh,8Bh,57h,38h,8Bh,2Ch
+    db 2Ah,83h,0FDh,0FFh,75h,0CAh,66h,39h,5Ch,24h,1Ch,0Fh,86h,8Fh,00h,00h
+    db 00h,39h,1Dh,8Ch,07h,2Fh,01h,75h,5Fh,6Ah,40h,0E8h,0F0h,0C7h,52h,00h
+    db 83h,0C4h,04h,3Bh,0C3h,74h,4Ah,0C7h,00h,8Ch,85h,0Eh,01h,89h,58h,04h
+    db 89h,58h,08h,89h,58h,0Ch,89h,58h,10h,0C6h,40h,14h,01h,0C6h,40h,15h
+    db 01h,0C6h,40h,16h,01h,88h,58h,17h,0C6h,40h,18h,01h,0C6h,40h,19h,01h
+    db 0C6h,40h,1Ah,01h,89h,58h,1Ch,89h,58h,20h,89h,58h,24h,89h,58h,28h
+    db 88h,58h,2Ch,89h,58h,30h,89h,58h,34h,89h,58h,38h,89h,58h,3Ch,0EBh
+    db 02h,33h,0C0h,0A3h,8Ch,07h,2Fh,01h,66h,39h,5Ch,24h,1Ch,74h,21h,90h
+    db 8Bh,0Dh,8Ch,07h,2Fh,01h,8Bh,06h,51h,8Bh,0CEh,0FFh,50h,30h,8Bh,44h
+    db 24h,1Ch,05h,0FFh,0FFh,00h,00h,66h,3Bh,0C3h,89h,44h,24h,1Ch,75h,0E0h
+    db 80h,7Ch,24h,11h,02h,72h,5Fh,8Bh,47h,0Ch,8Bh,57h,10h,2Bh,0D0h,8Bh
+    db 06h,8Dh,4Ch,24h,14h,0C1h,0FAh,02h,51h,8Bh,0CEh,89h,54h,24h,18h,0FFh
+    db 50h,7Ch,8Bh,6Fh,28h,83h,0FDh,0FFh,74h,3Ch,8Dh,9Bh,00h,00h,00h,00h
+    db 8Bh,47h,18h,8Bh,16h,8Dh,6Ch,0ADh,00h,0C1h,0E5h,02h,8Bh,4Ch,28h,10h
+    db 83h,0C1h,04h,51h,8Bh,0CEh,0FFh,52h,30h,8Bh,44h,24h,14h,05h,0FFh,0FFh
+    db 00h,00h,66h,3Bh,0C3h,89h,44h,24h,14h,74h,0Bh,8Bh,57h,18h,8Bh,2Ch
+    db 2Ah,83h,0FDh,0FFh,75h,0CAh,5Fh,5Eh,5Dh,5Bh,83h,0C4h,08h,0C2h,04h,00h
+?xfer@ScriptList@@MAEXPAVXfer@@@Z ENDP
+_TEXT ENDS
+END
