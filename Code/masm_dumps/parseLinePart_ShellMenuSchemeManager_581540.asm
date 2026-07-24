1da9111dad43b883f346cc4a4ec5b72735ed27c8
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:53:12 +0000

    Recover ShellMenuScheme parseLinePart

---
+++ Code/masm_dumps/parseLinePart_ShellMenuSchemeManager_581540.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+; ?parseLinePart@ShellMenuSchemeManager@@SAXPAVINI@@PAX1PBX@Z
+; Exact 124B @ 0x00581540; certified LinePart parser-table callback
+_TEXT SEGMENT
+public ?parseLinePart@ShellMenuSchemeManager@@SAXPAVINI@@PAX1PBX@Z
+?parseLinePart@ShellMenuSchemeManager@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 56h, 57h, 6Ah, 18h, 0E8h, 0E7h, 09h, 30h, 00h, 83h, 0C4h, 04h, 85h, 0C0h, 74h, 2Dh
+    db 0C7h, 40h, 04h, 00h, 00h, 00h, 00h, 0C7h, 00h, 00h, 00h, 00h, 00h, 0C7h, 40h, 0Ch
+    db 00h, 00h, 00h, 00h, 0C7h, 40h, 08h, 00h, 00h, 00h, 00h, 0C7h, 40h, 14h, 0FFh, 0FFh
+    db 0FFh, 00h, 0C7h, 40h, 10h, 01h, 00h, 00h, 00h, 8Bh, 0F8h, 0EBh, 02h, 33h, 0FFh, 8Bh
+    db 4Ch, 24h, 0Ch, 68h, 0A0h, 0B6h, 10h, 01h, 57h, 0E8h, 12h, 0Bh, 2Dh, 00h, 85h, 0FFh
+    db 74h, 27h, 8Bh, 44h, 24h, 10h, 8Bh, 70h, 08h, 6Ah, 0Ch, 0E8h, 0A0h, 0CFh, 2Ah, 00h
+    db 8Dh, 48h, 08h, 83h, 0C4h, 04h, 85h, 0C9h, 74h, 02h, 89h, 39h, 8Bh, 4Eh, 04h, 89h
+    db 30h, 89h, 48h, 04h, 89h, 01h, 89h, 46h, 04h, 5Fh, 5Eh, 0C3h
+?parseLinePart@ShellMenuSchemeManager@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
