085691edb94b57f449733756b33c4e687f858c47
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 07:58:40 +0000

    Recover DisconnectManager applyDisconnectVote retail body

---
+++ Code/masm_dumps/DisconnectManager_applyDisconnectVote_66B910.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?applyDisconnectVote@DisconnectManager@@IAEXHIHPAVConnectionManager@@@Z
+; Exact 82 retail bytes @ RVA 0x0066B910
+_TEXT SEGMENT
+public ?applyDisconnectVote@DisconnectManager@@IAEXHIHPAVConnectionManager@@@Z
+?applyDisconnectVote@DisconnectManager@@IAEXHIHPAVConnectionManager@@@Z PROC
+    db 08Bh,044h,024h,00Ch,08Bh,054h,024h,008h,053h,056h,08Bh,074h,024h,00Ch,057h,08Bh
+    db 07Ch,024h,01Ch,08Dh,004h,0F0h,057h,056h,0C6h,044h,0C1h,030h,001h,089h,054h,0C1h
+    db 034h,0E8h,01Bh,07Eh,099h,0FFh,08Bh,0CFh,08Bh,0D8h,0E8h,052h,0E9h,09Dh,0FFh,03Bh
+    db 0F0h,07Ch,003h,074h,017h,04Eh,083h,0FEh,0FFh,074h,011h,08Bh,00Dh,064h,049h,02Fh
+    db 001h,085h,0C9h,074h,007h,053h,056h,0E8h,05Ch,064h,09Ch,0FFh,05Fh,05Eh,05Bh,0C2h
+    db 010h,000h
+?applyDisconnectVote@DisconnectManager@@IAEXHIHPAVConnectionManager@@@Z ENDP
+_TEXT ENDS
+END
