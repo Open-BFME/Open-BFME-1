cdceb3cf47cfeddaac0c0e6b17f6440639014e1c
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:12:08 +0000

    Recover Player battle plan bonus application

---
+++ Code/masm_dumps/Player_applyBattlePlanBonusesForPlayerObjects_000D5D20_6f1c7190.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ?applyBattlePlanBonusesForPlayerObjects@Player@@QAEXPBVBattlePlanBonuses@@@Z
+; Exact 289 retail bytes @ 0x000D5D20; certified packet 333
+_TEXT SEGMENT
+public ?applyBattlePlanBonusesForPlayerObjects@Player@@QAEXPBVBattlePlanBonuses@@@Z
+?applyBattlePlanBonusesForPlayerObjects@Player@@QAEXPBVBattlePlanBonuses@@@Z PROC
+    db 053h,08Bh,0D9h,08Bh,04Bh,070h,085h,0C9h,056h,057h,00Fh,085h,082h,000h,000h,000h
+    db 06Ah,044h,0E8h,0F9h,0C1h,07Ah,000h,083h,0C4h,004h,085h,0C0h,074h,04Fh,033h,0C9h
+    db 08Dh,050h,014h,089h,00Ah,089h,04Ah,004h,089h,04Ah,008h,089h,04Ah,00Ch,089h,04Ah
+    db 010h,089h,04Ah,014h,08Dh,050h,02Ch,089h,00Ah,089h,04Ah,004h,089h,04Ah,008h,089h
+    db 04Ah,00Ch,089h,04Ah,010h,08Bh,0F8h,08Bh,044h,024h,010h,089h,04Ah,014h,08Bh,0F0h
+    db 089h,07Bh,070h,0B9h,011h,000h,000h,000h,050h,0F3h,0A5h,068h,040h,05Ah,04Dh,000h
+    db 08Bh,0CBh,0E8h,044h,094h,0F5h,0FFh,05Fh,05Eh,05Bh,0C2h,004h,000h,08Bh,044h,024h
+    db 010h,033h,0FFh,08Bh,0F0h,089h,07Bh,070h,0B9h,011h,000h,000h,000h,050h,0F3h,0A5h
+    db 068h,040h,05Ah,04Dh,000h,08Bh,0CBh,0E8h,01Fh,094h,0F5h,0FFh,05Fh,05Eh,05Bh,0C2h
+    db 004h,000h,0D9h,001h,08Bh,044h,024h,010h,0D8h,008h,050h,068h,040h,05Ah,04Dh,000h
+    db 0D9h,019h,08Bh,04Bh,070h,0D9h,040h,010h,0D8h,049h,010h,0D9h,059h,010h,08Bh,04Bh
+    db 070h,08Bh,071h,004h,08Bh,050h,004h,003h,0F2h,089h,071h,004h,08Bh,073h,070h,08Bh
+    db 07Eh,004h,0B9h,000h,000h,000h,000h,00Fh,098h,0C1h,049h,023h,0CFh,089h,04Eh,004h
+    db 08Bh,04Bh,070h,08Bh,071h,00Ch,08Bh,050h,00Ch,003h,0F2h,089h,071h,00Ch,08Bh,073h
+    db 070h,08Bh,07Eh,00Ch,0B9h,000h,000h,000h,000h,00Fh,098h,0C1h,049h,023h,0CFh,089h
+    db 04Eh,00Ch,08Bh,04Bh,070h,08Bh,071h,008h,08Bh,050h,008h,003h,0F2h,089h,071h,008h
+    db 08Bh,073h,070h,08Bh,07Eh,008h,0B9h,000h,000h,000h,000h,00Fh,098h,0C1h,049h,023h
+    db 0CFh,089h,04Eh,008h,08Bh,0CBh,0E8h,090h,093h,0F5h,0FFh,05Fh,05Eh,05Bh,0C2h,004h
+    db 000h
+?applyBattlePlanBonusesForPlayerObjects@Player@@QAEXPBVBattlePlanBonuses@@@Z ENDP
+_TEXT ENDS
+END
