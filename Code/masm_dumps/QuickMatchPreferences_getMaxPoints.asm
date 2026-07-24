094857063d09ecac37cb14b69728eb1b5266aedf
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:20:44 +0000

    Recover QuickMatchPreferences getMaxPoints

---
+++ Code/masm_dumps/QuickMatchPreferences_getMaxPoints.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getMaxPoints@QuickMatchPreferences@@QAEHXZ
+; Exact 87 retail bytes @ 0x000ABF80
+_TEXT SEGMENT
+public ?getMaxPoints@QuickMatchPreferences@@QAEHXZ
+?getMaxPoints@QuickMatchPreferences@@QAEHXZ PROC
+    db 51h,56h,57h,8Bh,0F1h,68h,98h,11h,08h,01h,8Dh,4Ch,24h,0Ch,0E8h,2Dh
+    db 0CCh,7Dh,00h,8Dh,44h,24h,08h,83h,0C6h,04h,50h,8Bh,0CEh,0E8h,0Ah,0EFh
+    db 0F5h,0FFh,8Dh,4Ch,24h,08h,8Bh,0F8h,0E8h,93h,0B9h,7Dh,00h,3Bh,3Eh,75h
+    db 09h,5Fh,0B8h,0E8h,03h,00h,00h,5Eh,59h,0C3h,8Bh,7Fh,14h,85h,0FFh,8Dh
+    db 47h,08h,75h,05h,0B8h,8Bh,38h,07h,01h,50h,0FFh,15h,84h,93h,35h,01h
+    db 83h,0C4h,04h,5Fh,5Eh,59h,0C3h
+?getMaxPoints@QuickMatchPreferences@@QAEHXZ ENDP
+_TEXT ENDS
+END
