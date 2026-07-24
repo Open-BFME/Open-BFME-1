7410b14613dbe4c023730df7c5d36bd0b54886f3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:42:16 +0000

    Recover BuildListInfo duplicate MASM

---
+++ Code/masm_dumps/duplicate_BuildListInfo_QAEPAV1_XZ_001944D0_6d7e0f9b.asm
@@ -0,0 +1,24 @@
+.386
+.model flat
+
+; ?duplicate@BuildListInfo@@QAEPAV1@XZ
+; Exact 194 retail bytes @ 0x001944D0; certified packet 163
+_TEXT SEGMENT
+public ?duplicate@BuildListInfo@@QAEPAV1@XZ
+?duplicate@BuildListInfo@@QAEPAV1@XZ PROC
+    db 6Ah,0FFh,68h,0A6h,6Fh,00h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,53h,55h,56h,57h,68h,8Ch,00h,00h,00h,8Bh
+    db 0F1h,0E8h,3Ah,0DAh,6Eh,00h,83h,0C4h,04h,89h,44h,24h,10h,33h,0FFh,3Bh
+    db 0C7h,89h,7Ch,24h,1Ch,74h,0Bh,8Bh,0C8h,0E8h,74h,84h,0E7h,0FFh,8Bh,0E8h
+    db 0EBh,02h,33h,0EDh,56h,8Bh,0CDh,0C7h,44h,24h,20h,0FFh,0FFh,0FFh,0FFh,0E8h
+    db 0A8h,8Fh,0E7h,0FFh,89h,7Dh,2Ch,8Bh,7Eh,2Ch,85h,0FFh,8Bh,0DDh,74h,4Dh
+    db 68h,8Ch,00h,00h,00h,0E8h,0F6h,0D9h,6Eh,00h,83h,0C4h,04h,89h,44h,24h
+    db 10h,85h,0C0h,0C7h,44h,24h,1Ch,01h,00h,00h,00h,74h,0Bh,8Bh,0C8h,0E8h
+    db 2Eh,84h,0E7h,0FFh,8Bh,0F0h,0EBh,02h,33h,0F6h,57h,8Bh,0CEh,0C7h,44h,24h
+    db 20h,0FFh,0FFh,0FFh,0FFh,0E8h,62h,8Fh,0E7h,0FFh,0C7h,46h,2Ch,00h,00h,00h
+    db 00h,89h,73h,2Ch,8Bh,7Fh,2Ch,85h,0FFh,8Bh,0DEh,75h,0B3h,8Bh,4Ch,24h
+    db 14h,5Fh,5Eh,8Bh,0C5h,5Dh,5Bh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h
+    db 10h,0C3h
+?duplicate@BuildListInfo@@QAEPAV1@XZ ENDP
+_TEXT ENDS
+END
