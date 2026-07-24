c539f4ece8dcb039019a629f1d03d1fbfe83274b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 07:43:16 +0000

    Recover WorldHeightMap stream constructor

---
+++ Code/masm_dumps/WorldHeightMap_stream_ctor_0074EB10.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+; ??0WorldHeightMap@@QAE@PAVChunkInputStream@@_N@Z
+; Exact 352B @ 0x0074EB10; certified packet 243
+_TEXT SEGMENT
+public ??0WorldHeightMap@@QAE@PAVChunkInputStream@@_N@Z
+??0WorldHeightMap@@QAE@PAVChunkInputStream@@_N@Z PROC
+    db 6Ah, 0FFh, 68h, 40h, 0E4h, 04h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 2Ch, 56h, 8Bh, 0F1h, 57h, 89h, 74h, 24h, 08h
+    db 0C7h, 46h, 04h, 01h, 00h, 00h, 00h, 33h, 0FFh, 0C7h, 06h, 94h, 1Bh, 12h, 01h, 89h
+    db 7Eh, 14h, 89h, 7Eh, 18h, 89h, 7Ch, 24h, 3Ch, 89h, 7Eh, 1Ch, 89h, 7Eh, 28h, 89h
+    db 7Eh, 2Ch, 89h, 7Eh, 30h, 89h, 7Eh, 38h, 89h, 7Eh, 3Ch, 89h, 7Eh, 40h, 89h, 7Eh
+    db 44h, 89h, 7Eh, 48h, 89h, 7Eh, 4Ch, 89h, 7Eh, 50h, 89h, 7Eh, 54h, 89h, 7Eh, 58h
+    db 89h, 7Eh, 5Ch, 89h, 7Eh, 60h, 89h, 7Eh, 64h, 89h, 7Eh, 68h, 89h, 7Eh, 6Ch, 89h
+    db 7Eh, 70h, 89h, 7Eh, 74h, 89h, 7Eh, 78h, 89h, 7Eh, 7Ch, 89h, 0BEh, 80h, 00h, 00h
+    db 00h, 89h, 0BEh, 84h, 00h, 00h, 00h, 89h, 0BEh, 88h, 00h, 00h, 00h, 89h, 0BEh, 0A4h
+    db 80h, 00h, 00h, 89h, 0BEh, 0A8h, 80h, 00h, 00h, 89h, 0BEh, 0ACh, 80h, 00h, 00h, 89h
+    db 0BEh, 0B0h, 80h, 00h, 00h, 89h, 0BEh, 0B4h, 80h, 00h, 00h, 89h, 0BEh, 0B8h, 80h, 00h
+    db 00h, 68h, 32h, 1Bh, 44h, 00h, 68h, 55h, 7Ch, 42h, 00h, 68h, 00h, 02h, 00h, 00h
+    db 6Ah, 28h, 8Dh, 86h, 0C0h, 80h, 00h, 00h, 50h, 0C6h, 44h, 24h, 50h, 0Bh, 0E8h, 01h
+    db 83h, 2Ah, 00h, 68h, 32h, 1Bh, 44h, 00h, 68h, 55h, 7Ch, 42h, 00h, 68h, 00h, 02h
+    db 00h, 00h, 6Ah, 28h, 8Dh, 8Eh, 0C4h, 0D0h, 00h, 00h, 51h, 0C6h, 44h, 24h, 50h, 0Ch
+    db 0E8h, 0DFh, 82h, 2Ah, 00h, 89h, 0BEh, 0C4h, 20h, 01h, 00h, 89h, 0BEh, 0CCh, 20h, 01h
+    db 00h, 89h, 0BEh, 0D4h, 20h, 01h, 00h, 89h, 0BEh, 0DCh, 20h, 01h, 00h, 0C6h, 44h, 24h
+    db 3Ch, 11h, 8Bh, 0CEh, 0E8h, 4Ah, 0F3h, 8Eh, 0FFh, 8Bh, 54h, 24h, 44h, 52h, 8Dh, 4Ch
+    db 24h, 10h, 0E8h, 2Ah, 1Ch, 8Ch, 0FFh, 8Bh, 44h, 24h, 48h, 50h, 8Dh, 4Ch, 24h, 10h
+    db 51h, 8Bh, 0CEh, 0C6h, 44h, 24h, 44h, 12h, 0E8h, 21h, 9Eh, 8Eh, 0FFh, 8Dh, 4Ch, 24h
+    db 0Ch, 0C6h, 44h, 24h, 3Ch, 11h, 0E8h, 5Eh, 72h, 8Dh, 0FFh, 8Bh, 4Ch, 24h, 34h, 5Fh
+    db 8Bh, 0C6h, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 38h, 0C2h, 08h, 00h
+??0WorldHeightMap@@QAE@PAVChunkInputStream@@_N@Z ENDP
+_TEXT ENDS
+END
