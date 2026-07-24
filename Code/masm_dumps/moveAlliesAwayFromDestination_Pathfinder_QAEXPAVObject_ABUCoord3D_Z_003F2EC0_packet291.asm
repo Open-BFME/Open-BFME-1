23b1f62bda7c6d530e57bc96bec48db7e018ad77
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:00:49 +0000

    Recover Pathfinder moveAlliesAwayFromDestination

---
+++ Code/masm_dumps/moveAlliesAwayFromDestination_Pathfinder_QAEXPAVObject_ABUCoord3D_Z_003F2EC0_packet291.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?moveAlliesAwayFromDestination@Pathfinder@@QAEXPAVObject@@ABUCoord3D@@@Z
+; Exact 127 retail bytes @ 0x003F2EC0; certified packet 291
+; SHA-256 1992ff7bd47f6858497a79ebe942be761d02ad48fc4cb780f548412a53235528
+_TEXT SEGMENT
+public ?moveAlliesAwayFromDestination@Pathfinder@@QAEXPAVObject@@ABUCoord3D@@@Z
+?moveAlliesAwayFromDestination@Pathfinder@@QAEXPAVObject@@ABUCoord3D@@@Z PROC
+    db 083h,0ECh,018h,053h,055h,056h,08Bh,074h,024h,028h,057h,08Bh,0F9h,08Bh,0CEh,0E8h
+    db 0BDh,074h,0C4h,0FFh,08Bh,06Ch,024h,030h,08Bh,0D8h,083h,0FBh,001h,075h,00Fh,08Bh
+    db 00Dh,0CCh,0F4h,02Eh,001h,055h,056h,0E8h,089h,097h,0C2h,0FFh,08Bh,0D8h,08Bh,08Eh
+    db 004h,002h,000h,000h,089h,074h,024h,010h,0E8h,06Eh,074h,0C2h,0FFh,089h,044h,024h
+    db 014h,08Dh,044h,024h,020h,050h,083h,0C6h,038h,056h,08Bh,0CFh,0E8h,0D7h,042h,0C2h
+    db 0FFh,08Dh,04Ch,024h,018h,051h,055h,08Bh,0CFh,0E8h,0CAh,042h,0C2h,0FFh,08Dh,054h
+    db 024h,010h,052h,053h,08Dh,044h,024h,020h,050h,08Dh,04Ch,024h,02Ch,051h,08Bh,0CFh
+    db 0E8h,05Dh,011h,0C2h,0FFh,05Fh,05Eh,05Dh,05Bh,083h,0C4h,018h,0C2h,008h,000h
+?moveAlliesAwayFromDestination@Pathfinder@@QAEXPAVObject@@ABUCoord3D@@@Z ENDP
+_TEXT ENDS
+END
