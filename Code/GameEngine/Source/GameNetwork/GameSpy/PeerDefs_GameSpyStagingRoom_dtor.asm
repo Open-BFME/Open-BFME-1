0a2a36bd2161b631af06d16688cdc00b6725ac9d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:54:29 +0000

    Recover GameSpyStagingRoom destructor

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/PeerDefs_GameSpyStagingRoom_dtor.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1GameSpyStagingRoom@@QAE@XZ
+; Exact 151 retail bytes @ 0x004FA3D0.
+_TEXT SEGMENT
+public ??1GameSpyStagingRoom@@QAE@XZ
+??1GameSpyStagingRoom@@QAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 28h, 0D0h, 02h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 89h, 74h, 24h, 04h, 8Dh, 8Eh, 48h
+    db 04h, 00h, 00h, 0C7h, 44h, 24h, 10h, 04h, 00h, 00h, 00h, 0E8h, 40h, 0D5h, 38h, 00h
+    db 8Dh, 8Eh, 44h, 04h, 00h, 00h, 0C6h, 44h, 24h, 10h, 03h, 0E8h, 30h, 0D5h, 38h, 00h
+    db 8Dh, 8Eh, 24h, 04h, 00h, 00h, 0C6h, 44h, 24h, 10h, 02h, 0E8h, 20h, 0D5h, 38h, 00h
+    db 8Dh, 8Eh, 18h, 04h, 00h, 00h, 0C6h, 44h, 24h, 10h, 01h, 0E8h, 0A0h, 0DDh, 38h, 00h
+    db 68h, 92h, 7Dh, 40h, 00h, 6Ah, 08h, 6Ah, 78h, 8Dh, 46h, 58h, 50h, 0C6h, 44h, 24h
+    db 20h, 00h, 0E8h, 2Fh, 0C9h, 4Fh, 00h, 8Dh, 4Eh, 3Ch, 0C7h, 44h, 24h, 10h, 0FFh, 0FFh
+    db 0FFh, 0FFh, 0E8h, 0E9h, 0D4h, 38h, 00h, 8Bh, 4Ch, 24h, 08h, 5Eh, 64h, 89h, 0Dh, 00h
+    db 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1GameSpyStagingRoom@@QAE@XZ ENDP
+_TEXT ENDS
+END
