4c35b6804506676a4740854954d19241b11d1807
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:09:48 +0000

    Recover UnicodeString map operator[] at 0x690370

---
+++ Code/masm_dumps/map_UnicodeString_operator_index_690370.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ??A?$map@VUnicodeString@@V1@U?$less@VUnicodeString@@@_STL@@V?$allocator@U?$pair@$$CBVUnicodeString@@V1@@_STL@@@3@@_STL@@QAEAAVUnicodeString@@ABV2@@Z
+; Exact 231 retail bytes @ 0x00690370
+_TEXT SEGMENT
+public ??A?$map@VUnicodeString@@V1@U?$less@VUnicodeString@@@_STL@@V?$allocator@U?$pair@$$CBVUnicodeString@@V1@@_STL@@@3@@_STL@@QAEAAVUnicodeString@@ABV2@@Z
+??A?$map@VUnicodeString@@V1@U?$less@VUnicodeString@@@_STL@@V?$allocator@U?$pair@$$CBVUnicodeString@@V1@@_STL@@@3@@_STL@@QAEAAVUnicodeString@@ABV2@@Z PROC
+    db 064h,0A1h,000h,000h,000h,000h,06Ah,0FFh,068h,010h,06Fh,004h,001h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,00Ch,055h,08Bh,06Ch,024h,020h,056h,057h,055h
+    db 08Bh,0F1h,0E8h,08Ch,04Ch,098h,0FFh,08Bh,0F8h,03Bh,03Eh,074h,013h,08Dh,047h,010h
+    db 050h,08Bh,0CDh,0E8h,044h,023h,099h,0FFh,085h,0C0h,00Fh,08Dh,090h,000h,000h,000h
+    db 0C7h,044h,024h,028h,000h,000h,000h,000h,055h,08Dh,04Ch,024h,014h,0C7h,044h,024h
+    db 024h,000h,000h,000h,000h,0E8h,036h,080h,01Fh,000h,08Dh,04Ch,024h,028h,051h,08Dh
+    db 04Ch,024h,018h,0C6h,044h,024h,024h,001h,0E8h,023h,080h,01Fh,000h,08Dh,054h,024h
+    db 010h,052h,051h,08Bh,0C4h,089h,038h,08Dh,044h,024h,014h,050h,08Bh,0CEh,0C6h,044h
+    db 024h,02Ch,002h,0E8h,029h,0D8h,09Ah,0FFh,08Bh,074h,024h,00Ch,08Dh,04Ch,024h,014h
+    db 0C6h,044h,024h,020h,003h,0E8h,0C6h,07Dh,01Fh,000h,08Dh,04Ch,024h,010h,0C6h,044h
+    db 024h,020h,000h,0E8h,0B8h,07Dh,01Fh,000h,08Dh,04Ch,024h,028h,0C7h,044h,024h,020h
+    db 0FFh,0FFh,0FFh,0FFh,0E8h,0A7h,07Dh,01Fh,000h,08Dh,046h,014h,08Bh,04Ch,024h,018h
+    db 064h,089h,00Dh,000h,000h,000h,000h,05Fh,05Eh,05Dh,083h,0C4h,018h,0C2h,004h,000h
+    db 08Bh,04Ch,024h,018h,08Dh,047h,014h,05Fh,05Eh,064h,089h,00Dh,000h,000h,000h,000h
+    db 05Dh,083h,0C4h,018h,0C2h,004h,000h
+??A?$map@VUnicodeString@@V1@U?$less@VUnicodeString@@@_STL@@V?$allocator@U?$pair@$$CBVUnicodeString@@V1@@_STL@@@3@@_STL@@QAEAAVUnicodeString@@ABV2@@Z ENDP
+_TEXT ENDS
+END
