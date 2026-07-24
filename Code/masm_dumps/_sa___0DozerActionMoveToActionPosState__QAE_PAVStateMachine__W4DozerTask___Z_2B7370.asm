951dabf1bdc18cc9a52b0c898b2bd93edeed76d7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:20:10 +0000

    Recover Dozer move-to-action-position constructor

---
+++ Code/masm_dumps/_sa___0DozerActionMoveToActionPosState__QAE_PAVStateMachine__W4DozerTask___Z_2B7370.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+; ??0DozerActionMoveToActionPosState@@QAE@PAVStateMachine@@W4DozerTask@@@Z
+; Exact 53B @ 0x2B7370; reviewed standalone constructor body
+_TEXT SEGMENT
+public ??0DozerActionMoveToActionPosState@@QAE@PAVStateMachine@@W4DozerTask@@@Z
+??0DozerActionMoveToActionPosState@@QAE@PAVStateMachine@@W4DozerTask@@@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 0F4h, 6Dh, 0Ch, 01h
+    db 0E8h, 3Bh, 18h, 5Dh, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 21h, 0C2h, 0D4h
+    db 0FFh, 8Bh, 4Ch, 24h, 10h, 0C7h, 06h, 0A0h, 6Dh, 0Ch, 01h, 89h, 4Eh, 24h, 8Bh, 0C6h
+    db 5Eh, 59h, 0C2h, 08h, 00h
+??0DozerActionMoveToActionPosState@@QAE@PAVStateMachine@@W4DozerTask@@@Z ENDP
+_TEXT ENDS
+END
