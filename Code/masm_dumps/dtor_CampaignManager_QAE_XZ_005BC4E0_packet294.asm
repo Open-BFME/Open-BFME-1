5a08abc17c499d3ca18102197fea8751d6c5cd14
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:09:21 +0000

    Recover CampaignManager destructor packet 294

---
+++ Code/masm_dumps/dtor_CampaignManager_QAE_XZ_005BC4E0_packet294.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ??1CampaignManager@@QAE@XZ
+; Exact 146 retail bytes @ 0x005BC4E0; certified packet 294
+; SHA-256 b6b1a8ffab386afc7f4d5b4b8b0cfcaca796fa98cbeed12dc4acbd3a07489dd1
+_TEXT SEGMENT
+public ??1CampaignManager@@QAE@XZ
+??1CampaignManager@@QAE@XZ PROC
+    db 06Ah,0FFh,068h,0D3h,096h,003h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,08Bh,0D9h,056h,089h,05Ch,024h,008h,0C7h,003h
+    db 060h,0F6h,010h,001h,033h,0C0h,089h,043h,008h,089h,043h,00Ch,08Bh,043h,004h,08Bh
+    db 030h,03Bh,0F0h,0C7h,044h,024h,014h,001h,000h,000h,000h,074h,031h,055h,057h,090h
+    db 08Bh,03Eh,08Bh,046h,004h,08Bh,06Eh,008h,06Ah,00Ch,089h,038h,056h,089h,047h,004h
+    db 0E8h,0BBh,020h,027h,000h,083h,0C4h,008h,085h,0EDh,08Bh,0F7h,074h,009h,08Bh,045h
+    db 000h,06Ah,001h,08Bh,0CDh,0FFh,010h,03Bh,07Bh,004h,075h,0D4h,05Fh,05Dh,08Dh,04Bh
+    db 004h,0C6h,044h,024h,014h,000h,0E8h,02Bh,091h,0A5h,0FFh,08Bh,04Ch,024h,00Ch,0C7h
+    db 003h,044h,037h,007h,001h,05Eh,05Bh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h
+    db 010h,0C3h
+??1CampaignManager@@QAE@XZ ENDP
+_TEXT ENDS
+END
