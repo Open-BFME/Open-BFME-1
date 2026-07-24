62904be49d8f0a42d5a6f907868e135313f0781e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:56:00 +0000

    Match vector Video destructor retail body

---
+++ Code/masm_dumps/__1__vector_UVideo__V__allocator_UVideo____STL____STL__QAE_XZ_0081D040_2e3068dd.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ??1?$vector@UVideo@@V?$allocator@UVideo@@@_STL@@@_STL@@QAE@XZ
+; Retail @ 0x0081D040 size 165
+_TEXT SEGMENT
+public ??1?$vector@UVideo@@V?$allocator@UVideo@@@_STL@@@_STL@@QAE@XZ
+??1?$vector@UVideo@@V?$allocator@UVideo@@@_STL@@@_STL@@QAE@XZ PROC
+    db 06Ah,0FFh,068h,0F8h,046h,005h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,056h,057h,08Bh,0F9h,089h,07Ch,024h,00Ch,08Bh
+    db 05Fh,004h,08Bh,037h,03Bh,0F3h,0C7h,044h,024h,018h,000h,000h,000h,000h,074h,00Eh
+    db 08Bh,0CEh,0E8h,058h,097h,07Fh,0FFh,083h,0C6h,01Ch,03Bh,0F3h,075h,0F2h,08Bh,037h
+    db 085h,0F6h,0C7h,044h,024h,018h,0FFh,0FFh,0FFh,0FFh,074h,047h,08Bh,04Fh,008h,02Bh
+    db 0CEh,0B8h,093h,024h,049h,092h,0F7h,0E9h,003h,0D1h,0C1h,0FAh,004h,08Bh,0C2h,0C1h
+    db 0E8h,01Fh,003h,0C2h,06Bh,0C0h,01Ch,03Dh,080h,000h,000h,000h,076h,01Bh,056h,0E8h
+    db 0FCh,04Dh,006h,000h,083h,0C4h,004h,05Fh,05Eh,05Bh,08Bh,04Ch,024h,004h,064h,089h
+    db 00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h,050h,056h,0E8h,020h,015h,001h,000h
+    db 083h,0C4h,008h,08Bh,04Ch,024h,010h,05Fh,05Eh,05Bh,064h,089h,00Dh,000h,000h,000h
+    db 000h,083h,0C4h,010h,0C3h
+??1?$vector@UVideo@@V?$allocator@UVideo@@@_STL@@@_STL@@QAE@XZ ENDP
+_TEXT ENDS
+END
