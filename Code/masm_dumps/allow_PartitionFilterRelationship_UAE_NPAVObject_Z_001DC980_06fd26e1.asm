6796797954691efbf410b824e4a0f235709d32da
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:47:05 +0000

    Recover PartitionFilterRelationship allow

---
+++ Code/masm_dumps/allow_PartitionFilterRelationship_UAE_NPAVObject_Z_001DC980_06fd26e1.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?allow@PartitionFilterRelationship@@UAE_NPAVObject@@@Z
+; Exact 51 retail bytes @ 0x001DC980; certified packet 228
+; SHA-256 06fd26e1f0a209e6bc711c311092772ea044ca4fa1172f5ca780714fe10421ef
+_TEXT SEGMENT
+public ?allow@PartitionFilterRelationship@@UAE_NPAVObject@@@Z
+?allow@PartitionFilterRelationship@@UAE_NPAVObject@@@Z PROC
+    db 56h,8Bh,0F1h,8Ah,46h,10h,84h,0C0h,8Bh,4Eh,08h,75h,07h,8Bh,44h,24h
+    db 08h,50h,0EBh,05h,51h,8Bh,4Ch,24h,0Ch,0E8h,7Bh,0DDh,0E6h,0FFh,8Bh,0C8h
+    db 8Bh,46h,0Ch,0BAh,01h,00h,00h,00h,0D3h,0E2h,5Eh,85h,0D0h,0Fh,95h,0C0h
+    db 0C2h,04h,00h
+?allow@PartitionFilterRelationship@@UAE_NPAVObject@@@Z ENDP
+_TEXT ENDS
+END
