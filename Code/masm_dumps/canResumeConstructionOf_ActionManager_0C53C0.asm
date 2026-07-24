2fbc49367394b446f322b789ab2715821c96614a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:33:40 +0000

    Recover ActionManager canResumeConstructionOf

---
+++ Code/masm_dumps/canResumeConstructionOf_ActionManager_0C53C0.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?canResumeConstructionOf@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z
+; Exact 92-byte retail body at RVA 0x000C53C0.
+_TEXT SEGMENT
+public ?canResumeConstructionOf@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z
+?canResumeConstructionOf@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z PROC
+    db 056h,057h,08Bh,07Ch,024h,00Ch,085h,0FFh,074h,04Bh,08Bh,074h,024h,010h,085h,0F6h
+    db 074h,043h,06Ah,00Eh,08Bh,0CFh,0E8h,044h,0D1h,0F6h,0FFh,084h,0C0h,074h,036h,056h
+    db 08Bh,0CFh,0E8h,032h,053h,0F8h,0FFh,083h,0F8h,002h,075h,029h,0F6h,086h,090h,000h
+    db 000h,000h,004h,074h,020h,0F6h,087h,044h,003h,000h,000h,001h,075h,017h,08Bh,0CEh
+    db 0E8h,0D2h,0D5h,0F3h,0FFh,085h,0C0h,074h,005h,03Bh,047h,074h,075h,007h,05Fh,0B0h
+    db 001h,05Eh,0C2h,00Ch,000h,05Fh,032h,0C0h,05Eh,0C2h,00Ch,000h
+?canResumeConstructionOf@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z ENDP
+_TEXT ENDS
+END
