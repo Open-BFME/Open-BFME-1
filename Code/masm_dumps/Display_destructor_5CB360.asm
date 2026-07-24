3108d1a90a03d43c4f82ce25e63cf6f9e814be99
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:38:20 +0000

    Recover Display destructor retail body

---
+++ Code/masm_dumps/Display_destructor_5CB360.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+; ??1Display@@UAE@XZ
+; Exact 402B @ 0x005CB360; certified Display vtable and derived-to-base destructor chains
+_TEXT SEGMENT
+public ??1Display@@UAE@XZ
+??1Display@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0A9h, 9Fh, 03h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 10h, 56h, 57h, 8Bh, 0F9h, 89h, 7Ch, 24h, 14h
+    db 0C7h, 07h, 0CCh, 0FDh, 10h, 01h, 0C7h, 47h, 08h, 0B8h, 0FDh, 10h, 01h, 0C7h, 44h, 24h
+    db 20h, 04h, 00h, 00h, 00h, 0E8h, 0D7h, 89h, 0A5h, 0FFh, 8Dh, 44h, 24h, 0Ch, 8Dh, 8Fh
+    db 9Ch, 00h, 00h, 00h, 50h, 0E8h, 27h, 0F8h, 0A4h, 0FFh, 8Bh, 74h, 24h, 0Ch, 85h, 0F6h
+    db 0Fh, 84h, 0A9h, 00h, 00h, 00h, 53h, 8Bh, 5Ch, 24h, 14h, 55h, 8Dh, 64h, 24h, 00h
+    db 8Bh, 4Eh, 08h, 85h, 0C9h, 74h, 06h, 8Bh, 11h, 6Ah, 01h, 0FFh, 12h, 8Bh, 06h, 85h
+    db 0C0h, 74h, 04h, 8Bh, 0F0h, 0EBh, 7Eh, 8Bh, 7Bh, 08h, 8Bh, 43h, 04h, 83h, 0C6h, 04h
+    db 2Bh, 0F8h, 56h, 8Dh, 4Ch, 24h, 18h, 0C1h, 0FFh, 02h, 0E8h, 71h, 0C7h, 2Bh, 00h, 8Bh
+    db 44h, 24h, 14h, 85h, 0C0h, 8Dh, 68h, 08h, 75h, 05h, 0BDh, 8Bh, 38h, 07h, 01h, 8Ah
+    db 45h, 00h, 33h, 0F6h, 84h, 0C0h, 8Bh, 0CDh, 74h, 16h, 8Dh, 9Bh, 00h, 00h, 00h, 00h
+    db 0Fh, 0BEh, 0C0h, 8Dh, 34h, 0B6h, 03h, 0F0h, 8Ah, 41h, 01h, 41h, 84h, 0C0h, 75h, 0F0h
+    db 8Dh, 4Ch, 24h, 14h, 0E8h, 17h, 0C5h, 2Bh, 00h, 8Bh, 0C6h, 8Bh, 73h, 04h, 33h, 0D2h
+    db 0F7h, 0F7h, 8Bh, 43h, 08h, 2Bh, 0C6h, 0C1h, 0F8h, 02h, 33h, 0C9h, 8Dh, 64h, 24h, 00h
+    db 42h, 3Bh, 0D0h, 73h, 0Ah, 8Bh, 4Bh, 04h, 8Bh, 0Ch, 91h, 85h, 0C9h, 74h, 0F1h, 8Bh
+    db 7Ch, 24h, 1Ch, 8Bh, 0F1h, 85h, 0F6h, 0Fh, 85h, 63h, 0FFh, 0FFh, 0FFh, 5Dh, 5Bh, 8Bh
+    db 8Fh, 0B0h, 00h, 00h, 00h, 85h, 0C9h, 0C6h, 44h, 24h, 20h, 03h, 74h, 2Ah, 8Bh, 87h
+    db 0B8h, 00h, 00h, 00h, 2Bh, 0C1h, 0C1h, 0F8h, 02h, 0C1h, 0E0h, 02h, 3Dh, 80h, 00h, 00h
+    db 00h, 76h, 0Bh, 51h, 0E8h, 27h, 6Ah, 2Bh, 00h, 83h, 0C4h, 04h, 0EBh, 0Ah, 50h, 51h
+    db 0E8h, 5Bh, 31h, 26h, 00h, 83h, 0C4h, 08h, 8Dh, 8Fh, 9Ch, 00h, 00h, 00h, 0C6h, 44h
+    db 24h, 20h, 02h, 0E8h, 0F3h, 71h, 0A6h, 0FFh, 8Dh, 0B7h, 80h, 00h, 00h, 00h, 8Bh, 0CEh
+    db 0C6h, 44h, 24h, 20h, 01h, 0E8h, 9Fh, 28h, 0A5h, 0FFh, 8Bh, 36h, 85h, 0F6h, 74h, 0Bh
+    db 6Ah, 14h, 56h, 0E8h, 28h, 31h, 26h, 00h, 83h, 0C4h, 08h, 8Bh, 0CFh, 0C7h, 47h, 08h
+    db 44h, 37h, 07h, 01h, 0C7h, 44h, 24h, 20h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 5Fh, 65h, 3Dh
+    db 00h, 8Bh, 4Ch, 24h, 18h, 5Fh, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h
+    db 1Ch, 0C3h
+??1Display@@UAE@XZ ENDP
+_TEXT ENDS
+END
