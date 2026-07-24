982029a65d91fc3ec5ac0a33fef259c59df114f5
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:01:35 +0000

    Recover OptionPreferences heat effects getter

---
+++ Code/masm_dumps/getUseHeatEffects_OptionPreferences_92070.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?getUseHeatEffects@OptionPreferences@@QAE_NXZ
+; Certified body @ RVA 0x00092070, size 98.
+_TEXT SEGMENT
+public ?getUseHeatEffects@OptionPreferences@@QAE_NXZ
+?getUseHeatEffects@OptionPreferences@@QAE_NXZ PROC
+    db 51h, 56h, 57h, 8Bh, 0F1h, 68h, 0C8h, 0FBh, 07h, 01h, 8Dh, 4Ch, 24h, 0Ch, 0E8h, 3Dh
+    db 6Bh, 7Fh, 00h, 8Dh, 44h, 24h, 08h, 83h, 0C6h, 04h, 50h, 8Bh, 0CEh, 0E8h, 1Ah, 8Eh
+    db 0F7h, 0FFh, 8Dh, 4Ch, 24h, 08h, 8Bh, 0F8h, 0E8h, 0A3h, 58h, 7Fh, 00h, 3Bh, 3Eh, 75h
+    db 0Dh, 8Bh, 0Dh, 0C8h, 0D5h, 2Eh, 01h, 8Ah, 41h, 1Dh, 5Fh, 5Eh, 59h, 0C3h, 8Bh, 7Fh
+    db 14h, 85h, 0FFh, 8Dh, 77h, 08h, 75h, 05h, 0BEh, 8Bh, 38h, 07h, 01h, 0BFh, 6Ch, 0C7h
+    db 07h, 01h, 0B9h, 04h, 00h, 00h, 00h, 33h, 0D2h, 0F3h, 0A6h, 5Fh, 0Fh, 94h, 0C0h
+    db 5Eh, 59h, 0C3h
+?getUseHeatEffects@OptionPreferences@@QAE_NXZ ENDP
+_TEXT ENDS
+END
