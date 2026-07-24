bbf5a0c63b6709ba49c033f1401f8affa978703d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:55:52 +0000

    Recover InGameUI militarySubtitle retail body

---
+++ Code/masm_dumps/militarySubtitle_InGameUI_UAEXABVAsciiString_H_Z_00441D30_packet349.asm
@@ -0,0 +1,48 @@
+.386
+.model flat
+
+; ?militarySubtitle@InGameUI@@UAEXABVAsciiString@@H@Z
+; Exact 585 retail bytes @ 0x00441D30; certified packet 349
+_TEXT SEGMENT
+public ?militarySubtitle@InGameUI@@UAEXABVAsciiString@@H@Z
+?militarySubtitle@InGameUI@@UAEXABVAsciiString@@H@Z PROC
+    db 6Ah,0FFh,68h,68h,2Ch,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,53h,56h,57h,8Bh,0F1h,8Bh,06h,33h,0DBh,89h
+    db 5Ch,24h,18h,0FFh,50h,54h,8Dh,4Ch,24h,20h,53h,51h,0E8h,41h,0B3h,0BDh
+    db 0FFh,8Bh,44h,24h,28h,83h,0C4h,08h,3Bh,0C3h,0Fh,84h,0E4h,01h,00h,00h
+    db 66h,39h,58h,04h,0Fh,84h,0DAh,01h,00h,00h,8Bh,7Ch,24h,24h,3Bh,0FBh
+    db 0Fh,8Eh,0CEh,01h,00h,00h,6Ah,01h,8Dh,54h,24h,28h,52h,8Dh,4Ch,24h
+    db 28h,0C7h,44h,24h,2Ch,20h,00h,00h,00h,0E8h,62h,68h,44h,00h,6Ah,48h
+    db 0E8h,8Bh,01h,44h,00h,83h,0C4h,04h,3Bh,0C3h,74h,06h,89h,18h,8Bh,0C8h
+    db 0EBh,02h,33h,0C9h,8Dh,44h,24h,20h,50h,89h,8Eh,18h,08h,00h,00h,0E8h
+    db 6Ch,67h,44h,00h,8Bh,8Eh,18h,08h,00h,00h,89h,79h,30h,8Bh,96h,18h
+    db 08h,00h,00h,89h,5Ah,38h,8Bh,86h,18h,08h,00h,00h,89h,58h,34h,0FFh
+    db 15h,44h,95h,35h,01h,8Bh,8Eh,18h,08h,00h,00h,89h,41h,3Ch,8Bh,96h
+    db 18h,08h,00h,00h,89h,5Ah,04h,8Bh,86h,18h,08h,00h,00h,88h,58h,44h
+    db 8Bh,8Eh,18h,08h,00h,00h,89h,59h,0Ch,8Bh,96h,18h,08h,00h,00h,89h
+    db 5Ah,1Ch,8Bh,86h,18h,08h,00h,00h,89h,58h,10h,8Bh,8Eh,18h,08h,00h
+    db 00h,89h,59h,20h,8Bh,96h,18h,08h,00h,00h,89h,5Ah,14h,8Bh,86h,18h
+    db 08h,00h,00h,89h,58h,24h,8Bh,96h,7Ch,08h,00h,00h,33h,0C9h,8Ah,8Eh
+    db 80h,08h,00h,00h,51h,8Bh,0Dh,84h,14h,2Fh,01h,52h,0E8h,16h,30h,0BCh
+    db 0FFh,89h,44h,24h,10h,0DBh,44h,24h,10h,51h,8Bh,0Dh,38h,1Bh,2Fh,01h
+    db 8Dh,86h,78h,08h,00h,00h,0D9h,1Ch,24h,50h,0E8h,54h,8Dh,0BCh,0FFh,8Bh
+    db 8Eh,18h,08h,00h,00h,89h,59h,2Ch,8Bh,0Dh,0CCh,12h,2Fh,01h,8Bh,11h
+    db 8Bh,0F8h,0FFh,52h,24h,8Bh,8Eh,18h,08h,00h,00h,89h,41h,08h,8Bh,96h
+    db 18h,08h,00h,00h,8Bh,4Ah,08h,8Bh,01h,0FFh,50h,14h,8Bh,8Eh,18h,08h
+    db 00h,00h,8Bh,49h,08h,8Bh,11h,57h,0FFh,52h,18h,38h,9Eh,74h,08h,00h
+    db 00h,74h,24h,8Bh,86h,18h,08h,00h,00h,8Bh,48h,08h,50h,33h,0DBh,0E8h
+    db 0FCh,0C6h,0FFh,0FFh,8Bh,8Eh,18h,08h,00h,00h,99h,2Bh,0C2h,0D1h,0F8h,83h
+    db 0C4h,04h,0F7h,0D8h,89h,41h,18h,8Bh,0Dh,0CCh,12h,2Fh,01h,8Bh,11h,0FFh
+    db 52h,24h,8Bh,8Eh,18h,08h,00h,00h,89h,41h,28h,8Bh,96h,18h,08h,00h
+    db 00h,8Bh,4Ah,28h,8Bh,01h,0FFh,50h,14h,8Bh,8Eh,18h,08h,00h,00h,8Bh
+    db 49h,28h,8Bh,11h,57h,0FFh,52h,18h,0Fh,0B6h,8Eh,60h,08h,00h,00h,0Fh
+    db 0B6h,96h,64h,08h,00h,00h,33h,0C0h,8Ah,0A6h,68h,08h,00h,00h,8Ah,86h
+    db 5Ch,08h,00h,00h,0C1h,0E0h,08h,0Bh,0C1h,8Bh,8Eh,18h,08h,00h,00h,0C1h
+    db 0E0h,08h,0Bh,0C2h,89h,41h,40h,0DBh,86h,90h,08h,00h,00h,0D8h,1Dh,34h
+    db 53h,07h,01h,0DFh,0E0h,0F6h,0C4h,05h,7Ah,0Ah,0C7h,86h,90h,08h,00h,00h
+    db 01h,00h,00h,00h,8Dh,4Ch,24h,20h,0C7h,44h,24h,18h,0FFh,0FFh,0FFh,0FFh
+    db 0E8h,6Bh,62h,44h,00h,8Bh,4Ch,24h,10h,5Fh,5Eh,5Bh,64h,89h,0Dh,00h
+    db 00h,00h,00h,83h,0C4h,10h,0C2h,08h,00h
+?militarySubtitle@InGameUI@@UAEXABVAsciiString@@H@Z ENDP
+_TEXT ENDS
+END
