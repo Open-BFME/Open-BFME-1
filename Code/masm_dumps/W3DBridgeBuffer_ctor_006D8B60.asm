31953e1b945bdd9e28851e9946545286c959d674
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:35:44 +0000

    Recover W3DBridgeBuffer constructor

---
+++ Code/masm_dumps/W3DBridgeBuffer_ctor_006D8B60.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+; ??0W3DBridgeBuffer@@QAE@XZ
+; Exact 139B @ 0x006D8B60; certified packet 124 retail SHA-256 9e17d15da229af3c63833afcdaecdeb75ce975b705ed266f3a424f43e592f2d2
+_TEXT SEGMENT
+public ??0W3DBridgeBuffer@@QAE@XZ
+??0W3DBridgeBuffer@@QAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 57h, 0ABh, 04h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 53h, 56h, 8Bh, 0F1h, 33h, 0DBh, 89h, 74h, 24h, 08h
+    db 89h, 5Eh, 0Ch, 68h, 74h, 81h, 41h, 00h, 68h, 0F6h, 7Ah, 43h, 00h, 68h, 0C8h, 00h
+    db 00h, 00h, 68h, 14h, 01h, 00h, 00h, 8Dh, 46h, 18h, 50h, 89h, 5Ch, 24h, 28h, 0E8h
+    db 40h, 0E3h, 31h, 00h, 8Bh, 0CEh, 0C6h, 44h, 24h, 14h, 01h, 88h, 9Eh, 0BCh, 0D7h, 00h
+    db 00h, 89h, 5Eh, 08h, 89h, 1Eh, 89h, 5Eh, 04h, 89h, 5Eh, 10h, 89h, 5Eh, 14h, 89h
+    db 9Eh, 0B8h, 0D7h, 00h, 00h, 0E8h, 8Fh, 0B2h, 92h, 0FFh, 8Bh, 0CEh, 0E8h, 5Ch, 63h, 95h
+    db 0FFh, 8Bh, 4Ch, 24h, 0Ch, 0C6h, 86h, 0BCh, 0D7h, 00h, 00h, 01h, 8Bh, 0C6h, 5Eh, 5Bh
+    db 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??0W3DBridgeBuffer@@QAE@XZ ENDP
+_TEXT ENDS
+END
