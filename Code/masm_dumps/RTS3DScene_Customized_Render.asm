f97df4e42ad994c30b4320f7dc0e895e33c9eba1
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:33:15 +0000

    Recover RTS3DScene Customized_Render

---
+++ Code/masm_dumps/RTS3DScene_Customized_Render.asm
@@ -0,0 +1,67 @@
+.386
+.model flat
+
+; ?Customized_Render@RTS3DScene@@UAEXAAVRenderInfoClass@@@Z
+; Exact 888 retail bytes @ 0x007143B0; certified RTS3DScene vtable slot +0x5C.
+_TEXT SEGMENT
+public ?Customized_Render@RTS3DScene@@UAEXAAVRenderInfoClass@@@Z
+?Customized_Render@RTS3DScene@@UAEXAAVRenderInfoClass@@@Z PROC
+    db 83h, 0ECh, 08h, 53h, 55h, 56h, 33h, 0F6h, 57h, 8Bh, 0F9h, 89h, 0B7h, 6Ch, 08h, 00h
+    db 00h, 89h, 0B7h, 74h, 08h, 00h, 00h, 0A1h, 48h, 0D7h, 2Eh, 01h, 3Bh, 0C6h, 89h, 74h
+    db 24h, 10h, 74h, 0Ch, 8Bh, 40h, 0Ch, 8Bh, 48h, 24h, 89h, 4Ch, 24h, 14h, 0EBh, 04h
+    db 89h, 74h, 24h, 14h, 39h, 0B7h, 68h, 08h, 00h, 00h, 75h, 0Eh, 8Bh, 44h, 24h, 1Ch
+    db 8Bh, 08h, 8Bh, 17h, 51h, 8Bh, 0CFh, 0FFh, 52h, 6Ch, 0A1h, 0CCh, 0F4h, 2Eh, 01h, 33h
+    db 0DBh, 3Bh, 0C6h, 74h, 07h, 0Fh, 0B6h, 98h, 0F4h, 18h, 00h, 00h, 39h, 0B7h, 68h, 08h
+    db 00h, 00h, 0Fh, 85h, 4Fh, 01h, 00h, 00h, 8Bh, 6Fh, 7Ch, 8Dh, 47h, 78h, 3Bh, 0E8h
+    db 0Fh, 84h, 41h, 01h, 00h, 00h, 8Bh, 45h, 0Ch, 85h, 0C0h, 74h, 05h, 8Dh, 70h, 0F8h
+    db 0EBh, 02h, 33h, 0F6h, 8Bh, 16h, 8Bh, 0CEh, 0FFh, 52h, 0Ch, 83h, 0F8h, 04h, 75h, 08h
+    db 85h, 0DBh, 89h, 74h, 24h, 10h, 75h, 1Ch, 0E8h, 83h, 0CAh, 1Fh, 00h, 84h, 0C0h, 75h
+    db 13h, 8Bh, 45h, 0Ch, 85h, 0C0h, 74h, 05h, 8Dh, 48h, 0F8h, 0EBh, 02h, 33h, 0C9h, 8Bh
+    db 01h, 0FFh, 50h, 34h, 8Bh, 6Dh, 04h, 8Dh, 47h, 78h, 3Bh, 0E8h, 75h, 0B8h, 8Bh, 44h
+    db 24h, 10h, 85h, 0C0h, 0Fh, 84h, 0EDh, 00h, 00h, 00h, 8Bh, 0Dh, 0C8h, 0D5h, 2Eh, 01h
+    db 8Ah, 81h, 0CCh, 0Dh, 00h, 00h, 84h, 0C0h, 0Fh, 85h, 0D9h, 00h, 00h, 00h, 85h, 0DBh
+    db 0Fh, 85h, 0D1h, 00h, 00h, 00h, 8Bh, 74h, 24h, 1Ch, 8Bh, 0Eh, 53h, 89h, 5Eh, 1Ch
+    db 8Bh, 11h, 57h, 0FFh, 92h, 54h, 01h, 00h, 00h, 8Bh, 87h, 68h, 08h, 00h, 00h, 85h
+    db 0C0h, 75h, 61h, 8Bh, 4Ch, 24h, 10h, 8Bh, 01h, 56h, 0FFh, 50h, 30h, 8Bh, 0Dh, 04h
+    db 11h, 2Fh, 01h, 85h, 0C9h, 0Fh, 84h, 9Ch, 00h, 00h, 00h, 8Bh, 11h, 0FFh, 52h, 3Ch
+    db 8Bh, 0Dh, 04h, 11h, 2Fh, 01h, 8Bh, 01h, 0FFh, 50h, 28h, 84h, 0C0h, 0Fh, 84h, 84h
+    db 00h, 00h, 00h, 6Ah, 04h, 8Bh, 0CEh, 0E8h, 44h, 0F2h, 20h, 00h, 8Bh, 0Dh, 48h, 80h
+    db 2Fh, 01h, 51h, 8Bh, 0CEh, 0E8h, 96h, 0F2h, 20h, 00h, 8Bh, 4Ch, 24h, 10h, 8Bh, 11h
+    db 56h, 0FFh, 52h, 30h, 8Bh, 0CEh, 0E8h, 0B5h, 0F2h, 20h, 00h, 8Bh, 0CEh, 0E8h, 3Eh, 0F2h
+    db 20h, 00h, 0EBh, 53h, 83h, 0F8h, 03h, 74h, 24h, 83h, 0F8h, 04h, 74h, 1Fh, 83h, 0F8h
+    db 05h, 74h, 44h, 83h, 0F8h, 07h, 74h, 3Fh, 83h, 0F8h, 06h, 74h, 3Ah, 8Bh, 4Ch, 24h
+    db 10h, 8Bh, 54h, 24h, 1Ch, 8Bh, 01h, 52h, 0FFh, 50h, 30h, 0EBh, 2Ah, 8Bh, 5Ch, 24h
+    db 10h, 8Bh, 03h, 8Bh, 0CBh, 0FFh, 90h, 0C4h, 01h, 00h, 00h, 85h, 0C0h, 75h, 10h, 0E8h
+    db 7Dh, 9Eh, 8Fh, 0FFh, 8Ah, 48h, 20h, 84h, 0C9h, 74h, 0Ch, 8Bh, 74h, 24h, 1Ch, 8Bh
+    db 13h, 56h, 8Bh, 0CBh, 0FFh, 52h, 30h, 8Ah, 87h, 28h, 01h, 00h, 00h, 84h, 0C0h, 0Fh
+    db 85h, 0A9h, 01h, 00h, 00h, 83h, 0BFh, 68h, 08h, 00h, 00h, 04h, 0Fh, 84h, 9Ch, 01h
+    db 00h, 00h, 8Bh, 0AFh, 0F4h, 00h, 00h, 00h, 8Dh, 9Fh, 0F0h, 00h, 00h, 00h, 3Bh, 0EBh
+    db 0Fh, 84h, 2Dh, 01h, 00h, 00h, 8Bh, 45h, 0Ch, 85h, 0C0h, 74h, 05h, 8Dh, 70h, 0F8h
+    db 0EBh, 02h, 33h, 0F6h, 8Bh, 06h, 8Bh, 6Dh, 04h, 8Bh, 0CEh, 0FFh, 50h, 0Ch, 83h, 0F8h
+    db 04h, 0Fh, 84h, 04h, 01h, 00h, 00h, 8Bh, 16h, 8Bh, 0CEh, 0FFh, 92h, 7Ch, 01h, 00h
+    db 00h, 85h, 0C0h, 0Fh, 84h, 0F2h, 00h, 00h, 00h, 8Bh, 87h, 68h, 08h, 00h, 00h, 83h
+    db 0F8h, 03h, 74h, 77h, 83h, 0F8h, 05h, 74h, 72h, 83h, 0F8h, 06h, 74h, 6Dh, 83h, 0F8h
+    db 07h, 75h, 23h, 8Bh, 06h, 8Bh, 0CEh, 0FFh, 90h, 0BCh, 01h, 00h, 00h, 8Ah, 0Dh, 32h
+    db 9Ch, 2Fh, 01h, 85h, 0C0h, 0Fh, 95h, 0C0h, 84h, 0C9h, 75h, 2Dh, 84h, 0C0h, 0Fh, 84h
+    db 0B7h, 00h, 00h, 00h, 0EBh, 23h, 85h, 0C0h, 8Bh, 06h, 75h, 33h, 8Bh, 0CEh, 0FFh, 90h
+    db 58h, 01h, 00h, 00h, 85h, 0C0h, 74h, 11h, 8Bh, 48h, 04h, 85h, 0C9h, 74h, 0Ah, 0F6h
+    db 40h, 0Ch, 1Eh, 0Fh, 85h, 92h, 00h, 00h, 00h, 8Bh, 4Ch, 24h, 14h, 8Bh, 54h, 24h
+    db 1Ch, 6Ah, 00h, 51h, 56h, 52h, 8Bh, 0CFh, 0E8h, 0FBh, 2Bh, 90h, 0FFh, 0EBh, 7Ch, 8Bh
+    db 4Ch, 24h, 1Ch, 51h, 8Bh, 0CEh, 0FFh, 50h, 30h, 0EBh, 70h, 8Bh, 15h, 0C8h, 0D5h, 2Eh
+    db 01h, 8Ah, 82h, 0BDh, 0Dh, 00h, 00h, 84h, 0C0h, 8Bh, 06h, 8Bh, 0CEh, 74h, 23h, 0FFh
+    db 90h, 0BCh, 01h, 00h, 00h, 85h, 0C0h, 8Bh, 87h, 68h, 08h, 00h, 00h, 0Fh, 95h, 0C1h
+    db 83h, 0F8h, 06h, 75h, 04h, 84h, 0C9h, 74h, 42h, 83h, 0F8h, 05h, 75h, 0ABh, 84h, 0C9h
+    db 0EBh, 0A1h, 0FFh, 90h, 0C4h, 01h, 00h, 00h, 85h, 0C0h, 74h, 2Fh, 8Bh, 16h, 8Bh, 0CEh
+    db 0FFh, 92h, 0CCh, 01h, 00h, 00h, 8Bh, 4Ch, 24h, 14h, 8Bh, 54h, 24h, 1Ch, 6Ah, 00h
+    db 51h, 85h, 0C0h, 56h, 0Fh, 95h, 0C0h, 52h, 8Bh, 0CFh, 0A2h, 2Ch, 0F4h, 33h, 01h, 0E8h
+    db 84h, 2Bh, 90h, 0FFh, 0C6h, 05h, 2Ch, 0F4h, 33h, 01h, 00h, 3Bh, 0EBh, 0Fh, 85h, 0D3h
+    db 0FEh, 0FFh, 0FFh, 0A1h, 0ECh, 6Eh, 30h, 01h, 85h, 0C0h, 74h, 2Ah, 8Bh, 44h, 24h, 10h
+    db 85h, 0C0h, 74h, 4Ah, 0E8h, 0F7h, 0C7h, 1Fh, 00h, 84h, 0C0h, 75h, 19h, 8Bh, 47h, 18h
+    db 85h, 0C0h, 75h, 12h, 8Bh, 87h, 68h, 08h, 00h, 00h, 85h, 0C0h, 75h, 08h, 0A1h, 0ECh
+    db 6Eh, 30h, 01h, 0C6h, 00h, 01h, 8Bh, 44h, 24h, 10h, 85h, 0C0h, 74h, 20h, 8Bh, 0Dh
+    db 0BCh, 64h, 2Fh, 01h, 85h, 0C9h, 74h, 16h, 8Bh, 47h, 18h, 85h, 0C0h, 75h, 0Fh, 8Bh
+    db 87h, 68h, 08h, 00h, 00h, 85h, 0C0h, 75h, 05h, 8Bh, 11h, 0FFh, 52h, 34h, 5Fh, 5Eh
+    db 5Dh, 5Bh, 83h, 0C4h, 08h, 0C2h, 04h, 00h
+?Customized_Render@RTS3DScene@@UAEXAAVRenderInfoClass@@@Z ENDP
+_TEXT ENDS
+END
