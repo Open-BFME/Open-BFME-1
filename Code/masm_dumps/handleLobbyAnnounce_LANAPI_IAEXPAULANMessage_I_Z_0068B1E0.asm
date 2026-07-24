2e7c7fc6131522e0857941b6e500c73be18cf6a5
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:32:18 +0000

    Recover LANAPI lobby announce handler

---
+++ Code/masm_dumps/handleLobbyAnnounce_LANAPI_IAEXPAULANMessage_I_Z_0068B1E0.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+
+; ?handleLobbyAnnounce@LANAPI@@IAEXPAULANMessage@@I@Z
+; Exact 331 retail bytes @ 0x0068B1E0; certified packet 112
+_TEXT SEGMENT
+public ?handleLobbyAnnounce@LANAPI@@IAEXPAULANMessage@@I@Z
+?handleLobbyAnnounce@LANAPI@@IAEXPAULANMessage@@I@Z PROC
+    db 64h,0A1h,00h,00h,00h,00h,6Ah,0FFh,68h,0D8h,69h,04h,01h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,53h,8Bh,5Ch,24h,18h,55h,56h,57h,8Bh,0F9h,8Bh
+    db 07h,53h,0FFh,90h,0D8h,00h,00h,00h,8Bh,0F0h,33h,0EDh,3Bh,0F5h,75h,43h
+    db 6Ah,1Ch,0E8h,19h,6Dh,1Fh,00h,83h,0C4h,04h,3Bh,0C5h,74h,24h,89h,28h
+    db 89h,68h,04h,89h,68h,08h,89h,68h,0Ch,89h,68h,10h,89h,68h,14h,66h
+    db 89h,68h,18h,8Bh,0Bh,89h,48h,14h,8Bh,53h,04h,8Bh,0F0h,89h,50h,18h
+    db 0EBh,19h,8Bh,0Bh,33h,0C0h,89h,48h,14h,8Bh,53h,04h,8Bh,0F0h,89h,50h
+    db 18h,0EBh,08h,56h,8Bh,0CFh,0E8h,99h,0Bh,9Bh,0FFh,8Bh,5Ch,24h,20h,8Dh
+    db 43h,04h,50h,8Dh,4Ch,24h,28h,0E8h,74h,0DBh,1Fh,00h,8Dh,4Ch,24h,24h
+    db 51h,8Bh,0CEh,89h,6Ch,24h,1Ch,0E8h,0B4h,0D2h,1Fh,00h,83h,0CDh,0FFh,8Dh
+    db 4Ch,24h,24h,89h,6Ch,24h,18h,0E8h,44h,0CFh,1Fh,00h,8Dh,53h,20h,52h
+    db 8Dh,4Ch,24h,28h,0E8h,27h,0D9h,1Fh,00h,8Dh,44h,24h,24h,50h,8Dh,4Eh
+    db 08h,0C7h,44h,24h,1Ch,01h,00h,00h,00h,0E8h,42h,0DFh,1Fh,00h,8Dh,4Ch
+    db 24h,24h,89h,6Ch,24h,18h,0E8h,85h,0C6h,1Fh,00h,83h,0C3h,1Eh,53h,8Dh
+    db 4Ch,24h,24h,0E8h,0F8h,0D8h,1Fh,00h,8Dh,4Ch,24h,20h,51h,8Dh,4Eh,04h
+    db 0C7h,44h,24h,1Ch,02h,00h,00h,00h,0E8h,13h,0DFh,1Fh,00h,8Dh,4Ch,24h
+    db 20h,89h,6Ch,24h,18h,0E8h,56h,0C6h,1Fh,00h,0FFh,15h,44h,95h,35h,01h
+    db 56h,8Bh,0CFh,89h,46h,0Ch,0E8h,68h,0A0h,9Bh,0FFh,51h,89h,64h,24h,28h
+    db 8Bh,0CCh,56h,0E8h,0F8h,0D0h,1Fh,00h,8Bh,17h,83h,0C6h,14h,56h,8Bh,0CFh
+    db 0FFh,92h,0A4h,00h,00h,00h,8Bh,4Ch,24h,10h,5Fh,5Eh,5Dh,64h,89h,0Dh
+    db 00h,00h,00h,00h,5Bh,83h,0C4h,0Ch,0C2h,08h,00h
+?handleLobbyAnnounce@LANAPI@@IAEXPAULANMessage@@I@Z ENDP
+_TEXT ENDS
+END
