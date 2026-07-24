8448f2a8bcf8359d3267b09878b2c6460edf0c54
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:15:26 +0000

    Recover GameSpyGroupRoom destructor

---
+++ Code/masm_dumps/GameSpyGroupRoom_destructor.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1GameSpyGroupRoom@@QAE@XZ
+; Retail @ 0x004F9850 size 76
+_TEXT SEGMENT
+public ??1GameSpyGroupRoom@@QAE@XZ
+??1GameSpyGroupRoom@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,0A8h,0CEh,02h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,89h,74h,24h,04h,8Dh,4Eh,04h
+    db 0C7h,44h,24h,10h,00h,00h,00h,00h
+    db 0E8h,53h,0E9h,38h,00h,8Bh,0CEh,0C7h
+    db 44h,24h,10h,0FFh,0FFh,0FFh,0FFh,0E8h
+    db 0B4h,0E0h,38h,00h,8Bh,4Ch,24h,08h,5Eh
+    db 64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C3h
+??1GameSpyGroupRoom@@QAE@XZ ENDP
+_TEXT ENDS
+END
