a02f8c49f28e9190b65ffec46c539e647bfd5dbd
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:55:41 +0000

    Recover HeaderTemplateManager populateGameFonts

---
+++ Code/GameEngine/Source/GameClient/GUI/HeaderTemplateManager_populateGameFonts.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?populateGameFonts@HeaderTemplateManager@@AAEXXZ
+; Exact 85 retail bytes @ 0x0048C480
+_TEXT SEGMENT
+public ?populateGameFonts@HeaderTemplateManager@@AAEXXZ
+?populateGameFonts@HeaderTemplateManager@@AAEXXZ PROC
+    db 051h,053h,08Bh,0D9h,08Bh,003h,057h,08Bh
+    db 038h,03Bh,0F8h,074h,044h,056h,08Bh,0FFh
+    db 08Bh,077h,008h,08Bh,046h,00Ch,08Bh,00Dh
+    db 084h,014h,02Fh,001h,050h,0E8h,0C5h,089h
+    db 0B7h,0FFh,033h,0C9h,08Ah,04Eh,010h,089h
+    db 044h,024h,00Ch,0DBh,044h,024h,00Ch,08Dh
+    db 046h,008h,0D9h,05Ch,024h,00Ch,08Bh,054h
+    db 024h,00Ch,051h,08Bh,00Dh,038h,01Bh,02Fh
+    db 001h,052h,050h,0E8h,0FBh,0E6h,0B7h,0FFh
+    db 089h,006h,08Bh,03Fh,03Bh,03Bh,075h,0C0h
+    db 05Eh,05Fh,05Bh,059h,0C3h
+?populateGameFonts@HeaderTemplateManager@@AAEXXZ ENDP
+_TEXT ENDS
+
+END
