7f27b4f3101fba5be5f50d860bf90878ebc6093b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:18:44 +0000

    Recover WeaponStore parser retail body

---
+++ Code/GameEngine/Source/GameLogic/Object/WeaponStore_parseWeaponTemplateDefinition.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ?parseWeaponTemplateDefinition@WeaponStore@@SAXPAVINI@@@Z
+; Certified packet 178: exact 223 retail bytes @ 0x001E9C40
+; SHA-256: da43851aa11804b5070b0273f73a23217690057e7b934ba66d66deb312c45aab
+_TEXT SEGMENT
+public ?parseWeaponTemplateDefinition@WeaponStore@@SAXPAVINI@@@Z
+?parseWeaponTemplateDefinition@WeaponStore@@SAXPAVINI@@@Z PROC
+    db 6Ah,0FFh,68h,58h,0A6h,00h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h,56h,57h,0C7h,44h,24h,08h,00h,00h
+    db 00h,00h,8Bh,7Ch,24h,20h,6Ah,00h,8Bh,0CFh,0C7h,44h,24h,1Ch,00h,00h
+    db 00h,00h,0E8h,0F9h,6Ch,66h,00h,85h,0C0h,74h,10h,8Bh,0C8h,8Dh,71h,01h
+    db 8Ah,11h,41h,84h,0D2h,75h,0F9h,2Bh,0CEh,0EBh,02h,33h,0C9h,51h,50h,8Dh
+    db 4Ch,24h,10h,0E8h,88h,0E0h,69h,00h,8Bh,44h,24h,08h,85h,0C0h,74h,05h
+    db 83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h,8Bh,0Dh,00h,0D6h,2Eh,01h
+    db 50h,0E8h,21h,11h,0E5h,0FFh,8Bh,0Dh,38h,0F7h,2Eh,01h,50h,0E8h,0C4h,37h
+    db 0E2h,0FFh,85h,0C0h,74h,0Eh,83h,7Fh,08h,02h,75h,31h,50h,0E8h,0A0h,21h
+    db 0E4h,0FFh,0EBh,1Ch,51h,8Dh,44h,24h,0Ch,89h,64h,24h,10h,8Bh,0CCh,50h
+    db 0E8h,7Bh,0DEh,69h,00h,8Bh,0Dh,38h,0F7h,2Eh,01h,0E8h,12h,0ADh,0E1h,0FFh
+    db 68h,18h,0FEh,09h,01h,50h,8Bh,0CFh,0E8h,0A3h,83h,66h,00h,8Dh,4Ch,24h
+    db 08h,0C7h,44h,24h,18h,0FFh,0FFh,0FFh,0FFh,0E8h,32h,0DCh,69h,00h,8Bh,4Ch
+    db 24h,10h,5Fh,64h,89h,0Dh,00h,00h,00h,00h,5Eh,83h,0C4h,14h,0C3h
+?parseWeaponTemplateDefinition@WeaponStore@@SAXPAVINI@@@Z ENDP
+_TEXT ENDS
+END
