26808bb5af63b6fffd7108cc25575332eb536b8b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:11:18 +0000

    Recover AITunnelNetworkGuardState constructor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIStates_AITunnelNetworkGuardState_ctor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??0AITunnelNetworkGuardState@@QAE@PAVStateMachine@@@Z
+; Exact 53 retail bytes @ 0x001717B0
+_TEXT SEGMENT
+public ??0AITunnelNetworkGuardState@@QAE@PAVStateMachine@@@Z
+??0AITunnelNetworkGuardState@@QAE@PAVStateMachine@@@Z PROC
+    db 051h,056h,051h,08Bh,0F1h,089h,064h,024h
+    db 008h,08Bh,0CCh,068h,00Ch,083h,009h,001h
+    db 0E8h,0FBh,073h,071h,000h,08Bh,044h,024h
+    db 010h,050h,08Bh,0CEh,0E8h,0E1h,01Dh,0E9h
+    db 0FFh,0C7h,006h,0B8h,082h,009h,001h,0C7h
+    db 046h,024h,000h,000h,000h,000h,08Bh,0C6h
+    db 05Eh,059h,0C2h,004h,000h
+??0AITunnelNetworkGuardState@@QAE@PAVStateMachine@@@Z ENDP
+_TEXT ENDS
+END
