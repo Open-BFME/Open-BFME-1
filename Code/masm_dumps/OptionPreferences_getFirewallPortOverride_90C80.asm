892f48f6d13ca17c675c651de62ea08a7029ff39
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:29:40 +0000

    Recover OptionPreferences firewall port override

---
+++ Code/masm_dumps/OptionPreferences_getFirewallPortOverride_90C80.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getFirewallPortOverride@OptionPreferences@@QAEGXZ
+; Exact 95B @ 0x00090C80; certified string/source-order/call-graph identity
+_TEXT SEGMENT
+public ?getFirewallPortOverride@OptionPreferences@@QAEGXZ
+?getFirewallPortOverride@OptionPreferences@@QAEGXZ PROC
+    db 51h, 56h, 57h, 8Bh, 0F1h, 68h, 60h, 0A1h, 07h, 01h, 8Dh, 4Ch, 24h, 0Ch, 0E8h, 2Dh
+    db 7Fh, 7Fh, 00h, 8Dh, 44h, 24h, 08h, 83h, 0C6h, 04h, 50h, 8Bh, 0CEh, 0E8h, 0Ah, 0A2h
+    db 0F7h, 0FFh, 8Dh, 4Ch, 24h, 08h, 8Bh, 0F8h, 0E8h, 93h, 6Ch, 7Fh, 00h, 3Bh, 3Eh, 75h
+    db 11h, 8Bh, 0Dh, 0C8h, 0D5h, 2Eh, 01h, 66h, 8Bh, 81h, 1Ch, 0Bh, 00h, 00h, 5Fh, 5Eh
+    db 59h, 0C3h, 8Bh, 7Fh, 14h, 85h, 0FFh, 8Dh, 47h, 08h, 75h, 05h, 0B8h, 8Bh, 38h, 07h
+    db 01h, 50h, 0FFh, 15h, 84h, 93h, 35h, 01h, 83h, 0C4h, 04h, 5Fh, 5Eh, 59h, 0C3h
+?getFirewallPortOverride@OptionPreferences@@QAEGXZ ENDP
+_TEXT ENDS
+END
