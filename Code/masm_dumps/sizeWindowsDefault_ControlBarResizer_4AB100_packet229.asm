064afa42c4cda1c775852b0e33a742701b16c1f4
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:51:50 +0000

    Recover ControlBarResizer sizeWindowsDefault

---
+++ Code/masm_dumps/sizeWindowsDefault_ControlBarResizer_4AB100_packet229.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?sizeWindowsDefault@ControlBarResizer@@QAEXXZ
+; Exact 122 retail bytes @ 0x004AB100; certified packet 229
+; SHA-256 8e769ae3b84ff3a81f7e98fc36c10a362f552c8361f9009c7c08338860f40cb7
+_TEXT SEGMENT
+public ?sizeWindowsDefault@ControlBarResizer@@QAEXXZ
+?sizeWindowsDefault@ControlBarResizer@@QAEXXZ PROC
+    db 55h,8Bh,0E9h,8Bh,45h,00h,57h,8Bh,38h,3Bh,0F8h,74h,6Ah,53h,56h,90h
+    db 8Bh,77h,08h,85h,0F6h,74h,57h,8Bh,06h,85h,0C0h,74h,05h,83h,0C0h,08h
+    db 0EBh,05h,0B8h,8Bh,38h,07h,01h,8Bh,0Dh,40h,1Bh,2Fh,01h,8Bh,19h,8Bh
+    db 0Dh,00h,0D6h,2Eh,01h,50h,0E8h,9Ch,0FCh,0B8h,0FFh,8Bh,0Dh,40h,1Bh,2Fh
+    db 01h,50h,6Ah,00h,0FFh,93h,0DCh,00h,00h,00h,8Bh,0D8h,85h,0DBh,74h,1Eh
+    db 8Bh,56h,10h,8Bh,46h,0Ch,52h,50h,8Bh,0CBh,0E8h,3Eh,0E3h,0B6h,0FFh,8Bh
+    db 4Eh,08h,8Bh,56h,04h,51h,52h,8Bh,0CBh,0E8h,3Eh,0D1h,0B9h,0FFh,8Bh,3Fh
+    db 3Bh,7Dh,00h,75h,9Bh,5Eh,5Bh,5Fh,5Dh,0C3h
+?sizeWindowsDefault@ControlBarResizer@@QAEXXZ ENDP
+_TEXT ENDS
+END
