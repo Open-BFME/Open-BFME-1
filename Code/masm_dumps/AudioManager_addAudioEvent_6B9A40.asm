50d46a74e0a12b5749683cf2c5db11537fb42b50
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:19:38 +0000

    Recover AudioManager addAudioEvent wrapper

---
+++ Code/masm_dumps/AudioManager_addAudioEvent_6B9A40.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+
+; ?addAudioEvent@AudioManager@@UAEIPBVAudioEventRTS@@@Z
+; Retail @ 0x006B9A40 size 19
+_TEXT SEGMENT
+public ?addAudioEvent@AudioManager@@UAEIPBVAudioEventRTS@@@Z
+?addAudioEvent@AudioManager@@UAEIPBVAudioEventRTS@@@Z PROC
+    db 08Bh,044h,024h,004h,06Ah,001h,06Ah,001h,06Ah,000h,050h,0E8h,0B8h,070h,096h,0FFh
+    db 0C2h,004h,000h
+?addAudioEvent@AudioManager@@UAEIPBVAudioEventRTS@@@Z ENDP
+_TEXT ENDS
+END
