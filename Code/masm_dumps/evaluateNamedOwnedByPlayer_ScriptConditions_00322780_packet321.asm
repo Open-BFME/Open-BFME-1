8e0bbdbd99010011728a8b05f054089c5de62452
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 11:29:28 +0000

    Recover ScriptConditions named-owned evaluator

---
+++ Code/masm_dumps/evaluateNamedOwnedByPlayer_ScriptConditions_00322780_packet321.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?evaluateNamedOwnedByPlayer@ScriptConditions@@IAE_NPAVParameter@@0@Z
+; Exact 115 retail bytes @ 0x00322780; certified packet 321
+_TEXT SEGMENT
+public ?evaluateNamedOwnedByPlayer@ScriptConditions@@IAE_NPAVParameter@@0@Z
+?evaluateNamedOwnedByPlayer@ScriptConditions@@IAE_NPAVParameter@@0@Z PROC
+    db 8Bh,0Dh,6Ch,07h,2Fh,01h,8Bh,54h,24h,04h,8Bh,01h,57h,52h,0FFh,50h
+    db 68h,8Bh,0F8h,85h,0FFh,75h,06h,32h,0C0h,5Fh,0C2h,08h,00h,8Bh,44h,24h
+    db 0Ch,8Bh,0Dh,6Ch,07h,2Fh,01h,56h,50h,0E8h,07h,09h,0D0h,0FFh,66h,85h
+    db 0C0h,89h,44h,24h,0Ch,74h,2Eh,0EBh,07h,8Dh,0A4h,24h,00h,00h,00h,00h
+    db 8Dh,4Ch,24h,0Ch,51h,8Bh,0Dh,48h,0D7h,2Eh,01h,0E8h,90h,0C6h,0D0h,0FFh
+    db 8Bh,0CFh,8Bh,0F0h,0E8h,4Bh,0E0h,0CFh,0FFh,3Bh,0C6h,74h,0Fh,66h,83h,7Ch
+    db 24h,0Ch,00h,75h,0DBh,5Eh,32h,0C0h,5Fh,0C2h,08h,00h,5Eh,0B0h,01h,5Fh
+    db 0C2h,08h,00h
+?evaluateNamedOwnedByPlayer@ScriptConditions@@IAE_NPAVParameter@@0@Z ENDP
+_TEXT ENDS
+END
