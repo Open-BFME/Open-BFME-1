064d1c1d119fe99842ca5bce0578de12d87047e5
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:43:32 +0000

    Recover Mission default constructor

---
+++ Code/masm_dumps/Mission_ctor_QAE_XZ_005BB9B0_00ce5396.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ??0Mission@@QAE@XZ
+; Exact 164 retail bytes @ 0x005BB9B0
+_TEXT SEGMENT
+public ??0Mission@@QAE@XZ
+??0Mission@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,0EBh,94h,03h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh,0F1h,57h,33h,0FFh,0C7h,06h,58h,0F6h
+    db 10h,01h,89h,74h,24h,08h,89h,7Eh,04h,89h,7Ch,24h,14h,89h,7Eh,08h
+    db 89h,7Eh,0Ch,89h,7Eh,10h,68h,28h,0D8h,40h,00h,68h,0D9h,7Bh,41h,00h
+    db 6Ah,05h,6Ah,04h,8Dh,46h,14h,50h,0C6h,44h,24h,28h,03h,0E8h,0E2h,0B4h
+    db 43h,00h,6Ah,02h,68h,50h,6Eh,33h,01h,8Dh,4Eh,28h,0C6h,44h,24h,1Ch
+    db 04h,0E8h,0F0h,98h,0A6h,0FFh,89h,0BEh,98h,00h,00h,00h,68h,28h,0D8h,40h
+    db 00h,68h,0D9h,7Bh,41h,00h,6Ah,03h,6Ah,04h,8Dh,8Eh,9Ch,00h,00h,00h
+    db 51h,0C6h,44h,24h,28h,06h,0E8h,0A9h,0B4h,43h,00h,8Bh,4Ch,24h,0Ch,89h
+    db 0BEh,0A8h,00h,00h,00h,5Fh,8Bh,0C6h,5Eh,64h,89h,0Dh,00h,00h,00h,00h
+    db 83h,0C4h,10h,0C3h
+??0Mission@@QAE@XZ ENDP
+_TEXT ENDS
+END
