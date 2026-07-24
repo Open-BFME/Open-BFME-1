cdfa6c6c8d449ac19aa1e4435854128a1d1d2264
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:52:04 +0000

    Recover SidesList isPlayerDefaultTeam packet 308

---
+++ Code/masm_dumps/isPlayerDefaultTeam_SidesList_001967F0_packet308.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+
+; ?isPlayerDefaultTeam@SidesList@@QAE_NPAVTeamsInfo@@@Z
+; Exact 380 retail bytes @ 0x001967F0; certified packet 308
+; SHA-256 26d2d2f0635fd4e762581b65bbe9c583971adcca6d3b762940e710fbcef4b2a5
+_TEXT SEGMENT
+public ?isPlayerDefaultTeam@SidesList@@QAE_NPAVTeamsInfo@@@Z
+?isPlayerDefaultTeam@SidesList@@QAE_NPAVTeamsInfo@@@Z PROC
+    db 6Ah,0FFh,68h,0C0h,71h,00h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,10h,53h,55h,56h,57h,8Bh,0F1h,33h,0FFh
+    db 57h,0B9h,0B8h,75h,2Ah,01h,89h,74h,24h,20h,0E8h,0E5h,2Ah,0E7h,0FFh,8Bh
+    db 4Ch,24h,34h,50h,8Dh,44h,24h,18h,50h,0E8h,3Fh,97h,0E9h,0FFh,6Ah,04h
+    db 68h,1Ch,3Eh,08h,01h,8Dh,4Ch,24h,18h,89h,7Ch,24h,30h,0E8h,5Eh,0Dh
+    db 6Fh,00h,84h,0C0h,0Fh,84h,0C4h,00h,00h,00h,8Bh,44h,24h,10h,3Bh,0C7h
+    db 74h,05h,83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h,8Dh,58h,04h,39h
+    db 7Eh,28h,89h,7Ch,24h,18h,0Fh,8Eh,0A2h,00h,00h,00h,83h,0C6h,30h,89h
+    db 74h,24h,30h,6Ah,00h,0B9h,18h,79h,2Ah,01h,0E8h,85h,2Ah,0E7h,0FFh,50h
+    db 8Dh,4Ch,24h,1Ch,51h,8Bh,4Ch,24h,3Ch,0E8h,0DFh,96h,0E9h,0FFh,85h,0DBh
+    db 74h,10h,8Bh,0C3h,8Dh,50h,01h,8Ah,08h,40h,84h,0C9h,75h,0F9h,2Bh,0C2h
+    db 0EBh,02h,33h,0C0h,8Bh,4Ch,24h,14h,85h,0C9h,74h,09h,0Fh,0B7h,69h,04h
+    db 8Dh,71h,08h,0EBh,07h,33h,0EDh,0BEh,8Bh,38h,07h,01h,3Bh,0E8h,8Bh,0CDh
+    db 7Ch,02h,8Bh,0C8h,8Bh,0FBh,33h,0D2h,0F3h,0A6h,74h,05h,1Bh,0D2h,83h,0DAh
+    db 0FFh,85h,0D2h,75h,08h,2Bh,0E8h,8Bh,0D5h,85h,0D2h,74h,59h,8Dh,4Ch,24h
+    db 14h,0C6h,44h,24h,28h,00h,0E8h,55h,10h,6Fh,00h,8Bh,54h,24h,30h,8Bh
+    db 44h,24h,18h,83h,0C2h,18h,89h,54h,24h,30h,8Bh,54h,24h,1Ch,8Bh,4Ah
+    db 28h,40h,3Bh,0C1h,89h,44h,24h,18h,0Fh,8Ch,65h,0FFh,0FFh,0FFh,8Dh,4Ch
+    db 24h,10h,0C7h,44h,24h,28h,0FFh,0FFh,0FFh,0FFh,0E8h,21h,10h,6Fh,00h,5Fh
+    db 5Eh,5Dh,32h,0C0h,5Bh,8Bh,4Ch,24h,10h,64h,89h,0Dh,00h,00h,00h,00h
+    db 83h,0C4h,1Ch,0C2h,04h,00h,8Dh,4Ch,24h,14h,0C6h,44h,24h,28h,00h,0E8h
+    db 0FCh,0Fh,6Fh,00h,8Dh,4Ch,24h,10h,0C7h,44h,24h,28h,0FFh,0FFh,0FFh,0FFh
+    db 0E8h,0EBh,0Fh,6Fh,00h,8Bh,4Ch,24h,20h,5Fh,5Eh,5Dh,0B0h,01h,5Bh,64h
+    db 89h,0Dh,00h,00h,00h,00h,83h,0C4h,1Ch,0C2h,04h,00h
+?isPlayerDefaultTeam@SidesList@@QAE_NPAVTeamsInfo@@@Z ENDP
+_TEXT ENDS
+END
