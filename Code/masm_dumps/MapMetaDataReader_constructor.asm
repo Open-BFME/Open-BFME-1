5734faaf2741a1eb6bb8f64ae12dec8c127d2886
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:09:43 +0000

    Recover MapMetaDataReader constructor

---
+++ Code/masm_dumps/MapMetaDataReader_constructor.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+
+; ??0MapMetaDataReader@@QAE@XZ
+; Exact 334 retail bytes @ 0x000C0EA0.
+_TEXT SEGMENT
+public ??0MapMetaDataReader@@QAE@XZ
+??0MapMetaDataReader@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,68h,8Fh,0FFh,00h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,53h,33h,0DBh,56h,8Bh,0F1h,89h,5Eh,18h,88h
+    db 5Eh,1Ch,88h,5Eh,1Dh,57h,89h,74h,24h,0Ch,89h,5Eh,20h,89h,5Ch,24h
+    db 18h,89h,5Eh,24h,68h,4Ch,36h,41h,00h,68h,93h,6Ch,41h,00h,6Ah,08h
+    db 6Ah,0Ch,8Dh,7Eh,3Ch,57h,0C6h,44h,24h,2Ch,01h,88h,5Eh,28h,89h,5Eh
+    db 34h,89h,5Eh,38h,0E8h,0EBh,5Fh,93h,00h,6Ah,14h,0C6h,44h,24h,1Ch,02h
+    db 89h,9Eh,0A8h,00h,00h,00h,0E8h,35h,0D6h,76h,00h,89h,00h,89h,40h,04h
+    db 89h,86h,0A8h,00h,00h,00h,6Ah,14h,0C6h,44h,24h,20h,03h,89h,9Eh,0ACh
+    db 00h,00h,00h,0E8h,18h,0D6h,76h,00h,89h,00h,89h,40h,04h,83h,0C4h,08h
+    db 89h,86h,0ACh,00h,00h,00h,68h,51h,0F9h,41h,00h,68h,60h,0A7h,43h,00h
+    db 6Ah,08h,6Ah,14h,8Dh,86h,0B0h,00h,00h,00h,50h,0C6h,44h,24h,2Ch,04h
+    db 0E8h,8Fh,5Fh,93h,00h,89h,1Eh,89h,5Eh,04h,89h,5Eh,08h,89h,5Eh,0Ch
+    db 89h,5Eh,10h,89h,5Eh,14h,89h,5Eh,30h,89h,5Eh,2Ch,89h,9Eh,9Ch,00h
+    db 00h,00h,89h,9Eh,0A0h,00h,00h,00h,89h,9Eh,0A4h,00h,00h,00h,89h,1Fh
+    db 89h,5Fh,04h,89h,5Fh,08h,89h,5Eh,48h,89h,5Eh,4Ch,89h,5Eh,50h,89h
+    db 5Eh,54h,89h,5Eh,58h,89h,5Eh,5Ch,89h,5Eh,60h,89h,5Eh,64h,89h,5Eh
+    db 68h,89h,5Eh,6Ch,89h,5Eh,70h,89h,5Eh,74h,89h,5Eh,78h,89h,5Eh,7Ch
+    db 89h,9Eh,80h,00h,00h,00h,89h,9Eh,84h,00h,00h,00h,8Bh,4Ch,24h,10h
+    db 89h,9Eh,88h,00h,00h,00h,89h,9Eh,8Ch,00h,00h,00h,89h,9Eh,90h,00h
+    db 00h,00h,89h,9Eh,94h,00h,00h,00h,89h,9Eh,98h,00h,00h,00h,5Fh,8Bh
+    db 0C6h,5Eh,5Bh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h,0C3h
+??0MapMetaDataReader@@QAE@XZ ENDP
+_TEXT ENDS
+END
