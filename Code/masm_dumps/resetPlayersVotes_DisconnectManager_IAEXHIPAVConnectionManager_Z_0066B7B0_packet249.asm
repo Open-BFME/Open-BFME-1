984a8a16b159dbefe814363db1cd40264b68e354
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:02:34 +0000

    Match DisconnectManager resetPlayersVotes

---
+++ Code/masm_dumps/resetPlayersVotes_DisconnectManager_IAEXHIPAVConnectionManager_Z_0066B7B0_packet249.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?resetPlayersVotes@DisconnectManager@@IAEXHIPAVConnectionManager@@@Z
+; Exact 89 retail bytes @ 0x0066B7B0; certified packet 249
+_TEXT SEGMENT
+public ?resetPlayersVotes@DisconnectManager@@IAEXHIPAVConnectionManager@@@Z
+?resetPlayersVotes@DisconnectManager@@IAEXHIPAVConnectionManager@@@Z PROC
+    db 053h,056h,08Bh,074h,024h,00Ch,057h,08Bh,07Ch,024h,014h,08Dh,044h,0F1h,030h,0BAh
+    db 008h,000h,000h,000h,039h,078h,004h,077h,003h,0C6h,000h,000h,083h,0C0h,040h,04Ah
+    db 075h,0F2h,08Bh,07Ch,024h,018h,057h,056h,0E8h,074h,07Fh,099h,0FFh,08Bh,0CFh,08Bh
+    db 0D8h,0E8h,0ABh,0EAh,09Dh,0FFh,03Bh,0F0h,07Ch,003h,074h,017h,04Eh,083h,0FEh,0FFh
+    db 074h,011h,08Bh,00Dh,064h,049h,02Fh,001h,085h,0C9h,074h,007h,053h,056h,0E8h,0B5h
+    db 065h,09Ch,0FFh,05Fh,05Eh,05Bh,0C2h,00Ch,000h
+?resetPlayersVotes@DisconnectManager@@IAEXHIPAVConnectionManager@@@Z ENDP
+_TEXT ENDS
+END
