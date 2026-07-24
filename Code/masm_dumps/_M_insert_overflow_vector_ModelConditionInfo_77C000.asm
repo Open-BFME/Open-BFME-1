13b97f954176655aa231949ee2cace239e64b7ab
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:03:13 +0000

    Recover ModelConditionInfo vector insert overflow

---
+++ Code/masm_dumps/_M_insert_overflow_vector_ModelConditionInfo_77C000.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ?_M_insert_overflow@?$vector@UModelConditionInfo@@V?$allocator@UModelConditionInfo@@@_STL@@@_STL@@IAEXPAUModelConditionInfo@@ABU3@ABU__false_type@2@I_N@Z
+; Exact 289 retail bytes @ 0x0077C000
+_TEXT SEGMENT
+public ?_M_insert_overflow@?$vector@UModelConditionInfo@@V?$allocator@UModelConditionInfo@@@_STL@@@_STL@@IAEXPAUModelConditionInfo@@ABU3@ABU__false_type@2@I_N@Z
+?_M_insert_overflow@?$vector@UModelConditionInfo@@V?$allocator@UModelConditionInfo@@@_STL@@@_STL@@IAEXPAUModelConditionInfo@@ABU3@ABU__false_type@2@I_N@Z PROC
+    db 083h, 0ECh, 008h, 053h, 08Bh, 0D9h, 08Bh, 013h, 08Bh, 04Bh, 004h, 02Bh, 0CAh, 0B8h, 0A7h, 0C8h
+    db 067h, 0DDh, 0F7h, 0E9h, 003h, 0D1h, 08Bh, 04Ch, 024h, 01Ch, 0C1h, 0FAh, 008h, 08Bh, 0C2h, 0C1h
+    db 0E8h, 01Fh, 003h, 0C2h, 055h, 03Bh, 0C1h, 057h, 089h, 044h, 024h, 00Ch, 08Dh, 04Ch, 024h, 024h
+    db 072h, 004h, 08Dh, 04Ch, 024h, 00Ch, 08Bh, 009h, 003h, 0C8h, 089h, 04Ch, 024h, 010h, 074h, 02Ch
+    db 08Bh, 0C1h, 069h, 0C0h, 028h, 001h, 000h, 000h, 03Dh, 080h, 000h, 000h, 000h, 050h, 076h, 00Eh
+    db 0E8h, 0DBh, 05Eh, 010h, 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 00Ch, 0EBh, 016h, 0E8h, 0DDh
+    db 024h, 00Bh, 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 00Ch, 0EBh, 008h, 0C7h, 044h, 024h, 00Ch
+    db 000h, 000h, 000h, 000h, 08Bh, 06Ch, 024h, 018h, 08Bh, 07Ch, 024h, 00Ch, 056h, 08Bh, 033h, 03Bh
+    db 0F5h, 074h, 01Ah, 056h, 057h, 0E8h, 0ECh, 0D5h, 08Bh, 0FFh, 081h, 0C6h, 028h, 001h, 000h, 000h
+    db 083h, 0C4h, 008h, 081h, 0C7h, 028h, 001h, 000h, 000h, 03Bh, 0F5h, 075h, 0E6h, 08Bh, 074h, 024h
+    db 028h, 083h, 0FEh, 001h, 075h, 016h, 08Bh, 044h, 024h, 020h, 050h, 057h, 0E8h, 0C5h, 0D5h, 08Bh
+    db 0FFh, 083h, 0C4h, 008h, 081h, 0C7h, 028h, 001h, 000h, 000h, 0EBh, 01Fh, 085h, 0F6h, 076h, 01Bh
+    db 08Bh, 06Ch, 024h, 020h, 055h, 057h, 0E8h, 0ABh, 0D5h, 08Bh, 0FFh, 083h, 0C4h, 008h, 081h, 0C7h
+    db 028h, 001h, 000h, 000h, 04Eh, 075h, 0EDh, 08Bh, 06Ch, 024h, 01Ch, 08Ah, 044h, 024h, 02Ch, 084h
+    db 0C0h, 05Eh, 075h, 015h, 08Bh, 053h, 004h, 08Dh, 04Ch, 024h, 028h, 051h, 057h, 052h, 055h, 0E8h
+    db 043h, 078h, 08Ch, 0FFh, 083h, 0C4h, 010h, 08Bh, 0F8h, 08Bh, 0CBh, 0E8h, 004h, 00Eh, 089h, 0FFh
+    db 08Bh, 044h, 024h, 010h, 08Bh, 04Ch, 024h, 00Ch, 069h, 0C0h, 028h, 001h, 000h, 000h, 089h, 07Bh
+    db 004h, 05Fh, 003h, 0C1h, 05Dh, 089h, 00Bh, 089h, 043h, 008h, 05Bh, 083h, 0C4h, 008h, 0C2h, 014h
+    db 000h
+?_M_insert_overflow@?$vector@UModelConditionInfo@@V?$allocator@UModelConditionInfo@@@_STL@@@_STL@@IAEXPAUModelConditionInfo@@ABU3@ABU__false_type@2@I_N@Z ENDP
+_TEXT ENDS
+END
