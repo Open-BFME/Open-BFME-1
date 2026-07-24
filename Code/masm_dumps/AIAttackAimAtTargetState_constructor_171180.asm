28597963bd577e7964909e02d215ad0867879c76
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:49:32 +0000

    Recover AIAttackAimAtTargetState constructor

---
+++ Code/masm_dumps/AIAttackAimAtTargetState_constructor_171180.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+; ??0AIAttackAimAtTargetState@@QAE@PAVStateMachine@@_N1@Z
+; Exact 68B @ 0x00171180; certified packet 210
+_TEXT SEGMENT
+public ??0AIAttackAimAtTargetState@@QAE@PAVStateMachine@@_N1@Z
+??0AIAttackAimAtTargetState@@QAE@PAVStateMachine@@_N1@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 0B4h, 7Ch, 09h, 01h
+    db 0E8h, 2Bh, 7Ah, 71h, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 11h, 24h, 0E9h
+    db 0FFh, 8Ah, 4Ch, 24h, 10h, 8Ah, 54h, 24h, 14h, 32h, 0C0h, 88h, 46h, 25h, 88h, 46h
+    db 26h, 0C7h, 06h, 60h, 7Ch, 09h, 01h, 88h, 4Eh, 24h, 88h, 56h, 27h, 8Bh, 0C6h, 5Eh
+    db 59h, 0C2h, 0Ch, 00h
+??0AIAttackAimAtTargetState@@QAE@PAVStateMachine@@_N1@Z ENDP
+_TEXT ENDS
+END
