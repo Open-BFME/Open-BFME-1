2cd9448708986ed0b5628ff63ca19e65315def8f
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:42:20 +0000

    Recover ControlBarSchemeManager newControlBarScheme

---
+++ Code/masm_dumps/newControlBarScheme_ControlBarSchemeManager_QAEPAVControlBarScheme_VAsciiString_Z_004AEB20_packet343.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?newControlBarScheme@ControlBarSchemeManager@@QAEPAVControlBarScheme@@VAsciiString@@@Z
+; Exact 267 retail bytes @ 0x004AEB20; certified packet 343
+; SHA-256 4a8dcd316fc04c1e5e244a5e7ddeadf4b47ec8100d14e5dc020a449da5586b51
+_TEXT SEGMENT
+public ?newControlBarScheme@ControlBarSchemeManager@@QAEPAVControlBarScheme@@VAsciiString@@@Z
+?newControlBarScheme@ControlBarSchemeManager@@QAEPAVControlBarScheme@@VAsciiString@@@Z PROC
+    db 06Ah,0FFh,068h,0C3h,088h,002h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,057h,08Bh,0F9h,051h,08Dh,044h,024h,020h,089h
+    db 064h,024h,00Ch,08Bh,0CCh,050h,0C7h,044h,024h,01Ch,000h,000h,000h,000h,0E8h,00Dh
+    db 090h,03Dh,000h,08Bh,0CFh,0E8h,0D1h,058h,0B5h,0FFh,08Bh,0F0h,085h,0F6h,074h,01Ch
+    db 08Bh,0CEh,0E8h,06Eh,0F7h,0B7h,0FFh,08Dh,04Ch,024h,01Ch,051h,08Bh,0CEh,0E8h,01Dh
+    db 091h,03Dh,000h,08Bh,0CEh,0E8h,026h,092h,03Dh,000h,0EBh,063h,068h,074h,001h,000h
+    db 000h,0E8h,0AAh,033h,03Dh,000h,083h,0C4h,004h,089h,044h,024h,008h,085h,0C0h,0C6h
+    db 044h,024h,014h,001h,074h,00Bh,08Bh,0C8h,0E8h,0F5h,085h,0B7h,0FFh,08Bh,0F0h,0EBh
+    db 002h,033h,0F6h,085h,0F6h,0C6h,044h,024h,014h,000h,089h,074h,024h,008h,074h,055h
+    db 08Bh,044h,024h,01Ch,085h,0C0h,074h,04Dh,066h,083h,078h,004h,000h,074h,046h,08Dh
+    db 054h,024h,01Ch,052h,08Bh,0CEh,0E8h,0C5h,090h,03Dh,000h,08Bh,0CEh,0E8h,0CEh,091h
+    db 03Dh,000h,08Dh,044h,024h,008h,050h,08Dh,04Fh,00Ch,0E8h,0C4h,0E4h,0B5h,0FFh,08Dh
+    db 04Ch,024h,01Ch,0C7h,044h,024h,014h,0FFh,0FFh,0FFh,0FFh,0E8h,050h,08Dh,03Dh,000h
+    db 08Bh,0C6h,08Bh,04Ch,024h,00Ch,064h,089h,00Dh,000h,000h,000h,000h,05Fh,05Eh,083h
+    db 0C4h,010h,0C2h,004h,000h,08Dh,04Ch,024h,01Ch,0C7h,044h,024h,014h,0FFh,0FFh,0FFh
+    db 0FFh,0E8h,02Ah,08Dh,03Dh,000h,08Bh,04Ch,024h,00Ch,05Fh,033h,0C0h,064h,089h,00Dh
+    db 000h,000h,000h,000h,05Eh,083h,0C4h,010h,0C2h,004h,000h
+?newControlBarScheme@ControlBarSchemeManager@@QAEPAVControlBarScheme@@VAsciiString@@@Z ENDP
+_TEXT ENDS
+END
