a28390374fc1bcac7fa1c4d1022195589c2f7fa3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:03:27 +0000

    Recover CustomMatchPreferences getLastLadderAddr

---
+++ Code/masm_dumps/CustomMatchPreferences_getLastLadderAddr.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?getLastLadderAddr@CustomMatchPreferences@@QAE?AVAsciiString@@XZ
+; Exact 98 retail bytes @ 0x000AC1B0; certified packet 292
+_TEXT SEGMENT
+public ?getLastLadderAddr@CustomMatchPreferences@@QAE?AVAsciiString@@XZ
+?getLastLadderAddr@CustomMatchPreferences@@QAE?AVAsciiString@@XZ PROC
+    db 51h,56h,57h,8Bh,0F1h,68h,5Ch,11h,08h,01h,8Dh,4Ch,24h,0Ch,0C7h,44h
+    db 24h,0Ch,00h,00h,00h,00h,0E8h,0F5h,0C9h,7Dh,00h,8Dh,44h,24h,08h,83h
+    db 0C6h,04h,50h,8Bh,0CEh,0E8h,0D2h,0ECh,0F5h,0FFh,8Dh,4Ch,24h,08h,8Bh,0F8h
+    db 0E8h,5Bh,0B7h,7Dh,00h,3Bh,3Eh,8Bh,74h,24h,10h,8Bh,0CEh,75h,12h,68h
+    db 50h,6Eh,33h,01h,0E8h,67h,0B9h,7Dh,00h,5Fh,8Bh,0C6h,5Eh,59h,0C2h,04h
+    db 00h,83h,0C7h,14h,57h,0E8h,56h,0B9h,7Dh,00h,5Fh,8Bh,0C6h,5Eh,59h,0C2h
+    db 04h,00h
+?getLastLadderAddr@CustomMatchPreferences@@QAE?AVAsciiString@@XZ ENDP
+_TEXT ENDS
+END
