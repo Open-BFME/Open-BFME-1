262819e48b60a1d5bfe2f4e17dd7fb2b76287a51
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:34:59 +0000

    Recover W3DProjectedShadowManager init

---
+++ Code/masm_dumps/init_W3DProjectedShadowManager_QAE_NXZ_007AF630_a1e7261b.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?init@W3DProjectedShadowManager@@QAE_NXZ
+; Exact 156B @ 0x007AF630; certified caller/thunk and constructor-chain identity
+_TEXT SEGMENT
+public ?init@W3DProjectedShadowManager@@QAE_NXZ
+?init@W3DProjectedShadowManager@@QAE_NXZ PROC
+    db 06Ah,0FFh,068h,0C6h,026h,005h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,008h,08Bh,0F1h,0E8h,0E0h,028h,00Dh,000h
+    db 083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h
+    db 000h,074h,009h,08Bh,0C8h,0E8h,0BAh,0D5h,086h,0FFh,0EBh,002h,033h,0C0h,068h,0C0h
+    db 003h,000h,000h,0C7h,044h,024h,014h,0FFh,0FFh,0FFh,0FFh,089h,086h,04Ch,002h,000h
+    db 000h,0E8h,0AAh,028h,00Dh,000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h
+    db 044h,024h,010h,001h,000h,000h,000h,074h,01Ch,08Bh,0C8h,0E8h,0C0h,02Eh,018h,000h
+    db 089h,046h,020h,0B0h,001h,05Eh,08Bh,04Ch,024h,004h,064h,089h,00Dh,000h,000h,000h
+    db 000h,083h,0C4h,010h,0C3h,08Bh,04Ch,024h,008h,033h,0C0h,089h,046h,020h,0B0h,001h
+    db 05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?init@W3DProjectedShadowManager@@QAE_NXZ ENDP
+_TEXT ENDS
+END
