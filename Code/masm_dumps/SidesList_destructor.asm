1ecfa08737d8331a8c0b3a1fa620269a628e0d1c
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:44:43 +0000

    Recover SidesList destructor

---
+++ Code/masm_dumps/SidesList_destructor.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+; ??1SidesList@@UAE@XZ
+; Exact 290B @ 0x0019E640; certified retail recovery
+_TEXT SEGMENT
+public ??1SidesList@@UAE@XZ
+??1SidesList@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0BAh, 7Ah, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 0Ch, 56h, 8Bh, 0F1h, 57h, 89h, 74h, 24h, 0Ch
+    db 0C7h, 06h, 04h, 0C2h, 09h, 01h, 0C7h, 46h, 08h, 0F0h, 0C1h, 09h, 01h, 68h, 39h, 39h
+    db 42h, 00h, 6Ah, 20h, 6Ah, 1Ch, 8Dh, 86h, 6Ch, 06h, 00h, 00h, 50h, 0C7h, 44h, 24h
+    db 2Ch, 08h, 00h, 00h, 00h, 0E8h, 0ECh, 86h, 85h, 00h, 8Dh, 0BEh, 4Ch, 06h, 00h, 00h
+    db 89h, 7Ch, 24h, 10h, 8Dh, 4Fh, 0Ch, 0C6h, 44h, 24h, 1Ch, 09h, 0E8h, 04h, 0F0h, 0E8h
+    db 0FFh, 8Bh, 0CFh, 0C6h, 44h, 24h, 1Ch, 07h, 0E8h, 63h, 0C2h, 0E7h, 0FFh, 8Dh, 0BEh, 30h
+    db 06h, 00h, 00h, 89h, 7Ch, 24h, 10h, 8Dh, 4Fh, 0Ch, 0C6h, 44h, 24h, 1Ch, 0Ah, 0E8h
+    db 0E1h, 0EFh, 0E8h, 0FFh, 8Bh, 0CFh, 0C6h, 44h, 24h, 1Ch, 06h, 0E8h, 40h, 0C2h, 0E7h, 0FFh
+    db 68h, 90h, 55h, 42h, 00h, 6Ah, 20h, 6Ah, 18h, 8Dh, 8Eh, 30h, 03h, 00h, 00h, 51h
+    db 0C6h, 44h, 24h, 2Ch, 05h, 0E8h, 8Ch, 86h, 85h, 00h, 68h, 90h, 55h, 42h, 00h, 6Ah
+    db 20h, 6Ah, 18h, 8Dh, 56h, 2Ch, 52h, 0C6h, 44h, 24h, 2Ch, 04h, 0E8h, 75h, 86h, 85h
+    db 00h, 8Dh, 4Eh, 1Ch, 0C6h, 44h, 24h, 1Ch, 03h, 0E8h, 0FEh, 0CFh, 0E7h, 0FFh, 8Dh, 4Eh
+    db 10h, 0C6h, 44h, 24h, 1Ch, 02h, 0E8h, 0DBh, 0B9h, 0E6h, 0FFh, 8Dh, 7Eh, 0Ch, 8Bh, 0CFh
+    db 0C6h, 44h, 24h, 1Ch, 01h, 0E8h, 0E1h, 0A8h, 0E7h, 0FFh, 8Bh, 3Fh, 85h, 0FFh, 74h, 0Bh
+    db 6Ah, 0Ch, 57h, 0E8h, 0B8h, 0FEh, 68h, 00h, 83h, 0C4h, 08h, 8Bh, 0CEh, 0C7h, 46h, 08h
+    db 44h, 37h, 07h, 01h, 0C7h, 44h, 24h, 1Ch, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0EFh, 32h, 80h
+    db 00h, 8Bh, 4Ch, 24h, 14h, 5Fh, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h
+    db 18h, 0C3h
+??1SidesList@@UAE@XZ ENDP
+_TEXT ENDS
+END
