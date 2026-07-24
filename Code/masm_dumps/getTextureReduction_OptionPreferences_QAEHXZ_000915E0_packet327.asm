deb5582459bb07e8d05f1939863b588240138018
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 11:50:40 +0000

    Recover OptionPreferences texture reduction

---
+++ Code/masm_dumps/getTextureReduction_OptionPreferences_QAEHXZ_000915E0_packet327.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?getTextureReduction@OptionPreferences@@QAEHXZ
+; Exact 139 retail bytes @ 0x000915E0; certified packet 327
+; SHA-256 46ce9cd590baf46d18d10c88e6fa0d5fe8f5f9bd945ad8e9a9dd36e9b56fc7a8
+_TEXT SEGMENT
+public ?getTextureReduction@OptionPreferences@@QAEHXZ
+?getTextureReduction@OptionPreferences@@QAEHXZ PROC
+    db 051h,056h,057h,08Bh,0F1h,068h,0B0h,06Dh,007h,001h,08Dh,04Ch,024h,00Ch,0E8h,0CDh
+    db 075h,07Fh,000h,08Dh,044h,024h,008h,083h,0C6h,004h,050h,08Bh,0CEh,0E8h,0AAh,098h
+    db 0F7h,0FFh,08Dh,04Ch,024h,008h,08Bh,0F8h,0E8h,033h,063h,07Fh,000h,03Bh,03Eh,075h
+    db 02Bh,08Bh,015h,0C8h,0D5h,02Eh,001h,08Bh,042h,068h,0B9h,002h,000h,000h,000h,02Bh
+    db 0C8h,089h,04Ch,024h,008h,0DBh,044h,024h,008h,0D8h,00Dh,0A8h,0FAh,007h,001h,0D8h
+    db 005h,03Ch,053h,007h,001h,0E8h,0FEh,057h,096h,000h,0EBh,019h,08Bh,07Fh,014h,085h
+    db 0FFh,08Dh,047h,008h,075h,005h,0B8h,08Bh,038h,007h,001h,050h,0FFh,015h,084h,093h
+    db 035h,001h,083h,0C4h,004h,085h,0C0h,05Fh,05Eh,07Dh,004h,033h,0C0h,059h,0C3h,083h
+    db 0F8h,064h,07Eh,005h,0B8h,064h,000h,000h,000h,059h,0C3h
+?getTextureReduction@OptionPreferences@@QAEHXZ ENDP
+_TEXT ENDS
+END
