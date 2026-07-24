330e555900eecc6920ba252f2aa1b07f22a24ff9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:15:15 +0000

    Recover ReverseSoundTransition update retail body

---
+++ Code/masm_dumps/ReverseSoundTransition_update_59EED0.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?update@ReverseSoundTransition@@UAEXH@Z
+; Certified retail body @ RVA 0x0059EED0, 52 bytes.
+_TEXT SEGMENT
+public ?update@ReverseSoundTransition@@UAEXH@Z
+?update@ReverseSoundTransition@@UAEXH@Z PROC
+    db 8Bh, 44h, 24h, 04h, 85h, 0C0h, 7Ch, 29h, 83h, 0F8h, 02h, 7Fh, 24h, 83h, 0E8h, 00h
+    db 74h, 14h, 48h, 74h, 03h, 48h, 75h, 19h, 8Ah, 41h, 09h, 84h, 0C0h, 74h, 12h, 0C6h
+    db 41h, 08h, 01h, 0C2h, 04h, 00h, 8Ah, 41h, 09h, 84h, 0C0h, 75h, 04h, 0C6h, 41h, 08h, 01h
+    db 0C2h, 04h, 00h
+?update@ReverseSoundTransition@@UAEXH@Z ENDP
+_TEXT ENDS
+END
