4f2237515e601efab46b8c9b8a6529cb05005117
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:55:02 +0000

    Recover GameWindow destructor MASM

---
+++ Code/masm_dumps/GameWindow_destructor.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+; ??1GameWindow@@MAE@XZ
+; Exact 203B @ 0x00479CD0; certified retail MASM recovery.
+_TEXT SEGMENT
+public ??1GameWindow@@MAE@XZ
+??1GameWindow@@MAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0CBh, 64h, 02h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 89h, 74h, 24h, 04h, 0C7h, 06h, 78h
+    db 77h, 0Fh, 01h, 8Bh, 0Dh, 44h, 33h, 2Fh, 01h, 85h, 0C9h, 0C7h, 44h, 24h, 10h, 00h
+    db 00h, 00h, 00h, 74h, 14h, 8Bh, 01h, 0FFh, 50h, 3Ch, 3Bh, 0C6h, 75h, 0Bh, 8Bh, 0Dh
+    db 44h, 33h, 2Fh, 01h, 8Bh, 11h, 0FFh, 52h, 28h, 8Bh, 86h, 0D8h, 01h, 00h, 00h, 85h
+    db 0C0h, 74h, 09h, 50h, 0E8h, 87h, 81h, 40h, 00h, 83h, 0C4h, 04h, 57h, 8Bh, 0BEh, 14h
+    db 02h, 00h, 00h, 85h, 0FFh, 0C7h, 86h, 0D8h, 01h, 00h, 00h, 00h, 00h, 00h, 00h, 74h
+    db 10h, 8Bh, 0CFh, 0E8h, 0Fh, 14h, 0BBh, 0FFh, 57h, 0E8h, 62h, 81h, 40h, 00h, 83h, 0C4h
+    db 04h, 8Bh, 46h, 04h, 85h, 0C0h, 0C7h, 86h, 14h, 02h, 00h, 00h, 00h, 00h, 00h, 00h
+    db 5Fh, 74h, 18h, 0E8h, 28h, 93h, 48h, 00h, 8Bh, 4Eh, 04h, 85h, 0C9h, 74h, 07h, 8Bh
+    db 01h, 6Ah, 01h, 0FFh, 50h, 14h, 0E8h, 95h, 0BDh, 48h, 00h, 8Dh, 4Eh, 30h, 0C7h, 44h
+    db 24h, 10h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0A6h, 76h, 0BAh, 0FFh, 8Bh, 4Ch, 24h, 08h, 5Eh
+    db 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1GameWindow@@MAE@XZ ENDP
+_TEXT ENDS
+END
