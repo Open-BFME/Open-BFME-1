24f039c93fbcb2e0ae7d8b79a10f4be0568b6601
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:33:04 +0000

    Recover AIMoveAndTightenState onEnter

---
+++ Code/masm_dumps/AIMoveAndTightenState_onEnter_173A80.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+; ?onEnter@AIMoveAndTightenState@@UAE?AW4StateReturnType@@XZ
+; Exact 124B @ 0x00173A80; certified retail MASM recovery
+_TEXT SEGMENT
+public ?onEnter@AIMoveAndTightenState@@UAE?AW4StateReturnType@@XZ
+?onEnter@AIMoveAndTightenState@@UAE?AW4StateReturnType@@XZ PROC
+    db 0A0h, 39h, 02h, 2Fh, 01h, 84h, 0C0h, 53h, 56h, 57h, 8Bh, 0F1h, 74h, 17h, 0A1h, 0FCh
+    db 0D4h, 2Eh, 01h, 85h, 0C0h, 74h, 0Eh, 68h, 48h, 90h, 09h, 01h, 50h, 0E8h, 0D8h, 66h
+    db 0ECh, 0FFh, 83h, 0C4h, 08h, 8Bh, 46h, 1Ch, 0C6h, 46h, 4Ch, 00h, 8Bh, 40h, 10h, 8Bh
+    db 0B8h, 04h, 02h, 00h, 00h, 0B9h, 01h, 00h, 00h, 00h, 89h, 4Eh, 50h, 88h, 4Eh, 54h
+    db 8Bh, 0Dh, 14h, 0F2h, 2Eh, 01h, 8Bh, 49h, 0Ch, 50h, 0E8h, 33h, 22h, 0EAh, 0FFh, 8Bh
+    db 56h, 1Ch, 83h, 0C2h, 24h, 8Bh, 1Ah, 8Dh, 46h, 24h, 8Bh, 0C8h, 89h, 19h, 8Bh, 5Ah
+    db 04h, 89h, 59h, 04h, 8Bh, 52h, 08h, 89h, 51h, 08h, 50h, 8Bh, 0CFh, 0E8h, 0A1h, 22h
+    db 0EAh, 0FFh, 5Fh, 8Bh, 0CEh, 5Eh, 5Bh, 0E9h, 2Bh, 0E3h, 0EAh, 0FFh
+?onEnter@AIMoveAndTightenState@@UAE?AW4StateReturnType@@XZ ENDP
+_TEXT ENDS
+END
