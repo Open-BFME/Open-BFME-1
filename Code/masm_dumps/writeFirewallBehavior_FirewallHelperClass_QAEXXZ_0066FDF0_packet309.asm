bc5d2b38040dc3ac718fccf376456293837e8770
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:55:10 +0000

    Recover FirewallHelper writeFirewallBehavior

---
+++ Code/masm_dumps/writeFirewallBehavior_FirewallHelperClass_QAEXXZ_0066FDF0_packet309.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+
+; ?writeFirewallBehavior@FirewallHelperClass@@QAEXXZ
+; Exact 380 retail bytes @ 0x0066FDF0; certified packet 309
+_TEXT SEGMENT
+public ?writeFirewallBehavior@FirewallHelperClass@@QAEXXZ
+?writeFirewallBehavior@FirewallHelperClass@@QAEXXZ PROC
+    db 06ah,0ffh,068h,0d0h,04ch,004h,001h,064h,0a1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ech,02ch,053h,056h,057h,08dh,04ch,024h,024h,0e8h
+    db 028h,073h,09ch,0ffh,08bh,00dh,0c8h,0d5h,02eh,001h,08bh,035h,0fch,092h,035h,001h
+    db 033h,0dbh,06ah,00ah,08dh,044h,024h,018h,088h,05ch,024h,018h,08bh,091h,014h,00bh
+    db 000h,000h,050h,052h,089h,05ch,024h,04ch,0ffh,0d6h,083h,0c4h,00ch,089h,05ch,024h
+    db 00ch,08dh,044h,024h,014h,0c6h,044h,024h,040h,001h,08dh,078h,001h,08dh,049h,000h
+    db 08ah,008h,040h,03ah,0cbh,075h,0f9h,02bh,0c7h,050h,08dh,044h,024h,018h,050h,08dh
+    db 04ch,024h,014h,0e8h,0b8h,07eh,021h,000h,068h,07ch,0a1h,007h,001h,08dh,04ch,024h
+    db 014h,0e8h,04ah,08dh,021h,000h,08dh,04ch,024h,010h,051h,08dh,04ch,024h,02ch,0c6h
+    db 044h,024h,044h,002h,0e8h,051h,0e8h,09ch,0ffh,08dh,054h,024h,00ch,052h,08bh,0c8h
+    db 0e8h,0fbh,07dh,021h,000h,08dh,04ch,024h,010h,0c6h,044h,024h,040h,001h,0e8h,09dh
+    db 07ah,021h,000h,0a1h,008h,077h,02fh,001h,066h,08bh,040h,00ch,08bh,00dh,0c8h,0d5h
+    db 02eh,001h,066h,089h,081h,020h,00bh,000h,000h,0a1h,0c8h,0d5h,02eh,001h,088h,05ch
+    db 024h,014h,00fh,0bfh,088h,020h,00bh,000h,000h,06ah,00ah,08dh,054h,024h,018h,052h
+    db 051h,0ffh,0d6h,08dh,044h,024h,020h,083h,0c4h,00ch,08dh,070h,001h,08dh,049h,000h
+    db 08ah,008h,040h,03ah,0cbh,075h,0f9h,02bh,0c6h,050h,08dh,054h,024h,018h,052h,08dh
+    db 04ch,024h,014h,0e8h,028h,07eh,021h,000h,068h,03ch,0a1h,007h,001h,08dh,04ch,024h
+    db 014h,0e8h,0bah,08ch,021h,000h,08dh,044h,024h,010h,050h,08dh,04ch,024h,02ch,0c6h
+    db 044h,024h,044h,003h,0e8h,0c1h,0e7h,09ch,0ffh,08dh,04ch,024h,00ch,051h,08bh,0c8h
+    db 0e8h,06bh,07dh,021h,000h,08dh,04ch,024h,010h,0c6h,044h,024h,040h,001h,0e8h,00dh
+    db 07ah,021h,000h,08dh,04ch,024h,024h,0e8h,059h,005h,09ch,0ffh,08dh,04ch,024h,00ch
+    db 088h,05ch,024h,040h,0e8h,0f7h,079h,021h,000h,08dh,04ch,024h,024h,0c7h,044h,024h
+    db 040h,0ffh,0ffh,0ffh,0ffh,0e8h,0adh,013h,099h,0ffh,08bh,04ch,024h,038h,05fh,05eh
+    db 05bh,064h,089h,00dh,000h,000h,000h,000h,083h,0c4h,038h,0c3h
+?writeFirewallBehavior@FirewallHelperClass@@QAEXXZ ENDP
+_TEXT ENDS
+END
