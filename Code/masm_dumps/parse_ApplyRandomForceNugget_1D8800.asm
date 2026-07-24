d91b49f73c02e1eab9e7c70c3b2aa928b43537fc
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:25:11 +0000

    Recover ApplyRandomForceNugget parser

---
+++ Code/masm_dumps/parse_ApplyRandomForceNugget_1D8800.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?parse@ApplyRandomForceNugget@@SAXPAVINI@@PAX1PBX@Z
+; Certified packet 105: exact 64 retail bytes at RVA 0x001D8800.
+_TEXT SEGMENT
+public ?parse@ApplyRandomForceNugget@@SAXPAVINI@@PAX1PBX@Z
+?parse@ApplyRandomForceNugget@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 056h, 06Ah, 014h, 0E8h, 028h, 097h, 06Ah, 000h, 033h, 0F6h, 083h, 0C4h, 004h, 03Bh, 0C6h, 074h
+    db 014h, 089h, 070h, 004h, 089h, 070h, 008h, 089h, 070h, 00Ch, 089h, 070h, 010h, 0C7h, 000h, 0F4h
+    db 0F0h, 009h, 001h, 08Bh, 0F0h, 08Bh, 04Ch, 024h, 008h, 068h, 0D8h, 0F6h, 009h, 001h, 056h, 0E8h
+    db 06Ch, 098h, 067h, 000h, 08Bh, 04Ch, 024h, 00Ch, 056h, 0E8h, 012h, 084h, 0E6h, 0FFh, 05Eh, 0C3h
+?parse@ApplyRandomForceNugget@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END
