70691494d7d5eaa4e262abc9dd8bc6f16b84a224
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:14:27 +0000

    Recover timed bottom-slide reverse animation init

---
+++ Code/masm_dumps/ProcessAnimateWindowSlideFromBottomTimed_initReverseAnimateWindow_496860.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?initReverseAnimateWindow@ProcessAnimateWindowSlideFromBottomTimed@@UAEXPAVAnimateWindow@@I@Z
+; Certified body @ 0x00496860 size 150.
+_TEXT SEGMENT
+public ?initReverseAnimateWindow@ProcessAnimateWindowSlideFromBottomTimed@@UAEXPAVAnimateWindow@@I@Z
+?initReverseAnimateWindow@ProcessAnimateWindowSlideFromBottomTimed@@UAEXPAVAnimateWindow@@I@Z PROC
+    db 83h, 0ECh, 0Ch, 53h, 8Bh, 5Ch, 24h, 14h, 55h, 89h, 4Ch, 24h, 08h, 33h, 0C9h, 3Bh
+    db 0D9h, 56h, 89h, 4Ch, 24h, 10h, 89h, 4Ch, 24h, 14h, 89h, 4Ch, 24h, 10h, 89h, 4Ch
+    db 24h, 14h, 57h, 89h, 4Ch, 24h, 14h, 89h, 4Ch, 24h, 18h, 74h, 5Fh, 8Bh, 43h, 28h
+    db 3Bh, 0C1h, 89h, 44h, 24h, 20h, 74h, 54h, 8Bh, 0Dh, 70h, 12h, 2Fh, 01h, 8Bh, 01h
+    db 8Bh, 73h, 20h, 8Bh, 7Bh, 24h, 0FFh, 50h, 2Ch, 8Bh, 4Ch, 24h, 20h, 57h, 56h, 8Dh
+    db 2Ch, 38h, 0E8h, 0E6h, 2Bh, 0B8h, 0FFh, 0E8h, 28h, 19h, 0BBh, 0FFh, 8Bh, 4Ch, 24h, 10h
+    db 8Bh, 51h, 04h, 03h, 0D0h, 52h, 50h, 83h, 0ECh, 08h, 8Bh, 0C4h, 89h, 64h, 24h, 30h
+    db 57h, 56h, 57h, 56h, 55h, 56h, 57h, 56h, 8Bh, 0CBh, 0C7h, 00h, 00h, 00h, 00h, 00h
+    db 0C7h, 40h, 04h, 00h, 00h, 00h, 00h, 0E8h, 0CDh, 6Ch, 0BAh, 0FFh, 5Fh, 5Eh, 5Dh, 5Bh
+    db 83h, 0C4h, 0Ch, 0C2h, 08h, 00h
+?initReverseAnimateWindow@ProcessAnimateWindowSlideFromBottomTimed@@UAEXPAVAnimateWindow@@I@Z ENDP
+_TEXT ENDS
+END
