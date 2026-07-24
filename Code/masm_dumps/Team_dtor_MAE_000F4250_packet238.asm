082b3023dd4e781dc70b9855e1e75e7c90e234ef
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 07:24:56 +0000

    Recover Team destructor retail body

---
+++ Code/masm_dumps/Team_dtor_MAE_000F4250_packet238.asm
@@ -0,0 +1,42 @@
+.386
+.model flat
+
+; ??1Team@@MAE@XZ
+; Exact 480 retail bytes @ 0x000F4250; certified packet 238
+; SHA-256 b5f02508996b8030029cc75dab2c51a6216c596d95a010d25f3da7df1b37e417
+_TEXT SEGMENT
+public ??1Team@@MAE@XZ
+??1Team@@MAE@XZ PROC
+    db 6Ah,0FFh,68h,6Ah,0BCh,0FFh,00h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,55h,56h,8Bh,0F1h,57h,89h,74h,24h,0Ch,0C7h
+    db 06h,88h,5Fh,08h,01h,8Bh,0Dh,6Ch,07h,2Fh,01h,85h,0C9h,0C7h,44h,24h
+    db 18h,04h,00h,00h,00h,74h,06h,56h,0E8h,83h,0E6h,0F4h,0FFh,8Bh,86h,08h
+    db 01h,00h,00h,85h,0C0h,74h,29h,0EBh,07h,8Dh,0A4h,24h,00h,00h,00h,00h
+    db 8Bh,86h,04h,01h,00h,00h,8Bh,40h,08h,8Bh,48h,10h,6Ah,00h,51h,8Bh
+    db 0CEh,0E8h,0FFh,0A3h,0F1h,0FFh,8Bh,86h,08h,01h,00h,00h,85h,0C0h,75h,0E0h
+    db 8Bh,0Dh,48h,0D7h,2Eh,01h,85h,0C9h,74h,29h,8Bh,41h,10h,33h,0FFh,85h
+    db 0C0h,7Eh,20h,57h,0E8h,57h,0Ch,0F5h,0FFh,85h,0C0h,74h,08h,56h,8Bh,0C8h
+    db 0E8h,0E6h,0Ah,0F2h,0FFh,8Bh,0Dh,48h,0D7h,2Eh,01h,8Bh,41h,10h,47h,3Bh
+    db 0F8h,7Ch,0E0h,8Bh,4Eh,0Ch,85h,0C9h,74h,09h,8Bh,11h,6Ah,00h,0FFh,52h
+    db 50h,0EBh,0F0h,8Bh,4Eh,04h,85h,0C9h,74h,1Ch,39h,0B1h,74h,02h,00h,00h
+    db 74h,0Eh,8Bh,46h,10h,85h,0C0h,75h,07h,8Bh,46h,14h,85h,0C0h,74h,06h
+    db 56h,0E8h,0F0h,0A0h,0F2h,0FFh,8Bh,8Eh,0ECh,00h,00h,00h,85h,0C9h,74h,06h
+    db 8Bh,01h,6Ah,01h,0FFh,10h,8Bh,8Eh,0F0h,00h,00h,00h,85h,0C9h,0C7h,86h
+    db 0ECh,00h,00h,00h,00h,00h,00h,00h,74h,06h,8Bh,11h,6Ah,01h,0FFh,12h
+    db 8Dh,6Eh,1Ch,8Bh,0CDh,0C7h,86h,0F0h,00h,00h,00h,00h,00h,00h,00h,0E8h
+    db 59h,0AEh,0F2h,0FFh,8Bh,86h,00h,01h,00h,00h,8Bh,38h,3Bh,0F8h,74h,19h
+    db 8Bh,0C7h,8Bh,3Fh,6Ah,0Ch,50h,0E8h,74h,0A2h,73h,00h,8Bh,86h,00h,01h
+    db 00h,00h,83h,0C4h,08h,3Bh,0F8h,75h,0E7h,8Bh,86h,00h,01h,00h,00h,89h
+    db 00h,8Bh,86h,00h,01h,00h,00h,8Dh,8Eh,04h,01h,00h,00h,89h,40h,04h
+    db 0C6h,44h,24h,18h,03h,0E8h,84h,0BAh,0F2h,0FFh,8Bh,86h,00h,01h,00h,00h
+    db 8Bh,38h,3Bh,0F8h,0C6h,44h,24h,18h,02h,74h,1Eh,0EBh,03h,8Dh,49h,00h
+    db 8Bh,0C7h,8Bh,3Fh,6Ah,0Ch,50h,0E8h,24h,0A2h,73h,00h,8Bh,86h,00h,01h
+    db 00h,00h,83h,0C4h,08h,3Bh,0F8h,75h,0E7h,8Bh,86h,00h,01h,00h,00h,89h
+    db 00h,8Bh,86h,00h,01h,00h,00h,89h,40h,04h,8Bh,86h,00h,01h,00h,00h
+    db 85h,0C0h,74h,0Bh,6Ah,0Ch,50h,0E8h,0F4h,0A1h,73h,00h,83h,0C4h,08h,8Bh
+    db 0CDh,0C6h,44h,24h,18h,01h,0E8h,0ADh,5Ah,0F5h,0FFh,8Dh,4Eh,18h,0C6h,44h
+    db 24h,18h,00h,0E8h,28h,35h,79h,00h,8Bh,4Ch,24h,10h,5Fh,0C7h,06h,44h
+    db 37h,07h,01h,5Eh,5Dh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h,0C3h
+??1Team@@MAE@XZ ENDP
+_TEXT ENDS
+END
