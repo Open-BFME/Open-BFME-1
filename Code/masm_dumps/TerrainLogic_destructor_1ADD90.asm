2d91465a83024803c9470dd8885ebb1404fecd88
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:05:38 +0000

    Recover TerrainLogic destructor retail body

---
+++ Code/masm_dumps/TerrainLogic_destructor_1ADD90.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ??1TerrainLogic@@UAE@XZ
+; Certified exact retail body @ 0x001ADD90, size 285.
+_TEXT SEGMENT
+public ??1TerrainLogic@@UAE@XZ
+??1TerrainLogic@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0FCh, 85h, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 53h, 56h, 8Bh, 0F1h, 8Dh, 5Eh, 04h, 57h
+    db 89h, 74h, 24h, 10h, 0C7h, 06h, 28h, 0C4h, 09h, 01h, 0C7h, 03h, 0F8h, 0C3h, 09h, 01h
+    db 8Bh, 0CBh, 0C7h, 44h, 24h, 1Ch, 06h, 00h, 00h, 00h, 0E8h, 38h, 0D9h, 0E6h, 0FFh, 8Bh
+    db 0BEh, 5Ch, 05h, 00h, 00h, 85h, 0FFh, 0C6h, 44h, 24h, 1Ch, 05h, 74h, 3Bh, 8Bh, 8Eh
+    db 64h, 05h, 00h, 00h, 2Bh, 0CFh, 0B8h, 0ABh, 0AAh, 0AAh, 2Ah, 0F7h, 0E9h, 0C1h, 0FAh, 03h
+    db 8Bh, 0C2h, 0C1h, 0E8h, 1Fh, 03h, 0C2h, 8Dh, 04h, 40h, 0C1h, 0E0h, 04h, 3Dh, 80h, 00h
+    db 00h, 00h, 76h, 0Bh, 57h, 0E8h, 0A6h, 40h, 6Dh, 00h, 83h, 0C4h, 04h, 0EBh, 0Ah, 50h
+    db 57h, 0E8h, 0DAh, 07h, 68h, 00h, 83h, 0C4h, 08h, 8Dh, 8Eh, 50h, 05h, 00h, 00h, 0C6h
+    db 44h, 24h, 1Ch, 04h, 0E8h, 0D6h, 04h, 0E6h, 0FFh, 8Dh, 7Eh, 48h, 8Bh, 0CFh, 0C6h, 44h
+    db 24h, 1Ch, 03h, 0E8h, 0AFh, 0CCh, 0E5h, 0FFh, 8Bh, 3Fh, 85h, 0FFh, 74h, 0Bh, 6Ah, 0Ch
+    db 57h, 0E8h, 0AAh, 07h, 68h, 00h, 83h, 0C4h, 08h, 8Dh, 4Eh, 40h, 0C6h, 44h, 24h, 1Ch
+    db 02h, 0E8h, 0EAh, 9Ah, 6Dh, 00h, 8Bh, 4Eh, 24h, 85h, 0C9h, 0C6h, 44h, 24h, 1Ch, 01h
+    db 74h, 27h, 8Bh, 46h, 2Ch, 2Bh, 0C1h, 0C1h, 0F8h, 03h, 0C1h, 0E0h, 03h, 3Dh, 80h, 00h
+    db 00h, 00h, 76h, 0Bh, 51h, 0E8h, 36h, 40h, 6Dh, 00h, 83h, 0C4h, 04h, 0EBh, 0Ah, 50h
+    db 51h, 0E8h, 6Ah, 07h, 68h, 00h, 83h, 0C4h, 08h, 8Bh, 0CBh, 0C6h, 44h, 24h, 1Ch, 00h
+    db 0E8h, 0ABh, 3Bh, 7Fh, 00h, 8Bh, 4Ch, 24h, 14h, 5Fh, 0C7h, 06h, 44h, 37h, 07h, 01h
+    db 5Eh, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 14h, 0C3h
+??1TerrainLogic@@UAE@XZ ENDP
+_TEXT ENDS
+END
