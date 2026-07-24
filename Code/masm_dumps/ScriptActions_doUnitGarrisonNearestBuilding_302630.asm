bd1b75e9ad532dc2cd07b4c9abae0fb44d99d875
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:38:30 +0000

    Recover ScriptActions nearest garrison MASM

---
+++ Code/masm_dumps/ScriptActions_doUnitGarrisonNearestBuilding_302630.asm
@@ -0,0 +1,40 @@
+.386
+.model flat
+
+; ?doUnitGarrisonNearestBuilding@ScriptActions@@IAEXABVAsciiString@@@Z
+; Exact 451 retail bytes @ 0x00302630
+_TEXT SEGMENT
+public ?doUnitGarrisonNearestBuilding@ScriptActions@@IAEXABVAsciiString@@@Z
+?doUnitGarrisonNearestBuilding@ScriptActions@@IAEXABVAsciiString@@@Z PROC
+    db 64h,0A1h,00h,00h,00h,00h,8Bh,0Dh,6Ch,07h,2Fh,01h,8Bh,54h,24h,04h
+    db 6Ah,0FFh,68h,98h,6Ch,01h,01h,50h,64h,89h,25h,00h,00h,00h,00h,8Bh
+    db 01h,83h,0ECh,60h,55h,52h,0FFh,50h,68h,8Bh,0E8h,85h,0EDh,0Fh,84h,7Eh
+    db 01h,00h,00h,8Bh,85h,04h,02h,00h,00h,85h,0C0h,89h,44h,24h,04h,0Fh
+    db 84h,6Ch,01h,00h,00h,0C7h,44h,24h,0Ch,00h,00h,00h,00h,0C7h,44h,24h
+    db 08h,0D0h,5Dh,08h,01h,89h,6Ch,24h,10h,33h,0D2h,8Bh,0C2h,89h,54h,24h
+    db 18h,89h,54h,24h,1Ch,8Bh,0CAh,0Dh,80h,00h,00h,00h,81h,0C9h,00h,00h
+    db 20h,00h,89h,44h,24h,14h,68h,0B8h,0D8h,2Eh,01h,8Dh,44h,24h,18h,89h
+    db 54h,24h,28h,89h,4Ch,24h,24h,50h,8Dh,4Ch,24h,34h,0C7h,44h,24h,74h
+    db 00h,00h,00h,00h,89h,54h,24h,30h,0E8h,30h,5Ch,0D3h,0FFh,6Ah,01h,8Dh
+    db 4Ch,24h,0Ch,51h,8Bh,0C8h,0C6h,44h,24h,74h,01h,0E8h,00h,04h,6Fh,00h
+    db 8Bh,0Dh,0B8h,0D5h,2Eh,01h,50h,6Ah,02h,68h,00h,24h,74h,49h,8Dh,55h
+    db 38h,52h,8Dh,84h,24h,88h,00h,00h,00h,50h,0E8h,61h,02h,6Fh,00h,0B8h
+    db 5Ch,3Bh,08h,01h,56h,89h,44h,24h,30h,0C6h,44h,24h,70h,03h,89h,44h
+    db 24h,0Ch,57h,8Bh,44h,24h,7Ch,8Bh,48h,04h,8Bh,50h,0Ch,3Bh,0D1h,74h
+    db 70h,8Bh,0CAh,8Bh,39h,83h,0C1h,08h,85h,0FFh,89h,48h,0Ch,74h,5Eh,8Bh
+    db 0B7h,0FCh,01h,00h,00h,85h,0F6h,74h,0DAh,8Bh,16h,8Bh,0CEh,0FFh,12h,85h
+    db 0C0h,74h,10h,8Bh,06h,8Bh,0CEh,0FFh,10h,8Ah,88h,0B6h,00h,00h,00h,84h
+    db 0C9h,74h,0C0h,8Bh,8Fh,0FCh,01h,00h,00h,8Bh,11h,0FFh,92h,30h,01h,00h
+    db 00h,8Bh,0F0h,66h,85h,0F6h,74h,16h,8Bh,0CDh,0E8h,0B5h,0E0h,0D1h,0FFh,8Bh
+    db 48h,24h,0BAh,01h,00h,00h,00h,0D3h,0E2h,66h,3Bh,0F2h,75h,95h,8Bh,4Ch
+    db 24h,0Ch,6Ah,01h,57h,83h,0C1h,20h,0E8h,14h,1Bh,0D4h,0FFh,8Bh,44h,24h
+    db 7Ch,0FFh,48h,10h,8Bh,44h,24h,7Ch,8Bh,48h,10h,85h,0C9h,0C7h,44h,24h
+    db 74h,0FFh,0FFh,0FFh,0FFh,75h,38h,8Bh,08h,85h,0C9h,8Bh,0F0h,74h,27h,8Bh
+    db 40h,08h,2Bh,0C1h,0C1h,0F8h,03h,0C1h,0E0h,03h,3Dh,80h,00h,00h,00h,76h
+    db 0Bh,51h,0E8h,0E9h,0F6h,57h,00h,83h,0C4h,04h,0EBh,0Ah,50h,51h,0E8h,1Dh
+    db 0BEh,52h,00h,83h,0C4h,08h,56h,0E8h,0D4h,0F6h,57h,00h,83h,0C4h,04h,5Fh
+    db 5Eh,8Bh,4Ch,24h,64h,5Dh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,6Ch
+    db 0C2h,04h,00h
+?doUnitGarrisonNearestBuilding@ScriptActions@@IAEXABVAsciiString@@@Z ENDP
+_TEXT ENDS
+END
