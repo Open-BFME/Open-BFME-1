696b5063b42d409ce3e59ce7d62bb6ad02d6a4cb
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:06:56 +0000

    Recover ScriptActions doGiveMoney

---
+++ Code/masm_dumps/ScriptActions_doGiveMoney.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?doGiveMoney@ScriptActions@@IAEXABVAsciiString@@H@Z
+; Exact 95 retail bytes @ 0x002EF930
+_TEXT SEGMENT
+public ?doGiveMoney@ScriptActions@@IAEXABVAsciiString@@H@Z
+?doGiveMoney@ScriptActions@@IAEXABVAsciiString@@H@Z PROC
+    db 08Bh,044h,024h,004h,08Bh,00Dh,06Ch,007h,02Fh,001h,06Ah,000h,050h,0E8h,04Eh,0B9h
+    db 0D5h,0FFh,066h,085h,0C0h,089h,044h,024h,004h,074h,041h,056h,08Bh,074h,024h,00Ch
+    db 08Dh,04Ch,024h,008h,051h,08Bh,00Dh,048h,0D7h,02Eh,001h,0E8h,000h,0F5h,0D3h,0FFh
+    db 085h,0C0h,074h,01Fh,08Dh,048h,048h,085h,0C9h,074h,018h,085h,0F6h,06Ah,001h,07Dh
+    db 00Ch,08Bh,0D6h,0F7h,0DAh,052h,0E8h,019h,01Fh,0D5h,0FFh,0EBh,006h,056h,0E8h,0EAh
+    db 083h,0D3h,0FFh,066h,083h,07Ch,024h,008h,000h,075h,0C5h,05Eh,0C2h,008h,000h
+?doGiveMoney@ScriptActions@@IAEXABVAsciiString@@H@Z ENDP
+_TEXT ENDS
+END
