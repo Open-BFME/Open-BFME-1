355e74d649071f55c005d48c990350b312f3c1b4
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:04:52 +0000

    Recover AIEnterState constructor

---
+++ Code/masm_dumps/AIEnterState_ctor_17FFF0.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??0AIEnterState@@QAE@PAVStateMachine@@@Z
+; Exact 53 retail bytes @ 0x0017FFF0
+_TEXT SEGMENT
+public ??0AIEnterState@@QAE@PAVStateMachine@@@Z
+??0AIEnterState@@QAE@PAVStateMachine@@@Z PROC
+    db 051h,056h,051h,08Bh,0F1h,089h,064h,024h,008h,08Bh,0CCh,068h,028h,0ACh,009h,001h
+    db 0E8h,0BBh,08Bh,070h,000h,08Bh,044h,024h,010h,050h,08Bh,0CEh,0E8h,071h,021h,0EBh
+    db 0FFh,0C7h,006h,0D0h,0ABh,009h,001h,0C7h,046h,050h,000h,000h,000h,000h,08Bh,0C6h
+    db 05Eh,059h,0C2h,004h,000h
+??0AIEnterState@@QAE@PAVStateMachine@@@Z ENDP
+_TEXT ENDS
+END
