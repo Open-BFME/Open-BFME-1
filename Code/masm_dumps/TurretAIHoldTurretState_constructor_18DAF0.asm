3f0d9bf4cd8e13b52b434c89296954e473baf512
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:26:55 +0000

    Recover TurretAIHoldTurretState constructor

---
+++ Code/masm_dumps/TurretAIHoldTurretState_constructor_18DAF0.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??0TurretAIHoldTurretState@@QAE@PAVTurretStateMachine@@@Z
+; Exact 53 retail bytes @ 0x0018DAF0
+_TEXT SEGMENT
+public ??0TurretAIHoldTurretState@@QAE@PAVTurretStateMachine@@@Z
+??0TurretAIHoldTurretState@@QAE@PAVTurretStateMachine@@@Z PROC
+    db 051h,056h,051h,08Bh,0F1h,089h,064h,024h,008h,08Bh,0CCh,068h,08Ch,0BEh,009h,001h
+    db 0E8h,0BBh,0B0h,06Fh,000h,08Bh,044h,024h,010h,050h,08Bh,0CEh,0E8h,046h,05Fh,0E8h
+    db 0FFh,0C7h,006h,038h,0BEh,009h,001h,0C7h,046h,024h,000h,000h,000h,000h,08Bh,0C6h
+    db 05Eh,059h,0C2h,004h,000h
+??0TurretAIHoldTurretState@@QAE@PAVTurretStateMachine@@@Z ENDP
+_TEXT ENDS
+END
