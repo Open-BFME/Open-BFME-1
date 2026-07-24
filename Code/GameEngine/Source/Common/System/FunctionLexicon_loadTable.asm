ffb930b8b6e25d1ba99812010ef8ab25b4492b5a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 02:50:08 +0000

    Recover FunctionLexicon loadTable

---
+++ Code/GameEngine/Source/Common/System/FunctionLexicon_loadTable.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?loadTable@FunctionLexicon@@IAEXPAUTableEntry@1@W4TableIndex@1@@Z
+; Certified exact 159 retail bytes @ RVA 0x00105570
+_TEXT SEGMENT
+public ?loadTable@FunctionLexicon@@IAEXPAUTableEntry@1@W4TableIndex@1@@Z
+?loadTable@FunctionLexicon@@IAEXPAUTableEntry@1@W4TableIndex@1@@Z PROC
+    db 06Ah,0FFh,068h,098h,0CDh,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,055h,08Bh,06Ch,024h,018h,085h,0EDh,089h,04Ch,024h
+    db 004h,074h,06Ah,08Bh,045h,004h,085h,0C0h,056h,08Dh,075h,004h,074h,052h,08Bh,0FFh
+    db 08Bh,006h,050h,08Dh,04Ch,024h,020h,0E8h,014h,036h,078h,000h,08Bh,044h,024h,01Ch
+    db 085h,0C0h,0C7h,044h,024h,014h,000h,000h,000h,000h,074h,005h,083h,0C0h,008h,0EBh
+    db 005h,0B8h,08Bh,038h,007h,001h,08Bh,00Dh,000h,0D6h,02Eh,001h,050h,0E8h,005h,058h
+    db 0F3h,0FFh,08Dh,04Ch,024h,01Ch,089h,046h,0FCh,0C7h,044h,024h,014h,0FFh,0FFh,0FFh
+    db 0FFh,0E8h,05Ah,023h,078h,000h,08Bh,046h,00Ch,083h,0C6h,00Ch,085h,0C0h,075h,0B0h
+    db 08Bh,044h,024h,020h,08Bh,04Ch,024h,008h,089h,06Ch,081h,008h,05Eh,08Bh,04Ch,024h
+    db 008h,05Dh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C2h,008h,000h
+?loadTable@FunctionLexicon@@IAEXPAUTableEntry@1@W4TableIndex@1@@Z ENDP
+_TEXT ENDS
+END
