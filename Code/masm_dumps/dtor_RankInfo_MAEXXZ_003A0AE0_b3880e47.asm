aa3827863b56fb77dddca0cb301b4ef90d063626
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:25:01 +0000

    Recover RankInfo complete destructor

---
+++ Code/masm_dumps/dtor_RankInfo_MAEXXZ_003A0AE0_b3880e47.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??1RankInfo@@MAE@XZ
+; Exact 146 retail bytes @ 0x003A0AE0; certified packet 122
+_TEXT SEGMENT
+public ??1RankInfo@@MAE@XZ
+??1RankInfo@@MAE@XZ PROC
+    db 6Ah,0FFh,68h,03h,0CEh,01h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh,0F1h,89h,74h,24h,04h,8Bh,4Eh,2Ch
+    db 85h,0C9h,0C7h,44h,24h,10h,01h,00h,00h,00h,74h,27h,8Bh,46h,34h,2Bh
+    db 0C1h,0C1h,0F8h,02h,0C1h,0E0h,02h,3Dh,80h,00h,00h,00h,76h,0Bh,51h,0E8h
+    db 8Ch,13h,4Eh,00h,83h,0C4h,04h,0EBh,0Ah,50h,51h,0E8h,0C0h,0DAh,48h,00h
+    db 83h,0C4h,08h,8Dh,4Eh,0Ch,0C6h,44h,24h,10h,00h,0E8h,90h,76h,4Eh,00h
+    db 8Bh,4Eh,04h,85h,0C9h,0C7h,44h,24h,10h,0FFh,0FFh,0FFh,0FFh,0C7h,06h,0B0h
+    db 0FCh,07h,01h,74h,06h,8Bh,01h,6Ah,01h,0FFh,10h,8Bh,4Ch,24h,08h,0C7h
+    db 46h,04h,00h,00h,00h,00h,5Eh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h
+    db 10h,0C3h
+??1RankInfo@@MAE@XZ ENDP
+_TEXT ENDS
+END
