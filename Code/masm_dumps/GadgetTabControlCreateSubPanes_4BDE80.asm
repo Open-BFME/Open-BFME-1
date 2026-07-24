5a78384d58aaed7759466f2108d353d9d014e319
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:22:20 +0000

    Recover GadgetTabControlCreateSubPanes retail body

---
+++ Code/masm_dumps/GadgetTabControlCreateSubPanes_4BDE80.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+
+; ?GadgetTabControlCreateSubPanes@@YAXPAVGameWindow@@@Z
+; Exact 343 retail bytes @ 0x004BDE80; certified packet 299.
+_TEXT SEGMENT
+public ?GadgetTabControlCreateSubPanes@@YAXPAVGameWindow@@@Z
+?GadgetTabControlCreateSubPanes@@YAXPAVGameWindow@@@Z PROC
+    db 083h, 0ECh, 060h, 053h, 055h, 056h, 08Bh, 074h, 024h, 070h, 057h, 08Bh, 0CEh, 0E8h, 0A6h, 086h
+    db 0B8h, 0FFh, 08Bh, 0F8h, 08Dh, 044h, 024h, 010h, 050h, 08Dh, 04Ch, 024h, 018h, 051h, 08Dh, 054h
+    db 024h, 024h, 052h, 08Dh, 044h, 024h, 02Ch, 050h, 056h, 089h, 07Ch, 024h, 038h, 0E8h, 0A8h, 0C7h
+    db 0B4h, 0FFh, 033h, 0EDh, 083h, 0C4h, 014h, 033h, 0DBh, 089h, 06Ch, 024h, 018h, 083h, 0C7h, 014h
+    db 08Bh, 00Fh, 03Bh, 0CBh, 00Fh, 085h, 0C3h, 000h, 000h, 000h, 08Bh, 04Ch, 024h, 014h, 08Bh, 054h
+    db 024h, 010h, 089h, 04Ch, 024h, 044h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 089h, 054h, 024h, 048h
+    db 08Dh, 054h, 024h, 03Ch, 089h, 05Ch, 024h, 04Ch, 089h, 05Ch, 024h, 050h, 089h, 05Ch, 024h, 054h
+    db 089h, 05Ch, 024h, 05Ch, 089h, 05Ch, 024h, 060h, 089h, 05Ch, 024h, 064h, 089h, 05Ch, 024h, 068h
+    db 089h, 05Ch, 024h, 06Ch, 089h, 074h, 024h, 03Ch, 089h, 05Ch, 024h, 040h, 0C7h, 044h, 024h, 058h
+    db 0ABh, 0F0h, 040h, 000h, 08Bh, 001h, 052h, 0FFh, 050h, 074h, 08Bh, 0C8h, 089h, 007h, 0E8h, 06Eh
+    db 06Ah, 0B7h, 0FFh, 08Bh, 0F0h, 08Bh, 04Eh, 00Ch, 055h, 08Dh, 044h, 024h, 02Ch, 081h, 0C9h, 000h
+    db 040h, 000h, 000h, 068h, 0FCh, 0D8h, 00Fh, 001h, 050h, 089h, 04Eh, 00Ch, 0FFh, 015h, 08Ch, 094h
+    db 035h, 001h, 08Dh, 044h, 024h, 034h, 083h, 0C4h, 00Ch, 08Dh, 068h, 001h, 08Dh, 064h, 024h, 000h
+    db 08Ah, 008h, 040h, 03Ah, 0CBh, 075h, 0F9h, 02Bh, 0C5h, 050h, 08Dh, 04Ch, 024h, 02Ch, 051h, 08Dh
+    db 08Eh, 08Ch, 001h, 000h, 000h, 0E8h, 0B6h, 09Dh, 03Ch, 000h, 08Bh, 04Ch, 024h, 074h, 0E8h, 067h
+    db 05Eh, 0B6h, 0FFh, 08Bh, 00Fh, 0C1h, 0E8h, 003h, 025h, 001h, 0FFh, 0FFh, 0FFh, 050h, 0E8h, 078h
+    db 0C2h, 0B8h, 0FFh, 08Bh, 06Ch, 024h, 018h, 08Bh, 074h, 024h, 074h, 0EBh, 020h, 08Bh, 054h, 024h
+    db 01Ch, 08Bh, 044h, 024h, 020h, 052h, 050h, 0E8h, 010h, 0A3h, 0B8h, 0FFh, 08Bh, 04Ch, 024h, 010h
+    db 08Bh, 054h, 024h, 014h, 051h, 08Bh, 00Fh, 052h, 0E8h, 0F0h, 0B4h, 0B5h, 0FFh, 045h, 083h, 0C7h
+    db 004h, 083h, 0FDh, 008h, 089h, 06Ch, 024h, 018h, 00Fh, 08Ch, 002h, 0FFh, 0FFh, 0FFh, 08Bh, 044h
+    db 024h, 024h, 08Bh, 048h, 040h, 051h, 056h, 0E8h, 06Ah, 0A9h, 0B4h, 0FFh, 083h, 0C4h, 008h, 05Fh
+    db 05Eh, 05Dh, 05Bh, 083h, 0C4h, 060h, 0C3h
+?GadgetTabControlCreateSubPanes@@YAXPAVGameWindow@@@Z ENDP
+_TEXT ENDS
+END
