c868f1e1345eda3c2eaccc64dc63178010088fc3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:58:57 +0000

    Recover GameTextManager initMapStringFile

---
+++ Code/masm_dumps/GameTextManager_initMapStringFile_438D50.asm
@@ -0,0 +1,38 @@
+.386
+.model flat
+
+; ?initMapStringFile@GameTextManager@@UAEXABVAsciiString@@@Z
+; Exact 432 retail bytes @ 0x00438D50.
+_TEXT SEGMENT
+public ?initMapStringFile@GameTextManager@@UAEXABVAsciiString@@@Z
+?initMapStringFile@GameTextManager@@UAEXABVAsciiString@@@Z PROC
+    db 6Ah,0FFh,68h,8Bh,23h,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,53h,55h,56h,8Bh,0F1h,8Bh,86h,2Ch,78h,00h
+    db 00h,33h,0EDh,3Bh,0C5h,57h,8Dh,0BEh,30h,78h,00h,00h,89h,2Fh,74h,0Fh
+    db 50h,0E8h,6Ah,91h,44h,00h,83h,0C4h,04h,89h,0AEh,2Ch,78h,00h,00h,8Bh
+    db 86h,28h,78h,00h,00h,3Bh,0C5h,74h,23h,8Bh,48h,0FCh,8Dh,58h,0FCh,68h
+    db 0Eh,51h,41h,00h,51h,6Ah,08h,50h,0E8h,0C9h,0DFh,5Bh,00h,53h,0E8h,3Dh
+    db 91h,44h,00h,83h,0C4h,04h,89h,0AEh,28h,78h,00h,00h,8Bh,54h,24h,24h
+    db 8Bh,02h,3Bh,0C5h,74h,05h,83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h
+    db 8Bh,0Dh,48h,0CBh,34h,01h,6Ah,21h,50h,0E8h,82h,0FAh,58h,00h,8Bh,0D8h
+    db 3Bh,0DDh,89h,5Ch,24h,24h,0Fh,84h,0FFh,00h,00h,00h,57h,53h,8Bh,0CEh
+    db 0C6h,43h,0Dh,01h,0E8h,0E4h,61h,0BEh,0FFh,39h,2Fh,0Fh,8Eh,0E3h,00h,00h
+    db 00h,8Bh,03h,55h,55h,8Bh,0CBh,0FFh,50h,14h,8Bh,2Fh,8Dh,0Ch,0EDh,04h
+    db 00h,00h,00h,51h,0E8h,57h,91h,44h,00h,83h,0C4h,04h,89h,44h,24h,10h
+    db 85h,0C0h,0C7h,44h,24h,1Ch,00h,00h,00h,00h,74h,20h,68h,0Eh,51h,41h
+    db 00h,68h,04h,66h,40h,00h,55h,8Dh,58h,04h,6Ah,08h,53h,89h,28h,0E8h
+    db 0A0h,0E0h,5Bh,00h,8Bh,0C3h,8Bh,5Ch,24h,24h,0EBh,02h,33h,0C0h,53h,8Bh
+    db 0CEh,0C7h,44h,24h,20h,0FFh,0FFh,0FFh,0FFh,89h,86h,28h,78h,00h,00h,0E8h
+    db 7Dh,49h,0BEh,0FFh,8Bh,17h,0C1h,0E2h,03h,52h,0E8h,01h,91h,44h,00h,8Bh
+    db 2Fh,8Bh,8Eh,28h,78h,00h,00h,83h,0C4h,04h,33h,0D2h,85h,0EDh,89h,86h
+    db 2Ch,78h,00h,00h,7Eh,12h,89h,48h,04h,89h,08h,8Bh,2Fh,83h,0C0h,08h
+    db 83h,0C1h,08h,42h,3Bh,0D5h,7Ch,0EEh,8Bh,0B6h,2Ch,78h,00h,00h,8Bh,07h
+    db 8Dh,3Ch,0C6h,3Bh,0F7h,0C6h,44h,24h,24h,00h,74h,38h,8Bh,0C7h,2Bh,0C6h
+    db 0C1h,0F8h,03h,33h,0C9h,83h,0F8h,01h,74h,0Eh,8Dh,9Bh,00h,00h,00h,00h
+    db 0D1h,0F8h,41h,83h,0F8h,01h,75h,0F8h,8Bh,6Ch,24h,24h,55h,03h,0C9h,51h
+    db 6Ah,00h,57h,56h,0E8h,1Ah,95h,0BFh,0FFh,55h,57h,56h,0E8h,6Eh,0F0h,0BCh
+    db 0FFh,83h,0C4h,20h,8Bh,13h,8Bh,0CBh,0FFh,52h,08h,8Bh,4Ch,24h,14h,5Fh
+    db 5Eh,5Dh,5Bh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h,0C2h,04h,00h
+?initMapStringFile@GameTextManager@@UAEXABVAsciiString@@@Z ENDP
+_TEXT ENDS
+END
