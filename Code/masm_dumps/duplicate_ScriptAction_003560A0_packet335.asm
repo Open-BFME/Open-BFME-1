e56b01c2a8216f9c46f70881d729c884aee9ce8b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:18:35 +0000

    Recover ScriptAction duplicate retail body

---
+++ Code/masm_dumps/duplicate_ScriptAction_003560A0_packet335.asm
@@ -0,0 +1,45 @@
+.386
+.model flat
+
+; ?duplicate@ScriptAction@@QBEPAV1@XZ
+; Exact 532 retail bytes @ 0x003560A0; certified packet 335
+_TEXT SEGMENT
+public ?duplicate@ScriptAction@@QBEPAV1@XZ
+?duplicate@ScriptAction@@QBEPAV1@XZ PROC
+    db 6Ah,0FFh,68h,86h,94h,01h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,10h,53h,55h,56h,57h,6Ah,48h,8Bh,0E9h
+    db 0E8h,6Bh,0BEh,52h,00h,83h,0C4h,04h,89h,44h,24h,1Ch,33h,0FFh,3Bh,0C7h
+    db 89h,7Ch,24h,28h,74h,13h,8Bh,4Dh,04h,51h,8Bh,0C8h,0E8h,35h,59h,0CEh
+    db 0FFh,8Bh,0C8h,89h,4Ch,24h,14h,0EBh,06h,89h,7Ch,24h,14h,8Bh,0CFh,39h
+    db 7Dh,08h,0C7h,44h,24h,28h,0FFh,0FFh,0FFh,0FFh,89h,7Ch,24h,10h,7Eh,7Ah
+    db 8Bh,0C5h,2Bh,0C1h,8Dh,59h,0Ch,89h,44h,24h,18h,0EBh,03h,8Dh,49h,00h
+    db 8Bh,33h,3Bh,0F7h,74h,51h,8Bh,3Ch,18h,8Bh,17h,89h,16h,8Ah,47h,04h
+    db 88h,46h,04h,8Bh,4Fh,08h,89h,4Eh,08h,8Bh,57h,0Ch,8Dh,47h,10h,8Dh
+    db 4Eh,10h,50h,89h,56h,0Ch,0E8h,55h,1Bh,53h,00h,8Dh,4Fh,14h,8Bh,01h
+    db 8Dh,56h,14h,89h,02h,8Bh,41h,04h,89h,42h,04h,8Bh,49h,08h,89h,4Ah
+    db 08h,8Bh,57h,20h,8Bh,4Ch,24h,14h,89h,56h,20h,8Bh,47h,24h,89h,46h
+    db 24h,8Bh,44h,24h,18h,33h,0FFh,8Bh,54h,24h,10h,8Bh,75h,08h,42h,83h
+    db 0C3h,04h,3Bh,0D6h,89h,54h,24h,10h,7Ch,96h,8Bh,5Dh,3Ch,3Bh,0DFh,8Bh
+    db 0E9h,75h,1Dh,5Fh,5Eh,5Dh,8Bh,0C1h,5Bh,8Bh,4Ch,24h,10h,64h,89h,0Dh
+    db 00h,00h,00h,00h,83h,0C4h,1Ch,0C3h,8Bh,6Ch,24h,18h,8Dh,64h,24h,00h
+    db 6Ah,48h,0E8h,89h,0BDh,52h,00h,8Bh,0F0h,83h,0C4h,04h,89h,74h,24h,1Ch
+    db 3Bh,0F7h,0C7h,44h,24h,28h,01h,00h,00h,00h,74h,46h,8Bh,43h,04h,33h
+    db 0C9h,0C7h,06h,0E0h,84h,0Eh,01h,89h,7Eh,08h,89h,7Eh,3Ch,0C6h,46h,40h
+    db 00h,89h,7Eh,44h,89h,4Eh,0Ch,89h,4Eh,10h,89h,4Eh,14h,89h,4Eh,18h
+    db 89h,4Eh,1Ch,89h,4Eh,20h,89h,4Eh,24h,89h,4Eh,28h,89h,4Eh,2Ch,89h
+    db 4Eh,30h,89h,4Eh,34h,89h,4Eh,38h,50h,8Bh,0CEh,0E8h,46h,28h,0CEh,0FFh
+    db 0EBh,02h,33h,0F6h,89h,75h,3Ch,39h,7Bh,08h,0C7h,44h,24h,28h,0FFh,0FFh
+    db 0FFh,0FFh,89h,74h,24h,18h,89h,7Ch,24h,10h,7Eh,76h,8Bh,0C3h,2Bh,0C6h
+    db 8Dh,6Eh,0Ch,89h,44h,24h,1Ch,8Bh,7Dh,00h,85h,0FFh,74h,4Fh,8Bh,34h
+    db 28h,85h,0F6h,74h,48h,8Bh,16h,89h,17h,8Ah,46h,04h,88h,47h,04h,8Bh
+    db 4Eh,08h,89h,4Fh,08h,8Bh,56h,0Ch,8Dh,46h,10h,50h,8Dh,4Fh,10h,89h
+    db 57h,0Ch,0E8h,39h,1Ah,53h,00h,8Dh,4Eh,14h,8Bh,01h,8Dh,57h,14h,89h
+    db 02h,8Bh,41h,04h,89h,42h,04h,8Bh,49h,08h,89h,4Ah,08h,8Bh,56h,20h
+    db 89h,57h,20h,8Bh,46h,24h,89h,47h,24h,8Bh,44h,24h,1Ch,8Bh,4Ch,24h
+    db 10h,8Bh,53h,08h,41h,83h,0C5h,04h,3Bh,0CAh,89h,4Ch,24h,10h,7Ch,97h
+    db 33h,0FFh,8Bh,5Bh,3Ch,3Bh,0DFh,0Fh,85h,0FBh,0FEh,0FFh,0FFh,8Bh,4Ch,24h
+    db 20h,8Bh,44h,24h,14h,5Fh,5Eh,5Dh,5Bh,64h,89h,0Dh,00h,00h,00h,00h
+    db 83h,0C4h,1Ch,0C3h
+?duplicate@ScriptAction@@QBEPAV1@XZ ENDP
+_TEXT ENDS
+END
