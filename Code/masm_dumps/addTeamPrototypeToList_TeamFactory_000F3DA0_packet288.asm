5452a1a8df732ed78339138b699284b72777071b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:50:13 +0000

    Recover TeamFactory addTeamPrototypeToList

---
+++ Code/masm_dumps/addTeamPrototypeToList_TeamFactory_000F3DA0_packet288.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?addTeamPrototypeToList@TeamFactory@@QAEXPAVTeamPrototype@@@Z
+; Exact 122 retail bytes @ 0x000F3DA0; certified packet 288
+; SHA-256 31ac1c7cb17c8994a142f0b44361461a57d0b8569d1b12b480d2d04905b82e85
+_TEXT SEGMENT
+public ?addTeamPrototypeToList@TeamFactory@@QAEXPAVTeamPrototype@@@Z
+?addTeamPrototypeToList@TeamFactory@@QAEXPAVTeamPrototype@@@Z PROC
+    db 83h,0ECh,08h,53h,56h,57h,8Bh,7Ch,24h,18h,8Bh,47h,14h,85h,0C0h,8Bh
+    db 0D9h,74h,05h,83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h,8Bh,0Dh,00h
+    db 0D6h,2Eh,01h,50h,0E8h,0Eh,70h,0F4h,0FFh,8Bh,0F0h,8Bh,47h,10h,85h,0C0h
+    db 74h,05h,83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h,8Bh,0Dh,00h,0D6h
+    db 2Eh,01h,50h,0E8h,0EFh,6Fh,0F4h,0FFh,89h,44h,24h,0Ch,89h,74h,24h,10h
+    db 8Dh,44h,24h,0Ch,8Dh,73h,0Ch,50h,8Bh,0CEh,0E8h,0B7h,0DEh,0F1h,0FFh,3Bh
+    db 06h,75h,0Eh,8Dh,4Ch,24h,0Ch,51h,8Bh,0CEh,0E8h,11h,0FDh,0F0h,0FFh,89h
+    db 38h,5Fh,5Eh,5Bh,83h,0C4h,08h,0C2h,04h,00h
+?addTeamPrototypeToList@TeamFactory@@QAEXPAVTeamPrototype@@@Z ENDP
+_TEXT ENDS
+END
