83fa95a2abcbc8bf48cb1eb1aad0be5ca4dab81a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:11:26 +0000

    Recover PartitionFilterPlayerAffiliation allow

---
+++ Code/masm_dumps/allow_PartitionFilterPlayerAffiliation_001DD140_2078175b.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?allow@PartitionFilterPlayerAffiliation@@MAE_NPAVObject@@@Z
+; Exact 86 retail bytes @ 0x001DD140; certified packet 194
+_TEXT SEGMENT
+public ?allow@PartitionFilterPlayerAffiliation@@MAE_NPAVObject@@@Z
+?allow@PartitionFilterPlayerAffiliation@@MAE_NPAVObject@@@Z PROC
+    db 08Bh,044h,024h,004h,08Bh,080h,03Ch,002h,000h,000h,056h,08Bh,0F1h,08Bh,04Eh,008h
+    db 050h,0E8h,024h,016h,0E6h,0FFh,083h,0E8h,000h,074h,020h,048h,074h,010h,048h,075h
+    db 027h,0F6h,046h,010h,002h,074h,021h,08Ah,046h,00Ch,05Eh,0C2h,004h,000h,0F6h,046h
+    db 010h,008h,074h,014h,08Ah,046h,00Ch,05Eh,0C2h,004h,000h,0F6h,046h,010h,004h,074h
+    db 007h,08Ah,046h,00Ch,05Eh,0C2h,004h,000h,08Ah,04Eh,00Ch,033h,0C0h,084h,0C9h,00Fh
+    db 094h,0C0h,05Eh,0C2h,004h,000h
+?allow@PartitionFilterPlayerAffiliation@@MAE_NPAVObject@@@Z ENDP
+_TEXT ENDS
+END
