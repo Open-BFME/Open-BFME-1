d190c1dfceb393f6f1bd1f4e6105ad690c5ae797
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 16:20:21 -0400

    Open-BFME5 convert saveCameraInReplays to C++

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/OptionsMenu.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/OptionsMenu.cpp
@@ -499,6 +499,17 @@ Bool OptionPreferences::usesSystemMapDir(void)
 
 // ?saveCameraInReplays@OptionPreferences@@QAE_NXZ
 // Body in Code/masm_dumps/_saveCameraInReplays_OptionPreferences_QAE_NXZ_90800.asm (exact 95B retail).
+Bool OptionPreferences::saveCameraInReplays(void)
+{
+	OptionPreferences::const_iterator it = find("SaveCameraInReplays");
+	if (it == end())
+		return TRUE;
+
+	if (stricmp(it->second.str(), "yes") == 0) {
+		return TRUE;
+	}
+	return FALSE;
+}
 
 // ?useCameraInReplays@OptionPreferences@@QAE_NXZ present-unmatched
 Bool OptionPreferences::useCameraInReplays(void)
