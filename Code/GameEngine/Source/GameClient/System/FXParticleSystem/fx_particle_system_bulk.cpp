a30ce2ec840144bc1cc7d50362cb31399819e3af
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 13:01:43 -0400

    Open-BFME5-convert-CylindricalEmissionVelocityModuleTemplate-destructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -17787,60 +17787,8 @@ CylinderEmissionVolumeModuleTemplate::~CylinderEmissionVolumeModuleTemplate()
 }
 
 // ??1CylindricalEmissionVelocityModuleTemplate@FXParticleSystem@@UAE@XZ
-__declspec(naked) CylindricalEmissionVelocityModuleTemplate::~CylindricalEmissionVelocityModuleTemplate()
+CylindricalEmissionVelocityModuleTemplate::~CylindricalEmissionVelocityModuleTemplate()
 {
-    __asm {
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x41
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x74
-        __emit 0x0e
-        __emit 0xc7
-        __emit 0x41
-        __emit 0x04
-        __emit 0xcc
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x01
-        __emit 0x58
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc3
-        __emit 0x33
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x00
-        __emit 0xcc
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x01
-        __emit 0x58
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc3
-    }
 }
 
 // ??1HemisphericalEmissionVelocityModuleTemplate@FXParticleSystem@@UAE@XZ
