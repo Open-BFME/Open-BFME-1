85a4dfea6a81925c2ea6358762b9a11e9eea207e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 16:18:04 -0400

    Open-BFME5 convert usesSystemMapDir to C++

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/OptionsMenu.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/OptionsMenu.cpp
@@ -485,6 +485,17 @@ Real OptionPreferences::getScrollFactor(void)
 
 // ?usesSystemMapDir@OptionPreferences@@QAE_NXZ
 // Body in Code/masm_dumps/_usesSystemMapDir_OptionPreferences_QAE_NXZ_90780.asm (exact 95B retail).
+Bool OptionPreferences::usesSystemMapDir(void)
+{
+	OptionPreferences::const_iterator it = find("UseSystemMapDir");
+	if (it == end())
+		return TRUE;
+
+	if (stricmp(it->second.str(), "yes") == 0) {
+		return TRUE;
+	}
+	return FALSE;
+}
 
 // ?saveCameraInReplays@OptionPreferences@@QAE_NXZ
 // Body in Code/masm_dumps/_saveCameraInReplays_OptionPreferences_QAE_NXZ_90800.asm (exact 95B retail).
