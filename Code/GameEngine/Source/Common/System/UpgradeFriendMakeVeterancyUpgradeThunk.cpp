53d4694e630750d853cbdae62fcffed94df6997f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 09:43:07 -0400

    Open-BFME5 convert veterancy upgrade helper

---
+++ Code/GameEngine/Source/Common/System/UpgradeFriendMakeVeterancyUpgradeThunk.cpp
@@ -0,0 +1,175 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+enum VeterancyLevel
+{
+    VETERANCY_LEVEL_1
+};
+
+class UpgradeTemplate
+{
+public:
+    void friend_makeVeterancyUpgrade(VeterancyLevel);
+};
+
+// ?friend_makeVeterancyUpgrade@UpgradeTemplate@@QAEXW4VeterancyLevel@@@Z
+__declspec(naked) void UpgradeTemplate::friend_makeVeterancyUpgrade(VeterancyLevel)
+{
+    __asm {
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xf8
+        __emit 0xd3
+        __emit 0xff
+        __emit 0x00
+        __emit 0x50
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x56
+        __emit 0x57
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x50
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x1c
+        __emit 0x51
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x04
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x51
+        __emit 0xfd
+        __emit 0xff
+        __emit 0xff
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x08
+        __emit 0x8d
+        __emit 0x7e
+        __emit 0x08
+        __emit 0x50
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x14
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x3b
+        __emit 0xcc
+        __emit 0x77
+        __emit 0x00
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0xda
+        __emit 0xc8
+        __emit 0x77
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x3f
+        __emit 0x85
+        __emit 0xff
+        __emit 0x8d
+        __emit 0x47
+        __emit 0x08
+        __emit 0x75
+        __emit 0x05
+        __emit 0xb8
+        __emit 0x8b
+        __emit 0x38
+        __emit 0x07
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x00
+        __emit 0xd6
+        __emit 0x2e
+        __emit 0x01
+        __emit 0x50
+        __emit 0xe8
+        __emit 0x57
+        __emit 0xfd
+        __emit 0xf2
+        __emit 0xff
+        __emit 0x8d
+        __emit 0x4e
+        __emit 0x10
+        __emit 0x89
+        __emit 0x46
+        __emit 0x0c
+        __emit 0xe8
+        __emit 0xb5
+        __emit 0xc8
+        __emit 0x77
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x18
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x1c
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5f
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5e
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x0c
+        __emit 0xc2
+        __emit 0x04
+        __emit 0x00
+    }
+}
