07ceeb0111ffab23e330669e4ed75e2cc59437b6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 09:24:59 -0400

    Open-BFME5 convert HotKey search

---
+++ Code/GameEngine/Source/GameClient/MessageStream/HotKeyManagerSearchHotKeyThunk.cpp
@@ -0,0 +1,153 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class AsciiString
+{
+};
+
+class HotKeyManager
+{
+public:
+    AsciiString searchHotKey(const AsciiString &);
+};
+
+// ?searchHotKey@HotKeyManager@@QAE?AVAsciiString@@ABV2@@Z
+__declspec(naked) AsciiString HotKeyManager::searchHotKey(const AsciiString &)
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xa1
+        __emit 0x8f
+        __emit 0x03
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xec
+        __emit 0x08
+        __emit 0x56
+        __emit 0x57
+        __emit 0x6a
+        __emit 0x00
+        __emit 0x51
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x2c
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x64
+        __emit 0x24
+        __emit 0x14
+        __emit 0x8b
+        __emit 0xcc
+        __emit 0x50
+        __emit 0xe8
+        __emit 0x59
+        __emit 0x50
+        __emit 0x2d
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x7c
+        __emit 0x14
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x11
+        __emit 0x8d
+        __emit 0x44
+        __emit 0x24
+        __emit 0x2c
+        __emit 0x50
+        __emit 0xff
+        __emit 0x52
+        __emit 0x24
+        __emit 0x8b
+        __emit 0x7c
+        __emit 0x24
+        __emit 0x20
+        __emit 0x50
+        __emit 0x57
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x20
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x1d
+        __emit 0xff
+        __emit 0xa5
+        __emit 0xff
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x24
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x08
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc6
+        __emit 0x44
+        __emit 0x24
+        __emit 0x18
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x8e
+        __emit 0x56
+        __emit 0x2d
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x10
+        __emit 0x8b
+        __emit 0xc7
+        __emit 0x5f
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5e
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x14
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+    }
+}
