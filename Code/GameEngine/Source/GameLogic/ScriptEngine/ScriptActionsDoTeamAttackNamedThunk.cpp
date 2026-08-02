9375d3e6b14aa78cada2a95eb6929cdc79dcd249
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 09:31:19 -0400

    Open-BFME5 convert team attack named

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActionsDoTeamAttackNamedThunk.cpp
@@ -0,0 +1,162 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class AsciiString
+{
+};
+
+class ScriptActions
+{
+protected:
+    void doTeamAttackNamed(const AsciiString &, const AsciiString &);
+};
+
+// ?doTeamAttackNamed@ScriptActions@@IAEXABVAsciiString@@0@Z
+__declspec(naked) void ScriptActions::doTeamAttackNamed(const AsciiString &, const AsciiString &)
+{
+    __asm {
+        __emit 0x51
+        __emit 0x53
+        __emit 0x55
+        __emit 0x8b
+        __emit 0x6c
+        __emit 0x24
+        __emit 0x10
+        __emit 0x56
+        __emit 0x57
+        __emit 0x6a
+        __emit 0x00
+        __emit 0x51
+        __emit 0x89
+        __emit 0x64
+        __emit 0x24
+        __emit 0x18
+        __emit 0x8b
+        __emit 0xcc
+        __emit 0x55
+        __emit 0xe8
+        __emit 0xa8
+        __emit 0xe5
+        __emit 0x58
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x6c
+        __emit 0x07
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x01
+        __emit 0xff
+        __emit 0x50
+        __emit 0x44
+        __emit 0x8b
+        __emit 0xf8
+        __emit 0x85
+        __emit 0xff
+        __emit 0x74
+        __emit 0x5f
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x6c
+        __emit 0x07
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x1c
+        __emit 0x8b
+        __emit 0x11
+        __emit 0x50
+        __emit 0xff
+        __emit 0x52
+        __emit 0x68
+        __emit 0x8b
+        __emit 0xd8
+        __emit 0x85
+        __emit 0xdb
+        __emit 0x74
+        __emit 0x49
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x14
+        __emit 0xf2
+        __emit 0x2e
+        __emit 0x01
+        __emit 0xe8
+        __emit 0x86
+        __emit 0x1f
+        __emit 0xd4
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0x85
+        __emit 0xf6
+        __emit 0x74
+        __emit 0x38
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xe8
+        __emit 0xc9
+        __emit 0x98
+        __emit 0xd2
+        __emit 0xff
+        __emit 0x68
+        __emit 0x08
+        __emit 0x03
+        __emit 0x0d
+        __emit 0x01
+        __emit 0x8b
+        __emit 0xcd
+        __emit 0xe8
+        __emit 0x16
+        __emit 0x1a
+        __emit 0xd5
+        __emit 0xff
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x75
+        __emit 0x0f
+        __emit 0x50
+        __emit 0x68
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0x7f
+        __emit 0x53
+        __emit 0x50
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xe8
+        __emit 0xba
+        __emit 0x86
+        __emit 0xd4
+        __emit 0xff
+        __emit 0x6a
+        __emit 0x01
+        __emit 0x68
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0x7f
+        __emit 0x53
+        __emit 0x6a
+        __emit 0x00
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xe8
+        __emit 0xa9
+        __emit 0x86
+        __emit 0xd4
+        __emit 0xff
+        __emit 0x5f
+        __emit 0x5e
+        __emit 0x5d
+        __emit 0x5b
+        __emit 0x59
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+    }
+}
