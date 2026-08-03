837deee5ae162794f3af065deef55201e9bbf73e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:28:29 -0400

    Open-BFME5 convert DisplayString::getText to clean C++
    Replace naked __emit dump with return of UnicodeString member at +4;
    focused build byte-matches retail virtual getter.

--- Code/GameEngine/Source/GameClient/DisplayStringGetTextThunk.cpp
+++ Code/GameEngine/Source/GameClient/DisplayStringGetTextThunk.cpp
@@ -1,49 +1,25 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: DisplayString::getText returns UnicodeString member at +4.
+
 class UnicodeString
 {
+public:
+	UnicodeString();
+	UnicodeString(const UnicodeString &);
+	~UnicodeString();
 };
 
 class DisplayString
 {
 public:
-    virtual UnicodeString getText();
+	virtual UnicodeString getText();
+private:
+	UnicodeString m_text;
 };
 
-__declspec(naked) UnicodeString DisplayString::getText()
+// ?getText@DisplayString@@UAE?AVUnicodeString@@XZ
+UnicodeString DisplayString::getText()
 {
-    __asm {
-        _emit 051h
-        _emit 056h
-        _emit 08Bh
-        _emit 074h
-        _emit 024h
-        _emit 00Ch
-        _emit 083h
-        _emit 0C1h
-        _emit 004h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 008h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 067h
-        _emit 083h
-        _emit 047h
-        _emit 000h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 059h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	return m_text;
 }
