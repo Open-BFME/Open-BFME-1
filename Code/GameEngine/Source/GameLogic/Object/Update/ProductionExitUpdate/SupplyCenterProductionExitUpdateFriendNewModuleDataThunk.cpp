b8188604abd4915f1ff5a76c29d6d3927118e1d1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:07:01 -0400

    Open-BFME5 convert SupplyCenterProductionExitUpdate factory; repair ledger
    Restore functions.csv after double-header corruption; convert 0x20 ModuleData factory to clean C++.

--- Code/GameEngine/Source/GameLogic/Object/Update/ProductionExitUpdate/SupplyCenterProductionExitUpdateFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/ProductionExitUpdate/SupplyCenterProductionExitUpdateFriendNewModuleDataThunk.cpp
@@ -1,87 +1,45 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
 class INI;
 class ModuleData;
 
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class SupplyCenterProductionExitUpdateModuleData
+{
+public:
+	SupplyCenterProductionExitUpdateModuleData();
+	virtual ~SupplyCenterProductionExitUpdateModuleData();
+
+private:
+	int m_pad;
+	int m0, m1, m2, m3, m4, m5;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char SupplyCenterProductionExitUpdateFieldParse;
+
 class SupplyCenterProductionExitUpdate
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
-// ?friend_newModuleData@SupplyCenterProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *SupplyCenterProductionExitUpdate::friend_newModuleData(INI *)
+SupplyCenterProductionExitUpdateModuleData::SupplyCenterProductionExitUpdateModuleData()
+	: m0(0), m1(0), m2(0), m3(0), m4(0), m5(0)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x20
-        __emit 0xe8
-        __emit 0x88
-        __emit 0xdb
-        __emit 0x75
-        __emit 0x00
-        __emit 0x33
-        __emit 0xd2
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x3b
-        __emit 0xc2
-        __emit 0x74
-        __emit 0x1c
-        __emit 0xc7
-        __emit 0x00
-        __emit 0xa8
-        __emit 0xd5
-        __emit 0x08
-        __emit 0x01
-        __emit 0x89
-        __emit 0x50
-        __emit 0x08
-        __emit 0x89
-        __emit 0x50
-        __emit 0x0c
-        __emit 0x89
-        __emit 0x50
-        __emit 0x10
-        __emit 0x89
-        __emit 0x50
-        __emit 0x14
-        __emit 0x89
-        __emit 0x50
-        __emit 0x18
-        __emit 0x89
-        __emit 0x50
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x3b
-        __emit 0xca
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0xe6
-        __emit 0x53
-        __emit 0x43
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x4e
-        __emit 0xdd
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+}
+
+ModuleData *SupplyCenterProductionExitUpdate::friend_newModuleData(INI *ini)
+{
+	SupplyCenterProductionExitUpdateModuleData *data = new SupplyCenterProductionExitUpdateModuleData;
+	if (ini)
+		ini->initFromINI(data, &SupplyCenterProductionExitUpdateFieldParse);
+	return (ModuleData *)data;
 }
