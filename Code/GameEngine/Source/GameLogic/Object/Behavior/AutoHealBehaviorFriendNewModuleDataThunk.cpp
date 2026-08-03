8e368146ac5284581ff1433efa06d8bb961e8c88
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:59:49 -0400

    Open-BFME5 convert AutoHealBehavior::friend_newModuleData to clean C++
    0xa8 ModuleData factory with pinned ctor and optional initFromINI; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/Object/Behavior/AutoHealBehaviorFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/AutoHealBehaviorFriendNewModuleDataThunk.cpp
@@ -1,71 +1,42 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: AutoHealBehavior::friend_newModuleData factory.
 
 class INI;
 class ModuleData;
 
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class AutoHealBehaviorModuleData
+{
+public:
+	AutoHealBehaviorModuleData();
+	virtual ~AutoHealBehaviorModuleData();
+
+private:
+	unsigned char m_pad[0xa4];
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char AutoHealBehaviorFieldParse;
+
 class AutoHealBehavior
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@AutoHealBehavior@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *AutoHealBehavior::friend_newModuleData(INI *)
+ModuleData *AutoHealBehavior::friend_newModuleData(INI *ini)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x68
-        __emit 0xa8
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xe5
-        __emit 0x80
-        __emit 0x75
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0x31
-        __emit 0xb4
-        __emit 0xee
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0x21
-        __emit 0xaf
-        __emit 0x43
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xbe
-        __emit 0x82
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	AutoHealBehaviorModuleData *data = new AutoHealBehaviorModuleData;
+	if (ini)
+		ini->initFromINI(data, &AutoHealBehaviorFieldParse);
+	return (ModuleData *)data;
 }
