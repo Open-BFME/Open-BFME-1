bb21e3d045ffec4ebc5d8b5c84f9006e82045713
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:25:52 -0400

    Open-BFME5 convert CritterEmitterUpdate factory to C++

--- Code/GameEngine/Source/GameLogic/AI/CritterEmitterUpdateFriendNewModuleInstanceThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/CritterEmitterUpdateFriendNewModuleInstanceThunk.cpp
@@ -6,109 +6,15 @@ class ModuleData;
 
 class CritterEmitterUpdate
 {
+    unsigned char m_storage[0x28];
+
 public:
+    CritterEmitterUpdate(Thing *, const ModuleData *);
     static Module *friend_newModuleInstance(Thing *, const ModuleData *);
 };
 
 // ?friend_newModuleInstance@CritterEmitterUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) Module *CritterEmitterUpdate::friend_newModuleInstance(Thing *, const ModuleData *)
+Module *CritterEmitterUpdate::friend_newModuleInstance(Thing *thing, const ModuleData *moduleData)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x0b;
-        __emit 0x11;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x6a;
-        __emit 0x28;
-        __emit 0xe8;
-        __emit 0xc3;
-        __emit 0x05;
-        __emit 0x76;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x04;
-        __emit 0x24;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x74;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x8b;
-        __emit 0x54;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x51;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xc8;
-        __emit 0xe8;
-        __emit 0xf2;
-        __emit 0xa7;
-        __emit 0xf1;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    return (Module *)new CritterEmitterUpdate(thing, moduleData);
 }
