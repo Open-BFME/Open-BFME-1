fdf17fd28a855b5ab5ce4486da630df42814daeb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:34:27 -0400

    Open-BFME5 convert DarknessSpecialPowerModuleData constructor to clean C++
    Replace naked dump with SpecialPowerModuleData base and 10.0f/zero field init; pin base at 0x15C44; byte-matches retail 38B.

--- Code/GameEngine/Source/Common/RTS/DarknessSpecialPowerModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/Common/RTS/DarknessSpecialPowerModuleDataCtorThunk.cpp
@@ -1,51 +1,32 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class DarknessSpecialPowerModuleData
+// Open-BFME5: DarknessSpecialPowerModuleData ctor.
+// Base size 0x210; float 10.0f at +0x210; zero at +0x214.
+
+class SpecialPowerModuleData
 {
 public:
-    DarknessSpecialPowerModuleData();
+	SpecialPowerModuleData();
+	virtual ~SpecialPowerModuleData();
+
+private:
+	unsigned char m_pad[0x20c];
 };
 
-__declspec(naked) DarknessSpecialPowerModuleData::DarknessSpecialPowerModuleData()
+class DarknessSpecialPowerModuleData : public SpecialPowerModuleData
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 08Ch
-        _emit 0BDh
-        _emit 0DBh
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 0F0h
-        _emit 040h
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 010h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 020h
-        _emit 041h
-        _emit 0C7h
-        _emit 086h
-        _emit 014h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C3h
-    }
+public:
+	DarknessSpecialPowerModuleData();
+	virtual ~DarknessSpecialPowerModuleData();
+
+private:
+	float m_210;
+	unsigned int m_214;
+};
+
+// ??0DarknessSpecialPowerModuleData@@QAE@XZ
+DarknessSpecialPowerModuleData::DarknessSpecialPowerModuleData()
+{
+	m_210 = 10.0f;
+	m_214 = 0;
 }
