7110f5c9f9fdaddc162b841a8e14eeb2aa4a2c96
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:56:02 -0400

    Open-BFME5 convert CastleMemberBehavior constructor to clean C++
    No-arg ctor body (0xFFFFFFFE field init) mapped via object-symbol for retail c3 epilogue; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/Object/Behavior/CastleMemberBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/CastleMemberBehaviorCtorThunk.cpp
@@ -1,75 +1,39 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class Thing;
-class ModuleData;
+// Open-BFME5: CastleMemberBehavior leaf ctor (no-arg body, retail 2-arg claim via object-symbol).
 
 class CastleMemberBehavior
 {
 public:
-    CastleMemberBehavior(Thing *, const ModuleData *);
+	CastleMemberBehavior();
+	virtual ~CastleMemberBehavior();
+
+private:
+	unsigned int m_gap4;
+	unsigned int m_08;
+	unsigned int m_0c;
+	unsigned int m_10;
+	unsigned int m_14;
+	unsigned int m_18;
+	unsigned int m_1c;
+	unsigned int m_20;
+	unsigned int m_24;
+	unsigned char m_28;
+	unsigned char m_29;
 };
 
-__declspec(naked) CastleMemberBehavior::CastleMemberBehavior(Thing *, const ModuleData *)
+// object-symbol=??0CastleMemberBehavior@@QAE@XZ
+CastleMemberBehavior::CastleMemberBehavior()
 {
-    __asm {
-        _emit 08Bh
-        _emit 0C1h
-        _emit 0B9h
-        _emit 0FEh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 089h
-        _emit 048h
-        _emit 008h
-        _emit 089h
-        _emit 048h
-        _emit 00Ch
-        _emit 089h
-        _emit 048h
-        _emit 010h
-        _emit 089h
-        _emit 048h
-        _emit 014h
-        _emit 033h
-        _emit 0C9h
-        _emit 0C7h
-        _emit 000h
-        _emit 0C8h
-        _emit 097h
-        _emit 00Eh
-        _emit 001h
-        _emit 0C7h
-        _emit 040h
-        _emit 018h
-        _emit 00Bh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 040h
-        _emit 01Ch
-        _emit 00Ch
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 040h
-        _emit 020h
-        _emit 00Ah
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 048h
-        _emit 024h
-        _emit 088h
-        _emit 048h
-        _emit 028h
-        _emit 088h
-        _emit 048h
-        _emit 029h
-        _emit 0C3h
-    }
+	unsigned int n = 0xfffffffeu;
+	m_08 = n;
+	m_0c = n;
+	m_10 = n;
+	m_14 = n;
+	m_18 = 0xb;
+	m_1c = 0xc;
+	m_20 = 0xa;
+	m_24 = 0;
+	m_28 = 0;
+	m_29 = 0;
 }
-
