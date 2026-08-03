9402868e85571974e797b354dbe0c5de0dfb22b6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:40:11 -0400

    Open-BFME5 convert ActiveCategoryStackClass destructor to clean C++
    VectorClass Clear shape: free owned Vector then zero VectorMax/IsAllocated.

--- Code/GameEngine/Source/Common/ActiveCategoryStackClassDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/ActiveCategoryStackClassDestructorThunk.cpp
@@ -1,60 +1,30 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class __declspec(novtable) ActiveCategoryStackClass
+// ICF owner for DynamicVectorClass/VectorClass empty-element dtors at 0x00AFF130.
+// Layout matches WW VectorClass: Vector@4, VectorMax@8, IsValid@c, IsAllocated@d.
+
+void __cdecl operator delete(void *);
+
+class ActiveCategoryStackClass
 {
 public:
-    virtual ~ActiveCategoryStackClass();
+	virtual ~ActiveCategoryStackClass();
+
+protected:
+	void *Vector;
+	int VectorMax;
+	bool IsValid;
+	bool IsAllocated;
+	bool pad[2];
 };
 
 // ??1ActiveCategoryStackClass@@UAE@XZ
-__declspec(naked) ActiveCategoryStackClass::~ActiveCategoryStackClass()
+ActiveCategoryStackClass::~ActiveCategoryStackClass()
 {
-    __asm {
-        __emit 0x53
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x04
-        __emit 0x33
-        __emit 0xdb
-        __emit 0x3b
-        __emit 0xc3
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x4c
-        __emit 0xc4
-        __emit 0x1b
-        __emit 0x01
-        __emit 0x74
-        __emit 0x11
-        __emit 0x38
-        __emit 0x5e
-        __emit 0x0d
-        __emit 0x74
-        __emit 0x0c
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x62
-        __emit 0x2d
-        __emit 0xd8
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x04
-        __emit 0x88
-        __emit 0x5e
-        __emit 0x0d
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0xc3
-    }
+	if (Vector && IsAllocated) {
+		operator delete(Vector);
+		Vector = 0;
+	}
+	IsAllocated = false;
+	VectorMax = 0;
 }
