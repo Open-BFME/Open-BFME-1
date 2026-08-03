4396c6248d242253b9dc1a157c2230590c6cc738
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:38:13 -0400

    Open-BFME5 convert StopSpecialPower and HordeContain ModuleData ctors
    Replace naked dumps with base call plus zeroed trailing field; pin
    unique base symbols; focused builds byte-match.

--- Code/GameEngine/Source/GameLogic/Object/SpecialPower/StopSpecialPowerModuleData.cpp
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/StopSpecialPowerModuleData.cpp
@@ -1,42 +1,28 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class StopSpecialPowerModuleData
+// Open-BFME5: StopSpecialPowerModuleData ctor.
+
+class ModuleDataBase_StopSpecialPowerModuleData
 {
 public:
-    StopSpecialPowerModuleData();
+	ModuleDataBase_StopSpecialPowerModuleData();
+	virtual ~ModuleDataBase_StopSpecialPowerModuleData();
+private:
+	unsigned char m_base[524];
 };
 
-__declspec(naked) StopSpecialPowerModuleData::StopSpecialPowerModuleData()
+class StopSpecialPowerModuleData : public ModuleDataBase_StopSpecialPowerModuleData
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 08Ch
-        _emit 0A6h
-        _emit 0DAh
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 0F0h
-        _emit 082h
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 010h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C3h
-    }
+public:
+	StopSpecialPowerModuleData();
+	virtual ~StopSpecialPowerModuleData();
+private:
+	unsigned int m_zero;
+};
+
+// ??0StopSpecialPowerModuleData@@QAE@XZ
+StopSpecialPowerModuleData::StopSpecialPowerModuleData()
+	: ModuleDataBase_StopSpecialPowerModuleData()
+{
+	m_zero = 0;
 }
--- Code/GameEngine/Source/GameLogic/Object/Update/HordeUpdateModuleData.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/HordeUpdateModuleData.cpp
@@ -1,42 +1,28 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class HordeContainModuleData
+// Open-BFME5: HordeContainModuleData ctor.
+
+class ModuleDataBase_HordeContainModuleData
 {
 public:
-    HordeContainModuleData();
+	ModuleDataBase_HordeContainModuleData();
+	virtual ~ModuleDataBase_HordeContainModuleData();
+private:
+	unsigned char m_base[356];
 };
 
-__declspec(naked) HordeContainModuleData::HordeContainModuleData()
+class HordeContainModuleData : public ModuleDataBase_HordeContainModuleData
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 0F8h
-        _emit 03Ah
-        _emit 0E2h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 0F8h
-        _emit 0BCh
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 068h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C3h
-    }
+public:
+	HordeContainModuleData();
+	virtual ~HordeContainModuleData();
+private:
+	unsigned int m_zero;
+};
+
+// ??0HordeContainModuleData@@QAE@XZ
+HordeContainModuleData::HordeContainModuleData()
+	: ModuleDataBase_HordeContainModuleData()
+{
+	m_zero = 0;
 }
