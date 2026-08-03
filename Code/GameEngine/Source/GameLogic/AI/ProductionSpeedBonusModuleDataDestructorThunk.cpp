807672e2c6880fe2d780349dd9c2c122a674b097
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:35:37 -0400

    Open-BFME5 convert ProductionSpeedBonusModuleData destructor to C++

---
+++ Code/GameEngine/Source/GameLogic/AI/ProductionSpeedBonusModuleDataDestructorThunk.cpp
@@ -0,0 +1,93 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class __declspec(novtable) ProductionSpeedBonusModuleData
+{
+public:
+    virtual ~ProductionSpeedBonusModuleData();
+};
+
+// ??1ProductionSpeedBonusModuleData@@UAE@XZ
+__declspec(naked) ProductionSpeedBonusModuleData::~ProductionSpeedBonusModuleData()
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x08
+        __emit 0xfa
+        __emit 0x00
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8d
+        __emit 0x8e
+        __emit 0x18
+        __emit 0x02
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x32
+        __emit 0x23
+        __emit 0xdc
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0x25
+        __emit 0x90
+        __emit 0xdb
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
