7609c777b74e01014450a552f1fe5d1565681b6c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:25:55 -0400

    Open-BFME5 convert ModuleData destructors MASM batch
    Lift CivilianSpawnCollideModuleData, ConvertToCarBombCrateCollideModuleData, ExperienceScalarUpgradeModuleData, FadeAndDieOrnamentUpdateModuleData, FireWeaponWhenDeadBehaviorModuleData into standalone C++ thunks.

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/ExperienceScalarUpgradeModuleDataDestructorThunk.cpp
@@ -0,0 +1,82 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift ModuleData dtor MASM dump to standalone C++ thunk.
+
+class __declspec(novtable) ExperienceScalarUpgradeModuleData
+{
+public:
+    virtual ~ExperienceScalarUpgradeModuleData();
+};
+
+// ??1ExperienceScalarUpgradeModuleData@@UAE@XZ
+__declspec(naked) ExperienceScalarUpgradeModuleData::~ExperienceScalarUpgradeModuleData()
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xa8
+        __emit 0x48
+        __emit 0x01
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8d
+        __emit 0x4e
+        __emit 0x08
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x70
+        __emit 0x67
+        __emit 0xd4
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0xc7
+        __emit 0x06
+        __emit 0x44
+        __emit 0x37
+        __emit 0x07
+        __emit 0x01
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
