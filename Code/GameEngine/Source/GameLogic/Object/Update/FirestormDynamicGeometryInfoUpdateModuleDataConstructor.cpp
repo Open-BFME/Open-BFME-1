fc54cc64b9faf0a2d1ec1da9fc44aa831d9c8fdc
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:55:21 -0400

    Open-BFME5 convert FirestormDynamicGeometryInfoUpdateModuleData constructor to clean C++
    Replace naked dump with base inheritance, field zeros and float defaults (100/10/1); pin base at 0x48A3B; byte-matches retail 59B.

--- Code/GameEngine/Source/GameLogic/Object/Update/FirestormDynamicGeometryInfoUpdateModuleDataConstructor.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/FirestormDynamicGeometryInfoUpdateModuleDataConstructor.cpp
@@ -1,73 +1,44 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /EHsc
 
-class FirestormDynamicGeometryInfoUpdateModuleData
+// Open-BFME5: FirestormDynamicGeometryInfoUpdateModuleData ctor.
+
+class FirestormDynamicGeometryInfoUpdateModuleDataBase
+{
+public:
+	FirestormDynamicGeometryInfoUpdateModuleDataBase();
+	virtual ~FirestormDynamicGeometryInfoUpdateModuleDataBase();
+
+private:
+	unsigned char m_pad[0x60];
+};
+
+class FirestormDynamicGeometryInfoUpdateModuleData : public FirestormDynamicGeometryInfoUpdateModuleDataBase
 {
 public:
 	FirestormDynamicGeometryInfoUpdateModuleData();
+	virtual ~FirestormDynamicGeometryInfoUpdateModuleData();
+
+private:
+	unsigned int m_64;
+	unsigned int m_68;
+	unsigned int m_6c;
+	unsigned int m_70;
+	float m_74;
+	float m_78;
+	float m_7c;
+	unsigned int m_80;
 };
 
-__declspec(naked) FirestormDynamicGeometryInfoUpdateModuleData::FirestormDynamicGeometryInfoUpdateModuleData()
+// ??0FirestormDynamicGeometryInfoUpdateModuleData@@QAE@XZ
+FirestormDynamicGeometryInfoUpdateModuleData::FirestormDynamicGeometryInfoUpdateModuleData()
+	: FirestormDynamicGeometryInfoUpdateModuleDataBase()
 {
-	__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 0E8h
-		_emit 0A3h
-		_emit 0C8h
-		_emit 0D8h
-		_emit 0FFh
-		_emit 033h
-		_emit 0C0h
-		_emit 089h
-		_emit 046h
-		_emit 064h
-		_emit 089h
-		_emit 046h
-		_emit 068h
-		_emit 089h
-		_emit 046h
-		_emit 06Ch
-		_emit 089h
-		_emit 046h
-		_emit 070h
-		_emit 089h
-		_emit 086h
-		_emit 080h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 0C7h
-		_emit 006h
-		_emit 040h
-		_emit 075h
-		_emit 00Ch
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 074h
-		_emit 000h
-		_emit 000h
-		_emit 0C8h
-		_emit 042h
-		_emit 0C7h
-		_emit 046h
-		_emit 078h
-		_emit 000h
-		_emit 000h
-		_emit 020h
-		_emit 041h
-		_emit 0C7h
-		_emit 046h
-		_emit 07Ch
-		_emit 000h
-		_emit 000h
-		_emit 080h
-		_emit 03Fh
-		_emit 08Bh
-		_emit 0C6h
-		_emit 05Eh
-		_emit 0C3h
-	}
+	m_64 = 0;
+	m_68 = 0;
+	m_6c = 0;
+	m_70 = 0;
+	m_80 = 0;
+	m_74 = 100.0f;
+	m_78 = 10.0f;
+	m_7c = 1.0f;
 }
-
