176a9836594a59a9ac7facf1216d071f52ba8e68
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:43:09 -0400

    Open-BFME5 convert GiveUpgradeUpdateModuleData destructor MASM

---
+++ Code/GameEngine/Source/GameLogic/AI/GiveUpgradeUpdateModuleDataDestructorThunk.cpp
@@ -0,0 +1,135 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift the exact retail MASM destructor into a C++ thunk.
+
+class __declspec(novtable) GiveUpgradeUpdateModuleData
+{
+public:
+    virtual ~GiveUpgradeUpdateModuleData();
+};
+
+// ??1GiveUpgradeUpdateModuleData@@UAE@XZ
+__declspec(naked) GiveUpgradeUpdateModuleData::~GiveUpgradeUpdateModuleData()
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x88
+        __emit 0xf3
+        __emit 0x00
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8b
+        __emit 0x8e
+        __emit 0x54
+        __emit 0x02
+        __emit 0x00
+        __emit 0x00
+        __emit 0x85
+        __emit 0xc9
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x74
+        __emit 0x2a
+        __emit 0x8b
+        __emit 0x86
+        __emit 0x5c
+        __emit 0x02
+        __emit 0x00
+        __emit 0x00
+        __emit 0x2b
+        __emit 0xc1
+        __emit 0xc1
+        __emit 0xf8
+        __emit 0x02
+        __emit 0xc1
+        __emit 0xe0
+        __emit 0x02
+        __emit 0x3d
+        __emit 0x80
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x76
+        __emit 0x0b
+        __emit 0x51
+        __emit 0xe8
+        __emit 0xd6
+        __emit 0x3f
+        __emit 0x62
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x04
+        __emit 0xeb
+        __emit 0x0a
+        __emit 0x50
+        __emit 0x51
+        __emit 0xe8
+        __emit 0x0a
+        __emit 0x07
+        __emit 0x5d
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x08
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0x15
+        __emit 0xb9
+        __emit 0xdb
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
