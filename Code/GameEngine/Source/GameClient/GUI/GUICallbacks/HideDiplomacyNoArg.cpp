fde0c85f1859af751a2f068dba256b5ae662a927
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:43:01 -0400

    Open-BFME5 convert HideDiplomacy to C++

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideDiplomacyNoArg.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideDiplomacyNoArg.cpp
@@ -1,55 +1,42 @@
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 
-__declspec(naked) void HideDiplomacy()
+class DiplomacyWindow
 {
-	__asm {
-		_emit 0A1h
-		_emit 0E4h
-		_emit 049h
-		_emit 02Fh
-		_emit 001h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 025h
-		_emit 08Ah
-		_emit 088h
-		_emit 054h
-		_emit 002h
-		_emit 000h
-		_emit 000h
-		_emit 084h
-		_emit 0C9h
-		_emit 075h
-		_emit 01Bh
-		_emit 0B1h
-		_emit 001h
-		_emit 088h
-		_emit 088h
-		_emit 054h
-		_emit 002h
-		_emit 000h
-		_emit 000h
-		_emit 0A1h
-		_emit 058h
-		_emit 04Bh
-		_emit 02Fh
-		_emit 001h
-		_emit 088h
-		_emit 048h
-		_emit 050h
-		_emit 08Bh
-		_emit 00Dh
-		_emit 0E8h
-		_emit 019h
-		_emit 02Fh
-		_emit 001h
-		_emit 0E9h
-		_emit 0C4h
-		_emit 0E2h
-		_emit 0AFh
-		_emit 0FFh
-		_emit 0C3h
-	}
-}
+public:
+    char m_pad[0x254];
+    unsigned char m_visible;
+};
 
+class ControlBar
+{
+public:
+    char m_pad[0x50];
+    unsigned char m_field50;
+};
+
+class WindowManager
+{
+public:
+    void hideQuitMenu();
+};
+
+extern DiplomacyWindow *g_diplomacyWindow;
+extern ControlBar *g_controlBar;
+extern WindowManager *g_theWindowManager;
+
+// ?HideDiplomacy@@YAXXZ
+void HideDiplomacy()
+{
+    DiplomacyWindow *window = g_diplomacyWindow;
+    if (window)
+    {
+        unsigned char visible = window->m_visible;
+        if (!visible)
+        {
+            visible = 1;
+            window->m_visible = visible;
+            g_controlBar->m_field50 = visible;
+            g_theWindowManager->hideQuitMenu();
+        }
+    }
+}
