6a42def0fc5bd30891b13dbf91b804185649360e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:37:42 -0400

    Open-BFME5 convert W3DBufferManager::getNextVertexBuffer to clean C++

--- Code/Libraries/Source/WWVegas/WW3D2/W3DBufferManagerVertexBuffer.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/W3DBufferManagerVertexBuffer.cpp
@@ -1,51 +1,33 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /EHsc
+
+// Open-BFME5: W3DBufferManager::getNextVertexBuffer
 
 class W3DBufferManager
 {
 public:
-    struct W3DVertexBuffer
-    {
-    };
+	struct W3DVertexBuffer
+	{
+		char pad[0x10];
+		W3DVertexBuffer *next;
+	};
 
-    enum VBM_FVF_TYPES
-    {
-        VBM_FVF_INVALID = 0
-    };
+	enum VBM_FVF_TYPES
+	{
+		VBM_FVF_INVALID = 0
+	};
 
-    W3DVertexBuffer *getNextVertexBuffer(W3DVertexBuffer *buffer, VBM_FVF_TYPES type);
+	W3DVertexBuffer *getNextVertexBuffer(W3DVertexBuffer *buffer, VBM_FVF_TYPES type);
+
+private:
+	char m_pad[0x9000];
+	W3DVertexBuffer *m_heads[1];
 };
 
-__declspec(naked) W3DBufferManager::W3DVertexBuffer *W3DBufferManager::getNextVertexBuffer(W3DVertexBuffer *, VBM_FVF_TYPES)
+// ?getNextVertexBuffer@W3DBufferManager@@QAEPAUW3DVertexBuffer@1@PAU21@W4VBM_FVF_TYPES@1@@Z
+W3DBufferManager::W3DVertexBuffer *W3DBufferManager::getNextVertexBuffer(
+	W3DVertexBuffer *buffer, VBM_FVF_TYPES type)
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 004h
-        _emit 085h
-        _emit 0C0h
-        _emit 075h
-        _emit 00Eh
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 008h
-        _emit 08Bh
-        _emit 084h
-        _emit 081h
-        _emit 000h
-        _emit 090h
-        _emit 000h
-        _emit 000h
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-        _emit 08Bh
-        _emit 040h
-        _emit 010h
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
+	if (!buffer)
+		return m_heads[type];
+	return buffer->next;
 }
