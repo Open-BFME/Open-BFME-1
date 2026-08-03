a3ef2d848aac03de5847f38e5ac36b27c7412dd0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:48:26 -0400

    Open-BFME5 convert BridgeTowerBehavior friend_newModuleData to clean C++
    8-byte ModuleData factory with optional INI::initFromINI field parse.

--- Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeTowerBehaviorFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeTowerBehaviorFriendNewModuleDataThunk.cpp
@@ -1,67 +1,45 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
 class INI;
 class ModuleData;
 
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class BridgeTowerBehaviorModuleData
+{
+public:
+	BridgeTowerBehaviorModuleData();
+	virtual ~BridgeTowerBehaviorModuleData();
+
+private:
+	int m_pad; // force 8-byte allocation like retail
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+// Retail field-parse table address
+extern "C" char BridgeTowerBehaviorFieldParse;
+
 class BridgeTowerBehavior
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
-// ?friend_newModuleData@BridgeTowerBehavior@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *BridgeTowerBehavior::friend_newModuleData(INI *)
+BridgeTowerBehaviorModuleData::BridgeTowerBehaviorModuleData()
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x08
-        __emit 0xe8
-        __emit 0x08
-        __emit 0xcd
-        __emit 0x75
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0a
-        __emit 0xc7
-        __emit 0x00
-        __emit 0xe0
-        __emit 0xad
-        __emit 0x08
-        __emit 0x01
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0x72
-        __emit 0x9a
-        __emit 0x43
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xe2
-        __emit 0xce
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+}
+
+// ?friend_newModuleData@BridgeTowerBehavior@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *BridgeTowerBehavior::friend_newModuleData(INI *ini)
+{
+	BridgeTowerBehaviorModuleData *data = new BridgeTowerBehaviorModuleData;
+	if (ini)
+		ini->initFromINI(data, &BridgeTowerBehaviorFieldParse);
+	return (ModuleData *)data;
 }
