1ff8644e92b9c2dbd1f4ffd66117471ad71237f6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:23:57 -0400

    Open-BFME5 convert BannerCarrierUpdate factory to C++

--- Code/GameEngine/Source/GameLogic/AI/BannerCarrierUpdateFriendNewModuleInstanceThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/BannerCarrierUpdateFriendNewModuleInstanceThunk.cpp
@@ -1,114 +1,28 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class Module;
 class Thing;
 class ModuleData;
 
-class BannerCarrierUpdate
+class Module
 {
 public:
+	virtual ~Module();
+};
+
+class BannerCarrierUpdate : public Module
+{
+public:
+	BannerCarrierUpdate(Thing *, const ModuleData *);
+	virtual ~BannerCarrierUpdate();
+
     static Module *friend_newModuleInstance(Thing *, const ModuleData *);
+
+private:
+	unsigned char m_data[0x1c];
 };
 
 // ?friend_newModuleInstance@BannerCarrierUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) Module *BannerCarrierUpdate::friend_newModuleInstance(Thing *, const ModuleData *)
+Module *BannerCarrierUpdate::friend_newModuleInstance(Thing *thing, const ModuleData *moduleData)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x0b;
-        __emit 0xf5;
-        __emit 0xff;
-        __emit 0x00;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x6a;
-        __emit 0x20;
-        __emit 0xe8;
-        __emit 0x53;
-        __emit 0x83;
-        __emit 0x76;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x04;
-        __emit 0x24;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x74;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x8b;
-        __emit 0x54;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x51;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xc8;
-        __emit 0xe8;
-        __emit 0x36;
-        __emit 0x3f;
-        __emit 0xf2;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    return new BannerCarrierUpdate(thing, moduleData);
 }
