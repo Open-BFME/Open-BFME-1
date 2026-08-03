d573268e229a8aeb19ee72cf27ec47257af5c3cd
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:58:00 -0400

    Open-BFME5 convert SpawnPointProductionExitUpdate friend_newModuleData to clean C++
    Replace naked factory dump with new ModuleData + optional INI::initFromINI; pin field parse table; byte-matches retail 57B.

--- Code/GameEngine/Source/GameLogic/Object/Update/ProductionExitUpdate/SpawnPointProductionExitUpdateFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/ProductionExitUpdate/SpawnPointProductionExitUpdateFriendNewModuleDataThunk.cpp
@@ -1,74 +1,46 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class INI;
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class SpawnPointProductionExitUpdateModuleData
+{
+public:
+	SpawnPointProductionExitUpdateModuleData();
+	virtual ~SpawnPointProductionExitUpdateModuleData();
+
+private:
+	int m_tag;
+	int m_field8;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char SpawnPointProductionExitUpdateFieldParse;
 
 class SpawnPointProductionExitUpdate
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
-// ?friend_newModuleData@SpawnPointProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *SpawnPointProductionExitUpdate::friend_newModuleData(INI *)
+// ??0SpawnPointProductionExitUpdateModuleData@@QAE@XZ
+SpawnPointProductionExitUpdateModuleData::SpawnPointProductionExitUpdateModuleData()
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x0c
-        __emit 0xe8
-        __emit 0xb8
-        __emit 0xb2
-        __emit 0x75
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x11
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x88
-        __emit 0xe7
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x08
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0x79
-        __emit 0x29
-        __emit 0x44
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x8b
-        __emit 0xb4
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	m_field8 = 0;
+}
+
+// ?friend_newModuleData@SpawnPointProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *SpawnPointProductionExitUpdate::friend_newModuleData(INI *ini)
+{
+	SpawnPointProductionExitUpdateModuleData *data = new SpawnPointProductionExitUpdateModuleData;
+	if (ini)
+		ini->initFromINI(data, &SpawnPointProductionExitUpdateFieldParse);
+	return (ModuleData *)data;
 }
