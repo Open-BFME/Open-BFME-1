f06dec867c1ae1a3f7fbe67f03842ff02dee0b89
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:29:54 -0400

    Open-BFME5 convert FellBeastSwoopPowerModuleData constructor to clean C++
    Replace naked dump with SpecialPowerModuleData base call and two zeroed dwords at +0x254/+0x258; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/Object/Update/FellBeastSwoopPowerModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/FellBeastSwoopPowerModuleDataCtorThunk.cpp
@@ -1,45 +1,33 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class FellBeastSwoopPowerModuleData
+// Open-BFME5: FellBeastSwoopPowerModuleData ctor - base size 0x254, then two dwords.
+
+class SpecialPowerModuleData
 {
 public:
-    FellBeastSwoopPowerModuleData();
+	SpecialPowerModuleData();
+	virtual ~SpecialPowerModuleData();
+
+private:
+	// vptr (4) + 0x250 = 0x254 total base
+	unsigned char m_base[0x250];
 };
 
-__declspec(naked) FellBeastSwoopPowerModuleData::FellBeastSwoopPowerModuleData()
+class FellBeastSwoopPowerModuleData : public SpecialPowerModuleData
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 0EBh
-        _emit 025h
-        _emit 0DEh
-        _emit 0FFh
-        _emit 033h
-        _emit 0C0h
-        _emit 089h
-        _emit 086h
-        _emit 054h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 086h
-        _emit 058h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 006h
-        _emit 090h
-        _emit 04Dh
-        _emit 00Bh
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C3h
-    }
+public:
+	FellBeastSwoopPowerModuleData();
+	virtual ~FellBeastSwoopPowerModuleData();
+
+private:
+	unsigned int m_field254;
+	unsigned int m_field258;
+};
+
+// ??0FellBeastSwoopPowerModuleData@@QAE@XZ
+FellBeastSwoopPowerModuleData::FellBeastSwoopPowerModuleData()
+	: SpecialPowerModuleData()
+{
+	m_field254 = 0;
+	m_field258 = 0;
 }
