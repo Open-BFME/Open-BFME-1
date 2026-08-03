ba72f394f095ee5e9bd5db9bb424f6f40f1e280d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:51:48 -0400

    Open-BFME5 convert ShareBufferClass unsigned short destructor to clean C++

--- Code/Libraries/Source/WWVegas/WWLib/ShareBufferUnsignedShort.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ShareBufferUnsignedShort.cpp
@@ -1,43 +1,30 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
+// Open-BFME5: ShareBufferClass<unsigned short> destructor
+// Load buffer, install interim vtbl, free buffer, install final vtbl.
+
+extern "C" char ShareBufferClassUnsignedShort_vtbl_interim;
+extern "C" char ShareBufferClassUnsignedShort_vtbl_final;
+
+void __cdecl operator_delete_sized(void *p);
+
+// object-symbol used by ledger: ShareBufferClassUnsignedShort dtor (QAE)
 class ShareBufferClassUnsignedShort
 {
 public:
-    ~ShareBufferClassUnsignedShort();
+	~ShareBufferClassUnsignedShort();
+
+private:
+	char *m_vtbl;
+	unsigned int m_04;
+	void *m_buf;
 };
 
-__declspec(naked) ShareBufferClassUnsignedShort::~ShareBufferClassUnsignedShort()
+// ??1?$ShareBufferClass@G@@UAE@XZ
+ShareBufferClassUnsignedShort::~ShareBufferClassUnsignedShort()
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 046h
-        _emit 008h
-        _emit 050h
-        _emit 0C7h
-        _emit 006h
-        _emit 010h
-        _emit 0C1h
-        _emit 013h
-        _emit 001h
-        _emit 0E8h
-        _emit 00Eh
-        _emit 0DEh
-        _emit 0F5h
-        _emit 0FFh
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 0C7h
-        _emit 006h
-        _emit 0ACh
-        _emit 035h
-        _emit 011h
-        _emit 001h
-        _emit 05Eh
-        _emit 0C3h
-    }
+	void *buf = m_buf;
+	m_vtbl = &ShareBufferClassUnsignedShort_vtbl_interim;
+	operator_delete_sized(buf);
+	m_vtbl = &ShareBufferClassUnsignedShort_vtbl_final;
 }
