24417d1a8932ae626e35d9d097053414d49bc1b6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:32:02 -0400

    Open-BFME5 convert WindModuleInfo constructor to clean C++
    Replace naked float/global field init with MSVC 7.1 clean C++.

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/WindModuleInfoCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/WindModuleInfoCtorThunk.cpp
@@ -1,131 +1,59 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
 namespace FXParticleSystem
 {
 
+// globals referenced by retail ctor
+extern void *g_windGlobalA; // 0x012F7004
+extern void *g_windGlobalB; // 0x012B8E00
+
 class WindModuleInfo
 {
 public:
-    WindModuleInfo();
+	WindModuleInfo();
+	virtual void dummy();
+
+private:
+	int field04;
+	float field08;
+	float field0c;
+	float field10;
+	int field14;
+	float field18;
+	float field1c;
+	float field20;
+	void *field24;
+	int field28;
+	float field2c;
+	void *field30;
+	float field34;
+	float field38;
+	unsigned char field3c;
+	unsigned char pad3d[3];
+	int field40;
+	int field44;
 };
 
 // ??0WindModuleInfo@FXParticleSystem@@QAE@XZ
-__declspec(naked) WindModuleInfo::WindModuleInfo()
+WindModuleInfo::WindModuleInfo()
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0xc1;
-        __emit 0x33;
-        __emit 0xc9;
-        __emit 0xba;
-        __emit 0x9a;
-        __emit 0x99;
-        __emit 0x19;
-        __emit 0x3e;
-        __emit 0xc7;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xfa;
-        __emit 0x10;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x04;
-        __emit 0x01;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x08;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x40;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x0c;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x96;
-        __emit 0x42;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x10;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x48;
-        __emit 0x43;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x50;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x50;
-        __emit 0x1c;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x20;
-        __emit 0x66;
-        __emit 0x66;
-        __emit 0xe6;
-        __emit 0x3e;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x28;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x2c;
-        __emit 0xdb;
-        __emit 0x0f;
-        __emit 0x49;
-        __emit 0x3f;
-        __emit 0x8b;
-        __emit 0x15;
-        __emit 0x04;
-        __emit 0x70;
-        __emit 0x2f;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x50;
-        __emit 0x24;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x34;
-        __emit 0xe0;
-        __emit 0xed;
-        __emit 0xaf;
-        __emit 0x40;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x38;
-        __emit 0xdb;
-        __emit 0x0f;
-        __emit 0xc9;
-        __emit 0x40;
-        __emit 0x8b;
-        __emit 0x15;
-        __emit 0x00;
-        __emit 0x8e;
-        __emit 0x2b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x50;
-        __emit 0x30;
-        __emit 0xc6;
-        __emit 0x40;
-        __emit 0x3c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x40;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x44;
-        __emit 0xc3;
-    }
+	field04 = 1;
+	field08 = 2.0f;
+	field0c = 75.0f;
+	field10 = 200.0f;
+	field14 = 0;
+	field18 = 0.15f;
+	field1c = 0.15f;
+	field20 = 0.45f;
+	field28 = 0;
+	field2c = 0.7853981852531433f;
+	field24 = g_windGlobalA;
+	field34 = 5.4977874755859375f;
+	field38 = 6.2831854820251465f;
+	field30 = g_windGlobalB;
+	field3c = 1;
+	field40 = 0;
+	field44 = 0;
 }
 
 }
