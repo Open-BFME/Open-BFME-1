1ddc2a591b1b6fd57b5b860e84cac78c2b59f053
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:48:20 -0400

    Open-BFME5 convert RainOfFireUpdateModuleData constructor to clean C++
    Replace naked leaf dump with field defaults (0x96/0x78, 100.0f, 0.25f, 1.0f pair); byte-matches retail 62B.

--- Code/GameEngine/Source/GameLogic/AI/RainOfFireUpdateModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/RainOfFireUpdateModuleDataCtorThunk.cpp
@@ -1,75 +1,38 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: RainOfFireUpdateModuleData leaf ctor.
+
 class RainOfFireUpdateModuleData
 {
 public:
-    RainOfFireUpdateModuleData();
+	RainOfFireUpdateModuleData();
+	virtual ~RainOfFireUpdateModuleData();
+
+private:
+	unsigned int m_gap4;
+	unsigned int m_08;
+	unsigned int m_0c;
+	float m_10;
+	float m_14;
+	unsigned int m_18;
+	float m_1c;
+	float m_20;
+	unsigned int m_24;
+	unsigned int m_28;
+	unsigned int m_2c;
 };
 
-__declspec(naked) RainOfFireUpdateModuleData::RainOfFireUpdateModuleData()
+// ??0RainOfFireUpdateModuleData@@QAE@XZ
+RainOfFireUpdateModuleData::RainOfFireUpdateModuleData()
 {
-    __asm {
-        _emit 08Bh
-        _emit 0C1h
-        _emit 033h
-        _emit 0C9h
-        _emit 0BAh
-        _emit 000h
-        _emit 000h
-        _emit 080h
-        _emit 03Fh
-        _emit 0C7h
-        _emit 000h
-        _emit 098h
-        _emit 016h
-        _emit 00Ch
-        _emit 001h
-        _emit 0C7h
-        _emit 040h
-        _emit 008h
-        _emit 096h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 040h
-        _emit 00Ch
-        _emit 078h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 040h
-        _emit 010h
-        _emit 000h
-        _emit 000h
-        _emit 0C8h
-        _emit 042h
-        _emit 0C7h
-        _emit 040h
-        _emit 014h
-        _emit 000h
-        _emit 000h
-        _emit 080h
-        _emit 03Eh
-        _emit 089h
-        _emit 048h
-        _emit 018h
-        _emit 089h
-        _emit 050h
-        _emit 01Ch
-        _emit 089h
-        _emit 050h
-        _emit 020h
-        _emit 089h
-        _emit 048h
-        _emit 024h
-        _emit 089h
-        _emit 048h
-        _emit 028h
-        _emit 089h
-        _emit 048h
-        _emit 02Ch
-        _emit 0C3h
-    }
+	m_08 = 0x96;
+	m_0c = 0x78;
+	m_10 = 100.0f;
+	m_14 = 0.25f;
+	m_18 = 0;
+	m_1c = 1.0f;
+	m_20 = 1.0f;
+	m_24 = 0;
+	m_28 = 0;
+	m_2c = 0;
 }
