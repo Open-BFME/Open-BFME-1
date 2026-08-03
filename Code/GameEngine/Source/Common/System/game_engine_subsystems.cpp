2121ddcce5b283b25486e7692c4c483b2fd0f96a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:02:55 -0400

    Open-BFME5 convert GlobalData user-data accessor to C++

--- Code/GameEngine/Source/Common/System/game_engine_subsystems.cpp
+++ Code/GameEngine/Source/Common/System/game_engine_subsystems.cpp
@@ -4,49 +4,16 @@
 
 class GlobalData
 {
+    unsigned char m_padding[0x127c];
+    AsciiString m_pathUserData;
+
 public:
     AsciiString getPath_UserData() const;
 };
 
 // The retail GlobalData accessor returns the hidden output object through the
-// caller's stack slot and copies the global path string via the shared body.
-__declspec(naked) AsciiString GlobalData::getPath_UserData() const
+// caller's stack slot and copies the path string from its +0x127c member.
+AsciiString GlobalData::getPath_UserData() const
 {
-	__asm {
-		_emit 051h
-		_emit 056h
-		_emit 08Bh
-		_emit 074h
-		_emit 024h
-		_emit 00Ch
-		_emit 081h
-		_emit 0C1h
-		_emit 07Ch
-		_emit 012h
-		_emit 000h
-		_emit 000h
-		_emit 051h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0C7h
-		_emit 044h
-		_emit 024h
-		_emit 008h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 0E8h
-		_emit 0D4h
-		_emit 03Dh
-		_emit 080h
-		_emit 000h
-		_emit 08Bh
-		_emit 0C6h
-		_emit 05Eh
-		_emit 059h
-		_emit 0C2h
-		_emit 004h
-		_emit 000h
-	}
+	return m_pathUserData;
 }
