46e2731b353f77ed2a57802e349199375d805f96
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:34:06 -0400

    Open-BFME5 convert LevelGrantSpecialPowerModuleData destructor to C++

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/LevelGrantSpecialPowerModuleDataDestructorThunk.cpp
@@ -0,0 +1,93 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class __declspec(novtable) LevelGrantSpecialPowerModuleData
+{
+public:
+    virtual ~LevelGrantSpecialPowerModuleData();
+};
+
+// ??1LevelGrantSpecialPowerModuleData@@UAE@XZ
+__declspec(naked) LevelGrantSpecialPowerModuleData::~LevelGrantSpecialPowerModuleData()
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xf8
+        __emit 0xf5
+        __emit 0x00
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8d
+        __emit 0x8e
+        __emit 0x60
+        __emit 0x02
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0xe1
+        __emit 0xa3
+        __emit 0xdb
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0xde
+        __emit 0x97
+        __emit 0xdb
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
