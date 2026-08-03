01ae728dc576549cc232be72339a2e8472293a91
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:51:51 -0400

    Open-BFME5 convert SiegeDeployHordeSpecialPowerModuleData ctor to clean C++
    Replace naked dump with base ModuleData call plus zeroed flag at +0x1d0;
    pin base ctor; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/Object/SpecialPower/SiegeDeployHordeSpecialPowerModuleData.cpp
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/SiegeDeployHordeSpecialPowerModuleData.cpp
@@ -1,39 +1,28 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class SiegeDeployHordeSpecialPowerModuleData
+// Open-BFME5: SiegeDeployHordeSpecialPowerModuleData ctor.
+
+class ModuleDataBase_SiegeDeployHordeSpecialPowerModuleData
 {
 public:
-    SiegeDeployHordeSpecialPowerModuleData();
+	ModuleDataBase_SiegeDeployHordeSpecialPowerModuleData();
+	virtual ~ModuleDataBase_SiegeDeployHordeSpecialPowerModuleData();
+private:
+	unsigned char m_base[460];
 };
 
-__declspec(naked) SiegeDeployHordeSpecialPowerModuleData::SiegeDeployHordeSpecialPowerModuleData()
+class SiegeDeployHordeSpecialPowerModuleData : public ModuleDataBase_SiegeDeployHordeSpecialPowerModuleData
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 04Fh
-        _emit 0E2h
-        _emit 0DBh
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 040h
-        _emit 072h
-        _emit 00Bh
-        _emit 001h
-        _emit 0C6h
-        _emit 086h
-        _emit 0D0h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C3h
-    }
+public:
+	SiegeDeployHordeSpecialPowerModuleData();
+	virtual ~SiegeDeployHordeSpecialPowerModuleData();
+private:
+	unsigned char m_flag;
+};
+
+// ??0SiegeDeployHordeSpecialPowerModuleData@@QAE@XZ
+SiegeDeployHordeSpecialPowerModuleData::SiegeDeployHordeSpecialPowerModuleData()
+	: ModuleDataBase_SiegeDeployHordeSpecialPowerModuleData()
+{
+	m_flag = 0;
 }
