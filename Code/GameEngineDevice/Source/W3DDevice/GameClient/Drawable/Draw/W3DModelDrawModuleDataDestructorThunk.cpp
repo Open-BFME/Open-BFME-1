c5b762902f1943f9cff2ff6ae404e6a7c6975a2b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:19:20 -0400

    Open-BFME5 convert W3DModelDrawModuleData destructor MASM

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Drawable/Draw/W3DModelDrawModuleDataDestructorThunk.cpp
@@ -0,0 +1,89 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class __declspec(novtable) W3DModelDrawModuleData
+{
+public:
+    virtual ~W3DModelDrawModuleData();
+};
+
+__declspec(naked) W3DModelDrawModuleData::~W3DModelDrawModuleData()
+{
+    __asm {
+        _emit 06Ah
+        _emit 0FFh
+        _emit 068h
+        _emit 0A8h
+        _emit 0C7h
+        _emit 000h
+        _emit 001h
+        _emit 064h
+        _emit 0A1h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 050h
+        _emit 064h
+        _emit 089h
+        _emit 025h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 051h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 074h
+        _emit 024h
+        _emit 004h
+        _emit 08Dh
+        _emit 04Eh
+        _emit 05Ch
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0E8h
+        _emit 084h
+        _emit 06Ch
+        _emit 0E0h
+        _emit 0FFh
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0E8h
+        _emit 027h
+        _emit 093h
+        _emit 0DFh
+        _emit 0FFh
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 05Eh
+        _emit 064h
+        _emit 089h
+        _emit 00Dh
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 083h
+        _emit 0C4h
+        _emit 010h
+        _emit 0C3h
+    }
+}
