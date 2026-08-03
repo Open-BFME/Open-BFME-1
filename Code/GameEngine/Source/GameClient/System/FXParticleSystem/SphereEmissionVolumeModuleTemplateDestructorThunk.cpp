84073f78003d752fd4579e9cbddcf163d8a640c1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:21:03 -0400

    Open-BFME5 convert SphereEmissionVolume destructor to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/SphereEmissionVolumeModuleTemplateDestructorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/SphereEmissionVolumeModuleTemplateDestructorThunk.cpp
@@ -14,12 +14,12 @@ public:
 // ??1SphereEmissionVolumeModuleTemplate@FXParticleSystem@@UAE@XZ
 SphereEmissionVolumeModuleTemplate::~SphereEmissionVolumeModuleTemplate()
 {
-	unsigned char *info = this ? (unsigned char *)this + 8 : 0;
-	*(volatile unsigned int *)info = 0x01073744;
+    unsigned char *info = this ? (unsigned char *)this + 8 : 0;
+    *(volatile unsigned int *)info = 0x01073744;
 
-	unsigned char *base = this ? (unsigned char *)this + 4 : 0;
-	*(volatile unsigned int *)base = 0x0110f9ac;
-	*(volatile unsigned int *)this = 0x01073758;
+    unsigned char *base = this ? (unsigned char *)this + 4 : 0;
+    *(volatile unsigned int *)base = 0x0110f9ac;
+    *(volatile unsigned int *)this = 0x01073758;
 }
 
 }
