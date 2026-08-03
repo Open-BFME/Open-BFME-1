904f0044b48aaa64c2920873521d1fd7eeb41030
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:09:34 -0400

    Open-BFME5 convert UpgradeDieModuleData destructor to C++

--- Code/GameEngine/Source/GameLogic/Object/Upgrade/UpgradeDieModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/UpgradeDieModuleDataDestructorThunk.cpp
@@ -1,82 +1,34 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift ModuleData dtor MASM dump to standalone C++ thunk.
+// Open-BFME5: lift UpgradeDieModuleData dtor MASM to clean C++.
+// Retail destroys the AsciiString member at this+0x34 under an SEH frame,
+// then runs the inlined DieModuleData base dtor (base vtable store).
 
-class __declspec(novtable) UpgradeDieModuleData
+class AsciiString
 {
 public:
-    virtual ~UpgradeDieModuleData();
+	~AsciiString();
+};
+
+class UpgradeDieModuleDataBase
+{
+public:
+	virtual ~UpgradeDieModuleDataBase() {}
+
+private:
+	unsigned char m_pad[0x30];
+};
+
+class __declspec(novtable) UpgradeDieModuleData
+	: public UpgradeDieModuleDataBase
+{
+public:
+	virtual ~UpgradeDieModuleData();
+
+private:
+	AsciiString m_upgradeName;
 };
 
 // ??1UpgradeDieModuleData@@UAE@XZ
-__declspec(naked) UpgradeDieModuleData::~UpgradeDieModuleData()
+UpgradeDieModuleData::~UpgradeDieModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0x19
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x34
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x73
-        __emit 0x10
-        __emit 0x76
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
