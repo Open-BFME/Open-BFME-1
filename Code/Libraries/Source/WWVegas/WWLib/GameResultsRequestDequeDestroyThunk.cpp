162e627a7eef1b8e4a6547dd3d37459b9570e936
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 08:56:02 -0400

    Open-BFME5 convert GameResultsRequest deque destroy MASM to C++

---
+++ Code/Libraries/Source/WWVegas/WWLib/GameResultsRequestDequeDestroyThunk.cpp
@@ -0,0 +1,86 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+extern "C" __declspec(naked) void bfme_export_2ea8ff67()
+{
+    __asm {
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x14
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x18
+        __emit 0x8d
+        __emit 0x44
+        __emit 0x24
+        __emit 0x14
+        __emit 0x50
+        __emit 0x83
+        __emit 0xec
+        __emit 0x10
+        __emit 0x8b
+        __emit 0xc4
+        __emit 0x89
+        __emit 0x08
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x30
+        __emit 0x89
+        __emit 0x50
+        __emit 0x04
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x34
+        __emit 0x89
+        __emit 0x48
+        __emit 0x08
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0x89
+        __emit 0x50
+        __emit 0x0c
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x1c
+        __emit 0x83
+        __emit 0xec
+        __emit 0x10
+        __emit 0x8b
+        __emit 0xc4
+        __emit 0x89
+        __emit 0x08
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x30
+        __emit 0x89
+        __emit 0x50
+        __emit 0x04
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x34
+        __emit 0x89
+        __emit 0x48
+        __emit 0x08
+        __emit 0x89
+        __emit 0x50
+        __emit 0x0c
+        __emit 0xe8
+        __emit 0x32
+        __emit 0x52
+        __emit 0xa0
+        __emit 0xff
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x24
+        __emit 0xc3
+    }
+}
