cbe2f09906db4b2fcc0ecd38dca40a13428e7680
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:00:04 -0400

    Open-BFME5 convert AssistedTargetingUpdate::friend_newModuleData to clean C++
    Replace naked dump with new ModuleData (size 0x18, field init 1/0/0/0)
    and optional initFromINI; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/AI/AssistedTargetingUpdateFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AssistedTargetingUpdateFriendNewModuleDataThunk.cpp
@@ -1,85 +1,52 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class INI;
+// Open-BFME5: AssistedTargetingUpdate::friend_newModuleData
+
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class AssistedTargetingUpdateModuleData
+{
+public:
+	AssistedTargetingUpdateModuleData();
+	virtual ~AssistedTargetingUpdateModuleData();
+private:
+	unsigned int m_pad;
+	unsigned int m_one;
+	unsigned int m_a;
+	unsigned int m_b;
+	unsigned int m_c;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char AssistedTargetingUpdateFieldParse;
 
 class AssistedTargetingUpdate
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
-// ?friend_newModuleData@AssistedTargetingUpdate@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *AssistedTargetingUpdate::friend_newModuleData(INI *)
+AssistedTargetingUpdateModuleData::AssistedTargetingUpdateModuleData()
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x18
-        __emit 0xe8
-        __emit 0x78
-        __emit 0xe7
-        __emit 0x75
-        __emit 0x00
-        __emit 0x33
-        __emit 0xd2
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x3b
-        __emit 0xc2
-        __emit 0x74
-        __emit 0x1a
-        __emit 0xc7
-        __emit 0x00
-        __emit 0xe0
-        __emit 0xb8
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x08
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x89
-        __emit 0x50
-        __emit 0x0c
-        __emit 0x89
-        __emit 0x50
-        __emit 0x10
-        __emit 0x89
-        __emit 0x50
-        __emit 0x14
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x3b
-        __emit 0xca
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0xc2
-        __emit 0x4c
-        __emit 0x41
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x40
-        __emit 0xe9
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	m_one = 1;
+	m_a = 0;
+	m_b = 0;
+	m_c = 0;
+}
+
+// ?friend_newModuleData@AssistedTargetingUpdate@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *AssistedTargetingUpdate::friend_newModuleData(INI *ini)
+{
+	AssistedTargetingUpdateModuleData *data = new AssistedTargetingUpdateModuleData;
+	if (ini)
+		ini->initFromINI(data, &AssistedTargetingUpdateFieldParse);
+	return (ModuleData *)data;
 }
