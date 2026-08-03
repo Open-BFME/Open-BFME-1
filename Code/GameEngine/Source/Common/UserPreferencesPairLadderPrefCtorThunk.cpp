f944961d3cb0ee8c3789880028bb632ca7aff9df
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:46:03 -0400

    Open-BFME5 convert STL pair const long LadderPref constructor to clean C++
    Replace naked dump with pair first-copy and LadderPref copy-ctor for second; focused build byte-matches 30B.

--- Code/GameEngine/Source/Common/UserPreferencesPairLadderPrefCtorThunk.cpp
+++ Code/GameEngine/Source/Common/UserPreferencesPairLadderPrefCtorThunk.cpp
@@ -2,6 +2,8 @@
 
 class LadderPref
 {
+public:
+	LadderPref(const LadderPref &);
 };
 
 namespace _STL
@@ -10,44 +12,15 @@ template <class First, class Second>
 class pair
 {
 public:
-    pair(const First &, const Second &);
+	First first;
+	Second second;
+	pair(const First &, const Second &);
 };
 
-template <class First, class Second>
-__declspec(naked) pair<First, Second>::pair(const First &, const Second &)
+template <>
+pair<const long, LadderPref>::pair(const long &a, const LadderPref &b)
+	: first(a), second(b)
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 004h
-        _emit 08Bh
-        _emit 054h
-        _emit 024h
-        _emit 008h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 008h
-        _emit 089h
-        _emit 00Eh
-        _emit 052h
-        _emit 08Dh
-        _emit 04Eh
-        _emit 004h
-        _emit 0E8h
-        _emit 0C1h
-        _emit 0E5h
-        _emit 0F9h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
 }
 }
 
