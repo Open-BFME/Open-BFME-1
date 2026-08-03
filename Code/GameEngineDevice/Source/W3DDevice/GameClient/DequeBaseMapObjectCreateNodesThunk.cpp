64230389d7bc593d0b982e4eea054d429ec9c6d9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:29:21 -0400

    Open-BFME5 convert MapObject deque _M_create_nodes to clean C++
    Replace naked __emit dump with no-SEH operator-new loop allocating 0x78-byte nodes; focused build byte-matches retail 40B.

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/DequeBaseMapObjectCreateNodesThunk.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/DequeBaseMapObjectCreateNodesThunk.cpp
@@ -1,6 +1,5 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump for no-SEH _M_create_nodes (MapObject deque).
-// Clean C++ emits SEH around operator new (121B); retail is 40B no-SEH.
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+// Open-BFME5: no-SEH MapObject deque _M_create_nodes (0x78-byte nodes).
 
 class MapObject
 {
@@ -21,50 +20,13 @@ protected:
 };
 
 // ?_M_create_nodes@?$_Deque_base@PAVMapObject@@V?$allocator@PAVMapObject@@@_STL@@@_STL@@IAEXPAPAPAVMapObject@@0@Z
-// Type = MapObject* => Type** = MapObject***
-__declspec(naked) void _Deque_base<MapObject *, allocator<MapObject *> >::_M_create_nodes(MapObject ***, MapObject ***)
+void _Deque_base<MapObject *, allocator<MapObject *> >::_M_create_nodes(MapObject ***start, MapObject ***finish)
 {
-	__asm {
-		__emit 0x56;
-		__emit 0x8b;
-		__emit 0x74;
-		__emit 0x24;
-		__emit 0x08;
-		__emit 0x57;
-		__emit 0x8b;
-		__emit 0x7c;
-		__emit 0x24;
-		__emit 0x10;
-		__emit 0x3b;
-		__emit 0xf7;
-		__emit 0x73;
-		__emit 0x15;
-		__emit 0x8b;
-		__emit 0xff;
-		__emit 0x6a;
-		__emit 0x78;
-		__emit 0xe8;
-		__emit 0xf9;
-		__emit 0x46;
-		__emit 0xf3;
-		__emit 0xff;
-		__emit 0x89;
-		__emit 0x06;
-		__emit 0x83;
-		__emit 0xc6;
-		__emit 0x04;
-		__emit 0x83;
-		__emit 0xc4;
-		__emit 0x04;
-		__emit 0x3b;
-		__emit 0xf7;
-		__emit 0x72;
-		__emit 0xed;
-		__emit 0x5f;
-		__emit 0x5e;
-		__emit 0xc2;
-		__emit 0x08;
-		__emit 0x00;
+	MapObject ***p = start;
+	while (p < finish)
+	{
+		*p = reinterpret_cast<MapObject **>(::operator new(0x78));
+		++p;
 	}
 }
 
