0b3bbc9b46efb375b0706d5df40189220bedf26b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:01:26 -0400

    Open-BFME5 convert BroadcastStealthUpdate::friend_newModuleData to clean C++
    0x94 ModuleData factory with pinned ctor and optional initFromINI; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/AI/BroadcastStealthUpdateFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/BroadcastStealthUpdateFriendNewModuleDataThunk.cpp
@@ -1,71 +1,42 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: BroadcastStealthUpdate::friend_newModuleData factory.
 
 class INI;
 class ModuleData;
 
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class BroadcastStealthUpdateModuleData
+{
+public:
+	BroadcastStealthUpdateModuleData();
+	virtual ~BroadcastStealthUpdateModuleData();
+
+private:
+	unsigned char m_pad[0x90]; // vptr+0x90=0x94
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char BroadcastStealthUpdateFieldParse;
+
 class BroadcastStealthUpdate
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@BroadcastStealthUpdate@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *BroadcastStealthUpdate::friend_newModuleData(INI *)
+ModuleData *BroadcastStealthUpdate::friend_newModuleData(INI *ini)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x68
-        __emit 0x94
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xd5
-        __emit 0x7b
-        __emit 0x75
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0x9e
-        __emit 0x45
-        __emit 0xef
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0xf3
-        __emit 0x94
-        __emit 0x40
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xae
-        __emit 0x7d
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	BroadcastStealthUpdateModuleData *data = new BroadcastStealthUpdateModuleData;
+	if (ini)
+		ini->initFromINI(data, &BroadcastStealthUpdateFieldParse);
+	return (ModuleData *)data;
 }
