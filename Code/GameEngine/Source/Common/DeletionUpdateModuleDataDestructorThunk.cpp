de33eda3d57420931dd2948186b91475ea59d9f7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:56:00 -0400

    Open-BFME5 convert DeletionUpdateModuleData destructor MASM
    Lift 112B retail destructor from masm_dumps into standalone C++ thunk.

---
+++ Code/GameEngine/Source/Common/DeletionUpdateModuleDataDestructorThunk.cpp
@@ -0,0 +1,127 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class __declspec(novtable) DeletionUpdateModuleData
+{
+public:
+    virtual ~DeletionUpdateModuleData();
+};
+
+// ??1DeletionUpdateModuleData@@UAE@XZ
+__declspec(naked) DeletionUpdateModuleData::~DeletionUpdateModuleData()
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xe8
+        __emit 0x57
+        __emit 0xff
+        __emit 0x00
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x57
+        __emit 0x8b
+        __emit 0xf9
+        __emit 0x89
+        __emit 0x7c
+        __emit 0x24
+        __emit 0x04
+        __emit 0xc7
+        __emit 0x07
+        __emit 0xbc
+        __emit 0xd5
+        __emit 0x07
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x4f
+        __emit 0x08
+        __emit 0x85
+        __emit 0xc9
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x74
+        __emit 0x11
+        __emit 0x56
+        __emit 0x8b
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x71
+        __emit 0x04
+        __emit 0x6a
+        __emit 0x01
+        __emit 0xff
+        __emit 0x10
+        __emit 0x85
+        __emit 0xf6
+        __emit 0x8b
+        __emit 0xce
+        __emit 0x75
+        __emit 0xf1
+        __emit 0x5e
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xc7
+        __emit 0x47
+        __emit 0x08
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x47
+        __emit 0x0c
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0x21
+        __emit 0x35
+        __emit 0xf8
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x5f
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
