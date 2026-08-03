c44d059ba4cc3707d5c06d0f717e65a1ed3cdbf4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:58:16 -0400

    Open-BFME5 convert TemporarilyDefectUpdate::friend_newModuleData to clean C++
    Replace naked dump with new ModuleData (size 0x0c) and optional
    initFromINI; focused build byte-matches.

--- Code/GameEngine/Source/GameLogic/Object/Update/TemporarilyDefectUpdateFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/TemporarilyDefectUpdateFriendNewModuleDataThunk.cpp
@@ -1,74 +1,46 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class INI;
+// Open-BFME5: TemporarilyDefectUpdate::friend_newModuleData
+
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class TemporarilyDefectUpdateModuleData
+{
+public:
+	TemporarilyDefectUpdateModuleData();
+	virtual ~TemporarilyDefectUpdateModuleData();
+private:
+	unsigned int m_pad;
+	unsigned int m_field;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char TemporarilyDefectUpdateFieldParse;
 
 class TemporarilyDefectUpdate
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
-// ?friend_newModuleData@TemporarilyDefectUpdate@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *TemporarilyDefectUpdate::friend_newModuleData(INI *)
+TemporarilyDefectUpdateModuleData::TemporarilyDefectUpdateModuleData()
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x0c
-        __emit 0xe8
-        __emit 0x08
-        __emit 0xd3
-        __emit 0x75
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x11
-        __emit 0xc7
-        __emit 0x00
-        __emit 0xb0
-        __emit 0xe5
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x08
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0xe3
-        __emit 0xdd
-        __emit 0x43
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xdb
-        __emit 0xd4
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	m_field = 0;
+}
+
+// ?friend_newModuleData@TemporarilyDefectUpdate@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *TemporarilyDefectUpdate::friend_newModuleData(INI *ini)
+{
+	TemporarilyDefectUpdateModuleData *data = new TemporarilyDefectUpdateModuleData;
+	if (ini)
+		ini->initFromINI(data, &TemporarilyDefectUpdateFieldParse);
+	return (ModuleData *)data;
 }
