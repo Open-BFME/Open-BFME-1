2d19c7be10b5e7fd515e895fcc2dc76cf75d11fa
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:36:07 -0400

    Open-BFME5 convert Debug::operator<<(bool) to clean C++

--- Code/Libraries/Source/WWVegas/WWDebug/debug_operator_bool.cpp
+++ Code/Libraries/Source/WWVegas/WWDebug/debug_operator_bool.cpp
@@ -1,43 +1,35 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /Oy- /EHsc
+
+// Open-BFME5: Debug::operator<<(bool). const char* stream at vtable +0x38.
 
 class Debug
 {
 public:
-    Debug &operator<<(bool);
+	virtual ~Debug();
+	virtual void pad01();
+	virtual void pad02();
+	virtual void pad03();
+	virtual void pad04();
+	virtual void pad05();
+	virtual void pad06();
+	virtual void pad07();
+	virtual void pad08();
+	virtual void pad09();
+	virtual void pad10();
+	virtual void pad11();
+	virtual void pad12();
+	virtual void pad13();
+	virtual Debug &operator<<(char const *);
+	Debug &operator<<(bool);
 };
 
-__declspec(naked) Debug &Debug::operator<<(bool)
+// ??6Debug@@QAEAAV0@_N@Z
+Debug &Debug::operator<<(bool value)
 {
-    __asm {
-        _emit 055h
-        _emit 08Bh
-        _emit 0ECh
-        _emit 08Ah
-        _emit 045h
-        _emit 008h
-        _emit 084h
-        _emit 0C0h
-        _emit 0B8h
-        _emit 0B4h
-        _emit 00Fh
-        _emit 008h
-        _emit 001h
-        _emit 075h
-        _emit 005h
-        _emit 0B8h
-        _emit 0A0h
-        _emit 0C2h
-        _emit 011h
-        _emit 001h
-        _emit 08Bh
-        _emit 011h
-        _emit 050h
-        _emit 0FFh
-        _emit 052h
-        _emit 038h
-        _emit 05Dh
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	const char *text;
+	if (value)
+		text = "true";
+	else
+		text = "false";
+	return (*this) << text;
 }
