91c7378e4811643b41a1c69026ff7ccbb206ea35
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:44:33 -0400

    Open-BFME5 convert TexBufferClass constructor to clean C++
    ShareBuffer-style base call then memset of pointer slots.

--- Code/Libraries/Source/WWVegas/WW3D2/TexBufferClassCtorThunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/TexBufferClassCtorThunk.cpp
@@ -1,74 +1,29 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class TexBufferClass
+#include <string.h>
+
+class ShareBufferClassBase
 {
 public:
-    TexBufferClass(int, const char *);
+	ShareBufferClassBase(int count, const char *name, int flags);
+	virtual ~ShareBufferClassBase();
+
+protected:
+	int field04;
+	int field08;
+	void *Array;
+	int Count;
 };
 
-__declspec(naked) TexBufferClass::TexBufferClass(int, const char *)
+class TexBufferClass : public ShareBufferClassBase
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x57;
-        __emit 0x6a;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0x49;
-        __emit 0xe5;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4e;
-        __emit 0x10;
-        __emit 0x8b;
-        __emit 0x7e;
-        __emit 0x0c;
-        __emit 0xc1;
-        __emit 0xe1;
-        __emit 0x02;
-        __emit 0x8b;
-        __emit 0xd1;
-        __emit 0xc1;
-        __emit 0xe9;
-        __emit 0x02;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x40;
-        __emit 0xc3;
-        __emit 0x13;
-        __emit 0x01;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0xf3;
-        __emit 0xab;
-        __emit 0x8b;
-        __emit 0xca;
-        __emit 0x83;
-        __emit 0xe1;
-        __emit 0x03;
-        __emit 0xf3;
-        __emit 0xaa;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+public:
+	TexBufferClass(int count, const char *name);
+};
+
+// ??0TexBufferClass@@QAE@HPBD@Z
+TexBufferClass::TexBufferClass(int count, const char *name)
+	: ShareBufferClassBase(count, name, 0)
+{
+	memset(Array, 0, (unsigned)Count * 4u);
 }
