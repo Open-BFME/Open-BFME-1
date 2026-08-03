cdf0e34fd40577cd2679486b2b8eb2e1d79bd8ff
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:29:57 -0400

    Open-BFME5 convert UpgradeSoundSelectorClientBehaviorModuleData constructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/UpgradeSoundSelector.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/UpgradeSoundSelector.cpp
@@ -1,34 +1,24 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /EHsc
+
+// Open-BFME5: UpgradeSoundSelectorClientBehaviorModuleData default ctor.
 
 class UpgradeSoundSelectorClientBehaviorModuleData
 {
 public:
-    UpgradeSoundSelectorClientBehaviorModuleData();
+	UpgradeSoundSelectorClientBehaviorModuleData();
+	virtual ~UpgradeSoundSelectorClientBehaviorModuleData();
+
+private:
+	unsigned int m_gap4;
+	unsigned int m_08;
+	unsigned int m_0c;
+	unsigned int m_10;
 };
 
-__declspec(naked) UpgradeSoundSelectorClientBehaviorModuleData::UpgradeSoundSelectorClientBehaviorModuleData()
+// ??0UpgradeSoundSelectorClientBehaviorModuleData@@QAE@XZ
+UpgradeSoundSelectorClientBehaviorModuleData::UpgradeSoundSelectorClientBehaviorModuleData()
 {
-    __asm {
-        _emit 08Bh
-        _emit 0C1h
-        _emit 033h
-        _emit 0C9h
-        _emit 0C7h
-        _emit 000h
-        _emit 038h
-        _emit 05Ah
-        _emit 011h
-        _emit 001h
-        _emit 089h
-        _emit 048h
-        _emit 008h
-        _emit 089h
-        _emit 048h
-        _emit 00Ch
-        _emit 089h
-        _emit 048h
-        _emit 010h
-        _emit 0C3h
-    }
+	m_08 = 0;
+	m_0c = 0;
+	m_10 = 0;
 }
