4b8bfc712d15057f3a744db64a4967a62bbf5cbb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 18:04:40 -0400

    Open-BFME5 convert Dozer repair priority predicate

--- Code/GameEngine/Source/Common/DozerPrimaryStateMachine_isRepairMostImportant_Thunk.cpp
+++ Code/GameEngine/Source/Common/DozerPrimaryStateMachine_isRepairMostImportant_Thunk.cpp
@@ -1,93 +1,54 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: native Dozer repair-priority predicate.
 
-class State;
+class AIUpdateInterface;
+class DozerAIInterface;
+
+class Object
+{
+public:
+    unsigned char m_pad[0x204];
+    AIUpdateInterface *m_ai;
+};
+
+class StateMachine
+{
+public:
+    unsigned char m_pad[0x10];
+    Object *m_owner;
+};
+
+class State
+{
+public:
+    unsigned char m_pad[0x1c];
+    StateMachine *m_machine;
+};
+
+typedef DozerAIInterface *(__fastcall *GetDozerAIInterfaceProc)(AIUpdateInterface *);
+typedef bool (__fastcall *IsIdleProc)(AIUpdateInterface *);
+typedef int (__fastcall *GetMostRecentCommandProc)(DozerAIInterface *);
+
 class DozerPrimaryStateMachine
 {
 public:
-	static bool __cdecl isRepairMostImportant(State *, void *);
+    static bool __cdecl isRepairMostImportant(State *state, void *);
 };
 
 // ?isRepairMostImportant@DozerPrimaryStateMachine@@SA_NPAVState@@PAX@Z
-__declspec(naked) bool __cdecl DozerPrimaryStateMachine::isRepairMostImportant(State *, void *)
+bool __cdecl DozerPrimaryStateMachine::isRepairMostImportant(State *state, void *)
 {
-	__asm {
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x48
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0x51
-        __emit 0x10
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xb2
-        __emit 0x04
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x75
-        __emit 0x04
-        __emit 0x32
-        __emit 0xc0
-        __emit 0x5e
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x06
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x90
-        __emit 0x3c
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf8
-        __emit 0x85
-        __emit 0xff
-        __emit 0x75
-        __emit 0x05
-        __emit 0x5f
-        __emit 0x32
-        __emit 0xc0
-        __emit 0x5e
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x92
-        __emit 0x80
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x84
-        __emit 0xc0
-        __emit 0x74
-        __emit 0xed
-        __emit 0x8b
-        __emit 0x07
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xff
-        __emit 0x50
-        __emit 0x14
-        __emit 0x48
-        __emit 0xf7
-        __emit 0xd8
-        __emit 0x1b
-        __emit 0xc0
-        __emit 0x5f
-        __emit 0x40
-        __emit 0x5e
-        __emit 0xc3
-	}
+    AIUpdateInterface *ai = state->m_machine->m_owner->m_ai;
+    if (!ai)
+        return false;
+
+    DozerAIInterface *dozerAI =
+        ((GetDozerAIInterfaceProc)(*(void ***)ai)[0x13c / sizeof(void *)])(ai);
+    if (!dozerAI)
+        return false;
+    if (!((IsIdleProc)(*(void ***)ai)[0x180 / sizeof(void *)])(ai))
+        return false;
+
+    void **dozerVtable = *(void ***)dozerAI;
+    return ((GetMostRecentCommandProc)dozerVtable[0x14 / sizeof(void *)])(dozerAI) == 1;
 }
