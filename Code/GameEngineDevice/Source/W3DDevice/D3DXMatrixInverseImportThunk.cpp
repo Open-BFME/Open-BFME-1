7bb15bfab27e9fc744fa81d4c107090f3c681309
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 14:39:37 -0400

    Open-BFME5 correct and convert D3DX inverse import thunk

---
+++ Code/GameEngineDevice/Source/W3DDevice/D3DXMatrixInverseImportThunk.cpp
@@ -0,0 +1,12 @@
+// Exact retail import thunk at RVA 0x009FB321.
+extern "C" __declspec(naked) void bfme_D3DXMatrixInverseImport_9FB321()
+{
+    __asm {
+        __emit 0xFF
+        __emit 0x25
+        __emit 0x40
+        __emit 0xBE
+        __emit 0x2D
+        __emit 0x01
+    }
+}
