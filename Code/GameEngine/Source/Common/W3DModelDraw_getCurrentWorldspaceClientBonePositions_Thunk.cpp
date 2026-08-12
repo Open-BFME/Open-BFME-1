fd17640bffc28448aa6333e941b87be567647fb3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 03:18:34 -0400

    Open-BFME5 convert W3DModelDraw bone positions to C++

--- Code/GameEngine/Source/Common/W3DModelDraw_getCurrentWorldspaceClientBonePositions_Thunk.cpp
+++ Code/GameEngine/Source/Common/W3DModelDraw_getCurrentWorldspaceClientBonePositions_Thunk.cpp
@@ -1,143 +1,108 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class Matrix3D;
+class Matrix3D
+{
+public:
+    unsigned int values[12];
+
+    __forceinline Matrix3D &operator=(const Matrix3D &that)
+    {
+        values[0] = that.values[0];
+        values[1] = that.values[1];
+        values[2] = that.values[2];
+        values[3] = that.values[3];
+        values[4] = that.values[4];
+        values[5] = that.values[5];
+        values[6] = that.values[6];
+        values[7] = that.values[7];
+        values[8] = that.values[8];
+        values[9] = that.values[9];
+        values[10] = that.values[10];
+        values[11] = that.values[11];
+        return *this;
+    }
+};
+
+class RenderObject
+{
+public:
+    virtual void slot00() = 0;
+    virtual void slot01() = 0;
+    virtual void slot02() = 0;
+    virtual void slot03() = 0;
+    virtual void slot04() = 0;
+    virtual void slot05() = 0;
+    virtual void slot06() = 0;
+    virtual void slot07() = 0;
+    virtual void slot08() = 0;
+    virtual void slot09() = 0;
+    virtual void slot10() = 0;
+    virtual void slot11() = 0;
+    virtual void slot12() = 0;
+    virtual void slot13() = 0;
+    virtual void slot14() = 0;
+    virtual void slot15() = 0;
+    virtual void slot16() = 0;
+    virtual void slot17() = 0;
+    virtual void slot18() = 0;
+    virtual void slot19() = 0;
+    virtual void slot20() = 0;
+    virtual void slot21() = 0;
+    virtual void slot22() = 0;
+    virtual void slot23() = 0;
+    virtual void slot24() = 0;
+    virtual void slot25() = 0;
+    virtual void slot26() = 0;
+    virtual void slot27() = 0;
+    virtual void slot28() = 0;
+    virtual void slot29() = 0;
+    virtual void slot30() = 0;
+    virtual void slot31() = 0;
+    virtual void slot32() = 0;
+    virtual void slot33() = 0;
+    virtual void slot34() = 0;
+    virtual void slot35() = 0;
+    virtual void slot36() = 0;
+    virtual void slot37() = 0;
+    virtual void slot38() = 0;
+    virtual void slot39() = 0;
+    virtual void slot40() = 0;
+    virtual void slot41() = 0;
+    virtual void slot42() = 0;
+    virtual void slot43() = 0;
+    virtual void slot44() = 0;
+    virtual void slot45() = 0;
+    virtual void slot46() = 0;
+    virtual void slot47() = 0;
+    virtual void slot48() = 0;
+    virtual int Get_Bone_Index(const char *boneName) = 0;
+    virtual const Matrix3D &Get_Bone_Transform(int boneIndex) = 0;
+};
+
 class W3DModelDraw
 {
 public:
-	virtual bool getCurrentWorldspaceClientBonePositions(char const *, Matrix3D &) const;
+    virtual bool getCurrentWorldspaceClientBonePositions(const char *boneName,
+                                                          Matrix3D &transform) const;
+
+private:
+    unsigned char m_padding[0x24];
+    RenderObject *m_renderObject;
 };
 
 // ?getCurrentWorldspaceClientBonePositions@W3DModelDraw@@UBE_NPBDAAVMatrix3D@@@Z
-__declspec(naked) bool W3DModelDraw::getCurrentWorldspaceClientBonePositions(char const *, Matrix3D &) const
+bool W3DModelDraw::getCurrentWorldspaceClientBonePositions(const char *boneName,
+                                                            Matrix3D &transform) const
 {
-	__asm {
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x28
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x13
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x08
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x52
-		__emit 0xff
-		__emit 0x90
-		__emit 0xc4
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x06
-		__emit 0x32
-		__emit 0xc0
-		__emit 0x5e
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x28
-		__emit 0x8b
-		__emit 0x11
-		__emit 0x50
-		__emit 0xff
-		__emit 0x92
-		__emit 0xc8
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x89
-		__emit 0x11
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x04
-		__emit 0x89
-		__emit 0x51
-		__emit 0x04
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x08
-		__emit 0x89
-		__emit 0x51
-		__emit 0x08
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x0c
-		__emit 0x89
-		__emit 0x51
-		__emit 0x0c
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x10
-		__emit 0x89
-		__emit 0x51
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x14
-		__emit 0x89
-		__emit 0x51
-		__emit 0x14
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x18
-		__emit 0x89
-		__emit 0x51
-		__emit 0x18
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x1c
-		__emit 0x89
-		__emit 0x51
-		__emit 0x1c
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x20
-		__emit 0x89
-		__emit 0x51
-		__emit 0x20
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x24
-		__emit 0x89
-		__emit 0x51
-		__emit 0x24
-		__emit 0x8b
-		__emit 0x50
-		__emit 0x28
-		__emit 0x89
-		__emit 0x51
-		__emit 0x28
-		__emit 0x8b
-		__emit 0x40
-		__emit 0x2c
-		__emit 0x89
-		__emit 0x41
-		__emit 0x2c
-		__emit 0xb0
-		__emit 0x01
-		__emit 0x5e
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-	}
+    if (m_renderObject == 0) {
+        return false;
+    }
+
+    int boneIndex = m_renderObject->Get_Bone_Index(boneName);
+    if (boneIndex == 0) {
+        return false;
+    }
+
+    transform = m_renderObject->Get_Bone_Transform(boneIndex);
+    return true;
 }
