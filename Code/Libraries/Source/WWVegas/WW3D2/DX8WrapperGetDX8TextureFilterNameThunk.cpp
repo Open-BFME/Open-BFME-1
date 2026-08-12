fad9767f59151b352489b99c10955ad78db68987
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 02:28:04 -0400

    Open-BFME5: convert DX8 stencil name table to C++

--- Code/Libraries/Source/WWVegas/WW3D2/DX8WrapperGetDX8TextureFilterNameThunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/DX8WrapperGetDX8TextureFilterNameThunk.cpp
@@ -1,84 +1,23 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to semantic C++.
 
 class DX8Wrapper
 {
 public:
-    static const char* Get_DX8_Texture_Filter_Name(unsigned value);
+	static const char *Get_DX8_Texture_Filter_Name(unsigned value);
 };
 
-__declspec(naked) const char* DX8Wrapper::Get_DX8_Texture_Filter_Name(unsigned)
+const char *DX8Wrapper::Get_DX8_Texture_Filter_Name(unsigned value)
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 004h
-        _emit 048h
-        _emit 083h
-        _emit 0F8h
-        _emit 007h
-        _emit 077h
-        _emit 037h
-        _emit 0FFh
-        _emit 024h
-        _emit 085h
-        _emit 008h
-        _emit 02Fh
-        _emit 0D0h
-        _emit 000h
-        _emit 0B8h
-        _emit 080h
-        _emit 09Eh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 06Ch
-        _emit 09Eh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 054h
-        _emit 09Eh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 03Ch
-        _emit 09Eh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 024h
-        _emit 09Eh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 010h
-        _emit 09Eh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 0FCh
-        _emit 09Dh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 0E8h
-        _emit 09Dh
-        _emit 013h
-        _emit 001h
-        _emit 0C3h
-        _emit 0B8h
-        _emit 034h
-        _emit 002h
-        _emit 008h
-        _emit 001h
-        _emit 0C3h
-    }
+	switch (value) {
+		case 1: return "D3DSTENCILOP_KEEP";
+		case 2: return "D3DSTENCILOP_ZERO";
+		case 3: return "D3DSTENCILOP_REPLACE";
+		case 4: return "D3DSTENCILOP_INCRSAT";
+		case 5: return "D3DSTENCILOP_DECRSAT";
+		case 6: return "D3DSTENCILOP_INVERT";
+		case 7: return "D3DSTENCILOP_INCR";
+		case 8: return "D3DSTENCILOP_DECR";
+		default: return "UNKNOWN";
+	}
 }
