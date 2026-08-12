7057b4b8d5953afeb42004462456f1d650560b8d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 02:50:43 -0400

    Open-BFME5 convert DataChunkInput constructor to C++

--- Code/GameEngine/Source/Common/System/DataChunkInputCtorThunk.cpp
+++ Code/GameEngine/Source/Common/System/DataChunkInputCtorThunk.cpp
@@ -1,125 +1,51 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class ChunkInputStream;
+class ChunkInputStream
+{
+public:
+    virtual int read(char *data, int size) = 0;
+    virtual int tell() = 0;
+};
+
+class DataChunkTableOfContents
+{
+public:
+    DataChunkTableOfContents() : m_list(0), m_listLength(0), m_nextID(1), m_headerOpened(false) {}
+    ~DataChunkTableOfContents();
+    void read(ChunkInputStream &stream);
+
+private:
+    void *m_list;
+    int m_listLength;
+    unsigned int m_nextID;
+    bool m_headerOpened;
+};
 
 class DataChunkInput
 {
 public:
-    DataChunkInput(ChunkInputStream *);
+    DataChunkInput(ChunkInputStream *stream);
+
+private:
+    ChunkInputStream *m_file;
+    DataChunkTableOfContents m_contents;
+    int m_fileposOfFirstChunk;
+    void *m_parserList;
+    void *m_chunkStack;
+
+public:
+    void *m_currentObject;
+    void *m_userData;
 };
 
-__declspec(naked) DataChunkInput::DataChunkInput(ChunkInputStream *)
+// ??0DataChunkInput@@QAE@PAVChunkInputStream@@@Z
+DataChunkInput::DataChunkInput(ChunkInputStream *stream) :
+    m_file(stream)
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 07Bh
-        _emit 0C9h
-        _emit 0FFh
-        _emit 000h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 014h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 089h
-        _emit 006h
-        _emit 08Dh
-        _emit 04Eh
-        _emit 004h
-        _emit 033h
-        _emit 0C0h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 004h
-        _emit 089h
-        _emit 001h
-        _emit 089h
-        _emit 041h
-        _emit 004h
-        _emit 0C7h
-        _emit 041h
-        _emit 008h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 088h
-        _emit 041h
-        _emit 00Ch
-        _emit 08Bh
-        _emit 016h
-        _emit 052h
-        _emit 089h
-        _emit 044h
-        _emit 024h
-        _emit 014h
-        _emit 089h
-        _emit 046h
-        _emit 01Ch
-        _emit 089h
-        _emit 046h
-        _emit 020h
-        _emit 089h
-        _emit 046h
-        _emit 024h
-        _emit 089h
-        _emit 046h
-        _emit 018h
-        _emit 0E8h
-        _emit 079h
-        _emit 0F5h
-        _emit 0F0h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 00Eh
-        _emit 08Bh
-        _emit 001h
-        _emit 0FFh
-        _emit 050h
-        _emit 004h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 089h
-        _emit 046h
-        _emit 014h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+    m_chunkStack = 0;
+    m_currentObject = 0;
+    m_userData = 0;
+    m_parserList = 0;
+    m_contents.read(*m_file);
+    m_fileposOfFirstChunk = m_file->tell();
 }
-
