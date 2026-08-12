9865431342aa27418b1f607fdd3d51931a95241f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 03:00:33 -0400

    Open-BFME5 convert additional disconnect lookup to C++

--- Code/GameEngine/Source/Common/GetAdditionalDisconnectsFromUserFile_Thunk.cpp
+++ Code/GameEngine/Source/Common/GetAdditionalDisconnectsFromUserFile_Thunk.cpp
@@ -1,111 +1,38 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-int __cdecl GetAdditionalDisconnectsFromUserFile(int);
+int __fastcall getTotalDisconnectsFromFile(int playerID);
+
+class GameSpyInfo;
+
+struct GameSpyInfoVTable
+{
+    void *slots[92];
+    int (__fastcall *getAdditionalDisconnects)(GameSpyInfo *self);
+    void (__fastcall *clearAdditionalDisconnects)(GameSpyInfo *self);
+};
+
+class GameSpyInfo
+{
+public:
+    GameSpyInfoVTable *vtable;
+};
+
+extern GameSpyInfo *TheGameSpyInfo;
 
 // ?GetAdditionalDisconnectsFromUserFile@@YAHH@Z
-__declspec(naked) int __cdecl GetAdditionalDisconnectsFromUserFile(int)
+int __cdecl GetAdditionalDisconnectsFromUserFile(int playerID)
 {
-	__asm {
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x08
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0xc3
-        __emit 0xfb
-        __emit 0xff
-        __emit 0xff
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0xf8
-        __emit 0x75
-        __emit 0x05
-        __emit 0x5f
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x5e
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x01
-        __emit 0xff
-        __emit 0x90
-        __emit 0x70
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x7e
-        __emit 0x12
-        __emit 0x85
-        __emit 0xff
-        __emit 0x75
-        __emit 0x0e
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x11
-        __emit 0xff
-        __emit 0x92
-        __emit 0x74
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x01
-        __emit 0xff
-        __emit 0x90
-        __emit 0x70
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xf8
-        __emit 0xff
-        __emit 0x74
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x11
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0xff
-        __emit 0xa2
-        __emit 0x70
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc7
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0xc3
-	}
+    int result = getTotalDisconnectsFromFile(playerID);
+    if (playerID == 0) {
+        return 0;
+    }
+
+    if (TheGameSpyInfo->vtable->getAdditionalDisconnects(TheGameSpyInfo) > 0 && result == 0) {
+        TheGameSpyInfo->vtable->clearAdditionalDisconnects(TheGameSpyInfo);
+    }
+
+    if (TheGameSpyInfo->vtable->getAdditionalDisconnects(TheGameSpyInfo) != -1) {
+        return TheGameSpyInfo->vtable->getAdditionalDisconnects(TheGameSpyInfo);
+    }
+    return result;
 }
