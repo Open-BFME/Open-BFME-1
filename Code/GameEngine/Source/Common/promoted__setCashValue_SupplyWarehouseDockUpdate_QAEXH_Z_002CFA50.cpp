43dfc8820ab39585b2fc95d1f7737090cd841b47
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 01:51:27 -0400

    Open-BFME5: convert supply cash value update to C++

--- Code/GameEngine/Source/Common/promoted__setCashValue_SupplyWarehouseDockUpdate_QAEXH_Z_002CFA50.cpp
+++ Code/GameEngine/Source/Common/promoted__setCashValue_SupplyWarehouseDockUpdate_QAEXH_Z_002CFA50.cpp
@@ -1,94 +1,61 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
-// Grok promote from masm_dumps — retail 0x002CFA50 size 81
-// was: Code/masm_dumps/setCashValue_SupplyWarehouseDockUpdate_QAEXH_Z_002CFA50_packet286.asm
 
-class SupplyWarehouseDockUpdate { public: void setCashValue(int); };
+extern "C" __declspec(dllimport) double __cdecl ceil(double);
+
+class Drawable
+{
+public:
+    void updateDrawableSupplyStatus(int, int);
+};
+
+class SupplyWarehouseObject
+{
+public:
+    virtual void slot0();
+    virtual void slot1();
+    virtual void slot2();
+    virtual void slot3();
+    virtual void slot4();
+    virtual void slot5();
+    virtual void slot6();
+    virtual void slot7();
+    virtual void slot8();
+    virtual void slot9();
+    virtual Drawable *getDrawable();
+};
+
+struct SupplyWarehouseModuleData
+{
+    unsigned char padding[0x10];
+    int startingBoxesData;
+};
+
+struct SupplyWarehouseGlobalData
+{
+    unsigned char padding[0xb24];
+    int baseValuePerSupplyBox;
+};
+
+extern SupplyWarehouseGlobalData *TheSupplyWarehouseGlobalData;
+
+class SupplyWarehouseDockUpdate
+{
+public:
+    void setCashValue(int);
+
+private:
+    unsigned int vtable;
+    SupplyWarehouseModuleData *moduleData;
+    SupplyWarehouseObject *object;
+    unsigned char padding[0x7c];
+    int boxesStored;
+};
 
 // ?setCashValue@SupplyWarehouseDockUpdate@@QAEXH@Z
-__declspec(naked) void SupplyWarehouseDockUpdate::setCashValue(int)
+void SupplyWarehouseDockUpdate::setCashValue(int cashValue)
 {
-__asm {
-		_emit 0DBh
-		_emit 044h
-		_emit 024h
-		_emit 004h
-		_emit 0A1h
-		_emit 0C8h
-		_emit 0D5h
-		_emit 02Eh
-		_emit 001h
-		_emit 056h
-		_emit 083h
-		_emit 0ECh
-		_emit 008h
-		_emit 0DAh
-		_emit 0B0h
-		_emit 024h
-		_emit 00Bh
-		_emit 000h
-		_emit 000h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 0DDh
-		_emit 01Ch
-		_emit 024h
-		_emit 0FFh
-		_emit 015h
-		_emit 094h
-		_emit 093h
-		_emit 035h
-		_emit 001h
-		_emit 083h
-		_emit 0C4h
-		_emit 008h
-		_emit 0E8h
-		_emit 0C2h
-		_emit 073h
-		_emit 072h
-		_emit 000h
-		_emit 08Bh
-		_emit 04Eh
-		_emit 008h
-		_emit 089h
-		_emit 086h
-		_emit 088h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 08Bh
-		_emit 011h
-		_emit 0FFh
-		_emit 052h
-		_emit 028h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 015h
-		_emit 08Bh
-		_emit 08Eh
-		_emit 088h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 08Bh
-		_emit 056h
-		_emit 004h
-		_emit 051h
-		_emit 08Bh
-		_emit 04Ah
-		_emit 010h
-		_emit 051h
-		_emit 08Bh
-		_emit 0C8h
-		_emit 0E8h
-		_emit 078h
-		_emit 01Bh
-		_emit 0D6h
-		_emit 0FFh
-		_emit 05Eh
-		_emit 0C2h
-		_emit 004h
-		_emit 000h
-	}
+    boxesStored = static_cast<int>(ceil(cashValue / static_cast<double>(TheSupplyWarehouseGlobalData->baseValuePerSupplyBox)));
+    Drawable *draw = object->getDrawable();
+    if (draw)
+        draw->updateDrawableSupplyStatus(moduleData->startingBoxesData, boxesStored);
 }
-
