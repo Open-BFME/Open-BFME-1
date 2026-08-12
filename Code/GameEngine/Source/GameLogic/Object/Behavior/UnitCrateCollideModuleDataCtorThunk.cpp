3ef2fcd9a4f6b409198e0b8ebea6bd54ea520781
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 02:20:24 -0400

    Open-BFME5 convert UnitCrateCollideModuleData constructor to C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/UnitCrateCollideModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/UnitCrateCollideModuleDataCtorThunk.cpp
@@ -1,101 +1,47 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class UnitCrateCollideModuleData
+// Open-BFME5: UnitCrateCollideModuleData constructor lifted from retail.
+
+class CrateCollideModuleData
 {
 public:
-    UnitCrateCollideModuleData();
+	CrateCollideModuleData();
+	virtual ~CrateCollideModuleData();
+
+private:
+	unsigned char m_data[0x50];
 };
 
-__declspec(naked) UnitCrateCollideModuleData::UnitCrateCollideModuleData()
+class AsciiString
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x63;
-        __emit 0x18;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0xe8;
-        __emit 0x08;
-        __emit 0xe3;
-        __emit 0xf1;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x58;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xe8;
-        __emit 0xe8;
-        __emit 0x08;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x01;
-        __emit 0x50;
-        __emit 0x68;
-        __emit 0x1c;
-        __emit 0x30;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x54;
-        __emit 0xe8;
-        __emit 0x3a;
-        __emit 0x1e;
-        __emit 0x76;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+public:
+	AsciiString()
+		: m_data(0)
+	{
+	}
+
+	~AsciiString();
+	void set(const char *text, int length);
+
+private:
+	char *m_data;
+};
+
+class UnitCrateCollideModuleData : public CrateCollideModuleData
+{
+public:
+	UnitCrateCollideModuleData();
+	virtual ~UnitCrateCollideModuleData();
+
+private:
+	unsigned int m_unitCount;
+	AsciiString m_unitType;
+};
+
+// ??0UnitCrateCollideModuleData@@QAE@XZ
+UnitCrateCollideModuleData::UnitCrateCollideModuleData()
+	: CrateCollideModuleData(), m_unitType()
+{
+	m_unitCount = 0;
+	m_unitType.set("", 0);
 }
