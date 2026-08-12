71bfad6d4d05d2f48d5b2e98b82b7529ff8569f6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 01:54:07 -0400

    Open-BFME5: convert GameSpyInfo factory to C++

--- Code/GameEngine/Source/Common/GameSpyInfoInterface_createNewGameSpyInfoInterface_Thunk.cpp
+++ Code/GameEngine/Source/Common/GameSpyInfoInterface_createNewGameSpyInfoInterface_Thunk.cpp
@@ -1,104 +1,22 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
 class GameSpyInfoInterface
 {
 public:
-	static GameSpyInfoInterface * __cdecl createNewGameSpyInfoInterface();
+    static GameSpyInfoInterface *__cdecl createNewGameSpyInfoInterface();
+};
+
+class GameSpyInfo
+{
+public:
+    GameSpyInfo();
+
+private:
+    unsigned char storage[0x700];
 };
 
 // ?createNewGameSpyInfoInterface@GameSpyInfoInterface@@SAPAV1@XZ
-__declspec(naked) GameSpyInfoInterface * __cdecl GameSpyInfoInterface::createNewGameSpyInfoInterface()
+GameSpyInfoInterface *__cdecl GameSpyInfoInterface::createNewGameSpyInfoInterface()
 {
-	__asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x0b
-        __emit 0x12
-        __emit 0x04
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x68
-        __emit 0x00
-        __emit 0x07
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xb0
-        __emit 0xa7
-        __emit 0x24
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x89
-        __emit 0x04
-        __emit 0x24
-        __emit 0x85
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x16
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0x7a
-        __emit 0x2d
-        __emit 0x9f
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x04
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x04
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-	}
+    return reinterpret_cast<GameSpyInfoInterface *>(new GameSpyInfo);
 }
