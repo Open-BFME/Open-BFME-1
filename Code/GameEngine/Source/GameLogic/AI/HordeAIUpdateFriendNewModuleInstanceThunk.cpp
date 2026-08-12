2f4ae54b8ad9d161b5cae791a9bcbb86058b9f1c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 01:55:45 -0400

    Open-BFME5: convert HordeAIUpdate factory to C++

--- Code/GameEngine/Source/GameLogic/AI/HordeAIUpdateFriendNewModuleInstanceThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/HordeAIUpdateFriendNewModuleInstanceThunk.cpp
@@ -7,111 +7,15 @@ class ModuleData;
 class HordeAIUpdate
 {
 public:
+    HordeAIUpdate(Thing *, const ModuleData *);
     static Module *friend_newModuleInstance(Thing *, const ModuleData *);
+
+private:
+    unsigned char storage[0x344];
 };
 
 // ?friend_newModuleInstance@HordeAIUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) Module *HordeAIUpdate::friend_newModuleInstance(Thing *, const ModuleData *)
+Module *HordeAIUpdate::friend_newModuleInstance(Thing *thing, const ModuleData *moduleData)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xab;
-        __emit 0xf6;
-        __emit 0xff;
-        __emit 0x00;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x68;
-        __emit 0x44;
-        __emit 0x03;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xe8;
-        __emit 0x90;
-        __emit 0x78;
-        __emit 0x76;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x04;
-        __emit 0x24;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x74;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x8b;
-        __emit 0x54;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x51;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xc8;
-        __emit 0xe8;
-        __emit 0xdd;
-        __emit 0xad;
-        __emit 0xf0;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    return reinterpret_cast<Module *>(new HordeAIUpdate(thing, moduleData));
 }
