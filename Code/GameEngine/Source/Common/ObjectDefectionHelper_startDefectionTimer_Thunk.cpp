86259b5f1cdf0178e033d785ff603f8f05e54af2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 01:09:53 -0400

    Open-BFME5 convert defection timer scheduling to C++

--- Code/GameEngine/Source/Common/ObjectDefectionHelper_startDefectionTimer_Thunk.cpp
+++ Code/GameEngine/Source/Common/ObjectDefectionHelper_startDefectionTimer_Thunk.cpp
@@ -1,97 +1,41 @@
 // cl: /DNDEBUG /MD /EHsc
 // Open-BFME5: lift MASM dump to standalone C++ thunk.
 
+class Object;
+
+class ObjectDefectionWakeFrameShim
+{
+public:
+    void setWakeFrame(Object *, unsigned int);
+};
+
 class ObjectDefectionHelper
 {
+    unsigned char padding0[8];
+    Object *object;
+    unsigned char padding1[20];
+    unsigned int detectionStart;
+    unsigned int detectionEnd;
+    unsigned int unknown;
+    bool doDefectorFX;
+
 public:
 	void startDefectionTimer(unsigned int, bool);
 };
 
 // ?startDefectionTimer@ObjectDefectionHelper@@QAEXI_N@Z
-__declspec(naked) void ObjectDefectionHelper::startDefectionTimer(unsigned int, bool)
+void ObjectDefectionHelper::startDefectionTimer(unsigned int numFrames, bool withDefectorFX)
 {
-	__asm {
-        __emit 0x8b
-        __emit 0x51
-        __emit 0x08
-        __emit 0xf6
-        __emit 0x82
-        __emit 0x44
-        __emit 0x03
-        __emit 0x00
-        __emit 0x00
-        __emit 0x02
-        __emit 0x75
-        __emit 0x11
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x3f
-        __emit 0x89
-        __emit 0x54
-        __emit 0x24
-        __emit 0x04
-        __emit 0xe9
-        __emit 0xcd
-        __emit 0xf4
-        __emit 0xdb
-        __emit 0xff
-        __emit 0xa1
-        __emit 0x98
-        __emit 0x08
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x40
-        __emit 0x3c
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x08
-        __emit 0x89
-        __emit 0x41
-        __emit 0x20
-        __emit 0x03
-        __emit 0xc6
-        __emit 0x89
-        __emit 0x41
-        __emit 0x24
-        __emit 0x8a
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x41
-        __emit 0x28
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x88
-        __emit 0x41
-        __emit 0x2c
-        __emit 0x5e
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x89
-        __emit 0x54
-        __emit 0x24
-        __emit 0x04
-        __emit 0xe9
-        __emit 0x98
-        __emit 0xf4
-        __emit 0xdb
-        __emit 0xff
+	if (!(reinterpret_cast<const unsigned char *>(object)[0x344] & 2)) {
+		reinterpret_cast<ObjectDefectionWakeFrameShim *>(this)->setWakeFrame(object, 0x3fffffff);
+		return;
 	}
+
+	unsigned int now = *reinterpret_cast<const unsigned int *>(
+		*reinterpret_cast<const unsigned int *>(0x012f0898) + 0x3c);
+	detectionStart = now;
+	detectionEnd = now + numFrames;
+	unknown = 0;
+	doDefectorFX = withDefectorFX;
+	reinterpret_cast<ObjectDefectionWakeFrameShim *>(this)->setWakeFrame(object, 1);
 }
