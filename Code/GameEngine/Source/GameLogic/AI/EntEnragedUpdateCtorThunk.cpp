3c5cbcad8c29343c9555df72676f8a28a0b98d58
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 00:47:46 -0400

    Open-BFME5 convert Ent enraged constructor

--- Code/GameEngine/Source/GameLogic/AI/EntEnragedUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/EntEnragedUpdateCtorThunk.cpp
@@ -1,196 +1,74 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: EntEnragedUpdate retail constructor expressed as C++.
 
 class Thing;
 class ModuleData;
+class Object;
 
-class EntEnragedUpdate
+class PB_DeepBase
 {
 public:
-    EntEnragedUpdate(Thing *, const ModuleData *);
+	PB_DeepBase(Thing *, const ModuleData *);
+	virtual ~PB_DeepBase();
+
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+};
+
+class EntEnragedIface1 { public: virtual void slot(); };
+class EntEnragedIface2 { public: virtual void slot(); };
+
+class UpdateModule : public PB_DeepBase, public EntEnragedIface1,
+	public EntEnragedIface2
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData)
+		: PB_DeepBase(thing, moduleData), m_f14(0), m_f18(-1), m_f1c(-1) {}
+
+private:
+	unsigned int m_f14;
+	int m_f18;
+	int m_f1c;
+};
+
+struct EntEnragedModuleDataView
+{
+	unsigned char m_pad[8];
+	unsigned int m_initialValue;
+};
+
+int __cdecl entEnragedInitialValue(int, unsigned int, const void *, int);
+
+class EntEnragedUpdate : public UpdateModule
+{
+public:
+	EntEnragedUpdate(Thing *, const ModuleData *);
+
+private:
+	unsigned int m_f20;
+	unsigned int m_f24;
+	unsigned int m_f28;
+	bool m_f2c;
+	bool m_f2d;
+	bool m_f2e;
+	bool m_f2f;
+	bool m_f30;
 };
 
 // ??0EntEnragedUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) EntEnragedUpdate::EntEnragedUpdate(Thing *, const ModuleData *)
+EntEnragedUpdate::EntEnragedUpdate(Thing *thing, const ModuleData *moduleData)
+	: UpdateModule(thing, moduleData), m_f20(0)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x18;
-        __emit 0x19;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0xe8;
-        __emit 0xb5;
-        __emit 0x56;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc8;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x1c;
-        __emit 0x8b;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x6a;
-        __emit 0x4c;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xac;
-        __emit 0xe8;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0xe7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xdc;
-        __emit 0xe7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x50;
-        __emit 0x08;
-        __emit 0x68;
-        __emit 0x88;
-        __emit 0xe7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x52;
-        __emit 0x53;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x24;
-        __emit 0xe8;
-        __emit 0x34;
-        __emit 0x01;
-        __emit 0xd7;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4e;
-        __emit 0x20;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0x03;
-        __emit 0xc8;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2c;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2e;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2f;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2d;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x28;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x30;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x3f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	const EntEnragedModuleDataView *data =
+		reinterpret_cast<const EntEnragedModuleDataView *>(m_moduleData);
+	m_f20 += entEnragedInitialValue(0, data->m_initialValue,
+		reinterpret_cast<const void *>(0x010be788), 0x4c);
+	m_f2c = false;
+	m_f2e = false;
+	m_f2f = false;
+	m_f2d = false;
+	m_f28 = 0;
+	m_f30 = false;
+	m_f24 = 0x3fffffff;
 }
