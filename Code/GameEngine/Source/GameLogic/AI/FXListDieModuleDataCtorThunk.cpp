f805bedc905358e0ff52399c8280c3dec2ef81ff
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 01:17:48 -0400

    Open-BFME5 convert FXListDieModuleData constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/FXListDieModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/FXListDieModuleDataCtorThunk.cpp
@@ -1,99 +1,51 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class FXListDieModuleData
+// Open-BFME5: FXListDieModuleData constructor lifted from retail.
+
+class Snapshot
 {
 public:
-    FXListDieModuleData();
+	Snapshot() {}
+	virtual ~Snapshot() {}
+
+private:
+	unsigned char m_data[4];
 };
 
-__declspec(naked) FXListDieModuleData::FXListDieModuleData()
+class InstantDeathDieMuxData
 {
-    __asm {
-        _emit 6Ah
-        _emit 0FFh
-        _emit 68h
-        _emit 0A8h
-        _emit 13h
-        _emit 00h
-        _emit 01h
-        _emit 64h
-        _emit 0A1h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 50h
-        _emit 64h
-        _emit 89h
-        _emit 25h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 51h
-        _emit 56h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 89h
-        _emit 74h
-        _emit 24h
-        _emit 04h
-        _emit 8Dh
-        _emit 4Eh
-        _emit 08h
-        _emit 0C7h
-        _emit 44h
-        _emit 24h
-        _emit 10h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 0C7h
-        _emit 06h
-        _emit 78h
-        _emit 0AFh
-        _emit 08h
-        _emit 01h
-        _emit 0E8h
-        _emit 0C1h
-        _emit 3Ch
-        _emit 0EEh
-        _emit 0FFh
-        _emit 8Bh
-        _emit 4Ch
-        _emit 24h
-        _emit 08h
-        _emit 0C7h
-        _emit 06h
-        _emit 58h
-        _emit 0B7h
-        _emit 08h
-        _emit 01h
-        _emit 0C7h
-        _emit 46h
-        _emit 34h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 0C6h
-        _emit 46h
-        _emit 38h
-        _emit 01h
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 64h
-        _emit 89h
-        _emit 0Dh
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 83h
-        _emit 0C4h
-        _emit 10h
-        _emit 0C3h
-    }
+public:
+	InstantDeathDieMuxData();
+
+private:
+	unsigned char m_data[0x2c];
+};
+
+class DieModuleData : public Snapshot
+{
+public:
+	DieModuleData() {}
+	virtual ~DieModuleData();
+
+private:
+	InstantDeathDieMuxData m_dieMuxData;
+};
+
+class FXListDieModuleData : public DieModuleData
+{
+public:
+	FXListDieModuleData();
+	virtual ~FXListDieModuleData();
+
+private:
+	unsigned int m_fxList;
+	bool m_orientToObject;
+};
+
+// ??0FXListDieModuleData@@QAE@XZ
+FXListDieModuleData::FXListDieModuleData()
+	: DieModuleData()
+{
+	m_fxList = 0;
+	m_orientToObject = true;
 }
