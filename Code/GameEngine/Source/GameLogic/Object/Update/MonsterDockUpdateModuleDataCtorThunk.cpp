719b2e76743268ca7ca9d19a5e783acb07ef3d6e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 03:09:31 -0400

    Open-BFME5 convert MonsterDock module data constructor to C++

--- Code/GameEngine/Source/GameLogic/Object/Update/MonsterDockUpdateModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/MonsterDockUpdateModuleDataCtorThunk.cpp
@@ -1,94 +1,40 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: retail-layout C++ conversion of MonsterDockUpdateModuleData.
 
-class MonsterDockUpdateModuleData
+class DockUpdateModuleData
 {
 public:
-    MonsterDockUpdateModuleData();
+	DockUpdateModuleData();
+	virtual ~DockUpdateModuleData() {}
+
+private:
+	unsigned char m_pad[0x0c];
 };
 
-__declspec(naked) MonsterDockUpdateModuleData::MonsterDockUpdateModuleData()
+class MonsterDockDataMember
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xb8;
-        __emit 0x42;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0xe8;
-        __emit 0x3b;
-        __emit 0xe8;
-        __emit 0xd5;
-        __emit 0xff;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x10;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x58;
-        __emit 0xab;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x52;
-        __emit 0x97;
-        __emit 0xd6;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+public:
+	MonsterDockDataMember();
+
+private:
+	unsigned int m_value;
+};
+
+class MonsterDockUpdateModuleData : public DockUpdateModuleData
+{
+public:
+	MonsterDockUpdateModuleData();
+	virtual ~MonsterDockUpdateModuleData();
+
+private:
+	MonsterDockDataMember m_member;
+	unsigned int m_initialDockCount;
+};
+
+// ??0MonsterDockUpdateModuleData@@QAE@XZ
+MonsterDockUpdateModuleData::MonsterDockUpdateModuleData() :
+	DockUpdateModuleData(),
+	m_member(),
+	m_initialDockCount(0)
+{
 }
