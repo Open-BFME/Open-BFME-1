cde2586396659769c341177dde19c67cb9da2753
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 01:22:25 -0400

    Open-BFME5 convert RefundDieModuleData constructor to C++

--- Code/GameEngine/Source/Common/RTS/RefundDieModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/Common/RTS/RefundDieModuleDataCtorThunk.cpp
@@ -1,119 +1,59 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class RefundDieModuleData
+// Open-BFME5: RefundDieModuleData constructor lifted from retail.
+
+class Snapshot
 {
 public:
-    RefundDieModuleData();
+	Snapshot() {}
+	virtual ~Snapshot() {}
+
+private:
+	unsigned char m_data[4];
 };
 
-__declspec(naked) RefundDieModuleData::RefundDieModuleData()
+class InstantDeathDieMuxData
 {
-    __asm {
-        _emit 6Ah
-        _emit 0FFh
-        _emit 68h
-        _emit 30h
-        _emit 13h
-        _emit 00h
-        _emit 01h
-        _emit 64h
-        _emit 0A1h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 50h
-        _emit 64h
-        _emit 89h
-        _emit 25h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 51h
-        _emit 56h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 89h
-        _emit 74h
-        _emit 24h
-        _emit 04h
-        _emit 8Dh
-        _emit 4Eh
-        _emit 08h
-        _emit 0C7h
-        _emit 44h
-        _emit 24h
-        _emit 10h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 0C7h
-        _emit 06h
-        _emit 78h
-        _emit 0AFh
-        _emit 08h
-        _emit 01h
-        _emit 0E8h
-        _emit 0B1h
-        _emit 3Fh
-        _emit 0EEh
-        _emit 0FFh
-        _emit 8Dh
-        _emit 4Eh
-        _emit 3Ch
-        _emit 0C7h
-        _emit 44h
-        _emit 24h
-        _emit 10h
-        _emit 01h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 0C7h
-        _emit 06h
-        _emit 0E0h
-        _emit 0B4h
-        _emit 08h
-        _emit 01h
-        _emit 0E8h
-        _emit 31h
-        _emit 42h
-        _emit 0F1h
-        _emit 0FFh
-        _emit 8Bh
-        _emit 4Ch
-        _emit 24h
-        _emit 08h
-        _emit 0C7h
-        _emit 46h
-        _emit 38h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 0C7h
-        _emit 46h
-        _emit 34h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 64h
-        _emit 89h
-        _emit 0Dh
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 83h
-        _emit 0C4h
-        _emit 10h
-        _emit 0C3h
-    }
-}
+public:
+	InstantDeathDieMuxData();
 
+private:
+	unsigned char m_data[0x2c];
+};
+
+class DieModuleData : public Snapshot
+{
+public:
+	DieModuleData() {}
+	virtual ~DieModuleData();
+
+private:
+	InstantDeathDieMuxData m_dieMuxData;
+};
+
+class RS_Member
+{
+public:
+	RS_Member();
+	~RS_Member();
+};
+
+class RefundDieModuleData : public DieModuleData
+{
+public:
+	RefundDieModuleData();
+	virtual ~RefundDieModuleData();
+
+private:
+	unsigned int m_refundPercent;
+	unsigned int m_refundMinimum;
+	RS_Member m_refundData;
+};
+
+// ??0RefundDieModuleData@@QAE@XZ
+RefundDieModuleData::RefundDieModuleData()
+	: DieModuleData(), m_refundData()
+{
+	m_refundMinimum = 0;
+	m_refundPercent = 0;
+}
