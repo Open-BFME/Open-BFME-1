f797f25beec69bada4e2a47acb3f764b6dc28d5f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 21:40:27 -0400

    Open-BFME5 convert Tensile formation constructor to C++

--- Code/GameEngine/Source/GameLogic/Object/Update/TensileFormationUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/TensileFormationUpdateCtorThunk.cpp
@@ -1,109 +1,64 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: TensileFormationUpdate constructor.
+
+#include <string.h>
+
 class Thing;
 class ModuleData;
+class Object;
 
-class TensileFormationUpdate
+extern "C" char TensileFormationUpdate_vtbl0;
+extern "C" char TensileFormationUpdate_vtblC;
+extern "C" char TensileFormationUpdate_vtbl10;
+
+class PB_DeepBase
 {
 public:
-    TensileFormationUpdate(Thing *, const ModuleData *);
+	PB_DeepBase(Thing *, const ModuleData *);
+	virtual ~PB_DeepBase();
+
+protected:
+	void *m_f04;
+	Object *m_object;
+};
+
+class TensileFormationUpdateIface1
+{
+public:
+	virtual void slot();
+};
+
+class TensileFormationUpdateIface2
+{
+public:
+	virtual void slot();
+};
+
+class __declspec(novtable) TensileFormationUpdate : public PB_DeepBase,
+	public TensileFormationUpdateIface1,
+	public TensileFormationUpdateIface2
+{
+public:
+	TensileFormationUpdate(Thing *, const ModuleData *);
+	virtual ~TensileFormationUpdate();
+
+private:
+	unsigned int m_formationData[0x30];
+	volatile unsigned int m_fD4;
+	volatile unsigned int m_fD8;
+	volatile unsigned int m_fDC;
 };
 
 // ??0TensileFormationUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) TensileFormationUpdate::TensileFormationUpdate(Thing *, const ModuleData *)
+TensileFormationUpdate::TensileFormationUpdate(Thing *thing, const ModuleData *moduleData)
+	: PB_DeepBase(thing, moduleData)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0x7f;
-        __emit 0x41;
-        __emit 0xdc;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xfc;
-        __emit 0x1b;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x5c;
-        __emit 0x26;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x98;
-        __emit 0x25;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x88;
-        __emit 0x25;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x86;
-        __emit 0xd4;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x86;
-        __emit 0xd8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x86;
-        __emit 0xdc;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x14;
-        __emit 0xb9;
-        __emit 0x30;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xf3;
-        __emit 0xab;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	*(void *volatile *)this = &TensileFormationUpdate_vtbl0;
+	*(void *volatile *)((char *)this + 0x0c) = &TensileFormationUpdate_vtblC;
+	*(void *volatile *)((char *)this + 0x10) = &TensileFormationUpdate_vtbl10;
+	m_fD4 = 0;
+	m_fD8 = 0;
+	m_fDC = 0;
+	memset(m_formationData, 0, sizeof(m_formationData));
 }
