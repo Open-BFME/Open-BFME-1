7b9c62b0b7aaf24826e2fa99c54d5a91ed0729ea
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 21:55:43 -0400

    Open-BFME5 convert D3DX matrix inverse import thunk to C++

--- Code/GameEngineDevice/Source/W3DDevice/D3DXMatrixInverseImportThunk.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/D3DXMatrixInverseImportThunk.cpp
@@ -1,12 +1,10 @@
-// Exact retail import thunk at RVA 0x009FB321.
-extern "C" __declspec(naked) void bfme_D3DXMatrixInverseImport_9FB321()
+// cl: /DNDEBUG /MD /EHsc
+
+typedef void *(__stdcall *D3DXMatrixInverseFunction)(void *, float *, const void *);
+extern D3DXMatrixInverseFunction g_D3DXMatrixInverseImport;
+
+extern "C" void *__stdcall D3DXMatrixInverse(void *output, float *determinant,
+                                              const void *input)
 {
-    __asm {
-        __emit 0xFF
-        __emit 0x25
-        __emit 0x40
-        __emit 0xBE
-        __emit 0x2D
-        __emit 0x01
-    }
+    return g_D3DXMatrixInverseImport(output, determinant, input);
 }
