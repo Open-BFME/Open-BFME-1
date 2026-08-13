dee1df1bef09537986d176cfa73690bbc274d431
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 21:40:18 -0400

    Open-BFME5 convert waypoint special power dispatch to C++

--- Code/GameEngine/Source/Common/Object_doSpecialPowerUsingWaypoints_Thunk.cpp
+++ Code/GameEngine/Source/Common/Object_doSpecialPowerUsingWaypoints_Thunk.cpp
@@ -1,94 +1,63 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
 class SpecialPowerTemplate;
 class Waypoint;
+class Object;
+
+class SpecialPowerModuleInterface
+{
+public:
+    virtual void slot00() = 0;
+    virtual void slot01() = 0;
+    virtual void slot02() = 0;
+    virtual void slot03() = 0;
+    virtual void slot04() = 0;
+    virtual void slot05() = 0;
+    virtual void slot06() = 0;
+    virtual void slot07() = 0;
+    virtual void slot08() = 0;
+    virtual void slot09() = 0;
+    virtual void slot10() = 0;
+    virtual void slot11() = 0;
+    virtual void slot12() = 0;
+    virtual void doSpecialPowerUsingWaypoints(const Waypoint *, unsigned int) = 0;
+};
+
+class SpecialPowerStore
+{
+public:
+    bool canUseSpecialPower(Object *, const SpecialPowerTemplate *);
+};
+
+extern SpecialPowerStore *TheSpecialPowerStore;
+
 class Object
 {
 public:
-	void doSpecialPowerUsingWaypoints(const SpecialPowerTemplate *, const Waypoint *, unsigned int, bool);
+    void doSpecialPowerUsingWaypoints(const SpecialPowerTemplate *, const Waypoint *, unsigned int, bool);
+    SpecialPowerModuleInterface *getSpecialPowerModule(const SpecialPowerTemplate *);
+
+private:
+    unsigned char m_padding[0x1a4];
+    unsigned int m_status;
 };
 
 // ?doSpecialPowerUsingWaypoints@Object@@QAEXPBVSpecialPowerTemplate@@PBVWaypoint@@I_N@Z
-__declspec(naked) void Object::doSpecialPowerUsingWaypoints(const SpecialPowerTemplate *, const Waypoint *, unsigned int, bool)
+void Object::doSpecialPowerUsingWaypoints(const SpecialPowerTemplate *specialPowerTemplate,
+                                           const Waypoint *way,
+                                           unsigned int commandOptions,
+                                           bool forced)
 {
-	__asm {
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x8b
-		__emit 0x86
-		__emit 0xa4
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x3c
-		__emit 0x8a
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x57
-		__emit 0x8b
-		__emit 0x7c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x75
-		__emit 0x11
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x0c
-		__emit 0xd8
-		__emit 0x2e
-		__emit 0x01
-		__emit 0x57
-		__emit 0x56
-		__emit 0xe8
-		__emit 0x2a
-		__emit 0xac
-		__emit 0xe6
-		__emit 0xff
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x1d
-		__emit 0x57
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0x3c
-		__emit 0xc9
-		__emit 0xe7
-		__emit 0xff
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x11
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x8b
-		__emit 0x10
-		__emit 0x51
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x51
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xff
-		__emit 0x52
-		__emit 0x34
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0xc2
-		__emit 0x10
-		__emit 0x00
-	}
+    if (m_status != 0) {
+        return;
+    }
+
+    if (!forced && !TheSpecialPowerStore->canUseSpecialPower(this, specialPowerTemplate)) {
+        return;
+    }
+
+    SpecialPowerModuleInterface *module = getSpecialPowerModule(specialPowerTemplate);
+    if (module) {
+        module->doSpecialPowerUsingWaypoints(way, commandOptions);
+    }
 }
