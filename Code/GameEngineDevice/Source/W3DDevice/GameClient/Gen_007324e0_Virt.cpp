0e5e542b129358318ff0359a8608c27c1ed100c4
Jonathan <jonathan@users.noreply.github.com>
2026-08-20 16:20:42 +0000

    Open-BFME5: convert the three-arg virtual wrapper at 0x007324E0
    obj->virtual slot 0x90(&g, arg, 4).

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Gen_007324e0_Virt.cpp
@@ -0,0 +1,53 @@
+// cl: /DNDEBUG /MD /EHsc
+
+// Retail 0x007324E0. obj->virtual slot 0x90(&g, arg, 4).
+
+class Gen_007324e0_Obj
+{
+public:
+	virtual void v0(const void *, void *, int);
+	virtual void v1(const void *, void *, int);
+	virtual void v2(const void *, void *, int);
+	virtual void v3(const void *, void *, int);
+	virtual void v4(const void *, void *, int);
+	virtual void v5(const void *, void *, int);
+	virtual void v6(const void *, void *, int);
+	virtual void v7(const void *, void *, int);
+	virtual void v8(const void *, void *, int);
+	virtual void v9(const void *, void *, int);
+	virtual void v10(const void *, void *, int);
+	virtual void v11(const void *, void *, int);
+	virtual void v12(const void *, void *, int);
+	virtual void v13(const void *, void *, int);
+	virtual void v14(const void *, void *, int);
+	virtual void v15(const void *, void *, int);
+	virtual void v16(const void *, void *, int);
+	virtual void v17(const void *, void *, int);
+	virtual void v18(const void *, void *, int);
+	virtual void v19(const void *, void *, int);
+	virtual void v20(const void *, void *, int);
+	virtual void v21(const void *, void *, int);
+	virtual void v22(const void *, void *, int);
+	virtual void v23(const void *, void *, int);
+	virtual void v24(const void *, void *, int);
+	virtual void v25(const void *, void *, int);
+	virtual void v26(const void *, void *, int);
+	virtual void v27(const void *, void *, int);
+	virtual void v28(const void *, void *, int);
+	virtual void v29(const void *, void *, int);
+	virtual void v30(const void *, void *, int);
+	virtual void v31(const void *, void *, int);
+	virtual void v32(const void *, void *, int);
+	virtual void v33(const void *, void *, int);
+	virtual void v34(const void *, void *, int);
+	virtual void v35(const void *, void *, int);
+	virtual void v36(const void *, void *, int);
+};
+
+char g_007324e0;
+
+// ?run_007324e0@@YAXPAVGen_007324e0_Obj@@PAX@Z
+void run_007324e0(Gen_007324e0_Obj *o, void *p)
+{
+	o->v36(&g_007324e0, p, 4);
+}
