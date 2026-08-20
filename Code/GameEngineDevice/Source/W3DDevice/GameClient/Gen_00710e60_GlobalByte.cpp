abd7172b0f8b9e4becae14abd16e3b438c75df12
Jonathan <jonathan@users.noreply.github.com>
2026-08-20 15:44:37 +0000

    Open-BFME5: convert the global-byte load at 0x00710E60
    Unidentified leaf: return a global byte.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Gen_00710e60_GlobalByte.cpp
@@ -0,0 +1,11 @@
+// cl: /DNDEBUG /MD /EHsc
+
+// Retail 0x00710E60. Return a global byte.
+
+unsigned char g_get_00710e60;
+
+// ?get_00710e60@@YAEXZ
+unsigned char get_00710e60(void)
+{
+	return g_get_00710e60;
+}
