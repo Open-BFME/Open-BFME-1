91aba83222d0020d218ee2fce5e41b79fe7b952c
Jonathan <jonathan@users.noreply.github.com>
2026-08-20 15:58:16 +0000

    Open-BFME5: convert the zero-init constructor at 0x00704980
    Zeros 7 dwords then 4 trailing bytes.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Gen_00704980_Ctor.cpp
@@ -0,0 +1,38 @@
+// cl: /DNDEBUG /MD /EHsc
+
+// Retail 0x00704980. Constructor: zero 7 dwords then 4 bytes.
+
+class Gen_00704980
+{
+public:
+	Gen_00704980(void);
+
+private:
+	int m_0;
+	int m_4;
+	int m_8;
+	int m_c;
+	int m_10;
+	int m_14;
+	int m_18;
+	unsigned char m_1c;
+	unsigned char m_1d;
+	unsigned char m_1e;
+	unsigned char m_1f;
+};
+
+// ??0Gen_00704980@@QAE@XZ
+Gen_00704980::Gen_00704980(void)
+{
+	m_0 = 0;
+	m_4 = 0;
+	m_8 = 0;
+	m_c = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1c = 0;
+	m_1d = 0;
+	m_1e = 0;
+	m_1f = 0;
+}
