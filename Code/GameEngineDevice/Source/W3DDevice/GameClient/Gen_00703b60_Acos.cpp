985a4ab494603aaf069779026e435c750c47412b
Jonathan <jonathan@users.noreply.github.com>
2026-08-20 16:05:32 +0000

    Open-BFME5: convert the acos wrapper at 0x00703B60
    fld arg; call __CIacos; ret.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Gen_00703b60_Acos.cpp
@@ -0,0 +1,11 @@
+// cl: /DNDEBUG /MD /EHsc
+
+// Retail 0x00703B60. fld arg; call __CIacos; ret.
+
+#include <math.h>
+
+// ?acos_00703b60@@YAMM@Z
+float acos_00703b60(float v)
+{
+	return (float)acos(v);
+}
