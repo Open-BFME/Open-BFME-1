bf7c33ae35f05af7eb9470bd9a23257056dff627
Jonathan <jonathan@users.noreply.github.com>
2026-08-20 15:59:04 +0000

    Open-BFME5: convert the 0x44-byte zero constructor at 0x007049B0
    Zeros +0..+14, +38..+40, then +18..+34.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Gen_007049b0_Ctor.cpp
@@ -0,0 +1,50 @@
+// cl: /DNDEBUG /MD /EHsc
+
+// Retail 0x007049B0. Constructor: zero +0..+14, +38..+40, then +18..+34.
+
+class Gen_007049b0
+{
+public:
+	Gen_007049b0(void);
+
+private:
+	int m_0;
+	int m_4;
+	int m_8;
+	int m_c;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1c;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2c;
+	int m_30;
+	int m_34;
+	int m_38;
+	int m_3c;
+	int m_40;
+};
+
+// ??0Gen_007049b0@@QAE@XZ
+Gen_007049b0::Gen_007049b0(void)
+{
+	m_0 = 0;
+	m_4 = 0;
+	m_8 = 0;
+	m_c = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_38 = 0;
+	m_3c = 0;
+	m_40 = 0;
+	m_18 = 0;
+	m_1c = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2c = 0;
+	m_30 = 0;
+	m_34 = 0;
+}
