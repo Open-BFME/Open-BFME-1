b147e1edcc82218815723337d89fe5523ad8f65d
Jonathan <jonathan@users.noreply.github.com>
2026-08-20 16:18:43 +0000

    Open-BFME5: convert the virtual tail-call at 0x007309B0
    Tail-call m_10 virtual slot 0x214.

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Gen_007309b0_Virt.cpp
@@ -0,0 +1,158 @@
+// cl: /DNDEBUG /MD /EHsc
+
+// Retail 0x007309B0. Tail-call m_10 virtual slot 0x214.
+
+class Gen_007309b0_Iface
+{
+public:
+	virtual int v0(void);
+	virtual int v1(void);
+	virtual int v2(void);
+	virtual int v3(void);
+	virtual int v4(void);
+	virtual int v5(void);
+	virtual int v6(void);
+	virtual int v7(void);
+	virtual int v8(void);
+	virtual int v9(void);
+	virtual int v10(void);
+	virtual int v11(void);
+	virtual int v12(void);
+	virtual int v13(void);
+	virtual int v14(void);
+	virtual int v15(void);
+	virtual int v16(void);
+	virtual int v17(void);
+	virtual int v18(void);
+	virtual int v19(void);
+	virtual int v20(void);
+	virtual int v21(void);
+	virtual int v22(void);
+	virtual int v23(void);
+	virtual int v24(void);
+	virtual int v25(void);
+	virtual int v26(void);
+	virtual int v27(void);
+	virtual int v28(void);
+	virtual int v29(void);
+	virtual int v30(void);
+	virtual int v31(void);
+	virtual int v32(void);
+	virtual int v33(void);
+	virtual int v34(void);
+	virtual int v35(void);
+	virtual int v36(void);
+	virtual int v37(void);
+	virtual int v38(void);
+	virtual int v39(void);
+	virtual int v40(void);
+	virtual int v41(void);
+	virtual int v42(void);
+	virtual int v43(void);
+	virtual int v44(void);
+	virtual int v45(void);
+	virtual int v46(void);
+	virtual int v47(void);
+	virtual int v48(void);
+	virtual int v49(void);
+	virtual int v50(void);
+	virtual int v51(void);
+	virtual int v52(void);
+	virtual int v53(void);
+	virtual int v54(void);
+	virtual int v55(void);
+	virtual int v56(void);
+	virtual int v57(void);
+	virtual int v58(void);
+	virtual int v59(void);
+	virtual int v60(void);
+	virtual int v61(void);
+	virtual int v62(void);
+	virtual int v63(void);
+	virtual int v64(void);
+	virtual int v65(void);
+	virtual int v66(void);
+	virtual int v67(void);
+	virtual int v68(void);
+	virtual int v69(void);
+	virtual int v70(void);
+	virtual int v71(void);
+	virtual int v72(void);
+	virtual int v73(void);
+	virtual int v74(void);
+	virtual int v75(void);
+	virtual int v76(void);
+	virtual int v77(void);
+	virtual int v78(void);
+	virtual int v79(void);
+	virtual int v80(void);
+	virtual int v81(void);
+	virtual int v82(void);
+	virtual int v83(void);
+	virtual int v84(void);
+	virtual int v85(void);
+	virtual int v86(void);
+	virtual int v87(void);
+	virtual int v88(void);
+	virtual int v89(void);
+	virtual int v90(void);
+	virtual int v91(void);
+	virtual int v92(void);
+	virtual int v93(void);
+	virtual int v94(void);
+	virtual int v95(void);
+	virtual int v96(void);
+	virtual int v97(void);
+	virtual int v98(void);
+	virtual int v99(void);
+	virtual int v100(void);
+	virtual int v101(void);
+	virtual int v102(void);
+	virtual int v103(void);
+	virtual int v104(void);
+	virtual int v105(void);
+	virtual int v106(void);
+	virtual int v107(void);
+	virtual int v108(void);
+	virtual int v109(void);
+	virtual int v110(void);
+	virtual int v111(void);
+	virtual int v112(void);
+	virtual int v113(void);
+	virtual int v114(void);
+	virtual int v115(void);
+	virtual int v116(void);
+	virtual int v117(void);
+	virtual int v118(void);
+	virtual int v119(void);
+	virtual int v120(void);
+	virtual int v121(void);
+	virtual int v122(void);
+	virtual int v123(void);
+	virtual int v124(void);
+	virtual int v125(void);
+	virtual int v126(void);
+	virtual int v127(void);
+	virtual int v128(void);
+	virtual int v129(void);
+	virtual int v130(void);
+	virtual int v131(void);
+	virtual int v132(void);
+	virtual int v133(void);
+};
+
+class Gen_007309b0
+{
+public:
+	int call(void);
+
+private:
+	unsigned char m_pad[0x10];
+	Gen_007309b0_Iface *m_10;
+};
+
+// ?call@Gen_007309b0@@QAEHXZ
+int Gen_007309b0::call(void)
+{
+	return m_10->v133();
+}
