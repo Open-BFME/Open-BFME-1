32e3a3f996f70301b70208de1b46ee1b19b21d9f
dginovker <danielginovker@gmail.com>
2026-08-28 16:28:45 -0700

    Bank AIUpdate ground-movement reconstruction

---
+++ reverse/attempts/0x00273080.cpp
@@ -0,0 +1,83 @@
+// ?isDoingGroundMovement@AIUpdateInterface@@UBE_NXZ
+// partial score=0.96 date=2026-08-28
+// cl: /DNDEBUG /MD /EHsc
+// readable body: Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate.cpp
+
+typedef bool Bool;
+typedef unsigned int UnsignedInt;
+
+enum LocomotorSurfaceType
+{
+	LOCOMOTORSURFACE_AIR = 1 << 3
+};
+
+class LocomotorOverridable
+{
+public:
+	virtual ~LocomotorOverridable();
+	LocomotorOverridable *friend_getFinalOverride();
+
+	LocomotorOverridable *m_nextOverride;
+};
+
+class Locomotor : public LocomotorOverridable
+{
+public:
+	UnsignedInt getLegalSurfaces() const
+	{
+		LocomotorOverridable *o = m_nextOverride;
+		if( o && o->m_nextOverride )
+			o = o->m_nextOverride->friend_getFinalOverride();
+		return static_cast<const Locomotor *>(o)->m_legalSurfaces;
+	}
+
+private:
+	unsigned char m_unmodelled_008[ 0x10 - 8 ];
+	UnsignedInt m_legalSurfaces;
+};
+
+class Object
+{
+public:
+	__forceinline Bool isNotHeldDisabled() const
+	{
+		unsigned char disabled = m_disabledMask;
+		disabled >>= 3;
+		disabled = ~disabled;
+		disabled &= 1;
+		return disabled;
+	}
+
+	unsigned char m_unmodelled_000[ 0x1A4 ];
+	unsigned char m_disabledMask;
+};
+
+class AIUpdateInterface
+{
+public:
+	virtual Bool isDoingGroundMovement() const;
+
+private:
+	unsigned char m_unmodelled_004[ 4 ];
+	Object *m_object;
+	unsigned char m_unmodelled_00C[ 0x1B8 - 0x0C ];
+	UnsignedInt m_validLocomotorSurfaces;
+	unsigned char m_unmodelled_1BC[ 0x1CC - 0x1BC ];
+	Locomotor *m_curLocomotor;
+};
+
+// ?isDoingGroundMovement@AIUpdateInterface@@UBE_NXZ
+Bool AIUpdateInterface::isDoingGroundMovement() const
+{
+	if( m_validLocomotorSurfaces == LOCOMOTORSURFACE_AIR )
+		return false;
+
+	Locomotor *curLocomotor = m_curLocomotor;
+	if( curLocomotor == 0 )
+		return false;
+
+	if( curLocomotor->getLegalSurfaces() & LOCOMOTORSURFACE_AIR )
+		return false;
+
+	return m_object->isNotHeldDisabled();
+}
