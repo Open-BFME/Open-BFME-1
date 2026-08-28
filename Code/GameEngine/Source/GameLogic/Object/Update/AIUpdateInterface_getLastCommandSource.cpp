e4d9398f18531ab41d3b9954f6b574ebae10f994
dginovker <danielginovker@gmail.com>
2026-08-28 15:52:49 -0700

    Match AIUpdate last command source getter

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/AIUpdateInterface_getLastCommandSource.cpp
@@ -0,0 +1,21 @@
+// cl: /DNDEBUG /MD /EHsc
+// readable body: Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate.cpp
+
+enum CommandSourceType {};
+
+class AIUpdateInterface
+{
+public:
+	virtual CommandSourceType getLastCommandSource() const;
+
+private:
+	// BFME carries eight fewer bytes than the Zero Hour interface before this field.
+	unsigned char m_unmodelled_04[ 0x48 - 4 ];
+	CommandSourceType m_lastCommandSource;
+};
+
+// ?getLastCommandSource@AIUpdateInterface@@UBE?AW4CommandSourceType@@XZ
+CommandSourceType AIUpdateInterface::getLastCommandSource() const
+{
+	return m_lastCommandSource;
+}
