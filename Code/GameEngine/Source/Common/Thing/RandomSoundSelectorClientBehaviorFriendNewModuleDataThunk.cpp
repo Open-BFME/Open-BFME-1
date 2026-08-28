e8735313557624a4a440eb46383189b9c638a3f4
dginovker <danielginovker@gmail.com>
2026-08-28 15:46:47 -0700

    Recover RandomSoundSelector module data constructor

--- Code/GameEngine/Source/Common/Thing/RandomSoundSelectorClientBehaviorFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/Common/Thing/RandomSoundSelectorClientBehaviorFriendNewModuleDataThunk.cpp
@@ -16,15 +16,84 @@ class ModuleData;
 void *__cdecl operator new(unsigned int);
 void __cdecl operator delete(void *);
 
-class RandomSoundSelectorClientBehaviorModuleData
+class ModuleData
+{
+public:
+	virtual ~ModuleData();
+
+private:
+	unsigned int m_baseField;
+};
+
+class RandomSoundSelectorSoundNames
+{
+public:
+	RandomSoundSelectorSoundNames()
+	{
+		for (int i = 0; i < 109; ++i)
+			m_values[i] = 0;
+	}
+	~RandomSoundSelectorSoundNames();
+
+private:
+	unsigned int m_values[109];
+};
+
+class RandomSoundSelectorMap
+{
+	struct Node
+	{
+		unsigned char m_flag;
+		unsigned char m_pad[3];
+		unsigned int m_value;
+		Node *m_next;
+		Node *m_previous;
+		unsigned char m_payload[0x74];
+	};
+
+public:
+	RandomSoundSelectorMap()
+	{
+		m_node = 0;
+		m_node = (Node *)operator new(sizeof(Node));
+		m_count = 0;
+		m_node->m_flag = 0;
+		m_node->m_value = 0;
+		m_node->m_next = m_node;
+		m_node->m_previous = m_node;
+	}
+	~RandomSoundSelectorMap();
+
+private:
+	Node *m_node;
+	unsigned int m_count;
+	unsigned int m_reserved;
+};
+
+class RandomSoundSelectorClientBehaviorModuleData : public ModuleData
 {
 public:
 	RandomSoundSelectorClientBehaviorModuleData();
 	virtual ~RandomSoundSelectorClientBehaviorModuleData();
 
 private:
-	unsigned char m_pad[0x1D0];
+	RandomSoundSelectorSoundNames m_soundNames;
+	RandomSoundSelectorMap m_soundMap;
+	float m_defaultVolume;
+	unsigned int m_selectionIndex;
+	bool m_shuffle;
+	bool m_enabled;
+	unsigned char m_pad[2];
 };
+
+// ??0RandomSoundSelectorClientBehaviorModuleData@@QAE@XZ
+RandomSoundSelectorClientBehaviorModuleData::RandomSoundSelectorClientBehaviorModuleData()
+	: m_defaultVolume(0.5f),
+	  m_selectionIndex(0),
+	  m_shuffle(false),
+	  m_enabled(true)
+{
+}
 
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/Common/INI.h
 class INI
