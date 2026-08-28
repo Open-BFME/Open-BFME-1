a4f2437f94afb336986e7b47e446de15f05336fa
dginovker <danielginovker@gmail.com>
2026-08-28 16:03:11 -0700

    Convert BridgeBehavior scaffolding removal

--- Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeBehaviorOnHealing.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeBehaviorOnHealing.cpp
@@ -11,6 +11,15 @@ typedef float Real;
 typedef unsigned int UnsignedInt;
 enum ObjectID { INVALID_ID = 0 };
 enum KindOfType { KINDOF_BRIDGE_TOWER = 24 };
+enum BodyDamageType { BODY_RUBBLE = 3 };
+enum PathfindLayerEnum {};
+
+struct Coord3D
+{
+	Real x;
+	Real y;
+	Real z;
+};
 
 class DamageInfo;
 
@@ -24,6 +33,8 @@ public:
 	virtual void unused04();
 	virtual void unused05();
 	virtual Real getMaxHealth() const;
+	virtual void unused07();
+	virtual BodyDamageType getDamageState() const;
 };
 
 class Overridable
@@ -79,10 +90,13 @@ public:
 
 	BodyModuleInterface *getBodyModule() const { return m_body; }
 	ObjectID getID() const { return m_id; }
+	const Coord3D *getPosition() const { return &m_position; }
 
 private:
 	ThingTemplate *m_template;
-	unsigned char m_unreconstructed08[0x74 - 0x08];
+	unsigned char m_unreconstructed08[0x38 - 0x08];
+	Coord3D m_position;
+	unsigned char m_unreconstructed44[0x74 - 0x44];
 	ObjectID m_id;
 	unsigned char m_unreconstructed78[0x200 - 0x78];
 	BodyModuleInterface *m_body;
@@ -127,6 +141,7 @@ public:
 	virtual void unused00();
 	virtual void unused01();
 	virtual int getCurrentMotion() const;
+	virtual void reverseMotion();
 };
 
 class BridgeScaffoldBehavior
@@ -134,6 +149,77 @@ class BridgeScaffoldBehavior
 public:
 	static BridgeScaffoldBehaviorInterface *getBridgeScaffoldBehaviorInterfaceFromObject(Object *object);
 };
+
+void __stlp_deallocate_small(void *node, UnsignedInt bytes);
+
+template <class T>
+class BridgeScaffoldAllocator : public _STL::allocator<T>
+{
+public:
+	template <class U> struct rebind { typedef BridgeScaffoldAllocator<U> other; };
+
+	BridgeScaffoldAllocator() {}
+	template <class U> BridgeScaffoldAllocator(const BridgeScaffoldAllocator<U> &) {}
+
+	void deallocate(T *node, UnsignedInt count) const
+	{
+		__stlp_deallocate_small(node, count * sizeof(T));
+	}
+};
+
+template <class T, class U>
+BridgeScaffoldAllocator<U> &__stl_alloc_rebind(
+	BridgeScaffoldAllocator<T> &allocator, const U *)
+{
+	return (BridgeScaffoldAllocator<U> &)allocator;
+}
+
+typedef _STL::list<ObjectID, BridgeScaffoldAllocator<ObjectID> > BridgeScaffoldList;
+
+class Bridge
+{
+public:
+	PathfindLayerEnum getLayer() const { return m_layer; }
+
+private:
+	unsigned char m_unreconstructed00[0x88];
+	PathfindLayerEnum m_layer;
+};
+
+class TerrainLogic
+{
+public:
+	virtual void unused00(); virtual void unused01(); virtual void unused02(); virtual void unused03();
+	virtual void unused04(); virtual void unused05(); virtual void unused06(); virtual void unused07();
+	virtual void unused08(); virtual void unused09(); virtual void unused10(); virtual void unused11();
+	virtual void unused12(); virtual void unused13(); virtual void unused14(); virtual void unused15();
+	virtual void unused16(); virtual void unused17(); virtual void unused18(); virtual void unused19();
+	virtual void unused20(); virtual void unused21(); virtual void unused22(); virtual void unused23();
+	virtual void unused24(); virtual void unused25(); virtual void unused26(); virtual void unused27();
+	virtual void unused28(); virtual void unused29(); virtual void unused30(); virtual void unused31();
+	virtual void unused32(); virtual void unused33(); virtual void unused34(); virtual void unused35();
+	virtual void unused36(); virtual void unused37();
+	virtual Bridge *findBridgeAt(const Coord3D *position);
+};
+
+class Pathfinder
+{
+public:
+	void friend_changeBridgeState(PathfindLayerEnum layer, bool open);
+};
+
+class AI
+{
+public:
+	Pathfinder *pathfinder() const { return m_pathfinder; }
+
+private:
+	unsigned char m_unreconstructed00[0x0C];
+	Pathfinder *m_pathfinder;
+};
+
+extern TerrainLogic *TheTerrainLogic;
+extern AI *TheAI;
 
 class DamageInfo
 {
@@ -156,14 +242,17 @@ public:
 	virtual void onHealing(DamageInfo *damageInfo);
 	virtual void onDamage(DamageInfo *damageInfo);
 	virtual bool isScaffoldInMotion();
+	virtual void removeScaffolding();
 	Object *getObject() const
 	{
 		return *(Object **)((unsigned char *)this - 0x1C);
 	}
 
 private:
-	unsigned char m_unreconstructed04[0x460 - 0x04];
-	_STL::list<ObjectID> m_scaffoldObjectIDList;
+	unsigned char m_unreconstructed04[0x45D - 0x04];
+	bool m_scaffoldPresent;
+	unsigned char m_unreconstructed45E[0x460 - 0x45E];
+	BridgeScaffoldList m_scaffoldObjectIDList;
 };
 
 // ?onHealing@BridgeBehavior@@UAEXPAVDamageInfo@@@Z
@@ -191,7 +280,7 @@ void BridgeBehavior::onHealing(DamageInfo *damageInfo)
 // ?isScaffoldInMotion@BridgeBehavior@@UAE_NXZ
 bool BridgeBehavior::isScaffoldInMotion()
 {
-	for (_STL::list<ObjectID>::iterator it = m_scaffoldObjectIDList.begin();
+	for (BridgeScaffoldList::iterator it = m_scaffoldObjectIDList.begin();
 		it != m_scaffoldObjectIDList.end(); ++it)
 	{
 		Object *object = TheGameLogic->findObjectByIDInline(*it);
@@ -205,6 +294,36 @@ bool BridgeBehavior::isScaffoldInMotion()
 	}
 
 	return false;
+}
+
+// ?removeScaffolding@BridgeBehavior@@UAEXXZ
+void BridgeBehavior::removeScaffolding()
+{
+	if (!m_scaffoldPresent)
+		return;
+
+	for (BridgeScaffoldList::iterator it = m_scaffoldObjectIDList.begin();
+		it != m_scaffoldObjectIDList.end(); ++it)
+	{
+		Object *object = TheGameLogic->findObjectByID(*it);
+		if (object == 0)
+			continue;
+
+		BridgeScaffoldBehaviorInterface *scaffold =
+			BridgeScaffoldBehavior::getBridgeScaffoldBehaviorInterfaceFromObject(object);
+		scaffold->reverseMotion();
+	}
+
+	m_scaffoldObjectIDList.clear();
+	m_scaffoldPresent = false;
+
+	Object *object = *(Object **)((unsigned char *)this - 0x18);
+	if (object->getBodyModule()->getDamageState() != BODY_RUBBLE)
+	{
+		Bridge *bridge = TheTerrainLogic->findBridgeAt(object->getPosition());
+		if (bridge)
+			TheAI->pathfinder()->friend_changeBridgeState(bridge->getLayer(), true);
+	}
 }
 
 // ?onDamage@BridgeBehavior@@UAEXPAVDamageInfo@@@Z
