14d8b4afcf784e26fcb6f42b24c2db2a202983a0
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-29 03:49:57 -0400

    Convert Object last-shot position query

---
+++ Code/GameEngine/Source/GameLogic/Object/Object_bfmeGetLastShotPosition.cpp
@@ -0,0 +1,47 @@
+// cl: /DNDEBUG /MD /EHsc
+
+union BfmeFiringPositionWord
+{
+	float f;
+	volatile unsigned int u;
+};
+
+struct BfmeFiringPosition
+{
+	BfmeFiringPosition() {}
+	BfmeFiringPosition(const BfmeFiringPosition &other)
+	{
+		x.u = other.x.u;
+		y.u = other.y.u;
+		z.u = other.z.u;
+	}
+	BfmeFiringPositionWord x;
+	BfmeFiringPositionWord y;
+	BfmeFiringPositionWord z;
+};
+
+class BfmeFiringTracker
+{
+public:
+	BfmeFiringPosition bfmeGetLastShotPosition() const;
+};
+
+class Object
+{
+public:
+	BfmeFiringPosition bfmeGetLastShotPosition() const;
+
+private:
+	unsigned char m_pad[0x1ec];
+	BfmeFiringTracker *m_firingTracker;
+};
+
+BfmeFiringPosition Object::bfmeGetLastShotPosition() const
+{
+	volatile float scratch = 0.0f;
+	BfmeFiringPosition empty;
+	empty.x.f = 0.0f;
+	empty.y.f = 0.0f;
+	empty.z.f = 0.0f;
+	return m_firingTracker ? m_firingTracker->bfmeGetLastShotPosition() : empty;
+}
