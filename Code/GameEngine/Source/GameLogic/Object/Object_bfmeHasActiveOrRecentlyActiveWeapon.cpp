7d09d52b35e1999f3a5cc27ca1cb7bccd8dad5f0
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-29 02:59:23 -0400

    Convert Object weapon activity predicate

---
+++ Code/GameEngine/Source/GameLogic/Object/Object_bfmeHasActiveOrRecentlyActiveWeapon.cpp
@@ -0,0 +1,72 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+typedef unsigned int UnsignedInt;
+
+class Object;
+
+enum WeaponStatus
+{
+	READY_TO_FIRE = 0
+};
+
+class Weapon
+{
+friend class Object;
+
+private:
+	WeaponStatus bfmeComputeStatus(Bool *unknown) const;
+};
+
+class FiringTracker
+{
+public:
+	unsigned char m_pad[0x44];
+	UnsignedInt m_recentActivityFrame;
+};
+
+class GameLogic
+{
+public:
+	unsigned char m_pad[0x3c];
+	UnsignedInt m_frame;
+};
+
+extern GameLogic *TheGameLogic;
+
+class Object
+{
+public:
+	Bool bfmeHasActiveOrRecentlyActiveWeapon() const;
+
+private:
+	unsigned char m_pad000[0x1ec];
+	FiringTracker *m_firingTracker;
+	unsigned char m_pad1f0[0x26c - 0x1f0];
+	Weapon *m_weapons[4];
+	int m_currentWeaponSlot;
+	unsigned char m_pad280[4];
+	void *m_currentWeaponTemplateSet;
+};
+
+Bool Object::bfmeHasActiveOrRecentlyActiveWeapon() const
+{
+	if (m_currentWeaponTemplateSet)
+	{
+		Weapon *weapon = m_weapons[m_currentWeaponSlot];
+		if (weapon)
+		{
+			if (weapon->bfmeComputeStatus(0) != READY_TO_FIRE)
+				return true;
+
+			FiringTracker *tracker = m_firingTracker;
+			if (tracker)
+			{
+				UnsignedInt recentFrame = tracker->m_recentActivityFrame;
+				if (recentFrame > 2 && recentFrame + 10 > TheGameLogic->m_frame)
+					return true;
+			}
+		}
+	}
+	return false;
+}
