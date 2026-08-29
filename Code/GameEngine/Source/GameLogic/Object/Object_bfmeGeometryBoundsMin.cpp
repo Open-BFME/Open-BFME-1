108982571de6aa069c490b9b362caacf7a3b194f
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-29 03:39:01 -0400

    Convert Object geometry lower bound

---
+++ Code/GameEngine/Source/GameLogic/Object/Object_bfmeGeometryBoundsMin.cpp
@@ -0,0 +1,59 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef float Real;
+
+struct Coord3D
+{
+	Real x, y, z;
+	void set(Real xIn, Real yIn, Real zIn)
+	{
+		x = xIn;
+		y = yIn;
+		z = zIn;
+	}
+};
+
+class GeometryInfo
+{
+public:
+	Real getMaxHeightBelowPosition() const;
+	inline Real getMajorRadius() const { return m_majorRadius; }
+
+private:
+	unsigned char m_unmodelled_000[0x10];
+	Real m_majorRadius;
+};
+
+class ObjectGeometryBoundsData
+{
+public:
+	unsigned char m_unmodelled_000[0x38];
+	Coord3D m_position;
+	unsigned char m_unmodelled_044[0xac - 0x44];
+	GeometryInfo m_geometryInfo;
+};
+
+class ObjectGeometryBoundsShim
+{
+public:
+	void getMin(Coord3D &out) const;
+	__forceinline const ObjectGeometryBoundsData *getObject() const
+	{
+		return reinterpret_cast<const ObjectGeometryBoundsData *>(
+			reinterpret_cast<const char *>(this) - 0x70);
+	}
+};
+
+void ObjectGeometryBoundsShim::getMin(Coord3D &out) const
+{
+	const ObjectGeometryBoundsData *object = getObject();
+	const GeometryInfo &geometry = object->m_geometryInfo;
+	Coord3D result;
+	result.x = object->m_position.x;
+	result.y = object->m_position.y;
+	result.z = object->m_position.z;
+	result.z -= geometry.getMaxHeightBelowPosition();
+	result.x -= geometry.getMajorRadius();
+	result.y -= geometry.getMajorRadius();
+	out.set(result.x, result.y, result.z);
+}
