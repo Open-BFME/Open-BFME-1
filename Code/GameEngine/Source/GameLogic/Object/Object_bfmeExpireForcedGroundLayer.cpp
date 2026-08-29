ad1c13bcad0948187b9a00cbaa1dbccb377424ea
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-29 04:06:53 -0400

    Convert Object forced-ground-layer expiry

---
+++ Code/GameEngine/Source/GameLogic/Object/Object_bfmeExpireForcedGroundLayer.cpp
@@ -0,0 +1,71 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+typedef unsigned int UnsignedInt;
+
+struct Coord3D
+{
+	Coord3D() {}
+	Coord3D(float ax, float ay, float az) : x(ax), y(ay), z(az) {}
+	Coord3D(const Coord3D &other)
+		: x(other.x), y(other.y), z(other.z) {}
+	~Coord3D() {}
+
+	float x;
+	float y;
+	float z;
+};
+
+class Object;
+
+class Pathfinder
+{
+public:
+	int bfmeLayerForPosition(Object *object, Coord3D position);
+};
+
+class AI
+{
+public:
+	unsigned char m_pad[0x0c];
+	Pathfinder *m_pathfinder;
+};
+
+class GameLogic
+{
+public:
+	unsigned char m_pad[0x3c];
+	UnsignedInt m_frame;
+};
+
+extern AI *TheAI;
+extern GameLogic *TheGameLogic;
+
+class Object
+{
+public:
+	void bfmeExpireForcedGroundLayer();
+
+private:
+	unsigned char m_pad000[0x38];
+	Coord3D m_position;
+	unsigned char m_pad044[0x3a8 - 0x44];
+	Bool m_forceGroundLayer;
+	unsigned char m_pad3a9[3];
+	UnsignedInt m_forceGroundLayerFrame;
+};
+
+void Object::bfmeExpireForcedGroundLayer()
+{
+	if (m_forceGroundLayer &&
+		m_forceGroundLayerFrame + 13 <= TheGameLogic->m_frame)
+	{
+		Coord3D position(m_position);
+		if (TheAI->m_pathfinder->bfmeLayerForPosition(
+			this, position) == 1)
+		{
+			m_forceGroundLayer = false;
+			m_forceGroundLayerFrame = static_cast<UnsignedInt>(-1);
+		}
+	}
+}
