58be41be83062d5b3c10119a8d8e25f675e36d01
argalad <github@argalad.com>
2026-08-29 16:46:39 +0200

    Open-BFME5: convert a three-slot release loop and a guarded tail dispatch

---
+++ Code/GameEngine/Source/Common/BfmeConv1083.cpp
@@ -0,0 +1,191 @@
+// Open-BFME5 conversions.
+
+class BfmeS1083
+{
+public:
+	virtual void bfmeSlot1083S_0(void);
+	virtual void bfmeSlot1083S_1(void);
+};
+
+extern BfmeS1083 *g_bfmeS1083_0;
+extern BfmeS1083 *g_bfmeS1083_1;
+extern BfmeS1083 *g_bfmeS1083_2;
+
+void bfmeGo1083A(void)
+{
+	if (g_bfmeS1083_0) {
+		g_bfmeS1083_0->bfmeSlot1083S_1();
+		g_bfmeS1083_0 = 0;
+	}
+	if (g_bfmeS1083_1) {
+		g_bfmeS1083_1->bfmeSlot1083S_1();
+		g_bfmeS1083_1 = 0;
+	}
+	if (g_bfmeS1083_2) {
+		g_bfmeS1083_2->bfmeSlot1083S_1();
+		g_bfmeS1083_2 = 0;
+	}
+}
+
+class BfmeD1083
+{
+public:
+	virtual void bfmeSlot1083D_0(void);
+	virtual void bfmeSlot1083D_1(void);
+	virtual void bfmeSlot1083D_2(void);
+	virtual void bfmeSlot1083D_3(void);
+	virtual void bfmeSlot1083D_4(void);
+	virtual void bfmeSlot1083D_5(void);
+	virtual void bfmeSlot1083D_6(void);
+	virtual void bfmeSlot1083D_7(void);
+	virtual void bfmeSlot1083D_8(void);
+	virtual void bfmeSlot1083D_9(void);
+	virtual void bfmeSlot1083D_10(void);
+	virtual void bfmeSlot1083D_11(void);
+	virtual void bfmeSlot1083D_12(void);
+	virtual void bfmeSlot1083D_13(void);
+	virtual void bfmeSlot1083D_14(void);
+	virtual void bfmeSlot1083D_15(void);
+	virtual void bfmeSlot1083D_16(void);
+	virtual void bfmeSlot1083D_17(void);
+	virtual void bfmeSlot1083D_18(void);
+	virtual void bfmeSlot1083D_19(void);
+	virtual void bfmeSlot1083D_20(void);
+	virtual void bfmeSlot1083D_21(void);
+	virtual void bfmeSlot1083D_22(void);
+	virtual void bfmeSlot1083D_23(void);
+	virtual void bfmeSlot1083D_24(void);
+	virtual void bfmeSlot1083D_25(void);
+	virtual void bfmeSlot1083D_26(void);
+	virtual void bfmeSlot1083D_27(void);
+	virtual void bfmeSlot1083D_28(void);
+	virtual void bfmeSlot1083D_29(void);
+	virtual void bfmeSlot1083D_30(void);
+	virtual void bfmeSlot1083D_31(void);
+	virtual void bfmeSlot1083D_32(void);
+	virtual void bfmeSlot1083D_33(void);
+	virtual void bfmeSlot1083D_34(void);
+	virtual void bfmeSlot1083D_35(void);
+	virtual void bfmeSlot1083D_36(void);
+	virtual void bfmeSlot1083D_37(void);
+	virtual void bfmeSlot1083D_38(void);
+	virtual void bfmeSlot1083D_39(void);
+	virtual void bfmeSlot1083D_40(void);
+	virtual void bfmeSlot1083D_41(void);
+	virtual void bfmeSlot1083D_42(void);
+	virtual void bfmeSlot1083D_43(void);
+	virtual void bfmeSlot1083D_44(void);
+	virtual void bfmeSlot1083D_45(void);
+	virtual void bfmeSlot1083D_46(void);
+	virtual void bfmeSlot1083D_47(void);
+	virtual void bfmeSlot1083D_48(void);
+	virtual void bfmeSlot1083D_49(void);
+	virtual void bfmeSlot1083D_50(void);
+	virtual void bfmeSlot1083D_51(void);
+	virtual void bfmeSlot1083D_52(void);
+	virtual void bfmeSlot1083D_53(void);
+	virtual void bfmeSlot1083D_54(void);
+	virtual void bfmeSlot1083D_55(void);
+	virtual void bfmeSlot1083D_56(void);
+	virtual void bfmeSlot1083D_57(void);
+	virtual void bfmeSlot1083D_58(void);
+	virtual void bfmeSlot1083D_59(void);
+	virtual void bfmeSlot1083D_60(void);
+	virtual void bfmeSlot1083D_61(void);
+	virtual void bfmeSlot1083D_62(void);
+	virtual void bfmeSlot1083D_63(void);
+	virtual void bfmeSlot1083D_64(void);
+	virtual void bfmeSlot1083D_65(void);
+	virtual void bfmeSlot1083D_66(void);
+	virtual void bfmeSlot1083D_67(void);
+	virtual void bfmeSlot1083D_68(void);
+	virtual void bfmeSlot1083D_69(void);
+	virtual void bfmeSlot1083D_70(void);
+	virtual void bfmeSlot1083D_71(void);
+	virtual void bfmeSlot1083D_72(void);
+	virtual void bfmeSlot1083D_73(void);
+	virtual void bfmeSlot1083D_74(void);
+	virtual void bfmeSlot1083D_75(void);
+	virtual void bfmeSlot1083D_76(void);
+	virtual void bfmeSlot1083D_77(void);
+	virtual void bfmeSlot1083D_78(void);
+	virtual void bfmeSlot1083D_79(void);
+	virtual void bfmeSlot1083D_80(void);
+	virtual void bfmeSlot1083D_81(void);
+	virtual void bfmeSlot1083D_82(void);
+	virtual void bfmeSlot1083D_83(void);
+	virtual void bfmeSlot1083D_84(void);
+	virtual void bfmeSlot1083D_85(void);
+	virtual void bfmeSlot1083D_86(void);
+	virtual void bfmeSlot1083D_87(void);
+	virtual void bfmeSlot1083D_88(void);
+	virtual void bfmeSlot1083D_89(void);
+	virtual void bfmeSlot1083D_90(void);
+	virtual void bfmeSlot1083D_91(void);
+	virtual void bfmeSlot1083D_92(void);
+	virtual void bfmeSlot1083D_93(void);
+	virtual void bfmeSlot1083D_94(void);
+	virtual void bfmeSlot1083D_95(void);
+	virtual void bfmeSlot1083D_96(void);
+	virtual void bfmeSlot1083D_97(void);
+	virtual void bfmeSlot1083D_98(void);
+	virtual void bfmeSlot1083D_99(void);
+	virtual void bfmeSlot1083D_100(void);
+	virtual void bfmeSlot1083D_101(void);
+	virtual void bfmeSlot1083D_102(void);
+	virtual void bfmeSlot1083D_103(void);
+	virtual void bfmeSlot1083D_104(void);
+	virtual void bfmeSlot1083D_105(void);
+	virtual void bfmeSlot1083D_106(void);
+	virtual void bfmeSlot1083D_107(void);
+	virtual void bfmeSlot1083D_108(void);
+	virtual void bfmeSlot1083D_109(void);
+	virtual void bfmeSlot1083D_110(void);
+	virtual void bfmeSlot1083D_111(void);
+	virtual void bfmeSlot1083D_112(void);
+	virtual void bfmeSlot1083D_113(void);
+	virtual char bfmeSlot1083D_114(void);
+};
+
+extern BfmeD1083 *g_bfmeD1083;
+
+class BfmeB1083
+{
+public:
+	char bfmeChk1083(void);
+};
+
+extern BfmeB1083 *g_bfmeB1083;
+
+class BfmeZ1083
+{
+public:
+	virtual void bfmeSlot1083Z_0(void);
+	virtual void bfmeSlot1083Z_1(void);
+	virtual void bfmeSlot1083Z_2(void);
+	virtual void bfmeSlot1083Z_3(void);
+	virtual void bfmeSlot1083Z_4(void);
+	virtual void bfmeSlot1083Z_5(void);
+	virtual void bfmeSlot1083Z_6(void);
+	virtual void bfmeSlot1083Z_7(void);
+	virtual void bfmeSlot1083Z_8(void);
+	virtual void bfmeSlot1083Z_9(void);
+	virtual void bfmeSlot1083Z_10(void);
+	virtual void bfmeSlot1083Z_11(void);
+	virtual void bfmeSlot1083Z_12(void);
+	virtual void bfmeSlot1083Z_13(void);
+	virtual void bfmeSlot1083Z_14(void);
+	virtual void bfmeSlot1083Z_15(void);
+	virtual void bfmeSlot1083Z_16(void);
+	virtual void bfmeSlot1083Z_17(void);
+};
+
+extern BfmeZ1083 *g_bfmeZ1083;
+
+void bfmeGo1083B(void)
+{
+	if (g_bfmeD1083 && g_bfmeD1083->bfmeSlot1083D_114() && !g_bfmeB1083->bfmeChk1083())
+		g_bfmeZ1083->bfmeSlot1083Z_17();
+	else
+		g_bfmeZ1083->bfmeSlot1083Z_17();
+}
