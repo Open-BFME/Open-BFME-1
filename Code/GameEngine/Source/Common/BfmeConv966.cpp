f02e0a3999049926bd24edb1b8b5d15022e7c630
argalad <github@argalad.com>
2026-08-29 07:50:15 +0200

    Open-BFME5: convert two vftable-restore prologues
    Ten vftable pointers written into the slots at 0, 0xc, 0x10, 0x20-0x34 and
    0xd4, then a tail jump into the shared base routine at 0x00039D6A.  Volatile
    slots keep the ten stores in retail order.

---
+++ Code/GameEngine/Source/Common/BfmeConv966.cpp
@@ -0,0 +1,93 @@
+// Open-BFME5 conversions.
+
+extern char g_bfme966A0[];
+extern char g_bfme966A1[];
+extern char g_bfme966A2[];
+extern char g_bfme966A3[];
+extern char g_bfme966A4[];
+extern char g_bfme966A5[];
+extern char g_bfme966A6[];
+extern char g_bfme966A7[];
+extern char g_bfme966A8[];
+extern char g_bfme966A9[];
+
+class BfmeVft966A
+{
+public:
+	void bfmeGo966A();
+	void bfmeBase966A();
+	char *volatile m_bfmeSlot0;
+	char m_bfmePad0[0x8];
+	char *volatile m_bfmeSlot1;
+	char *volatile m_bfmeSlot2;
+	char m_bfmePad1[0xc];
+	char *volatile m_bfmeSlot3;
+	char *volatile m_bfmeSlot4;
+	char *volatile m_bfmeSlot5;
+	char *volatile m_bfmeSlot6;
+	char *volatile m_bfmeSlot7;
+	char *volatile m_bfmeSlot8;
+	char m_bfmePad2[0x9c];
+	char *volatile m_bfmeSlot9;
+};
+
+void BfmeVft966A::bfmeGo966A()
+{
+	m_bfmeSlot0 = g_bfme966A0;
+	m_bfmeSlot1 = g_bfme966A1;
+	m_bfmeSlot2 = g_bfme966A2;
+	m_bfmeSlot3 = g_bfme966A3;
+	m_bfmeSlot4 = g_bfme966A4;
+	m_bfmeSlot5 = g_bfme966A5;
+	m_bfmeSlot6 = g_bfme966A6;
+	m_bfmeSlot7 = g_bfme966A7;
+	m_bfmeSlot8 = g_bfme966A8;
+	m_bfmeSlot9 = g_bfme966A9;
+	bfmeBase966A();
+}
+
+extern char g_bfme966B0[];
+extern char g_bfme966B1[];
+extern char g_bfme966B2[];
+extern char g_bfme966B3[];
+extern char g_bfme966B4[];
+extern char g_bfme966B5[];
+extern char g_bfme966B6[];
+extern char g_bfme966B7[];
+extern char g_bfme966B8[];
+extern char g_bfme966B9[];
+
+class BfmeVft966B
+{
+public:
+	void bfmeGo966B();
+	void bfmeBase966B();
+	char *volatile m_bfmeSlot0;
+	char m_bfmePad0[0x8];
+	char *volatile m_bfmeSlot1;
+	char *volatile m_bfmeSlot2;
+	char m_bfmePad1[0xc];
+	char *volatile m_bfmeSlot3;
+	char *volatile m_bfmeSlot4;
+	char *volatile m_bfmeSlot5;
+	char *volatile m_bfmeSlot6;
+	char *volatile m_bfmeSlot7;
+	char *volatile m_bfmeSlot8;
+	char m_bfmePad2[0x9c];
+	char *volatile m_bfmeSlot9;
+};
+
+void BfmeVft966B::bfmeGo966B()
+{
+	m_bfmeSlot0 = g_bfme966B0;
+	m_bfmeSlot1 = g_bfme966B1;
+	m_bfmeSlot2 = g_bfme966B2;
+	m_bfmeSlot3 = g_bfme966B3;
+	m_bfmeSlot4 = g_bfme966B4;
+	m_bfmeSlot5 = g_bfme966B5;
+	m_bfmeSlot6 = g_bfme966B6;
+	m_bfmeSlot7 = g_bfme966B7;
+	m_bfmeSlot8 = g_bfme966B8;
+	m_bfmeSlot9 = g_bfme966B9;
+	bfmeBase966B();
+}
