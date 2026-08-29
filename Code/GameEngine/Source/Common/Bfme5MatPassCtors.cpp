4480c7bb32890b68b90ee14d3cb5ea93fbda66e5
302596555 <302596555@student.rocmondriaan.nl>
2026-08-30 00:47:14 +0200

    Open-BFME5: two constructors over library base types (99B)
    A MaterialPassClass subclass that stores two arguments, zeroes two fields and
    bumps a global instance count, and a copy constructor whose third member is a
    MultiplayerColorDefinition copied through its own constructor.
    
    The colour record's trailing byte is the check on the member's size: the
    stand-in has to be four bytes wide, not the one byte an empty class with only a
    copy constructor occupies, or the byte lands at +0x0d instead of +0x10.

---
+++ Code/GameEngine/Source/Common/Bfme5MatPassCtors.cpp
@@ -0,0 +1,58 @@
+// Open-BFME5 conversions: two constructors over library base types.
+
+class MaterialPassClass
+{
+public:
+	MaterialPassClass(void);
+	virtual ~MaterialPassClass();
+
+	char m_bfmePad[0x34];
+};
+
+extern int g_bfme5MatPassCount;
+
+class Bfme5MaterialPass : public MaterialPassClass
+{
+public:
+	Bfme5MaterialPass(int a, int b);
+	virtual ~Bfme5MaterialPass();
+
+	int m_bfme38;
+	int m_bfme3c;
+	int m_bfme40;
+	int m_bfme44;
+};
+
+Bfme5MaterialPass::Bfme5MaterialPass(int a, int b)
+{
+	m_bfme38 = a;
+	m_bfme3c = b;
+	m_bfme40 = 0;
+	m_bfme44 = 0;
+	++g_bfme5MatPassCount;
+}
+
+class MultiplayerColorDefinition
+{
+public:
+	MultiplayerColorDefinition(const MultiplayerColorDefinition &o);
+
+	int m_bfmeColor;
+};
+
+class Bfme5ColorRecord
+{
+public:
+	Bfme5ColorRecord(const Bfme5ColorRecord &o);
+	virtual ~Bfme5ColorRecord();
+
+	int m_bfme04;
+	int m_bfme08;
+	MultiplayerColorDefinition m_bfme0c;
+	char m_bfme10;
+};
+
+Bfme5ColorRecord::Bfme5ColorRecord(const Bfme5ColorRecord &o)
+	: m_bfme04(o.m_bfme04), m_bfme08(o.m_bfme08), m_bfme0c(o.m_bfme0c), m_bfme10(o.m_bfme10)
+{
+}
