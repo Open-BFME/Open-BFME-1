1ab23287ef135b1c27d5bf1068a6f5d787991540
dginovker <danielginovker@gmail.com>
2026-08-30 05:34:43 -0700

    Convert wide AsciiString hash lookup

---
+++ Code/GameEngine/Source/Common/Rva0046C2A0HashIndex.cpp
@@ -0,0 +1,60 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// stlport
+
+class AsciiString
+{
+public:
+	AsciiString(const AsciiString &other);
+	~AsciiString();
+
+private:
+	void *m_data;
+};
+
+class Rva0046C2A0Counted
+{
+public:
+	virtual void release(unsigned int deleting);
+	int m_references;
+};
+
+struct Rva0046C2A0Mapped
+{
+	Rva0046C2A0Mapped() : m_counted(0), m_value(0) {}
+	Rva0046C2A0Mapped(const Rva0046C2A0Mapped &other) :
+		m_counted(other.m_counted), m_value(other.m_value)
+	{
+		if (m_counted)
+			++m_counted->m_references;
+	}
+	~Rva0046C2A0Mapped()
+	{
+		if (m_counted && --m_counted->m_references <= 0)
+			m_counted->release(1);
+	}
+
+	Rva0046C2A0Counted *m_counted;
+	void *m_value;
+};
+
+namespace rts
+{
+	template <class T> struct hash;
+	template <class T> struct equal_to;
+
+	template <> struct hash<AsciiString>
+	{
+		unsigned int operator()(AsciiString value) const;
+	};
+
+	template <> struct equal_to<AsciiString>
+	{
+		int operator()(const AsciiString &left, const AsciiString &right) const;
+	};
+}
+
+#include <hash_map>
+
+typedef std::hash_map<AsciiString, Rva0046C2A0Mapped, rts::hash<AsciiString>, rts::equal_to<AsciiString> > Rva0046C2A0Map;
+
+template Rva0046C2A0Mapped &Rva0046C2A0Map::operator[](const AsciiString &key);
