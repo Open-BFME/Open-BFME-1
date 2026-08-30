6d5ec91f0b2c47396130cf175799c0646c20879c
dginovker <danielginovker@gmail.com>
2026-08-30 07:39:02 -0700

    Convert GameSpy global-key request

--- Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerMainBlockingOperations.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerMainBlockingOperations.c
@@ -7,7 +7,9 @@ typedef struct piConnection
 {
 	char reserved0[4];
 	char nick[64];
-	char reserved1[0x1824 - 0x44];
+	int connecting;
+	int connected;
+	char reserved1[0x1824 - 0x4C];
 	int callbackDepth;
 	char reserved2[0x1F08 - 0x1828];
 	int shutdown;
@@ -18,6 +20,11 @@ int piNewChangeNickOperation(PEER peer, const char *newNick,
 	void *callback, void *param, int opID);
 void piAddChangeNickCallback(PEER peer, int success, const char *oldNick,
 	const char *newNick, void *callback, void *param, int opID);
+int piNewGetGlobalKeysOperation(PEER peer, const char *target, int num,
+	const char **keys, void *callback, void *param, int opID);
+void piAddGetGlobalKeysCallback(PEER peer, int success, const char *nick,
+	int num, const char **keys, const char **values, void *callback,
+	void *param, int opID);
 void msleep(unsigned int milliseconds);
 void bfmePiThinkFromEsi(int opID);
 int piIsOperationFinished(PEER peer, int opID);
@@ -37,6 +44,40 @@ void peerChangeNickA(PEER peer, const char *newNick, void *callback,
 	if (!success)
 		piAddChangeNickCallback(peer, 0, connection->nick, newNick,
 			callback, param, opID);
+
+	if (blocking)
+	{
+		do
+		{
+			msleep(1);
+			bfmePiThinkFromEsi(opID);
+		}
+		while (!piIsOperationFinished(peer, opID) ||
+			!piIsCallbackFinished(peer, opID));
+
+		if (connection->shutdown && connection->callbackDepth == 0)
+			peerShutdown(peer);
+	}
+}
+
+void peerGetPlayerGlobalKeysA(PEER peer, const char *nick, int num,
+	const char **keys, void *callback, void *param, int blocking)
+{
+	piConnection *connection = (piConnection *)peer;
+	int success = 1;
+	int opID = piGetNextID(peer);
+
+	if (!connection->connected)
+		return;
+	if (!nick || !nick[0])
+		nick = connection->nick;
+
+	if (!piNewGetGlobalKeysOperation(peer, nick, num, keys, callback, param,
+			opID))
+		success = 0;
+	if (!success)
+		piAddGetGlobalKeysCallback(peer, 0, nick, 0, 0, 0, callback, param,
+			opID);
 
 	if (blocking)
 	{
