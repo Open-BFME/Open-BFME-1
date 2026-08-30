06b620a222dcbe6417da243642faf93b9c218f62
dginovker <danielginovker@gmail.com>
2026-08-30 06:53:47 -0700

    Convert GameSpy channel enumeration

--- Code/GameEngine/Source/GameNetwork/GameSpy/chat/chatQuietMode.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/chat/chatQuietMode.c
@@ -18,6 +18,17 @@ void ciSocketSendf(void *socket, const char *format, ...);
 void ciClearAllUsers(CHAT chat);
 void ciSetQuietModeEnumJoinedChannelsA(void);
 void ciEnumJoinedChannels(CHAT chat, void *callback, void *param);
+int ciAddLISTFilter(CHAT chat, void *callbackEach, void *callbackAll,
+	void *param);
+void bfmeCiThinkFromEsi(int ID);
+void msleep(unsigned int milliseconds);
+int ciCheckFiltersForID(CHAT chat, int ID);
+int ciCheckCallbacksForID(CHAT chat, int ID);
+
+static int ciCheckForID(CHAT chat, int ID)
+{
+	return ciCheckFiltersForID(chat, ID) || ciCheckCallbacksForID(chat, ID);
+}
 
 void chatSetQuietMode(CHAT chat, int quiet)
 {
@@ -38,5 +49,30 @@ void chatSetQuietMode(CHAT chat, int quiet)
 	{
 		ciClearAllUsers(chat);
 		ciEnumJoinedChannels(chat, ciSetQuietModeEnumJoinedChannelsA, 0);
+	}
+}
+
+void chatEnumChannelsA(CHAT chat, const char *filter, void *callbackEach,
+	void *callbackAll, void *param, int blocking)
+{
+	ciConnection *connection = (ciConnection *)chat;
+	int ID;
+
+	if (!chat || !connection->connected)
+		return;
+	if (!filter)
+		filter = "";
+
+	ciSocketSendf(&connection->chatSocket, "LIST %s", filter);
+	ID = ciAddLISTFilter(chat, callbackEach, callbackAll, param);
+
+	if (blocking)
+	{
+		do
+		{
+			bfmeCiThinkFromEsi(ID);
+			msleep(10);
+		}
+		while (ciCheckForID(chat, ID));
 	}
 }
