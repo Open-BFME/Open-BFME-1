2262457f4941c4fc2449cd10c91eea286e55f92e
dginovker <danielginovker@gmail.com>
2026-08-30 21:49:56 -0700

    Convert team-reference actions

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doSetTeamReference_Thunk.cpp
@@ -0,0 +1,85 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+
+template <class T> class StringBase
+{
+	friend class AsciiString;
+
+private:
+	StringBase(const StringBase &);
+	~StringBase();
+};
+
+class AsciiString
+{
+public:
+	AsciiString(const AsciiString &that)
+	{
+		((StringBase<char> *)this)->StringBase<char>::StringBase(
+			*(const StringBase<char> *)&that);
+	}
+	~AsciiString();
+
+private:
+	char *m_text;
+};
+
+class ScriptActionParameter
+{
+	friend class ScriptActions;
+
+private:
+	unsigned char m_unmodelled[0x10];
+	AsciiString m_string;
+};
+
+class Team;
+
+class ScriptEngine
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual void slot15() = 0;
+	virtual void slot16() = 0;
+	virtual Team *getTeamNamed(AsciiString, Bool) = 0;
+
+	void assignTeamReference(const AsciiString &, Team *);
+	void copyTeamReference(const AsciiString &, AsciiString);
+};
+
+extern ScriptEngine *TheScriptEngine;
+
+class ScriptActions
+{
+protected:
+	void doSetTeamReference(
+		const AsciiString &, ScriptActionParameter *, Bool);
+};
+
+// ?doSetTeamReference@ScriptActions@@IAEXABVAsciiString@@PAVScriptActionParameter@@_N@Z
+void ScriptActions::doSetTeamReference(const AsciiString &referenceName,
+	ScriptActionParameter *parameter, Bool referenceToReference)
+{
+	if (referenceToReference) {
+		TheScriptEngine->copyTeamReference(referenceName, parameter->m_string);
+		return;
+	}
+
+	Team *team = TheScriptEngine->getTeamNamed(parameter->m_string, false);
+	TheScriptEngine->assignTeamReference(referenceName, team);
+}
