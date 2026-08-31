253ba34e1966fc57e8f2db67c511bcaea2b70f4b
dginovker <danielginovker@gmail.com>
2026-08-30 21:20:28 -0700

    Convert team experience points action

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doTeamGiveExperiencePoints_Thunk.cpp
@@ -0,0 +1,90 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+typedef int Int;
+
+class AsciiString
+{
+private:
+	char *m_text;
+};
+
+class BfmeStringArgBase
+{
+	friend class BfmeAsciiStringArg;
+
+private:
+	BfmeStringArgBase(const BfmeStringArgBase &);
+	~BfmeStringArgBase();
+};
+
+class BfmeAsciiStringArg
+{
+public:
+	BfmeAsciiStringArg(const AsciiString &that)
+	{
+		((BfmeStringArgBase *)this)->BfmeStringArgBase::BfmeStringArgBase(
+			*(const BfmeStringArgBase *)&that);
+	}
+	~BfmeAsciiStringArg();
+
+private:
+	char *m_text;
+};
+
+class Object;
+class Team;
+
+typedef void (__cdecl *BfmeObjectVisitor)(Object *, void *);
+
+class BfmeInnerIterable
+{
+public:
+	Int bfmeVisit(BfmeObjectVisitor, void *);
+};
+
+extern void __cdecl bfmeTeamGiveExperiencePointsVisitor(Object *, void *);
+
+class ScriptEngine
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual void slot15() = 0;
+	virtual void slot16() = 0;
+	virtual Team *getTeamNamed(BfmeAsciiStringArg, Bool) = 0;
+};
+
+extern ScriptEngine *TheScriptEngine;
+
+class ScriptActions
+{
+protected:
+	void doTeamGiveExperiencePoints(const AsciiString &, Int);
+};
+
+// ?doTeamGiveExperiencePoints@ScriptActions@@IAEXABVAsciiString@@H@Z
+void ScriptActions::doTeamGiveExperiencePoints(
+	const AsciiString &teamName, Int points)
+{
+	Team *team = TheScriptEngine->getTeamNamed(teamName, false);
+	if (!team) {
+		return;
+	}
+
+	((BfmeInnerIterable *)team)->bfmeVisit(
+		bfmeTeamGiveExperiencePointsVisitor, (void *)points);
+}
