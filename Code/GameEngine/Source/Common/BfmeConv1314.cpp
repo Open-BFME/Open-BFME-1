e685e6b872752aeb0a006bc2e83c5e346bcbff97
argalad <github@argalad.com>
2026-09-01 01:54:08 +0200

    Open-BFME5: convert a self-registering constructor and a degrees read
    0x007DCBA0 (21 bytes): the constructor hands itself to the
    initialiser twice alongside its parameter.
    0x0045B830 (22 bytes): vftable slot 135 returns radians and the
    body scales them by 180/pi.

---
+++ Code/GameEngine/Source/Common/BfmeConv1314.cpp
@@ -0,0 +1,165 @@
+// Open-BFME5 conversions.
+
+class BfmeThingTHB;
+
+void __stdcall bfmeInitTHB(BfmeThingTHB *a, BfmeThingTHB *b, void *p);
+
+class BfmeThingTHB
+{
+public:
+	BfmeThingTHB(void *p);
+};
+
+BfmeThingTHB::BfmeThingTHB(void *p)
+{
+	bfmeInitTHB(this, this, p);
+}
+
+extern float g_bfmeKTHE;
+
+class BfmeThingTHE
+{
+public:
+	virtual void bfmeV0THE() = 0;
+	virtual void bfmeV1THE() = 0;
+	virtual void bfmeV2THE() = 0;
+	virtual void bfmeV3THE() = 0;
+	virtual void bfmeV4THE() = 0;
+	virtual void bfmeV5THE() = 0;
+	virtual void bfmeV6THE() = 0;
+	virtual void bfmeV7THE() = 0;
+	virtual void bfmeV8THE() = 0;
+	virtual void bfmeV9THE() = 0;
+	virtual void bfmeV10THE() = 0;
+	virtual void bfmeV11THE() = 0;
+	virtual void bfmeV12THE() = 0;
+	virtual void bfmeV13THE() = 0;
+	virtual void bfmeV14THE() = 0;
+	virtual void bfmeV15THE() = 0;
+	virtual void bfmeV16THE() = 0;
+	virtual void bfmeV17THE() = 0;
+	virtual void bfmeV18THE() = 0;
+	virtual void bfmeV19THE() = 0;
+	virtual void bfmeV20THE() = 0;
+	virtual void bfmeV21THE() = 0;
+	virtual void bfmeV22THE() = 0;
+	virtual void bfmeV23THE() = 0;
+	virtual void bfmeV24THE() = 0;
+	virtual void bfmeV25THE() = 0;
+	virtual void bfmeV26THE() = 0;
+	virtual void bfmeV27THE() = 0;
+	virtual void bfmeV28THE() = 0;
+	virtual void bfmeV29THE() = 0;
+	virtual void bfmeV30THE() = 0;
+	virtual void bfmeV31THE() = 0;
+	virtual void bfmeV32THE() = 0;
+	virtual void bfmeV33THE() = 0;
+	virtual void bfmeV34THE() = 0;
+	virtual void bfmeV35THE() = 0;
+	virtual void bfmeV36THE() = 0;
+	virtual void bfmeV37THE() = 0;
+	virtual void bfmeV38THE() = 0;
+	virtual void bfmeV39THE() = 0;
+	virtual void bfmeV40THE() = 0;
+	virtual void bfmeV41THE() = 0;
+	virtual void bfmeV42THE() = 0;
+	virtual void bfmeV43THE() = 0;
+	virtual void bfmeV44THE() = 0;
+	virtual void bfmeV45THE() = 0;
+	virtual void bfmeV46THE() = 0;
+	virtual void bfmeV47THE() = 0;
+	virtual void bfmeV48THE() = 0;
+	virtual void bfmeV49THE() = 0;
+	virtual void bfmeV50THE() = 0;
+	virtual void bfmeV51THE() = 0;
+	virtual void bfmeV52THE() = 0;
+	virtual void bfmeV53THE() = 0;
+	virtual void bfmeV54THE() = 0;
+	virtual void bfmeV55THE() = 0;
+	virtual void bfmeV56THE() = 0;
+	virtual void bfmeV57THE() = 0;
+	virtual void bfmeV58THE() = 0;
+	virtual void bfmeV59THE() = 0;
+	virtual void bfmeV60THE() = 0;
+	virtual void bfmeV61THE() = 0;
+	virtual void bfmeV62THE() = 0;
+	virtual void bfmeV63THE() = 0;
+	virtual void bfmeV64THE() = 0;
+	virtual void bfmeV65THE() = 0;
+	virtual void bfmeV66THE() = 0;
+	virtual void bfmeV67THE() = 0;
+	virtual void bfmeV68THE() = 0;
+	virtual void bfmeV69THE() = 0;
+	virtual void bfmeV70THE() = 0;
+	virtual void bfmeV71THE() = 0;
+	virtual void bfmeV72THE() = 0;
+	virtual void bfmeV73THE() = 0;
+	virtual void bfmeV74THE() = 0;
+	virtual void bfmeV75THE() = 0;
+	virtual void bfmeV76THE() = 0;
+	virtual void bfmeV77THE() = 0;
+	virtual void bfmeV78THE() = 0;
+	virtual void bfmeV79THE() = 0;
+	virtual void bfmeV80THE() = 0;
+	virtual void bfmeV81THE() = 0;
+	virtual void bfmeV82THE() = 0;
+	virtual void bfmeV83THE() = 0;
+	virtual void bfmeV84THE() = 0;
+	virtual void bfmeV85THE() = 0;
+	virtual void bfmeV86THE() = 0;
+	virtual void bfmeV87THE() = 0;
+	virtual void bfmeV88THE() = 0;
+	virtual void bfmeV89THE() = 0;
+	virtual void bfmeV90THE() = 0;
+	virtual void bfmeV91THE() = 0;
+	virtual void bfmeV92THE() = 0;
+	virtual void bfmeV93THE() = 0;
+	virtual void bfmeV94THE() = 0;
+	virtual void bfmeV95THE() = 0;
+	virtual void bfmeV96THE() = 0;
+	virtual void bfmeV97THE() = 0;
+	virtual void bfmeV98THE() = 0;
+	virtual void bfmeV99THE() = 0;
+	virtual void bfmeV100THE() = 0;
+	virtual void bfmeV101THE() = 0;
+	virtual void bfmeV102THE() = 0;
+	virtual void bfmeV103THE() = 0;
+	virtual void bfmeV104THE() = 0;
+	virtual void bfmeV105THE() = 0;
+	virtual void bfmeV106THE() = 0;
+	virtual void bfmeV107THE() = 0;
+	virtual void bfmeV108THE() = 0;
+	virtual void bfmeV109THE() = 0;
+	virtual void bfmeV110THE() = 0;
+	virtual void bfmeV111THE() = 0;
+	virtual void bfmeV112THE() = 0;
+	virtual void bfmeV113THE() = 0;
+	virtual void bfmeV114THE() = 0;
+	virtual void bfmeV115THE() = 0;
+	virtual void bfmeV116THE() = 0;
+	virtual void bfmeV117THE() = 0;
+	virtual void bfmeV118THE() = 0;
+	virtual void bfmeV119THE() = 0;
+	virtual void bfmeV120THE() = 0;
+	virtual void bfmeV121THE() = 0;
+	virtual void bfmeV122THE() = 0;
+	virtual void bfmeV123THE() = 0;
+	virtual void bfmeV124THE() = 0;
+	virtual void bfmeV125THE() = 0;
+	virtual void bfmeV126THE() = 0;
+	virtual void bfmeV127THE() = 0;
+	virtual void bfmeV128THE() = 0;
+	virtual void bfmeV129THE() = 0;
+	virtual void bfmeV130THE() = 0;
+	virtual void bfmeV131THE() = 0;
+	virtual void bfmeV132THE() = 0;
+	virtual void bfmeV133THE() = 0;
+	virtual void bfmeV134THE() = 0;
+	virtual float bfmeGetTHE(int a) = 0;
+	float bfmeGoTHE(int a);
+};
+
+float BfmeThingTHE::bfmeGoTHE(int a)
+{
+	return bfmeGetTHE(a) * g_bfmeKTHE;
+}
