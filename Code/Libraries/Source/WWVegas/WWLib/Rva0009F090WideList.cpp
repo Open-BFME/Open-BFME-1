845b56e645ac9015f82418a896161d6395951601
dginovker <danielginovker@gmail.com>
2026-08-31 15:26:06 -0700

    Convert wide string list removal

--- Code/Libraries/Source/WWVegas/WWLib/Rva0009F090WideList.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/Rva0009F090WideList.cpp
@@ -10,6 +10,30 @@ class StringBase
 	friend class UnicodeString;
 
 public:
+	__forceinline int compare(const StringBase<T> &other) const throw()
+	{
+		const int otherLength = other.m_data ? other.m_data->length : 0;
+		const T *otherText = other.m_data ? other.m_data->text : (const T *)L"";
+		const int thisLength = m_data ? m_data->length : 0;
+		const T *thisText = m_data ? m_data->text : (const T *)L"";
+		int remaining = thisLength < otherLength ? thisLength : otherLength;
+		int result = 0;
+		while (remaining > 0)
+		{
+			if (*thisText != *otherText)
+			{
+				result = *thisText - *otherText;
+				break;
+			}
+			++thisText;
+			++otherText;
+			--remaining;
+		}
+		if (result != 0)
+			return result;
+		return thisLength - otherLength;
+	}
+
 	int compareNoCase(const StringBase<T> &other) const throw()
 	{
 		const int otherLength = other.m_data ? other.m_data->length : 0;
@@ -48,6 +72,14 @@ public:
 			*(const StringBase<WideChar> *)&other);
 	}
 
+	__forceinline int compare(const UnicodeString &other) const throw()
+	{
+		return ((const StringBase<WideChar> *)this)->compare(
+			*(const StringBase<WideChar> *)&other);
+	}
+
+	void destroyNodeValue();
+
 private:
 	void *m_data;
 };
@@ -58,6 +90,8 @@ struct Rva0009F090WideNode
 	Rva0009F090WideNode *previous;
 	UnicodeString value;
 };
+
+void __cdecl Rva0009F3E0Deallocate(void *memory, unsigned int bytes);
 
 class Rva0009F090WideIterator
 {
@@ -78,10 +112,30 @@ public:
 	Rva0009F090WideIterator begin() { return Rva0009F090WideIterator(m_head->next); }
 	Rva0009F090WideIterator end() { return Rva0009F090WideIterator(m_head); }
 	const UnicodeString &front() const { return m_head->next->value; }
+	void remove(const UnicodeString &value);
 
 private:
 	Rva0009F090WideNode *m_head;
 };
+
+void Rva0009F090WideListData::remove(const UnicodeString &value)
+{
+	Rva0009F090WideNode *const end = m_head;
+	Rva0009F090WideNode *node = end->next;
+	while (node != end)
+	{
+		Rva0009F090WideNode *next = node->next;
+		if (value.compare(node->value) == 0)
+		{
+			Rva0009F090WideNode *previous = node->previous;
+			previous->next = next;
+			next->previous = previous;
+			node->value.destroyNodeValue();
+			Rva0009F3E0Deallocate(node, sizeof(Rva0009F090WideNode));
+		}
+		node = next;
+	}
+}
 
 class Rva0009F090WideList
 {
