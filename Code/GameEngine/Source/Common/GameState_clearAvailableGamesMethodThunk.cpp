4ff615cbd9fa8b2aca6a2fdfe3e0cf1ed41e8f1e
dginovker <danielginovker@gmail.com>
2026-08-31 16:50:09 -0700

    Correct scratch save deletion identity

--- Code/GameEngine/Source/Common/GameState_clearAvailableGamesMethodThunk.cpp
+++ Code/GameEngine/Source/Common/GameState_clearAvailableGamesMethodThunk.cpp
@@ -1,6 +1,5 @@
 // cl: /DNDEBUG /MD /EHsc /Ireference/shims/sweep /Ireference/shims/campaignmanagerascii /ICode/Libraries/Source/WWVegas/WWLib
-// readable body of ?clearAvailableGames@GameState@@AAEXXZ: Code/GameEngine/Source/Common/System/SaveGame/GameState.cpp
-// Open-BFME5: exact C++ lift of the retail scratch-save cleanup body.
+// Open-BFME5: exact C++ reconstruction of the retail scratch-save cleanup body.
 
 #include "Common/AsciiString.h"
 
@@ -11,14 +10,13 @@ public:
     AsciiString getFilePathInSaveDirectory(const AsciiString &leaf) const;
 
 private:
-    void clearAvailableGames();
+    void deleteScratchSaveFile();
 };
 
 extern GameState *TheGameState;
 extern "C" __declspec(dllimport) int __stdcall DeleteFileA(const char *fileName);
 
-// ?clearAvailableGames@GameState@@AAEXXZ
-void GameState::clearAvailableGames()
+void GameState::deleteScratchSaveFile()
 {
     AsciiString fileName = TheGameState->getFilePathInSaveDirectory(AsciiString("00000000.sav"));
     const char *text = *(const char **)&fileName;
