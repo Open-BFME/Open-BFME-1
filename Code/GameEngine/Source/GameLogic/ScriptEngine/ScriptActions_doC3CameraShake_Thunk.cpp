c5c0331c3f886390d9e650f8aa236200d07a8141
dginovker <danielginovker@gmail.com>
2026-08-30 21:32:44 -0700

    Convert camera shaker action

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doC3CameraShake_Thunk.cpp
@@ -0,0 +1,151 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef float Real;
+
+struct Coord3D
+{
+	Real x;
+	Real y;
+	Real z;
+};
+
+class AsciiString
+{
+private:
+	char *m_text;
+};
+
+class BfmeStringArgBase
+{
+	friend class BfmeAsciiStringArg;
+
+private:
+	BfmeStringArgBase(const BfmeStringArgBase &);
+	~BfmeStringArgBase();
+};
+
+class BfmeAsciiStringArg
+{
+public:
+	BfmeAsciiStringArg(const AsciiString &that)
+	{
+		((BfmeStringArgBase *)this)->BfmeStringArgBase::BfmeStringArgBase(
+			*(const BfmeStringArgBase *)&that);
+	}
+	~BfmeAsciiStringArg();
+
+private:
+	char *m_text;
+};
+
+class Waypoint
+{
+public:
+	const Coord3D *getLocation() const { return &m_location; }
+
+private:
+	unsigned char m_unmodelled[12];
+	Coord3D m_location;
+};
+
+class TerrainLogic
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual void slot15() = 0;
+	virtual void slot16() = 0;
+	virtual void slot17() = 0;
+	virtual void slot18() = 0;
+	virtual void slot19() = 0;
+	virtual void slot20() = 0;
+	virtual void slot21() = 0;
+	virtual void slot22() = 0;
+	virtual void slot23() = 0;
+	virtual void slot24() = 0;
+	virtual void slot25() = 0;
+	virtual void slot26() = 0;
+	virtual void slot27() = 0;
+	virtual void slot28() = 0;
+	virtual void slot29() = 0;
+	virtual void slot30() = 0;
+	virtual Waypoint *getWaypointByName(BfmeAsciiStringArg) = 0;
+};
+
+class TacticalView
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual void slot15() = 0;
+	virtual void slot16() = 0;
+	virtual void slot17() = 0;
+	virtual void slot18() = 0;
+	virtual void slot19() = 0;
+	virtual void slot20() = 0;
+	virtual void slot21() = 0;
+	virtual void slot22() = 0;
+	virtual void slot23() = 0;
+	virtual void slot24() = 0;
+	virtual void slot25() = 0;
+	virtual void slot26() = 0;
+	virtual void slot27() = 0;
+	virtual void slot28() = 0;
+	virtual void slot29() = 0;
+	virtual void slot30() = 0;
+	virtual void slot31() = 0;
+	virtual void slot32() = 0;
+	virtual void slot33() = 0;
+	virtual void slot34() = 0;
+	virtual void slot35() = 0;
+	virtual void slot36() = 0;
+	virtual void slot37() = 0;
+	virtual void slot38() = 0;
+	virtual void slot39() = 0;
+	virtual void slot40() = 0;
+	virtual void Add_Camera_Shake(const Coord3D *, Real, Real, Real) = 0;
+};
+
+extern TerrainLogic *TheTerrainLogic;
+extern TacticalView *TheTacticalView;
+
+class ScriptActions
+{
+protected:
+	void doC3CameraShake(const AsciiString &, Real, Real, Real);
+};
+
+// ?doC3CameraShake@ScriptActions@@IAEXABVAsciiString@@MMM@Z
+void ScriptActions::doC3CameraShake(
+	const AsciiString &waypointName, Real amplitude, Real duration, Real radius)
+{
+	Waypoint *waypoint = TheTerrainLogic->getWaypointByName(waypointName);
+	TheTacticalView->Add_Camera_Shake(
+		waypoint->getLocation(), radius, duration, amplitude);
+}
