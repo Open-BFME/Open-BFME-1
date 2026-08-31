aa15faf586234e91363000bcd84028ae33c4aee2
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 14:07:25 -0400

    Convert SelectionXlat select-count hash lookup

---
+++ Code/GameEngine/Source/GameClient/MessageStream/SelectionXlat_selectCountMap.cpp
@@ -0,0 +1,22 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad
+// stlport
+#define Matrix4x4 Matrix4
+#include "PreRTS.h"
+#include <hash_map>
+
+class ThingTemplate;
+namespace _STL {
+template <> struct hash<const ThingTemplate *>
+{
+	size_t operator()(const ThingTemplate *value) const
+	{
+		return reinterpret_cast<size_t>(value);
+	}
+};
+}
+typedef std::hash_map<const ThingTemplate *, Bool> SelectCountMap;
+
+Bool &selectionCountFor(SelectCountMap &counts, const ThingTemplate *thingTemplate)
+{
+	return counts[thingTemplate];
+}
