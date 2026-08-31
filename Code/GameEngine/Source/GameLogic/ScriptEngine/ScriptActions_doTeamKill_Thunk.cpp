56c0c996562bd35532c6eaed250e88ed909f23eb
dginovker <danielginovker@gmail.com>
2026-08-30 20:43:03 -0700

    Convert ScriptActions team kill

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doTeamKill_Thunk.cpp
@@ -0,0 +1,78 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+
+class AsciiString
+{
+private:
+	char *m_text;
+};
+
+class BfmeStringArgBase
+{
+	friend class BfmeAsciiStringArg;
+
+private:
+	BfmeStringArgBase(const BfmeStringArgBase &);
+	~BfmeStringArgBase();
+};
+
+class BfmeAsciiStringArg
+{
+public:
+	BfmeAsciiStringArg(const AsciiString &that)
+	{
+		((BfmeStringArgBase *)this)->BfmeStringArgBase::BfmeStringArgBase(
+			*(const BfmeStringArgBase *)&that);
+	}
+	~BfmeAsciiStringArg();
+
+private:
+	char *m_text;
+};
+
+class Team
+{
+public:
+	void killTeam();
+};
+
+class ScriptEngine
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual void slot15() = 0;
+	virtual void slot16() = 0;
+	virtual Team *getTeamNamed(BfmeAsciiStringArg, Bool) = 0;
+};
+
+extern ScriptEngine *TheScriptEngine;
+
+class ScriptActions
+{
+protected:
+	void doTeamKill(const AsciiString &);
+};
+
+// ?doTeamKill@ScriptActions@@IAEXABVAsciiString@@@Z
+void ScriptActions::doTeamKill(const AsciiString &teamName)
+{
+	Team *team = TheScriptEngine->getTeamNamed(teamName, false);
+	if (team) {
+		team->killTeam();
+	}
+}
