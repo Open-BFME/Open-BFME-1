1d87c8634ab4e51b3f8d6609d704a12769aa6e67
GL <gleontra@gmail.com>
2026-08-31 15:23:46 -0300

    Convert Rva00361420FloatVector methods from gen_asm to clean C++ (0x003612E0, 0x00361350, 0x003613A0, 0x003614D0, 0x00361530)

--- Code/GameEngine/Source/Common/Rva00361420FloatVectorAccess.cpp
+++ Code/GameEngine/Source/Common/Rva00361420FloatVectorAccess.cpp
@@ -21,8 +21,11 @@ class Rva003608F0FloatElement
 public:
 	__declspec( noinline ) float value();
 
-private:
-	char m_lead[ 0x28 ];
+	int m_unknown0;
+	int m_id;
+	char m_name[ 0x18 ];
+	int m_state;
+	int m_duration;
 	int m_endFrame;
 	int m_startFrame;
 	char m_tail[ 0x28 ];
@@ -44,22 +47,59 @@ class Rva00361420FloatRange
 {
 public:
 	unsigned int size() const { return m_last - m_first; }
+	Rva003608F0FloatElement &operator[]( unsigned int index ) { return m_first[ index ]; }
+	const Rva003608F0FloatElement &operator[]( unsigned int index ) const { return m_first[ index ]; }
 	Rva003608F0FloatElement &operator[]( int index ) { return m_first[ index ]; }
+	const Rva003608F0FloatElement &operator[]( int index ) const { return m_first[ index ]; }
 
 private:
 	Rva003608F0FloatElement *m_first;
 	Rva003608F0FloatElement *m_last;
 };
 
+
 class Rva00361420FloatVector
 {
 public:
+	void resetAllStates();
+	int getState( int index ) const;
+	void startState( int index );
 	float valueAt( int index );
+	int getRemainingFrames( int index ) const;
+	int findFirstNamedId() const;
 
 private:
 	char m_lead[ 0x18 ];
 	Rva00361420FloatRange m_values;
 };
+
+void Rva00361420FloatVector::resetAllStates()
+{
+	for ( unsigned int i = 0; i < m_values.size(); ++i ) {
+		m_values[ i ].m_state = 1;
+	}
+}
+
+int Rva00361420FloatVector::getState( int index ) const
+{
+	if ( index >= 0 && static_cast<unsigned int>( index ) < m_values.size() ) {
+		return m_values[ index ].m_state;
+	}
+
+	return 1;
+}
+
+void Rva00361420FloatVector::startState( int index )
+{
+	if ( index >= 0 && static_cast<unsigned int>( index ) < m_values.size() ) {
+		Rva003608F0FloatElement &elem = m_values[ index ];
+		if ( elem.m_state == 0 ) {
+			elem.m_state = 2;
+			elem.m_startFrame = TheGameLogic->m_frame;
+			elem.m_endFrame = TheGameLogic->m_frame + elem.m_duration;
+		}
+	}
+}
 
 float Rva00361420FloatVector::valueAt( int index )
 {
@@ -69,3 +109,29 @@ float Rva00361420FloatVector::valueAt( int index )
 
 	return 0.0f;
 }
+
+int Rva00361420FloatVector::getRemainingFrames( int index ) const
+{
+	if ( index >= 0 && static_cast<unsigned int>( index ) < m_values.size() ) {
+		const Rva003608F0FloatElement &elem = m_values[ index ];
+		int frame = TheGameLogic->m_frame;
+		if ( frame < elem.m_endFrame ) {
+			return elem.m_endFrame - frame;
+		}
+	}
+
+	return 0;
+}
+
+
+int Rva00361420FloatVector::findFirstNamedId() const
+{
+	for ( unsigned int i = 0; i < m_values.size(); ++i ) {
+		if ( m_values[ i ].m_name[ 0 ] != '\0' ) {
+			return m_values[ i ].m_id;
+		}
+	}
+
+	return -1;
+}
+
