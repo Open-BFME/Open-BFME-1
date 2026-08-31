86a3b23012d77fb8522712c04855d6974882f720
dginovker <danielginovker@gmail.com>
2026-08-30 20:45:15 -0700

    Convert ScriptActions display text

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doDisplayText_Thunk.cpp
@@ -0,0 +1,62 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class AsciiString
+{
+private:
+	char *m_text;
+};
+
+class BfmeStringArgBase
+{
+	friend class BfmeAsciiStringArg;
+
+private:
+	BfmeStringArgBase(const BfmeStringArgBase &);
+	~BfmeStringArgBase();
+};
+
+class BfmeAsciiStringArg
+{
+public:
+	BfmeAsciiStringArg(const AsciiString &that)
+	{
+		((BfmeStringArgBase *)this)->BfmeStringArgBase::BfmeStringArgBase(
+			*(const BfmeStringArgBase *)&that);
+	}
+	~BfmeAsciiStringArg();
+
+private:
+	char *m_text;
+};
+
+class InGameUI
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0C() = 0;
+	virtual void slot10() = 0;
+	virtual void slot14() = 0;
+	virtual void slot18() = 0;
+	virtual void slot1C() = 0;
+	virtual void slot20() = 0;
+	virtual void slot24() = 0;
+	virtual void slot28() = 0;
+	virtual void slot2C() = 0;
+	virtual void message(BfmeAsciiStringArg, ...) = 0;
+};
+
+extern InGameUI *TheInGameUI;
+
+class ScriptActions
+{
+protected:
+	void doDisplayText(const AsciiString &);
+};
+
+// ?doDisplayText@ScriptActions@@IAEXABVAsciiString@@@Z
+void ScriptActions::doDisplayText(const AsciiString &displayText)
+{
+	TheInGameUI->message(displayText);
+}
