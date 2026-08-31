3adf000d3034b48d0f88ae13fc973765139a8351
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 16:30:06 -0400

    Convert W3DDisplayString setTextColor

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DDisplayStringColors.cpp
@@ -0,0 +1,40 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef int Color;
+
+extern "C" void _ReadWriteBarrier(void);
+#pragma intrinsic(_ReadWriteBarrier)
+
+class W3DDisplayString
+{
+public:
+	void setTextColor(Color *colors);
+
+private:
+	unsigned char m_unmodelled_000[0x1CC];
+	Color m_textColors[4];
+	Color m_dropColors[4];
+	volatile bool m_colorsChanged;
+};
+
+void W3DDisplayString::setTextColor(Color *colors)
+{
+	if (!m_colorsChanged && m_textColors[0] != colors[0])
+		m_colorsChanged = true;
+	m_textColors[0] = colors[0];
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_textColors[1] != colors[1])
+		m_colorsChanged = true;
+	m_textColors[1] = colors[1];
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_textColors[2] != colors[2])
+		m_colorsChanged = true;
+	m_textColors[2] = colors[2];
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_textColors[3] != colors[3])
+		m_colorsChanged = true;
+	m_textColors[3] = colors[3];
+}
