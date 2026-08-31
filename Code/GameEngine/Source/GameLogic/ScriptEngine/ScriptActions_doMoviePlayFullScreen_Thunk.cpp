0b16e7510472d33928049fd36dc8366447711439
dginovker <danielginovker@gmail.com>
2026-08-30 20:49:15 -0700

    Convert ScriptActions fullscreen movie

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doMoviePlayFullScreen_Thunk.cpp
@@ -0,0 +1,102 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+typedef float Real;
+
+class AsciiString
+{
+private:
+	char *m_text;
+};
+
+class BfmeStringArgBase
+{
+	friend class BfmeAsciiStringArg;
+
+private:
+	BfmeStringArgBase(const BfmeStringArgBase &);
+	~BfmeStringArgBase();
+};
+
+class BfmeAsciiStringArg
+{
+public:
+	BfmeAsciiStringArg(const AsciiString &that)
+	{
+		((BfmeStringArgBase *)this)->BfmeStringArgBase::BfmeStringArgBase(
+			*(const BfmeStringArgBase *)&that);
+	}
+	~BfmeAsciiStringArg();
+
+private:
+	char *m_text;
+};
+
+class Display
+{
+public:
+	virtual void slot00() = 0; virtual void slot01() = 0;
+	virtual void slot02() = 0; virtual void slot03() = 0;
+	virtual void slot04() = 0; virtual void slot05() = 0;
+	virtual void slot06() = 0; virtual void slot07() = 0;
+	virtual void slot08() = 0; virtual void slot09() = 0;
+	virtual void slot10() = 0; virtual void slot11() = 0;
+	virtual void slot12() = 0; virtual void slot13() = 0;
+	virtual void slot14() = 0; virtual void slot15() = 0;
+	virtual void slot16() = 0; virtual void slot17() = 0;
+	virtual void slot18() = 0; virtual void slot19() = 0;
+	virtual void slot20() = 0; virtual void slot21() = 0;
+	virtual void slot22() = 0; virtual void slot23() = 0;
+	virtual void slot24() = 0; virtual void slot25() = 0;
+	virtual void slot26() = 0; virtual void slot27() = 0;
+	virtual void slot28() = 0; virtual void slot29() = 0;
+	virtual void slot30() = 0; virtual void slot31() = 0;
+	virtual void slot32() = 0; virtual void slot33() = 0;
+	virtual void slot34() = 0; virtual void slot35() = 0;
+	virtual void slot36() = 0; virtual void slot37() = 0;
+	virtual void slot38() = 0; virtual void slot39() = 0;
+	virtual void slot40() = 0; virtual void slot41() = 0;
+	virtual void slot42() = 0; virtual void slot43() = 0;
+	virtual void slot44() = 0; virtual void slot45() = 0;
+	virtual void slot46() = 0; virtual void slot47() = 0;
+	virtual void slot48() = 0; virtual void slot49() = 0;
+	virtual void slot50() = 0; virtual void slot51() = 0;
+	virtual void slot52() = 0; virtual void slot53() = 0;
+	virtual void slot54() = 0; virtual void slot55() = 0;
+	virtual void slot56() = 0;
+	virtual void playLogoMovie(BfmeAsciiStringArg, int, int, int) = 0;
+	virtual void slot58() = 0;
+	virtual void slot59() = 0;
+	virtual Bool isMoviePlaying() = 0;
+
+	void rva002ED2E0(Real, Real, Real, Real);
+};
+
+struct BfmeGameLODManager
+{
+	unsigned char m_unreconstructed[0x16c4];
+	int m_staticLODLevel;
+};
+
+extern Display *TheDisplay;
+extern BfmeGameLODManager *TheGameLODManager;
+
+class ScriptActions
+{
+protected:
+	void doMoviePlayFullScreen(const AsciiString &, Bool);
+};
+
+// ?doMoviePlayFullScreen@ScriptActions@@IAEXABVAsciiString@@_N@Z
+void ScriptActions::doMoviePlayFullScreen(const AsciiString &movieName, Bool skipLowDetail)
+{
+	if (skipLowDetail && TheGameLODManager->m_staticLODLevel <= 1) {
+		return;
+	}
+	if (TheDisplay->isMoviePlaying()) {
+		return;
+	}
+
+	TheDisplay->rva002ED2E0(0.0f, 0.0f, 1.0f, 1.0f);
+	TheDisplay->playLogoMovie(movieName, 64, -1, -1);
+}
