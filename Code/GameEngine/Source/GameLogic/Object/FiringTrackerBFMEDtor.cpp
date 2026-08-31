a3dc1a544466610e8aaf5b6cada3df8d8fc3f1d8
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 11:40:41 -0400

    Convert BFME FiringTracker destructor

---
+++ Code/GameEngine/Source/GameLogic/Object/FiringTrackerBFMEDtor.cpp
@@ -0,0 +1,78 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef int AudioHandle;
+
+class AudioManager
+{
+public:
+	virtual void slot00(); virtual void slot04(); virtual void slot08();
+	virtual void slot0c(); virtual void slot10(); virtual void slot14();
+	virtual void slot18(); virtual void slot1c(); virtual void slot20();
+	virtual void slot24(); virtual void slot28(); virtual void slot2c();
+	virtual void slot30(); virtual void slot34(); virtual void slot38();
+	virtual void slot3c(); virtual void slot40(); virtual void slot44();
+	virtual void slot48();
+	virtual void removeAudioEvent(AudioHandle handle);
+};
+
+extern AudioManager *TheAudio;
+
+class Gen_dtor_00113d40
+{
+public:
+	virtual ~Gen_dtor_00113d40();
+
+private:
+	const void *m_moduleData;
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void getBehaviorModuleInterface() = 0;
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void updateModuleInterface() = 0;
+};
+
+class ObjectModule : public Gen_dtor_00113d40
+{
+private:
+	void *m_object;
+};
+
+class BehaviorModule : public ObjectModule, public BehaviorModuleInterface
+{
+public:
+	virtual ~BehaviorModule() {}
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+public:
+	virtual ~UpdateModule() {}
+
+private:
+	unsigned int m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	unsigned int m_updateState;
+};
+
+class FiringTracker : public UpdateModule
+{
+public:
+	virtual ~FiringTracker();
+
+private:
+	unsigned char m_unreconstructed_20[0x38];
+	AudioHandle m_audioHandle;
+};
+
+FiringTracker::~FiringTracker()
+{
+	TheAudio->removeAudioEvent(m_audioHandle);
+	m_audioHandle = 1;
+}
