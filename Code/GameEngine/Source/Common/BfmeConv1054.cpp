c57f3628016f68c14aa8b1f464e2de5435dd834b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 20:36:14 -0400

    Convert particle category head initializer to C++

--- Code/GameEngine/Source/Common/BfmeConv1054.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1054.cpp
@@ -37,6 +37,7 @@ int bfmeMake1054(int n);
 void __stdcall bfmeTailA1054(BfmeA1054 *p);
 void __stdcall bfmeTailB1054(BfmeB1054 *p);
 void bfmeTailA1054Cdecl(BfmeA1054 *p);
+void bfmeTailB1054Cdecl(BfmeB1054 *p);
 
 void __stdcall bfmeGo1054A(BfmeA1054 *p)
 {
@@ -78,6 +79,40 @@ void bfmeTailB1054Impl(BfmeB1054 *p)
 	p->m_bfme58 = z;
 	p->m_bfme5c = z;
 	bfmeTailA1054Cdecl(reinterpret_cast<BfmeA1054 *>(p));
+}
+
+struct BfmeCategoryHead1054
+{
+	int m_key0;
+	void (*m_callback0)(void);
+	int m_zero08;
+	int m_zero0c;
+	int m_key1;
+	void (*m_callback1)(void);
+	int m_zero18;
+	int m_zero1c;
+	int m_key2;
+	void (*m_callback2)(void);
+	int m_zero28;
+	int m_zero2c;
+};
+
+void bfmeCategoryHead1054(BfmeCategoryHead1054 *p)
+{
+	int z = 0;
+	p->m_key0 = bfmeMake1054(z);
+	p->m_callback0 = reinterpret_cast<void (*)(void)>(0x00414556);
+	p->m_zero08 = z;
+	p->m_zero0c = z;
+	p->m_key1 = bfmeMake1054(1);
+	p->m_callback1 = reinterpret_cast<void (*)(void)>(0x004391a8);
+	p->m_zero18 = z;
+	p->m_zero1c = z;
+	p->m_key2 = bfmeMake1054(2);
+	p->m_callback2 = reinterpret_cast<void (*)(void)>(0x00411897);
+	p->m_zero28 = z;
+	p->m_zero2c = z;
+	bfmeTailB1054Cdecl(reinterpret_cast<BfmeB1054 *>(p));
 }
 
 class BfmeSubC1054
