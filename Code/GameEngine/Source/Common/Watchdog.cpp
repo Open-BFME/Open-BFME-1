9b6d4ebafb989b91d8bc3ec358cf7b897fc85176
dginovker <danielginovker@gmail.com>
2026-08-31 17:10:46 -0700

    Convert watchdog thread start

--- Code/GameEngine/Source/Common/Watchdog.cpp
+++ Code/GameEngine/Source/Common/Watchdog.cpp
@@ -33,27 +33,42 @@ private:
 	WatchdogCriticalSection &m_criticalSection;
 };
 
-class BFMENetworkThreadBase
+class ThreadClass
 {
 public:
-	void stop(void);
+	void Execute(void);
+	void Stop(void);
 
 private:
 	char m_storage[0x50];
 };
 
-class WatchdogOwnedState
+class MutexClass
 {
 public:
-	~WatchdogOwnedState();
+	class LockClass
+	{
+	public:
+		LockClass(MutexClass &mutex, int timeout);
+		~LockClass();
+
+private:
+		MutexClass &m_mutex;
+		bool m_failed;
+	};
+
+private:
+	void *m_handle;
+	unsigned int m_locked;
 };
 
-class Watchdog : public BFMENetworkThreadBase
+class Watchdog : public ThreadClass
 {
 public:
 	void suppressTimeouts(void);
 	void update(void);
 	void resumeTimeouts(void);
+	void start(void);
 	void stop(void);
 
 private:
@@ -66,8 +81,8 @@ private:
 	UnsignedInt m_nextWarning;
 	volatile int m_suppressionCount;
 	WatchdogCriticalSection m_criticalSection;
-	char m_event[8];
-	WatchdogOwnedState *m_ownedState;
+	MutexClass m_mutex;
+	MutexClass::LockClass *m_ownedLock;
 };
 
 void Watchdog::suppressTimeouts(void)
@@ -106,9 +121,20 @@ void Watchdog::resumeTimeouts(void)
 	}
 }
 
+void Watchdog::start(void)
+{
+	MutexClass::LockClass *lock = new MutexClass::LockClass(m_mutex, -1);
+	if (lock != m_ownedLock)
+	{
+		delete m_ownedLock;
+		m_ownedLock = lock;
+	}
+	ThreadClass::Execute();
+}
+
 void Watchdog::stop(void)
 {
-	delete m_ownedState;
-	m_ownedState = 0;
-	BFMENetworkThreadBase::stop();
+	delete m_ownedLock;
+	m_ownedLock = 0;
+	ThreadClass::Stop();
 }
