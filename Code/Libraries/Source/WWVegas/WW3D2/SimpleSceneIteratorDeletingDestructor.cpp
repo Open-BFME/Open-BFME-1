81631544826ee88265ae5aee8d5382f99804ae92
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 03:48:20 -0400

    Match SimpleSceneIterator constructor

--- Code/Libraries/Source/WWVegas/WW3D2/SimpleSceneIteratorDeletingDestructor.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/SimpleSceneIteratorDeletingDestructor.cpp
@@ -6,18 +6,36 @@ public:
 	virtual ~SceneIterator() {}
 };
 
+class RenderObjClass;
+
+template <class T> class RefMultiListClass
+{
+public:
+	void *m_unused[2];
+	void *m_first;
+};
+
 class SimpleSceneIterator : public SceneIterator
 {
 public:
 	virtual ~SimpleSceneIterator();
+
+protected:
+	SimpleSceneIterator(RefMultiListClass<RenderObjClass> *render_list);
+
+private:
+	RefMultiListClass<RenderObjClass> *m_render_list;
+	void *m_current;
 };
 
-__declspec(noinline) SimpleSceneIterator::~SimpleSceneIterator() {}
-
-void Force_SimpleSceneIterator_Vtable()
+__declspec(noinline) SimpleSceneIterator::SimpleSceneIterator(
+	RefMultiListClass<RenderObjClass> *render_list) :
+	m_render_list(render_list),
+	m_current(render_list->m_first)
 {
-	SimpleSceneIterator iterator;
 }
+
+__declspec(noinline) SimpleSceneIterator::~SimpleSceneIterator() {}
 
 void Force_SimpleSceneIterator_Deleting_Destructor(SimpleSceneIterator *iterator)
 {
