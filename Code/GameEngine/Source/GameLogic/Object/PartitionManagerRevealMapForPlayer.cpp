2e1a4bd29e5aaf72a8922a67a3fdcefe266d3f5f
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 22:04:03 -0400

    Convert partition reveal sweep to C++

--- Code/GameEngine/Source/GameLogic/Object/PartitionManagerRevealMapForPlayer.cpp
+++ Code/GameEngine/Source/GameLogic/Object/PartitionManagerRevealMapForPlayer.cpp
@@ -5,10 +5,28 @@
 
 typedef int Int;
 
+class PartitionManagerImpl008F8D60;
+
+class PartitionManagerImpl008F8D60Element
+{
+public:
+	void updatePlayerCells008FC300(PartitionManagerImpl008F8D60 *manager, Int playerIndex);
+	void updatePlayerCells008FC3B0(PartitionManagerImpl008F8D60 *manager, Int playerIndex);
+
+private:
+	char m_data[0x68];
+};
+
 class PartitionManagerImpl008F8D60
 {
 public:
 	void revealMapForPlayer(Int playerIndex);
+
+private:
+	char m_head[0x24];
+	Int m_width;
+	Int m_height;
+	PartitionManagerImpl008F8D60Element *m_elements;
 };
 
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/PartitionManager.h
@@ -25,4 +43,19 @@ private:
 void PartitionManager::revealMapForPlayer(Int playerIndex)
 {
 	m_impl->revealMapForPlayer(playerIndex);
+}
+
+void PartitionManagerImpl008F8D60::revealMapForPlayer(Int playerIndex)
+{
+	if (playerIndex < 0 || playerIndex >= 16) {
+		return;
+	}
+
+	PartitionManagerImpl008F8D60Element *element = m_elements;
+	PartitionManagerImpl008F8D60Element *end = element + m_width * m_height;
+	while (element != end) {
+		element->updatePlayerCells008FC300(this, playerIndex);
+		element->updatePlayerCells008FC3B0(this, playerIndex);
+		++element;
+	}
 }
