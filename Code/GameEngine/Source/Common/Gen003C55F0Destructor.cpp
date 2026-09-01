98f493861b6edc67f9470c49848dbd6aae2079aa
dginovker <danielginovker@gmail.com>
2026-08-31 21:03:49 -0700

    Convert Gen 003C55F0 destructor

---
+++ Code/GameEngine/Source/Common/Gen003C55F0Destructor.cpp
@@ -0,0 +1,22 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+// Open-BFME: Gen_003C55F0::~Gen_003C55F0, retail 0x003C5160, 217 bytes.
+
+#include <vector>
+
+class Gen_003C55F0
+{
+public:
+	~Gen_003C55F0();
+
+private:
+	std::vector<void *> m_first;
+	int m_gapA;
+	int m_gapB;
+	std::vector<void *> m_second;
+	std::vector<void *> m_third;
+};
+
+Gen_003C55F0::~Gen_003C55F0()
+{
+}
