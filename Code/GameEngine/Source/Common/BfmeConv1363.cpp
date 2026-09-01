2cae8afa948e6c8e63104ec8c44165259eeef4da
argalad <github@argalad.com>
2026-09-01 19:36:54 +0200

    Open-BFME5: convert a trace body that tail-jumps into the log call
    0x0025A930 (75 bytes): the caller and the final virtual both take one dword, so
    MSVC writes the argument over the incoming slot and jumps instead of calling.

---
+++ Code/GameEngine/Source/Common/BfmeConv1363.cpp
@@ -0,0 +1,82 @@
+// Open-BFME5 conversions.
+
+char __cdecl bfmeCheckVHJ();
+void __cdecl bfmeRecordVHJ(int n);
+
+class BfmeMsgVHJ
+{
+public:
+	virtual void bfmeSlot00VHJ();
+	virtual void bfmeSlot04VHJ();
+	virtual void bfmeSlot08VHJ();
+	virtual void bfmeSlot0CVHJ();
+	virtual void bfmeSlot10VHJ();
+	virtual void bfmeSlot14VHJ();
+	virtual void bfmeSlot18VHJ();
+	virtual void bfmeSlot1CVHJ();
+	virtual void bfmeSlot20VHJ();
+	virtual void bfmeSlot24VHJ();
+	virtual void bfmeSlot28VHJ();
+	virtual void bfmeSlot2CVHJ();
+	virtual void bfmeSlot30VHJ();
+	virtual void bfmeSlot34VHJ();
+	virtual class BfmeMsgVHJ *bfmeSlot38VHJ(const char *t);
+	virtual void bfmeSlot3CVHJ();
+	virtual void bfmeSlot40VHJ();
+	virtual void bfmeSlot44VHJ();
+	virtual void bfmeSlot48VHJ();
+	virtual void bfmeSlot4cVHJ(int n);
+	virtual void bfmeSlot50VHJ();
+	virtual void bfmeSlot54VHJ();
+	virtual void bfmeSlot58VHJ();
+	virtual void bfmeSlot5CVHJ();
+	virtual void bfmeSlot60VHJ();
+	virtual void bfmeSlot64VHJ();
+	virtual void bfmeSlot68VHJ();
+	virtual void bfmeSlot6CVHJ();
+};
+
+class BfmeLogVHJ
+{
+public:
+	virtual void bfmeOwn00VHJ();
+	virtual void bfmeOwn04VHJ();
+	virtual void bfmeOwn08VHJ();
+	virtual void bfmeOwn0CVHJ();
+	virtual void bfmeOwn10VHJ();
+	virtual void bfmeOwn14VHJ();
+	virtual void bfmeOwn18VHJ();
+	virtual void bfmeOwn1CVHJ();
+	virtual void bfmeOwn20VHJ();
+	virtual void bfmeOwn24VHJ();
+	virtual void bfmeOwn28VHJ();
+	virtual void bfmeOwn2CVHJ();
+	virtual void bfmeOwn30VHJ();
+	virtual void bfmeOwn34VHJ();
+	virtual void bfmeOwn38VHJ();
+	virtual void bfmeOwn3CVHJ();
+	virtual void bfmeOwn40VHJ();
+	virtual void bfmeOwn44VHJ();
+	virtual void bfmeOwn48VHJ();
+	virtual void bfmeOwn4CVHJ();
+	virtual void bfmeOwn50VHJ();
+	virtual void bfmeOwn54VHJ();
+	virtual void bfmeOwn58VHJ();
+	virtual void bfmeOwn5CVHJ();
+	virtual void bfmeOwn60VHJ();
+	virtual void bfmeOwn64VHJ();
+	virtual void bfmeOwn68VHJ();
+	virtual class BfmeMsgVHJ *bfmeOwn6cVHJ(int a, int b);
+};
+
+extern BfmeLogVHJ *g_bfmeLogVHJ;
+
+void __stdcall bfmeGoVHL(int a)
+{
+	if (bfmeCheckVHJ())
+	{
+		bfmeRecordVHJ(1);
+		g_bfmeLogVHJ->bfmeOwn60VHJ();
+		g_bfmeLogVHJ->bfmeOwn6cVHJ(0, 0)->bfmeSlot38VHJ("Error! Devastate Power requires either a target object or location")->bfmeSlot4cVHJ(2);
+	}
+}
