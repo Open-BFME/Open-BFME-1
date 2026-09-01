e28e7caa60c0302dcafa324fb10caf87c78ce8a6
dginovker <danielginovker@gmail.com>
2026-08-31 19:57:35 -0700

    Refine S4 sink reconstruction identity

--- reverse/attempts/0x004135c0.cpp
+++ reverse/attempts/0x004135c0.cpp
@@ -1,14 +1,60 @@
-// ?d_004135c0@@YAXXZ
-// partial score=0.9 date=2026-08-29
-// Banked near-miss for 0x004135C0 (81B), sibling of BfmeConv1158.cpp.
-// Exact length and instruction sequence; only the register allocation differs
-// (retail keeps a0/a3/a4 in ebp/ebx/edi, MSVC 7.1 keeps a2/a3/a4).
-void BfmeOwner1158::bfmeSubMAll1158(int a, int b, int c, int d, int e)
-{
-	for (BfmeItem1158 **p = m_bfmeItems; *p; ++p) {
-		BfmeSub1158 *s = (*p)->bfmeGetSub1158();
+// ?invoke@S4Sink004135C0@@QAEXABVAsciiString@@HHHH@Z
+// partial score=0.9 date=2026-08-31
+// cl: /DNDEBUG /MD /EHsc
+class AsciiString;
+class S4Sub004135C0;
 
-		if (s)
-			s->bfmeSubM1158(a, b, c, d, e);
+class S4Sub004135C0
+{
+public:
+	virtual void slot00(); virtual void slot01(); virtual void slot02();
+	virtual void slot03(); virtual void slot04(); virtual void slot05();
+	virtual void slot06(); virtual void slot07(); virtual void slot08();
+	virtual void slot09(); virtual void slot10(); virtual void slot11();
+	virtual void slot12(); virtual void slot13(); virtual void slot14();
+	virtual void slot15(); virtual void slot16(); virtual void slot17();
+	virtual void slot18(); virtual void slot19(); virtual void slot20();
+	virtual void slot21(); virtual void slot22(); virtual void slot23();
+	virtual void slot24(); virtual void slot25(); virtual void slot26();
+	virtual void slot27(); virtual void slot28(); virtual void slot29();
+	virtual void invoke( const AsciiString &, int, int, int, int );
+};
+
+class S4Item004135C0
+{
+public:
+	virtual void slot00(); virtual void slot01(); virtual void slot02();
+	virtual void slot03(); virtual void slot04(); virtual void slot05();
+	virtual void slot06(); virtual void slot07(); virtual void slot08();
+	virtual void slot09(); virtual void slot10(); virtual void slot11();
+	virtual void slot12(); virtual void slot13(); virtual void slot14();
+	virtual void slot15(); virtual void slot16(); virtual void slot17();
+	virtual void slot18(); virtual void slot19(); virtual void slot20();
+	virtual void slot21(); virtual void slot22(); virtual void slot23();
+	virtual void slot24(); virtual void slot25(); virtual void slot26();
+	virtual void slot27(); virtual void slot28(); virtual void slot29();
+	virtual void slot30(); virtual void slot31(); virtual void slot32();
+	virtual void slot33(); virtual void slot34(); virtual void slot35();
+	virtual void slot36(); virtual void slot37(); virtual void slot38();
+	virtual S4Sub004135C0 *getSub();
+};
+
+class S4Sink004135C0
+{
+public:
+	void invoke( const AsciiString &name, int a, int b, int c, int d );
+
+private:
+	char m_padding[0x150];
+	S4Item004135C0 **m_items;
+};
+
+void S4Sink004135C0::invoke( const AsciiString &name, int a, int b, int c, int d )
+{
+	for(S4Item004135C0 **item = m_items; *item; ++item)
+	{
+		S4Sub004135C0 *sub = (*item)->getSub();
+		if(sub)
+			sub->invoke(name, a, b, c, d);
 	}
 }
