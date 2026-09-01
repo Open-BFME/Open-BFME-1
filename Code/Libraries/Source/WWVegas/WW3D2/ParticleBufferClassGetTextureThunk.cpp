8198e9d9f3c938a9fb3c3347d104be48db7c171d
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-01 06:32:18 -0700

    Convert ParticleBuffer texture accessor to C++

--- Code/Libraries/Source/WWVegas/WW3D2/ParticleBufferClassGetTextureThunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/ParticleBufferClassGetTextureThunk.cpp
@@ -1,126 +1,60 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME: ParticleBufferClass::Get_Texture, retail 0x00988F60.
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/part_buf.h
-class ParticleBufferClass {
+class TextureClass;
+
+class ParticleBufferClass
+{
 public:
-    class TextureHandleClass {};
-    TextureHandleClass Get_Texture() const;
+	class TextureHandleClass
+	{
+	public:
+		TextureHandleClass( TextureClass *texture ) : m_texture( texture ) {}
+
+	private:
+		TextureClass *m_texture;
+	};
+
+	class PointGroupShim
+	{
+	public:
+		TextureHandleClass getTexture() const;
+	};
+
+	class LineGroupShim
+	{
+	public:
+		TextureHandleClass getTexture() const;
+	};
+
+	class LineRendererShim
+	{
+	public:
+		TextureHandleClass getTexture() const;
+	};
+
+	TextureHandleClass Get_Texture() const;
+
+private:
+	unsigned char m_prefix[ 0x208 ];
+	PointGroupShim *m_pointGroup;
+	LineRendererShim *m_lineRenderer;
+	LineGroupShim *m_lineGroup;
 };
 
 // ?Get_Texture@ParticleBufferClass@@QBE?AVTextureHandleClass@1@XZ
-__declspec(naked) ParticleBufferClass::TextureHandleClass ParticleBufferClass::Get_Texture() const
+ParticleBufferClass::TextureHandleClass ParticleBufferClass::Get_Texture() const
 {
-    __asm {
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xc1
-        __emit 0x8b
-        __emit 0x88
-        __emit 0x08
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x56
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x11
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xb0
-        __emit 0x98
-        __emit 0xf8
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x59
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x88
-        __emit 0x10
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x11
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x25
-        __emit 0x5d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x59
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x80
-        __emit 0x0c
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x13
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0xc8
-        __emit 0x70
-        __emit 0xfd
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x59
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5e
-        __emit 0x59
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
+	// Retail keeps the empty handle's pointer slot as a stack temporary even
+	// though the three successful arms construct directly into the caller's
+	// return storage.
+	TextureClass *volatile emptyTexture = 0;
+
+	if (m_pointGroup)
+		return m_pointGroup->getTexture();
+	if (m_lineGroup)
+		return m_lineGroup->getTexture();
+	if (m_lineRenderer)
+		return m_lineRenderer->getTexture();
+	return TextureHandleClass( 0 );
 }
