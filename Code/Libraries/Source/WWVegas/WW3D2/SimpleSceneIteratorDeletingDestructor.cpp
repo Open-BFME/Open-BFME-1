a18ac82e4b607a5dd837ffda0916adb34a9a4eb6
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 03:38:59 -0400

    Match SimpleSceneIterator scalar destructor

--- Code/Libraries/Source/WWVegas/WW3D2/SimpleSceneIteratorDeletingDestructor.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/SimpleSceneIteratorDeletingDestructor.cpp
@@ -1,12 +1,23 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class SimpleSceneIterator
+class SceneIterator
+{
+public:
+	virtual ~SceneIterator() {}
+};
+
+class SimpleSceneIterator : public SceneIterator
 {
 public:
 	virtual ~SimpleSceneIterator();
 };
 
 __declspec(noinline) SimpleSceneIterator::~SimpleSceneIterator() {}
+
+void Force_SimpleSceneIterator_Vtable()
+{
+	SimpleSceneIterator iterator;
+}
 
 void Force_SimpleSceneIterator_Deleting_Destructor(SimpleSceneIterator *iterator)
 {
