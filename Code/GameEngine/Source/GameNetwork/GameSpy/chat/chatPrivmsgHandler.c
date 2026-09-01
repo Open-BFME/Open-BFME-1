b2099b37ba12e75270d3cbb89416801db1007afe
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 07:12:07 -0400

    Convert ciNoticeHandler to C

--- Code/GameEngine/Source/GameNetwork/GameSpy/chat/chatPrivmsgHandler.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/chat/chatPrivmsgHandler.c
@@ -6,7 +6,8 @@ typedef int CHATBool;
 enum
 {
 	CHAT_MESSAGE,
-	CHAT_ACTION
+	CHAT_ACTION,
+	CHAT_NOTICE = 3
 };
 
 enum
@@ -143,6 +144,49 @@ void ciPrivmsgHandler(CHAT chat, const ciServerMessage *message)
 			params.user = from;
 			params.message = msg;
 			params.type = action ? CHAT_ACTION : CHAT_MESSAGE;
+			ciAddCallback_(chat, CALLBACK_CHANNEL_MESSAGE,
+				callbacks->channelMessage, &params, callbacks->param,
+				0, target, sizeof(params));
+		}
+	}
+}
+
+void ciNoticeHandler(CHAT chat, const ciServerMessage *message)
+{
+	char *target;
+	char *msg;
+	ciConnection *connection = (ciConnection *)chat;
+
+	if (message->numParams != 2)
+		return;
+
+	target = message->params[0];
+	msg = message->params[1];
+
+	if (strcasecmp(target, connection->nick) == 0)
+	{
+		if (connection->globalCallbacks.privateMessage != 0)
+		{
+			ciCallbackPrivateMessageParams params;
+			params.user = message->nick ? message->nick : 0;
+			params.message = msg;
+			params.type = CHAT_NOTICE;
+			ciAddCallback_(chat, CALLBACK_PRIVATE_MESSAGE,
+				connection->globalCallbacks.privateMessage, &params,
+				connection->globalCallbacks.param, 0, 0, sizeof(params));
+		}
+	}
+	else
+	{
+		chatChannelCallbacks *callbacks =
+			ciGetChannelCallbacks(chat, target);
+		if (callbacks != 0 && callbacks->channelMessage != 0)
+		{
+			ciCallbackChannelMessageParams params;
+			params.channel = target;
+			params.user = message->nick ? message->nick : 0;
+			params.message = msg;
+			params.type = CHAT_NOTICE;
 			ciAddCallback_(chat, CALLBACK_CHANNEL_MESSAGE,
 				callbacks->channelMessage, &params, callbacks->param,
 				0, target, sizeof(params));
