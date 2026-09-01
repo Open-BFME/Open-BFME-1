d5256cb37df048fdf7b33392ea17d6e7b7026825
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 20:53:19 -0400

    Convert transactional filter setter to C++

---
+++ Code/GameEngine/Source/Common/BfmeValidatedHandle.cpp
@@ -0,0 +1,38 @@
+// Open-BFME5 conversion for the validated handle setter at 0x0073B5F0.
+
+enum FilterTypes {};
+enum FilterModes {};
+
+class W3DShaderManager
+{
+public:
+	static bool filterSetup(FilterTypes type, FilterModes mode);
+};
+
+class BfmeValidatedHandle
+{
+public:
+	bool setValue(void *value);
+
+private:
+	char m_pad0[0x10c];
+	void *volatile m_value;
+	void *m_reference;
+};
+
+bool BfmeValidatedHandle::setValue(void *value)
+{
+	void *oldValue = m_value;
+	m_value = value;
+
+	if (value != 0 && m_reference != 0 &&
+		!W3DShaderManager::filterSetup(
+			static_cast<FilterTypes>(reinterpret_cast<unsigned int>(m_reference)),
+			static_cast<FilterModes>(reinterpret_cast<unsigned int>(value))))
+	{
+		m_value = oldValue;
+		return false;
+	}
+
+	return true;
+}
