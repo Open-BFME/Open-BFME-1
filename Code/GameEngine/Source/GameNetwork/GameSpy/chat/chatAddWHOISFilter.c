059411c9010e990ee72fd4d32c1baf233db9e978
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 06:35:51 -0400

    Convert ciAddWHOISFilter to C

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/chat/chatAddWHOISFilter.c
@@ -0,0 +1,89 @@
+// GameSpy Chat SDK -- chatHandlers.c, 2007 release.
+
+typedef void *CHAT;
+typedef void (*chatGetUserInfoCallback)(CHAT chat, int success,
+	const char *nick, const char *user, const char *name,
+	const char *address, int numChannels, const char **channels, void *param);
+
+typedef struct WHOISData
+{
+	char *user;
+	char *name;
+	char *address;
+	int numChannels;
+	char **channels;
+} WHOISData;
+
+typedef struct ciServerMessageFilter
+{
+	int type;
+	unsigned int timeout;
+	char *name;
+	char *name2;
+	void *callback;
+	void *callback2;
+	void *param;
+	void *data;
+	int ID;
+	struct ciServerMessageFilter *pnext;
+} ciServerMessageFilter;
+
+typedef struct ciConnection
+{
+	char reserved[0x814];
+	ciServerMessageFilter *filterList;
+	ciServerMessageFilter *lastFilter;
+	int nextID;
+} ciConnection;
+
+__declspec(dllimport) void * __cdecl malloc(unsigned int size);
+__declspec(dllimport) void __cdecl free(void *memory);
+void * __cdecl memset(void *dest, int value, unsigned int count);
+unsigned int current_time(void);
+char *goastrdup(const char *source);
+int ciGetNextID(CHAT chat);
+
+static int ciAddFilter(CHAT chat, int type, const char *name,
+	const char *name2, void *callback, void *callback2, void *param, void *data)
+{
+	ciServerMessageFilter *filter;
+	ciConnection *connection = (ciConnection *)chat;
+
+	filter = (ciServerMessageFilter *)malloc(sizeof(ciServerMessageFilter));
+	if (filter == 0)
+		return 0;
+
+	memset(filter, 0, sizeof(ciServerMessageFilter));
+	filter->type = type;
+	filter->timeout = current_time() + 60000;
+	filter->callback = callback;
+	filter->callback2 = callback2;
+	filter->param = param;
+	filter->data = data;
+	filter->name = name ? goastrdup(name) : 0;
+	filter->name2 = name2 ? goastrdup(name2) : 0;
+	filter->ID = ciGetNextID(chat);
+
+	if (connection->filterList == 0)
+		connection->filterList = filter;
+	else
+		connection->lastFilter->pnext = filter;
+	connection->lastFilter = filter;
+
+	return filter->ID;
+}
+
+int ciAddWHOISFilter(CHAT chat, const char *user,
+	chatGetUserInfoCallback callback, void *param)
+{
+	int rcode;
+	WHOISData *data = (WHOISData *)malloc(sizeof(WHOISData));
+	if (data == 0)
+		return 0;
+
+	memset(data, 0, sizeof(WHOISData));
+	rcode = ciAddFilter(chat, 4, user, 0, (void *)callback, 0, param, data);
+	if (rcode == 0)
+		free(data);
+	return rcode;
+}
