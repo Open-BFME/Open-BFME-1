f821f66583271ac1addb2ae37ea97e5b77a99983
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-01 06:36:21 -0700

    Convert disconnect count lookup to C++

--- Code/GameEngine/Source/Common/GetAdditionalDisconnectsFromUserFile_Thunk.cpp
+++ Code/GameEngine/Source/Common/GetAdditionalDisconnectsFromUserFile_Thunk.cpp
@@ -1,111 +1,45 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME: GetAdditionalDisconnectsFromUserFile, retail 0x004DB4E0.
 
-int __cdecl GetAdditionalDisconnectsFromUserFile(int);
+struct BfmeDisconnectQueue;
+
+typedef int (__fastcall *BfmeGetDisconnectCount)( BfmeDisconnectQueue *queue );
+typedef void (__fastcall *BfmeRefreshDisconnectCount)( BfmeDisconnectQueue *queue );
+
+struct BfmeDisconnectQueueVtable
+{
+	void *m_beforeCount[ 0x170 / sizeof( void * ) ];
+	BfmeGetDisconnectCount m_getCount;
+	BfmeRefreshDisconnectCount m_refreshCount;
+};
+
+struct BfmeDisconnectQueue
+{
+	BfmeDisconnectQueueVtable *m_vtable;
+
+	int getCount() { return m_vtable->m_getCount( this ); }
+	void refreshCount() { m_vtable->m_refreshCount( this ); }
+};
+
+int __fastcall readAdditionalDisconnectsFromUserFile( int playerID );
+
+static BfmeDisconnectQueue *getDisconnectQueue()
+{
+	return *reinterpret_cast<BfmeDisconnectQueue **>( 0x012F7194 );
+}
 
 // ?GetAdditionalDisconnectsFromUserFile@@YAHH@Z
-__declspec(naked) int __cdecl GetAdditionalDisconnectsFromUserFile(int)
+int __cdecl GetAdditionalDisconnectsFromUserFile( int playerID )
 {
-	__asm {
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x08
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0xc3
-        __emit 0xfb
-        __emit 0xff
-        __emit 0xff
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0xf8
-        __emit 0x75
-        __emit 0x05
-        __emit 0x5f
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x5e
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x01
-        __emit 0xff
-        __emit 0x90
-        __emit 0x70
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x7e
-        __emit 0x12
-        __emit 0x85
-        __emit 0xff
-        __emit 0x75
-        __emit 0x0e
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x11
-        __emit 0xff
-        __emit 0x92
-        __emit 0x74
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x01
-        __emit 0xff
-        __emit 0x90
-        __emit 0x70
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xf8
-        __emit 0xff
-        __emit 0x74
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x94
-        __emit 0x71
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x11
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0xff
-        __emit 0xa2
-        __emit 0x70
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc7
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0xc3
-	}
+	int fileCount = readAdditionalDisconnectsFromUserFile( playerID );
+	if (playerID == 0)
+		return 0;
+
+	if (getDisconnectQueue()->getCount() > 0 && fileCount == 0)
+		getDisconnectQueue()->refreshCount();
+
+	if (getDisconnectQueue()->getCount() != -1)
+		return getDisconnectQueue()->getCount();
+
+	return fileCount;
 }
