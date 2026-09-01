359d6d8f0157ade5c8aa92caadf0d0368566667e
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-31 22:03:42 -0400

    Convert Mem00197C60 destructor

---
+++ Code/GameEngine/Source/Common/Mem00197C60Destructor.cpp
@@ -0,0 +1,69 @@
+// cl: /DNDEBUG /MD /EHsc /O2 /Ob2
+
+#include <new>
+
+class Mem00197C60;
+
+class Dict
+{
+	friend class Mem00197C60;
+	void releaseData();
+};
+
+struct Mem00197C60Element
+{
+	unsigned int m_payload[ 3 ];
+	Dict m_dictionary;
+};
+
+namespace _STL
+{
+
+template <bool Threads, int Instance>
+class __node_alloc
+{
+	static void _M_deallocate( void *storage, unsigned int bytes );
+
+public:
+	static void deallocate( void *storage, unsigned int bytes )
+	{
+		if ( bytes > 128 )
+			::operator delete( storage );
+		else
+			_M_deallocate( storage, bytes );
+	}
+};
+
+}
+
+class Mem00197C60Storage
+{
+protected:
+	Mem00197C60Element *m_begin;
+	Mem00197C60Element *m_end;
+	Mem00197C60Element *m_capacity;
+
+	__forceinline ~Mem00197C60Storage()
+	{
+		if ( m_begin )
+		{
+			const unsigned int bytes =
+				( m_capacity - m_begin ) * sizeof( Mem00197C60Element );
+			_STL::__node_alloc<true, 0>::deallocate( m_begin, bytes );
+		}
+	}
+};
+
+class Mem00197C60 : private Mem00197C60Storage
+{
+public:
+	~Mem00197C60();
+};
+
+Mem00197C60::~Mem00197C60()
+{
+	Mem00197C60Element *finish = m_end;
+	Mem00197C60Element *item = m_begin;
+	for ( ; item != finish; ++item )
+		item->m_dictionary.releaseData();
+}
