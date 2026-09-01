a03d0ab427041ecf17451437b69983a98c88794c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 05:39:19 -0400

    gamelogic: convert ActAsDozerState constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/WorkerAIUpdateStateConstructors.cpp
@@ -0,0 +1,49 @@
+// cl: /DNDEBUG /DWIN32 /MD /D_STLP_USE_STATIC_LIB
+// stlport
+//
+// Authentic constructors for the two worker master-state wrappers.  Their
+// definitions come directly from WorkerAIUpdate.cpp; this focused TU keeps the
+// retail out-of-line constructor bodies available even though the full source
+// inlines them into WorkerStateMachine's constructor.
+#include "../../../../../../Libraries/Source/WWVegas/WWLib/string_base.h"
+
+class AsciiString
+{
+public:
+	AsciiString(const char *text) : m_string(text) {}
+
+	StringBase<char> m_string;
+};
+
+class StateMachine;
+
+class State
+{
+public:
+	State(StateMachine *machine, AsciiString name);
+	virtual void stateAnchor(void);
+};
+
+class ActAsDozerState : public State
+{
+public:
+	ActAsDozerState(StateMachine *machine);
+	virtual void stateAnchor(void);
+};
+
+class ActAsSupplyTruckState : public State
+{
+public:
+	ActAsSupplyTruckState(StateMachine *machine);
+	virtual void stateAnchor(void);
+};
+
+ActAsDozerState::ActAsDozerState(StateMachine *machine) :
+	State(machine, AsciiString("ActAsDozerState"))
+{
+}
+
+ActAsSupplyTruckState::ActAsSupplyTruckState(StateMachine *machine) :
+	State(machine, AsciiString("ActAsSupplyTruckState"))
+{
+}
