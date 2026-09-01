b95b19ce63955cdb7e156c4ed6b83fe1f95efb1b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 02:09:31 -0400

    Bank clean DamageFXStore constructor

---
+++ reverse/attempts/0x00067c10.cpp
@@ -0,0 +1,12 @@
+// ??0DamageFXStore@@QAE@XZ
+// partial score=0.96 date=2026-09-01
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /DBFME_STLP_NODE_ALLOC /Ireference/shims/stlp_nodealloc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib
+// stlport
+
+#include "PreRTS.h"
+#include "Common/DamageFX.h"
+
+DamageFXStore::DamageFXStore()
+{
+	m_dfxmap.clear();
+}
