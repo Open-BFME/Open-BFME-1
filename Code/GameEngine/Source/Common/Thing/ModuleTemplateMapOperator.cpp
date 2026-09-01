27457c83cd1b698d248d55d2f425d50524108dcc
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-09-01 00:43:26 -0400

    Convert ModuleTemplateMap operator to C++

---
+++ Code/GameEngine/Source/Common/Thing/ModuleTemplateMapOperator.cpp
@@ -0,0 +1,24 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include
+// stlport
+
+#include <map>
+
+class ModuleFactory
+{
+public:
+	class ModuleTemplate
+	{
+	public:
+		ModuleTemplate() : m_first( 0 ), m_second( 0 ), m_third( 0 ), m_fourth( 0 ) {}
+
+	private:
+		int m_first;
+		int m_second;
+		int m_third;
+		int m_fourth;
+	};
+
+};
+
+// ModuleFactory::ModuleTemplateMap is the retail typedef for this exact map.
+template class std::map<int, ModuleFactory::ModuleTemplate>;
