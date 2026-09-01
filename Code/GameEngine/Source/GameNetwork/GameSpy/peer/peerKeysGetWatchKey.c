6c16ca3fe3f1e08e4cc16fe057b9b01e9c9031b0
dginovker <danielginovker@gmail.com>
2026-08-31 23:41:09 -0700

    Convert GameSpy room cache cleanser to C source

--- Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerKeysGetWatchKey.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerKeysGetWatchKey.c
@@ -7,10 +7,29 @@ typedef int RoomType;
 
 typedef struct piConnection
 {
-	unsigned char pad0[0x18C4];
+	unsigned char pad0[0x18AC];
+	HashTable globalWatchKeys[3];
+	HashTable roomWatchKeys[3];
 	HashTable globalWatchCache;
 	HashTable roomWatchCache[3];
 } piConnection;
+
+typedef struct piPlayer
+{
+	char nick[0x40];
+	int inRoom[3];
+} piPlayer;
+
+typedef struct piWatchKey
+{
+	char *key;
+} piWatchKey;
+
+typedef struct piCleanseRoomCacheMapData
+{
+	PEER peer;
+	RoomType roomType;
+} piCleanseRoomCacheMapData;
 
 typedef struct piCacheKey
 {
@@ -20,6 +39,8 @@ typedef struct piCacheKey
 } piCacheKey;
 
 void *TableLookup(HashTable table, const void *element);
+void TableRemove(HashTable table, const void *element);
+piPlayer *piGetPlayer(PEER peer, const char *nick);
 
 static const char *piGetWatchKeyA(const char *nick, const char *key,
 	HashTable watchCache)
@@ -48,4 +69,21 @@ const char *piGetRoomWatchKeyA(PEER peer, RoomType roomType, const char *nick,
 {
 	piConnection *connection = (piConnection *)peer;
 	return piGetWatchKeyA(nick, key, connection->roomWatchCache[roomType]);
+}
+
+void piCleanseRoomCacheMap(void *elem, void *clientData)
+{
+	piPlayer *player;
+	piCacheKey *cacheKey = (piCacheKey *)elem;
+	piCleanseRoomCacheMapData *data = (piCleanseRoomCacheMapData *)clientData;
+	piConnection *connection = (piConnection *)data->peer;
+	piWatchKey watchKeyTemp;
+
+	watchKeyTemp.key = cacheKey->key;
+	player = piGetPlayer(data->peer, cacheKey->nick);
+	if (player && player->inRoom[data->roomType]) {
+		if (TableLookup(connection->roomWatchKeys[data->roomType], &watchKeyTemp))
+			return;
+	}
+	TableRemove(connection->globalWatchCache, cacheKey);
 }
