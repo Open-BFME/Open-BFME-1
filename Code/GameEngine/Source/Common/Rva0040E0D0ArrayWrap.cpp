449a7135d6f515280665918d005579a6e6d44f0f
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-02 00:52:20 -0400

    Open-BFME5: convert 0x0040D820 element tail unsigned-float scales
    BfmeElemVMZ::bfmeTailVMZ stores a0, four (float)unsigned virt(0x2C/0x30)
    products, and k into the 0x18-stride slot the 0x0040E0D0 wrappers select.

--- Code/GameEngine/Source/Common/Rva0040E0D0ArrayWrap.cpp
+++ Code/GameEngine/Source/Common/Rva0040E0D0ArrayWrap.cpp
@@ -1,10 +1,33 @@
 // cl: /O2 /Ob0
 
+class BfmeSrcVMZ
+{
+public:
+	virtual int v0();
+	virtual int v1();
+	virtual int v2();
+	virtual int v3();
+	virtual int v4();
+	virtual int v5();
+	virtual int v6();
+	virtual int v7();
+	virtual int v8();
+	virtual int v9();
+	virtual int v10();
+	virtual unsigned v11();
+	virtual unsigned v12();
+};
+
 class BfmeElemVMZ
 {
 public:
 	void bfmeTailVMZ(void *self, int a0, int a2, int a3, int a4, int a5, int k);
-	char m_pad[0x18];
+	int m_a0;
+	float m_f0;
+	float m_f1;
+	float m_f2;
+	float m_f3;
+	int m_k;
 };
 
 class BfmeThingVMZ
@@ -26,4 +49,15 @@ void BfmeThingVMZ::bfmeGo2VMZ(int a0, int i, int a2, int a3, int a4, int a5)
 {
 	if (i < 3)
 		m_arr[i].bfmeTailVMZ(this, a0, a2, a3, a4, a5, 2);
+}
+
+void BfmeElemVMZ::bfmeTailVMZ(void *self, int a0, int a2, int a3, int a4, int a5, int k)
+{
+	BfmeSrcVMZ *o = (BfmeSrcVMZ *)self;
+	m_a0 = a0;
+	m_f0 = (float)o->v11() * *(float *)&a2;
+	m_f1 = (float)o->v12() * *(float *)&a3;
+	m_f2 = (float)o->v11() * *(float *)&a4;
+	m_f3 = (float)o->v12() * *(float *)&a5;
+	m_k = k;
 }
