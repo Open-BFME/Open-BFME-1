d521f9f826545019e572662b0f6da74807e9fba9
Jonathan <jonathan@users.noreply.github.com>
2026-09-02 07:51:54 -0400

    Open-BFME5: convert DataChunkInput readNameKey

--- Code/GameEngine/Source/Common/System/DataChunkInput_readNameKey_Thunk.cpp
+++ Code/GameEngine/Source/Common/System/DataChunkInput_readNameKey_Thunk.cpp
@@ -1,204 +1,134 @@
 // cl: /DNDEBUG /MD /EHsc
 // readable body of ?readNameKey@DataChunkInput@@QAE?AW4NameKeyType@@XZ: Code/GameEngine/Source/Common/System/DataChunk.cpp
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: convert DataChunkInput::readNameKey to clean C++.
 
-enum NameKeyType {};
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/Common/DataChunk.h
-class DataChunkInput
+typedef int Int;
+
+enum NameKeyType
 {
-public:
-	NameKeyType readNameKey();
+	NAMEKEY_INVALID = 0
 };
 
-// ?readNameKey@DataChunkInput@@QAE?AW4NameKeyType@@XZ
-__declspec(naked) NameKeyType DataChunkInput::readNameKey()
+class AsciiString
 {
-	__asm {
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x88
-        __emit 0xca
-        __emit 0xff
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xec
-        __emit 0x08
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0xff
-        __emit 0x15
-        __emit 0x30
-        __emit 0x8f
-        __emit 0x35
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x24
-        __emit 0xd5
-        __emit 0x2e
-        __emit 0x01
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8b
-        __emit 0x01
-        __emit 0xff
-        __emit 0x50
-        __emit 0x40
-        __emit 0x8b
-        __emit 0x0e
-        __emit 0x8b
-        __emit 0x11
-        __emit 0x6a
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x50
-        __emit 0xff
-        __emit 0x12
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x1c
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x14
-        __emit 0xb9
-        __emit 0xfc
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xeb
-        __emit 0x03
-        __emit 0x8d
-        __emit 0x49
-        __emit 0x00
-        __emit 0x01
-        __emit 0x48
-        __emit 0x18
-        __emit 0x8b
-        __emit 0x40
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x75
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc1
-        __emit 0xf8
-        __emit 0x08
-        __emit 0x50
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x51
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x04
-        __emit 0xe8
-        __emit 0x2b
-        __emit 0x08
-        __emit 0xf0
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x05
-        __emit 0x83
-        __emit 0xc0
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x05
-        __emit 0xb8
-        __emit 0x8b
-        __emit 0x38
-        __emit 0x07
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x00
-        __emit 0xd6
-        __emit 0x2e
-        __emit 0x01
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x72
-        __emit 0x74
-        __emit 0xf3
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xc8
-        __emit 0x3f
-        __emit 0x78
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x14
-        __emit 0xc3
+	void *m_data;
+
+public:
+	const char *str() const
+	{
+		static const char empty = 0;
+		return m_data ? static_cast<const char *>(m_data) + 8 : &empty;
 	}
+	~AsciiString();
+};
+
+class DataChunkTableOfContents
+{
+	void *m_list;
+	Int m_listLength;
+	unsigned int m_nextID;
+	int m_headerOpened;
+
+public:
+	AsciiString getName(unsigned int id);
+};
+
+class ChunkInputStream
+{
+public:
+	virtual Int read(void *pData, Int numBytes) = 0;
+};
+
+struct InputChunk
+{
+	void *vtable;
+	InputChunk *next;
+	unsigned int id;
+	unsigned short version;
+	unsigned short padding;
+	Int chunkStart;
+	Int dataSize;
+	Int dataLeft;
+};
+
+class DataChunkInput
+{
+	ChunkInputStream *m_file;
+	DataChunkTableOfContents m_contents;
+	Int m_fileposOfFirstChunk;
+	void *m_parserList;
+	InputChunk *m_chunkStack;
+
+	void decrementDataLeft(Int size)
+	{
+		InputChunk *c = m_chunkStack;
+		while (c)
+		{
+			c->dataLeft -= size;
+			c = c->next;
+		}
+	}
+
+	Int readInt(void)
+	{
+		Int i;
+		m_file->read(&i, sizeof(Int));
+		decrementDataLeft(sizeof(Int));
+		return i;
+	}
+
+public:
+	NameKeyType readNameKey(void);
+};
+
+class BFME_GameEngineServiceWindowsOS
+{
+public:
+	virtual void _bfme_ge_slot00() = 0;
+	virtual void _bfme_ge_slot01() = 0;
+	virtual void _bfme_ge_slot02() = 0;
+	virtual void _bfme_ge_slot03() = 0;
+	virtual void _bfme_ge_slot04() = 0;
+	virtual void _bfme_ge_slot05() = 0;
+	virtual void _bfme_ge_slot06() = 0;
+	virtual void _bfme_ge_slot07() = 0;
+	virtual void _bfme_ge_slot08() = 0;
+	virtual void _bfme_ge_slot09() = 0;
+	virtual void _bfme_ge_slot10() = 0;
+	virtual void _bfme_ge_slot11() = 0;
+	virtual void _bfme_ge_slot12() = 0;
+	virtual void _bfme_ge_slot13() = 0;
+	virtual void _bfme_ge_slot14() = 0;
+	virtual void _bfme_ge_slot15() = 0;
+	virtual void serviceWindowsOS() = 0;
+};
+
+class GameEngine;
+extern GameEngine *TheGameEngine;
+
+class NameKeyGenerator
+{
+public:
+	NameKeyType nameToKey(const char *name);
+};
+extern NameKeyGenerator *TheNameKeyGenerator;
+
+extern "C" __declspec(dllimport) void __stdcall Sleep(unsigned long milliseconds);
+
+static inline void bfmeDataChunkYieldToOS(void)
+{
+	::Sleep(0);
+	if (TheGameEngine)
+		reinterpret_cast<BFME_GameEngineServiceWindowsOS *>(TheGameEngine)->serviceWindowsOS();
+}
+
+NameKeyType DataChunkInput::readNameKey(void)
+{
+	bfmeDataChunkYieldToOS();
+	Int keyAndType = readInt();
+	keyAndType >>= 8;
+
+	AsciiString kname = m_contents.getName(keyAndType);
+	NameKeyType k = TheNameKeyGenerator->nameToKey(kname.str());
+	return k;
 }
