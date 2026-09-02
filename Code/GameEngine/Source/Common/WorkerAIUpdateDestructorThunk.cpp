f7d8234666a5d8ccac5d6e7ce477f6742c1cab26
Jonathan <jonathan@users.noreply.github.com>
2026-09-02 01:20:22 -0400

    Open-BFME5: convert WorkerAIUpdate destructor to clean C++

--- Code/GameEngine/Source/Common/WorkerAIUpdateDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/WorkerAIUpdateDestructorThunk.cpp
@@ -1,274 +1,145 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: convert WorkerAIUpdate::~WorkerAIUpdate to clean C++.
+//
+// The retail BFME object has the module's eight polymorphic subobjects at
+// 0x00, 0x0c, 0x10, 0x20, 0x24, 0x340, 0x344 and 0x348.  These small local
+// base views preserve those boundaries while leaving the ZH header untouched.
+// The first base is the out-of-line BFME destructor at 0x0001c774; the other
+// interface tails are empty in this destructor and only contribute their
+// vptr stores.
 
-class __declspec(novtable) WorkerAIUpdate
+class Rva0001C774FlatBase
 {
 public:
-    virtual ~WorkerAIUpdate();
+	virtual ~Rva0001C774FlatBase();
+
+private:
+	unsigned char m_pad[0x08];
+};
+
+class BfmeWorkerInterfaceTail0
+{
+public:
+	virtual void marker();
+};
+
+class BfmeWorkerInterfaceTail1
+{
+public:
+	virtual void marker();
+
+private:
+	unsigned char m_pad[0x0c];
+};
+
+class BfmeWorkerInterfaceTail2
+{
+public:
+	virtual void marker();
+};
+
+class BfmeWorkerInterfaceTail3
+{
+public:
+	virtual void marker();
+
+private:
+	unsigned char m_pad[0x318];
+};
+
+class BfmeWorkerInterfaceTail4
+{
+public:
+	virtual void marker();
+};
+
+class BfmeWorkerInterfaceTail5
+{
+public:
+	virtual void marker();
+};
+
+class BfmeWorkerInterfaceTail6
+{
+public:
+	virtual void marker();
+};
+
+class WorkerStateMachine
+{
+public:
+	virtual ~WorkerStateMachine();
+	void deleteInstance() { delete this; }
+};
+
+class DozerPrimaryStateMachine
+{
+public:
+	virtual ~DozerPrimaryStateMachine();
+	void deleteInstance() { delete this; }
+};
+
+class SupplyTruckStateMachine
+{
+public:
+	virtual ~SupplyTruckStateMachine();
+	void deleteInstance() { delete this; }
+};
+
+class AudioEventRTS
+{
+public:
+	virtual ~AudioEventRTS();
+
+private:
+	unsigned char m_data[0x6c];
+};
+
+class BfmeWorkerDockPoint
+{
+public:
+	~BfmeWorkerDockPoint();
+
+private:
+	unsigned char m_data[0x10];
+};
+
+class WorkerAIUpdate
+	: public Rva0001C774FlatBase,
+	  public BfmeWorkerInterfaceTail0,
+	  public BfmeWorkerInterfaceTail1,
+	  public BfmeWorkerInterfaceTail2,
+	  public BfmeWorkerInterfaceTail3,
+	  public BfmeWorkerInterfaceTail4,
+	  public BfmeWorkerInterfaceTail5,
+	  public BfmeWorkerInterfaceTail6
+{
+public:
+	virtual ~WorkerAIUpdate();
+
+private:
+	unsigned char m_beforeDockPoints[0x1c];
+	BfmeWorkerDockPoint m_dockPoint[9];
+	unsigned char m_betweenDockPointsAndMachines[0x24];
+	WorkerStateMachine *m_workerMachine;
+	DozerPrimaryStateMachine *m_dozerMachine;
+	SupplyTruckStateMachine *m_supplyTruckStateMachine;
+	AudioEventRTS m_buildingSound;
+	AudioEventRTS m_suppliesDepletedVoice;
 };
 
 // ??1WorkerAIUpdate@@UAE@XZ
-__declspec(naked) WorkerAIUpdate::~WorkerAIUpdate()
+WorkerAIUpdate::~WorkerAIUpdate()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x8c
-        __emit 0x40
-        __emit 0x01
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x53
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x58
-        __emit 0x9f
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x90
-        __emit 0x9e
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0x80
-        __emit 0x9e
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x20
-        __emit 0x7c
-        __emit 0x9e
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x24
-        __emit 0x60
-        __emit 0x9e
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x86
-        __emit 0x40
-        __emit 0x03
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x9d
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x86
-        __emit 0x44
-        __emit 0x03
-        __emit 0x00
-        __emit 0x00
-        __emit 0x88
-        __emit 0x9d
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x86
-        __emit 0x48
-        __emit 0x03
-        __emit 0x00
-        __emit 0x00
-        __emit 0x84
-        __emit 0x9d
-        __emit 0x0c
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x8e
-        __emit 0x20
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x33
-        __emit 0xdb
-        __emit 0x3b
-        __emit 0xcb
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x03
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x06
-        __emit 0x8b
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x01
-        __emit 0xff
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x8e
-        __emit 0x24
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x3b
-        __emit 0xcb
-        __emit 0x89
-        __emit 0x9e
-        __emit 0x20
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x06
-        __emit 0x8b
-        __emit 0x11
-        __emit 0x6a
-        __emit 0x01
-        __emit 0xff
-        __emit 0x12
-        __emit 0x8b
-        __emit 0x8e
-        __emit 0x1c
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x3b
-        __emit 0xcb
-        __emit 0x89
-        __emit 0x9e
-        __emit 0x24
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x06
-        __emit 0x8b
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x01
-        __emit 0xff
-        __emit 0x10
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x98
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x89
-        __emit 0x9e
-        __emit 0x1c
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x02
-        __emit 0xe8
-        __emit 0xfb
-        __emit 0xf3
-        __emit 0xd5
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x28
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x01
-        __emit 0xe8
-        __emit 0xeb
-        __emit 0xf3
-        __emit 0xd5
-        __emit 0xff
-        __emit 0x68
-        __emit 0x6c
-        __emit 0xfd
-        __emit 0x40
-        __emit 0x00
-        __emit 0x6a
-        __emit 0x09
-        __emit 0x6a
-        __emit 0x10
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x68
-        __emit 0x03
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x88
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x24
-        __emit 0xe8
-        __emit 0x13
-        __emit 0xf2
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x02
-        __emit 0x4c
-        __emit 0xd5
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	if (m_dozerMachine)
+		m_dozerMachine->deleteInstance();
+	m_dozerMachine = 0;
+
+	if (m_supplyTruckStateMachine)
+		m_supplyTruckStateMachine->deleteInstance();
+	m_supplyTruckStateMachine = 0;
+
+	if (m_workerMachine)
+		m_workerMachine->deleteInstance();
+	m_workerMachine = 0;
 }
