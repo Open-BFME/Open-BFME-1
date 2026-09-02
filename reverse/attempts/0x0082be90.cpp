3727a9f745934ee3f04c921e9d5c2c7ca8dffd02
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-01 23:52:47 -0400

    Open-BFME5: convert an /Od signed-char inequality
    Inline equality is expanded at /Od /Ob1 into sub/neg/sbb/inc, then inverted
    with movzx/neg/sbb/inc. Right-to-left argument evaluation loads this first.

--- reverse/attempts/0x0082be90.cpp
+++
@@ -1,8 +0,0 @@
-// ?d_0082be90@@YAXXZ
-// partial score=0.7 date=2026-08-30
-// cl: /Od
-class BfmeChar82BE90 { public: bool bfmeDiffers82BE90(const BfmeChar82BE90 *other) const; private: char m_value; };
-bool BfmeChar82BE90::bfmeDiffers82BE90(const BfmeChar82BE90 *other) const
-{
-	return !((other->m_value - m_value) == 0 ? true : false);
-}
