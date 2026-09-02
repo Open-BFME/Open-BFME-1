6e5bd523ff42161b8c4552a52dd030afcc5ed7fd
dginovker <danielginovker@gmail.com>
2026-09-03 05:09:19 +0900

    Fold Gen_00588D10 home; close the transition window cleanly

---
+++ Code/GameEngine/Source/Common/Bfme5TransitionWindowClose.cpp
@@ -0,0 +1,45 @@
+class GameWindow
+{
+public:
+	int winEnable(bool enable);
+	void bfmeClose(bool close);
+};
+
+class BfmeTransitionMD
+{
+public:
+	void bfmeDetach(GameWindow *window);
+};
+
+extern BfmeTransitionMD *g_bfmeTransitionMD;
+void bfmeTransitionFinished(void);
+
+class Gen_00588D10
+{
+public:
+	void bfmeFinish(void);
+
+private:
+	char m_bfme00[0x1A0];
+	bool m_bfmeActive;
+	char m_bfme1A1[3];
+	GameWindow *m_bfmeWindow;
+};
+
+// ?bfmeFinish@Gen_00588D10@@QAEXXZ
+void Gen_00588D10::bfmeFinish(void)
+{
+	if (!m_bfmeActive)
+		return;
+
+	if (m_bfmeWindow)
+	{
+		if (g_bfmeTransitionMD)
+			g_bfmeTransitionMD->bfmeDetach(m_bfmeWindow);
+		m_bfmeWindow->winEnable(false);
+		m_bfmeWindow->bfmeClose(true);
+	}
+
+	bfmeTransitionFinished();
+	m_bfmeActive = false;
+}
