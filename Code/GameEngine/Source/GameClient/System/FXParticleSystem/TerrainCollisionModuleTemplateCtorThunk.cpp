637403ccdecd45c8414417169469881ba6fc8c59
Jonathan <jonathan@users.noreply.github.com>
2026-09-02 10:45:59 -0400

    Open-BFME5: convert TerrainCollisionModuleTemplate constructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleTemplateCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleTemplateCtorThunk.cpp
@@ -3,131 +3,99 @@
 namespace FXParticleSystem
 {
 
+// Retail layout: CategoryModuleTemplate<8> has a ModuleTemplate primary base,
+// a CategoryModuleInfo<8> secondary base at +4, and two flag bytes at +8/+9.
+class __declspec(novtable) ModuleTemplate
+{
+public:
+    virtual ~ModuleTemplate();
+};
+
+template <int N>
+class CategoryModuleInfo;
+
+template <>
+class __declspec(novtable) CategoryModuleInfo<8>
+{
+public:
+    CategoryModuleInfo()
+    {
+        *(volatile unsigned int *)this = 0x0107375c;
+        m_a = true;
+        m_b = true;
+    }
+
+    virtual void unused();
+
+private:
+    volatile bool m_a;
+    volatile bool m_b;
+};
+
+template <int N>
+class __declspec(novtable) CategoryModuleTemplateBase
+{
+public:
+    CategoryModuleTemplateBase() {}
+    virtual ~CategoryModuleTemplateBase();
+};
+
+template <>
+class __declspec(novtable) CategoryModuleTemplateBase<8>
+    : public ModuleTemplate,
+      public CategoryModuleInfo<8>
+{
+public:
+    CategoryModuleTemplateBase()
+        : ModuleTemplate(), CategoryModuleInfo<8>() {}
+    virtual ~CategoryModuleTemplateBase();
+};
+
+template <int N>
+class __declspec(novtable) CategoryModuleTemplate
+{
+public:
+    CategoryModuleTemplate();
+    virtual ~CategoryModuleTemplate();
+};
+
+template <>
+class __declspec(novtable) CategoryModuleTemplate<8>
+    : public CategoryModuleTemplateBase<8>
+{
+public:
+    CategoryModuleTemplate()
+        : CategoryModuleTemplateBase<8>()
+    {
+        *(unsigned int *)((unsigned char *)this + 0) = 0x01073848;
+        *(unsigned int *)((unsigned char *)this + 4) = 0x01073844;
+    }
+    virtual ~CategoryModuleTemplate();
+};
+
+class __declspec(novtable) TerrainCollisionModuleInfo
+{
+public:
+    virtual ~TerrainCollisionModuleInfo();
+    TerrainCollisionModuleInfo();
+};
+
 class TerrainCollisionModuleTemplate
+    : public CategoryModuleTemplate<8>,
+      public TerrainCollisionModuleInfo
 {
 public:
     TerrainCollisionModuleTemplate();
 };
 
 // ??0TerrainCollisionModuleTemplate@FXParticleSystem@@QAE@XZ
-__declspec(naked) TerrainCollisionModuleTemplate::TerrainCollisionModuleTemplate()
+TerrainCollisionModuleTemplate::TerrainCollisionModuleTemplate()
+    : CategoryModuleTemplate<8>(),
+      TerrainCollisionModuleInfo()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x58;
-        __emit 0xaf;
-        __emit 0x03;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x5c;
-        __emit 0x37;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0xb0;
-        __emit 0x01;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x08;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x09;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x48;
-        __emit 0x38;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x44;
-        __emit 0x38;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x0c;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xe8;
-        __emit 0x8b;
-        __emit 0x05;
-        __emit 0xa4;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xc7;
-        __emit 0x07;
-        __emit 0x24;
-        __emit 0x13;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x10;
-        __emit 0x13;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x0c;
-        __emit 0x13;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    *(unsigned int *)((unsigned char *)this + 0xc) = 0x01111324;
+    *(unsigned int *)((unsigned char *)this + 0) = 0x01111310;
+    *(unsigned int *)((unsigned char *)this + 4) = 0x0111130c;
 }
 
 }
