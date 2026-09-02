256db0d06cda45b9fc63975f0898f42fa10d9734
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-02 01:01:00 -0400

    Open-BFME5: convert stdcall cdecl adapter

---
+++ Code/GameEngine/Source/Common/Rva008C3E70CallAdapter.cpp
@@ -0,0 +1,8 @@
+// Open-BFME5: clean C++ conversion of the stdcall-to-cdecl adapter.
+
+void rva008AF650Implementation(int first, int second, int third);
+
+void __stdcall rva008C3E70Adapter(int first, int second, int third)
+{
+	rva008AF650Implementation(first, second, third);
+}
