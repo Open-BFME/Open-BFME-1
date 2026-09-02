9b1156dafe43669d73320b346965276b2a7c9282
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-01 23:16:46 -0400

    Open-BFME5: convert 0x0082BDC0 /Od if(0) global member call (27B)
    Unused int forces the 8-byte /Od frame. if (0) still emits xor-eax/jz
    around mov ecx, &g / call Gen_0082ad50::m, whose DIR32/REL32 the patcher
    fills from retail.

---
+++ Code/GameEngine/Source/Common/BfmeGrokOdIf0.cpp
@@ -0,0 +1,21 @@
+// cl: /Od
+
+struct Gen_0082ad50
+{
+	void m();
+};
+
+Gen_0082ad50 g_bfme0130b250;
+
+class BfmeIf0VMJ
+{
+public:
+	void grokIf0();
+};
+
+void BfmeIf0VMJ::grokIf0()
+{
+	int n1;
+	if (0)
+		g_bfme0130b250.m();
+}
