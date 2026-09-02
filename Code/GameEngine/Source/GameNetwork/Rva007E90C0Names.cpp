6d267087ccd43e7201e8095568c7b89422aa7bcc
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-02 01:56:37 -0400

    Open-BFME5: convert 0x007E9130 errorContainer indexed getString/getInt
    Rva007E9130Errors::get sprintf's errorContainer.%d.fieldName then
    fieldError, getString/getInt on the message at +0, and increments.

--- Code/GameEngine/Source/GameNetwork/Rva007E90C0Names.cpp
+++ Code/GameEngine/Source/GameNetwork/Rva007E90C0Names.cpp
@@ -1,11 +1,12 @@
 // cl: /GS
-// 0x007E90C0 / 0x007E91D0 / 0x007E9240: sprintf indexed key then getString
-// on the message at +0. Success increments the count at +4.
+// 0x007E90C0 / 0x007E91D0 / 0x007E9240 / 0x007E9130: sprintf indexed key
+// then getString (and getInt for errors) on the message at +0.
 
 class Rva007E8810Message
 {
 public:
 	bool getString(const char *key, char *dest, int destSize);
+	int getInt(const char *key, int defaultValue);
 };
 
 extern "C" int __cdecl sprintf(char *buf, const char *fmt, ...);
@@ -68,4 +69,29 @@ bool Rva007E9240SubAccounts::get(char *dest, int destSize)
 		return false;
 	m_count++;
 	return true;
+}
+
+class Rva007E9130Errors
+{
+public:
+	bool get(char *fieldError, int *fieldName, int destSize);
+
+private:
+	Rva007E8810Message *m_msg;
+	int m_count;
+};
+
+bool Rva007E9130Errors::get(char *fieldError, int *fieldName, int destSize)
+{
+	char key[0x40];
+	sprintf(key, "errorContainer.%d.fieldName", m_count);
+	if (m_msg->getString(key, fieldError, destSize))
+	{
+		sprintf(key, "errorContainer.%d.fieldError", m_count);
+		*fieldName = m_msg->getInt(key, -1);
+		m_count++;
+		return true;
+	}
+	m_count = 0;
+	return false;
 }
