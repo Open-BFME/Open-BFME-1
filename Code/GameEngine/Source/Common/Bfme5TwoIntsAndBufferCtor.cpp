25b3d533d6c028157683f4c9dabfde604c33b244
dginovker <danielginovker@gmail.com>
2026-09-03 04:50:22 +0900

    Fold Gen_00581ED0 home; two live members precede the buffer

---
+++ Code/GameEngine/Source/Common/Bfme5TwoIntsAndBufferCtor.cpp
@@ -0,0 +1,31 @@
+class HRBMD_Buffer
+{
+public:
+	HRBMD_Buffer(void);
+};
+
+class BfmeFirstMember1134
+{
+public:
+	BfmeFirstMember1134(void) : m_bfmeValue(0) {}
+	~BfmeFirstMember1134(void);
+
+private:
+	int m_bfmeValue;
+};
+
+class Gen_00581ED0
+{
+public:
+	Gen_00581ED0(void);
+
+private:
+	BfmeFirstMember1134 m_bfme00;
+	BfmeFirstMember1134 m_bfme04;
+	HRBMD_Buffer m_bfme08;
+};
+
+// ??0Gen_00581ED0@@QAE@XZ
+Gen_00581ED0::Gen_00581ED0(void)
+{
+}
