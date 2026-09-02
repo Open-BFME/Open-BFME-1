69c83b81485c7d47fdb41b1543c697a524c94510
Jonathan <jonathan@users.noreply.github.com>
2026-09-02 14:09:15 -0400

    Open-BFME5: convert ScriptActions doTeamFlash

--- Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doTeamFlash_Thunk.cpp
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptActions_doTeamFlash_Thunk.cpp
@@ -1,300 +1,230 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/objectdlink
+// Open-BFME5: clean C++ conversion of ScriptActions::doTeamFlash.
+//
+// The team walk uses the BFME Object DLINK pointer-to-member shape from
+// ObjectDlinkPmf.h.  The retail body is the team analogue of doNamedFlash:
+// seconds are scaled by five and divided by fifteen, then each drawable gets
+// the selected colour and flash count.
 
-class AsciiString;
-struct RGBColor;
+typedef bool Bool;
+typedef int Int;
+typedef float Real;
+typedef unsigned int UnsignedInt;
+typedef UnsignedInt Color;
+
+enum
+{
+	LOGICFRAMES_PER_SECOND = 5,
+	DRAWABLE_FRAMES_PER_FLASH = 15
+};
+
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include/Lib/BaseType.h
+struct RGBColor
+{
+	Real red, green, blue;
+
+	inline Int getAsInt(void) const
+	{
+		return
+			((Int)(red * 255.0) << 16) |
+			((Int)(green * 255.0) << 8) |
+			((Int)(blue * 255.0) << 0);
+	}
+};
+
+class AsciiString
+{
+	char *m_data;
+};
+
+class BfmeStringArgBase
+{
+	friend class BfmeAsciiStringArg;
+
+private:
+	BfmeStringArgBase(const BfmeStringArgBase &);
+	~BfmeStringArgBase();
+};
+
+class BfmeAsciiStringArg
+{
+public:
+	BfmeAsciiStringArg(const AsciiString &that)
+	{
+		((BfmeStringArgBase *)this)->BfmeStringArgBase::BfmeStringArgBase(
+			*(const BfmeStringArgBase *)&that);
+	}
+	~BfmeAsciiStringArg();
+
+private:
+	char *m_text;
+};
+
+class Drawable
+{
+public:
+	void setFlashCount(Int count) { m_flashCount = count; }
+	void setFlashColor(Color color) { m_flashColor = color; }
+
+private:
+	unsigned char m_unreconstructed_00[0x160];
+	Int m_flashCount;
+	Color m_flashColor;
+};
+
+class Object;
+
+class BfmeObjectVirtualTail
+{
+public:
+	unsigned char m_vt[4];
+};
+
+class BfmeObjectVtbl
+{
+public:
+	virtual void unused00(void);
+};
+
+class BfmeObjectDlinkBase
+{
+public:
+	Object *dlink_next_TeamMemberList(void) const;
+};
+
+class BfmeObjectDlinkPad
+{
+public:
+	unsigned char m_pad[0x64];
+};
+
+class BfmeObjectVbptrCarrier : public virtual BfmeObjectVirtualTail
+{
+public:
+	unsigned char m_carrier[4];
+};
+
+class Object : public BfmeObjectVtbl, public BfmeObjectDlinkBase,
+	public BfmeObjectDlinkPad, public BfmeObjectVbptrCarrier
+{
+public:
+	virtual void unused01(void);
+	virtual void unused02(void);
+	virtual void unused03(void);
+	virtual void unused04(void);
+	virtual void unused05(void);
+	virtual void unused06(void);
+	virtual void unused07(void);
+	virtual void unused08(void);
+	virtual void unused09(void);
+	virtual Drawable *getDrawable(void) const;
+
+	Color getIndicatorColor(void) const;
+};
+
+#define callMemberFunction(object,ptrToMember) ((object).*(ptrToMember))
+
+template<class OBJCLASS>
+class DLINK_ITERATOR
+{
+public:
+	typedef OBJCLASS * (OBJCLASS::*GetNextFunc)(void) const;
+
+private:
+	OBJCLASS *m_cur;
+	GetNextFunc m_getNextFunc;
+
+public:
+	DLINK_ITERATOR(OBJCLASS *cur, GetNextFunc getNextFunc)
+		: m_cur(cur), m_getNextFunc(getNextFunc)
+	{
+	}
+
+	void advance(void)
+	{
+		if (m_cur)
+			m_cur = callMemberFunction(*m_cur, m_getNextFunc)();
+	}
+
+	Bool done(void) const { return m_cur == 0; }
+	OBJCLASS *cur(void) const { return m_cur; }
+};
+
+class Team
+{
+	void *m_vptr;
+	void *m_proto;
+	void *m_id;
+	Object *m_head;
+
+public:
+	Bool hasAnyObjects(Bool includeDead) const;
+
+	DLINK_ITERATOR<Object> iterate_TeamMemberList(void) const
+	{
+		return DLINK_ITERATOR<Object>(m_head, Object::dlink_next_TeamMemberList);
+	}
+};
+
+class ScriptEngine
+{
+public:
+	virtual void unused00(void);
+	virtual void unused01(void);
+	virtual void unused02(void);
+	virtual void unused03(void);
+	virtual void unused04(void);
+	virtual void unused05(void);
+	virtual void unused06(void);
+	virtual void unused07(void);
+	virtual void unused08(void);
+	virtual void unused09(void);
+	virtual void unused10(void);
+	virtual void unused11(void);
+	virtual void unused12(void);
+	virtual void unused13(void);
+	virtual void unused14(void);
+	virtual void unused15(void);
+	virtual void unused16(void);
+	virtual Team *getTeamNamed(BfmeAsciiStringArg, Bool);
+};
+
+extern ScriptEngine *TheScriptEngine;
+
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/ScriptActions.h
 class ScriptActions
 {
 protected:
-	void doTeamFlash(const AsciiString &, int, const RGBColor *);
+	void doTeamFlash(const AsciiString &, Int, const RGBColor *);
 };
 
 // ?doTeamFlash@ScriptActions@@IAEXABVAsciiString@@HPBURGBColor@@@Z
-__declspec(naked) void ScriptActions::doTeamFlash(const AsciiString &, int, const RGBColor *)
+void ScriptActions::doTeamFlash(const AsciiString &teamName, Int timeInSeconds,
+	const RGBColor *color)
 {
-	__asm {
-        __emit 0x83
-        __emit 0xec
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x53
-        __emit 0x55
-        __emit 0x56
-        __emit 0x57
-        __emit 0x6a
-        __emit 0x00
-        __emit 0x51
-        __emit 0x89
-        __emit 0x64
-        __emit 0x24
-        __emit 0x18
-        __emit 0x8b
-        __emit 0xcc
-        __emit 0x50
-        __emit 0xe8
-        __emit 0xb6
-        __emit 0x3c
-        __emit 0x59
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x6c
-        __emit 0x07
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x11
-        __emit 0xff
-        __emit 0x52
-        __emit 0x44
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x0f
-        __emit 0x84
-        __emit 0xe1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x6a
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0xc3
-        __emit 0x08
-        __emit 0xd2
-        __emit 0xff
-        __emit 0x84
-        __emit 0xc0
-        __emit 0x0f
-        __emit 0x84
-        __emit 0xd0
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x76
-        __emit 0x0c
-        __emit 0x33
-        __emit 0xd2
-        __emit 0x85
-        __emit 0xf6
-        __emit 0xb8
-        __emit 0x40
-        __emit 0x11
-        __emit 0x40
-        __emit 0x00
-        __emit 0xb9
-        __emit 0x9c
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x89
-        __emit 0x44
-        __emit 0x24
-        __emit 0x1c
-        __emit 0x89
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x20
-        __emit 0x89
-        __emit 0x54
-        __emit 0x24
-        __emit 0x24
-        __emit 0x0f
-        __emit 0x84
-        __emit 0xad
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0xde
-        __emit 0x0f
-        __emit 0x84
-        __emit 0xa3
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x68
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x24
-        __emit 0x8b
-        __emit 0x14
-        __emit 0x08
-        __emit 0x03
-        __emit 0x54
-        __emit 0x24
-        __emit 0x20
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x32
-        __emit 0x68
-        __emit 0xff
-        __emit 0x54
-        __emit 0x24
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0x8b
-        __emit 0x03
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xff
-        __emit 0x50
-        __emit 0x28
-        __emit 0x8b
-        __emit 0xe8
-        __emit 0x85
-        __emit 0xed
-        __emit 0x74
-        __emit 0x7e
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x34
-        __emit 0x8d
-        __emit 0x0c
-        __emit 0x89
-        __emit 0xb8
-        __emit 0x89
-        __emit 0x88
-        __emit 0x88
-        __emit 0x88
-        __emit 0xf7
-        __emit 0xe9
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x38
-        __emit 0x03
-        __emit 0xd1
-        __emit 0xc1
-        __emit 0xfa
-        __emit 0x03
-        __emit 0x8b
-        __emit 0xfa
-        __emit 0xc1
-        __emit 0xef
-        __emit 0x1f
-        __emit 0x03
-        __emit 0xfa
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x75
-        __emit 0x0b
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xe8
-        __emit 0x55
-        __emit 0x5d
-        __emit 0xd1
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xd8
-        __emit 0xeb
-        __emit 0x3d
-        __emit 0xd9
-        __emit 0x00
-        __emit 0xdc
-        __emit 0x0d
-        __emit 0x48
-        __emit 0xf9
-        __emit 0x07
-        __emit 0x01
-        __emit 0xe8
-        __emit 0xdc
-        __emit 0x2e
-        __emit 0x70
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x38
-        __emit 0xd9
-        __emit 0x41
-        __emit 0x04
-        __emit 0x8b
-        __emit 0xd8
-        __emit 0xdc
-        __emit 0x0d
-        __emit 0x48
-        __emit 0xf9
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc1
-        __emit 0xe3
-        __emit 0x08
-        __emit 0xe8
-        __emit 0xc5
-        __emit 0x2e
-        __emit 0x70
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x54
-        __emit 0x24
-        __emit 0x38
-        __emit 0xd9
-        __emit 0x42
-        __emit 0x08
-        __emit 0x0b
-        __emit 0xd8
-        __emit 0xdc
-        __emit 0x0d
-        __emit 0x48
-        __emit 0xf9
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc1
-        __emit 0xe3
-        __emit 0x08
-        __emit 0xe8
-        __emit 0xae
-        __emit 0x2e
-        __emit 0x70
-        __emit 0x00
-        __emit 0x0b
-        __emit 0xd8
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x89
-        __emit 0x9d
-        __emit 0x64
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x89
-        __emit 0xbd
-        __emit 0x60
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x0f
-        __emit 0x85
-        __emit 0x53
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5d
-        __emit 0x5b
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x1c
-        __emit 0xc2
-        __emit 0x0c
-        __emit 0x00
+	Team *team = TheScriptEngine->getTeamNamed(teamName, false);
+	if (team == 0 || !team->hasAnyObjects(false))
+		return;
+
+	DLINK_ITERATOR<Object> iter = team->iterate_TeamMemberList();
+
+	while (!iter.done()) {
+		Object *nextObj = iter.cur();
+		Object *obj = nextObj;
+		if (!obj) {
+			break;
+		}
+
+		iter.advance();
+		Drawable *draw = obj->getDrawable();
+		if (!draw) {
+			break;
+		}
+
+		Int frames = LOGICFRAMES_PER_SECOND * timeInSeconds;
+		Int count = frames / DRAWABLE_FRAMES_PER_FLASH;
+		Color flashy = (color == 0) ? obj->getIndicatorColor() : color->getAsInt();
+		draw->setFlashColor(flashy);
+		draw->setFlashCount(count);
 	}
 }
