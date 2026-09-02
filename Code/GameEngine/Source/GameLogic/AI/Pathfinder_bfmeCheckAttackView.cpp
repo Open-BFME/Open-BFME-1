e9af9a2599e7132675ea18b9bdcae0995655ab3a
GL <gleontra@gmail.com>
2026-09-02 11:52:35 -0300

    Open-BFME5: convert 0x003EA940 Pathfinder attack-view alt wrapper
    Replace 22-byte gen_asm dump at 0x003EA940 with clean C++ Pathfinder
    member function. Sibling of 0x003EA960 forwarding (obj, obj+0x38, pos)
    to the ILT entry at 0x0000204A.

--- Code/GameEngine/Source/GameLogic/AI/Pathfinder_bfmeCheckAttackView.cpp
+++ Code/GameEngine/Source/GameLogic/AI/Pathfinder_bfmeCheckAttackView.cpp
@@ -6,9 +6,19 @@ class Object;
 class Pathfinder
 {
 public:
+	int bfmeCheckAttackViewAlt(Object *obj, void *targetPos);
+	int bfmeCheckAttackViewAltHelper(Object *obj, Coord3D *cachedPos, void *targetPos);
+
 	int bfmeCheckAttackView(Object *obj, void *targetPos);
 	int bfmeCheckAttackViewHelper(Object *obj, Coord3D *cachedPos, void *targetPos);
 };
+
+// ?bfmeCheckAttackViewAlt@Pathfinder@@QAEHPAVObject@@PAX@Z
+int Pathfinder::bfmeCheckAttackViewAlt(Object *obj, void *targetPos)
+{
+	Coord3D *cachedPos = (Coord3D *)((char *)obj + 0x38);
+	return bfmeCheckAttackViewAltHelper(obj, cachedPos, targetPos);
+}
 
 // ?bfmeCheckAttackView@Pathfinder@@QAEHPAVObject@@PAX@Z
 int Pathfinder::bfmeCheckAttackView(Object *obj, void *targetPos)
