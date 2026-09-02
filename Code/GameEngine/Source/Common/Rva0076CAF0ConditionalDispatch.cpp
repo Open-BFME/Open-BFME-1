23d76be0e45e1b16c346eafc5559e607d7ff51c0
GL <gleontra@gmail.com>
2026-09-02 11:55:28 -0300

    Open-BFME5: convert 0x0076CAF0 stale-stamp conditional tail dispatcher
    Replace 19-byte gen_asm dump at 0x0076CAF0 with clean C++ member function.
    Resolves banked partial by recognizing that member tail-call target() emits
    jmp 0x00024F5F with this in ecx.

--- Code/GameEngine/Source/Common/Rva0076CAF0ConditionalDispatch.cpp
+++ Code/GameEngine/Source/Common/Rva0076CAF0ConditionalDispatch.cpp
@@ -1,23 +1,22 @@
-// ?d_0076caf0@@YAXXZ
-// partial score=0.95 date=2026-08-31
-// Clean reconstruction of the small stale-stamp dispatcher at 0x0076CAF0.
+// cl: /O2 /DNDEBUG /DWIN32 /D_WINDOWS /MD
 
 extern int g_Va012F8064;
-void j_00024f5a(void);
 
 class Rva0076CAF0ConditionalDispatch
 {
 public:
 	void dispatchIfStale(void);
+	void target(void);
 
 private:
 	unsigned char reserved[0x9c];
 	int stamp;
 };
 
+// ?dispatchIfStale@Rva0076CAF0ConditionalDispatch@@QAEXXZ
 void Rva0076CAF0ConditionalDispatch::dispatchIfStale(void)
 {
 	if (g_Va012F8064 != stamp) {
-		j_00024f5a();
+		target();
 	}
 }
