53591b47e63f548af8db9ad2c86bdae8bbfef6f8
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-02 09:04:46 -0400

    Open-BFME5: convert guarded buffer release

--- Code/GameEngine/Source/Common/Rva003F7380State.cpp
+++ Code/GameEngine/Source/Common/Rva003F7380State.cpp
@@ -1,9 +1,20 @@
 // Open-BFME5: clean C++ conversion of the state reset at 0x003F7380.
 
+extern int TheMixFileInfoPool;
+
+class MixFileInfoBuffer
+{
+public:
+	void releaseInto(void *pool);
+};
+
 struct Rva003F7380Node
 {
 	unsigned char m_padding[0x20];
 	int m_value;
+	unsigned int m_nodeFlags;
+	unsigned char m_tailPadding[8];
+	void *m_link;
 };
 
 struct Rva003F7380Argument
@@ -36,5 +47,16 @@ void Rva003F7380State::resetIfMatching(const Rva003F7380Argument *argument)
 		m_flags &= ~7u;
 		node->m_value = 0;
 		finishReset();
+	}
+}
+
+void Rva003F7380State::finishReset()
+{
+	unsigned int flags = m_flags;
+	if ((flags & 7) != 4 && (flags & 0x38) == 0 &&
+		(flags & 0x80000) == 0 && m_node != 0 &&
+		m_node->m_link == 0 && (m_node->m_nodeFlags & 0x18) == 0) {
+		reinterpret_cast<MixFileInfoBuffer *>(m_node)->releaseInto(&TheMixFileInfoPool);
+		m_node = 0;
 	}
 }
