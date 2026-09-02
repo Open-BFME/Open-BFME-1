4e93d4fb1498e03c222f6f73f4b52aef393e8020
dginovker <danielginovker@gmail.com>
2026-09-03 07:08:59 +0900

    Fold Gen_0028CF50 home; restore owned object release

---
+++ Code/GameEngine/Source/Common/Bfme5OwnedObjectRelease.cpp
@@ -0,0 +1,44 @@
+class Object {};
+
+class GameLogic
+{
+public:
+	Object *findObjectByID(int id);
+	void destroyObject(Object *object);
+};
+
+extern GameLogic *TheGameLogic;
+
+class BfmeDestroyable
+{
+public:
+	virtual void bfmeDestroy(int deleting);
+};
+
+BfmeDestroyable *bfmeFindModule(void);
+
+class Gen_0028CF50
+{
+public:
+	void bfmeRelease(void);
+
+private:
+	char m_bfmeFields[0x24];
+	int m_bfmeObjectID;
+};
+
+// ?bfmeRelease@Gen_0028CF50@@QAEXXZ
+void Gen_0028CF50::bfmeRelease(void)
+{
+	if (m_bfmeObjectID != 0) {
+		Object *object = TheGameLogic->findObjectByID(m_bfmeObjectID);
+		if (object != 0) {
+			BfmeDestroyable *module = bfmeFindModule();
+			if (module != 0) {
+				module->bfmeDestroy(0);
+				TheGameLogic->destroyObject(object);
+			}
+		}
+		m_bfmeObjectID = 0;
+	}
+}
