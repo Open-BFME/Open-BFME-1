a6a68515b3ce3e91eec99ec5f3f2baf98dea002f
Jonathan <jonathan@users.noreply.github.com>
2026-09-02 00:02:12 -0400

    Open-BFME5: convert FireWeaponCollide module-data factory

--- Code/GameEngine/Source/GameLogic/Object/Collide/FireWeaponCollideFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Collide/FireWeaponCollideFriendNewModuleDataThunk.cpp
@@ -1,125 +1,75 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// Open-BFME5: clean C++ FireWeaponCollide module-data factory.
+// stlport
+
+#include <bitset>
 
 class INI;
 class ModuleData;
+class MultiIniFieldParse;
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/FireWeaponCollide.h
+void *__cdecl operator new(unsigned int);
+
+class ObjectStatusMaskType
+{
+public:
+	ObjectStatusMaskType();
+	void clear();
+
+private:
+	std::bitset<75> m_bits;
+};
+
+ObjectStatusMaskType::ObjectStatusMaskType()
+{
+}
+
+void ObjectStatusMaskType::clear()
+{
+	m_bits.reset();
+}
+
+class FireWeaponCollideModuleData
+{
+public:
+	FireWeaponCollideModuleData();
+	virtual ~FireWeaponCollideModuleData();
+
+private:
+	unsigned int m_moduleTagNameKey;
+	const void *m_collideWeaponTemplate;
+	ObjectStatusMaskType m_requiredStatus;
+	ObjectStatusMaskType m_forbiddenStatus;
+	unsigned char m_fireOnce;
+};
+
+FireWeaponCollideModuleData::FireWeaponCollideModuleData()
+{
+	m_collideWeaponTemplate = 0;
+	m_requiredStatus.clear();
+	m_forbiddenStatus.clear();
+	m_fireOnce = 0;
+}
+
+class INI
+{
+public:
+	void initFromINIMultiProc(void *what, void (*parseTable)(MultiIniFieldParse &));
+};
+
+void FireWeaponCollideFieldParse(MultiIniFieldParse &);
+
 class FireWeaponCollide
 {
 public:
-    static ModuleData *friend_newModuleData(INI *);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@FireWeaponCollide@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *FireWeaponCollide::friend_newModuleData(INI *)
+ModuleData *FireWeaponCollide::friend_newModuleData(INI *ini)
 {
-    __asm {
-        // Exact 106-byte retail collide module-data factory body.
-        __emit 0x56;
-        __emit 0x6a;
-        __emit 0x28;
-        __emit 0xe8;
-        __emit 0x28;
-        __emit 0xa2;
-        __emit 0x75;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0x74;
-        __emit 0x42;
-        __emit 0xc7;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0xeb;
-        __emit 0x08;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x48;
-        __emit 0x0c;
-        __emit 0x33;
-        __emit 0xd2;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x89;
-        __emit 0x16;
-        __emit 0x89;
-        __emit 0x56;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x56;
-        __emit 0x08;
-        __emit 0x8d;
-        __emit 0x50;
-        __emit 0x18;
-        __emit 0x33;
-        __emit 0xf6;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0xfa;
-        __emit 0x89;
-        __emit 0x37;
-        __emit 0x89;
-        __emit 0x77;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x77;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x70;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x31;
-        __emit 0x89;
-        __emit 0x71;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x71;
-        __emit 0x08;
-        __emit 0x33;
-        __emit 0xc9;
-        __emit 0x89;
-        __emit 0x0a;
-        __emit 0x89;
-        __emit 0x4a;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x4a;
-        __emit 0x08;
-        __emit 0x88;
-        __emit 0x48;
-        __emit 0x24;
-        __emit 0x8b;
-        __emit 0xf0;
-        __emit 0x5f;
-        __emit 0xeb;
-        __emit 0x02;
-        __emit 0x33;
-        __emit 0xf6;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x85;
-        __emit 0xc9;
-        __emit 0x74;
-        __emit 0x0b;
-        __emit 0x68;
-        __emit 0xc0;
-        __emit 0xab;
-        __emit 0x43;
-        __emit 0x00;
-        __emit 0x56;
-        __emit 0xe8;
-        __emit 0xca;
-        __emit 0xa3;
-        __emit 0x72;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc3;
-    }
+	FireWeaponCollideModuleData *data = new FireWeaponCollideModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, &FireWeaponCollideFieldParse);
+	return (ModuleData *)data;
 }
