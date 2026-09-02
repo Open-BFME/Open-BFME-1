d239c0544cb2afc0ca1a864c166e692bae953d9b
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-02 01:21:42 -0400

    Open-BFME5: convert 0x007E86D0 FESL dotted-quad address parse
    Rva007E8760Addr::parse sscanf's %d.%d.%d.%d into this+8 and stores the
    second argument at +0xC. Sibling of the 0x007E8760 formatter.

--- Code/GameEngine/Source/GameNetwork/Y2FeslAddressFormat.cpp
+++ Code/GameEngine/Source/GameNetwork/Y2FeslAddressFormat.cpp
@@ -1,6 +1,7 @@
 // cl: /O2
-// 0x007E8760: FESL address.cpp dotted-quad format. Buffer shorter than
-// 17 bytes is the "false" assert at line 49.
+// 0x007E8760 / 0x007E86D0: FESL address.cpp dotted-quad format and parse.
+// Format asserts if the destination is shorter than 17 bytes (line 49).
+// Parse packs %d.%d.%d.%d into this+8 and stores the second argument at +0xC.
 
 struct Rva007EB810Diag
 {
@@ -12,15 +13,18 @@ struct Rva007EB810Diag
 
 extern Rva007EB810Diag *Rva007EB810Get();
 extern "C" int __cdecl sprintf(char *buf, const char *fmt, ...);
+extern "C" int __cdecl sscanf(const char *buf, const char *fmt, ...);
 
 class Rva007E8760Addr
 {
 public:
 	void format(char *buf, unsigned size);
+	void parse(const char *str, int extra);
 
 private:
 	char m_pad[8];
 	unsigned m_addr;
+	int m_extra;
 };
 
 void Rva007E8760Addr::format(char *buf, unsigned size)
@@ -36,4 +40,16 @@ void Rva007E8760Addr::format(char *buf, unsigned size)
 		(unsigned char)(v >> 16),
 		(unsigned char)(v >> 8),
 		(unsigned char)v);
+}
+
+void Rva007E8760Addr::parse(const char *str, int extra)
+{
+	int a, b, c, d;
+	sscanf(str, "%d.%d.%d.%d", &a, &b, &c, &d);
+	unsigned v = (unsigned)a;
+	v = (v << 8) + (unsigned)b;
+	v = (v << 8) + (unsigned)c;
+	v = (v << 8) + (unsigned)d;
+	m_addr = v;
+	m_extra = extra;
 }
