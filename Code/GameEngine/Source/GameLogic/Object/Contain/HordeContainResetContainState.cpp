20fa9dc2e4137a1556a959d0a4be597662e702ba
GL <gleontra@gmail.com>
2026-09-02 13:13:48 -0300

    Open-BFME5: convert 0x00249C50 HordeContain reset contain state helper
    Replace 32-byte gen_asm dump at 0x00249C50 with clean C++ HordeContain
    member function. Resolves banked attempt 0x00249c50.cpp by pinning the
    two ILT targets 0x0001D0D4 and 0x00004B51.

--- Code/GameEngine/Source/GameLogic/Object/Contain/HordeContainResetContainState.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Contain/HordeContainResetContainState.cpp
@@ -1,12 +1,10 @@
-// ?resetContainState@Rva00249C50HordeContain@@QAEXXZ
-// partial score=0.96 date=2026-09-02
-// cl: /DNDEBUG /MD
+// cl: /O2 /DNDEBUG /DWIN32 /D_WINDOWS /MD
 
 class Rva00249C50HordeContain
 {
 public:
 	void resetContainState();
-	void resetPair( void *first, void *second );
+	void resetPair(void *first, void *second);
 	void finishReset();
 
 private:
@@ -15,8 +13,9 @@ private:
 	unsigned char m_second[0x0C];
 };
 
+// ?resetContainState@Rva00249C50HordeContain@@QAEXXZ
 void Rva00249C50HordeContain::resetContainState()
 {
-	resetPair( m_first, m_second );
+	resetPair(m_first, m_second);
 	finishReset();
 }
