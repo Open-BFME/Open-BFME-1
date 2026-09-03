4b35a2ebe793ee8e80342b3ea85db35fef912c3b
dginovker <danielginovker@gmail.com>
2026-09-03 15:59:23 +0900

    Open-BFME5: convert CollisionManager construction

---
+++ Code/GameEngine/Source/Common/System/CollisionManagerConstructor.cpp
@@ -0,0 +1,53 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class SubsystemInterface
+{
+public:
+	SubsystemInterface();
+	virtual ~SubsystemInterface();
+	virtual void init() = 0;
+	virtual void reset() = 0;
+	virtual void update() = 0;
+
+private:
+	void *m_name;
+};
+
+class Snapshot
+{
+public:
+	virtual ~Snapshot();
+	virtual void crc(void *xfer) = 0;
+	virtual void xfer(void *xfer) = 0;
+	virtual void loadPostProcess() = 0;
+};
+
+class Rva009A45A0CollisionData
+{
+public:
+	Rva009A45A0CollisionData();
+
+private:
+	unsigned char m_data[0xc070];
+};
+
+class CollisionManager : public SubsystemInterface, public Snapshot
+{
+public:
+	CollisionManager();
+	virtual ~CollisionManager();
+	virtual void init();
+	virtual void reset();
+	virtual void update();
+	virtual void crc(void *xfer);
+	virtual void xfer(void *xfer);
+	virtual void loadPostProcess();
+
+private:
+	Rva009A45A0CollisionData *m_data;
+};
+
+CollisionManager::CollisionManager()
+{
+	m_data = new Rva009A45A0CollisionData;
+}
