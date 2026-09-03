1d31e528c7d25aa74aa94b3a8caa024430f9b0dd
dginovker <danielginovker@gmail.com>
2026-09-03 15:17:05 +0900

    Open-BFME5: convert Player AI type switch

---
+++ Code/GameEngine/Source/Common/PlayerSetPlayerType.cpp
@@ -0,0 +1,74 @@
+// cl: /DNDEBUG /MD /EHsc
+
+enum PlayerType
+{
+	PLAYER_COMPUTER = 1
+};
+
+class BfmeOwnedAI
+{
+public:
+	virtual ~BfmeOwnedAI();
+};
+
+class Player
+{
+public:
+	void setPlayerType(PlayerType type, bool skirmish);
+
+private:
+	unsigned char m_bfmeBeforeType[0x2c];
+	PlayerType m_playerType;
+	unsigned char m_bfmeBeforeAI[0x220 - 0x30];
+	BfmeOwnedAI *m_ai;
+};
+
+class AIPlayer
+{
+public:
+	AIPlayer(Player *player);
+
+private:
+	unsigned char m_bfmeBody[0x78];
+};
+
+class AISkirmishPlayer
+{
+public:
+	AISkirmishPlayer(Player *player);
+
+private:
+	unsigned char m_bfmeBody[0xa0];
+};
+
+struct BfmeAIData
+{
+	unsigned char m_bfmeBeforeForceSkirmishAI[0x65];
+	bool m_forceSkirmishAI;
+};
+
+struct BfmeAI
+{
+	const BfmeAIData *getAiData() { return m_data; }
+
+	unsigned char m_bfmeBeforeData[0x14];
+	BfmeAIData *m_data;
+};
+
+extern "C" BfmeAI *TheAIParseDefinitionAI;
+
+void Player::setPlayerType(PlayerType type, bool skirmish)
+{
+	m_playerType = type;
+
+	delete m_ai;
+	m_ai = 0;
+
+	if (type == PLAYER_COMPUTER)
+	{
+		if (skirmish || TheAIParseDefinitionAI->getAiData()->m_forceSkirmishAI)
+			m_ai = reinterpret_cast<BfmeOwnedAI *>(new AISkirmishPlayer(this));
+		else
+			m_ai = reinterpret_cast<BfmeOwnedAI *>(new AIPlayer(this));
+	}
+}
