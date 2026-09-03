c3a01822aa5b235caa4d17cc862a01e79f8b137d
Jonathan <jonathan@users.noreply.github.com>
2026-09-03 21:21:34 +0000

    Convert 0x008414D0 basic_filebuf<char>::showmanyc from dump to C++.

---
+++ Code/stlport/FilebufShowmanycChar.cpp
@@ -0,0 +1,11 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+// STLport 4.5.3 basic_filebuf<char>::showmanyc (FILE-backed get area).
+
+#include <fstream>
+
+_STLP_BEGIN_NAMESPACE
+
+template streamsize basic_filebuf<char, char_traits<char> >::showmanyc();
+
+_STLP_END_NAMESPACE
