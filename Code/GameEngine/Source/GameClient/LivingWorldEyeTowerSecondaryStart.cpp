afd4658bf913056fb3dd85b49b1d1fab4e2b5500
dginovker <danielginovker@gmail.com>
2026-09-04 18:20:11 +0900

    Convert secondary eye tower particle start

---
+++ Code/GameEngine/Source/GameClient/LivingWorldEyeTowerSecondaryStart.cpp
@@ -0,0 +1,55 @@
+// cl: /DNDEBUG /MD /EHsc /O2 /Ob2
+
+class ParticleSystem;
+ParticleSystem *emptyParticleSystem();
+typedef bool Bool;
+
+class ParticleSystemHandle
+{
+public:
+	~ParticleSystemHandle() throw();
+
+	operator Bool() const
+	{
+		return m_system != 0;
+	}
+
+	ParticleSystem *operator->() const
+	{
+		if( !m_system )
+			return emptyParticleSystem();
+		return m_system;
+	}
+
+	ParticleSystem *m_system;
+	ParticleSystemHandle *m_previous;
+	ParticleSystemHandle *m_next;
+};
+
+class ParticleSystem
+{
+public:
+	void start();
+	ParticleSystemHandle getSlave();
+};
+
+class LivingWorldEyeTowerSecondaryState
+{
+public:
+	void startParticleSystems();
+
+private:
+	unsigned char m_unmodelled00[ 0x14 ];
+	ParticleSystemHandle m_particleSystem;
+};
+
+// ?startParticleSystems@LivingWorldEyeTowerSecondaryState@@QAEXXZ
+void LivingWorldEyeTowerSecondaryState::startParticleSystems()
+{
+	if( m_particleSystem )
+	{
+		m_particleSystem->start();
+		if( m_particleSystem->getSlave() )
+			m_particleSystem->getSlave()->start();
+	}
+}
