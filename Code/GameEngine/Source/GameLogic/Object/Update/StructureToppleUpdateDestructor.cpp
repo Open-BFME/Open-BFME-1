688a99da124e9e6be826ce8c4aa884c9fe1f8271
dginovker <danielginovker@gmail.com>
2026-09-05 06:36:16 +0900

    Recover StructureToppleUpdate destructor identity

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/StructureToppleUpdateDestructor.cpp
@@ -0,0 +1,51 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Module
+{
+public:
+	virtual ~Module();
+
+private:
+	unsigned int m_moduleStorage[2];
+};
+
+class BehaviorModuleInterface { public: virtual void behaviorAnchor(); };
+
+class BehaviorModule : public Module, public BehaviorModuleInterface
+{
+public:
+	virtual ~BehaviorModule() {}
+};
+
+class UpdateModuleInterface { public: virtual void updateAnchor(); };
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+public:
+	virtual ~UpdateModule() {}
+
+private:
+	unsigned int m_updateModuleStorage[3];
+};
+
+class DieModuleInterface { public: virtual void onDie(); };
+
+class StructureToppleUpdate : public UpdateModule, public DieModuleInterface
+{
+public:
+	static void forceDestructorEmission();
+
+protected:
+	virtual ~StructureToppleUpdate();
+};
+
+StructureToppleUpdate::~StructureToppleUpdate()
+{
+}
+
+// ?forceDestructorEmission@StructureToppleUpdate@@SAXXZ absent-from-retail
+void StructureToppleUpdate::forceDestructorEmission()
+{
+	// A local instance makes MSVC emit the protected scalar-deleting destructor.
+	StructureToppleUpdate value;
+}
