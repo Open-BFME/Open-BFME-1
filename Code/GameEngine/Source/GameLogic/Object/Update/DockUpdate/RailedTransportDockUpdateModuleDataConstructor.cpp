2f53985b2ff56f5c7400d765d37d65e756bf474d
dginovker <danielginovker@gmail.com>
2026-09-05 04:54:07 +0900

    Recover RailedTransport dock module data identity

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/RailedTransportDockUpdateModuleDataConstructor.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class DockUpdateModuleData
+{
+public:
+	DockUpdateModuleData();
+	virtual void handle();
+
+private:
+	char m_pad[0x0C];
+};
+
+class RailedTransportDockUpdateModuleData : public DockUpdateModuleData
+{
+	int m_pullInsideDurationInFrames;
+	int m_pushOutsideDurationInFrames;
+
+public:
+	RailedTransportDockUpdateModuleData();
+};
+
+RailedTransportDockUpdateModuleData::RailedTransportDockUpdateModuleData()
+{
+	m_pullInsideDurationInFrames = 0;
+	m_pushOutsideDurationInFrames = 0;
+}
