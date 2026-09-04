6cc8b6f555cd5c59aa70dbe3d32c5bfaa433202a
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-03 18:35:12 -0700

    Open-BFME5: convert BaseHeightMap construction clearing

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/BfmeBaseHeightMapRemoveTreesAndProps.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/BfmeBaseHeightMapRemoveTreesAndProps.cpp
@@ -1,9 +1,5 @@
-// ?removeTreesAndPropsForConstruction@BaseHeightMapRenderObjClass@@QAEXPBUCoord3D@@ABVGeometryInfo@@M@Z
-// partial score=0.99 date=2026-09-02
-// ?removeTreesAndPropsForConstruction@BaseHeightMapRenderObjClass@@QAEXPBUCoord3D@@ABVGeometryInfo@@M@Z
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB
-// stlport
-// Open-BFME5: clean C++ terrain construction-clearing dispatch.
+// Open-BFME5: BaseHeightMapRenderObjClass construction clearing, retail 0x006C88E0, 61 bytes.
 
 struct Coord3D;
 class GeometryInfo;
@@ -20,17 +16,20 @@ public:
 	void removePropsForConstruction(const Coord3D *pos, const GeometryInfo &geom, float angle);
 };
 
-class BaseHeightMapRenderObjClass
+// This view names the older BFME construction-clearing body separately from
+// the 18-byte prop-only method retained in BaseHeightMap.cpp at 0x006C8930.
+class BfmeBaseHeightMapRenderObjClass
 {
 public:
 	void removeTreesAndPropsForConstruction(const Coord3D *pos, const GeometryInfo &geom, float angle);
+
 private:
 	char m_pad[0x3098];
 	W3DTreeBuffer *m_treeBuffer;
 	W3DPropBuffer *m_propBuffer;
 };
 
-void BaseHeightMapRenderObjClass::removeTreesAndPropsForConstruction(
+void BfmeBaseHeightMapRenderObjClass::removeTreesAndPropsForConstruction(
 	const Coord3D *pos, const GeometryInfo &geom, float angle)
 {
 	if (m_treeBuffer)
