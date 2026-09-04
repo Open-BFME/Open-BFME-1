eb61cb56870766fb3299ecaca24d10da525df29e
dginovker <danielginovker@gmail.com>
2026-09-05 04:25:19 +0900

    Correct terrain buffer reset owner

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DTerrainBackgroundResetBuffers.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DTerrainBackgroundResetBuffers.cpp
@@ -4,21 +4,18 @@ void W3DRadarResetUnlock( void );
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include/W3DDevice/GameClient/W3DTerrainBackground.h
 class W3DTerrainBackground
 {
-protected:
-	void freeTerrainBuffers( void );
-};
-
-class Rva00728C20TerrainReset : private W3DTerrainBackground
-{
 public:
 	void resetBuffers( void );
+
+protected:
+	void freeTerrainBuffers( void );
 
 private:
 	unsigned char m_padding[ 0x52 ];
 	unsigned char m_reset;
 };
 
-void Rva00728C20TerrainReset::resetBuffers( void )
+void W3DTerrainBackground::resetBuffers( void )
 {
 	W3DRadarResetLock();
 	freeTerrainBuffers();
