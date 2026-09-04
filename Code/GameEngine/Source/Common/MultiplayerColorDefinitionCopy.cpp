4b12d64ae0171a09d81de6e957a7d75d1b81bf81
dginovker <danielginovker@gmail.com>
2026-09-04 20:46:21 +0900

    Correct MultiplayerColorDefinition copy identity

---
+++ Code/GameEngine/Source/Common/MultiplayerColorDefinitionCopy.cpp
@@ -0,0 +1,42 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/asciistring_downloadmanager /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib
+/*
+** Command & Conquer Generals Zero Hour(tm)
+** Copyright 2025 Electronic Arts Inc.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+*/
+
+#include "Common/AsciiString.h"
+
+struct RGBColor
+{
+	float red;
+	float green;
+	float blue;
+};
+
+class MultiplayerColorDefinition
+{
+public:
+	MultiplayerColorDefinition(const MultiplayerColorDefinition &other);
+
+private:
+	AsciiString m_tooltipName;
+	RGBColor m_rgbValue;
+	int m_color;
+	RGBColor m_rgbValueNight;
+	int m_colorNight;
+};
+
+MultiplayerColorDefinition::MultiplayerColorDefinition(
+	const MultiplayerColorDefinition &other) :
+	m_tooltipName(other.m_tooltipName),
+	m_rgbValue(other.m_rgbValue),
+	m_color(other.m_color),
+	m_rgbValueNight(other.m_rgbValueNight),
+	m_colorNight(other.m_colorNight)
+{
+}
