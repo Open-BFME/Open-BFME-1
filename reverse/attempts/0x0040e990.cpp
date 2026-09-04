f6c38fc074d6d21f4d4949ccc4f08a3356fa7e64
Jonathan <jonathan@users.noreply.github.com>
2026-09-04 10:00:40 +0000

    Bank bfmeKeyboardHasFlaggedKey partial at 0x0040E990 (score 0.78).
    Found-in-bl DCE wall on d_003d5070 mid; stash kept for next draw.

--- reverse/attempts/0x0040e990.cpp
+++ reverse/attempts/0x0040e990.cpp
@@ -1,24 +1,15 @@
 // ?bfmeKeyboardHasFlaggedKey@@YA_N_N@Z
-// partial score=0.75 date=2026-09-04
+// partial score=0.78 date=2026-09-04
 // cl: /DNDEBUG /MD
-//
-// Retail 0x0040E990 (63B): walk the keyboard singleton key table after a
-// virtual update and report whether any down key also carries bit0 of the
-// flag byte when the caller asks for that.
-
 typedef bool Bool;
-
-struct BfmeKeySlot
-{
+struct BfmeKeySlot {
 	unsigned char m_state;
 	unsigned char m_pad1;
 	unsigned char m_flags;
 	unsigned char m_pad3;
 	unsigned int m_pad4;
 };
-
-class BfmeKeyboard
-{
+class BfmeKeyboard {
 public:
 	virtual void slot000(void);
 	virtual void slot004(void);
@@ -26,27 +17,23 @@ public:
 	virtual void slot00C(void);
 	virtual void slot010(void);
 	virtual void updateKeys(void);
-
 	unsigned char m_pad4[8];
 	BfmeKeySlot *m_begin;
 	BfmeKeySlot *m_end;
 };
-
 #define TheKeyboard (*(BfmeKeyboard **)0x012F4C50)
 
-// ?bfmeKeyboardHasFlaggedKey@@YA_N_N@Z
 Bool bfmeKeyboardHasFlaggedKey(Bool want)
 {
 	unsigned char found = 0;
-	BfmeKeyboard *k = TheKeyboard;
-	k->updateKeys();
+	TheKeyboard->updateKeys();
 	BfmeKeyboard *again = TheKeyboard;
 	BfmeKeySlot *end = again->m_end;
 	BfmeKeySlot *it = again->m_begin;
 	if (it != end)
 	{
 		unsigned char w = *(unsigned char *)&want;
-		do
+		for (;;)
 		{
 			if (it->m_state == 1)
 			{
@@ -56,8 +43,10 @@ Bool bfmeKeyboardHasFlaggedKey(Bool want)
 						return true;
 				}
 			}
-			++it;
-		} while (it != end);
+			it = (BfmeKeySlot *)((char *)it + 8);
+			if (it == end)
+				break;
+		}
 	}
-	return found != 0;
+	return (Bool)found;
 }
