7cda9c9646ac0ee2e0d5b61d86676d2b1d352d48
dginovker <danielginovker@gmail.com>
2026-09-05 06:49:57 +0900

    Recover LargeGroupAudioUpdate destructor identity

---
+++ Code/GameEngine/Source/GameLogic/AI/LargeGroupAudioUpdateDestructor.cpp
@@ -0,0 +1,30 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Module { public: virtual ~Module(); private: unsigned int m_storage[2]; };
+class BehaviorModuleInterface { public: virtual void anchor(); };
+class BehaviorModule : public Module, public BehaviorModuleInterface { public: virtual ~BehaviorModule() {} };
+class UpdateModuleInterface { public: virtual void anchor(); };
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+public:
+	virtual ~UpdateModule() {}
+private:
+	unsigned int m_storage[3];
+};
+class LargeGroupAudioUpdateInterface { public: virtual void anchor(); };
+class LargeGroupAudioUpdate : public UpdateModule, public LargeGroupAudioUpdateInterface
+{
+public:
+	static void forceDestructorEmission();
+protected:
+	virtual ~LargeGroupAudioUpdate();
+};
+
+LargeGroupAudioUpdate::~LargeGroupAudioUpdate() {}
+
+// ?forceDestructorEmission@LargeGroupAudioUpdate@@SAXXZ absent-from-retail
+void LargeGroupAudioUpdate::forceDestructorEmission()
+{
+	// A local instance makes MSVC emit the protected scalar-deleting destructor.
+	LargeGroupAudioUpdate value;
+}
