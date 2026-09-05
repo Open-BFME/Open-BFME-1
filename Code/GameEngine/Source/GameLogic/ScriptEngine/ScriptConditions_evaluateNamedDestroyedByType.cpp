2f267c50c2b5d195dcee23c26f83e7206a02f791
Jonathan <jonathan@users.noreply.github.com>
2026-09-05 02:04:46 -0400

    Convert destroyed-by-object-type script condition to exact C++

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptConditions_evaluateNamedDestroyedByType.cpp
@@ -0,0 +1,190 @@
+// cl: /DNDEBUG /DWIN32 /MD /EHsc
+// ScriptConditions::evaluateNamedDestroyedByType, retail 0x00329950.
+// BFME condition template 130 NAMED_DESTROYED_BY_OBJECTTYPE (script_conditions.cpp).
+// evaluateCondition wrapper 0x0032F55E; jump-table DIR32 at 0x0032FD40.
+// ZH has no twin; old attacked-by-type path plus Object+0x344 isEffectivelyDead.
+
+typedef bool Bool;
+typedef int Int;
+
+class AsciiString
+{
+public:
+	char *m_text;
+};
+
+class Parameter
+{
+};
+
+class ThingTemplate
+{
+public:
+	const AsciiString &getName() const
+	{
+		return *(const AsciiString *)((const char *)this + 0x20);
+	}
+};
+
+class Thing
+{
+public:
+	virtual ~Thing();
+	const ThingTemplate *getTemplate() const;
+};
+
+class DamageInfo
+{
+public:
+	unsigned char m_beforeSourceID[8];
+	Int m_sourceID;
+};
+
+class BodyModuleInterface
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual const DamageInfo *getLastDamageInfo() const = 0;
+};
+
+class Object : public Thing
+{
+public:
+	BodyModuleInterface *getBodyModule() const
+	{
+		return *(BodyModuleInterface **)((char *)this + 0x200);
+	}
+
+	Bool isEffectivelyDead() const
+	{
+		return (*(unsigned char *)((char *)this + 0x344) & 1) != 0;
+	}
+};
+
+class ScriptEngine
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot01() = 0;
+	virtual void slot02() = 0;
+	virtual void slot03() = 0;
+	virtual void slot04() = 0;
+	virtual void slot05() = 0;
+	virtual void slot06() = 0;
+	virtual void slot07() = 0;
+	virtual void slot08() = 0;
+	virtual void slot09() = 0;
+	virtual void slot10() = 0;
+	virtual void slot11() = 0;
+	virtual void slot12() = 0;
+	virtual void slot13() = 0;
+	virtual void slot14() = 0;
+	virtual void slot15() = 0;
+	virtual void slot16() = 0;
+	virtual void slot17() = 0;
+	virtual void slot18() = 0;
+	virtual void slot19() = 0;
+	virtual void slot20() = 0;
+	virtual void slot21() = 0;
+	virtual void slot22() = 0;
+	virtual void slot23() = 0;
+	virtual void slot24() = 0;
+	virtual void slot25() = 0;
+	virtual Object *getUnitNamed(const AsciiString &name) = 0;
+};
+
+class GameLogic
+{
+public:
+	Object *findObjectByID(Int id);
+};
+
+// ObjectTypes is 20 bytes (vptr + list name + vector triple). Matched
+// ObjectTypesTemp ctor at 0x002ED6C0 does push 0x14 / operator new /
+// ObjectTypes ctor. virtual dtor declared so delete emits push 1 / call [vtbl].
+class ObjectTypes
+{
+public:
+	ObjectTypes();
+	virtual ~ObjectTypes();
+	Bool isInSet(const AsciiString &objectType) const;
+private:
+	void *m_listName;
+	void *m_objTypesBegin;
+	void *m_objTypesEnd;
+	void *m_objTypesCap;
+};
+
+class ObjectTypesTemp
+{
+public:
+	ObjectTypes *m_types;
+	ObjectTypesTemp();
+	~ObjectTypesTemp()
+	{
+		if (m_types)
+			delete m_types;
+	}
+};
+
+// ??0ObjectTypesTemp@@QAE@XZ  retail 0x002ED6C0
+ObjectTypesTemp::ObjectTypesTemp() : m_types(0)
+{
+	m_types = new ObjectTypes;
+}
+
+extern ScriptEngine *TheScriptEngine;
+extern GameLogic *TheGameLogic;
+
+class ScriptConditions
+{
+protected:
+	Bool evaluateNamedDestroyedByType(Parameter *, Parameter *);
+	static void objectTypesFromParam(Parameter *, ObjectTypes *);
+};
+
+// ?evaluateNamedDestroyedByType@ScriptConditions@@IAE_NPAVParameter@@0@Z
+Bool ScriptConditions::evaluateNamedDestroyedByType(
+	Parameter *pUnitParm, Parameter *pTypeParm)
+{
+	ObjectTypes *objectTypes;
+	Object *theObj = TheScriptEngine->getUnitNamed(
+		*(const AsciiString *)pUnitParm);
+	if (theObj) {
+		BodyModuleInterface *theBodyModule = theObj->getBodyModule();
+		if (theBodyModule) {
+			const DamageInfo *lastDamageInfo = theBodyModule->getLastDamageInfo();
+			if (lastDamageInfo) {
+				if (theObj->isEffectivelyDead()) {
+					GameLogic *logic = TheGameLogic;
+					Int id = lastDamageInfo->m_sourceID;
+					Object *pAttacker = logic->findObjectByID(id);
+					if (pAttacker) {
+						if (pAttacker->getTemplate()) {
+							ObjectTypesTemp types;
+							objectTypes = types.m_types;
+							objectTypesFromParam(pTypeParm, objectTypes);
+							return objectTypes->isInSet(
+								pAttacker->getTemplate()->getName());
+						}
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
