dcc039d259164f9fa414af1a86f4f075644b27be
Gabe Borges <gbborges@proton.me>
2026-09-05 16:20:51 -0400

    feat(common): convert Gen003BBA40 transfer

--- Code/GameEngine/Source/Common/V4XferSlotSequences.cpp
+++ Code/GameEngine/Source/Common/V4XferSlotSequences.cpp
@@ -1,3 +1,4 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
 // Four __thiscall members that take ONE pointer argument and do nothing but call
 // a run of VIRTUAL slots on it, each time handing it the address of a member of
 // this object:
@@ -35,8 +36,8 @@ class GenXferInterface
 {
 public:
 	virtual void slot00( void * field );
-	virtual void slot04( void * field );
-	virtual void slot08( void * field );
+	virtual bool isLoading();
+	virtual bool isSaving();
 	virtual void slot0C( void * field );
 	virtual void slot10( void * field );
 	virtual void slot14( void * field );
@@ -73,11 +74,216 @@ public:
 };
 
 class Gen003BB8F0Member;
-class Gen003BBA40Member
+
+class Rva00064390
+{
+public:
+	Rva00064390();
+
+	int m_00;
+	int m_04;
+	int m_08;
+	int m_0C;
+	float m_10;
+	float m_14;
+	int m_18;
+};
+
+struct Gen_t_000648f0_p24pod
+{
+	int a[ 6 ];
+};
+
+namespace _STL
+{
+	template <class First, class Second>
+	struct pair
+	{
+		pair( void )
+		{
+		}
+
+		pair( const First & firstValue, const Second & secondValue )
+			: first( firstValue ), second( secondValue )
+		{
+		}
+
+		First first;
+		Second second;
+	};
+
+	template <class Pair>
+	struct _Select1st
+	{
+	};
+
+	template <class Type>
+	struct less
+	{
+	};
+
+	template <class Type>
+	class allocator
+	{
+	};
+
+	struct _Rb_tree_node_base
+	{
+		int m_color;
+		_Rb_tree_node_base *m_parent;
+		_Rb_tree_node_base *m_left;
+		_Rb_tree_node_base *m_right;
+	};
+
+	template <class Value>
+	struct _Rb_tree_node : public _Rb_tree_node_base
+	{
+		Value m_value_field;
+	};
+
+	template <class Threads>
+	struct _Rb_global
+	{
+		static _Rb_tree_node_base *_M_increment( _Rb_tree_node_base * node );
+	};
+
+	struct Gen003BBA40Iterator
+	{
+		Gen003BBA40Iterator( _Rb_tree_node_base * node ) : m_node( node )
+		{
+		}
+
+		Rva00064390 &operator *( void ) const
+		{
+			return *(Rva00064390 *)( (char *)m_node + 0x10 );
+		}
+
+		Gen003BBA40Iterator &operator ++( void )
+		{
+			m_node = _Rb_global<bool>::_M_increment( m_node );
+			return *this;
+		}
+
+		bool operator !=( const Gen003BBA40Iterator & that ) const
+		{
+			return m_node != that.m_node;
+		}
+
+		_Rb_tree_node_base *m_node;
+	};
+
+	typedef pair<const int, Gen_t_000648f0_p24pod> Gen003BBA40Value;
+
+	template <class Key, class Value, class KeyOfValue, class Compare, class Alloc>
+	class _Rb_tree
+	{
+	public:
+		typedef Gen003BBA40Iterator iterator;
+		typedef Value value_type;
+		typedef pair<iterator, bool> InsertResult;
+
+		unsigned int size( void ) const
+		{
+			return m_node_count;
+		}
+
+		iterator begin( void )
+		{
+			return iterator( m_header->m_left );
+		}
+
+		iterator end( void )
+		{
+			return iterator( m_header );
+		}
+
+		__forceinline void clear( void )
+		{
+			if ( m_node_count != 0 )
+			{
+				_M_erase( (_Node *)(void *)_M_root() );
+				_M_leftmost() = m_header;
+				_M_root() = 0;
+				_M_rightmost() = m_header;
+				m_node_count = 0;
+			}
+		}
+
+		InsertResult insert_unique( const Value & value );
+
+	private:
+		typedef _Rb_tree_node<Value> _Node;
+
+		_Rb_tree_node_base *&_M_root( void ) const
+		{
+			return m_header->m_parent;
+		}
+
+		_Rb_tree_node_base *&_M_leftmost( void ) const
+		{
+			return m_header->m_left;
+		}
+
+		_Rb_tree_node_base *&_M_rightmost( void ) const
+		{
+			return m_header->m_right;
+		}
+
+		void _M_erase( _Node * node );
+
+		_Rb_tree_node_base *m_header;
+		unsigned int m_node_count;
+		Compare m_key_compare;
+	};
+}
+
+typedef _STL::_Rb_tree<int, _STL::pair<const int, Gen_t_000648f0_p24pod>,
+	_STL::_Select1st<_STL::pair<const int, Gen_t_000648f0_p24pod> >,
+	_STL::less<int>, _STL::allocator<_STL::pair<const int,
+	Gen_t_000648f0_p24pod> > > Gen003BBA40Tree;
+
+class Gen003BBA40Member : public Gen003BBA40Tree
 {
 public:
 	void step( GenXferInterface * xfer );
 };
+
+void Gen003BBA40Member::step( GenXferInterface * xfer )
+{
+	if ( xfer->isSaving() )
+	{
+		int count = size();
+		xfer->slot78( &count );
+
+		for ( iterator it = begin(); it != end(); ++it )
+		{
+			Rva00064390 * value = &*it;
+			xfer->slot60( &value->m_00 );
+			xfer->slot6C( &value->m_0C );
+			xfer->slot6C( &value->m_10 );
+			xfer->slot6C( &value->m_14 );
+			xfer->slot74( &value->m_18 );
+		}
+	}
+	else if ( xfer->isLoading() )
+	{
+		int count;
+		xfer->slot78( &count );
+		if ( size() != 0 )
+			clear();
+
+		Rva00064390 value;
+		for ( int i = 0; i < count; ++i )
+		{
+			xfer->slot60( &value.m_00 );
+			xfer->slot6C( &value.m_0C );
+			xfer->slot6C( &value.m_10 );
+			xfer->slot6C( &value.m_14 );
+			xfer->slot74( &value.m_18 );
+			insert_unique( *(Gen003BBA40Tree::value_type *) &value );
+		}
+	}
+}
 
 void Gen003BB8F0Free( GenXferInterface * xfer, void * field );
 
@@ -151,4 +357,3 @@ void Rva003BB9A0::xfer( GenXferInterface * x )
 	x->slot6C( &m_at18 );
 	x->slot74( &m_at1C );
 }
-
