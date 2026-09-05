893f6a6fd488d5df68194fdb4cd0691d0aa3193d
Gabe Borges <gbborges@proton.me>
2026-09-05 15:27:15 -0400

    fix(gameclient): convert Unicode hot key lookup to C++

--- Code/GameEngine/Source/GameClient/MessageStream/HotKeyManager_searchHotKey_Unicode_Thunk.cpp
+++ Code/GameEngine/Source/GameClient/MessageStream/HotKeyManager_searchHotKey_Unicode_Thunk.cpp
@@ -1,262 +1,91 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /Ireference/shims/asciistring_downloadmanager
 
-class AsciiString {};
-class UnicodeString {};
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameClient/HotKey.h
-class HotKeyManager {
+class UnicodeString;
+
+template <typename T>
+class StringBase
+{
+	friend class AsciiString;
+	friend class UnicodeString;
+
 public:
-	AsciiString searchHotKey(const UnicodeString &);
+	void concat( const T *text, int length );
+
+private:
+	StringBase() : m_data( 0 ) { }
+	StringBase( const StringBase& that );
+	void releaseBuffer();
+
+	void *m_data;
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	static const AsciiString TheEmptyString;
+
+	AsciiString() : StringBase<char>() { }
+	AsciiString( const AsciiString& that ) : StringBase<char>( that ) { }
+	~AsciiString() { ((StringBase<char> *)this)->releaseBuffer(); }
+	void translate( const UnicodeString& that );
+};
+
+class UnicodeString : private StringBase<unsigned short>
+{
+public:
+	static UnicodeString TheEmptyString;
+
+	UnicodeString() : StringBase<unsigned short>() { }
+	UnicodeString( const UnicodeString& that ) : StringBase<unsigned short>( that ) { }
+	~UnicodeString() { ((StringBase<unsigned short> *)this)->releaseBuffer(); }
+
+	bool isEmpty() const
+	{
+		return m_data == 0
+			|| *reinterpret_cast<const unsigned short *>(
+				reinterpret_cast<const char *>(m_data) + 4) == 0;
+	}
+	const unsigned short *str() const
+	{
+		return m_data
+			? reinterpret_cast<const unsigned short *>(
+				reinterpret_cast<const char *>(m_data) + 8)
+			: (const unsigned short *)L"";
+	}
+	void concat( const unsigned short *text, int length )
+	{
+		StringBase<unsigned short>::concat( text, length );
+	}
+	void concat( unsigned short character )
+	{
+		StringBase<unsigned short>::concat( &character, 1 );
+	}
+};
+
+class HotKeyManager
+{
+public:
+	AsciiString searchHotKey( const UnicodeString& );
 };
 
 // ?searchHotKey@HotKeyManager@@QAE?AVAsciiString@@ABVUnicodeString@@@Z
-__declspec(naked) AsciiString HotKeyManager::searchHotKey(const UnicodeString &)
+AsciiString HotKeyManager::searchHotKey( const UnicodeString& uStr )
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x30
-		__emit 0x8f
-		__emit 0x03
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xec
-		__emit 0x08
-		__emit 0x53
-		__emit 0x33
-		__emit 0xdb
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x20
-		__emit 0x8b
-		__emit 0x00
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x56
-		__emit 0x74
-		__emit 0x20
-		__emit 0x66
-		__emit 0x39
-		__emit 0x58
-		__emit 0x04
-		__emit 0x74
-		__emit 0x1a
-		__emit 0x8d
-		__emit 0x70
-		__emit 0x08
-		__emit 0x3b
-		__emit 0xf3
-		__emit 0x74
-		__emit 0x13
-		__emit 0x66
-		__emit 0x8b
-		__emit 0x06
-		__emit 0x66
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x74
-		__emit 0x0b
-		__emit 0x66
-		__emit 0x3d
-		__emit 0x26
-		__emit 0x00
-		__emit 0x74
-		__emit 0x2a
-		__emit 0x83
-		__emit 0xc6
-		__emit 0x02
-		__emit 0x75
-		__emit 0xed
-		__emit 0x8b
-		__emit 0x74
-		__emit 0x24
-		__emit 0x20
-		__emit 0x8b
-		__emit 0xce
-		__emit 0x68
-		__emit 0x50
-		__emit 0x6e
-		__emit 0x33
-		__emit 0x01
-		__emit 0xe8
-		__emit 0x26
-		__emit 0x53
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x5e
-		__emit 0x5b
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x14
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-		__emit 0x68
-		__emit 0x54
-		__emit 0x6e
-		__emit 0x33
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xe8
-		__emit 0xa3
-		__emit 0x5b
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x33
-		__emit 0xc9
-		__emit 0x66
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x02
-		__emit 0x6a
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0xe8
-		__emit 0x85
-		__emit 0x5d
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x24
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x08
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x28
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x01
-		__emit 0xe8
-		__emit 0xbe
-		__emit 0x67
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x74
-		__emit 0x24
-		__emit 0x20
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x24
-		__emit 0x51
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0xbe
-		__emit 0x52
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x24
-		__emit 0x88
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x18
-		__emit 0xe8
-		__emit 0x91
-		__emit 0x50
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x10
-		__emit 0x59
-		__emit 0x2d
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x5e
-		__emit 0x5b
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x14
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
+	if (uStr.isEmpty())
+		return AsciiString::TheEmptyString;
+
+	const unsigned short *marker = uStr.str();
+	while (marker && *marker)
+	{
+		if (*marker == L'&')
+		{
+			UnicodeString tmp = UnicodeString::TheEmptyString;
+			tmp.concat( *(marker + 1) );
+			AsciiString retStr;
+			retStr.translate( tmp );
+			return retStr;
+		}
+		marker++;
 	}
+	return AsciiString::TheEmptyString;
 }
