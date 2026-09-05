f45d01bc9fce379509eb57dce0fc0f6b62926628
Gabe Borges <gbborges@proton.me>
2026-09-05 19:16:18 -0400

    feat(common): convert ShaderClass vector resize

--- Code/GameEngine/Source/Common/S3ReserveHighWaterMarks.cpp
+++ Code/GameEngine/Source/Common/S3ReserveHighWaterMarks.cpp
@@ -1,3 +1,96 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /I Code/Libraries/Source/WWVegas/WWLib /I Code/Libraries/Source/WWVegas/WW3D2 /I Code/Libraries/Source/WWVegas/WWDebug /I Code/Libraries/Source/WWVegas/WWMath
+
+#include <new.h>
+
+extern void *__cdecl operator new[](size_t size);
+extern void __cdecl operator delete[](void *pointer) throw();
+
+class ShaderClass
+{
+public:
+	__forceinline ShaderClass() : m_shaderBits(0x0010441b) {}
+
+	__forceinline ShaderClass &operator=(ShaderClass const &value)
+	{
+		m_shaderBits = value.m_shaderBits;
+		return *this;
+	}
+
+	__forceinline bool operator==(ShaderClass const &value) const
+	{
+		return m_shaderBits == value.m_shaderBits;
+	}
+
+	__forceinline bool operator!=(ShaderClass const &value) const
+	{
+		return m_shaderBits != value.m_shaderBits;
+	}
+
+private:
+	unsigned int m_shaderBits;
+};
+
+template <class T> class VectorClass
+{
+public:
+	virtual ~VectorClass();
+	virtual bool operator==(VectorClass<T> const &) const;
+	virtual bool Resize(int newsize, T const *array = 0);
+	virtual void Clear();
+	virtual int ID(T const *ptr);
+	virtual int ID(T const &ptr);
+
+protected:
+	T *Vector;
+	int VectorMax;
+	bool IsValid;
+	bool IsAllocated;
+	bool VectorClassPad[2];
+};
+
+template <class T>
+bool VectorClass<T>::Resize(int newsize, T const *array)
+{
+	if (newsize)
+	{
+		T *newptr;
+
+		IsValid = false;
+		if (!array)
+			newptr = new T[newsize];
+		else
+			newptr = new ((void *)array) T[newsize];
+		IsValid = true;
+		if (!newptr)
+			return false;
+
+		if (Vector != 0)
+		{
+			int copycount = (newsize < VectorMax) ? newsize : VectorMax;
+			for (int index = 0; index < copycount; ++index)
+				newptr[index] = Vector[index];
+
+			if (IsAllocated)
+			{
+				delete [] Vector;
+				Vector = 0;
+			}
+		}
+
+		Vector = newptr;
+		VectorMax = newsize;
+		IsAllocated = (Vector && !array);
+	}
+	else
+	{
+		Clear();
+	}
+
+	return true;
+}
+
+	template bool VectorClass<ShaderClass>::Resize(int, ShaderClass const *);
+
 // Six 47-byte bodies with one shape:
 //
 //     if (!allocate(a, b)) return false;
@@ -47,17 +140,10 @@ private:
 	int m_bfme0010;							// +0x10
 };
 
-class Gen_00930d00
+class Gen_00930d00 : public VectorClass<ShaderClass>
 {
 public:
 	bool bfmeReserve(int first, int second);
-
-private:
-	bool bfmeAllocate(int first, int second);			// retail 0x009304A0
-
-	char m_bfmeHead[0x08];
-	int m_bfme0008;							// +0x08
-	char m_bfmeMid[0x10 - 0x0C];
 	int m_bfme0010;							// +0x10
 };
 
@@ -134,17 +220,16 @@ bool Gen_009073d0::bfmeReserve(int first, int second)
 // ?bfmeReserve@Gen_00930d00@@QAE_NHH@Z
 bool Gen_00930d00::bfmeReserve(int first, int second)
 {
-	if (bfmeAllocate(first, second))
+	if (VectorClass<ShaderClass>::Resize(first, (ShaderClass const *)second))
 	{
-		if (m_bfme0008 < m_bfme0010)
-			m_bfme0010 = m_bfme0008;
+		if (VectorMax < m_bfme0010)
+			m_bfme0010 = VectorMax;
 
 		return true;
 	}
 
 	return false;
 }
-
 // ?bfmeReserve@Gen_009408d0@@QAE_NHH@Z
 bool Gen_009408d0::bfmeReserve(int first, int second)
 {
