13f3f7f074ccd335e52ab30bd4d3785a42fdac6e
dginovker <danielginovker@gmail.com>
2026-09-05 09:26:18 +0900

    Recover OathbreakerBody destructor identity

--- Code/GameEngine/Source/Common/MultiVptrTailJumpDestructors.cpp
+++ Code/GameEngine/Source/Common/MultiVptrTailJumpDestructors.cpp
@@ -324,16 +324,6 @@ Rva002132B0MultiTailDtor::~Rva002132B0MultiTailDtor()
 {
 }
 
-class Rva00213F80MultiTailDtor : public Rva00017EEAMultiBase, public TailMixinA, public TailMixinB
-{
-public:
-	virtual ~Rva00213F80MultiTailDtor();
-};
-
-Rva00213F80MultiTailDtor::~Rva00213F80MultiTailDtor()
-{
-}
-
 class Rva002141C0MultiTailDtor : public Rva00017EEAMultiBase, public TailMixinA, public TailMixinB
 {
 public:
--- Code/GameEngine/Source/GameLogic/Object/Body/OathbreakerBodyConstructor.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Body/OathbreakerBodyConstructor.cpp
@@ -13,6 +13,13 @@ private:
 	unsigned char m_data[8];
 };
 
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/BehaviorModule.h
+class BehaviorModuleInterface
+{
+public:
+	virtual void behaviorModuleInterfaceAnchor();
+};
+
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/BodyModule.h
 class BodyModuleInterface
 {
@@ -20,28 +27,28 @@ public:
 	virtual void bodyModuleInterfaceAnchor();
 };
 
-class ModuleInterface
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/ActiveBody.h
+class ActiveBody : public BehaviorModule,
+	public BehaviorModuleInterface,
+	public BodyModuleInterface
 {
 public:
-	virtual void moduleInterfaceAnchor();
+	ActiveBody( Thing *thing, const ModuleData *moduleData );
+	virtual ~ActiveBody();
 };
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/BodyModule.h
-class BodyModule : public BehaviorModule,
-	public BodyModuleInterface,
-	public ModuleInterface
-{
-public:
-	BodyModule( Thing *thing, const ModuleData *moduleData );
-};
-
-class OathbreakerBody : public BodyModule
+class OathbreakerBody : public ActiveBody
 {
 public:
 	OathbreakerBody( Thing *thing, const ModuleData *moduleData );
+	virtual ~OathbreakerBody();
 };
 
 OathbreakerBody::OathbreakerBody( Thing *thing, const ModuleData *moduleData )
-	: BodyModule( thing, moduleData )
+	: ActiveBody( thing, moduleData )
+{
+}
+
+OathbreakerBody::~OathbreakerBody()
 {
 }
