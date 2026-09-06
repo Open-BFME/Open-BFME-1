5b8de6864d51c5ec2e6fa326a471041ed1000811
Gabe Borges <gbborges@proton.me>
2026-09-06 05:49:45 -0400

    feat(gui): convert GameWindow destructor to C++

--- Code/GameEngine/Source/Common/GameWindowDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/GameWindowDestructorThunk.cpp
@@ -1,219 +1,99 @@
-// cl: /DNDEBUG /MD /EHsc
-// readable body of ??1GameWindow@@MAE@XZ: Code/GameEngine/Source/GameClient/GUI/GameWindow.cpp
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /EHsc
 
-class __declspec(novtable) GameWindow
+typedef bool Bool;
+
+class GameWindow;
+
+class Glo012F3344Type
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0C() = 0;
+	virtual void slot10() = 0;
+	virtual void slot14() = 0;
+	virtual void slot18() = 0;
+	virtual void slot1C() = 0;
+	virtual void slot20() = 0;
+	virtual GameWindow *slot24() = 0;
+	virtual void slot28() = 0;
+	virtual void slot2C() = 0;
+	virtual void slot30() = 0;
+	virtual void slot34() = 0;
+	virtual void slot38() = 0;
+	virtual GameWindow *slot3C() = 0;
+};
+
+extern Glo012F3344Type *Glo012F3344;
+
+class Open2Dtor4793C0
+{
+public:
+	~Open2Dtor4793C0();
+
+	char m_data[ 0x10 ];
+};
+
+class GameWindowInputData
+{
+};
+
+class Gen_uw_00021431
+{
+public:
+	~Gen_uw_00021431();
+
+	char m_data[ 0x1A8 ];
+};
+
+class GameWindowAnchor
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0C() = 0;
+	virtual void slot10() = 0;
+	virtual void release( int force ) = 0;
+};
+
+extern void W3DRadarResetLock();
+extern void W3DRadarResetUnlock();
+
+class GameWindow
 {
 protected:
 	virtual ~GameWindow();
+
+	GameWindowAnchor *m_anchor;
+	char m_data[ 0x28 ];
+	Gen_uw_00021431 m_instData;
+	GameWindowInputData *m_inputData;
+	char m_data2[ 0x38 ];
+	Open2Dtor4793C0 *m_editData;
 };
 
-// ??1GameWindow@@MAE@XZ
-__declspec(naked) GameWindow::~GameWindow()
+GameWindow::~GameWindow()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xcb
-		__emit 0x64
-		__emit 0x02
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x06
-		__emit 0x78
-		__emit 0x77
-		__emit 0x0f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x44
-		__emit 0x33
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x85
-		__emit 0xc9
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x14
-		__emit 0x8b
-		__emit 0x01
-		__emit 0xff
-		__emit 0x50
-		__emit 0x3c
-		__emit 0x3b
-		__emit 0xc6
-		__emit 0x75
-		__emit 0x0b
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x44
-		__emit 0x33
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x11
-		__emit 0xff
-		__emit 0x52
-		__emit 0x28
-		__emit 0x8b
-		__emit 0x86
-		__emit 0xd8
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x09
-		__emit 0x50
-		__emit 0xe8
-		__emit 0x87
-		__emit 0x81
-		__emit 0x40
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x57
-		__emit 0x8b
-		__emit 0xbe
-		__emit 0x14
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x85
-		__emit 0xff
-		__emit 0xc7
-		__emit 0x86
-		__emit 0xd8
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x10
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xe8
-		__emit 0x0f
-		__emit 0x14
-		__emit 0xbb
-		__emit 0xff
-		__emit 0x57
-		__emit 0xe8
-		__emit 0x62
-		__emit 0x81
-		__emit 0x40
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x04
-		__emit 0x85
-		__emit 0xc0
-		__emit 0xc7
-		__emit 0x86
-		__emit 0x14
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5f
-		__emit 0x74
-		__emit 0x18
-		__emit 0xe8
-		__emit 0x28
-		__emit 0x93
-		__emit 0x48
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x04
-		__emit 0x85
-		__emit 0xc9
-		__emit 0x74
-		__emit 0x07
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x01
-		__emit 0xff
-		__emit 0x50
-		__emit 0x14
-		__emit 0xe8
-		__emit 0x95
-		__emit 0xbd
-		__emit 0x48
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x30
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xa6
-		__emit 0x76
-		__emit 0xba
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
+	if( Glo012F3344 && Glo012F3344->slot3C() == this )
+		Glo012F3344->slot28();
+
+	if( m_inputData )
+		delete m_inputData;
+	m_inputData = 0;
+
+	if( m_editData )
+		delete m_editData;
+	m_editData = 0;
+
+	if( m_anchor )
+	{
+		W3DRadarResetLock();
+
+		if( m_anchor )
+			m_anchor->release( 1 );
+
+		W3DRadarResetUnlock();
 	}
 }
