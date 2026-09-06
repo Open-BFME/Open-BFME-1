73eb554cd5ac834cc52511897edc17ea4d91581e
Gabe Borges <gbborges@proton.me>
2026-09-06 01:57:00 -0400

    feat(w3d): convert asset registry render lookup

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/Render_Obj_Exists.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Render_Obj_Exists.cpp
@@ -16,6 +16,12 @@ extern "C" __declspec(dllimport) void __stdcall EnterCriticalSection(
 extern "C" __declspec(dllimport) void __stdcall LeaveCriticalSection(
 	CRITICAL_SECTION *lock);
 
+enum NameKeyType
+{
+	NAMEKEY_INVALID = 0,
+	FORCE_NAMEKEYTYPE_LONG = 0x7fffffff
+};
+
 class CriticalSectionLock
 {
 public:
@@ -34,10 +40,8 @@ public:
 class NameKeyGenerator
 {
 public:
-	unsigned int nameToLowercaseKey(const char *name);
+	NameKeyType nameToLowercaseKey(const char *name);
 };
-
-typedef _STL::hash_map<unsigned int, void *> AssetRegistryHash;
 
 class AssetRegistry
 {
@@ -47,8 +51,7 @@ public:
 private:
 	unsigned char m_unmodelled_000[0x2c];
 	CRITICAL_SECTION m_lock;
-	AssetRegistryHash m_assets;
-	unsigned char m_unmodelled_058[0x198];
+	unsigned char m_unmodelled_048[0x1a8];
 	NameKeyGenerator *m_hash_context;
 };
 
@@ -61,8 +64,11 @@ bool AssetRegistry::Render_Obj_Exists_Impl(const char *name)
 	unsigned int key = m_hash_context->nameToLowercaseKey(name);
 	if (key == 0)
 		return false;
-	AssetRegistryHash::iterator it = m_assets.find(key);
-	return it != m_assets.end();
+	typedef _STL::hash_map<unsigned int, void *> AssetRegistryHash;
+	AssetRegistryHash *assets =
+		(AssetRegistryHash *)((char *)this + 0x44);
+	AssetRegistryHash::iterator it = assets->find(key);
+	return it != assets->end();
 }
 
 // ?Render_Obj_Exists@@YA_NPBD@Z
