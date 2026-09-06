05461d2d6d0cf0313b01497bfb97ba7250819688
Jonathan <jonathan@users.noreply.github.com>
2026-09-06 15:16:32 -0400

    Convert GameSpy ciErrUniqueNickExpiredHandler

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/chat/Rva0086FC90CiErrUniqueNickExpiredHandler.cpp
@@ -0,0 +1,21 @@
+// cl: /DNDEBUG /MD
+
+typedef void *CHAT;
+
+struct ciConnection
+{
+	int connected;
+	int connecting;
+};
+
+extern "C" void __cdecl ciNickError(CHAT chat, int type, const char *nick,
+	int numSuggestedNicks, char **suggestedNicks);
+struct ciServerMessage;
+
+extern "C" void ciErrUniqueNickExpiredHandler(CHAT chat, const ciServerMessage *message)
+{
+	ciConnection *connection = (ciConnection *)chat;
+
+	if (connection->connecting)
+		ciNickError(chat, 2, "", 0, 0);
+}
