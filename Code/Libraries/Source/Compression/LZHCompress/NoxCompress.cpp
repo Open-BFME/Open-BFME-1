38b0d0e35332c5813c4574b491389c2df5b501d3
Gabe Borges <gbborges@proton.me>
2026-09-06 00:58:56 -0400

    feat(compression): convert LZH file decompressor

--- Code/Libraries/Source/Compression/LZHCompress/NoxCompress.cpp
+++ Code/Libraries/Source/Compression/LZHCompress/NoxCompress.cpp
@@ -22,6 +22,81 @@ LZHL_DHANDLE LZHLCreateDecompressor();
 __declspec(noinline) int LZHLDecompress(LZHL_DHANDLE, UnsignedByte *, UnsignedInt *, const UnsignedByte *, UnsignedInt *);
 void LZHLDestroyDecompressor(LZHL_DHANDLE);
 
+extern "C" __declspec(dllimport) void * __cdecl fopen(const char *, const char *);
+extern "C" __declspec(dllimport) int __cdecl fseek(void *, long, int);
+extern "C" __declspec(dllimport) long __cdecl ftell(void *);
+extern "C" __declspec(dllimport) unsigned int __cdecl fread(void *, unsigned int, unsigned int, void *);
+extern "C" __declspec(dllimport) unsigned int __cdecl fwrite(const void *, unsigned int, unsigned int, void *);
+extern "C" __declspec(dllimport) int __cdecl fclose(void *);
+extern "C" __declspec(dllimport) void * __cdecl malloc(unsigned int);
+extern "C" __declspec(dllimport) void __cdecl free(void *);
+
+Bool DecompressFile(char *infile, char *outfile)
+{
+	UnsignedInt rawSize = 0, compressedSize = 0;
+	void *inFilePtr = 0;
+	void *outFilePtr = 0;
+	char *inBlock = 0;
+	char *outBlock = 0;
+	LZHL_DHANDLE decompress;
+	Int ok = 0;
+	UnsignedInt srcSz, dstSz;
+
+	if ((infile == 0) || (outfile == 0))
+		return false;
+
+	inFilePtr = fopen(infile, (const char *)0x0107FF20);
+	if (inFilePtr)
+	{
+		fseek(inFilePtr, 0, 2);
+		compressedSize = ftell(inFilePtr);
+		fseek(inFilePtr, 0, 0);
+
+		compressedSize -= sizeof(UnsignedInt);
+		fread(&rawSize, 1, sizeof(UnsignedInt), inFilePtr);
+
+		inBlock = (char *)malloc(compressedSize);
+		outBlock = (char *)malloc(rawSize);
+
+		if ((inBlock == 0) || (outBlock == 0))
+			return false;
+
+		fread(inBlock, 1, compressedSize, inFilePtr);
+		fclose(inFilePtr);
+
+		srcSz = compressedSize;
+		dstSz = rawSize;
+		decompress = LZHLCreateDecompressor();
+
+		for (;;)
+		{
+			ok = LZHLDecompress(decompress, (UnsignedByte *)outBlock + rawSize - dstSz, &dstSz,
+				(const UnsignedByte *)inBlock + compressedSize - srcSz, &srcSz);
+
+			if (!ok)
+				break;
+			if (srcSz <= 0)
+				break;
+		}
+
+		LZHLDestroyDecompressor(decompress);
+		outFilePtr = fopen(outfile, (const char *)0x0107FF60);
+		if (outFilePtr)
+		{
+			fwrite(outBlock, rawSize, 1, outFilePtr);
+			fclose(outFilePtr);
+		}
+		else
+			return false;
+
+		free(inBlock);
+		free(outBlock);
+		return true;
+	}
+
+	return false;
+}
+
 LZHL_DHANDLE LZHLCreateDecompressor()
 {
 	return new LZHLDecompressor;
