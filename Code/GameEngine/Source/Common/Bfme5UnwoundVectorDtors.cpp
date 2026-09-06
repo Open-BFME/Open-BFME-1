c4338903ea40efefa303f3766e42e9ccbbd4465b
Gabe Borges <gbborges@proton.me>
2026-09-06 07:12:22 -0400

    feat(common): convert vector owner constructor

--- Code/GameEngine/Source/Common/Bfme5UnwoundVectorDtors.cpp
+++ Code/GameEngine/Source/Common/Bfme5UnwoundVectorDtors.cpp
@@ -20,21 +20,38 @@ inline void bfmeRelease(void *block, unsigned int bytes)
 		bfmeDeallocate(block, bytes);
 }
 
+class BfmeVecMember;
+
+class BfmeVecEnd
+{
+public:
+	BfmeVecEnd(BfmeVecMember *owner)
+	{
+		BfmeVecMember *volatile self = owner;
+		m_bfmeEnd = 0;
+	}
+
+	int *m_bfmeEnd;
+};
+
 class BfmeVecMember
 {
 public:
+	BfmeVecMember(void) : m_bfmeStart(0), m_bfmeFinish(0), m_bfmeEnd(this) {}
+	void bfmeStart(void *first, void *second, void **slot);
+
 	~BfmeVecMember(void)
 	{
 		int *start = m_bfmeStart;
 
 		if (start)
-			bfmeRelease(start, sizeof(int) * (m_bfmeEnd - start));
+			bfmeRelease(start, sizeof(int) * (m_bfmeEnd.m_bfmeEnd - start));
 	}
 
 private:
 	int *m_bfmeStart;					// +0x00
 	int *m_bfmeFinish;					// +0x04
-	int *m_bfmeEnd;						// +0x08
+	BfmeVecEnd m_bfmeEnd;					// +0x08
 };
 
 class BfmeDtorBase_00077560
@@ -55,18 +72,40 @@ private:
 	BfmeVecMember m_bfmeVector;				// +0x08
 };
 
-class BfmeDtorBase_0049A4B0
+class BfmeDtorPair_0049A4B0
 {
 public:
+	BfmeDtorPair_0049A4B0(void) : m_bfmeA(0), m_bfmeB(0) {}
+
+private:
+	int m_bfmeA;						// +0x00
+	int m_bfmeB;						// +0x04
+};
+
+class BfmeDtorSlot_0049A4B0
+{
+public:
+	BfmeDtorSlot_0049A4B0(void) : m_bfmeC(0) {}
+	~BfmeDtorSlot_0049A4B0(void);
+
+private:
+	int m_bfmeC;
+};
+
+class BfmeDtorBase_0049A4B0 : public BfmeDtorPair_0049A4B0
+{
+public:
+	BfmeDtorBase_0049A4B0(void) : BfmeDtorPair_0049A4B0(), m_bfmeSlot() {}
 	~BfmeDtorBase_0049A4B0(void);			// retail 0x00026AB2
 
 private:
-	int m_bfmeFields[3];					// +0x00
+	BfmeDtorSlot_0049A4B0 m_bfmeSlot;				// +0x08
 };
 
 class Gen_0049A4B0 : public BfmeDtorBase_0049A4B0
 {
 public:
+	Gen_0049A4B0(void *first);
 	~Gen_0049A4B0(void);
 
 private:
@@ -94,6 +133,13 @@ private:
 // ??1Gen_00077560@@QAE@XZ
 Gen_00077560::~Gen_00077560(void)
 {
+}
+
+// ??0Gen_0049A4B0@@QAE@PAX@Z
+Gen_0049A4B0::Gen_0049A4B0(void *first)
+	: BfmeDtorBase_0049A4B0(), m_bfmeVector()
+{
+	m_bfmeVector.bfmeStart(first, 0, &first);
 }
 
 // ??1Gen_0049A4B0@@QAE@XZ
