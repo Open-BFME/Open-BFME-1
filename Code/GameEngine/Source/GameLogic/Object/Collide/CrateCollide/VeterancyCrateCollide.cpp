4610b0bd35a5b958c74ba07d6b73d0111b8fb5ac
Gabe Borges <gbborges@proton.me>
2026-09-06 16:41:35 -0400

    feat(veterancy): convert validation body
    The VeterancyCrateCollide vtable slot and the retail boundary at RVA 0x00218D90 identify this body as ?isValidToExecute@VeterancyCrateCollide@@MBE_NPBVObject@@@Z.
    The clean C++ body lives in Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollide.cpp.
    Six adapter pins in reverse/symbols.csv preserve the retail thunk call targets.

--- Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollide.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollide.cpp
@@ -4,6 +4,8 @@
 #define __PLACEMENT_VEC_NEW_INLINE
 
 #include "PreRTS.h"
+#include "GameLogic/ExperienceTracker.h"
+#include "GameLogic/Object.h"
 #include "GameLogic/Module/VeterancyCrateCollide.h"
 
 VeterancyCrateCollide::VeterancyCrateCollide(Thing *thing, const ModuleData *moduleData)
@@ -13,4 +15,103 @@ VeterancyCrateCollide::VeterancyCrateCollide(Thing *thing, const ModuleData *mod
 
 VeterancyCrateCollide::~VeterancyCrateCollide()
 {
+}
+
+class BfmeThing932D
+{
+	public:
+	char go();
+};
+
+class BfmeExperienceTrackerCall
+{
+	public:
+	Bool isTrainable() const;
+};
+
+class BfmeObjectCall
+{
+	public:
+	Bool isSignificantlyAboveTerrain() const;
+	Player *getControllingPlayer() const;
+	Bool isUsingAirborneLocomotor() const;
+};
+
+class BfmeVeterancyCrateData
+{
+	public:
+	unsigned char m_pad00[0x58];
+	unsigned char m_addsOwnerVeterancy;
+	unsigned char m_isPilot;
+	unsigned char m_pad5a[2];
+	Int m_affectsUpToLevel;
+};
+
+class BfmeCrateCollideCall
+{
+	public:
+	Bool isValidToExecute(const Object *other) const;
+};
+
+#pragma comment(linker, "/alternatename:?go@BfmeThing932D@@QAEDXZ=?j_0001fde3@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isTrainable@BfmeExperienceTrackerCall@@QBE_NXZ=?j_0002a239@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isSignificantlyAboveTerrain@BfmeObjectCall@@QBE_NXZ=?j_00019ff1@@YAXXZ")
+#pragma comment(linker, "/alternatename:?getControllingPlayer@BfmeObjectCall@@QBEPAVPlayer@@XZ=?j_00020824@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isUsingAirborneLocomotor@BfmeObjectCall@@QBE_NXZ=?j_0000a001@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isValidToExecute@BfmeCrateCollideCall@@QBE_NPBVObject@@@Z=?j_00025ef5@@YAXXZ")
+inline const BfmeVeterancyCrateData *getBfmeVeterancyCrateData(const VeterancyCrateCollide *module)
+{
+	return *reinterpret_cast<const BfmeVeterancyCrateData *const *>(
+		reinterpret_cast<const char *>(module) + 0x04);
+}
+
+inline const Object *getBfmeVeterancyCrateObject(const VeterancyCrateCollide *module)
+{
+	return *reinterpret_cast<Object *const *>(reinterpret_cast<const char *>(module) + 0x08);
+}
+
+Bool VeterancyCrateCollide::isValidToExecute(const Object *other) const
+{
+	const BfmeVeterancyCrateData *data = getBfmeVeterancyCrateData(this);
+	if (!data)
+		return false;
+
+	if (!reinterpret_cast<const BfmeCrateCollideCall *>(this)->isValidToExecute(other))
+		return false;
+
+	if ((*reinterpret_cast<const unsigned char *>(
+		reinterpret_cast<const char *>(other) + 0x344) & 1) != 0)
+		return false;
+
+	if (reinterpret_cast<const BfmeObjectCall *>(other)->isSignificantlyAboveTerrain())
+		return false;
+
+	if (getBfmeVeterancyCrateData(this) && getBfmeVeterancyCrateData(this)->m_addsOwnerVeterancy != 0)
+		return false;
+
+	ExperienceTracker *tracker = *reinterpret_cast<ExperienceTracker *const *>(
+		reinterpret_cast<const char *>(other) + 0x210);
+	if (!tracker || !reinterpret_cast<const BfmeExperienceTrackerCall *>(tracker)->isTrainable())
+		goto invalid;
+
+	if (!reinterpret_cast<BfmeThing932D *>(tracker)->go())
+		goto invalid;
+
+	if (*reinterpret_cast<const Int *>(reinterpret_cast<const char *>(tracker) + 0x28) > data->m_affectsUpToLevel)
+		goto invalid;
+
+	if (data->m_isPilot != 0)
+	{
+		const Object *object = getBfmeVeterancyCrateObject(this);
+		if (reinterpret_cast<const BfmeObjectCall *>(other)->getControllingPlayer()
+			!= reinterpret_cast<const BfmeObjectCall *>(object)->getControllingPlayer())
+			goto invalid;
+		if (reinterpret_cast<const BfmeObjectCall *>(other)->isUsingAirborneLocomotor())
+			goto invalid;
+	}
+
+	return true;
+
+invalid:
+	return false;
 }
