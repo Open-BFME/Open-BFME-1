999f11860e98dad123cb15896b1707b5eff6d1a8
Gabe Borges <gbborges@proton.me>
2026-09-06 12:14:13 -0400

    feat(gui): convert establish connections show

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/EstablishConnectionsWindow.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/EstablishConnectionsWindow.cpp
@@ -35,6 +35,130 @@ public:
 extern EstablishConnectionsMenu *TheEstablishConnectionsMenu;	///< retail 0x012F363C
 extern int buttonQuitID;										///< retail 0x012F39C0
 
+class BFMERetailAsciiString;
+
+template <typename T> class StringBase
+{
+	friend class BFMERetailAsciiString;
+
+	private:
+	StringBase() : m_data( 0 ) {}
+	StringBase( const T *text );
+	StringBase( const StringBase<T> &other );
+	~StringBase();
+
+private:
+	void *m_data;
+};
+
+class BFMERetailAsciiString : private StringBase<char>
+{
+public:
+	BFMERetailAsciiString() : StringBase<char>() {}
+	BFMERetailAsciiString( const char *text ) : StringBase<char>( text ) {}
+	BFMERetailAsciiString( const BFMERetailAsciiString &other )
+		: StringBase<char>( other ) {}
+	~BFMERetailAsciiString() {}
+};
+
+class BfmeEstablishWindowLayout
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0c() = 0;
+	virtual void hide( int hidden ) = 0;
+
+	void *m_filename;
+	GameWindow *m_windowList;
+
+	GameWindow *getFirstWindow( void ) { return m_windowList; }
+};
+
+class BfmeEstablishWindowManager
+{
+public:
+	virtual void slot00(); virtual void slot01(); virtual void slot02();
+	virtual void slot03(); virtual void slot04(); virtual void slot05();
+	virtual void slot06(); virtual void slot07(); virtual void slot08();
+	virtual void slot09(); virtual void slot10(); virtual void slot11();
+	virtual void slot12(); virtual void slot13(); virtual void slot14();
+	virtual void slot15(); virtual void slot16(); virtual void slot17();
+	virtual void slot18(); virtual void slot19(); virtual void slot20();
+	virtual void slot21(); virtual void slot22(); virtual void slot23();
+	virtual void slot24(); virtual void slot25(); virtual void slot26();
+	virtual BfmeEstablishWindowLayout *winCreateLayout( BFMERetailAsciiString file );
+	virtual void slot28(); virtual void slot29(); virtual void slot30();
+	virtual void slot31(); virtual void slot32(); virtual void slot33();
+	virtual void slot34(); virtual void slot35(); virtual void slot36();
+	virtual void slot37(); virtual void slot38(); virtual void slot39();
+	virtual void slot40(); virtual void slot41(); virtual void slot42();
+	virtual void slot43();
+	virtual int winSetFocus( GameWindow *window );
+};
+
+class BfmeEstablishGameSpyGame
+{
+public:
+	char m_padding[0x43c];
+	unsigned char m_isQuickMatch;
+};
+
+extern BfmeEstablishWindowManager *TheWindowManager;
+extern BfmeEstablishGameSpyGame *TheGameSpyGame;
+extern void ShowUnderlyingGUIElements( bool show, const char *layoutFilename,
+	const char *parentName, const char **gadgetsToHide,
+	const char **perPlayerGadgetsToHide );
+extern void d_004c85b0( void );
+
+static BfmeEstablishWindowLayout *establishConnectionsLayout;
+static const char *layoutFilename = "GameSpyGameOptionsMenu.wnd";
+static const char *parentName = "GameSpyGameOptionsMenuParent";
+static const char *gadgetsToHide[] = {
+	"MapWindow", "StaticTextGameName", "StaticTextTeam",
+	"StaticTextFaction", "StaticTextColor", "StaticTextPlayers",
+	"TextEntryMapDisplay", "ButtonSelectMap", "ButtonStart",
+	"StaticTextMapPreview", 0
+};
+static const char *perPlayerGadgetsToHide[] = {
+	"ComboBoxTeam", "ComboBoxColor", "ComboBoxPlayerTemplate",
+	"ComboBoxPlayer", "ButtonAccept", "GenericPing", 0
+};
+static const char *qmlayoutFilename = "WOLQuickMatchMenu.wnd";
+static const char *qmparentName = "WOLQuickMatchMenuParent";
+static const char *qmgadgetsToHide[] = {
+	"StaticTextTitle", "ButtonBack", "ButtonOptions", "ButtonBuddies",
+	"ButtonWiden", "ButtonStop", "ButtonStart", 0
+};
+static const char *qmperPlayerGadgetsToHide[] = { 0 };
+
+// ?ShowEstablishConnectionsWindow@@YAXXZ 0x004C8660
+void ShowEstablishConnectionsWindow( void )
+{
+	if( establishConnectionsLayout == 0 )
+	{
+		establishConnectionsLayout = TheWindowManager->winCreateLayout(
+			"Menus/EstablishConnectionsScreen.wnd" );
+		d_004c85b0();
+	}
+	establishConnectionsLayout->hide( 0 );
+	TheWindowManager->winSetFocus( establishConnectionsLayout->getFirstWindow() );
+	GameWindow *firstWindow = establishConnectionsLayout->getFirstWindow();
+	if( firstWindow != 0 )
+		*reinterpret_cast<int *>( reinterpret_cast<char *>( firstWindow ) + 0x1f4 ) = 0;
+	if( !TheGameSpyGame->m_isQuickMatch )
+	{
+		ShowUnderlyingGUIElements( 0, layoutFilename, parentName,
+			gadgetsToHide, perPlayerGadgetsToHide );
+	}
+	else
+	{
+		ShowUnderlyingGUIElements( 0, qmlayoutFilename, qmparentName,
+			qmgadgetsToHide, qmperPlayerGadgetsToHide );
+	}
+}
+
 enum WindowMsgHandledType
 {
 	MSG_IGNORED = 0,
