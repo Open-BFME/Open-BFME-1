bd1b7c8fb384ac7eeb288c1221ed21bc6e37b7c1
Gabe Borges <gbborges@proton.me>
2026-09-06 14:01:50 -0400

    feat(bitflags): convert 181-bit testSetAndClear

---
+++ Code/GameEngine/Source/Common/BitFlags181TestSetAndClear.cpp
@@ -0,0 +1,39 @@
+// cl: /DNDEBUG /DWIN32 /MD /EHs-c- /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <bitset>
+
+typedef bool Bool;
+
+template <size_t NUMBITS>
+class BitFlags
+{
+public:
+	Bool testSetAndClear(const BitFlags &mustBeSet,
+		const BitFlags &mustBeClear) const;
+
+private:
+	_STL::bitset<NUMBITS> m_bits;
+};
+
+template <size_t NUMBITS>
+Bool BitFlags<NUMBITS>::testSetAndClear(const BitFlags &mustBeSet,
+	const BitFlags &mustBeClear) const
+{
+	BitFlags tmp = *this;
+	tmp.m_bits &= mustBeClear.m_bits;
+	if (tmp.m_bits.any())
+		return false;
+
+	tmp = *this;
+	tmp.m_bits.flip();
+	tmp.m_bits &= mustBeSet.m_bits;
+
+	if (tmp.m_bits.any())
+		return false;
+
+	return true;
+}
+
+template Bool BitFlags<181>::testSetAndClear(const BitFlags<181> &,
+	const BitFlags<181> &) const;
