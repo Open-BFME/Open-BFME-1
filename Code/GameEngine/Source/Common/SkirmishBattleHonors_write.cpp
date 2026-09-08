0ed1e669d96b3713a5f521c4f6a98e314a23fb82
Gabe Borges <gbborges@proton.me>
2026-09-08 18:39:26 -0400

    feat(common): convert SkirmishBattleHonors write
    The vtable at 0x01080460 slot +0x0c and retail writes to Color, PlayerTemplate, and Map identify SkirmishBattleHonors::write.
    The clean body now lives in Code/GameEngine/Source/Common/SkirmishBattleHonors_write.cpp and replaces the generated row at 0x0009EA10.

--- Code/GameEngine/Source/Common/SkirmishBattleHonors_write.cpp
+++ Code/GameEngine/Source/Common/SkirmishBattleHonors_write.cpp
@@ -1,9 +1,7 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/ini /Ireference/shims/iniexception /Ireference/shims/ini_noinline /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
-// partial score=0.9 date=2026-09-08
 // stlport
 
 #include "PreRTS.h"
-#include "Common/QuotedPrintable.h"
 #include "Common/UserPreferences.h"
 
 class SkirmishBattleHonors : public UserPreferences
@@ -24,8 +22,6 @@ public:
 	{
 		return *( (const Int *)((const char *)this + 0x14) );
 	}
-
-	UnicodeString getName( void ) const;
 };
 
 class GameInfo
@@ -50,9 +46,7 @@ Bool SkirmishBattleHonors::write( void )
 		( *this )["PlayerTemplate"] = tmp;
 
 		( *this )["Map"] = TheSkirmishGameInfo->getMap();
-		( *this )["UserName"] = UnicodeStringToQuotedPrintable(
-			TheSkirmishGameInfo->getConstSlot( 0 )->getName() );
-
-		return UserPreferences::write();
 	}
+
+	return UserPreferences::write();
 }
