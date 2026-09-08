cecf350a00069a0ee98257c394262e858f335d05
Rudi Theunissen <rudi@goaugment.com>
2026-09-08 16:06:19 -0700

    chore(reverse): account for d_001c7e60 candidates

---
+++ reverse/attempts/0x001cd540.cpp
@@ -0,0 +1,158 @@
+// ?unidentified_0002181E@Object@@QAEXABVModelConditionFlags@@H@Z
+// partial score=0.68 date=2026-09-08
+// cl: /DNDEBUG /MD /EHsc
+// stlport
+// Open-BFME: Object status propagation helper, retail 0x001CD540.
+
+#define _STLP_USE_NEWALLOC 1
+#define _STLP_NO_EXCEPTIONS 1
+
+#include <list>
+
+typedef bool Bool;
+typedef int Int;
+
+template <int NUMBITS>
+class BitFlags
+{
+public:
+	unsigned int m_bits[(NUMBITS + 31) / 32];
+};
+
+typedef BitFlags<86> ObjectStatusMaskType;
+
+class ModelConditionFlags
+{
+public:
+	unsigned int m_bits[3];
+};
+
+class Overridable
+{
+public:
+	virtual ~Overridable();
+	const Overridable *getFinalOverride() const;
+
+	Overridable *m_nextOverride;
+};
+
+class ThingTemplate : public Overridable
+{
+public:
+	unsigned char m_pad08[0xd4 - 8];
+	unsigned int m_flags;
+};
+
+enum KindOfType
+{
+	KINDOF_UNKNOWN = 0
+};
+
+class Thing
+{
+public:
+	Bool isKindOf(KindOfType kind) const;
+};
+
+class Object;
+typedef _STL::list<Object *> ObjectList;
+
+class HordeContainInterface
+{
+public:
+	virtual void slot00() = 0; virtual void slot01() = 0; virtual void slot02() = 0; virtual void slot03() = 0;
+	virtual void slot04() = 0; virtual void slot05() = 0; virtual void slot06() = 0; virtual void slot07() = 0;
+	virtual void slot08() = 0; virtual void slot09() = 0; virtual void slot10() = 0; virtual void slot11() = 0;
+	virtual void slot12() = 0; virtual void slot13() = 0; virtual void slot14() = 0; virtual void slot15() = 0;
+	virtual void slot16() = 0; virtual void slot17() = 0; virtual void slot18() = 0; virtual void slot19() = 0;
+	virtual void slot20() = 0; virtual void slot21() = 0; virtual void slot22() = 0; virtual void slot23() = 0;
+	virtual void slot24() = 0; virtual void slot25() = 0; virtual void slot26() = 0; virtual void slot27() = 0;
+	virtual void slot28() = 0; virtual void slot29() = 0; virtual void slot30() = 0; virtual void slot31() = 0;
+	virtual void slot32() = 0; virtual void slot33() = 0; virtual void slot34() = 0; virtual void slot35() = 0;
+	virtual void slot36() = 0; virtual void slot37() = 0; virtual void slot38() = 0; virtual void slot39() = 0;
+	virtual void slot40() = 0; virtual void slot41() = 0; virtual void slot42() = 0; virtual void slot43() = 0;
+	virtual void slot44() = 0; virtual void slot45() = 0; virtual void slot46() = 0; virtual void slot47() = 0;
+	virtual void slot48() = 0; virtual void slot49() = 0; virtual void slot50() = 0; virtual void slot51() = 0;
+	virtual void slot52() = 0; virtual void slot53() = 0; virtual void slot54() = 0; virtual void slot55() = 0;
+	virtual void slot56() = 0; virtual void slot57() = 0; virtual void slot58() = 0;
+	virtual const ObjectList *getContainedItemsList() const = 0;
+};
+
+class ContainModuleInterface
+{
+public:
+	virtual void slot00() = 0; virtual void slot01() = 0; virtual void slot02() = 0; virtual void slot03() = 0;
+	virtual void slot04() = 0; virtual void slot05() = 0; virtual void slot06() = 0; virtual void slot07() = 0;
+	virtual void slot08() = 0; virtual void slot09() = 0; virtual void slot10() = 0; virtual void slot11() = 0;
+	virtual void slot12() = 0; virtual void slot13() = 0; virtual void slot14() = 0; virtual void slot15() = 0;
+	virtual void slot16() = 0; virtual void slot17() = 0; virtual void slot18() = 0; virtual void slot19() = 0;
+	virtual void slot20() = 0; virtual void slot21() = 0; virtual void slot22() = 0; virtual void slot23() = 0;
+	virtual void slot24() = 0; virtual void slot25() = 0;
+	virtual HordeContainInterface *getHordeContainInterface() = 0;
+};
+
+class Object
+{
+public:
+	void setStatus(const ObjectStatusMaskType &flags, Bool set);
+	void unidentified_0002181E(const ModelConditionFlags &flags, Int set);
+};
+
+class ObjectLayout
+{
+public:
+	void *m_vtable;
+	ThingTemplate *m_template;
+	unsigned char m_pad08[0x1fc - 8];
+	ContainModuleInterface *m_contain;
+	unsigned char m_pad200[0x214 - 0x200];
+	Object *m_containedBy;
+	unsigned char m_pad218[0x369 - 0x218];
+	unsigned char m_flag369;
+};
+
+// ?unidentified_0002181E@Object@@QAEXABVModelConditionFlags@@H@Z
+void Object::unidentified_0002181E(const ModelConditionFlags &flags, Int set)
+{
+	ObjectLayout *self = reinterpret_cast<ObjectLayout *>(this);
+
+	if (self->m_flag369 == 0)
+	{
+		ThingTemplate *thingTemplate = self->m_template;
+		if (thingTemplate != 0 && thingTemplate->m_nextOverride != 0)
+		{
+			thingTemplate = const_cast<ThingTemplate *>(reinterpret_cast<const ThingTemplate *>(
+				thingTemplate->m_nextOverride->getFinalOverride()));
+		}
+
+		Object *target = this;
+		if ((thingTemplate->m_flags & 0x00100000) == 0)
+		{
+			target = self->m_containedBy;
+			if (target == 0 || !reinterpret_cast<const Thing *>(target)->isKindOf((KindOfType)0x6c))
+				return;
+		}
+
+		ObjectLayout *targetLayout = reinterpret_cast<ObjectLayout *>(target);
+		ContainModuleInterface *contain = targetLayout->m_contain;
+		if (contain == 0)
+			return;
+
+		HordeContainInterface *horde = contain->getHordeContainInterface();
+		if (horde == 0)
+			return;
+
+		ObjectList objects = *horde->getContainedItemsList();
+		for (ObjectList::iterator it = objects.begin(); it != objects.end(); ++it)
+		{
+			(*it)->setStatus(*reinterpret_cast<const ObjectStatusMaskType *>(&flags), set != 0);
+		}
+
+		target->setStatus(*reinterpret_cast<const ObjectStatusMaskType *>(&flags), set != 0);
+	}
+
+	setStatus(*reinterpret_cast<const ObjectStatusMaskType *>(&flags), set != 0);
+}
+
+#pragma comment(linker, "/alternatename:?getFinalOverride@Overridable@@QBEPBV1@XZ=?j_000022bb@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isKindOf@Thing@@QBE_NW4KindOfType@@@Z=?j_0003251f@@YAXXZ")
