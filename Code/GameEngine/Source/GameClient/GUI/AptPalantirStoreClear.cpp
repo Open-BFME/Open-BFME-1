1a7a9743ebd4967ec95c31fb857560041abe8371
Gabe Borges <gbborges@proton.me>
2026-09-09 00:43:31 -0400

    feat(game-client): recover AptPalantirStore clear
    AptPalantir::hide calls the ILT at 0x0002F801 which reaches retail 0x005918F0 and proves the store identity. The body lives in Code/GameEngine/Source/GameClient/GUI/AptPalantirStoreClear.cpp and models the two STLport trees plus the RegionUI portrait cleanup shown by the retail calls.

--- Code/GameEngine/Source/GameClient/GUI/AptPalantirStoreClear.cpp
+++ Code/GameEngine/Source/GameClient/GUI/AptPalantirStoreClear.cpp
@@ -42,9 +42,119 @@ static void clearValue( int value )
 	}
 }
 
-static void forceClearValue( int value )
+namespace _STL
 {
-	clearValue( value );
+
+template <class Type> class allocator {};
+template <class Type> struct _Identity {};
+template <class Type> struct less {};
+
+void nodeDeallocate( void *block, unsigned int bytes );
+
+struct _Rb_tree_node_base
+{
+	int m_color;
+	_Rb_tree_node_base *m_parent;
+	_Rb_tree_node_base *m_left;
+	_Rb_tree_node_base *m_right;
+};
+
+template <class Value>
+struct _Rb_tree_node : public _Rb_tree_node_base
+{
+	Value m_value_field;
+};
+
+template <class Dummy>
+struct _Rb_global
+{
+	static _Rb_tree_node_base *_M_increment( _Rb_tree_node_base *node );
+};
+
+template <class Key, class Value, class KeyOfValue, class Compare, class Alloc>
+class _Rb_tree
+{
+public:
+	typedef _Rb_tree_node<Value> Node;
+
+	__forceinline void clear()
+	{
+		Node *node = (Node *)(void *)m_header->m_left;
+		while ( node != m_header )
+		{
+			clearValue( node->m_value_field.m_pod );
+			node = (Node *)_Rb_global<bool>::_M_increment( node );
+		}
+
+		if ( m_node_count != 0 )
+		{
+			_M_erase( (Node *)(void *)m_header->m_parent );
+			m_header->m_left = m_header;
+			m_header->m_parent = 0;
+			m_header->m_right = m_header;
+			m_node_count = 0;
+		}
+	}
+
+private:
+	__forceinline void _M_erase( Node *node );
+
+	_Rb_tree_node_base *m_header;
+	unsigned int m_node_count;
+	Compare m_key_compare;
+};
+
+template <class Key, class Value, class KeyOfValue, class Compare, class Alloc>
+__forceinline void _Rb_tree<Key, Value, KeyOfValue, Compare, Alloc>::_M_erase( Node *node )
+{
+	while ( node != 0 )
+	{
+		_M_erase( (Node *)(void *)node->m_right );
+		Node *left = (Node *)(void *)node->m_left;
+		nodeDeallocate( node, sizeof( Node ) );
+		node = left;
+	}
 }
 
-void ( *s_forceClearValue )( int ) = forceClearValue;
+}
+
+struct Gen_t_00234730_m4pod
+{
+	int m_pod;
+};
+
+typedef _STL::_Rb_tree<
+	Gen_t_00234730_m4pod,
+	Gen_t_00234730_m4pod,
+	_STL::_Identity<Gen_t_00234730_m4pod>,
+	_STL::less<Gen_t_00234730_m4pod>,
+	_STL::allocator<Gen_t_00234730_m4pod> > AptPalantirStoreTree;
+
+class AptPalantirStore
+{
+public:
+	void clear();
+
+private:
+	unsigned int m_value00;
+	bool m_flag04;
+	unsigned char m_padding05[ 3 ];
+	int m_value08;
+	int m_value0c;
+	AptPalantirStoreTree m_firstTree;
+	AptPalantirStoreTree m_secondTree;
+};
+
+void AptPalantirStore::clear()
+{
+	if ( m_value08 != 0 )
+	{
+		clearValue( m_value0c );
+		m_value08 = 0;
+	}
+
+	m_firstTree.clear();
+	m_secondTree.clear();
+	m_value00 = 0;
+	m_flag04 = false;
+}
