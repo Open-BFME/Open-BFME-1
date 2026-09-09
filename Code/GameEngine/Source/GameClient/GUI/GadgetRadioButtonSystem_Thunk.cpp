020a84f9dfbbc5ffd39688fcc398801e64243013
TheThag <zackgreco@hotmail.com>
2026-09-09 17:05:45 -0300

    Convert GadgetRadioButtonSystem

--- Code/GameEngine/Source/GameClient/GUI/GadgetRadioButtonSystem_Thunk.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GadgetRadioButtonSystem_Thunk.cpp
@@ -1,283 +1,187 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/campaignmanagerascii /ICode/Libraries/Source/WWVegas/WWLib /Ireference/shims/gamewindowlist /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad
+// stlport
 
-class GameWindow;
-enum WindowMsgHandledType {};
-WindowMsgHandledType __cdecl GadgetRadioButtonSystem(GameWindow *, unsigned int, unsigned int, unsigned int);
+// Real source for the BFME radio-button system callback.  The callback table
+// registration is in GameWindowManager::gogoGadgetRadioButton; the reference
+// constants and RadioButtonData layout are in Gadget.h.
+
+// The campaign AsciiString shim supplies the shared StringBase template.  Use
+// the inline forwarding UnicodeString wrapper for the by-value label argument,
+// then let the established BFME headers supply the complete window layouts.
+#include "Common/AsciiString.h"
+#define ASCIISTRING_H
+#define UNICODESTRING_H
+
+class UnicodeString : private StringBase<unsigned short>
+{
+public:
+	static UnicodeString TheEmptyString;
+	UnicodeString() : StringBase<unsigned short>() {}
+	UnicodeString(const UnicodeString &other) : StringBase<unsigned short>(other) {}
+	~UnicodeString() {}
+};
+
+#define __PLACEMENT_VEC_NEW_INLINE
+#include "GameClient/Gadget.h"
+
+class GameWindowManager
+{
+public:
+	virtual void _bfme_pad_00() = 0;
+	virtual void _bfme_pad_04() = 0;
+	virtual void _bfme_pad_08() = 0;
+	virtual void _bfme_pad_0C() = 0;
+	virtual void _bfme_pad_10() = 0;
+	virtual void _bfme_pad_14() = 0;
+	virtual void _bfme_pad_18() = 0;
+	virtual void _bfme_pad_1C() = 0;
+	virtual void _bfme_pad_20() = 0;
+	virtual void _bfme_pad_24() = 0;
+	virtual void _bfme_pad_28() = 0;
+	virtual void _bfme_pad_2C() = 0;
+	virtual void _bfme_pad_30() = 0;
+	virtual void _bfme_pad_34() = 0;
+	virtual void _bfme_pad_38() = 0;
+	virtual void _bfme_pad_3C() = 0;
+	virtual void _bfme_pad_40() = 0;
+	virtual void _bfme_pad_44() = 0;
+	virtual void _bfme_pad_48() = 0;
+	virtual void _bfme_pad_4C() = 0;
+	virtual void _bfme_pad_50() = 0;
+	virtual void _bfme_pad_54() = 0;
+	virtual void _bfme_pad_58() = 0;
+	virtual void _bfme_pad_5C() = 0;
+	virtual void _bfme_pad_60() = 0;
+	virtual void _bfme_pad_64() = 0;
+	virtual void _bfme_pad_68() = 0;
+	virtual void _bfme_pad_6C() = 0;
+	virtual void _bfme_pad_70() = 0;
+	virtual void _bfme_pad_74() = 0;
+	virtual void _bfme_pad_78() = 0;
+	virtual void _bfme_pad_7C() = 0;
+	virtual void _bfme_pad_80() = 0;
+	virtual void _bfme_pad_84() = 0;
+	virtual void _bfme_pad_88() = 0;
+	virtual void _bfme_pad_8C() = 0;
+	virtual void _bfme_pad_90() = 0;
+	virtual void _bfme_pad_94() = 0;
+	virtual void _bfme_pad_98() = 0;
+	virtual void _bfme_pad_9C() = 0;
+	virtual void _bfme_pad_A0() = 0;
+	virtual void _bfme_pad_A4() = 0;
+	virtual void _bfme_pad_A8() = 0;
+	virtual void _bfme_pad_AC() = 0;
+	virtual void _bfme_pad_B0() = 0;
+	virtual void _bfme_pad_B4() = 0;
+	virtual void _bfme_pad_B8() = 0;
+	virtual void _bfme_pad_BC() = 0;
+	virtual void _bfme_pad_C0() = 0;
+	virtual void _bfme_pad_C4() = 0;
+	virtual void _bfme_pad_C8() = 0;
+	virtual void _bfme_pad_CC() = 0;
+	virtual void _bfme_pad_D0() = 0;
+	virtual void winSendSystemMsg(GameWindow *window, UnsignedInt msg,
+		WindowMsgData mData1, WindowMsgData mData2) = 0;
+	virtual GameWindow *winGetWindowList() = 0;
+};
+
+extern GameWindowManager *TheWindowManager;
+
+static void doRadioUnselect(GameWindow *window, Int group, Int screen,
+	GameWindow *except)
+{
+	if (window != except && BitTest(window->winGetStyle(), GWS_RADIO_BUTTON))
+	{
+		RadioButtonData *radioData = (RadioButtonData *)window->winGetUserData();
+		if (radioData->group == group && radioData->screen == screen)
+		{
+			WinInstanceData *instData = window->winGetInstanceData();
+			BitClear(instData->m_state, WIN_STATE_SELECTED);
+		}
+	}
+
+	GameWindow *child;
+	for (child = window->winGetChild(); child; child = child->winGetNext())
+		doRadioUnselect(child, group, screen, except);
+}
+
+static void unselectOtherRadioOfGroup(Int group, Int screen, GameWindow *except)
+{
+	GameWindow *window = TheWindowManager->winGetWindowList();
+	for (window = TheWindowManager->winGetWindowList(); window;
+		window = window->winGetNext())
+		doRadioUnselect(window, group, screen, except);
+}
 
 // ?GadgetRadioButtonSystem@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z
-__declspec(naked) WindowMsgHandledType __cdecl GadgetRadioButtonSystem(GameWindow *, unsigned int, unsigned int, unsigned int)
+WindowMsgHandledType GadgetRadioButtonSystem(GameWindow *window, UnsignedInt msg,
+	WindowMsgData mData1, WindowMsgData mData2)
 {
-	__asm {
-        __emit 0x53
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0x7c
-        __emit 0x24
-        __emit 0x10
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0xe3
-        __emit 0x76
-        __emit 0xb7
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x83
-        __emit 0xf8
-        __emit 0x17
-        __emit 0x77
-        __emit 0x6a
-        __emit 0x74
-        __emit 0x23
-        __emit 0x48
-        __emit 0x0f
-        __emit 0x84
-        __emit 0xe3
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x48
-        __emit 0x75
-        __emit 0x6a
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0x6c
-        __emit 0x92
-        __emit 0xb8
-        __emit 0xff
-        __emit 0x50
-        __emit 0xe8
-        __emit 0xde
-        __emit 0x4b
-        __emit 0x3c
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0xb8
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x18
-        __emit 0x85
-        __emit 0xdb
-        __emit 0x75
-        __emit 0x04
-        __emit 0x83
-        __emit 0x66
-        __emit 0x08
-        __emit 0xfd
-        __emit 0xa1
-        __emit 0x40
-        __emit 0x1b
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x30
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0xac
-        __emit 0xb5
-        __emit 0xb5
-        __emit 0xff
-        __emit 0x50
-        __emit 0x53
-        __emit 0x68
-        __emit 0x03
-        __emit 0x40
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0xa9
-        __emit 0x08
-        __emit 0xb7
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x40
-        __emit 0x1b
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x50
-        __emit 0xff
-        __emit 0x96
-        __emit 0xd4
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x1c
-        __emit 0xc6
-        __emit 0x01
-        __emit 0x01
-        __emit 0xb8
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0xc3
-        __emit 0x2d
-        __emit 0x01
-        __emit 0x40
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x63
-        __emit 0x83
-        __emit 0xe8
-        __emit 0x09
-        __emit 0x74
-        __emit 0x06
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0xc3
-        __emit 0xf6
-        __emit 0x46
-        __emit 0x08
-        __emit 0x04
-        __emit 0x75
-        __emit 0x6a
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x24
-        __emit 0x8b
-        __emit 0x15
-        __emit 0x40
-        __emit 0x1b
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x1a
-        __emit 0x6a
-        __emit 0x00
-        __emit 0x57
-        __emit 0x68
-        __emit 0x08
-        __emit 0x40
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0x55
-        __emit 0x08
-        __emit 0xb7
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x40
-        __emit 0x1b
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x50
-        __emit 0xff
-        __emit 0x93
-        __emit 0xd4
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0xca
-        __emit 0x91
-        __emit 0xb8
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x48
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x8b
-        __emit 0x18
-        __emit 0x51
-        __emit 0xe8
-        __emit 0xf3
-        __emit 0xfb
-        __emit 0xff
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x83
-        __emit 0x4e
-        __emit 0x08
-        __emit 0x04
-        __emit 0xb8
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0xc3
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x51
-        __emit 0x89
-        __emit 0x64
-        __emit 0x24
-        __emit 0x18
-        __emit 0x8b
-        __emit 0xcc
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x62
-        __emit 0xb0
-        __emit 0x3c
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0x8e
-        __emit 0xc8
-        __emit 0xb8
-        __emit 0xff
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0xb8
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5b
-        __emit 0xc3
+	WinInstanceData *instData = window->winGetInstanceData();
+
+	switch (msg)
+	{
+		case GBM_SET_SELECTION:
+		{
+			if (BitTest(instData->getState(), WIN_STATE_SELECTED) == FALSE)
+			{
+				if ((Bool)mData1 == TRUE)
+				{
+					TheWindowManager->winSendSystemMsg(window->winGetOwner(),
+																					GBM_SELECTED,
+																					(WindowMsgData)window,
+																					0);
+				}
+
+				RadioButtonData *radioData = (RadioButtonData *)window->winGetUserData();
+				if (radioData->group != 0)
+					unselectOtherRadioOfGroup(radioData->group, radioData->screen, window);
+
+				BitSet(instData->m_state, WIN_STATE_SELECTED);
+			}
+
+			break;
+
+		}
+
+		case GGM_SET_LABEL:
+		{
+			window->GameWindow::winSetText(*(UnicodeString *)mData1);
+			break;
+
+		}
+
+		case GWM_CREATE:
+			break;
+
+		case GWM_DESTROY:
+		{
+			RadioButtonData *radioData = (RadioButtonData *)window->winGetUserData();
+
+			delete radioData;
+
+			break;
+
+		}
+
+		case GWM_INPUT_FOCUS:
+		{
+			if (mData1 == FALSE)
+				BitClear(instData->m_state, WIN_STATE_HILITED);
+
+			TheWindowManager->winSendSystemMsg(window->winGetOwner(),
+																			GGM_FOCUS_CHANGE,
+																			mData1,
+																			window->winGetWindowId());
+
+			*(Bool *)mData2 = TRUE;
+			break;
+
+		}
+
+		default:
+			return MSG_IGNORED;
 	}
+
+	return MSG_HANDLED;
 }
