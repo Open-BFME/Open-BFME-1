3cf4f3752fc0e056deb946d1dbbea8bff02c4eef
Jacob Starheim <74994007+JacobStarheim@users.noreply.github.com>
2026-09-09 02:41:40 +0200

    Recover complete Xfer Unicode string transfer in C++

---
+++ Code/GameEngine/Source/Common/System/XferUnicodeStringTransfer.cpp
@@ -0,0 +1,98 @@
+// cl: /DNDEBUG /MD /EHsc /ICode/GameEngine/Source/Common/System /ICode/Libraries/Source/WWVegas/WWLib
+#include "xfer.h"
+#include "unicode_string.h"
+
+// Retail exposes these small StringBase operations inline at this caller.
+template <typename T>
+inline int StringBase<T>::getLength() const
+{
+    return m_data ? m_data->length : 0;
+}
+
+template <>
+inline const unsigned short *StringBase<unsigned short>::str() const
+{
+    return m_data ? m_data->data : reinterpret_cast<const unsigned short *>(L"");
+}
+
+template <typename T>
+inline void StringBase<T>::clear()
+{
+    releaseBuffer();
+}
+
+class Rva009D6A70TransferView
+{
+public:
+    virtual void slot00();
+    virtual void slot01();
+    virtual bool isStoring() const;
+    virtual void slot03();
+    virtual void slot04();
+    virtual void slot05();
+    virtual void slot06();
+    virtual void slot07();
+    virtual void slot08();
+    virtual void slot09();
+    virtual void slot10();
+    virtual void slot11();
+    virtual void slot12();
+    virtual void slot13();
+    virtual void slot14();
+    virtual void slot15();
+    virtual void slot16();
+    virtual void slot17();
+    virtual void slot18();
+    virtual void slot19();
+    virtual void slot20();
+    virtual void slot21();
+    virtual void slot22();
+    virtual void slot23();
+    virtual void slot24();
+    virtual void slot25();
+    virtual void slot26();
+    virtual void slot27();
+    virtual void slot28();
+    virtual void slot29();
+    virtual void slot30();
+    virtual void slot31();
+    virtual void slot32();
+    virtual void slot33();
+    virtual void slot34();
+    virtual void slot35();
+    virtual void slot36();
+    virtual void transfer(void *context, const void *data, unsigned int size);
+};
+
+Xfer &Xfer::operator==(UnicodeString &us)
+{
+    Rva009D6A70TransferView *receiver = reinterpret_cast<Rva009D6A70TransferView *>(this);
+    if (receiver->isStoring())
+    {
+        int length = us.getLength();
+        if (length >= 255)
+        {
+            unsigned char marker = 255;
+            receiver->transfer(reinterpret_cast<void *>(0x75737472), &marker, 1);
+            receiver->transfer(0, &length, 4);
+        }
+        else
+            receiver->transfer(reinterpret_cast<void *>(0x75737472), &length, 1);
+        receiver->transfer(0, us.str(), length * 2);
+    }
+    else
+    {
+        int length = 0;
+        receiver->transfer(reinterpret_cast<void *>(0x75737472), &length, 1);
+        if (length == 255)
+            receiver->transfer(0, &length, 4);
+        if (length != 0)
+        {
+            receiver->transfer(0, reinterpret_cast<StringBase<unsigned short> *>(&us)->getBufferForRead(length), length * 2);
+            reinterpret_cast<StringBase<unsigned short> *>(&us)->getBufferForRead(length)[length] = 0;
+        }
+        else
+            reinterpret_cast<StringBase<unsigned short> *>(&us)->clear();
+    }
+    return *this;
+}
