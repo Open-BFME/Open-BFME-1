ab27b7ef0bce8a5f10c597737324ac17f47f62b6
TheThag <zackgreco@hotmail.com>
2026-09-09 20:46:03 -0300

    Convert ControlBar contained-object lookup

--- Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBar_findContainedObject.cpp
+++ Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBar_findContainedObject.cpp
@@ -1,13 +1,13 @@
-// ?findContainedObject@ControlBar@@QAEPAVObject@@PAVGameWindow@@@Z
-// partial score=0.99 date=2026-09-04
-// ?findContainedObject@ControlBar@@QAEPAVObject@@PAVGameWindow@@@Z
-// partial score=0.94 date=2026-09-02
-// cl: /DNDEBUG /DWIN32 /MD /D_STLP_USE_STATIC_LIB
+// cl: /DNDEBUG /MD /D_STLP_USE_STATIC_LIB
 // stlport
-
-// ControlBar lookup through the static ContainEntry table at 0x012F3448
-// (same array resetContainData clears), then the inlined GameLogic hash_map
-// findObjectByID. retail 0x004AED60, 117 bytes.
+//
+// ControlBar::findContainedObject, retail RVA 0x004AED60 / 117 bytes.
+// Its identity is established by the retail static ContainEntry
+// table at 0x012F3448, the reference ControlBar::ContainEntry declaration,
+// and the matching ControlBarCommand population and object-map lookup code.
+//
+// Keep the BFME object-map view local: this body inlines the STLport hash-map
+// lookup rather than calling the later named findObjectByID body.
 #define _STLP_USE_NEWALLOC 1
 #define _STLP_NO_EXCEPTIONS 1
 #include <hash_map>
@@ -47,23 +47,24 @@ public:
 Object *ControlBar::findContainedObject(GameWindow *win)
 {
 	int i;
+	ObjectID id = 0;
 
 	for (i = 0; i < MAX_COMMANDS_PER_SET; i++)
 	{
 		if (m_containData[i].control == win)
 		{
-			ObjectID id = m_containData[i].objectID;
-			if (!id)
-				goto contained_not_found;
-
-			ObjectPtrHash::iterator it = TheGameLogic->m_objHash.find(id);
-			if (it == TheGameLogic->m_objHash.end())
-				goto contained_not_found;
-			return (*it).second;
+			id = m_containData[i].objectID;
+			break;
 		}
 	}
 
-	return 0;
+	if (!id)
+		return 0;
+
+	ObjectPtrHash::iterator it = TheGameLogic->m_objHash.find(id);
+	if (it == TheGameLogic->m_objHash.end())
+		goto contained_not_found;
+	return (*it).second;
 
 contained_not_found:
 	return 0;
