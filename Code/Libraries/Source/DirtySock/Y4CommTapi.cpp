db7ac91a806c788e40cc64fc394750e51ee9fdf7
Markus Rink <anmarkusr@web.de>
2026-09-09 12:34:11 +0200

    Replace TAPI constructor inline assembly with typed calls

--- Code/Libraries/Source/DirtySock/Y4CommTapi.cpp
+++ Code/Libraries/Source/DirtySock/Y4CommTapi.cpp
@@ -26,46 +26,34 @@ extern "C"
 	void Rva00814040( void );
 	void Rva00814070( void );
 	void Rva008140A0( void );
+	// Rva00812FD0 is the retail cdecl worker entry and returns its status in EAX.
 	int Rva00812FD0( void *comm );
 	void *memset( void *dest, int value, unsigned int size );
 	char *strcpy( char *dest, const char *source );
-	void *Rva01358DDC;
-	void *Rva01358E3C;
-	void *Rva01358E44;
-	void __cdecl _RTC_CheckEsp( void );
-	void Rva00819630( void );
+	// These six IAT slots resolve to the named KERNEL32 APIs below.
+	void *(__stdcall *Rva01358DDC)( void ); // GetProcessHeap
+	void *(__stdcall *Rva01358E3C)( void *heap, unsigned int flags,
+		unsigned int size ); // HeapAlloc
+	int (__stdcall *Rva01358E44)( void *heap, unsigned int flags,
+		void *block ); // HeapFree
+	void *(__stdcall *Rva01358D00)( void *security, unsigned int stackSize,
+		void *start, void *argument, unsigned int flags,
+		unsigned int *threadId ); // CreateThread
+	int (__stdcall *Rva01358CCC)( void *handle ); // CloseHandle
+	void (__stdcall *Rva01358F30)( unsigned int interval ); // Sleep
+	void *Rva00819630( int first, int second, int third );
 }
 
 void rva00813CF0( void );
 void rva00813A40( void );
 void Rva00814020( void );
 
-typedef int ( __stdcall *Rva00812DD0LineInitProc )(
-	int first, int second, void *callback, void *comm, int fifth,
-	unsigned int *workerId );
-typedef void ( __stdcall *Rva00812DD0CloseProc )( int result );
-typedef void ( __stdcall *Rva00812DD0YieldProc )( int interval );
-
 extern "C" Rva00812DD0Comm *Rva00812DD0( int first, int second, int third )
 {
-	int result;
+	void *threadHandle;
 	Rva00812DD0Comm *comm;
 
-	__asm
-	{
-		mov esi, esp
-		push 019Ch
-		push 0
-		mov edi, esp
-		call dword ptr [Rva01358DDC]
-		cmp edi, esp
-		call _RTC_CheckEsp
-		push eax
-		call dword ptr [Rva01358E3C]
-		cmp esi, esp
-		call _RTC_CheckEsp
-		mov comm, eax
-	}
+	comm = (Rva00812DD0Comm *)Rva01358E3C( Rva01358DDC(), 0, 0x19C );
 	if ( comm == 0 )
 		return 0;
 
@@ -84,52 +72,23 @@ extern "C" Rva00812DD0Comm *Rva00812DD0( int first, int second, int third )
 	comm->m_ops[ 11 ] = (void *)Rva00814040;
 	comm->m_ops[ 12 ] = (void *)Rva00814070;
 	comm->m_ops[ 13 ] = (void *)Rva008140A0;
-	strcpy( comm->m_name, (const char *)0x012C4A60 );
+	strcpy( comm->m_name, "TAPI" );
 	comm->m_state = 0;
-	result = ( *(Rva00812DD0LineInitProc *)0x01358D00 )(
+	threadHandle = Rva01358D00(
 		0, 0, (void *)Rva00812FD0, comm, 0, &comm->m_workerId );
-	( *(Rva00812DD0CloseProc *)0x01358CCC )( result );
+	Rva01358CCC( threadHandle );
 	while ( comm->m_state == 0 )
-		( *(Rva00812DD0YieldProc *)0x01358F30 )( 0 );
+		Rva01358F30( 0 );
 
 	if ( comm->m_state == 1 )
 	{
-		__asm
-		{
-			mov esi, esp
-			mov ecx, comm
-			push ecx
-			push 0
-			mov edi, esp
-			call dword ptr [Rva01358DDC]
-			cmp edi, esp
-			call _RTC_CheckEsp
-			push eax
-			call dword ptr [Rva01358E44]
-			cmp esi, esp
-			call _RTC_CheckEsp
-		}
+		Rva01358E44( Rva01358DDC(), 0, comm );
 		return 0;
 	}
 
-	__asm
-	{
-		mov edx, third
-		push edx
-		mov eax, second
-		push eax
-		mov ecx, first
-		push ecx
-		call Rva00819630
-		add esp, 0Ch
-		mov edx, comm
-		mov dword ptr [edx+78h], eax
-		mov eax, comm
-		mov dword ptr [eax+88h], 0FFFFFFFFh
-		mov ecx, comm
-		mov dword ptr [ecx+8Ch], 0FFFFFFFFh
-		mov edx, comm
-		mov dword ptr [edx+90h], 2
-	}
+	comm->m_transport = Rva00819630( first, second, third );
+	comm->m_field88 = -1;
+	comm->m_field8C = -1;
+	comm->m_state = 2;
 	return comm;
 }
