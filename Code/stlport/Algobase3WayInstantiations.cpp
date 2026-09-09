35a1ad0ff8a2a6ab39d82dd8f43c4e4fc387d05a
Jonathan <jonf38@users.noreply.github.com>
2026-09-09 17:47:39 -0400

    Move STLport three-way compare 0x00844230 to source tree

---
+++ Code/stlport/Algobase3WayInstantiations.cpp
@@ -0,0 +1,9 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+//
+// STLport 4.5.3 lexicographical three-way comparison instantiations.
+
+#include <algorithm>
+
+template int _STL::__lexicographical_compare_3way(const char*, const char*, const char*, const char*);
+template int _STL::__lexicographical_compare_3way(const wchar_t*, const wchar_t*, const wchar_t*, const wchar_t*);
