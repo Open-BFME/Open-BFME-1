62407049fbcea3deb9e7c2cee8097ea6b3e2431c
Rudi Theunissen <rudi@goaugment.com>
2026-09-09 12:09:10 -0700

    chore(reverse): bank FireWeaponCollide predicate partial

--- reverse/attempts/0x00216690.cpp
+++ reverse/attempts/0x00216690.cpp
@@ -1,61 +1,73 @@
-// ?accepts00216690@Rva00216690Predicate@@QBE_NXZ
-// partial score=0.72 date=2026-09-02
-// cl: /DNDEBUG /MD
-// Address-derived two-mask predicate at retail RVA 0x00216690.
+// ?shouldFireWeapon@FireWeaponCollide@@UAE_NXZ
+// partial score=0.85 date=2026-09-09
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// Retail-layout ABI shim for FireWeaponCollide::shouldFireWeapon.
 
-class ObjectStatusMaskType
+class BfmeFlagsZX
 {
 public:
-	bool testForAny( const ObjectStatusMaskType &other ) const;
-	void set( unsigned int word0, unsigned int word1, unsigned int word2 )
+	void set(unsigned int word0, unsigned int word1, unsigned int word2)
 	{
-		m_words[ 0 ] = word0;
-		m_words[ 1 ] = word1;
-		m_words[ 2 ] = word2;
+		m_words[0] = word0;
+		m_words[1] = word1;
+		m_words[2] = word2;
 	}
-	unsigned int m_words[ 3 ];
+
+	bool bfmeAnyZX(const BfmeFlagsZX *other) const;
+	bool bfmeRequiredAnyZX(const BfmeFlagsZX *other) const;
+
+	unsigned int m_words[3];
 };
 
-struct Rva00216690Policy
+class FireWeaponCollideModuleData
 {
-	char m_unmodelled_000[ 0x0c ];
-	ObjectStatusMaskType m_excluded;
-	ObjectStatusMaskType m_required;
-	bool m_allowEmpty;
+private:
+	unsigned char m_prefix[0x0c];
+
+public:
+	BfmeFlagsZX m_requiredStatus;
+	BfmeFlagsZX m_forbiddenStatus;
+	bool m_fireOnce;
 };
 
-struct Rva00216690StatusOwner
-{
-	char m_unmodelled_000[ 0x90 ];
-	ObjectStatusMaskType m_status;
-};
-
-class Rva00216690Predicate
+class FireWeaponCollideThing
 {
 public:
-	bool accepts00216690() const;
-private:
-	unsigned int m_unmodelled_000;
-	Rva00216690Policy *m_policy;
-	Rva00216690StatusOwner *m_owner;
-	char m_unmodelled_00c[ 0x18 - 0x0c ];
-	bool m_allowEmpty;
+	unsigned char m_prefix[0x90];
+	unsigned int m_status[3];
 };
 
-bool Rva00216690Predicate::accepts00216690() const
+class FireWeaponCollide
 {
-	Rva00216690Policy *policy = m_policy;
-	unsigned int word0 = m_owner->m_status.m_words[ 0 ];
-	unsigned int word1 = m_owner->m_status.m_words[ 1 ];
-	unsigned int word2 = m_owner->m_status.m_words[ 2 ];
-	ObjectStatusMaskType status;
-	status.set( word0, word1, word2 );
-	if( status.testForAny( policy->m_required ) )
+public:
+	virtual bool shouldFireWeapon();
+
+private:
+	FireWeaponCollideModuleData *m_moduleData;
+	FireWeaponCollideThing *m_object;
+	unsigned char m_between[0x0c];
+	bool m_everFired;
+};
+
+// ?shouldFireWeapon@FireWeaponCollide@@ present-unmatched
+bool FireWeaponCollide::shouldFireWeapon()
+{
+	FireWeaponCollideModuleData *d = m_moduleData;
+	unsigned int word0 = m_object->m_status[0];
+	unsigned int word1 = m_object->m_status[1];
+	unsigned int word2 = m_object->m_status[2];
+	BfmeFlagsZX status;
+	status.set(word0, word1, word2);
+
+	if (status.bfmeAnyZX(&d->m_forbiddenStatus))
 		return false;
-	status.set( ~word0, ~word1, ~word2 & 0x003fffff );
-	if( status.testForAny( policy->m_excluded ) )
+
+	status.set(~word0, ~word1, (~word2) & 0x003fffff);
+	if (status.bfmeRequiredAnyZX(&d->m_requiredStatus))
 		return false;
-	if( m_allowEmpty && policy->m_allowEmpty )
+
+	if (m_everFired && d->m_fireOnce)
 		return false;
+
 	return true;
 }
