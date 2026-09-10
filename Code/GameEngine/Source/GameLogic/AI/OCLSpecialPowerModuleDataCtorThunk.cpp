b91b84784697c3f1464deb5f4fa971451737b69d
TheThag <zackgreco@hotmail.com>
2026-09-10 06:46:50 -0300

    Convert OCL module-data constructor and remove wrong constructor pin

--- Code/GameEngine/Source/GameLogic/AI/OCLSpecialPowerModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/OCLSpecialPowerModuleDataCtorThunk.cpp
@@ -1,135 +1,79 @@
-// cl: /DNDEBUG /MD /EHsc
-// readable body of ??0OCLSpecialPowerModuleData@@QAE@XZ: Code/GameEngine/Source/GameLogic/Object/SpecialPower/OCLSpecialPower.cpp
+// cl: /DNDEBUG /DWIN32 /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Open-BFME5: OCLSpecialPower module-data constructor.
+//
+// The retail object is 0x230 bytes.  Its matched destructor proves an
+// vector of eight-byte Upgrades elements at +0x210 and an AsciiString vector at +0x224;
+// the two scalar words between them are the default OCL and create-location
+// fields.  The reference header's single AsciiString tail is from a different
+// source revision and does not describe this retail layout.
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/OCLSpecialPower.h
-class OCLSpecialPowerModuleData
+#include <vector>
+
+class BfmeSpecialPowerModuleDataBase
 {
 public:
-    OCLSpecialPowerModuleData();
+	BfmeSpecialPowerModuleDataBase();
+	virtual ~BfmeSpecialPowerModuleDataBase();
+
+private:
+	unsigned char m_unmodelled_04[ 0x210 - 4 ];
 };
 
-__declspec(naked) OCLSpecialPowerModuleData::OCLSpecialPowerModuleData()
+struct BFMERetailAsciiString
 {
-    __asm {
-        _emit 56h
-        _emit 57h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 8Bh
-        _emit 2Ah
-        _emit 0DBh
-        _emit 0FFh
-        _emit 33h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 06h
-        _emit 80h
-        _emit 61h
-        _emit 0Bh
-        _emit 01h
-        _emit 89h
-        _emit 0BEh
-        _emit 10h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 14h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 18h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 24h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 28h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 2Ch
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 1Ch
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 8Bh
-        _emit 86h
-        _emit 14h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 8Bh
-        _emit 8Eh
-        _emit 10h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 8Bh
-        _emit 0D0h
-        _emit 2Bh
-        _emit 0D0h
-        _emit 0C1h
-        _emit 0FAh
-        _emit 03h
-        _emit 3Bh
-        _emit 0D7h
-        _emit 7Eh
-        _emit 15h
-        _emit 53h
-        _emit 8Bh
-        _emit 18h
-        _emit 89h
-        _emit 19h
-        _emit 8Bh
-        _emit 58h
-        _emit 04h
-        _emit 89h
-        _emit 59h
-        _emit 04h
-        _emit 83h
-        _emit 0C0h
-        _emit 08h
-        _emit 83h
-        _emit 0C1h
-        _emit 08h
-        _emit 4Ah
-        _emit 75h
-        _emit 0EDh
-        _emit 5Bh
-        _emit 89h
-        _emit 8Eh
-        _emit 14h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 89h
-        _emit 0BEh
-        _emit 20h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 5Fh
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 0C3h
-    }
+	~BFMERetailAsciiString();
+
+	char *m_data;
+};
+
+class OCLSpecialPowerModuleData
+	: public BfmeSpecialPowerModuleDataBase
+{
+public:
+	struct Upgrades
+	{
+		int m_science;
+		const void *m_ocl;
+	};
+
+	OCLSpecialPowerModuleData();
+	virtual ~OCLSpecialPowerModuleData();
+
+private:
+	_STL::vector<Upgrades> m_upgradeOCL;             // +0x210
+	const void *m_defaultOCL;                       // +0x21c
+	unsigned int m_createLoc;                       // +0x220
+	_STL::vector<BFMERetailAsciiString> m_names;    // +0x224
+};
+
+typedef char OCLModuleDataSizeMustBe230[
+	sizeof(OCLSpecialPowerModuleData) == 0x230 ? 1 : -1];
+
+// The named module-data factory at 0x00120230 allocates 0x230 bytes and
+// calls this constructor through ILT 0x0003EFB8. The runtime-object factory
+// at 0x001201B0 is distinct. Retail ends at ret 0x00263227 (padding +0x78).
+// ??0OCLSpecialPowerModuleData@@QAE@XZ
+OCLSpecialPowerModuleData::OCLSpecialPowerModuleData()
+{
+	m_defaultOCL = 0;
+	m_upgradeOCL.clear();
+	m_createLoc = 0;
 }
