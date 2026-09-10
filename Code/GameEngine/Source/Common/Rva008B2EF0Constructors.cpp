b398e200a3212ee5b65e128c1d537ec76b5266c7
Gabe Borges <gbborges@proton.me>
2026-09-10 16:35:11 -0400

    feat(apt): convert Rva00899F00Base constructor
    The callers at 0x00899FC0 and 0x008B2EF0 plus vtable stores 0x01135D68 and 0x01136058 identify this body as Rva00899F00Base. I placed the Apt flag initialization and Rva0089C860State construction in Code/GameEngine/Source/Common/Rva008B2EF0Constructors.cpp so the existing callers share the verified class layout.

--- Code/GameEngine/Source/Common/Rva008B2EF0Constructors.cpp
+++ Code/GameEngine/Source/Common/Rva008B2EF0Constructors.cpp
@@ -1,13 +1,78 @@
 // cl: /DNDEBUG /MD /EHsc
-// Address-derived interfaces: original class and argument types are unresolved.
-class Rva00899F00Base {
-public:
-    Rva00899F00Base(unsigned int argument0, unsigned int argument1);
-    virtual void rva008991B0();
-private:
-    // The base constructor reaches +0x1F; no larger extent is assumed.
-    unsigned char m_unknown04[0x1c];
+struct Rva00899560Value;
+
+struct Rva00899560Pool
+{
+    int m_capacity;
+    int m_count;
+    Rva00899560Value **m_items;
+    inline void addPooled(Rva00899560Value *value);
 };
+
+extern Rva00899560Pool *g_rva8CD130IdleHook;
+
+struct Rva00899560Value
+{
+    virtual ~Rva00899560Value();
+    unsigned int m_flags;
+
+    __forceinline Rva00899560Value(unsigned int type)
+    {
+        unsigned int flags = (((m_flags & ~0x3f) | type) & 0xF000803F) | 0x8000;
+        m_flags = flags;
+        if (type != 0x1c && type != 0xa)
+        {
+            m_flags = flags | 0x40000000;
+            g_rva8CD130IdleHook->addPooled(this);
+        }
+        else
+        {
+            m_flags = flags & 0xBFFFFFFF;
+        }
+    }
+};
+
+// ?addPooled@Rva00899560Pool@@ absent-from-retail
+inline void Rva00899560Pool::addPooled(Rva00899560Value *value)
+{
+    if (m_count >= m_capacity)
+    {
+        value->m_flags &= 0xBFFFFFFF;
+        return;
+    }
+    m_items[m_count] = value;
+    ++m_count;
+}
+
+struct Rva0089C860State
+{
+    Rva0089C860State(int value);
+    int m_value;
+    int m_first;
+    int m_second;
+    int m_third;
+};
+
+class Rva00899F00Base : public Rva00899560Value
+{
+public:
+    Rva00899F00Base(unsigned int argument0, int argument1);
+    virtual ~Rva00899F00Base();
+    virtual void rva008991B0();
+
+private:
+    Rva0089C860State m_state;
+    unsigned int m_stateBits;
+    unsigned int m_bits;
+};
+
+Rva00899F00Base::Rva00899F00Base(unsigned int argument0, int argument1)
+    : Rva00899560Value(argument0), m_state(argument1)
+{
+    *(unsigned char *)&m_bits = 0;
+    m_bits &= 0xFFFFFCFF;
+    m_stateBits = 0;
+}
 
 class Rva008B2EF0 : public Rva00899F00Base {
 public:
