a7fa016c24c14d6a1be9ea0261b416415f587cf1
TheThag <zackgreco@hotmail.com>
2026-09-10 01:44:38 -0300

    Convert INI bit-string parser including exception paths

--- Code/GameEngine/Source/Common/INI_parseBitString32_Thunk.cpp
+++ Code/GameEngine/Source/Common/INI_parseBitString32_Thunk.cpp
@@ -1,484 +1,92 @@
-// cl: /DNDEBUG /MD /EHsc
-// readable body of ?parseBitString32@INI@@: Code/GameEngine/Source/Common/INI/INI_stl.cpp
-// readable body of ?parseBitString32@INI@@SAXPAV1@PAX1PBX@Z: Code/GameEngine/Source/Common/INI/ini_parsers.cpp
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ob1 /Ireference/shims/ini_inline /Ireference/shims/ini /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad
+// stlport
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Clean BFME INI field-parser body.  The former implementation was a literal
+// __emit lift of the 0x00850E70 retail body; this TU keeps the canonical
+// parser callback in a real C++ definition.
+#include "PreRTS.h"
+#include "Common/INI.h"
 
-class INI { public: static void parseBitString32(INI *, void *, void *, void const *); };
 // ?parseBitString32@INI@@SAXPAV1@PAX1PBX@Z
-__declspec(naked) void INI::parseBitString32(INI *, void *, void *, void const *)
+void INI::parseBitString32( INI* ini, void * /*instance*/, void *store, const void* userData )
 {
-	__asm {
-		__emit 0x83
-		__emit 0xec
-		__emit 0x08
-		__emit 0x55
-		__emit 0x8b
-		__emit 0x6c
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x85
-		__emit 0xed
-		__emit 0x0f
-		__emit 0x84
-		__emit 0xa5
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0x7d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x0f
-		__emit 0x84
-		__emit 0x9b
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x80
-		__emit 0x14
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x53
-		__emit 0x56
-		__emit 0x50
-		__emit 0x6a
-		__emit 0x00
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x2c
-		__emit 0x00
-		__emit 0x32
-		__emit 0xdb
-		__emit 0xff
-		__emit 0x15
-		__emit 0xd8
-		__emit 0x94
-		__emit 0x35
-		__emit 0x01
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x85
-		__emit 0xf6
-		__emit 0x0f
-		__emit 0x84
-		__emit 0xc5
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x57
-		__emit 0x8b
-		__emit 0x7c
-		__emit 0x24
-		__emit 0x24
-		__emit 0xeb
-		__emit 0x06
-		__emit 0x8d
-		__emit 0x9b
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x68
-		__emit 0xd8
-		__emit 0x36
-		__emit 0x07
-		__emit 0x01
-		__emit 0x56
-		__emit 0xff
-		__emit 0x15
-		__emit 0x3c
-		__emit 0x93
-		__emit 0x35
-		__emit 0x01
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x0f
-		__emit 0x84
-		__emit 0xa8
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8a
-		__emit 0x06
-		__emit 0x3c
-		__emit 0x2b
-		__emit 0x75
-		__emit 0x18
-		__emit 0x8a
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x0f
-		__emit 0x85
-		__emit 0xd3
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x46
-		__emit 0x55
-		__emit 0x56
-		__emit 0xe8
-		__emit 0xef
-		__emit 0xfa
-		__emit 0xff
-		__emit 0xff
-		__emit 0xb3
-		__emit 0x01
-		__emit 0xeb
-		__emit 0x50
-		__emit 0x3c
-		__emit 0x2d
-		__emit 0x75
-		__emit 0x2a
-		__emit 0x8a
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x0f
-		__emit 0x85
-		__emit 0xda
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x55
-		__emit 0x46
-		__emit 0x56
-		__emit 0xe8
-		__emit 0xd3
-		__emit 0xfa
-		__emit 0xff
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0x8b
-		__emit 0x07
-		__emit 0xba
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xd3
-		__emit 0xe2
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0xb3
-		__emit 0x01
-		__emit 0xf7
-		__emit 0xd2
-		__emit 0x23
-		__emit 0xc2
-		__emit 0xeb
-		__emit 0x32
-		__emit 0x84
-		__emit 0xdb
-		__emit 0x0f
-		__emit 0x85
-		__emit 0xd7
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8a
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x06
-		__emit 0xc7
-		__emit 0x07
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x55
-		__emit 0x56
-		__emit 0xe8
-		__emit 0xa0
-		__emit 0xfa
-		__emit 0xff
-		__emit 0xff
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x30
-		__emit 0x01
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0x8b
-		__emit 0x07
-		__emit 0xba
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xd3
-		__emit 0xe2
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x0b
-		__emit 0xc2
-		__emit 0x89
-		__emit 0x07
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x8b
-		__emit 0x80
-		__emit 0x14
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x6a
-		__emit 0x00
-		__emit 0xff
-		__emit 0x15
-		__emit 0xd8
-		__emit 0x94
-		__emit 0x35
-		__emit 0x01
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x85
-		__emit 0xf6
-		__emit 0x0f
-		__emit 0x85
-		__emit 0x49
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0x5b
-		__emit 0x5d
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0xc3
-		__emit 0x8a
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x12
-		__emit 0x84
-		__emit 0xdb
-		__emit 0x75
-		__emit 0x0e
-		__emit 0xc7
-		__emit 0x07
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0x5b
-		__emit 0x5d
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0xc3
-		__emit 0x68
-		__emit 0xa4
-		__emit 0xeb
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x6a
-		__emit 0x02
-		__emit 0x51
-		__emit 0xe8
-		__emit 0x56
-		__emit 0xf6
-		__emit 0xff
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0x68
-		__emit 0x30
-		__emit 0xfc
-		__emit 0x1d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0x52
-		__emit 0xe8
-		__emit 0x44
-		__emit 0x5d
-		__emit 0x1a
-		__emit 0x00
-		__emit 0x68
-		__emit 0xa4
-		__emit 0xeb
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x6a
-		__emit 0x02
-		__emit 0x50
-		__emit 0xe8
-		__emit 0x33
-		__emit 0xf6
-		__emit 0xff
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0x68
-		__emit 0x30
-		__emit 0xfc
-		__emit 0x1d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x51
-		__emit 0xe8
-		__emit 0x21
-		__emit 0x5d
-		__emit 0x1a
-		__emit 0x00
-		__emit 0x68
-		__emit 0xa4
-		__emit 0xeb
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0x6a
-		__emit 0x02
-		__emit 0x52
-		__emit 0xe8
-		__emit 0x10
-		__emit 0xf6
-		__emit 0xff
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0x68
-		__emit 0x30
-		__emit 0xfc
-		__emit 0x1d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xfe
-		__emit 0x5c
-		__emit 0x1a
-		__emit 0x00
-		__emit 0x68
-		__emit 0xa4
-		__emit 0xeb
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x6a
-		__emit 0x02
-		__emit 0x51
-		__emit 0xe8
-		__emit 0xed
-		__emit 0xf5
-		__emit 0xff
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0x68
-		__emit 0x30
-		__emit 0xfc
-		__emit 0x1d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0x52
-		__emit 0xe8
-		__emit 0xdb
-		__emit 0x5c
-		__emit 0x1a
-		__emit 0x00
-		__emit 0x68
-		__emit 0x6c
-		__emit 0x04
-		__emit 0x13
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x08
-		__emit 0x6a
-		__emit 0x02
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xca
-		__emit 0xf5
-		__emit 0xff
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0x68
-		__emit 0x30
-		__emit 0xfc
-		__emit 0x1d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x51
-		__emit 0xe8
-		__emit 0xb8
-		__emit 0x5c
-		__emit 0x1a
-		__emit 0x00
+	ConstCharPtrArray flagList = (ConstCharPtrArray)userData;
+	UnsignedInt *bits = (UnsignedInt *)store;
+
+	if( flagList == NULL || flagList[ 0 ] == NULL)
+	{
+		DEBUG_ASSERTCRASH( flagList, ("INTERNAL ERROR! parseBitString32: No flag list provided!\n") );
+		throw INIException( 2, "INTERNAL ERROR! parseBitString32: No flag list provided!" );
+	}
+
+	volatile Bool foundNormal = false;
+	Bool foundAddOrSub = false;
+
+	for (const char *token = ini->getNextTokenOrNull(); token != NULL; token = ini->getNextTokenOrNull())
+	{
+		if (stricmp(token, "NONE") == 0)
+		{
+			if (foundNormal || foundAddOrSub)
+			{
+				DEBUG_CRASH(("you may not mix normal and +- ops in bitstring lists"));
+				throw INIException( 2, "you may not mix normal and +- ops in bitstring lists" );
+			}
+			*bits = 0;
+			break;
+		}
+
+		if (token[0] == '+')
+		{
+			if (foundNormal)
+			{
+				DEBUG_CRASH(("you may not mix normal and +- ops in bitstring lists"));
+				throw INIException( 2, "you may not mix normal and +- ops in bitstring lists" );
+			}
+			Int bitIndex = INI::scanIndexList(token + 1, flagList);
+			*bits |= (1 << bitIndex);
+			foundAddOrSub = true;
+		}
+		else if (token[0] == '-')
+		{
+			if (foundNormal)
+			{
+				DEBUG_CRASH(("you may not mix normal and +- ops in bitstring lists"));
+				throw INIException( 2, "you may not mix normal and +- ops in bitstring lists" );
+			}
+			Int bitIndex = INI::scanIndexList(token + 1, flagList);
+			*bits &= ~(1 << bitIndex);
+			foundAddOrSub = true;
+		}
+		else
+		{
+			if (foundAddOrSub)
+			{
+				DEBUG_CRASH(("you may not mix normal and +- ops in bitstring lists"));
+				throw INIException( 2, "you may not mix normal and +- ops in bitstring lists" );
+			}
+
+			if (!foundNormal)
+				*bits = 0;
+
+			Int bitIndex = INI::scanIndexList(token, flagList);
+			*bits |= (1 << bitIndex);
+			foundNormal = true;
+		}
 	}
 }
