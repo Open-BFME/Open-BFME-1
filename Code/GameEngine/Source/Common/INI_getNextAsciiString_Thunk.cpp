c1a32fdf7bbda42d7f884b6e487b1db334d40e93
TheThag <zackgreco@hotmail.com>
2026-09-10 02:26:07 -0300

    Convert INI getNextAsciiString to verified C++

--- Code/GameEngine/Source/Common/INI_getNextAsciiString_Thunk.cpp
+++ Code/GameEngine/Source/Common/INI_getNextAsciiString_Thunk.cpp
@@ -1,405 +1,79 @@
-// cl: /DNDEBUG /MD /EHsc
-// readable body of ?getNextAsciiString@INI@@: Code/GameEngine/Source/Common/INI/INI_stl.cpp
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ob1 /Ireference/shims/ini_inline /Ireference/shims/ini /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad
+// stlport
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-class AsciiString {};
-class INI { public: AsciiString getNextAsciiString(); };
+////////////////////////////////////////////////////////////////////////////////
+//	(c) 2001-2003 Electronic Arts Inc.															//
+////////////////////////////////////////////////////////////////////////////////
+
+// Readable body recovered from the canonical donor:
+// Code/GameEngine/Source/Common/INI/INI_stl.cpp
+// The donor retains the retail regular getNextAsciiString behavior, including
+// its static token buffer and null-token path.  This source owns the real C++
+// definition for the ledger row at 0x008516E0.
+#include "PreRTS.h"
+#include "Common/INI.h"
+
 // ?getNextAsciiString@INI@@QAE?AVAsciiString@@XZ
-__declspec(naked) AsciiString INI::getNextAsciiString()
+AsciiString INI::getNextAsciiString()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xe1
-		__emit 0x63
-		__emit 0x05
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xec
-		__emit 0x08
-		__emit 0x53
-		__emit 0x55
-		__emit 0x33
-		__emit 0xdb
-		__emit 0x56
-		__emit 0x57
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x8b
-		__emit 0xf9
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x87
-		__emit 0x14
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x2d
-		__emit 0xd8
-		__emit 0x94
-		__emit 0x35
-		__emit 0x01
-		__emit 0x50
-		__emit 0x53
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xff
-		__emit 0xd5
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x0f
-		__emit 0x84
-		__emit 0x02
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x80
-		__emit 0x38
-		__emit 0x22
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0x8d
-		__emit 0x71
-		__emit 0x01
-		__emit 0x74
-		__emit 0x19
-		__emit 0x8a
-		__emit 0x11
-		__emit 0x41
-		__emit 0x3a
-		__emit 0xd3
-		__emit 0x75
-		__emit 0xf9
-		__emit 0x2b
-		__emit 0xce
-		__emit 0x51
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0xe8
-		__emit 0xd7
-		__emit 0x65
-		__emit 0x03
-		__emit 0x00
-		__emit 0xe9
-		__emit 0xdf
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x88
-		__emit 0x1d
-		__emit 0x70
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x8a
-		__emit 0x11
-		__emit 0x41
-		__emit 0x3a
-		__emit 0xd3
-		__emit 0x75
-		__emit 0xf9
-		__emit 0x2b
-		__emit 0xce
-		__emit 0x83
-		__emit 0xf9
-		__emit 0x01
-		__emit 0x76
-		__emit 0x18
-		__emit 0x40
-		__emit 0xba
-		__emit 0x70
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x2b
-		__emit 0xd0
-		__emit 0x8d
-		__emit 0x9b
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8a
-		__emit 0x08
-		__emit 0x88
-		__emit 0x0c
-		__emit 0x02
-		__emit 0x40
-		__emit 0x3a
-		__emit 0xcb
-		__emit 0x75
-		__emit 0xf6
-		__emit 0x8b
-		__emit 0x87
-		__emit 0x20
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x75
-		__emit 0x06
-		__emit 0x8b
-		__emit 0x87
-		__emit 0x14
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x53
-		__emit 0xff
-		__emit 0xd5
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x74
-		__emit 0x68
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0x8d
-		__emit 0x71
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x9b
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8a
-		__emit 0x11
-		__emit 0x41
-		__emit 0x3a
-		__emit 0xd3
-		__emit 0x75
-		__emit 0xf9
-		__emit 0x2b
-		__emit 0xce
-		__emit 0x83
-		__emit 0xf9
-		__emit 0x01
-		__emit 0x76
-		__emit 0x24
-		__emit 0x80
-		__emit 0x78
-		__emit 0x01
-		__emit 0x09
-		__emit 0x74
-		__emit 0x1e
-		__emit 0xbf
-		__emit 0x70
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x4f
-		__emit 0x8d
-		__emit 0x9b
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8a
-		__emit 0x4f
-		__emit 0x01
-		__emit 0x47
-		__emit 0x3a
-		__emit 0xcb
-		__emit 0x75
-		__emit 0xf8
-		__emit 0x66
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x1c
-		__emit 0xed
-		__emit 0x08
-		__emit 0x01
-		__emit 0x66
-		__emit 0x89
-		__emit 0x0f
-		__emit 0x8b
-		__emit 0xd0
-		__emit 0x8a
-		__emit 0x08
-		__emit 0x40
-		__emit 0x3a
-		__emit 0xcb
-		__emit 0x75
-		__emit 0xf9
-		__emit 0xbf
-		__emit 0x70
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x2b
-		__emit 0xc2
-		__emit 0x4f
-		__emit 0x8a
-		__emit 0x4f
-		__emit 0x01
-		__emit 0x47
-		__emit 0x3a
-		__emit 0xcb
-		__emit 0x75
-		__emit 0xf8
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xc1
-		__emit 0xe9
-		__emit 0x02
-		__emit 0x8b
-		__emit 0xf2
-		__emit 0xf3
-		__emit 0xa5
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0x83
-		__emit 0xe1
-		__emit 0x03
-		__emit 0xf3
-		__emit 0xa4
-		__emit 0xeb
-		__emit 0x22
-		__emit 0xb8
-		__emit 0x70
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x50
-		__emit 0x01
-		__emit 0x8a
-		__emit 0x08
-		__emit 0x40
-		__emit 0x3a
-		__emit 0xcb
-		__emit 0x75
-		__emit 0xf9
-		__emit 0x2b
-		__emit 0xc2
-		__emit 0x74
-		__emit 0x0f
-		__emit 0x80
-		__emit 0xb8
-		__emit 0x6f
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x22
-		__emit 0x75
-		__emit 0x06
-		__emit 0x88
-		__emit 0x98
-		__emit 0x6f
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x68
-		__emit 0x70
-		__emit 0xce
-		__emit 0x30
-		__emit 0x01
-		__emit 0xe8
-		__emit 0x26
-		__emit 0x95
-		__emit 0x7b
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x74
-		__emit 0x24
-		__emit 0x28
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0x23
-		__emit 0x63
-		__emit 0x03
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x88
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x20
-		__emit 0xe8
-		__emit 0xee
-		__emit 0x60
-		__emit 0x03
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x5e
-		__emit 0x5d
-		__emit 0x5b
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x14
-		__emit 0xc2
-		__emit 0x04
-		__emit 0x00
+	AsciiString result;
+
+	const char *token = getNextTokenOrNull();	// if null, just leave an empty string
+	if (token != NULL)
+	{
+		if (token[0] != '"')
+		{
+			// Retail keeps this arm's length calculation in the caller and calls
+			// StringBase::set(const char *, int) directly at 0x00887D20.
+			StringBase<char> *base = (StringBase<char> *)&result;
+			base->set( token, strlen(token) );
+		}
+		else
+		{
+			static char buff[INI_MAX_CHARS_PER_LINE];
+			buff[0] = 0;
+			if (strlen(token) > 1)
+			{
+				strcpy(buff, &token[1]);
+			}
+
+			token = getNextTokenOrNull(getSepsQuote());
+			if (token)
+			{
+				if (strlen(token) > 1 && token[1] != '\t')
+				{
+					strcat(buff, " ");
+				}
+				strcat(buff, token);
+				result.set(buff);
+			}
+			else
+			{
+				Int len = strlen(buff);
+				if (len && buff[len-1] == '"')
+				{
+					buff[len-1] = 0;
+				}
+				result.set(buff);
+			}
+		}
 	}
+	return result;
 }
