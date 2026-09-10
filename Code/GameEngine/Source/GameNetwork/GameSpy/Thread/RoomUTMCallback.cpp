e17ef084f3c9b03002d36a69a5f2eada2db97089
TheThag <zackgreco@hotmail.com>
2026-09-10 00:13:21 -0300

    Convert PeerThread room command callback to C++

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/Thread/RoomUTMCallback.cpp
@@ -0,0 +1,100 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /Oy
+// stlport
+//
+// TU-local reconstruction of the BFME PeerResponse ABI.  RoomType's values
+// are the retail peer-room values: TitleRoom=0, GroupRoom=1, StagingRoom=2.
+
+#include <string>
+
+typedef void *PEER;
+typedef int PEERBool;
+
+enum RoomType
+{
+	TitleRoom,
+	GroupRoom,
+	StagingRoom
+};
+
+class PeerResponse
+{
+public:
+	int peerResponseType;
+
+	std::string groupRoomName;
+	std::string nick;
+	std::string oldNick;
+	std::wstring text;
+	std::string locale;
+	std::string stagingServerGameOptions;
+	std::wstring stagingServerName;
+	std::string stagingServerPingString;
+	std::string stagingServerLadderIP;
+	std::string stagingRoomMapName;
+	std::string stagingRoomPlayerNames[8];
+	std::string command;
+	std::string commandOptions;
+
+	union
+	{
+		struct
+		{
+			int profileID;
+			int wins;
+			int losses;
+			int roomType;
+			int flags;
+			unsigned int IP;
+			int rankPoints;
+			int side;
+			int preorder;
+			char unknown[0x20c];
+			int rank1v1;
+			int rank2v2;
+			int lastLadder;
+		} player;
+		int words[143];
+	};
+};
+
+typedef char PeerResponseSizeCheck[sizeof(PeerResponse) == 0x330 ? 1 : -1];
+
+class PeerRequest;
+
+class GameSpyPeerMessageQueueInterface
+{
+public:
+	virtual ~GameSpyPeerMessageQueueInterface() {}
+	virtual void startThread(void) = 0;
+	virtual void endThread(void) = 0;
+	virtual bool isThreadRunning(void) = 0;
+	virtual bool isConnected(void) = 0;
+	virtual bool isConnecting(void) = 0;
+	virtual void addRequest(const PeerRequest &) = 0;
+	virtual bool getRequest(PeerRequest &) = 0;
+	virtual void addResponse(const PeerResponse &) = 0;
+};
+
+extern GameSpyPeerMessageQueueInterface *TheGameSpyPeerMessageQueue;
+
+#pragma comment(linker, "/alternatename:??0PeerResponse@@QAE@XZ=?j_00042069@@YAXXZ")
+#pragma comment(linker, "/alternatename:??1PeerResponse@@QAE@XZ=?j_00044733@@YAXXZ")
+
+#pragma optimize("y", on)
+void roomUTMCallback(PEER peer, RoomType roomType, const char *nick,
+	const char *command, const char *parameters, PEERBool authenticated,
+	void *param)
+{
+	if (roomType != StagingRoom)
+		return;
+	PeerResponse resp;
+	resp.peerResponseType = 15;
+	if (nick)
+	{
+		resp.nick = nick;
+		resp.command = command;
+		resp.commandOptions = parameters;
+		TheGameSpyPeerMessageQueue->addResponse(resp);
+	}
+}
+#pragma optimize("y", off)
