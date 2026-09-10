a909778120fb5aaed6d7c8b113864715c7f6a437
TheThag <zackgreco@hotmail.com>
2026-09-10 13:31:07 -0300

    Convert caller-proven entity expansion helper

--- Code/GameEngine/Source/Common/BfmeConv2008.cpp
+++ Code/GameEngine/Source/Common/BfmeConv2008.cpp
@@ -12,6 +12,50 @@ public:
 	char *m_bfmeTailEAN;
 };
 
+struct BfmeEntityEAN
+{
+	char *m_bfmeNameEAN;
+	char m_bfmeCodeEAN;
+	char m_bfmePaddingEAN[3];
+};
+
+extern BfmeEntityEAN g_bfmeEntityTableEAN[]; // retail 0x012B3ED0
+__declspec(dllimport) int __cdecl bfmeCmp1026(char *left, char *right, int count);
+
+char BfmeLexEAN::bfmeExpandEAN(char *out)
+{
+	++m_bfmePosEAN;
+
+	if (*m_bfmePosEAN == 0)
+		return 0;
+
+	for (unsigned int index = 0; index < 5; ++index)
+	{
+		unsigned char c;
+		char *name = g_bfmeEntityTableEAN[index].m_bfmeNameEAN;
+		char *end = name;
+		char *first = end + 1;
+
+		do
+			c = *end++;
+		while (c != 0);
+
+		int length = end - first;
+		if (bfmeCmp1026(m_bfmePosEAN,
+			g_bfmeEntityTableEAN[index].m_bfmeNameEAN, length) == 0)
+		{
+			m_bfmePosEAN += length;
+			if (*m_bfmePosEAN != ';')
+				return 0;
+
+			*out = g_bfmeEntityTableEAN[index].m_bfmeCodeEAN;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
 int BfmeLexEAN::bfmeScanEAN()
 {
 	int n = 0;
