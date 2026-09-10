fe6516bd64a585eb1d9b849b1a0566d509c2eafd
Jacob Starheim <74994007+JacobStarheim@users.noreply.github.com>
2026-09-10 20:17:26 +0200

    Recover AIGroup destructor

--- Code/GameEngine/Source/GameLogic/AI/AIGroupDestructor.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIGroupDestructor.cpp
@@ -1,5 +1,3 @@
-// ??1AIGroup@@UAE@XZ
-// partial score=0.99 date=2026-09-06
 // cl: /DNDEBUG /DWIN32 /MD /EHsc /D_STLP_USE_STATIC_LIB
 // stlport
 // Open-BFME: AIGroup::~AIGroup, retail 0x00151D10, 246 bytes.
@@ -25,7 +23,7 @@ public:
 	void leaveGroup();
 };
 
-class Path
+class __declspec(novtable) Path
 {
     friend class AIGroup;
 
@@ -33,12 +31,19 @@ protected:
     virtual ~Path();
 };
 
-void operator delete(void *block) throw();
+class BFMEDeletablePath : public Path
+{
+public:
+    void destroy() { Path::~Path(); }
+};
+
+void __cdecl operator delete(void *block);
 
 class AIGroup : public BfmeBaseVUQ
 {
 public:
 	virtual ~AIGroup();
+	friend class PathDeleteArgument;
 
 private:
 	_STL::list<Object *> m_memberList;
@@ -46,14 +51,32 @@ private:
 	float m_speed;
 	bool m_dirty;
     UnsignedInt m_id;
-    Path *m_groundPath;
-    void *m_pathState1C;
-    void *m_pathState20;
-    void *m_pathState24;
-    float m_pathState28;
-    void *m_pathState2C;
-    void *m_pathState30;
+    volatile Path *m_groundPath;
+    volatile void *m_pathState1C;
+    volatile void *m_pathState20;
+    volatile void *m_pathState24;
+    volatile float m_pathState28;
+    volatile void *m_pathState2C;
+    volatile void *m_pathState30;
 	_STL::vector<UnsignedInt> m_lastRequestedIDList;
+};
+
+class PathDeleteArgument
+{
+public:
+	PathDeleteArgument(Path *path, AIGroup *group) : m_path(path), m_group(group) { }
+	operator void *(void) const { return m_path; }
+	~PathDeleteArgument()
+	{
+		m_group->m_groundPath = 0;
+		m_group->m_pathState1C = 0;
+		m_group->m_pathState20 = 0;
+		m_group->m_pathState24 = 0;
+	}
+
+private:
+	Path *m_path;
+	AIGroup *m_group;
 };
 
 // ??1AIGroup@@UAE@XZ
@@ -61,6 +84,7 @@ AIGroup::~AIGroup()
 {
     register AIGroup *self = this;
     register UnsignedInt zero = self->m_memberListSize - self->m_memberListSize;
+    register Path *groundPath;
     std::list<Object *>::iterator i;
     for (i = self->m_memberList.begin(); i != self->m_memberList.end();)
     {
@@ -76,14 +100,11 @@ AIGroup::~AIGroup()
         }
     }
 
-    register Path *groundPath = self->m_groundPath;
+    groundPath = (Path *)self->m_groundPath;
     if (groundPath != (Path *)zero)
     {
-        groundPath->Path::~Path();
-        self->m_groundPath = (Path *)zero;
-        self->m_pathState1C = (void *)zero;
-        self->m_pathState20 = (void *)zero;
-        self->m_pathState24 = (void *)zero;
+        ((BFMEDeletablePath *)groundPath)->destroy();
+        operator delete(PathDeleteArgument(groundPath, self));
         self->m_pathState28 = 10.0f;
         self->m_pathState2C = (void *)zero;
         self->m_pathState30 = (void *)zero;
