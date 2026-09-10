7ca0f14d284eab97fe506eed240af2ed05f2da67
Gabe Borges <gbborges@proton.me>
2026-09-10 18:11:09 -0400

    feat(common): convert Rva0078F770 dispatch
    The direct selectFactory@Rva0078F770Owner caller at 0x0078F770 and the four-argument retail thunk identify 0x0047D150 as Rva0078F770Owner::dispatchFactory. The clean body now lives beside the selector implementations in Code/GameEngine/Source/Common/Rva0078F950FactoryDispatch.cpp, where the record fields and helper thunks match the retail ABI.

--- Code/GameEngine/Source/Common/Rva0078F950FactoryDispatch.cpp
+++ Code/GameEngine/Source/Common/Rva0078F950FactoryDispatch.cpp
@@ -19,6 +19,66 @@ class Rva007961D0VptrCtor;
 class Rva00796260VptrCtor;
 class Rva0078FCA0VptrCtor;
 class Rva0078FD30VptrCtor;
+class GameWindow;
+class GameFont;
+
+class DisplayString
+{
+public:
+	virtual void v00();
+	virtual void v01();
+	virtual void v02();
+	virtual int getTextLength();
+};
+
+class WinInstanceData
+{
+public:
+	char m_gap00[0xc];
+	unsigned int m_style;
+	char m_gap10[0x174];
+	GameFont *m_font;
+	char m_gap188[0x14];
+	DisplayString *m_text;
+	inline int getTextLength()
+	{
+		if (m_text != 0)
+			return m_text->getTextLength();
+		return 0;
+	}
+};
+
+struct Rva0078F770ListboxData
+{
+	short m_listLength;
+	char m_gap02[8];
+	unsigned char m_scrollBar;
+	char m_gap0b[0x1d];
+	int m_totalHeight;
+	short m_insertPos;
+	short m_selectPos;
+	char m_gap30[4];
+	int m_scrollPosition;
+	char m_gap38[4];
+	short m_displayHeight;
+	char m_gap3e[2];
+	int m_displayPos;
+	short m_totalHeightShort;
+	char m_gap46[6];
+};
+
+class GameWindow
+{
+public:
+	void winSetUserData(void *data);
+	int winSetOwner(GameWindow *owner);
+};
+
+void j_00002e69();
+void j_000194a7();
+void j_000272fa();
+void j_0002d939();
+void j_00047230();
 class Rva007996A0VptrCtor;
 class Rva00799730VptrCtor;
 class Rva00790C40VptrCtor;
@@ -67,10 +127,16 @@ struct Rva0078F950Record
 {
 	Rva0078F830Predicate *m_head;
 	signed char m_kind;
-	char m_gap[19];
+	char m_gap[15];
+	short m_height;
+	char m_gapAfterHeight[2];
 	Rva0078F950Factory m_factory;
 	char m_gap2[20];
-	Rva0078F830Context *m_context;
+	union
+	{
+		Rva0078F830Context *m_context;
+		WinInstanceData *m_instanceData;
+	};
 };
 
 class Rva0078F950Owner
@@ -120,14 +186,84 @@ struct Rva0078F770Secondary
 {
 	char m_gap[11];
 	unsigned char m_flag;
+	char m_rest[0x40];
 };
 
 class Rva0078F770Owner
 {
 public:
+	virtual void v00();
+	virtual void v01();
+	virtual void v02();
+	virtual void v03();
+	virtual void v04();
+	virtual void v05();
+	virtual void v06();
+	virtual void v07();
+	virtual void v08();
+	virtual void v09();
+	virtual void v10();
+	virtual void v11();
+	virtual void v12();
+	virtual void v13();
+	virtual void v14();
+	virtual void v15();
+	virtual void v16();
+	virtual void v17();
+	virtual void v18();
+	virtual void v19();
+	virtual void v20();
+	virtual void v21();
+	virtual void v22();
+	virtual void v23();
+	virtual void v24();
+	virtual void assignDefaultGadgetLook(GameWindow *, int, int);
+	virtual void v26();
+	virtual void v27();
+	virtual void v28();
+	virtual GameWindow *create(void *record);
+	virtual void v30();
+	virtual void v31();
+	virtual void v32();
+	virtual void v33();
+	virtual void v34();
+	virtual void v35();
+	virtual void v36();
+	virtual void v37();
+	virtual void v38();
+	virtual void v39();
+	virtual void v40();
+	virtual void v41();
+	virtual void v42();
+	virtual void v43();
+	virtual void v44();
+	virtual void v45();
+	virtual void v46();
+	virtual void v47();
+	virtual void v48();
+	virtual void v49();
+	virtual void v50();
+	virtual void v51();
+	virtual void v52();
+	virtual void v53();
+	virtual void v54();
+	virtual void v55();
+	virtual void v56();
+	virtual void v57();
+	virtual void v58();
+	virtual void v59();
+	virtual void v60();
+	virtual void v61();
+	virtual void v62();
+	virtual void v63();
+	virtual void v64();
+	virtual void v65();
+	virtual void v66();
+	virtual int winFontHeight(GameFont *font);
+
 	void selectFactory(Rva0078F950Record *record,
 		Rva0078F770Secondary *secondary, int ignored, int value);
-	void dispatchFactory(Rva0078F950Record *record,
+	GameWindow *dispatchFactory(Rva0078F950Record *record,
 		Rva0078F770Secondary *secondary, int ignored, int value);
 };
 
@@ -139,6 +275,56 @@ public:
 	int dispatchFactory(Rva0078F950Record *record, int first, int second,
 		int third);
 };
+
+GameWindow *Rva0078F770Owner::dispatchFactory(Rva0078F950Record *record,
+	Rva0078F770Secondary *secondary, int ignored, int value)
+{
+	bool title = false;
+
+	if ((record->m_instanceData->m_style & 0x20) == 0)
+		return 0;
+
+	Rva0078F770ListboxData *listboxData = new Rva0078F770ListboxData;
+	*listboxData = *(Rva0078F770ListboxData *)secondary;
+	GameWindow *listbox = create(record);
+	if (listbox == 0)
+		return 0;
+
+	listbox->winSetUserData(listboxData);
+	listbox->winSetOwner((GameWindow *)record->m_head);
+
+	{
+		WinInstanceData &instanceData = *record->m_instanceData;
+		if (instanceData.m_text != 0)
+		{
+			if (instanceData.m_text->getTextLength())
+				title = true;
+		}
+	}
+
+	int length = listboxData->m_listLength;
+	listboxData->m_listLength = 0;
+	((void (__cdecl *)(GameWindow *, int))j_0002d939)(listbox, length);
+	listboxData->m_displayHeight = record->m_height;
+	if (title)
+	{
+		WinInstanceData *instanceData = record->m_instanceData;
+		GameFont *font = instanceData->m_font;
+		listboxData->m_displayHeight -= winFontHeight(font);
+	}
+	listboxData->m_totalHeightShort = 0;
+	listboxData->m_displayPos = 0;
+	listboxData->m_selectPos = 0;
+	listboxData->m_insertPos = 0;
+	listboxData->m_totalHeight = 0;
+	listboxData->m_scrollPosition = -1;
+	if (listboxData->m_scrollBar)
+		((void (__cdecl *)(GameWindow *))j_000194a7)(listbox);
+	((void (__cdecl *)(GameWindow *))j_000272fa)(listbox);
+	Rva0078F770Owner *owner = this;
+	owner->assignDefaultGadgetLook(listbox, ignored, value);
+	return listbox;
+}
 
 void Rva0078F950Owner::selectFactory(Rva0078F950Record *record)
 {
