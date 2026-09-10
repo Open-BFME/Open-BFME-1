d5ef93dcf1c073f6fbfe64cc071bef787a402c8a
TheThag <zackgreco@hotmail.com>
2026-09-09 22:23:42 -0300

    Convert BattlePlan paralyze callback with integer return ABI

--- Code/GameEngine/Source/GameLogic/Object/Update/ParalyzeTroopBodyThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/ParalyzeTroopBodyThunk.cpp
@@ -1,77 +1,58 @@
-// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad
+// stlport
 
-__declspec(naked) void ParalyzeTroopBodyThunk()
+#define Matrix4x4 Matrix4
+
+#include "PreRTS.h"
+
+#include "Common/BitFlagsIO.h"
+#include "Common/Radar.h"
+#include "Common/PlayerList.h"
+#include "Common/ThingTemplate.h"
+#include "Common/ThingFactory.h"
+#include "Common/Player.h"
+#include "Common/Xfer.h"
+
+#include "GameClient/GameClient.h"
+#include "GameClient/Drawable.h"
+#include "GameClient/GameText.h"
+#include "GameClient/ParticleSys.h"
+#include "GameClient/FXList.h"
+#include "GameClient/ControlBar.h"
+
+#include "GameLogic/GameLogic.h"
+#include "GameLogic/PartitionManager.h"
+#include "GameLogic/Object.h"
+#include "GameLogic/ObjectIter.h"
+#include "GameLogic/Weaponset.h"
+#include "GameLogic/Weapon.h"
+#include "GameLogic/TerrainLogic.h"
+#include "GameLogic/Module/SpecialPowerModule.h"
+#include "GameLogic/Module/BattlePlanUpdate.h"
+#include "GameLogic/Module/PhysicsUpdate.h"
+#include "GameLogic/Module/ActiveBody.h"
+#include "GameLogic/Module/AIUpdate.h"
+#include "GameLogic/Module/StealthDetectorUpdate.h"
+
+struct BattlePlanUpdateModuleDataRetailView
 {
-	__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 074h
-		_emit 024h
-		_emit 00Ch
-		_emit 057h
-		_emit 08Bh
-		_emit 07Ch
-		_emit 024h
-		_emit 00Ch
-		_emit 08Dh
-		_emit 046h
-		_emit 054h
-		_emit 050h
-		_emit 08Bh
-		_emit 0CFh
-		_emit 0E8h
-		_emit 075h
-		_emit 0CFh
-		_emit 0DBh
-		_emit 0FFh
-		_emit 084h
-		_emit 0C0h
-		_emit 074h
-		_emit 025h
-		_emit 08Dh
-		_emit 04Eh
-		_emit 06Ch
-		_emit 051h
-		_emit 08Bh
-		_emit 0CFh
-		_emit 0E8h
-		_emit 066h
-		_emit 0CFh
-		_emit 0DBh
-		_emit 0FFh
-		_emit 084h
-		_emit 0C0h
-		_emit 075h
-		_emit 016h
-		_emit 08Bh
-		_emit 015h
-		_emit 098h
-		_emit 008h
-		_emit 02Fh
-		_emit 001h
-		_emit 08Bh
-		_emit 042h
-		_emit 03Ch
-		_emit 003h
-		_emit 046h
-		_emit 050h
-		_emit 050h
-		_emit 06Ah
-		_emit 004h
-		_emit 08Bh
-		_emit 0CFh
-		_emit 0E8h
-		_emit 08Ch
-		_emit 0E0h
-		_emit 0DAh
-		_emit 0FFh
-		_emit 05Fh
-		_emit 0B8h
-		_emit 001h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 05Eh
-		_emit 0C3h
+	unsigned char m_padding[ 0x6c ];
+	KindOfMaskType m_invalidMemberKindOf;
+};
+
+// Named setBattlePlan at RVA 0x00285920 passes this callback at +0x191.
+// Retail returns integer 1; ret at 0x002855C5 gives the full 70-byte body.
+int paralyzeTroop( Object *obj, void *userData )
+{
+	const BattlePlanUpdateModuleData *data = (const BattlePlanUpdateModuleData *)userData;
+	const BattlePlanUpdateModuleDataRetailView *retailData =
+		(const BattlePlanUpdateModuleDataRetailView *)data;
+	if( obj->isAnyKindOf( data->m_validMemberKindOf ) )
+	{
+		if( !obj->isAnyKindOf( retailData->m_invalidMemberKindOf ) )
+		{
+			obj->setDisabledUntil( DISABLED_PARALYZED, TheGameLogic->getFrame() + data->m_battlePlanParalyzeFrames );
+		}
 	}
+	return true;
 }
