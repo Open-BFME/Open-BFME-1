dfc607f65690543aaaf2b22f062893325eb14235
TheThag <zackgreco@hotmail.com>
2026-09-10 13:52:06 -0300

    Recover caller-proven Coord3D vector overflow specialization

---
+++ Code/Libraries/Source/WWVegas/WWLib/Coord3DVectorInsertOverflow.cpp
@@ -0,0 +1,132 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+
+//
+// Retail 0x000B7910 is STLport's reallocating insert for vector<Coord3D>.
+// The matched GiantBirdAIUpdate::privateFollowPathAppend body at 0x002C2830
+// reaches this specialization through the 0x000070CC ILT.  Its 12-byte
+// element stride, Coord3D _Construct callee (0x0003FDA5), allocator pair, and
+// five-stack-argument thiscall establish the concrete specialization and ABI.
+// The old storage release is inline in this variant rather than through
+// vector::_M_clear.
+
+struct Coord3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+namespace _STL
+{
+template <class Type>
+class allocator
+{
+};
+
+struct __false_type
+{
+};
+
+void *__cdecl vectorLargeAllocate(unsigned int bytes);
+void *__cdecl vectorSmallAllocate(unsigned int bytes);
+void __cdecl vectorLargeDeallocate(void *block);
+void __cdecl vectorSmallDeallocate(void *block, unsigned int bytes);
+
+template <class Type, class Value>
+void __cdecl _Construct(Type *destination, const Value &value);
+
+template <class Type>
+__forceinline Type *uninitialized_copy(Type *first, Type *last, Type *result)
+{
+	if (first != last)
+	{
+		do
+		{
+			_Construct(result, *first);
+			++first;
+			++result;
+		}
+		while (first != last);
+	}
+	return result;
+}
+
+template <class Type>
+__forceinline Type *uninitialized_fill_n(Type *result, unsigned int count,
+	const Type &value)
+{
+	for (; count > 0; --count)
+	{
+		_Construct(result, value);
+		++result;
+	}
+	return result;
+}
+
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *position, const Type &value,
+		const __false_type &, unsigned int fillLength, bool atEnd);
+
+	Type *_M_start;
+	Type *_M_finish;
+	Type *_M_end_of_storage;
+};
+
+template <class Type, class Allocator>
+void vector<Type, Allocator>::_M_insert_overflow(
+	Type *position, const Type &value, const __false_type &,
+	unsigned int fillLength, bool atEnd)
+{
+	unsigned int oldSize = (unsigned int)(_M_finish - _M_start);
+	const unsigned int &growth = oldSize < fillLength ? fillLength : oldSize;
+	unsigned int length = growth + oldSize;
+
+	Type *newStart;
+	if (length)
+	{
+		unsigned int bytes = length * sizeof(Type);
+		if (bytes > 128)
+			newStart = (Type *)vectorLargeAllocate(bytes);
+		else
+			newStart = (Type *)vectorSmallAllocate(bytes);
+	}
+	else
+	{
+		newStart = 0;
+	}
+
+	Type *newFinish = uninitialized_copy(_M_start, position, newStart);
+
+	if (fillLength == 1)
+	{
+		_Construct(newFinish, value);
+		++newFinish;
+	}
+	else
+	{
+		newFinish = uninitialized_fill_n(newFinish, fillLength, value);
+	}
+
+	if (!atEnd)
+		newFinish = uninitialized_copy(position, _M_finish, newFinish);
+
+	if (_M_start)
+	{
+		unsigned int bytes = (unsigned int)(_M_end_of_storage - _M_start)
+			* sizeof(Type);
+		if (bytes > 128)
+			vectorLargeDeallocate(_M_start);
+		else
+			vectorSmallDeallocate(_M_start, bytes);
+	}
+
+	_M_finish = newFinish;
+	_M_start = newStart;
+	_M_end_of_storage = newStart + length;
+}
+
+template class vector<Coord3D, allocator<Coord3D> >;
+}
