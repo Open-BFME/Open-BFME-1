f4718450e1e01fde3ff5d6332db18b3a23a5316f
TheThag <zackgreco@hotmail.com>
2026-09-10 15:56:35 -0300

    Convert geometry major-radius accessor

---
+++ Code/GameEngine/Source/GameLogic/Object/BfmeGeometryInfo_boxRadii.cpp
@@ -0,0 +1,22 @@
+// cl: /O2 /Ob2 /G6
+// BFME's box geometry accessors expose the major and minor radii at
+// this+0x24 and this+0x28 respectively.
+
+class BfmeGeometryInfo
+{
+public:
+	float boxMajorRadius(void) const;
+	float boxMinorRadius(void) const;
+
+private:
+	unsigned char m_prefix[0x24];
+	float m_majorRadius;
+	float m_minorRadius;
+};
+
+// ?boxMajorRadius@BfmeGeometryInfo@@QBEMXZ
+float BfmeGeometryInfo::boxMajorRadius(void) const
+{
+	return m_majorRadius;
+}
+
