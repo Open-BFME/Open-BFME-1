8f6f61af45cbbf0c9cd0f4225f13554a2dd3bbf2
TheThag <zackgreco@hotmail.com>
2026-09-10 13:54:19 -0300

    Convert lexer failure cleanup through its complete retail boundary

--- Code/GameEngine/Source/Common/BfmeConv2008.cpp
+++ Code/GameEngine/Source/Common/BfmeConv2008.cpp
@@ -6,7 +6,10 @@ public:
 	int bfmeFailEAN(int code);
 
 	char *m_bfmePosEAN;
-	unsigned char m_bfmeHeadEAN[0x10];
+	char *m_bfmeLineEAN;
+	char *m_bfmeSourceEAN;
+	int m_bfmeLineNumberEAN;
+	int m_bfmeTagEAN;
 	char *m_bfmeBufEAN;
 	int m_bfmeLimitEAN;
 	char *m_bfmeTailEAN;
@@ -54,6 +57,40 @@ char BfmeLexEAN::bfmeExpandEAN(char *out)
 	}
 
 	return 0;
+}
+
+extern "C" __declspec(dllimport) char *__cdecl strncpy(
+	char *destination, const char *source, unsigned int count);
+
+int BfmeLexEAN::bfmeFailEAN(int code)
+{
+	BfmeLexEAN *self = this;
+	char line[0x104];
+	int length = 0;
+
+	line[0] = 0;
+	if (self->m_bfmeSourceEAN != 0)
+	{
+		char *cursor = self->m_bfmeSourceEAN;
+		while (*cursor != 0 && *cursor != '\n')
+		{
+			++length;
+			++cursor;
+		}
+
+		if (length != 0)
+		{
+			if (length >= 0x104)
+				length = 0x103;
+			strncpy(line, self->m_bfmeSourceEAN, length);
+		}
+	}
+
+	self->m_bfmePosEAN = 0;
+	self->m_bfmeLineNumberEAN = 0;
+	self->m_bfmeSourceEAN = 0;
+	self->m_bfmeLineEAN = 0;
+	return code;
 }
 
 int BfmeLexEAN::bfmeScanEAN()
