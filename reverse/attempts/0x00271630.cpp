c12cd1645a66892bf8a7530685825a6a59aaa898
Gabe Borges <gbborges@proton.me>
2026-09-10 13:23:41 -0400

    chore(reverse): bank privateMoveToObject near miss
    AIUpdateInterface::privateMoveToObject matches the retail method order and the +0x08, +0x30, and +0x48 fields, while the probe isolates six vtable register bytes. The banked body lives at reverse/attempts/0x00271630.cpp so another worker can continue the register allocation investigation.

--- reverse/attempts/0x00271630.cpp
+++ reverse/attempts/0x00271630.cpp
@@ -1,5 +1,14 @@
-// ?d_00271630@@YAXXZ
+// ?privateMoveToObject@AIUpdateInterface@@MAEXPAVObject@@W4CommandSourceType@@@Z
 // partial score=0.92 date=2026-09-04
+class Object;
+
+enum CommandSourceType
+{
+	CMD_FROM_PLAYER = 0,
+	CMD_FROM_SCRIPT = 1,
+	CMD_FROM_AI = 2
+};
+
 class BfmeHostHA
 {
 public:
@@ -20,35 +29,96 @@ public:
 	virtual void bfmeAimHA(void *who) = 0;      // +0x38 slot 14
 };
 
-class BfmeGlobHA
+class BFMEActionManager
 {
 public:
-	unsigned char bfmeAskHA(void *obj, void *a, void *b);
+	bool canEnterObject(const Object *obj, const Object *objectToMoveTo,
+		CommandSourceType commandSource);
 };
 
-extern BfmeGlobHA *g_bfmeGlobHA;
+extern BFMEActionManager *TheActionManager;
 
-class BfmeThingHA
+class BFMEGoalObjectMachine
 {
 public:
-	void bfmeGoHA(void *a, void *b);
-	static BfmeGlobHA *globHA(void) { return g_bfmeGlobHA; }
-
-private:
-	unsigned char m_bfmePad00[8];
-	void *m_bfmeObject;                 // +0x08
-	unsigned char m_bfmePad0C[0x24];    // +0x0C
-	BfmeHostHA *m_bfmeHost;
-	unsigned char m_bfmePad34[0x14];    // +0x34
-	void *m_bfmeSaved;                  // +0x48
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0C() = 0;
+	virtual void slot10() = 0;
+	virtual void clear() = 0;
+	virtual void slot18() = 0;
+	virtual void slot1C() = 0;
+	virtual void setState(unsigned int state) = 0;
+	virtual void slot24() = 0;
+	virtual void slot28() = 0;
+	virtual void slot2C() = 0;
+	virtual void slot30() = 0;
+	virtual void slot34() = 0;
+	virtual void setGoalObject(const Object *object) = 0;
 };
 
-void BfmeThingHA::bfmeGoHA(void *a, void *b)
+struct BFMEAIUpdateFields
 {
-	if (!globHA()->bfmeAskHA(m_bfmeObject, a, b))
+	char m_beforeObject[8];
+	Object *m_object;
+	char m_beforeMachine[0x30 - 0x0C];
+	BFMEGoalObjectMachine *m_stateMachine;
+	char m_beforeSource[0x48 - 0x34];
+	CommandSourceType m_lastCommandSource;
+};
+
+class UpdateModule
+{
+	public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0C() = 0;
+	virtual void slot10() = 0;
+	virtual void slot14() = 0;
+	virtual void slot18() = 0;
+	virtual void slot1C() = 0;
+	virtual void slot20() = 0;
+	virtual void slot24() = 0;
+	virtual void slot28() = 0;
+
+	protected:
+	unsigned char m_bfmePad04[4];
+	Object *m_bfmeObject;
+	unsigned char m_bfmePad0C[0x20 - 0x0C];
+};
+
+class AICommandInterface
+{
+	public:
+	virtual void aiDoCommand() = 0;
+};
+
+class AIUpdateInterface : public UpdateModule, public AICommandInterface
+{
+	protected:
+	virtual void privateMoveToObject(Object *obj, CommandSourceType cmdSource);
+
+	private:
+	BFMEGoalObjectMachine *getMachine() const
+	{
+		return m_bfmeStateMachine;
+	}
+
+	unsigned char m_bfmePad24[0x30 - 0x24];
+	BFMEGoalObjectMachine *m_bfmeStateMachine;
+	unsigned char m_bfmePad34[0x48 - 0x34];
+	CommandSourceType m_bfmeLastCommandSource;
+};
+
+void AIUpdateInterface::privateMoveToObject(Object *obj, CommandSourceType cmdSource)
+{
+	const bool accepted = TheActionManager->canEnterObject(m_bfmeObject, obj, cmdSource);
+	if (!accepted)
 		return;
-	m_bfmeHost->bfmePrepHA();
-	m_bfmeHost->bfmeAimHA(a);
-	m_bfmeSaved = b;
-	m_bfmeHost->bfmePulseHA(0x3c);
+	getMachine()->clear();
+	getMachine()->setGoalObject(obj);
+	m_bfmeLastCommandSource = cmdSource;
+	getMachine()->setState(0x3c);
 }
