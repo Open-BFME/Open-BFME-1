3c11d2e65319917419008bdca30a5bc0ebcf0eef
TheThag <zackgreco@hotmail.com>
2026-09-10 09:40:16 -0300

    Bank measured router-fallback and audio-resume alternatives

--- reverse/attempts/0x006a8970.cpp
+++ reverse/attempts/0x006a8970.cpp
@@ -1,9 +1,12 @@
 // ?resumeAudio@MilesAudioManager@@UAEXIHH@Z
-// partial score=0.3 date=2026-09-10
+// partial score=0.55 date=2026-09-10
 // cl: /O2 /Ob1 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB
 // stlport
 //
-// Scratch reconstruction of the BFME MilesAudioManager resumeAudio body.
+// BFME's MilesAudioManager::resumeAudio is the vtable slot at
+// 0x0111C0C0 + 0x30.  The BFME caller view in
+// GameClient/GUI/GUICallbacks/Menus/BfmeGameLogicPause_setGamePaused.cpp
+// proves the three-argument ABI: (which, affectMask, clearStates).
 
 #include <list>
 
@@ -19,24 +22,12 @@ extern "C" __declspec(dllimport) long __stdcall InterlockedIncrement(
 extern "C" __declspec(dllimport) long __stdcall InterlockedDecrement(
 	long volatile *value);
 
-
-enum AudioAffect
-{
-	AudioAffect_Music = 0x01,
-	AudioAffect_Sound = 0x02,
-	AudioAffect_Sound3D = 0x04,
-	AudioAffect_Speech = 0x08,
-	AudioAffect_AmbientStream = 0x10,
-	AudioAffect_SystemSetting = 0x20
-};
-
-enum PlayingAudioType
-{
-	PAT_Sample,
-	PAT_3DSample,
-	PAT_Stream,
-	PAT_Invalid
-};
+// These are the retail ILT entries used by the body.  They are direct calls
+// to existing addresses, not alternate-name substitutions or local stubs.
+class PlayingAudioRef;
+extern void j_0002e668(void);
+extern void j_0000e04d(void);
+extern void j_00015a69(void);
 
 enum PlayingStatus
 {
@@ -50,11 +41,15 @@ class AudioEventRTS
 public:
 	unsigned int getSoundClass(void) const;
 
-	private:
-	char m_pad00[0x28];
+	unsigned int getTimeOfDay(void) const
+	{
+		return m_timeOfDay;
+	}
 
-	public:
-	int m_timeOfDay;
+private:
+	void *m_vftable;
+	char m_pad004[0x28 - 4];
+	unsigned int m_timeOfDay;
 };
 
 class RefCountedPlayingAudio
@@ -81,12 +76,14 @@ class PlayingAudio : public RefCountedPlayingAudio
 {
 public:
 	void *m_milesHandle;
-	PlayingAudioType m_type;
+	int m_type;
 	volatile PlayingStatus m_status;
 	AudioEventRTS *m_audioEventRTS;
-	char m_pad18[0x21];
-	unsigned char m_pauseFlags[4];
-	char m_pad3d[3];
+	char m_pad018[0x39 - 0x18];
+	bool m_flag39;
+	bool m_flag3a;
+	bool m_flag3b;
+	bool m_flag3c;
 };
 
 class PlayingAudioRef
@@ -150,17 +147,16 @@ typedef _STL::list<PlayingAudioRef> PlayingAudioList;
 class MilesAudioManager
 {
 public:
-	virtual void resumeAudio(unsigned int which, int mask, int clear);
-	void releasePlayingAudioRef(PlayingAudioRef *playing);
-	void updateAudioState(unsigned int which, int mask, int clear);
+	virtual void resumeAudio(unsigned int which, int affectMask, int clearStates);
 
 private:
 	char m_pad004[0x61c - 4];
-	unsigned int m_suppressedAudio;
-	unsigned int m_systemSuppressedAudio;
+	unsigned int m_audioAffects;
+	unsigned int m_systemAffects;
 	char m_pad624[0x63c - 0x624];
 	unsigned int m_audioStates[3];
-	char m_pad648[0x95c - 0x648];
+	unsigned int m_systemStates[3];
+	char m_pad654[0x95c - 0x654];
 	void *m_mutex;
 	char m_pad960[0x9c8 - 0x960];
 	PlayingAudioList m_playingSounds;
@@ -168,32 +164,33 @@ private:
 	PlayingAudioList m_playingStreams;
 };
 
-#pragma comment(linker, "/alternatename:?releasePlayingAudioRef@MilesAudioManager@@QAEXPAVPlayingAudioRef@@@Z=?j_0002e668@@YAXXZ")
-#pragma comment(linker, "/alternatename:?updateAudioState@MilesAudioManager@@QAEXIHH@Z=?j_0000e04d@@YAXXZ")
+typedef void (MilesAudioManager::*Resume3D)(PlayingAudioRef *);
+typedef void (MilesAudioManager::*UpdateAudioState)(unsigned int, unsigned int, int);
 
-void MilesAudioManager::resumeAudio(unsigned int which, int mask, int clear)
+void MilesAudioManager::resumeAudio(unsigned int which, int affectMask, int clearStates)
 {
-	MilesAudioScopedMutex guard(m_mutex);
+	MilesAudioManager *manager = this;
+	MilesAudioScopedMutex guard(manager->m_mutex);
 	PlayingAudioList::iterator it;
 	PlayingAudioRef playing;
 
-	if (which & AudioAffect_Sound)
+	if (which & 2)
 	{
 		for (it = m_playingSounds.begin(); it != m_playingSounds.end(); ++it)
 		{
 			playing = *it;
 			if (playing)
 			{
-				int timeOfDay = playing->m_audioEventRTS->m_timeOfDay;
-				if (mask & (1 << timeOfDay))
+				AudioEventRTS *event = playing->m_audioEventRTS;
+				if (affectMask & (1 << event->getTimeOfDay()))
 				{
-					if (which & AudioAffect_SystemSetting)
-						playing->m_pauseFlags[1] = 0;
+					if (which & 0x20)
+						playing->m_flag3a = false;
 					else
-						playing->m_pauseFlags[0] = 0;
+						playing->m_flag39 = false;
 
-					if (playing->m_pauseFlags[0] || playing->m_pauseFlags[1] ||
-						playing->m_pauseFlags[2] || playing->m_pauseFlags[3])
+					if (playing->m_flag39 || playing->m_flag3a ||
+						playing->m_flag3b || playing->m_flag3c)
 						AIL_stop_sample(playing->m_milesHandle);
 					else
 						AIL_resume_sample(playing->m_milesHandle);
@@ -202,68 +199,94 @@ void MilesAudioManager::resumeAudio(unsigned int which, int mask, int clear)
 		}
 	}
 
-	if (which & AudioAffect_Sound3D)
+	if (which & 4)
 	{
 		for (it = m_playing3DSounds.begin(); it != m_playing3DSounds.end(); ++it)
 		{
 			playing = *it;
 			if (playing)
 			{
-				int timeOfDay = playing->m_audioEventRTS->m_timeOfDay;
-				if (mask & (1 << timeOfDay))
+				AudioEventRTS *event = playing->m_audioEventRTS;
+				if (affectMask & (1 << event->getTimeOfDay()))
 				{
-					if (which & AudioAffect_SystemSetting)
-						playing->m_pauseFlags[1] = 0;
+					if (which & 0x20)
+						playing->m_flag3a = false;
 					else
-						playing->m_pauseFlags[0] = 0;
-					releasePlayingAudioRef(&playing);
+						playing->m_flag39 = false;
+
+					Resume3D resume3D;
+					union
+					{
+						void (*asFunction)(void);
+						Resume3D asMember;
+					} call;
+					call.asFunction = j_0002e668;
+					(reinterpret_cast<MilesAudioManager *>(this)->*call.asMember)(&playing);
 				}
 			}
 		}
 	}
 
-	if (which & AudioAffect_AmbientStream)
+	for (it = m_playingStreams.begin(); it != m_playingStreams.end(); ++it)
 	{
-		for (it = m_playingStreams.begin(); it != m_playingStreams.end(); ++it)
+		playing = *it;
+		if (playing)
 		{
-			playing = *it;
-			if (playing && (mask & (1 << playing->m_audioEventRTS->m_timeOfDay)) &&
-				(which & playing->m_audioEventRTS->getSoundClass()))
+			AudioEventRTS *event = playing->m_audioEventRTS;
+			if (affectMask & (1 << event->getTimeOfDay()))
 			{
-				if (which & AudioAffect_SystemSetting)
-					playing->m_pauseFlags[1] = 0;
-				else
-					playing->m_pauseFlags[0] = 0;
-				if (playing->m_pauseFlags[0] || playing->m_pauseFlags[1] ||
-					playing->m_pauseFlags[2] || playing->m_pauseFlags[3])
-					AIL_pause_stream(playing->m_milesHandle, 1);
-				else
-					AIL_pause_stream(playing->m_milesHandle, 0);
+				union
+				{
+					void (*asFunction)(void);
+					unsigned int (AudioEventRTS::*asMember)(void) const;
+				} getSoundClass;
+				getSoundClass.asFunction = j_00015a69;
+				if (which & (event->*getSoundClass.asMember)())
+				{
+					if (which & 0x20)
+						playing->m_flag3a = false;
+					else
+						playing->m_flag39 = false;
+
+					bool pause = playing->m_flag39 || playing->m_flag3a ||
+						playing->m_flag3b || playing->m_flag3c;
+					AIL_pause_stream(playing->m_milesHandle, pause ? 1 : 0);
+				}
 			}
 		}
 	}
 
-	if (which & AudioAffect_AmbientStream)
+	if (which & 0x10)
 	{
-		if (which & AudioAffect_SystemSetting)
-			m_systemSuppressedAudio &= ~mask;
+		if (which & 0x20)
+			m_systemAffects &= ~static_cast<unsigned int>(affectMask);
 		else
-			m_suppressedAudio &= ~mask;
-		updateAudioState(which, mask, clear);
+			m_audioAffects &= ~static_cast<unsigned int>(affectMask);
+	}
 
-		if (!clear)
+	union
+	{
+		void (*asFunction)(void);
+		UpdateAudioState asMember;
+	} call;
+	call.asFunction = j_0000e04d;
+	(manager->*call.asMember)(which, affectMask, 0);
+
+	if (!clearStates)
+	{
+		unsigned int *audioState = m_audioStates;
+		unsigned int *systemState = m_systemStates;
+		for (unsigned int i = 0; i < 3; ++i)
 		{
-			unsigned int *state = m_audioStates;
-			for (int i = 0; i < 3; ++i, ++state)
+			if (!(affectMask & (1 << i)))
 			{
-				if (mask & (1 << i))
-				{
-					if (which & AudioAffect_SystemSetting)
-						state[3] &= ~(which & ~AudioAffect_SystemSetting);
-					else
-						*state &= ~which;
-				}
+				if (which & 0x20)
+					*systemState &= ~(which & ~0x20u);
+				else
+					*audioState &= ~which;
 			}
+			++audioState;
+			++systemState;
 		}
 	}
 }
