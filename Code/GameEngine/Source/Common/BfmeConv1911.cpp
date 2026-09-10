e38d0ea228fd19393b66c7d78a019a14e93e1987
Gabe Borges <gbborges@proton.me>
2026-09-10 16:11:27 -0400

    feat(gamespy): convert BfmeHostBG constructor
    The retail call at 0x006F9CD0 constructs the local object at 0x006F81E0 and passes it to BfmeHostBG::bfmeSaveBG at 0x006F87D0, which proves the identity.
    The clean constructor sits in Code/GameEngine/Source/Common/BfmeConv1911.cpp, and _ReadWriteBarrier preserves the retail placement of the shared 1.0f load.

--- Code/GameEngine/Source/Common/BfmeConv1911.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1911.cpp
@@ -41,10 +41,56 @@ public:
 
 extern "C" void __cdecl bfmeXferOneBG(BfmeAgentBG *ag, void *dst);
 extern "C" void __cdecl bfmeXferTwoBG(BfmeAgentBG *ag, void *dst);
+extern "C" void _ReadWriteBarrier(void);
+#pragma intrinsic(_ReadWriteBarrier)
+
+struct BfmeHostBGInitFields
+{
+	unsigned int m_field00;
+	unsigned int m_field04;
+	unsigned int m_field08;
+	float m_scale0C;
+	unsigned char m_padding10[0x10];
+	unsigned int m_field20;
+	unsigned int m_field24;
+	unsigned int m_field28;
+	unsigned int m_field2C;
+	unsigned int m_field30;
+	unsigned int m_field34;
+	unsigned int m_field38;
+	unsigned int m_field3C;
+	unsigned int m_field40;
+	unsigned int m_field44;
+	unsigned int m_field48;
+	float m_scale4C;
+	unsigned int m_field50;
+	unsigned int m_field54;
+	unsigned int m_field58;
+	unsigned int m_field5C;
+	float m_scale60;
+	unsigned int m_field64;
+	unsigned int m_field68;
+	unsigned int m_field6C;
+	unsigned int m_field70;
+	float m_scale74;
+	unsigned int m_field78;
+	unsigned char m_field7C;
+	unsigned char m_field7D;
+	unsigned char m_field7E;
+	unsigned char m_field7F;
+	float m_scale80;
+	unsigned int m_field84;
+	unsigned int m_field88;
+	unsigned int m_field8C;
+	unsigned int m_field90;
+	unsigned char m_field94;
+	unsigned char m_field95;
+};
 
 class BfmeHostBG
 {
 public:
+	BfmeHostBG(void);
 	void bfmeSaveBG(BfmeAgentBG *ag);
 
 	unsigned char m_bfmeHeadBG[0x48];
@@ -62,6 +108,49 @@ public:
 	unsigned char m_bfmeSlotJBG[4];
 	unsigned char m_bfmeSlotKBG;
 };
+
+BfmeHostBG::BfmeHostBG(void)
+{
+	BfmeHostBGInitFields *fields = reinterpret_cast<BfmeHostBGInitFields *>(this);
+	fields->m_field00 = 0;
+	fields->m_field04 = 0;
+	fields->m_field08 = 0;
+	_ReadWriteBarrier();
+	fields->m_scale0C = 1.0f;
+	fields->m_field20 = 0;
+	fields->m_field24 = 0;
+	fields->m_field28 = 0;
+	fields->m_field2C = 0;
+	fields->m_field30 = 0;
+	fields->m_field34 = 0;
+	fields->m_field38 = 0;
+	fields->m_field3C = 0;
+	fields->m_field40 = 0;
+	fields->m_field44 = 0;
+	fields->m_field48 = 0;
+	fields->m_scale4C = 1.0f;
+	fields->m_field50 = 0;
+	fields->m_field54 = 0;
+	fields->m_field58 = 0;
+	fields->m_field5C = 0;
+	fields->m_scale60 = 1.0f;
+	fields->m_field64 = 0;
+	fields->m_field68 = 0;
+	fields->m_field6C = 0;
+	fields->m_field70 = 0;
+	fields->m_scale74 = 1.0f;
+	fields->m_field78 = 0;
+	fields->m_field7C = 0;
+	fields->m_field7D = 0;
+	fields->m_field7E = 0;
+	fields->m_scale80 = 1.0f;
+	fields->m_field84 = 0;
+	fields->m_field88 = 0;
+	fields->m_field8C = 0;
+	fields->m_field90 = 0;
+	fields->m_field94 = 0;
+	fields->m_field95 = 0;
+}
 
 void BfmeHostBG::bfmeSaveBG(BfmeAgentBG *ag)
 {
