a302ebd065d1367c9e73af726fbe9a65484f1004
TheThag <zackgreco@hotmail.com>
2026-09-10 14:59:02 -0300

    Convert string-record advance with typed name pointer

--- Code/GameEngine/Source/Common/BfmeConv1700.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1700.cpp
@@ -2,16 +2,60 @@ struct BfmeEntryFQ
 {
 	unsigned char m_bfmeHeadFQ[0xe];
 	short m_bfmeShortFQ;
-	unsigned char m_bfmeTailFQ[4];
+	struct BfmeNodeFQ *m_bfmeNodesFQ;
+};
+
+struct BfmeNodeFQ
+{
+	BfmeNodeFQ *m_bfmeNextFQ;
+};
+
+class AsciiString;
+
+class BfmeTableFQ;
+
+class Rva0035D2B0StringRecordTable
+{
+	friend class BfmeTableFQ;
+
+	int findOrCreateIndex(AsciiString *name);
+
+public:
+	void release(int index);
 };
 
 class BfmeTableFQ
 {
 public:
-	int bfmeAdvanceFQ(int index, int arg);
+	int bfmeAdvanceFQ(int index, AsciiString *name);
 
-	unsigned char m_bfmeDataFQ[4];
+	unsigned char m_bfmeDataFQ[0x0c];
+	BfmeEntryFQ *m_bfmeEntriesFQ;
 };
+
+int BfmeTableFQ::bfmeAdvanceFQ(int index, AsciiString *name)
+{
+	int next = reinterpret_cast<Rva0035D2B0StringRecordTable *>(this)->
+		findOrCreateIndex(name);
+	if (next == -1)
+		return index;
+
+	BfmeNodeFQ *node;
+	{
+		BfmeEntryFQ *entries = m_bfmeEntriesFQ;
+		BfmeEntryFQ *source = &entries[index];
+		node = source->m_bfmeNodesFQ;
+		source->m_bfmeNodesFQ = node->m_bfmeNextFQ;
+	}
+	{
+		BfmeEntryFQ *entries = m_bfmeEntriesFQ;
+		BfmeEntryFQ *destination = &entries[next];
+		node->m_bfmeNextFQ = destination->m_bfmeNodesFQ;
+		destination->m_bfmeNodesFQ = node;
+	}
+	reinterpret_cast<Rva0035D2B0StringRecordTable *>(this)->release(index);
+	return next;
+}
 
 class BfmeStateFQ
 {
@@ -28,16 +72,14 @@ public:
 
 	unsigned char m_bfmeHeadFQ[0x2c];
 	BfmeTableFQ m_bfmeTableFQ;
-	unsigned char m_bfmeMidFQ[8];
-	BfmeEntryFQ *m_bfmeEntriesFQ;
 };
 
 void BfmeThingFQ::bfmeStepFQ(BfmeStateFQ *state, int arg)
 {
 	int current = state->m_bfmeIndexFQ;
-	int index = m_bfmeTableFQ.bfmeAdvanceFQ(current, arg);
+	int index = m_bfmeTableFQ.bfmeAdvanceFQ(current, reinterpret_cast<AsciiString *>(arg));
 	state->m_bfmeIndexFQ = index;
-	state->m_bfmeValueFQ = m_bfmeEntriesFQ[index].m_bfmeShortFQ;
+	state->m_bfmeValueFQ = m_bfmeTableFQ.m_bfmeEntriesFQ[index].m_bfmeShortFQ;
 }
 
 class BfmeTableFR
@@ -45,7 +87,8 @@ class BfmeTableFR
 public:
 	int bfmeAdvanceFR(int index, int arg);
 
-	unsigned char m_bfmeDataFR[4];
+	unsigned char m_bfmeDataFR[0x0c];
+	BfmeEntryFQ *m_bfmeEntriesFR;
 };
 
 class BfmeThingFR
@@ -55,8 +98,6 @@ public:
 
 	unsigned char m_bfmeHeadFR[0xc];
 	BfmeTableFR m_bfmeTableFR;
-	unsigned char m_bfmeMidFR[8];
-	BfmeEntryFQ *m_bfmeEntriesFR;
 };
 
 void BfmeThingFR::bfmeStepFR(BfmeStateFQ *state, int arg)
@@ -64,5 +105,5 @@ void BfmeThingFR::bfmeStepFR(BfmeStateFQ *state, int arg)
 	int current = state->m_bfmeIndexFQ;
 	int index = m_bfmeTableFR.bfmeAdvanceFR(current, arg);
 	state->m_bfmeIndexFQ = index;
-	state->m_bfmeValueFQ = m_bfmeEntriesFR[index].m_bfmeShortFQ;
+	state->m_bfmeValueFQ = m_bfmeTableFR.m_bfmeEntriesFR[index].m_bfmeShortFQ;
 }
