903f6320fe540f74b48c46356a85b47c6542a4b1
Jacob Starheim <74994007+JacobStarheim@users.noreply.github.com>
2026-09-11 17:36:08 +0200

    Recover HashMap find ownership (71 authored bytes)

--- Code/Libraries/Source/WWVegas/WW3D2/HashMapPeekValue.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/HashMapPeekValue.cpp
@@ -1,31 +1,18 @@
-// cl: /DNDEBUG /MD /EHsc
-// Retail 0x008FF850: hash_map find wrapper returning mapped pointer or null.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+// Retail 0x008FF850: hash map find wrapper returning mapped pointer or null.
 
-struct HashNode008FF850
-{
-	char pad[0x14];
-	void *value;
-};
+#include <map>
 
-struct HashIter008FF850
-{
-	HashNode008FF850 *node;
-	HashIter008FF850(HashNode008FF850 *n = 0) : node(n) {}
-};
-
-class HashMap008FF850
-{
-public:
-	HashNode008FF850 *end_node;
-	HashIter008FF850 find(const int &key);
-};
+typedef _STL::map<int, void *, _STL::less<unsigned int> > HashMap008FF850;
+typedef HashMap008FF850::iterator HashIter008FF850;
 
 extern HashMap008FF850 g_map008FF850;
 
 void *PeekHashMapValue008FF850(int key)
 {
 	HashIter008FF850 it = g_map008FF850.find(key);
-	if (it.node == g_map008FF850.end_node)
+	if (it == g_map008FF850.end())
 		return 0;
-	return it.node->value;
+	return (*it).second;
 }
