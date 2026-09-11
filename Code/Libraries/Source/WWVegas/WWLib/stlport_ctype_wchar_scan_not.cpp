f277794b3c160830eb090946b4108eb8cc2029dd
TheThag <zackgreco@hotmail.com>
2026-09-11 07:51:41 -0300

    feat(stlport): convert wide ctype scan-is

--- Code/Libraries/Source/WWVegas/WWLib/stlport_ctype_wchar_scan_not.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_ctype_wchar_scan_not.cpp
@@ -1,6 +1,6 @@
 // cl: /O2 /EHsc /MD /D_STLP_USE_STATIC_LIB
 // stlport
-// STLport 4.5.3 src/ctype.cpp: ctype<wchar_t>::do_scan_not.
+// STLport 4.5.3 src/ctype.cpp: ctype<wchar_t>::do_scan_is/do_scan_not.
 
 #include <algorithm>
 #include <functional>
@@ -23,6 +23,13 @@ struct _Ctype_w_is_mask : public unary_function<wchar_t, bool>
 		return c >= 0 && size_t(c) < ctype<char>::table_size && (table[c] & M);
 	}
 };
+
+const wchar_t *ctype<wchar_t>::do_scan_is(ctype_base::mask m,
+	const wchar_t *low, const wchar_t *high) const
+{
+	return find_if(low, high,
+		_Ctype_w_is_mask(m, ctype<char>::classic_table()));
+}
 
 const wchar_t *ctype<wchar_t>::do_scan_not(ctype_base::mask m,
 	const wchar_t *low, const wchar_t *high) const
