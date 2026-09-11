33e7abe45f318bf3641d99a45dfcd0904c3eab1b
Gabe Borges <gbborges@proton.me>
2026-09-11 10:11:12 -0400

    feat(common): convert BfmeN1238 size accessor
    The 133 retail bytes at 0x008C4380 match BfmeN1238's two kind-flag arms. The pinned bfmeGet1238 and Rva008BD1B0Node::count callees confirm the identity. The clean C++ body lives in Code/GameEngine/Source/Common/BfmeConv1238.cpp because that file already owns the BfmeN1238 layout and related 1238 conversions.

--- Code/GameEngine/Source/Common/BfmeConv1238.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1238.cpp
@@ -29,16 +29,67 @@ public:
 	BfmeT1238 m_bfme24;
 };
 
+class Rva008BD1B0Node
+{
+public:
+	int count() const;
+};
+
 class BfmeN1238
 {
 public:
 	BfmeS1238 *bfmeAt1238(int i);
+	int bfmeSize1237() const;
 	unsigned m_bfme00;
 	unsigned m_bfme04;
 	char m_bfmePad08[0x4c - 0x08];
 	BfmeS1238 *m_bfme4c;
 	BfmeQ1238 *m_bfme50;
 };
+
+// ?bfmeSize1237@BfmeN1238@@QBEHXZ
+int BfmeN1238::bfmeSize1237() const
+{
+	BfmeN1238 *that = (BfmeN1238 *)this;
+	unsigned flags = that->m_bfme04;
+	BfmeS1238 *s;
+	BfmeQ1238 *q;
+	int n;
+
+	if ((flags & 0x3f) == 0x13 && !((unsigned char)(~(flags >> 15)) & 1))
+	{
+		s = 0;
+	}
+	else
+	{
+		q = that->m_bfme50;
+		if (q == 0 || q == (BfmeQ1238 *)0xbaadf00d)
+			s = 0;
+		else
+			s = q->m_bfme10;
+	}
+
+	n = 1;
+	if (s)
+		n = s->bfmeLen1238() + 1;
+
+	flags = that->m_bfme04;
+	if ((flags & 0x3f) == 0x0d)
+	{
+		if (!((unsigned char)(~(flags >> 15)) & 1))
+			goto add_count;
+	}
+	if ((flags & 0x3f) != 0x12)
+		return n;
+	if (((unsigned char)(~(flags >> 15)) & 1))
+		return n;
+
+add_count:
+	q = that->m_bfme50;
+	if (q != 0)
+		n += ((Rva008BD1B0Node *)q->m_bfme24.bfmeGet1238())->count();
+	return n;
+}
 
 BfmeS1238 *BfmeN1238::bfmeAt1238(int i)
 {
