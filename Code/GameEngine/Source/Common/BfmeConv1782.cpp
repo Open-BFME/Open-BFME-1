70095b56ed4d52009bb55f5326d2fb33bfbbe223
Gabe Borges <gbborges@proton.me>
2026-09-11 10:30:47 -0400

    feat(common): convert ClientRoot4120 resolution helper
    The 212 retail bytes at 0x004331D0 match ClientRoot4120::bfmeCloseHV, which BfmeOwnerHV::bfmeEndHV calls at 0x0051D300 when the HV flag is set. The Resolution and %d %d string references confirm the preference update. The clean body lives in Code/GameEngine/Source/Common/BfmeConv1782.cpp beside the named caller and its related HV declarations.

--- Code/GameEngine/Source/Common/BfmeConv1782.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1782.cpp
@@ -1,8 +1,92 @@
+struct Rva006C9270GlobalData1782
+{
+	unsigned char m_pad[0x2c];
+	int m_xResolution;
+	int m_yResolution;
+};
+
+extern Rva006C9270GlobalData1782 *TheWritableGlobalData;
+
+template <class T> class StringBase
+{
+	friend class AsciiString;
+
+private:
+	StringBase(void) : m_data(0) {}
+	StringBase(const T *text);
+	StringBase(const StringBase<T> &other);
+	~StringBase(void);
+
+	void set(const StringBase<T> &other);
+	void *m_data;
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	AsciiString(void) : StringBase<char>() {}
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	AsciiString(const AsciiString &other) : StringBase<char>(other) {}
+	~AsciiString(void);
+
+	void format(AsciiString format, ...);
+	AsciiString &operator=(const AsciiString &other)
+	{
+		StringBase<char>::set(other);
+		return *this;
+	}
+};
+
+namespace _STL
+{
+template <class T> struct less {};
+template <class T> class allocator {};
+template <class First, class Second> struct pair {};
+
+template <class Key, class Value, class Compare = less<Key>,
+	class Allocator = allocator<pair<const Key, Value> > >
+class map
+{
+public:
+	Value &operator[](const Key &key);
+
+private:
+	void *m_header;
+	unsigned int m_size;
+	unsigned int m_allocator;
+};
+}
+
+typedef _STL::map<AsciiString, AsciiString, _STL::less<AsciiString>,
+	_STL::allocator<_STL::pair<const AsciiString, AsciiString> > >
+	PreferenceMap;
+
+class UserPreferences : public PreferenceMap
+{
+public:
+	virtual ~UserPreferences(void);
+	virtual bool load(AsciiString filename);
+	virtual bool write(void);
+
+private:
+	AsciiString m_filename;
+};
+
+class OptionPreferences : public UserPreferences
+{
+public:
+	OptionPreferences(void);
+	virtual ~OptionPreferences(void);
+};
+
 class ClientRoot4120
 {
 public:
 	void bfmeCloseHV(void);
 	void bfmeOpenHV(void);
+
+	unsigned char m_pad[0xc6];
+	unsigned char m_resolutionChangePending;
 };
 
 extern ClientRoot4120 *TheGameClient;
@@ -13,6 +97,18 @@ struct Rva005A63D0Mouse
 };
 
 extern Rva005A63D0Mouse *TheMouse;
+
+void ClientRoot4120::bfmeCloseHV(void)
+{
+	m_resolutionChangePending = 0;
+
+	OptionPreferences optionPreferences;
+	AsciiString value;
+	value.format("%d %d", TheWritableGlobalData->m_xResolution,
+		TheWritableGlobalData->m_yResolution);
+	optionPreferences[AsciiString("Resolution")] = value;
+	optionPreferences.write();
+}
 
 class BfmeOwnerHV
 {
