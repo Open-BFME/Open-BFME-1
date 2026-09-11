1a3f470bdc4b483c27ec3ee8f5de30c00d26ca36
Gabe Borges <gbborges@proton.me>
2026-09-11 00:44:37 -0400

    feat(gamelogic): convert BoneFXUpdate update body
    The BoneFXUpdate secondary vtable slot at +0x10 and constructor offsets at +0x2C, +0x62C, and +0x634 prove the identity. The clean body lives in Code/GameEngine/Source/Common/BoneFXUpdate_update_Thunk.cpp, so the former generated assembly becomes real C++ while the verified helper ILTs remain pinned.

--- Code/GameEngine/Source/Common/BoneFXUpdate_update_Thunk.cpp
+++ Code/GameEngine/Source/Common/BoneFXUpdate_update_Thunk.cpp
@@ -1,479 +1,214 @@
-// cl: /DNDEBUG /MD /EHsc
-// readable body of ?update@BoneFXUpdate@@UAE?AW4UpdateSleepTime@@XZ: Code/GameEngine/Source/GameLogic/Object/Update/BoneFXUpdate.cpp
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
-
-enum UpdateSleepTime {};
-class BoneFXUpdate { public: virtual UpdateSleepTime update(); };
 // ?update@BoneFXUpdate@@UAE?AW4UpdateSleepTime@@XZ
-__declspec(naked) UpdateSleepTime BoneFXUpdate::update()
+// clean reconstruction of retail 0x00289580, 467 bytes
+// cl: /DNDEBUG /MD /EHsc
+
+typedef int Int;
+typedef unsigned int UnsignedInt;
+typedef bool Bool;
+
+enum UpdateSleepTime
 {
-	__asm {
-		__emit 0x83
-		__emit 0xec
-		__emit 0x08
-		__emit 0xa1
-		__emit 0x98
-		__emit 0x08
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x53
-		__emit 0x55
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x8b
-		__emit 0x48
-		__emit 0x3c
-		__emit 0x8a
-		__emit 0x86
-		__emit 0x24
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x8b
-		__emit 0x6e
-		__emit 0xf4
-		__emit 0x57
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x75
-		__emit 0x0f
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0xf0
-		__emit 0xe8
-		__emit 0xcb
-		__emit 0x61
-		__emit 0xda
-		__emit 0xff
-		__emit 0xc6
-		__emit 0x86
-		__emit 0x24
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x01
-		__emit 0x33
-		__emit 0xdb
-		__emit 0x8b
-		__emit 0x96
-		__emit 0x1c
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xd3
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x86
-		__emit 0x1c
-		__emit 0x83
-		__emit 0xf9
-		__emit 0xff
-		__emit 0x74
-		__emit 0x65
-		__emit 0x3b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x7f
-		__emit 0x5f
-		__emit 0x8d
-		__emit 0x0c
-		__emit 0x40
-		__emit 0x8d
-		__emit 0x94
-		__emit 0x8e
-		__emit 0x9c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xc0
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x85
-		__emit 0x2c
-		__emit 0x52
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0xf0
-		__emit 0xe8
-		__emit 0x88
-		__emit 0x59
-		__emit 0xda
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x96
-		__emit 0x1c
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x3c
-		__emit 0xd3
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xff
-		__emit 0x8a
-		__emit 0x4c
-		__emit 0x85
-		__emit 0x28
-		__emit 0x84
-		__emit 0xc9
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x85
-		__emit 0x0c
-		__emit 0x74
-		__emit 0x0a
-		__emit 0xc7
-		__emit 0x44
-		__emit 0xbe
-		__emit 0x1c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xeb
-		__emit 0x22
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x98
-		__emit 0x08
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x51
-		__emit 0x3c
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x10
-		__emit 0x89
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0xe8
-		__emit 0xaf
-		__emit 0xf2
-		__emit 0xd8
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x16
-		__emit 0xd8
-		__emit 0x76
-		__emit 0x00
-		__emit 0x03
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x89
-		__emit 0x44
-		__emit 0xbe
-		__emit 0x1c
-		__emit 0x8b
-		__emit 0x86
-		__emit 0x1c
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xc3
-		__emit 0x8b
-		__emit 0x8c
-		__emit 0x86
-		__emit 0x9c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xf9
-		__emit 0xff
-		__emit 0x74
-		__emit 0x74
-		__emit 0x3b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x7f
-		__emit 0x6e
-		__emit 0x8d
-		__emit 0x0c
-		__emit 0x40
-		__emit 0x8d
-		__emit 0x94
-		__emit 0x8e
-		__emit 0x1c
-		__emit 0x03
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xc0
-		__emit 0x8b
-		__emit 0x8c
-		__emit 0x85
-		__emit 0xb0
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x52
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0xf0
-		__emit 0xe8
-		__emit 0x90
-		__emit 0x1b
-		__emit 0xd9
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x96
-		__emit 0x1c
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x3c
-		__emit 0xd3
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xff
-		__emit 0x8a
-		__emit 0x8c
-		__emit 0x85
-		__emit 0xac
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x84
-		__emit 0xc9
-		__emit 0x8d
-		__emit 0x84
-		__emit 0x85
-		__emit 0x90
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x0d
-		__emit 0xc7
-		__emit 0x84
-		__emit 0xbe
-		__emit 0x9c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xeb
-		__emit 0x25
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x98
-		__emit 0x08
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x51
-		__emit 0x3c
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x10
-		__emit 0x89
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0xe8
-		__emit 0x29
-		__emit 0xf2
-		__emit 0xd8
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x90
-		__emit 0xd7
-		__emit 0x76
-		__emit 0x00
-		__emit 0x03
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x89
-		__emit 0x84
-		__emit 0xbe
-		__emit 0x9c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x86
-		__emit 0x1c
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xc3
-		__emit 0x8b
-		__emit 0x8c
-		__emit 0x86
-		__emit 0x1c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xf9
-		__emit 0xff
-		__emit 0x74
-		__emit 0x74
-		__emit 0x3b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x7f
-		__emit 0x6e
-		__emit 0x8d
-		__emit 0x0c
-		__emit 0x40
-		__emit 0x8d
-		__emit 0x94
-		__emit 0x8e
-		__emit 0x9c
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xc0
-		__emit 0x8b
-		__emit 0x8c
-		__emit 0x85
-		__emit 0x34
-		__emit 0x09
-		__emit 0x00
-		__emit 0x00
-		__emit 0x52
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0xf0
-		__emit 0xe8
-		__emit 0x34
-		__emit 0xfd
-		__emit 0xd8
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x96
-		__emit 0x1c
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x3c
-		__emit 0xd3
-		__emit 0x8d
-		__emit 0x04
-		__emit 0xff
-		__emit 0x8a
-		__emit 0x8c
-		__emit 0x85
-		__emit 0x30
-		__emit 0x09
-		__emit 0x00
-		__emit 0x00
-		__emit 0x84
-		__emit 0xc9
-		__emit 0x8d
-		__emit 0x84
-		__emit 0x85
-		__emit 0x14
-		__emit 0x09
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x0d
-		__emit 0xc7
-		__emit 0x84
-		__emit 0xbe
-		__emit 0x1c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xeb
-		__emit 0x25
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x98
-		__emit 0x08
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x51
-		__emit 0x3c
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x04
-		__emit 0x89
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0xe8
-		__emit 0x89
-		__emit 0x40
-		__emit 0xd8
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x07
-		__emit 0xd7
-		__emit 0x76
-		__emit 0x00
-		__emit 0x03
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x89
-		__emit 0x84
-		__emit 0xbe
-		__emit 0x1c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x43
-		__emit 0x83
-		__emit 0xfb
-		__emit 0x08
-		__emit 0x0f
-		__emit 0x8c
-		__emit 0x6d
-		__emit 0xfe
-		__emit 0xff
-		__emit 0xff
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0x5d
-		__emit 0xb8
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5b
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0xc3
+	UPDATE_SLEEP_NONE = 1
+};
+
+struct Coord3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+class FXList;
+class ObjectCreationList;
+class ParticleSystemTemplate;
+
+class AsciiString
+{
+	void *m_data;
+};
+
+class GameClientRandomVariable
+{
+	public:
+	float getValue() const;
+
+	private:
+	Int m_type;
+	float m_low;
+	float m_high;
+};
+
+class GameLogicRandomVariable
+{
+	public:
+	float getValue() const;
+
+	private:
+	Int m_type;
+	float m_low;
+	float m_high;
+};
+
+struct BaseBoneListInfo
+{
+	AsciiString boneName;
+	GameClientRandomVariable gameClientDelay;
+	GameLogicRandomVariable gameLogicDelay;
+	Bool onlyOnce;
+	unsigned char padding[3];
+};
+
+struct BoneFXListInfo : BaseBoneListInfo
+{
+	const FXList *fx;
+};
+
+struct BoneOCLInfo : BaseBoneListInfo
+{
+	const ObjectCreationList *ocl;
+};
+
+struct BoneParticleSystemInfo : BaseBoneListInfo
+{
+	const ParticleSystemTemplate *particleSysTemplate;
+};
+
+class BoneFXUpdateModuleData
+{
+public:
+	unsigned char header[0x0c];
+	BoneFXListInfo fxList[4][8];
+	Int damageOCLTypes;
+	BoneOCLInfo ocl[4][8];
+	Int damageParticleTypes;
+	BoneParticleSystemInfo particleSystem[4][8];
+};
+
+class GameLogic
+{
+public:
+	UnsignedInt getFrame() const
+	{
+		return m_frame;
 	}
+
+private:
+	unsigned char padding[0x3c];
+	UnsignedInt m_frame;
+};
+
+#define TheGameLogic (*(GameLogic **)0x012f0898)
+
+class BehaviorModulePrimary
+{
+public:
+	virtual void slot();
+	const BoneFXUpdateModuleData *m_moduleData;
+	void *m_object;
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void slot();
+};
+
+class BehaviorModule : public BehaviorModulePrimary, public BehaviorModuleInterface
+{
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual UpdateSleepTime update() = 0;
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+protected:
+	Int m_nextCallFrameAndPhase;
+	Int m_indexInLogic;
+	Int m_updateState;
+};
+
+class VectorInt
+{
+public:
+	Int *m_begin;
+	Int *m_end;
+	Int *m_capacity;
+};
+
+class BoneFXUpdate : public UpdateModule
+{
+public:
+	virtual UpdateSleepTime update();
+
+protected:
+	void initTimes();
+	void doFXListAtBone(const FXList *, const Coord3D *);
+	void doOCLAtBone(const ObjectCreationList *, const Coord3D *);
+	void doParticleSystemAtBone(const ParticleSystemTemplate *, const Coord3D *);
+	void computeNextLogicFXTime(const BaseBoneListInfo *info, Int &nextFrame)
+	{
+		if (info->onlyOnce) {
+			nextFrame = -1;
+			return;
+		}
+		nextFrame = TheGameLogic->getFrame() + (Int)info->gameLogicDelay.getValue();
+	}
+	void computeNextClientFXTime(const BaseBoneListInfo *info, Int &nextFrame)
+	{
+		if (info->onlyOnce) {
+			nextFrame = -1;
+			return;
+		}
+		nextFrame = TheGameLogic->getFrame() + (Int)info->gameClientDelay.getValue();
+	}
+
+private:
+	VectorInt m_particleSystemIDs;
+	Int m_nextFXFrame[4][8];
+	Int m_nextOCLFrame[4][8];
+	Int m_nextParticleSystemFrame[4][8];
+	Coord3D m_FXBonePositions[4][8];
+	Coord3D m_OCLBonePositions[4][8];
+	Coord3D m_PSBonePositions[4][8];
+	Int m_curBodyState;
+	Bool m_bonesResolved[4];
+	Bool m_active;
+};
+
+#pragma comment(linker, "/alternatename:?initTimes@BoneFXUpdate@@IAEXXZ=?j_0002f775@@YAXXZ")
+#pragma comment(linker, "/alternatename:?doFXListAtBone@BoneFXUpdate@@IAEXPBVFXList@@PBUCoord3D@@@Z=?j_0002ef6e@@YAXXZ")
+#pragma comment(linker, "/alternatename:?doOCLAtBone@BoneFXUpdate@@IAEXPBVObjectCreationList@@PBUCoord3D@@@Z=?j_0001b1f3@@YAXXZ")
+#pragma comment(linker, "/alternatename:?doParticleSystemAtBone@BoneFXUpdate@@IAEXPBVParticleSystemTemplate@@PBUCoord3D@@@Z=?j_00019420@@YAXXZ")
+#pragma comment(linker, "/alternatename:?getValue@GameLogicRandomVariable@@QBEMXZ=?j_000188cc@@YAXXZ")
+#pragma comment(linker, "/alternatename:?getValue@GameClientRandomVariable@@QBEMXZ=?j_0000d7b5@@YAXXZ")
+
+UpdateSleepTime BoneFXUpdate::update()
+{
+	const BoneFXUpdateModuleData *d = m_moduleData;
+	Int now = TheGameLogic->getFrame();
+
+	if (m_active == false) {
+		initTimes();
+		m_active = true;
+	}
+
+	for (Int i = 0; i < 8; ++i) {
+		if ((m_nextFXFrame[m_curBodyState][i] != -1) && (m_nextFXFrame[m_curBodyState][i] <= now)) {
+			doFXListAtBone(d->fxList[m_curBodyState][i].fx, &m_FXBonePositions[m_curBodyState][i]);
+			computeNextLogicFXTime(&d->fxList[m_curBodyState][i], m_nextFXFrame[m_curBodyState][i]);
+		}
+		if ((m_nextOCLFrame[m_curBodyState][i] != -1) && (m_nextOCLFrame[m_curBodyState][i] <= now)) {
+			doOCLAtBone(d->ocl[m_curBodyState][i].ocl, &m_OCLBonePositions[m_curBodyState][i]);
+			computeNextLogicFXTime(&d->ocl[m_curBodyState][i], m_nextOCLFrame[m_curBodyState][i]);
+		}
+		if ((m_nextParticleSystemFrame[m_curBodyState][i] != -1) && (m_nextParticleSystemFrame[m_curBodyState][i] <= now)) {
+			doParticleSystemAtBone(d->particleSystem[m_curBodyState][i].particleSysTemplate, &m_PSBonePositions[m_curBodyState][i]);
+			computeNextClientFXTime(&d->particleSystem[m_curBodyState][i], m_nextParticleSystemFrame[m_curBodyState][i]);
+		}
+	}
+	return UPDATE_SLEEP_NONE;
 }
