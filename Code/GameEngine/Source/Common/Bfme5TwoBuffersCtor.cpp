fa3c15af9f4d8a52959bf70cb96fb9a0f31462d6
Gabe Borges <gbborges@proton.me>
2026-09-12 05:00:34 -0400

    feat(reverse): convert 005878d0 tree state update
    The 0x0110BA30 vtable, the Gen_00587D40 constructor and destructor, and caller 0x005883DA prove that 0x005878D0 updates this class's two tree buffers.
    I placed the clean member reconstruction in Bfme5TwoBuffersCtor.cpp because it removes the matching first-tree entry, closes the region popup, and erases the second-tree node when the state reaches zero.

--- Code/GameEngine/Source/Common/Bfme5TwoBuffersCtor.cpp
+++ Code/GameEngine/Source/Common/Bfme5TwoBuffersCtor.cpp
@@ -1,4 +1,41 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib
+// stlport
+
+#define _STLP_NO_EXCEPTIONS 1
+
 void *bfmeAlloc1134(unsigned int bytes);
+
+#include "Common/STLTypedefs.h"
+#include <map>
+
+struct BfmeSplitValue
+{
+	~BfmeSplitValue();
+	unsigned int m_bfmeFirst;
+	unsigned int m_bfmeSecond;
+	char m_bfmePadding[0x18];
+	unsigned int m_bfmeState;
+	char m_bfmeTail[0x1C];
+};
+
+struct BfmeSplitNode
+{
+	BfmeSplitNode *m_bfmeRoot;
+	BfmeSplitNode *m_bfmeLeft;
+	BfmeSplitNode *m_bfmeParent;
+	BfmeSplitNode *m_bfmeRight;
+	BfmeSplitValue m_bfmeValue;
+};
+
+void Open2SendNotice51B0C0(int value);
+
+struct Gen_005878D0FirstValue
+{
+	unsigned int m_bfmeValue;
+};
+
+typedef _STL::map<int, Gen_005878D0FirstValue> Gen_00587D40FirstMap;
+typedef _STL::map<int, BfmeSplitValue> Gen_00587D40SecondMap;
 
 class BfmeBase1134
 {
@@ -33,6 +70,7 @@ class Gen_00587D40 : public BfmeBase1134
 public:
 	Gen_00587D40(void);
 	virtual ~Gen_00587D40(void);
+	void rva005878d0(BfmeSplitNode *node);
 
 private:
 	int m_bfme08;
@@ -52,4 +90,35 @@ Gen_00587D40::Gen_00587D40(void) :
 	m_bfmeFirst(0x18),
 	m_bfmeSecond(0x50)
 {
+}
+
+// ?rva005878d0@Gen_00587D40@@QAEXPAUBfmeSplitNode@@@Z
+void Gen_00587D40::rva005878d0(BfmeSplitNode *node)
+{
+	Gen_00587D40SecondMap *second =
+		reinterpret_cast<Gen_00587D40SecondMap *>(&m_bfmeSecond);
+	if (node == reinterpret_cast<BfmeSplitNode *>(m_bfmeSecond.m_bfmeData))
+		return;
+
+	unsigned int firstValue = node->m_bfmeValue.m_bfmeFirst;
+	if (node->m_bfmeValue.m_bfmeState == 3)
+		return;
+
+	Gen_00587D40FirstMap *first =
+		reinterpret_cast<Gen_00587D40FirstMap *>(&m_bfmeFirst);
+	Gen_00587D40FirstMap::iterator found = first->find(node->m_bfmeValue.m_bfmeSecond);
+
+	if (found != first->end() && found->second.m_bfmeValue == firstValue)
+		first->erase(found);
+	if (node->m_bfmeValue.m_bfmeState != 0)
+	{
+		Open2SendNotice51B0C0(firstValue);
+		node->m_bfmeValue.m_bfmeState = 3;
+	}
+	else
+	{
+		Gen_00587D40SecondMap::iterator secondIterator =
+			*reinterpret_cast<Gen_00587D40SecondMap::iterator *>(&node);
+		second->erase(secondIterator);
+	}
 }
