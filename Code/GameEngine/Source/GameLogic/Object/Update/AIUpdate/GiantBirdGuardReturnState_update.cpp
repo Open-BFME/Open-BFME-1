f05b742f07668d5b9d0e1cb5db8e69bfaa019a7f
dginovker <danielginovker@gmail.com>
2026-09-12 14:15:44 +0900

    Name TAiData INI members in the GiantBirdGuardReturnState update shim
    Spike-Lane: W2C1

--- Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/GiantBirdGuardReturnState_update.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/GiantBirdGuardReturnState_update.cpp
@@ -59,7 +59,23 @@ public:
 class TAiData
 {
 public:
-	unsigned char m_unreconstructed00[ 0x44 ];
+	unsigned char m_pad00[ 4 ];
+	float m_structureSeconds;
+	float m_teamSeconds;
+	int m_resourcesWealthy;
+	int m_resourcesPoor;
+	UnsignedInt m_forceIdleFramesCount;
+	float m_structuresWealthyMod;
+	float m_teamWealthyMod;
+	float m_structuresPoorMod;
+	float m_teamPoorMod;
+	float m_teamResourcesToBuild;
+	float m_guardInnerModifierAI;
+	float m_guardOuterModifierAI;
+	float m_guardInnerModifierHuman;
+	float m_guardOuterModifierHuman;
+	UnsignedInt m_guardChaseUnitFrames;
+	UnsignedInt m_guardEnemyScanRate;
 	UnsignedInt m_guardEnemyReturnScanRate;
 };
 
