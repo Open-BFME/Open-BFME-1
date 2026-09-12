7e024f4c5d9b63b11ea8621c23b09d0200cb311c
Jonathan <jonathan@users.noreply.github.com>
2026-09-12 06:18:01 -0400

    Convert address-qualified vector slot accessor

---
+++ Code/GameEngine/Source/Common/Rva00387D90OwnerAt.cpp
@@ -0,0 +1,35 @@
+﻿// cl: /DNDEBUG /MD /EHsc
+// stlport
+#include <vector>
+
+class BFMERetailAsciiString
+{
+public:
+	BFMERetailAsciiString(const char *text);
+	~BFMERetailAsciiString() { releaseBuffer(); }
+	void *m_buffer;
+private:
+	void releaseBuffer();
+};
+
+class Rva00387D90String : public BFMERetailAsciiString
+{
+public:
+	Rva00387D90String(const char *text) : BFMERetailAsciiString(text) {}
+};
+
+class Rva00387D90Owner
+{
+public:
+	Rva00387D90String *at(int index);
+	unsigned char m_head[0x54];
+	std::vector<Rva00387D90String> m_slots;
+};
+
+Rva00387D90String *Rva00387D90Owner::at(int index)
+{
+	if (index >= 0 && (unsigned int)index < (unsigned int)m_slots.size())
+		return &m_slots[index];
+	static Rva00387D90String invalid("Invalid Slot");
+	return &invalid;
+}
