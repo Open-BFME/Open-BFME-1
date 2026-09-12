c3d8d74e4e54ba04a6f799b9e9057398d1b877e8
Gabe Borges <gbborges@proton.me>
2026-09-12 00:56:02 -0400

    feat(common): convert TeamsInfoRec::clear
    The `Rva0019A1D0Owner` constructor establishes the tree and vector member
    layout, and the `TeamsInfoRec::clear` caller identifies the retail body at
    `0x001991F0`. The source now uses members that follow STLport's layout and the matched
    `_M_erase` body in the common source file, so the compiler emits all 115 retail
    bytes without byte-emitting assembly.

--- Code/GameEngine/Source/Common/TeamsInfoRec_clear.cpp
+++ Code/GameEngine/Source/Common/TeamsInfoRec_clear.cpp
@@ -1,129 +1,145 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
-// readable body of ?clear@TeamsInfoRec@@QAEXXZ: Code/GameEngine/Source/GameLogic/Map/SidesList.cpp
-// Grok promote from masm_dumps — retail 0x001991F0 size 115
-// was: Code/masm_dumps/TeamsInfoRec_clear.asm
+// stlport
+// Clean reconstruction of TeamsInfoRec::clear at retail 0x001991F0.
 
-class TeamsInfoRec { public: void clear(void); };
-
-// ?clear@TeamsInfoRec@@QAEXXZ
-__declspec(naked) void TeamsInfoRec::clear(void)
+struct Gen_t_00195a00_p8cd
 {
-__asm {
-		_emit 083h
-		_emit 0ECh
-		_emit 00Ch
-		_emit 056h
-		_emit 057h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 06Ah
-		_emit 001h
-		_emit 08Dh
-		_emit 04Ch
-		_emit 024h
-		_emit 00Ch
-		_emit 0E8h
-		_emit 027h
-		_emit 0EFh
-		_emit 0E7h
-		_emit 0FFh
-		_emit 08Bh
-		_emit 056h
-		_emit 00Ch
-		_emit 08Bh
-		_emit 008h
-		_emit 089h
-		_emit 010h
-		_emit 089h
-		_emit 04Eh
-		_emit 00Ch
-		_emit 08Bh
-		_emit 056h
-		_emit 010h
-		_emit 08Bh
-		_emit 048h
-		_emit 004h
-		_emit 089h
-		_emit 050h
-		_emit 004h
-		_emit 089h
-		_emit 04Eh
-		_emit 010h
-		_emit 08Bh
-		_emit 048h
-		_emit 008h
-		_emit 08Bh
-		_emit 056h
-		_emit 014h
-		_emit 089h
-		_emit 050h
-		_emit 008h
-		_emit 089h
-		_emit 04Eh
-		_emit 014h
-		_emit 08Dh
-		_emit 04Ch
-		_emit 024h
-		_emit 008h
-		_emit 0E8h
-		_emit 078h
-		_emit 044h
-		_emit 0E9h
-		_emit 0FFh
-		_emit 08Bh
-		_emit 046h
-		_emit 004h
-		_emit 033h
-		_emit 0FFh
-		_emit 03Bh
-		_emit 0C7h
-		_emit 074h
-		_emit 01Fh
-		_emit 08Bh
-		_emit 006h
-		_emit 08Bh
-		_emit 048h
-		_emit 004h
-		_emit 051h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 0EAh
-		_emit 003h
-		_emit 0E8h
-		_emit 0FFh
-		_emit 08Bh
-		_emit 006h
-		_emit 089h
-		_emit 040h
-		_emit 008h
-		_emit 08Bh
-		_emit 016h
-		_emit 089h
-		_emit 07Ah
-		_emit 004h
-		_emit 08Bh
-		_emit 006h
-		_emit 089h
-		_emit 040h
-		_emit 00Ch
-		_emit 089h
-		_emit 07Eh
-		_emit 004h
-		_emit 066h
-		_emit 089h
-		_emit 07Eh
-		_emit 018h
-		_emit 066h
-		_emit 089h
-		_emit 07Eh
-		_emit 01Ah
-		_emit 05Fh
-		_emit 05Eh
-		_emit 083h
-		_emit 0C4h
-		_emit 00Ch
-		_emit 0C3h
-	}
+	int m_value;
+};
+
+class TeamsInfoRec;
+
+namespace _STL
+{
+
+struct RbTreeNodeBase
+{
+	unsigned char m_color;
+	unsigned char m_padding[3];
+	void *m_parent;
+	void *m_left;
+	void *m_right;
+};
+
+template <class First, class Second>
+struct pair
+{
+};
+
+template <class Value>
+struct _Rb_tree_node
+{
+	unsigned char m_color;
+	unsigned char m_padding[3];
+	void *m_parent;
+	void *m_left;
+	void *m_right;
+};
+
+template <class Value>
+struct _Select1st
+{
+};
+
+template <class Key>
+struct less
+{
+};
+
+template <class Value>
+class allocator
+{
+};
+
+template <class Key, class Value, class Select, class Compare, class Alloc>
+class _Rb_tree
+{
+	friend class ::TeamsInfoRec;
+
+	private:
+	void _M_erase( _Rb_tree_node<Value> *root ) throw();
+
+	public:
+	_Rb_tree_node<Value> *m_header;
+	unsigned int m_nodeCount;
+	int m_compare;
+};
+
+template <class Type>
+inline void swap( Type &left, Type &right )
+{
+	Type value = left;
+	left = right;
+	right = value;
 }
 
+template <class Type>
+class vector
+{
+public:
+	void swap( vector<Type> &other )
+	{
+		_STL::swap( other.m_begin, m_begin );
+		_STL::swap( other.m_end, m_end );
+		_STL::swap( other.m_capacity, m_capacity );
+	}
+
+	Type *m_begin;
+	Type *m_end;
+	Type *m_capacity;
+};
+
+}
+
+class Rva0019A1D0Member : public _STL::vector<void *>
+{
+public:
+	Rva0019A1D0Member *construct( int count ) throw();
+	~Rva0019A1D0Member() throw();
+};
+
+class Rva0019A1D0Tree : public _STL::_Rb_tree<
+	int,
+	_STL::pair<const int, Gen_t_00195a00_p8cd>,
+	_STL::_Select1st<_STL::pair<const int, Gen_t_00195a00_p8cd> >,
+	_STL::less<int>,
+	_STL::allocator<_STL::pair<const int, Gen_t_00195a00_p8cd> > >
+{
+};
+
+class TeamsInfoRec
+{
+public:
+	void clear();
+
+private:
+	Rva0019A1D0Tree m_tree;
+	Rva0019A1D0Member m_member;
+	short m_a;
+	short m_b;
+};
+
+#pragma comment(linker, "/alternatename:?construct@Rva0019A1D0Member@@QAEPAV1@H@Z=?j_00018129@@YAXXZ")
+#pragma comment(linker, "/alternatename:??1Rva0019A1D0Member@@QAE@XZ=?j_0002d6a5@@YAXXZ")
+
+void TeamsInfoRec::clear()
+{
+	{
+		Rva0019A1D0Member storage;
+		Rva0019A1D0Member *member = storage.construct( 1 );
+		m_member.swap( *member );
+	}
+
+	if ( m_tree.m_nodeCount != 0 )
+	{
+		m_tree._M_erase( (_STL::_Rb_tree_node<
+			_STL::pair<const int, Gen_t_00195a00_p8cd> > *)m_tree.m_header->m_parent );
+		m_tree.m_header->m_left = m_tree.m_header;
+		m_tree.m_header->m_parent = 0;
+		m_tree.m_header->m_right = m_tree.m_header;
+		m_tree.m_nodeCount = 0;
+	}
+
+	m_a = 0;
+	m_b = 0;
+}
