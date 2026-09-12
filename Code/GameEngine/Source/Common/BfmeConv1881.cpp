5f592f45fc8bf084ccecbf1072c4865b37891874
Gabe Borges <gbborges@proton.me>
2026-09-12 14:10:53 -0400

    feat(common): convert BfmeItemZO::bfmeWriteZO
    The 143-byte retail body matches the BfmeOwnerZO::bfmeSaveZO caller, the existing BfmeItemZO ILT pin, and the STLport tree walk that writes each item string.
    The clean body sits in Code/GameEngine/Source/Common/BfmeConv1881.cpp and preserves the 0x14-byte item stride used by the caller.

--- Code/GameEngine/Source/Common/BfmeConv1881.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1881.cpp
@@ -1,10 +1,71 @@
 extern "C" char g_bfmeNameZO[];
+extern "C" char g_bfmeNameItemZO[];
+
+class AsciiString
+{
+};
+
+class DataChunkOutput
+{
+public:
+	void openDataChunk(char *name, unsigned short version);
+	void writeByte(char value);
+	void writeInt(int value);
+	void writeAsciiString(const AsciiString &value);
+	void closeDataChunk();
+};
+
+typedef AsciiString BfmeStringZO;
+
+struct BfmeTreeNodeZO
+{
+	unsigned char m_color;
+	BfmeTreeNodeZO *m_parent;
+	BfmeTreeNodeZO *m_left;
+	BfmeTreeNodeZO *m_right;
+	BfmeStringZO m_value;
+};
+
+struct BfmeTreeZO
+{
+	unsigned char m_pad[8];
+	BfmeTreeNodeZO *m_root;
+};
+
+namespace _STL
+{
+struct _Rb_tree_node_base;
+
+template <class T>
+struct _Rb_global
+{
+	static _Rb_tree_node_base *_M_increment(_Rb_tree_node_base *node);
+};
+}
 
 class BfmeChunkZO
 {
 public:
-	void bfmeOpenZO(char *name, unsigned short version);
-	void bfmeCloseZO();
+	void bfmeOpenZO(char *name, unsigned short version)
+	{
+		((DataChunkOutput *)this)->openDataChunk(name, version);
+	}
+	void bfmeWriteByteZO(char value)
+	{
+		((DataChunkOutput *)this)->writeByte(value);
+	}
+	void bfmeWriteIntZO(int value)
+	{
+		((DataChunkOutput *)this)->writeInt(value);
+	}
+	void bfmeWriteStringZO(const BfmeStringZO &value)
+	{
+		((DataChunkOutput *)this)->writeAsciiString(value);
+	}
+	void bfmeCloseZO()
+	{
+		((DataChunkOutput *)this)->closeDataChunk();
+	}
 };
 
 class BfmeItemZO
@@ -12,8 +73,35 @@ class BfmeItemZO
 public:
 	void bfmeWriteZO(BfmeChunkZO *out);
 
-	unsigned char m_bfmeBytesZO[0x14];
+	unsigned char m_bfmeFlag0ZO;
+	unsigned char m_bfmeFlag1ZO;
+	unsigned char m_bfmeFlag2ZO;
+	unsigned char m_bfmePadZO;
+	int m_bfmeInt4ZO;
+	BfmeTreeZO *m_bfmeTreeZO;
+	int m_bfmeIntCZO;
+	int m_bfmePad10ZO;
 };
+
+void BfmeItemZO::bfmeWriteZO(BfmeChunkZO *out)
+{
+	out->bfmeOpenZO(g_bfmeNameItemZO, 1);
+	out->bfmeWriteByteZO(m_bfmeFlag0ZO != 0);
+	out->bfmeWriteByteZO(m_bfmeFlag1ZO != 0);
+	out->bfmeWriteByteZO(m_bfmeFlag2ZO != 0);
+	out->bfmeWriteIntZO(m_bfmeInt4ZO);
+	out->bfmeWriteIntZO(m_bfmeIntCZO);
+
+	BfmeTreeNodeZO *node = m_bfmeTreeZO->m_root;
+	while (node != (BfmeTreeNodeZO *)m_bfmeTreeZO)
+	{
+		out->bfmeWriteStringZO(node->m_value);
+		node = (BfmeTreeNodeZO *)_STL::_Rb_global<bool>::_M_increment(
+			(_STL::_Rb_tree_node_base *)node);
+	}
+
+	out->bfmeCloseZO();
+}
 
 class BfmeOwnerZO
 {
