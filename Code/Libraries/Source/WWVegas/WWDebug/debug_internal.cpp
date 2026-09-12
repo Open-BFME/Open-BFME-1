060fce56392e63a4c80d85fb5641359abc6a8d6c
TheThag <zackgreco@hotmail.com>
2026-09-11 22:06:29 -0300

    Convert DebugAllocMemory to C++

--- Code/Libraries/Source/WWVegas/WWDebug/debug_internal.cpp
+++ Code/Libraries/Source/WWVegas/WWDebug/debug_internal.cpp
@@ -42,94 +42,6 @@ void DebugInternalAssert(const char *file, int line, const char *expr)
   TerminateProcess(GetCurrentProcess(),666);
 }
 
-__declspec(naked) void *DebugAllocMemory(unsigned)
-{
-	__asm {
-		__emit 0x55;
-		__emit 0x8b;
-		__emit 0xec;
-		__emit 0x8b;
-		__emit 0x45;
-		__emit 0x08;
-		__emit 0x56;
-		__emit 0x50;
-		__emit 0x6a;
-		__emit 0x00;
-		__emit 0xff;
-		__emit 0x15;
-		__emit 0x28;
-		__emit 0x8e;
-		__emit 0x35;
-		__emit 0x01;
-		__emit 0x8b;
-		__emit 0xf0;
-		__emit 0x85;
-		__emit 0xf6;
-		__emit 0x75;
-		__emit 0x37;
-		__emit 0x6a;
-		__emit 0x01;
-		__emit 0xe8;
-		__emit 0x53;
-		__emit 0xab;
-		__emit 0xff;
-		__emit 0xff;
-		__emit 0x8b;
-		__emit 0x0d;
-		__emit 0x5c;
-		__emit 0x6e;
-		__emit 0x33;
-		__emit 0x01;
-		__emit 0x8b;
-		__emit 0x11;
-		__emit 0x83;
-		__emit 0xc4;
-		__emit 0x04;
-		__emit 0xff;
-		__emit 0x52;
-		__emit 0x60;
-		__emit 0x8b;
-		__emit 0x0d;
-		__emit 0x5c;
-		__emit 0x6e;
-		__emit 0x33;
-		__emit 0x01;
-		__emit 0x8b;
-		__emit 0x01;
-		__emit 0x56;
-		__emit 0x56;
-		__emit 0xff;
-		__emit 0x50;
-		__emit 0x6c;
-		__emit 0x8b;
-		__emit 0x10;
-		__emit 0x68;
-		__emit 0xa0;
-		__emit 0x44;
-		__emit 0x13;
-		__emit 0x01;
-		__emit 0x8b;
-		__emit 0xc8;
-		__emit 0xff;
-		__emit 0x52;
-		__emit 0x38;
-		__emit 0x8b;
-		__emit 0x10;
-		__emit 0x6a;
-		__emit 0x01;
-		__emit 0x8b;
-		__emit 0xc8;
-		__emit 0xff;
-		__emit 0x52;
-		__emit 0x4c;
-		__emit 0x8b;
-		__emit 0xc6;
-		__emit 0x5e;
-		__emit 0x5d;
-		__emit 0xc3;
-	}
-}
-
 class BfmeDebugReport
 {
 public:
@@ -158,6 +70,21 @@ public:
 extern void _bfme_debugRecordCallsite(int kind);
 
 #define TheBfmeDebug (*(BfmeDebugManager **)0x01336E5C)
+
+#pragma optimize("y", off)
+void *DebugAllocMemory(unsigned numBytes)
+{
+  HGLOBAL h=GlobalAlloc(GMEM_FIXED,numBytes);
+  if (!h)
+  {
+    _bfme_debugRecordCallsite(1);
+    TheBfmeDebug->slot60();
+    BfmeDebugReport *report=TheBfmeDebug->slot6C(0,0);
+    report=report->slot38("Debug mem alloc failed");
+    report->slot4C(1);
+  }
+  return (void *)h;
+}
 
 void *DebugReAllocMemory(void *oldPtr, unsigned newSize)
 {
