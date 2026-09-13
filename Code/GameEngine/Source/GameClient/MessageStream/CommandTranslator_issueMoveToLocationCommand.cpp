2837b6d2c3f1fd11ce52325cd5022a22900081fa
Jonathan <jonathan@users.noreply.github.com>
2026-09-12 22:35:14 -0400

    Convert CommandTranslator::issueMoveToLocationCommand (0x005ACBB0 312B)

--- Code/GameEngine/Source/GameClient/MessageStream/CommandTranslator_issueMoveToLocationCommand.cpp
+++ Code/GameEngine/Source/GameClient/MessageStream/CommandTranslator_issueMoveToLocationCommand.cpp
@@ -1,6 +1,4 @@
-// ?issueMoveToLocationCommand@CommandTranslator@@QAE?AW4Type@GameMessage@@PBVCoord3D@@PAVDrawable@@W4CommandEvaluateType@1@@Z
-// partial score=0.95 date=2026-09-08
-// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /MD /EHsc
+// cl: /O2
 // stlport
 
 #include <list>
@@ -9,9 +7,8 @@ typedef bool Bool;
 typedef int Int;
 typedef unsigned int UnsignedInt;
 
-class Coord3D
+struct Coord3D
 {
-	public:
 	float x;
 	float y;
 	float z;
@@ -28,32 +25,30 @@ public:
 class ThingTemplate : public Overridable
 {
 public:
-	char m_padding[0xc8];
-	unsigned int m_kindOfD0;
+	unsigned char m_unreconstructed_08[0xC8 - 0x08];
+	UnsignedInt m_kindof[3];
 };
 
 class Object
 {
 public:
-	char m_padding[4];
+	void *m_vftable;
 	ThingTemplate *m_template;
-	char m_unmodelled[0x6c];
+	unsigned char m_pad08[0x30];
+	Coord3D m_cachedPos;
+	unsigned char m_pad44[0x30];
 	UnsignedInt m_id;
 
-
-	Bool isKindOfForceAttackable() const
+	const Coord3D *getPosition() const
 	{
-		ThingTemplate *thing = m_template;
-		if (thing && thing->m_nextOverride)
-			thing = (ThingTemplate *)thing->m_nextOverride->getFinalOverride();
-		return (thing->m_kindOfD0 >> 3) & 1;
+		return (const Coord3D *)((const unsigned char *)this + 0x38);
 	}
 };
 
 class Drawable
 {
 public:
-	char m_padding[0xfc];
+	unsigned char m_padding[0xFC];
 	Object *m_object;
 };
 
@@ -71,9 +66,6 @@ public:
 	void appendObjectIDArgument(UnsignedInt id);
 	void appendLocationArgument(const Coord3D &location);
 };
-
-extern void j_0002bcec();
-extern void j_00037ad8();
 
 typedef _STL::list<Drawable *> DrawableList;
 
@@ -99,6 +91,11 @@ public:
 	BFME_UI_SLOT(60) BFME_UI_SLOT(61) BFME_UI_SLOT(62)
 	virtual const DrawableList *getAllSelectedDrawables() const = 0;
 #undef BFME_UI_SLOT
+
+	unsigned char m_pad004[0x12AC];
+	unsigned char m_waypointMode;
+	unsigned char m_forceAttackMode;
+	unsigned char m_forceMoveMode;
 };
 
 class MessageStream
@@ -120,6 +117,7 @@ public:
 	PickAndPlayInfo();
 
 	Bool m_air;
+	char m_pad03[3];
 	Drawable *m_drawTarget;
 	void *m_weaponSlot;
 	Int m_specialPowerType;
@@ -134,26 +132,11 @@ public:
 	UnsignedInt m_moveCommands;
 };
 
-struct BfmeInGameUIState
-{
-	char m_padding[0x12b0];
-	unsigned char m_waypointMode;
-	unsigned char m_forceAttackMode;
-	unsigned char m_forceMoveMode;
-};
-
 extern InGameUI *TheInGameUI;
 extern MessageStream *TheMessageStream;
 extern StatsCollector *TheStatsCollector;
 extern void pickAndPlayUnitVoiceResponse(const DrawableList *list,
 	GameMessage::Type messageType, PickAndPlayInfo *info);
-
-#undef TheInGameUI
-#undef TheMessageStream
-#undef TheStatsCollector
-#define TheInGameUI (*(InGameUI **)0x012F148C)
-#define TheMessageStream (*(MessageStream **)0x012ED5EC)
-#define TheStatsCollector (*(StatsCollector **)0x012ED63C)
 
 class CommandTranslator
 {
@@ -177,7 +160,7 @@ GameMessage::Type CommandTranslator::issueMoveToLocationCommand(const Coord3D *p
 	Drawable *drawableInWay, CommandEvaluateType commandType)
 {
 	GameMessage::Type msgType = (GameMessage::Type)0;
-	Object *obj = drawableInWay ? *(Object **)((char *)drawableInWay + 0xfc) : 0;
+	Object *obj = drawableInWay ? drawableInWay->m_object : 0;
 
 	Bool isForceAttackable = false;
 	if (obj)
@@ -185,18 +168,17 @@ GameMessage::Type CommandTranslator::issueMoveToLocationCommand(const Coord3D *p
 		ThingTemplate *thing = obj->m_template;
 		if (thing && thing->m_nextOverride)
 			thing = (ThingTemplate *)thing->m_nextOverride->getFinalOverride();
-		isForceAttackable = (thing->m_kindOfD0 >> 3) & 1;
+		isForceAttackable = (thing->m_kindof[2] >> 3) & 1;
 	}
 	const Coord3D *destination = pos;
 
 	if (m_teamExists)
 	{
-		BfmeInGameUIState *ui = (BfmeInGameUIState *)TheInGameUI;
-		if (ui->m_waypointMode)
+		if (TheInGameUI->m_waypointMode)
 			msgType = GameMessage::MSG_ADD_WAYPOINT;
-		else if (ui->m_forceMoveMode)
+		else if (TheInGameUI->m_forceMoveMode)
 			msgType = GameMessage::MSG_DO_FORCEMOVETO;
-		else if (ui->m_forceAttackMode && isForceAttackable)
+		else if (TheInGameUI->m_forceAttackMode && isForceAttackable)
 			msgType = GameMessage::MSG_DO_ATTACK_OBJECT;
 		else
 			msgType = GameMessage::MSG_DO_MOVETO;
@@ -205,19 +187,9 @@ GameMessage::Type CommandTranslator::issueMoveToLocationCommand(const Coord3D *p
 		{
 			GameMessage *movemsg = TheMessageStream->appendMessage(msgType);
 			if (msgType == GameMessage::MSG_DO_ATTACK_OBJECT)
-			{
-				typedef void (GameMessage::*AppendObjectID)(UnsignedInt);
-				union { void (*raw)(void); AppendObjectID member; } call;
-				call.raw = j_0002bcec;
-				(movemsg->*call.member)(obj->m_id);
-			}
+				movemsg->appendObjectIDArgument(obj->m_id);
 			else
-			{
-				typedef void (GameMessage::*AppendLocation)(const Coord3D &);
-				union { void (*raw)(void); AppendLocation member; } call;
-				call.raw = j_00037ad8;
-				(movemsg->*call.member)(*destination);
-			}
+				movemsg->appendLocationArgument(*destination);
 		}
 	}
 
@@ -226,12 +198,7 @@ GameMessage::Type CommandTranslator::issueMoveToLocationCommand(const Coord3D *p
 		PickAndPlayInfo info;
 		info.m_drawTarget = drawableInWay;
 		if (msgType == GameMessage::MSG_DO_ATTACK_OBJECT)
-		{
-			obj = (Object *)((char *)obj + 0x38);
-			info.m_position.x = ((Coord3D *)obj)->x;
-			info.m_position.y = ((Coord3D *)obj)->y;
-			info.m_position.z = ((Coord3D *)obj)->z;
-		}
+			info.m_position = *obj->getPosition();
 		else
 			info.m_position = *destination;
 		pickAndPlayUnitVoiceResponse(TheInGameUI->getAllSelectedDrawables(),
