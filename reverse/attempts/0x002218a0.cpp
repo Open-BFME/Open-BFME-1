9bbb7f007dbfb0772e6998558df363bdf4f4e137
Jonathan <jonathan@users.noreply.github.com>
2026-09-12 20:22:29 -0400

    re: bank 0x002218A0 plane-test frame and record conversion walls

--- reverse/attempts/0x002218a0.cpp
+++ reverse/attempts/0x002218a0.cpp
@@ -1,5 +1,7 @@
 // ?bfmeTestEL@BfmeHostEL@@QBEDPBVBfmeMtxEL@@@Z
-// partial score=0.92 date=2026-09-08
+// partial score=0.93 date=2026-09-12
+// Address-derived reconstruction of retail RVA 0x002218A0.
+// ContestableContain secondary vtable 0x010AB140 slot 0xA8.
 extern const float BfmeZeroRange;
 
 class BfmeMtxEL
@@ -46,9 +48,13 @@ public:
 	char bfmeTestEL(const BfmeMtxEL *m) const;
 };
 
+// ?bfmeTestEL@BfmeHostEL@@QBEDPBVBfmeMtxEL@@@Z
 char BfmeHostEL::bfmeTestEL(const BfmeMtxEL *m) const
 {
-	if (m == 0)
+	const BfmeMtxEL *p = m;
+	volatile float v[3];
+
+	if (p == 0)
 		return 0;
 
 	const BfmeBaseEL *base = (const BfmeBaseEL *)((const char *)this - 0x1c);
@@ -59,16 +65,16 @@ char BfmeHostEL::bfmeTestEL(const BfmeMtxEL *m) const
 
 	BfmeOtherEL *o = base->m_bfmeOtherEL;
 
-	if (m->m_bfmeZEL - o->m_bfmeZEL > info->m_bfmeRangeEL)
+	if (p->m_bfmeZEL - o->m_bfmeZEL > info->m_bfmeRangeEL)
 		return 0;
 
-	float a = m->m_bfmeAEL;
-	float b = m->m_bfmeBEL;
-	float c = m->m_bfmeCEL;
-	volatile float e = o->m_bfmeXEL - m->m_bfmeXEL;
-	float d = o->m_bfmeYEL - m->m_bfmeYEL;
+	float a = p->m_bfmeAEL;
+	float b = p->m_bfmeBEL;
+	float c = p->m_bfmeCEL;
+	float d = o->m_bfmeYEL - p->m_bfmeYEL;
+	v[0] = o->m_bfmeXEL - p->m_bfmeXEL;
 
-	if (a * e + b * d + c * BfmeZeroRange < BfmeZeroRange)
+	if (a * v[0] + b * d + c * BfmeZeroRange < BfmeZeroRange)
 		return 0;
 
 	return 1;
