5055bb820f142e5ef93bf290f86486c4648e0869
Jonathan <jonf38@users.noreply.github.com>
2026-09-13 04:58:32 -0400

    Recover CylindricalVelocity vector destructor

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/CylindricalEmissionVelocityConcreteModuleTemplateVectorDeletingDestructor.cpp
@@ -0,0 +1,43 @@
+// cl: /O2
+// Retail RVA 0x005DBC50. Scalar destructor ILT 0x00044E2C routes to the
+// matched CylindricalEmissionVelocity specialization destructor at 0x005DBB50.
+void operator delete[](void *block);
+
+namespace FXParticleSystem
+{
+extern const char CYLINDRICAL_EMISSION_VELOCITY_MODULE_KEY[1];
+extern const char CYLINDRICAL_EMISSION_VELOCITY_MODULE_NAME[1];
+
+class CylindricalEmissionVelocityModule;
+class CylindricalEmissionVelocityModuleTemplate;
+template<int Category> class DefaultParticleModule;
+template<int Category> class DefaultParticleModuleTemplate;
+template<int Category, const char (&Key)[1], const char (&Name)[1], class Module,
+    class ModuleTemplate, class DefaultModule, class DefaultModuleTemplate>
+class ModuleTag;
+
+template<class Tag> class ConcreteModuleTemplate
+{
+public:
+    virtual ~ConcreteModuleTemplate();
+private:
+    unsigned char m_data[0x20];
+};
+
+typedef ConcreteModuleTemplate<ModuleTag<4,
+    CYLINDRICAL_EMISSION_VELOCITY_MODULE_KEY,
+    CYLINDRICAL_EMISSION_VELOCITY_MODULE_NAME,
+    CylindricalEmissionVelocityModule,
+    CylindricalEmissionVelocityModuleTemplate,
+    DefaultParticleModule<4>, DefaultParticleModuleTemplate<4> > > CylindricalTemplate;
+
+CylindricalTemplate *MakeCylindricalTemplateArray()
+{
+    return new CylindricalTemplate[2];
+}
+
+void DeleteCylindricalTemplateArray(CylindricalTemplate *array)
+{
+    delete[] array;
+}
+}
