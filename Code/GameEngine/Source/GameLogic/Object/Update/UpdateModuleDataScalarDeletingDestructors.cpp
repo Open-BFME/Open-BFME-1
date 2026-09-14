2e8c68889accc2a34a5d6d1aa1af6ca31b8f4150
dginovker <danielginovker@gmail.com>
2026-09-14 15:14:32 +0900

    Recover Update ModuleData scalar-deleting destructors

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/UpdateModuleDataScalarDeletingDestructors.cpp
@@ -0,0 +1,14 @@
+// cl: /O2
+// Authentic scalar-deleting destructors for identified Update ModuleData
+// classes. Each retail wrapper sits 48 bytes before its already-matched
+// destructor and is the unique 30-byte ??_G that calls that destructor.
+
+#define DECLARE_MODULE_DELETING_DESTRUCTOR(ModuleName) \
+    class ModuleName { public: virtual ~ModuleName(); }; \
+    ModuleName *Make##ModuleName() { return new ModuleName; } \
+    void Delete##ModuleName(ModuleName *object) { delete object; }
+
+DECLARE_MODULE_DELETING_DESTRUCTOR(RousingSpeechUpdateModuleData)
+DECLARE_MODULE_DELETING_DESTRUCTOR(AttributeModifierAuraUpdateModuleData)
+DECLARE_MODULE_DELETING_DESTRUCTOR(AutoPickUpUpdateModuleData)
+DECLARE_MODULE_DELETING_DESTRUCTOR(BoneFXUpdateModuleData)
