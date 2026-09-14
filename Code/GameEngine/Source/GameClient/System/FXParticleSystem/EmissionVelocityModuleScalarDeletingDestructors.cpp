2e0c7edddc510a56353423271a56ab22caf29c06
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-14 01:28:39 -0400

    Recover emission-velocity module destructors

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/EmissionVelocityModuleScalarDeletingDestructors.cpp
@@ -0,0 +1,15 @@
+// cl: /O2
+// Authentic scalar-deleting destructors for identified emission-velocity modules.
+// Each retail wrapper calls the corresponding already-identified destructor ILT.
+namespace FXParticleSystem
+{
+#define DECLARE_MODULE_DELETING_DESTRUCTOR(ModuleName) \
+    class ModuleName { public: virtual ~ModuleName(); }; \
+    ModuleName *Make##ModuleName() { return new ModuleName; } \
+    void Delete##ModuleName(ModuleName *object) { delete object; }
+
+DECLARE_MODULE_DELETING_DESTRUCTOR(OrthoEmissionVelocityModule)
+DECLARE_MODULE_DELETING_DESTRUCTOR(SphericalEmissionVelocityModule)
+DECLARE_MODULE_DELETING_DESTRUCTOR(CylindricalEmissionVelocityModule)
+DECLARE_MODULE_DELETING_DESTRUCTOR(OutwardEmissionVelocityModule)
+}
