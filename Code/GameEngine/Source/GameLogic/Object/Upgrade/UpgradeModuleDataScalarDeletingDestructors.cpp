68528e1387fd34cf34715b9294e1c1903a8a5eef
dginovker <danielginovker@gmail.com>
2026-09-14 15:21:02 +0900

    Recover Upgrade ModuleData scalar-deleting destructors

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/UpgradeModuleDataScalarDeletingDestructors.cpp
@@ -0,0 +1,14 @@
+// cl: /O2
+// Authentic scalar-deleting destructors for identified Upgrade ModuleData
+// classes. Each retail wrapper sits 48 bytes before its already-matched
+// destructor and is the unique 30-byte ??_G that calls that destructor.
+
+#define DECLARE_MODULE_DELETING_DESTRUCTOR(ModuleName) \
+    class ModuleName { public: virtual ~ModuleName(); }; \
+    ModuleName *Make##ModuleName() { return new ModuleName; } \
+    void Delete##ModuleName(ModuleName *object) { delete object; }
+
+DECLARE_MODULE_DELETING_DESTRUCTOR(ProductionSpeedBonusModuleData)
+DECLARE_MODULE_DELETING_DESTRUCTOR(BaseUpgradeModuleData)
+DECLARE_MODULE_DELETING_DESTRUCTOR(CostModifierUpgradeModuleData)
+DECLARE_MODULE_DELETING_DESTRUCTOR(ExperienceScalarUpgradeModuleData)
