6af5280760343aa9a250fb0e6a74d57660dd9355
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-14 03:05:24 -0400

    Retract synthetic Gen_00837560 scalar destructor

--- Code/gen_small/dtors_009.cpp
+++ Code/gen_small/dtors_009.cpp
@@ -212,9 +212,6 @@ void gen_force_00832420() { Gen_dtor_00832420 v; }
 struct Gen_dtor_00832450 { virtual ~Gen_dtor_00832450(); };
 void gen_force_00832450() { Gen_dtor_00832450 v; }
 
-struct Gen_dtor_00837540 { virtual ~Gen_dtor_00837540(); };
-void gen_force_00837540() { Gen_dtor_00837540 v; }
-
 struct Gen_dtor_008375b0 { virtual ~Gen_dtor_008375b0(); };
 void gen_force_008375b0() { Gen_dtor_008375b0 v; }
 
