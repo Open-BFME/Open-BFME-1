28edc5391d43e3a9d113b9e1eae0e2b4627b5422
Jonathan <jonf38@users.noreply.github.com>
2026-09-13 21:10:14 -0400

    Recover hemispherical velocity scalar destructor

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/HemisphericalEmissionVelocityConcreteModuleTemplateScalarDeletingDestructor.cpp
@@ -0,0 +1,30 @@
+// cl: /O2
+// Retail RVA 0x005DBA80. Destructor ILT 0x000408EA routes directly to the
+// matched HemisphericalEmissionVelocity specialization destructor at 0x005DB9D0.
+namespace FXParticleSystem
+{
+extern const char HEMISPHERICAL_EMISSION_VELOCITY_MODULE_KEY[1];
+extern const char HEMISPHERICAL_EMISSION_VELOCITY_MODULE_NAME[1];
+class HemisphericalEmissionVelocityModule;
+class HemisphericalEmissionVelocityModuleTemplate;
+template<int Category> class DefaultParticleModule;
+template<int Category> class DefaultParticleModuleTemplate;
+template<int Category, const char (&Key)[1], const char (&Name)[1], class Module,
+    class ModuleTemplate, class DefaultModule, class DefaultModuleTemplate>
+class ModuleTag;
+template<class Tag> class ConcreteModuleTemplate
+{
+public:
+    virtual ~ConcreteModuleTemplate();
+private:
+    unsigned char m_data[0x14];
+};
+typedef ConcreteModuleTemplate<ModuleTag<4,
+    HEMISPHERICAL_EMISSION_VELOCITY_MODULE_KEY,
+    HEMISPHERICAL_EMISSION_VELOCITY_MODULE_NAME,
+    HemisphericalEmissionVelocityModule,
+    HemisphericalEmissionVelocityModuleTemplate,
+    DefaultParticleModule<4>, DefaultParticleModuleTemplate<4> > > HemisphericalTemplate;
+HemisphericalTemplate *MakeHemisphericalTemplate() { return new HemisphericalTemplate; }
+void DeleteHemisphericalTemplate(HemisphericalTemplate *object) { delete object; }
+}
