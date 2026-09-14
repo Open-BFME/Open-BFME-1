5ada6e4191e013d5131ca3388b490f2d41c3d8ec
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-14 03:54:39 -0400

    Promote remaining STL exception scalar destructors

--- Code/Libraries/Source/WWVegas/WWLib/STLBadAllocDeletingDestructor.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLBadAllocDeletingDestructor.cpp
@@ -37,6 +37,36 @@ public:
 	virtual ~Rva0082AEC0Exc();
 };
 
+class Rva0082AEE0Exc : public Rva0082AE60Exc
+{
+public:
+	virtual ~Rva0082AEE0Exc();
+};
+
+class Rva0082AF00Exc : public Rva0082AE60Exc
+{
+public:
+	virtual ~Rva0082AF00Exc();
+};
+
+class Rva0082AF20Exc : public Rva0082AE80Exc
+{
+public:
+	virtual ~Rva0082AF20Exc();
+};
+
+class Rva0082AF40Exc : public Rva0082AE80Exc
+{
+public:
+	virtual ~Rva0082AF40Exc();
+};
+
+class Rva0082AF60Exc : public Rva0082AE80Exc
+{
+public:
+	virtual ~Rva0082AF60Exc();
+};
+
 void ForceRva0082AE60ExcDeletingDestructor()
 {
 	Rva0082AE60Exc value;
@@ -55,6 +85,31 @@ void ForceRva0082AEA0ExcDeletingDestructor()
 void ForceRva0082AEC0ExcDeletingDestructor()
 {
 	Rva0082AEC0Exc value;
+}
+
+void ForceRva0082AEE0ExcDeletingDestructor()
+{
+	Rva0082AEE0Exc value;
+}
+
+void ForceRva0082AF00ExcDeletingDestructor()
+{
+	Rva0082AF00Exc value;
+}
+
+void ForceRva0082AF20ExcDeletingDestructor()
+{
+	Rva0082AF20Exc value;
+}
+
+void ForceRva0082AF40ExcDeletingDestructor()
+{
+	Rva0082AF40Exc value;
+}
+
+void ForceRva0082AF60ExcDeletingDestructor()
+{
+	Rva0082AF60Exc value;
 }
 
 }
