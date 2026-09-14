8ee0c4f253ef3be6a0adc0520b6a4deed95c260c
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-14 02:52:16 -0400

    Recover Gen_00926630 scalar destructor

--- Code/GameEngine/Source/Common/Gen_00924110DeletingDestructor.cpp
+++ Code/GameEngine/Source/Common/Gen_00924110DeletingDestructor.cpp
@@ -15,12 +15,23 @@ public:
 	~Gen_00924150();
 };
 
+class Gen_00926630
+{
+public:
+	~Gen_00926630();
+};
+
 void DeleteGen00924110(Gen_00924110 *value)
 {
 	delete value;
 }
 
 void DeleteGen00924150(Gen_00924150 *value)
+{
+	delete value;
+}
+
+void DeleteGen00926630(Gen_00926630 *value)
 {
 	delete value;
 }
