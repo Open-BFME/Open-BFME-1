15645c2035c6afc8a91e39e850240055847668e6
Gabe Borges <gbborges@proton.me>
2026-09-14 21:35:02 -0400

    feat(reverse): convert terrain edge midpoint helper
    The constructor at retail 0x00729AE0 establishes the terrain fields for this helper. The matched bit-plane tests at 0x00729300 and 0x00729370 identify the map layout and Rva00729D30Terrain::checkEdges. The clean body lives in Code/GameEngine/Source/Common/Rva00729300BitPlanes.cpp and emits all 288 bytes with four relocations matching retail.

--- Code/GameEngine/Source/Common/Rva00729300BitPlanes.cpp
+++ Code/GameEngine/Source/Common/Rva00729300BitPlanes.cpp
@@ -16,7 +16,7 @@ class Rva00729300BitPlane
 public:
 	bool test( int x, int y ) const;
 
-private:
+public:
 	Byte m_opaque00[0x08];
 	int m_width;
 	int m_height;
@@ -31,7 +31,7 @@ class Rva00729370BitPlane
 public:
 	bool test( int x, int y ) const;
 
-private:
+public:
 	Byte m_opaque00[0x08];
 	int m_width;
 	int m_height;
@@ -81,4 +81,49 @@ bool Rva00729370BitPlane::test( int x, int y ) const
 	Byte value = self->m_bits[index];
 	bool result = (value & mask) != 0;
 	return result;
+}
+
+class Rva00729D30Terrain
+{
+public:
+	void checkEdges( int xOffset, int yOffset, int width,
+		bool *top, bool *right, bool *bottom, bool *left );
+
+private:
+	Byte m_opaque00[0x40];
+	int m_xOrigin;
+	int m_yOrigin;
+	int m_width;
+	Rva00729300BitPlane *m_map;
+};
+
+void Rva00729D30Terrain::checkEdges( int xOffset, int yOffset, int width,
+	bool *top, bool *right, bool *bottom, bool *left )
+{
+	int xOrigin = m_xOrigin;
+	Rva00729300BitPlane *map = m_map;
+	int limitX = map->m_width - 1;
+	int limitY = map->m_height - 1;
+	int minX = xOrigin + xOffset;
+	int minY = m_yOrigin + yOffset;
+	int maxX = xOffset + width;
+	if( m_xOrigin + maxX > limitX )
+		maxX = limitX - m_xOrigin;
+	register int maxY = yOffset + width;
+	if( m_yOrigin + maxY > limitY )
+		maxY = limitY - m_yOrigin;
+
+	int halfX = (maxX - xOffset) / 2;
+	int halfY = (maxY - yOffset) / 2;
+	int centerX = xOffset + halfX;
+	int centerY = yOffset + halfY;
+	*top = *right = *bottom = *left = false;
+	if( !m_map->test( minX, m_yOrigin + centerY ) )
+		*top = true;
+	if( !m_map->test( m_xOrigin + centerX, maxY + m_yOrigin ) )
+		*right = true;
+	if( !m_map->test( m_xOrigin + maxX, m_yOrigin + centerY ) )
+		*bottom = true;
+	if( !m_map->test( m_xOrigin + centerX, minY ) )
+		*left = true;
 }
