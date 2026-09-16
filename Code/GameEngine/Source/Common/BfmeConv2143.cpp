5c45582e9c4285110f9184a9b91bde9767cd5652
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-16 23:20:20 +0200

    fix: type BFME audio selector thunk

--- Code/GameEngine/Source/Common/BfmeConv2143.cpp
+++ Code/GameEngine/Source/Common/BfmeConv2143.cpp
@@ -9,7 +9,7 @@ public:
 	virtual void bfmeV14QR();
 	virtual void bfmeV18QR();
 	virtual void bfmeV1CQR();
-	virtual void *bfmeV20QR();
+	virtual int bfmeV20QR();
 };
 
 class BfmeMidQR
@@ -23,7 +23,7 @@ class BfmeHostQR
 {
 public:
 	void *bfmeApplyQR(void *arg);
-	void bfmeStoreQR(void *arg, void *val);
+	void Rva00417430AudioSelector(void *out, int selector);
 
 	unsigned char m_bfmeHeadQR[0xfc];
 	BfmeMidQR *m_bfmeFCQR;
@@ -32,13 +32,13 @@ public:
 void *BfmeHostQR::bfmeApplyQR(void *arg)
 {
 	BfmeMidQR *p = m_bfmeFCQR;
-	void *r = 0;
-	void *volatile deadQR = r;
+	int r = 0;
+	int volatile deadQR = r;
 
-	if (p != r)
+	if (p != 0)
 		r = p->m_bfme200QR->bfmeV20QR();
 
-	bfmeStoreQR(arg, r);
+	Rva00417430AudioSelector(arg, r);
 
 	return arg;
 }
