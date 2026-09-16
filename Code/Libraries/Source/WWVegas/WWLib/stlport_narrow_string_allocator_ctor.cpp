a76884115a0f88b76c3e0771ed625e5d6258c944
Gabe Borges <gbborges@proton.me>
2026-09-15 23:01:59 -0400

    feat(stlport): convert narrow allocator constructor
    The decorated STLport signature and vendor/stlport/stl/_string.h constructor
    prove the identity at retail 0x00830700. The clean body now lives in
    Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_string_allocator_ctor.cpp so
    the generated dump row points to the verified source.

--- Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_string_allocator_ctor.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_string_allocator_ctor.cpp
@@ -1,12 +1,24 @@
-// ??0?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@QAE@ABV?$allocator@D@1@@Z
-// partial score=0.98 date=2026-09-06
 // cl: /DNDEBUG /MD /EHsc /Od /Ob2
+
+// Open-BFME5: STLport basic_string<char> allocator constructor at retail
+// 0x00830700, 124 bytes.
+//
+// vendor/stlport/stl/_string.h defines this constructor with an eight-element
+// allocation in _String_base, followed by _M_terminate_string in
+// basic_string. The decorated signature identifies the narrow STLport
+// specialization, and the retail body matches that source structure.
 
 namespace _STL
 {
 
 template <class T>
-class allocator {};
+class allocator
+{
+public:
+	allocator(void) __declspec(nothrow) {}
+	allocator(const allocator &) __declspec(nothrow) {}
+	~allocator(void) __declspec(nothrow) {}
+};
 
 template <class T>
 class char_traits {};
@@ -14,10 +26,10 @@ class char_traits {};
 struct true_type {};
 
 template <class Pointer, class Value, class Alloc>
-class _STLP_alloc_proxy
+class _STLP_alloc_proxy : public Alloc
 {
 public:
-	_STLP_alloc_proxy(const Alloc &, Pointer p) : _M_data(p) {}
+	_STLP_alloc_proxy(const Alloc &a, Pointer p) : Alloc(a), _M_data(p) {}
 
 	Pointer _M_data;
 };
@@ -29,43 +41,50 @@ public:
 	typedef allocator<T> allocator_type;
 	void _M_allocate_block(unsigned n);
 
-	__forceinline _String_base(const allocator_type &a)
+	_String_base(const allocator_type &a)
 		: _M_start(0), _M_finish(0), _M_end_of_storage(a, (T *)0) {}
 
-	~_String_base(void);
+	_String_base(const allocator_type &a, unsigned n)
+		: _M_start(0), _M_finish(0), _M_end_of_storage(a, (T *)0)
+	{
+		_M_allocate_block(n);
+	}
 
-	private:
+	~_String_base(void)
+	{
+		_M_deallocate_block();
+	}
+
+	void _M_deallocate_block(void);
+
 	T *_M_start;
-	protected:
 	T *_M_finish;
 	_STLP_alloc_proxy<T *, T, allocator_type> _M_end_of_storage;
 };
 
 template <class CharT, class Traits, class Alloc>
-class basic_string : public _String_base<CharT, Alloc>
+class basic_string : protected _String_base<CharT, Alloc>
 {
 public:
 	typedef allocator<CharT> allocator_type;
 
 	basic_string(const allocator_type &a);
 
-	__forceinline void _M_terminate_string_aux(const true_type &)
+	void __declspec(nothrow) _M_terminate_string_aux(const true_type &)
 	{
 		*(this->_M_finish) = 0;
 	}
 
-	__forceinline void _M_terminate_string(void)
+	void __declspec(nothrow) _M_terminate_string(void)
 	{
 		_M_terminate_string_aux(true_type());
 	}
-
 };
 
 template <class CharT, class Traits, class Alloc>
 basic_string<CharT, Traits, Alloc>::basic_string(const allocator_type &a)
-	: _String_base<CharT, Alloc>(a)
+	: _String_base<CharT, Alloc>(a, 8)
 {
-	_M_allocate_block(8);
 	_M_terminate_string();
 }
 
