8628035368c60d434e76b0e99333d51d487942df
Jonathan <jonf38@users.noreply.github.com>
2026-09-16 23:55:51 -0400

    Improve BfmeHost limit stack shape

--- reverse/attempts/0x003a40a0.cpp
+++ reverse/attempts/0x003a40a0.cpp
@@ -1,5 +1,5 @@
 // ?bfmeLimitEO@BfmeHostEO@@QAEMXZ
-// partial score=0.93 date=2026-09-08
+// partial score=0.95 date=2026-09-16
 extern "C" double sqrt(double x);
 
 #pragma intrinsic(sqrt)
@@ -47,10 +47,11 @@ float BfmeHostEO::bfmeLimitEO()
 	volatile float dx = m_bfmeX1EO - m_bfmeX0EO;
 	dy -= m_bfmeY0EO;
 
-	float len = (float)sqrt(dx * dx + dy * dy);
+	float len[2];
+	len[0] = (float)sqrt(dx * dx + dy * dy);
 
 	if (g_bfmeKeyboardEO->bfmeIsShiftEO() && TheWritableGlobalData->m_bfmeFlagEO)
 		*(volatile float *)&cap = *(volatile float *)&cap * g_bfmeDirectionWeight1285;
 
-	return bfmeMinEO(cap, len);
+	return bfmeMinEO(cap, len[0]);
 }
