6afbc219f106fdb7fd6243c7a3f36094590b88f4
dginovker <danielginovker@gmail.com>
2026-09-18 00:37:06 +0900

    Merge campaign room accessor TU

--- Code/GameEngine/Source/Common/Glo012F1024Type_selectIndexedEntry.cpp
+++ Code/GameEngine/Source/Common/Glo012F1024Type_selectIndexedEntry.cpp
@@ -3,6 +3,25 @@
 
 class AsciiString;
 class Glo012F1024Item;
+class BfmeSubYC;
+class BfmeElemYC;
+
+class BfmeRoomYC
+{
+public:
+	BfmeRoomYC(const BfmeRoomYC &other);
+	~BfmeRoomYC();
+
+	unsigned char m_bfmeBytesYC[0x20];
+};
+
+class BfmeSubYC
+{
+public:
+	BfmeRoomYC bfmeRoomYC();
+};
+
+extern BfmeRoomYC g_bfmeDefaultYC;
 
 class GlobalData
 {
@@ -26,9 +45,27 @@ class BfmeEntryVector
 {
 public:
 	int bfmeSize(void) const { return (int)(m_bfmeFinish - m_bfmeStart); }
+	int bfmeSizeYC(void) const { return (int)(m_bfmeFinish - m_bfmeStart); }
 
 	Glo012F1024Entry *m_bfmeStart;
 	Glo012F1024Entry *m_bfmeFinish;
+};
+
+class BfmeElemYC
+{
+public:
+	BfmeSubYC *bfmeSubYC(void);
+
+	unsigned char m_bfmePadYC[0x20];
+};
+
+class BfmeVecYC
+{
+public:
+	int bfmeSizeYC(void) const { return (int)(m_bfmeEndYC - m_bfmeBeginYC); }
+
+	BfmeElemYC *m_bfmeBeginYC;
+	BfmeElemYC *m_bfmeEndYC;
 };
 
 class Glo012F1024Type
@@ -36,11 +73,16 @@ class Glo012F1024Type
 public:
 	Glo012F1024Item *selectEntry(AsciiString *key);
 	void step(void);
+	BfmeRoomYC Rva003B3950Room(void);
 
 private:
 	char m_pad00[0x0C];
 	int m_bfmeIndex;
-	BfmeEntryVector m_bfmeEntries;
+	union
+	{
+		BfmeEntryVector m_bfmeEntries;
+		BfmeVecYC m_bfmeVecYC;
+	};
 	char m_pad18[0x1C - 0x18];
 	bool m_at1C;
 	bool m_bfmeArmed;
@@ -73,4 +115,19 @@ void Glo012F1024Type::step(void)
 	m_bfmeArmed = false;
 
 	m_bfmeEntries.m_bfmeStart[m_bfmeIndex].bfmeStep();
+}
+
+#define m_bfmeIndexYC m_bfmeIndex
+
+BfmeRoomYC Glo012F1024Type::Rva003B3950Room(void)
+{
+	if (m_bfmeIndexYC >= 0
+		&& (unsigned int)m_bfmeIndexYC < (unsigned int)m_bfmeVecYC.bfmeSizeYC())
+	{
+		BfmeElemYC *begin = m_bfmeVecYC.m_bfmeBeginYC;
+		BfmeSubYC *sub = begin[m_bfmeIndexYC].bfmeSubYC();
+		return sub->bfmeRoomYC();
+	}
+
+	return g_bfmeDefaultYC;
 }
