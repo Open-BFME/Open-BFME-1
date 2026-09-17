2891a17c0b0420787c440f69032976eef096b90a
dginovker <danielginovker@gmail.com>
2026-09-18 03:36:46 +0900

    Merge paired string record index TU

--- Code/GameEngine/Source/Common/Rva00359330StringRecordFindIndex.cpp
+++ Code/GameEngine/Source/Common/Rva00359330StringRecordFindIndex.cpp
@@ -51,3 +51,45 @@ int Rva00359330StringRecordTable::findRecordIndex( AsciiString *name )
 
 	return -1;
 }
+
+struct Rva00359530Record
+{
+	int m_previous;
+	int m_next;
+	AsciiString m_name;
+	unsigned char m_released;
+	unsigned char m_pad;
+	unsigned short m_references;
+	void *m_nodes;
+};
+
+class Rva00359530StringRecordTable
+{
+public:
+	int findRecordIndex( AsciiString *name );
+
+private:
+	int findNameIndex( AsciiString *name );
+
+	int *m_nameIndexesBegin;
+	int *m_nameIndexesEnd;
+	int *m_nameIndexesCapacity;
+	Rva00359530Record *m_records;
+	int m_10;
+	int m_14;
+	int m_freeHead;
+	int m_activeTail;
+};
+
+int Rva00359530StringRecordTable::findRecordIndex( AsciiString *name )
+{
+	unsigned int nameIndex = findNameIndex( name );
+	if ( nameIndex < m_nameIndexesEnd - m_nameIndexesBegin )
+	{
+		int recordIndex = m_nameIndexesBegin[nameIndex];
+		if ( ((AsciiStringCompareShim &)m_records[recordIndex].m_name).compare( *name ) == 0 )
+			return recordIndex;
+	}
+
+	return -1;
+}
