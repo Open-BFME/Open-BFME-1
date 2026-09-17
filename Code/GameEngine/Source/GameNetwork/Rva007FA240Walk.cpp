b64dfb8f6e7483ff047767a9274a02e175e3e796
dginovker <danielginovker@gmail.com>
2026-09-18 02:39:53 +0900

    Merge Rva007FA2C0 timeout helper

--- Code/GameEngine/Source/GameNetwork/Rva007FA240Walk.cpp
+++ Code/GameEngine/Source/GameNetwork/Rva007FA240Walk.cpp
@@ -20,6 +20,33 @@ public:
 
 void *Rva007F93E0(void *a, void *b, void *svc);
 
+struct Rva007EB810Diag
+{
+	virtual void v0();
+	virtual void v1();
+	virtual void log(int level, const char *format, ...);
+	virtual void fail(const char *expr, const char *file, int line);
+};
+
+extern Rva007EB810Diag *Rva007EB810Get();
+
+class Rva007E8810Message
+{
+public:
+	Rva007E8810Message() throw();
+	~Rva007E8810Message() throw();
+	void setError(int code);
+};
+
+struct Rva007FA170Slot
+{
+	void *m_00;
+	int m_04;
+	Rva007E8810Message *m_08;
+	void (__cdecl *m_0C)(Rva007E8810Message *msg, void *arg);
+	void *m_10;
+};
+
 class Rva007FA2C0
 {
 public:
@@ -28,7 +55,8 @@ public:
 	void dispatch(void *a, void *b);
 	int onAccept(void *a);
 	void onBusy(void *a);
-	void helper(void *slot, int line);
+	void helper(void *slot, int line) throw();
+	void clearSlot(void *slot);
 
 private:
 	char m_pad[0x18];
@@ -91,4 +119,32 @@ void Rva007FA2C0::dispatch(void *a, void *b)
 		onBusy(a);
 	else
 		Rva007F93E0(a, b, m_1C);
+}
+
+void Rva007FA2C0::helper(void *slot, int line) throw()
+{
+	char buf[0x34];
+	Rva007FA170Slot *s = (Rva007FA170Slot *)slot;
+	if (s)
+	{
+		if (s->m_08)
+		{
+			Rva007EB810Get()->log(0, "--- timeout of Block request\n");
+			s->m_08->setError(line);
+			s->m_04 = 2;
+			return;
+		}
+		if (s->m_0C)
+		{
+			Rva007EB810Get()->log(0, "--- timeout of Nonblock request\n");
+			Rva007E8810Message *msg = (Rva007E8810Message *)buf;
+			msg->Rva007E8810Message::Rva007E8810Message();
+			msg->setError(line);
+			s->m_0C(msg, s->m_10);
+			clearSlot(s);
+			msg->Rva007E8810Message::~Rva007E8810Message();
+			return;
+		}
+		Rva007EB810Get()->log(0, "--- ignored timeout\n");
+	}
 }
