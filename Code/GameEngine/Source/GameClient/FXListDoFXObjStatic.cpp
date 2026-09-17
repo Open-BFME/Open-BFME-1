a7160a4419cbebfe18442fdb94d990e6c56eea7e
dginovker <danielginovker@gmail.com>
2026-09-18 03:00:31 +0900

    Merge FXList static wrappers

--- Code/GameEngine/Source/GameClient/FXListDoFXObjStatic.cpp
+++ Code/GameEngine/Source/GameClient/FXListDoFXObjStatic.cpp
@@ -4,14 +4,20 @@
 // through ILT 0x00011F77 (body 0x0042DAA0) and skips the play when it is set.
 
 class Object;
+struct Coord3D;
+class Matrix3D;
 
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameClient/FXList.h
 class FXList
 {
 public:
 	static void doFXObj(const FXList *fx, const Object *primary, const Object *secondary);
+	static void doFXPos(const FXList *fx, const Coord3D *position,
+		const Matrix3D *transform, float speed, const Coord3D *secondary);
 	bool bfmeIsBlocked(void) const;
 	void doFXObj(const Object *primary, const Object *secondary) const;
+	void doFXPos(const Coord3D *position, const Matrix3D *transform,
+		float speed, const Coord3D *secondary) const;
 };
 
 // ?doFXObj@FXList@@SAXPBV1@PBVObject@@1@Z
@@ -21,5 +27,16 @@ void FXList::doFXObj(const FXList *fx, const Object *primary, const Object *seco
 	{
 		if (!fx->bfmeIsBlocked())
 			fx->doFXObj(primary, secondary);
+	}
+}
+
+// ?doFXPos@FXList@@SAXPBV1@PBUCoord3D@@PBVMatrix3D@@M1@Z
+void FXList::doFXPos(const FXList *fx, const Coord3D *position,
+	const Matrix3D *transform, float speed, const Coord3D *secondary)
+{
+	if (fx)
+	{
+		if (!fx->bfmeIsBlocked())
+			fx->doFXPos(position, transform, speed, secondary);
 	}
 }
