28c0ab9595db3070a5ef2736e3e69f99c0456be0
Gabe Borges <gbborges@proton.me>
2026-09-17 17:42:02 -0400

    feat(common): convert opaque trigger callback
    The CastleBehavior literal at 0x01083C50, the bfmeGo1098B callback thunk, and the resolved Object, NameKeyGenerator, Module, and PolygonTrigger callees establish the callback evidence while its address-derived name remains opaque. I placed the 311-byte body at 0x00372210 in Code/GameEngine/Source/Common/BfmeConv1098.cpp because that file already owns the matched 1098 caller and thunk declaration.

--- Code/GameEngine/Source/Common/BfmeConv1098.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1098.cpp
@@ -1,5 +1,12 @@
 // Open-BFME5 conversions.
 
+typedef int Int;
+
+struct Coord3D
+{
+	float x, y, z;
+};
+
 struct BfmeQ1098
 {
 	char m_bfmePad[0xbd];
@@ -39,12 +46,134 @@ class BfmeCtx1098
 {
 public:
 	void bfmeInit1098(int a, int b);
-	char m_bfmePad[0x18];
+	char m_bfmePad[0x0c];
+	Coord3D m_bfmeCoord;
 	char m_bfme18;
 	char m_bfmePad1[3];
 };
 
 void __cdecl bfmeCb1098(void);
+
+enum NameKeyType
+{
+	NAMEKEY_INVALID = 0
+};
+
+class NameKeyGenerator
+{
+public:
+	NameKeyType nameToKey(const char *name);
+};
+
+extern NameKeyGenerator *TheNameKeyGenerator;
+
+class Module;
+class Overridable;
+class PolygonTrigger;
+
+class Rva00372210Vector
+{
+public:
+	inline PolygonTrigger **begin() const
+	{
+		return m_begin;
+	}
+	inline int size() const
+	{
+		return (unsigned int)((char *)m_end - (char *)m_begin);
+	}
+private:
+	PolygonTrigger **m_begin;
+	PolygonTrigger **m_end;
+};
+
+class Object
+{
+public:
+	char m_bfmeHead[4];
+	Overridable *m_bfmeTemplate;
+	Module *findModule(NameKeyType key) const;
+	char m_bfmePad08[0xe0];
+};
+
+class Module
+{
+public:
+	char m_bfmePad[0xe8];
+	Rva00372210Vector m_triggers;
+};
+
+class PolygonTrigger
+{
+public:
+	bool bfmeContainsPointAt0018FA20(Coord3D &point) const;
+};
+
+class Overridable
+{
+public:
+	const Overridable *getFinalOverride() const;
+	char m_bfmeHead[4];
+	Overridable *m_nextOverride;
+	char m_bfmePad[0xcc];
+	unsigned int m_bfmeFlags;
+};
+
+class Rva00372210
+{
+public:
+	static int __cdecl callback(Object *object,
+		BfmeCtx1098 *context);
+};
+
+// ?callback@Rva00372210@@SAHPAVObject@@PAVBfmeCtx1098@@@Z
+int __cdecl Rva00372210::callback(Object *objectParameter,
+	BfmeCtx1098 *context)
+{
+	Object *object = objectParameter;
+	Module *module;
+	const Overridable *objectTemplate = object->m_bfmeTemplate;
+	if (objectTemplate == 0)
+	{
+		objectTemplate = 0;
+		goto templateDone;
+	}
+	if (objectTemplate->m_nextOverride != 0)
+		objectTemplate = objectTemplate->m_nextOverride->getFinalOverride();
+templateDone:
+	PolygonTrigger *trigger;
+	if ((objectTemplate->m_bfmeFlags & 0x00800000) == 0)
+		return 1;
+
+	static NameKeyType key =
+		TheNameKeyGenerator->nameToKey((const char *)0x01083c50);
+	module = object->findModule(key);
+	if (module == 0)
+		return 1;
+	if ((module->m_triggers.size() & ~3) <= 0)
+		return 1;
+	BfmeCtx1098 *ctx = context;
+	Int index = 0;
+
+	do
+	{
+		if (index >= (unsigned int)(module->m_triggers.size() >> 2))
+			trigger = 0;
+		else
+			trigger = *(module->m_triggers.begin() + index);
+		if (trigger->bfmeContainsPointAt0018FA20(
+			*(Coord3D *)context))
+			return 0;
+
+		if (!ctx->m_bfme18 &&
+			trigger->bfmeContainsPointAt0018FA20(
+			ctx->m_bfmeCoord))
+			ctx->m_bfme18 = 1;
+		++index;
+	} while (index < module->m_triggers.size() >> 2);
+
+	return 1;
+}
 
 char bfmeGo1098B(int a, int b, int c)
 {
