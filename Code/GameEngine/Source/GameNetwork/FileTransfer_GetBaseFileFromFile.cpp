9da505eaa329bfe6749c8646d116a67fdc0f4f04
dginovker <danielginovker@gmail.com>
2026-09-18 00:02:10 +0900

    Merge FileTransfer extension TU

--- Code/GameEngine/Source/GameNetwork/FileTransfer_GetBaseFileFromFile.cpp
+++ Code/GameEngine/Source/GameNetwork/FileTransfer_GetBaseFileFromFile.cpp
@@ -10,6 +10,7 @@ class StringBase
 {
 	friend class AsciiString;
 	friend AsciiString GetBaseFileFromFile(AsciiString fname);
+	friend AsciiString GetExtensionFromFile(AsciiString fname);
 
 	StringBase() : m_data(0) {}
 	StringBase(const StringBase<T> &other);
@@ -58,6 +59,7 @@ public:
 
 	static const AsciiString TheEmptyString;
 	friend AsciiString GetBaseFileFromFile(AsciiString fname);
+	friend AsciiString GetExtensionFromFile(AsciiString fname);
 };
 
 AsciiString GetBaseFileFromFile(AsciiString fname)
@@ -73,4 +75,32 @@ AsciiString GetBaseFileFromFile(AsciiString fname)
 		return buffer;
 	}
 	return AsciiString::TheEmptyString;
+}
+
+// ?GetExtensionFromFile@@YA?AVAsciiString@@V1@@Z
+AsciiString GetExtensionFromFile(AsciiString fname)
+{
+	StringBase<char>::Header *data = fname.m_data;
+	const char *start;
+	unsigned int len;
+	if (data)
+	{
+		start = &data->data[0];
+		len = data->length;
+	}
+	else
+	{
+		start = (const char *)0x0107388B;
+		len = 0;
+	}
+
+	const char *p = start + len;
+	while (p != start)
+	{
+		char ch = p[-1];
+		--p;
+		if (ch == '.')
+			return p + 1;
+	}
+	return fname;
 }
