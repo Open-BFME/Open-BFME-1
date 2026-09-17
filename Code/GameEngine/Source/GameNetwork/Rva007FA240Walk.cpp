848508f250a658317360204eb2169028ad55b274
dginovker <danielginovker@gmail.com>
2026-09-18 02:37:18 +0900

    Merge Rva007FA2C0 dispatch

--- Code/GameEngine/Source/GameNetwork/Rva007FA240Walk.cpp
+++ Code/GameEngine/Source/GameNetwork/Rva007FA240Walk.cpp
@@ -3,17 +3,41 @@
 // above the argument call helper(slot, 0x9A) and suppress the +0x18 store;
 // otherwise keep the unsigned minimum in +0x18.
 
+class Rva007FA2C0Hub
+{
+public:
+	virtual void v0();
+	virtual void v1();
+	virtual void v2();
+	virtual void v3();
+	virtual void v4();
+	virtual void v5();
+	virtual void v6();
+	virtual void v7();
+	virtual void v8();
+	virtual bool v9(void *a);
+};
+
+void *Rva007F93E0(void *a, void *b, void *svc);
+
 class Rva007FA2C0
 {
 public:
 	void walk(unsigned arg);
 	void scan(void *key);
+	void dispatch(void *a, void *b);
+	int onAccept(void *a);
+	void onBusy(void *a);
 	void helper(void *slot, int line);
 
 private:
 	char m_pad[0x18];
 	unsigned m_18;
-	char m_pad1[0x0C];
+	void *m_1C;
+	char m_pad20[4];
+	Rva007FA2C0Hub *m_24;
+	char m_pad28[0x684];
+	void *m_6AC;
 };
 
 void Rva007FA2C0::walk(unsigned arg)
@@ -57,4 +81,14 @@ void Rva007FA2C0::scan(void *key)
 	slot = 0;
 found:
 	helper(slot, (signed char)0x98);
+}
+
+void Rva007FA2C0::dispatch(void *a, void *b)
+{
+	if (m_24->v9(a))
+		onAccept(a);
+	else if (m_6AC)
+		onBusy(a);
+	else
+		Rva007F93E0(a, b, m_1C);
 }
