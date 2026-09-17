bbcfb2b30133a9be1c3136d745dab35021f21b9b
dginovker <danielginovker@gmail.com>
2026-09-17 23:19:38 +0900

    Merge PolygonTrigger bounds TU

--- Code/GameEngine/Source/GameLogic/Map/PolygonTrigger_pointInTrigger.cpp
+++ Code/GameEngine/Source/GameLogic/Map/PolygonTrigger_pointInTrigger.cpp
@@ -2,9 +2,11 @@
 // stlport
 
 #include <algorithm>
+#include <math.h>
 
 typedef bool Bool;
 typedef int Int;
+typedef float Real;
 
 // BFME float-coordinate overload: the original method name is not recovered.
 // Only the three scalar coordinates are used by this TU's ABI view.
@@ -147,4 +149,28 @@ Bool PolygonTrigger::bfmeContainsPointAt0018FA20(Coord3D &point) const
 			inside = !inside;
 	}
 	return inside;
+}
+
+// ?updateBounds@PolygonTrigger@@IBEXXZ
+void PolygonTrigger::updateBounds() const
+{
+	const Int BIG_INT = 0x7ffff0;
+	m_bounds.lo.x = m_bounds.lo.y = BIG_INT;
+	m_bounds.hi.x = m_bounds.hi.y = -BIG_INT;
+	Int i;
+	for (i = 0; i < m_numPoints; ++i)
+	{
+		if (m_points[i].x < m_bounds.lo.x)
+			m_bounds.lo.x = m_points[i].x;
+		if (m_points[i].y < m_bounds.lo.y)
+			m_bounds.lo.y = m_points[i].y;
+		if (m_points[i].x > m_bounds.hi.x)
+			m_bounds.hi.x = m_points[i].x;
+		if (m_points[i].y > m_bounds.hi.y)
+			m_bounds.hi.y = m_points[i].y;
+	}
+	m_boundsNeedsUpdate = 0;
+	Real halfWidth = (m_bounds.hi.x - m_bounds.lo.x) / 2.0f;
+	Real halfHeight = (m_bounds.hi.y + m_bounds.lo.y) / 2.0f;
+	m_radius = sqrt(halfHeight * halfHeight + halfWidth * halfWidth);
 }
