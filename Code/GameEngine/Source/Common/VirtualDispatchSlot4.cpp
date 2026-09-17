fa8fa2ea1e1ece2dd71ffe1ca8a9dd414b050189
dginovker <danielginovker@gmail.com>
2026-09-18 03:49:49 +0900

    Merge virtual dispatch slot TU

--- Code/GameEngine/Source/Common/VirtualDispatchSlot4.cpp
+++ Code/GameEngine/Source/Common/VirtualDispatchSlot4.cpp
@@ -16,3 +16,17 @@ void AddressThunk0015EA70::forwardSlot4()
 {
 	slot4();
 }
+
+class AddressThunk00105360
+{
+public:
+	virtual void slot0();
+	virtual void slot1();
+
+	void forwardSlot1();
+};
+
+void AddressThunk00105360::forwardSlot1()
+{
+	slot1();
+}
