7e8a1115cb68843299cc05e5b72ec8418d017d0d
dginovker <danielginovker@gmail.com>
2026-09-18 03:10:43 +0900

    Merge redundant LAN byte reader

--- Code/GameEngine/Source/GameNetwork/readByteFromBuffer.cpp
+++ Code/GameEngine/Source/GameNetwork/readByteFromBuffer.cpp
@@ -6,7 +6,27 @@
 typedef unsigned char UnsignedByte;
 
 extern "C" UnsignedByte *readByteFromBuffer(UnsignedByte *buffer,
-	UnsignedByte *out, UnsignedByte *end)
+		UnsignedByte *out, UnsignedByte *end)
+{
+	if (end != 0)
+	{
+		if (buffer > end)
+			return buffer;
+		if (buffer + 1 > end)
+			return buffer;
+	}
+	__asm
+	{
+		mov dl, byte ptr [eax]
+		mov ecx, dword ptr [esp + 8]
+		mov byte ptr [ecx], dl
+	}
+	return buffer + 1;
+}
+
+// The sibling LAN decoder entry point has the same bounded-reader body.
+extern "C" UnsignedByte *readByteFromBuffer_0068D9C0(UnsignedByte *buffer,
+		UnsignedByte *out, UnsignedByte *end)
 {
 	if (end != 0)
 	{
