d3d3b4b3891342d7a237b4c1d684f672c2770f93
dginovker <danielginovker@gmail.com>
2026-09-18 03:44:01 +0900

    Merge adjacent flag setter TU

--- Code/GameEngine/Source/Common/FlagSet0015BA70.cpp
+++ Code/GameEngine/Source/Common/FlagSet0015BA70.cpp
@@ -14,3 +14,18 @@ void AddressState0015BA70::setFlag()
 {
 	m_flag = true;
 }
+
+class AddressState0015BB30
+{
+public:
+	void setFlag();
+
+private:
+	unsigned char m_padding[0x44];
+	bool m_flag;
+};
+
+void AddressState0015BB30::setFlag()
+{
+	m_flag = true;
+}
