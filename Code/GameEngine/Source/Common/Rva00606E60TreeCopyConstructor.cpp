392728b2add911eaf53fa70bd3146212e23ed886
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-18 18:33:18 +0200

    fix: correct tree copy key and value identity

--- Code/GameEngine/Source/Common/Rva00606E60TreeCopyConstructor.cpp
+++ Code/GameEngine/Source/Common/Rva00606E60TreeCopyConstructor.cpp
@@ -1,26 +1,42 @@
 // cl: /DNDEBUG /DWIN32 /MD /EHsc /D_STLP_USE_STATIC_LIB
 // stlport
-// Anonymous STLport red-black-tree copy constructor at retail 0x00606E60.
-// The retail header node is 0x84 bytes; the mapped payload is therefore 0x70
-// bytes and has a 0x74-byte pair with its int key.
+// STLport red-black-tree copy constructor at retail 0x00606E60.
 
 #include <map>
 
-struct Rva00606E60Value
+class AsciiString
 {
-	char m_body[ 0x70 ];
-
-	Rva00606E60Value();
-	Rva00606E60Value( const Rva00606E60Value &other );
-	~Rva00606E60Value();
-	Rva00606E60Value &operator=( const Rva00606E60Value &other );
+public:
+	AsciiString();
+	AsciiString( const AsciiString &other );
+	~AsciiString();
+	AsciiString &operator=( const AsciiString &other );
+	int compare( const AsciiString &other ) const;
+private:
+	void *m_data;
 };
 
-typedef _STL::pair<const int, Rva00606E60Value> Rva00606E60Pair;
-typedef _STL::_Rb_tree<int, Rva00606E60Pair,
-	_STL::_Select1st<Rva00606E60Pair>, _STL::less<int>,
+inline bool operator<( const AsciiString &left, const AsciiString &right )
+{
+	return left.compare( right ) < 0;
+}
+
+class AudioEventRTS
+{
+public:
+	AudioEventRTS();
+	AudioEventRTS( const AudioEventRTS &other );
+	virtual ~AudioEventRTS();
+	AudioEventRTS &operator=( const AudioEventRTS &other );
+private:
+	unsigned char m_data[ 0x6C ];
+};
+
+typedef _STL::pair<const AsciiString, AudioEventRTS> Rva00606E60Pair;
+typedef _STL::_Rb_tree<AsciiString, Rva00606E60Pair,
+	_STL::_Select1st<Rva00606E60Pair>, _STL::less<AsciiString>,
 	_STL::allocator<Rva00606E60Pair> > Rva00606E60Tree;
 
-template class _STL::_Rb_tree<int, Rva00606E60Pair,
-	_STL::_Select1st<Rva00606E60Pair>, _STL::less<int>,
+template class _STL::_Rb_tree<AsciiString, Rva00606E60Pair,
+	_STL::_Select1st<Rva00606E60Pair>, _STL::less<AsciiString>,
 	_STL::allocator<Rva00606E60Pair> >;
