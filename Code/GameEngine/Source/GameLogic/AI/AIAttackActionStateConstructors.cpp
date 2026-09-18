773d245f9f145e0282e9d912f6c1f84850b327cc
Jonathan <jonathan@users.noreply.github.com>
2026-09-17 23:11:50 -0400

    Unify attack-action state constructors and name notification member

--- Code/GameEngine/Source/GameLogic/AI/AIAttackActionStateConstructors.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIAttackActionStateConstructors.cpp
@@ -8,6 +8,7 @@
 #include "Common/AsciiString.h"
 
 class StateMachine;
+class NotifyWeaponFiredInterface;
 
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/Common/StateMachine.h
 class State
@@ -42,4 +43,26 @@ AIAttackAimAtTargetState::AIAttackAimAtTargetState(StateMachine *machine, bool a
 	m_setLocomotor = false;
 	m_isAttackingObject = attackingObject;
 	m_isForceAttacking = forceAttacking;
+}
+
+// The typed NotifyWeaponFiredInterface* argument is stored at this+0x24 by
+// retail 0x001712EB; AIStateMachine.h names this non-owning member m_att.
+// The BFME-only bool at +0x28 remains unnamed.
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
+class AIAttackFireWeaponState : public State
+{
+public:
+	AIAttackFireWeaponState(StateMachine *machine, NotifyWeaponFiredInterface * notify);
+
+private:
+	NotifyWeaponFiredInterface * m_att;
+	bool m_28;
+};
+
+// ??0AIAttackFireWeaponState@@QAE@PAVStateMachine@@PAVNotifyWeaponFiredInterface@@@Z
+AIAttackFireWeaponState::AIAttackFireWeaponState(StateMachine *machine, NotifyWeaponFiredInterface * notify) :
+	State(machine, "AIAttackFireWeaponState"),
+	m_att(notify),
+	m_28(false)
+{
 }
