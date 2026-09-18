a280449e6581aabc8b794c4216d92680fd32c800
Jonathan <jonf38@users.noreply.github.com>
2026-09-18 09:08:43 -0400

    Convert address-derived dword getter cluster

--- Code/GameEngine/Source/Common/Rva00207980DwordGetter.cpp
+++ Code/GameEngine/Source/Common/Rva00207980DwordGetter.cpp
@@ -11,3 +11,31 @@ __declspec(noinline) int Rva00207980Owner::Rva00207980_Get() const
 {
 	return value;
 }
+
+class Rva002078B0Owner
+{
+public:
+	__declspec(noinline) int Rva002078B0_Get() const;
+
+	char fields[4];
+	int value;
+};
+
+__declspec(noinline) int Rva002078B0Owner::Rva002078B0_Get() const
+{
+	return value;
+}
+
+class Rva00207960Owner
+{
+public:
+	__declspec(noinline) int Rva00207960_Get() const;
+
+	char fields[4];
+	int value;
+};
+
+__declspec(noinline) int Rva00207960Owner::Rva00207960_Get() const
+{
+	return value;
+}
