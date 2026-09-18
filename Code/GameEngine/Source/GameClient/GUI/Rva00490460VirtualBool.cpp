e89ebc713e6f72a09f46d4e76b854a07be231d17
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-18 19:00:17 +0200

    Add 0x00490460 bool stub

---
+++ Code/GameEngine/Source/GameClient/GUI/Rva00490460VirtualBool.cpp
@@ -0,0 +1,12 @@
+// cl: /O2 /Ob2 /G6 /DNDEBUG /MD /EHs-c-
+
+class Rva00490460VirtualBool
+{
+public:
+	virtual bool returnTrue();
+};
+
+bool Rva00490460VirtualBool::returnTrue()
+{
+	return true;
+}
