7a4cc0bc35ba730a8e9e523768b26ecabf97cb8f
Jonathan <jonf38@users.noreply.github.com>
2026-09-18 18:15:50 -0400

    Match W3D display string color setter

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DDisplayStringColors.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DDisplayStringColors.cpp
@@ -11,6 +11,7 @@ class W3DDisplayString
 public:
 	void setTextColor(Color *colors);
 	void setDropColor(Color *colors);
+	void setColors006F4690(Color textColor, Color dropColor);
 
 private:
 	unsigned char m_unmodelled_000[0x1CC];
@@ -61,4 +62,47 @@ void W3DDisplayString::setDropColor(Color *colors)
 	if (!m_colorsChanged && m_dropColors[3] != colors[3])
 		m_colorsChanged = true;
 	m_dropColors[3] = colors[3];
+}
+
+void W3DDisplayString::setColors006F4690(Color textColor, Color dropColor)
+{
+	if (!m_colorsChanged && m_textColors[0] != textColor)
+		m_colorsChanged = true;
+	m_textColors[0] = textColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_textColors[1] != textColor)
+		m_colorsChanged = true;
+	m_textColors[1] = textColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_textColors[2] != textColor)
+		m_colorsChanged = true;
+	m_textColors[2] = textColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_textColors[3] != textColor)
+		m_colorsChanged = true;
+	m_textColors[3] = textColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_dropColors[0] != dropColor)
+		m_colorsChanged = true;
+	m_dropColors[0] = dropColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_dropColors[1] != dropColor)
+		m_colorsChanged = true;
+	m_dropColors[1] = dropColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_dropColors[2] != dropColor)
+		m_colorsChanged = true;
+	m_dropColors[2] = dropColor;
+	_ReadWriteBarrier();
+
+	if (!m_colorsChanged && m_dropColors[3] != dropColor)
+		m_colorsChanged = true;
+	_ReadWriteBarrier();
+	m_dropColors[3] = dropColor;
 }
