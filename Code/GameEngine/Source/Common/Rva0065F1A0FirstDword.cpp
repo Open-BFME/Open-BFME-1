b582d3b12ba236ca62d91af3a53b5c634574c272
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-18 16:17:43 +0200

    feat(common): convert 0065F1A0 dword getter

---
+++ Code/GameEngine/Source/Common/Rva0065F1A0FirstDword.cpp
@@ -0,0 +1,16 @@
+// cl: /O2 /MD /EHsc-
+
+class Rva0065F1A0Owner
+{
+public:
+	__declspec(noinline) unsigned int get(void) const;
+
+	unsigned int m_value;
+};
+
+__declspec(noinline) unsigned int Rva0065F1A0Owner::get(void) const
+{
+	return m_value;
+}
+
+// ?get@Rva0065F1A0Owner@@QBEIXZ
