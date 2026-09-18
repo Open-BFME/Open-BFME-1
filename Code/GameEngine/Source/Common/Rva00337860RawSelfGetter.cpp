58d4c066f18f6cad80a08a638ba9df93b07cbc57
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-18 17:55:20 +0200

    feat: recover 00337860 raw getter

---
+++ Code/GameEngine/Source/Common/Rva00337860RawSelfGetter.cpp
@@ -0,0 +1,13 @@
+// ?get@Rva00337860RawSelf@@QBEIXZ
+// The retail body returns the receiver as one raw 32-bit value.
+
+class Rva00337860RawSelf
+{
+public:
+	unsigned int get() const;
+};
+
+unsigned int Rva00337860RawSelf::get() const
+{
+	return (unsigned int)this;
+}
