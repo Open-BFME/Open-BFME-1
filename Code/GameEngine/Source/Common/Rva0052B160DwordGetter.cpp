63e3dcad703c07ea429c510f1d6df2993f20b975
Gabe Borges <gbborges@proton.me>
2026-09-18 03:30:30 -0400

    feat(common): convert 0052b160 dword getter
    The carved boundary, one caller, and retail `mov eax, [ecx]; ret` sequence prove a dword getter from the object start at 0x0052B160. The exact 3-byte body lives in Code/GameEngine/Source/Common/Rva0052B160DwordGetter.cpp under an address-derived class because no owner name is proven.

---
+++ Code/GameEngine/Source/Common/Rva0052B160DwordGetter.cpp
@@ -0,0 +1,15 @@
+// cl: /O2
+
+class Rva0052B160DwordGetter
+{
+public:
+    unsigned int getValue();
+
+private:
+    unsigned int m_value;
+};
+
+unsigned int Rva0052B160DwordGetter::getValue()
+{
+    return m_value;
+}
