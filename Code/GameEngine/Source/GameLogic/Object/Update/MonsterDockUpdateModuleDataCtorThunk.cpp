58c238c3e8deddbedcd8be2272bd5b853e145460
dginovker <danielginovker@gmail.com>
2026-09-19 00:13:14 +0900

    Merge MonsterDock module-data lifecycle

--- Code/GameEngine/Source/GameLogic/Object/Update/MonsterDockUpdateModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/MonsterDockUpdateModuleDataCtorThunk.cpp
@@ -1,6 +1,8 @@
 // cl: /DNDEBUG /MD /EHsc
 // Open-BFME5: retail-layout C++ conversion of MonsterDockUpdateModuleData.
 
+#include "../../../../Include/GameLogic/Rva0039D550.h"
+
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/DockUpdate.h
 class DockUpdateModuleData
 {
@@ -38,4 +40,13 @@ MonsterDockUpdateModuleData::MonsterDockUpdateModuleData() :
 	m_member(),
 	m_initialDockCount(0)
 {
+}
+
+// Retail does not rewrite the derived vtable before member teardown.
+class __declspec(novtable) MonsterDockUpdateModuleData;
+
+// ??1MonsterDockUpdateModuleData@@UAE@XZ
+MonsterDockUpdateModuleData::~MonsterDockUpdateModuleData()
+{
+	reinterpret_cast<Rva0039D550 *>(&m_member)->~Rva0039D550();
 }
