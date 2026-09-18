6996c35badc31c52a31b4ee04667f15c8ed341a7
Gabe Borges <gbborges@proton.me>
2026-09-18 03:50:44 -0400

    feat(common): convert 001afd40 return-this body
    The carved boundary at retail RVA 0x001AFD40 contains only mov eax,ecx;ret. Place the address-derived Rva001AFD40::returnThis body in Code/GameEngine/Source/Common/Rva001AFD40Identity.cpp. Keep the address token because no named caller proves a stronger name.

---
+++ Code/GameEngine/Source/Common/Rva001AFD40Identity.cpp
@@ -0,0 +1,12 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva001AFD40
+{
+public:
+	Rva001AFD40 *returnThis(void);
+};
+
+Rva001AFD40 *Rva001AFD40::returnThis(void)
+{
+	return this;
+}
