7bf140cfdbe42675d6122f417544787a29885939
dginovker <danielginovker@gmail.com>
2026-09-19 00:33:19 +0900

    Merge ModelConditionSoundSelector module-data lifecycle

--- Code/GameEngine/Source/GameLogic/Object/Behavior/ModelConditionSoundSelector.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/ModelConditionSoundSelector.cpp
@@ -1,24 +1,47 @@
 // cl: /DNDEBUG /MD /EHsc
 
-// Open-BFME5: ModelConditionSoundSelectorClientBehaviorModuleData default ctor.
+// Open-BFME5: ModelConditionSoundSelectorClientBehaviorModuleData lifecycle.
 
-class ModelConditionSoundSelectorClientBehaviorModuleData
+class ModelConditionSoundSelectorClientBehaviorModuleDataMember
+{
+public:
+	~ModelConditionSoundSelectorClientBehaviorModuleDataMember();
+
+	unsigned int m_00;
+	unsigned int m_04;
+	unsigned int m_08;
+};
+
+class ModelConditionSoundSelectorClientBehaviorModuleDataBase
+{
+public:
+	virtual ~ModelConditionSoundSelectorClientBehaviorModuleDataBase() {}
+
+private:
+	unsigned char m_pad[0x4];
+};
+
+class ModelConditionSoundSelectorClientBehaviorModuleData : public ModelConditionSoundSelectorClientBehaviorModuleDataBase
 {
 public:
 	ModelConditionSoundSelectorClientBehaviorModuleData();
 	virtual ~ModelConditionSoundSelectorClientBehaviorModuleData();
 
 private:
-	unsigned int m_gap4;
-	unsigned int m_08;
-	unsigned int m_0c;
-	unsigned int m_10;
+	ModelConditionSoundSelectorClientBehaviorModuleDataMember m_member;
 };
 
 // ??0ModelConditionSoundSelectorClientBehaviorModuleData@@QAE@XZ
 ModelConditionSoundSelectorClientBehaviorModuleData::ModelConditionSoundSelectorClientBehaviorModuleData()
 {
-	m_08 = 0;
-	m_0c = 0;
-	m_10 = 0;
+	m_member.m_00 = 0;
+	m_member.m_04 = 0;
+	m_member.m_08 = 0;
+}
+
+class __declspec(novtable) ModelConditionSoundSelectorClientBehaviorModuleData;
+
+// ??1ModelConditionSoundSelectorClientBehaviorModuleData@@UAE@XZ
+ModelConditionSoundSelectorClientBehaviorModuleData::~ModelConditionSoundSelectorClientBehaviorModuleData()
+{
 }
