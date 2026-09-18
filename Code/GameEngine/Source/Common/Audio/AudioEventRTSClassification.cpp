69fe61ae8a984df2a0d256af167cc2a6d0051624
Jonathan <jonathan@users.noreply.github.com>
2026-09-17 21:50:53 -0400

    Unify AudioEventRTS sound classification and positional checks

--- Code/GameEngine/Source/Common/Audio/AudioEventRTSClassification.cpp
+++ Code/GameEngine/Source/Common/Audio/AudioEventRTSClassification.cpp
@@ -6,7 +6,9 @@
 
 struct AudioEventInfo
 {
-	unsigned char m_pad[0x84];
+	unsigned char m_pad00[0x38];
+	unsigned char m_type;
+	unsigned char m_pad39[0x4B];
 	unsigned int m_soundClass;
 };
 
@@ -14,12 +16,15 @@ class AudioEventRTS
 {
 public:
 	unsigned int getSoundClass(void) const;
-	bool isPositionalAudio(void) const;
+	__declspec(noinline) bool isPositionalAudio(void) const;
 
 private:
 	void *m_vftable;
 	void *m_filenameToLoad;
 	const AudioEventInfo *m_eventInfo;
+	char m_pad0C[0x20];
+	unsigned int m_ownerID;
+	int m_ownerType;
 };
 
 // ?getSoundClass@AudioEventRTS@@QBEIXZ
@@ -43,4 +48,28 @@ unsigned int AudioEventRTS::getSoundClass(void) const
 	default:
 		return 0;
 	}
+}
+
+// Retail RVA 0x000B28F0; the class mapper above calls this through its ILT.
+bool AudioEventRTS::isPositionalAudio() const
+{
+	if (m_eventInfo != 0 && (m_eventInfo->m_type & 2) == 0)
+		goto not_positional;
+
+	switch (m_ownerType)
+	{
+		case 0:
+			return true;
+		case 1:
+		case 2:
+		case 5:
+			if (m_ownerID == 0)
+				goto not_positional;
+			return true;
+		default:
+			goto not_positional;
+	}
+
+not_positional:
+	return false;
 }
