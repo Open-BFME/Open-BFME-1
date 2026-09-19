c9dcc0f91a3a9781408941a3a114cc190ef1da7d
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:31:42 +0200

    feat(common): recover 20 tiny bodies from 0x002C5690

---
+++ Code/GameEngine/Source/Common/Rva002C56TinyBodies.cpp
@@ -0,0 +1,222 @@
+// cl: /O2
+// Carved address-derived bodies in the 0x002C56xx-0x002C64xx range.
+
+class Rva002C5690
+{
+public:
+	int field() const;
+
+private:
+	char m_padding[0x1c];
+	int m_field;
+};
+
+int Rva002C5690::field() const
+{
+	return m_field;
+}
+
+class Rva002C56E0
+{
+public:
+	int field() const;
+
+private:
+	char m_padding[8];
+	int m_field;
+};
+
+int Rva002C56E0::field() const
+{
+	return m_field;
+}
+
+class Rva002C5720
+{
+public:
+	void method(int);
+};
+
+void Rva002C5720::method(int)
+{
+}
+
+class Rva002C5730
+{
+public:
+	int method() const;
+};
+
+int Rva002C5730::method() const
+{
+	return 0;
+}
+
+class Rva002C5A40
+{
+public:
+	int method() const;
+};
+
+int Rva002C5A40::method() const
+{
+	return 0;
+}
+
+class Rva002C5A60
+{
+public:
+	void method(int);
+};
+
+void Rva002C5A60::method(int)
+{
+}
+
+class Rva002C5AC0
+{
+public:
+	void method(int);
+};
+
+void Rva002C5AC0::method(int)
+{
+}
+
+class Rva002C5B20
+{
+public:
+	void method(int);
+};
+
+void Rva002C5B20::method(int)
+{
+}
+
+class Rva002C5D90
+{
+public:
+	int field() const;
+
+private:
+	char m_padding[4];
+	int m_field;
+};
+
+int Rva002C5D90::field() const
+{
+	return m_field;
+}
+
+class Rva002C6160
+{
+public:
+	void method(int);
+};
+
+void Rva002C6160::method(int)
+{
+}
+
+class Rva002C61D0
+{
+public:
+	void method(int);
+};
+
+void Rva002C61D0::method(int)
+{
+}
+
+class Rva002C61E0
+{
+public:
+	int method() const;
+};
+
+int Rva002C61E0::method() const
+{
+	return 0;
+}
+
+class Rva002C6250
+{
+public:
+	void method(int);
+};
+
+void Rva002C6250::method(int)
+{
+}
+
+class Rva002C62C0
+{
+public:
+	void method(int);
+};
+
+void Rva002C62C0::method(int)
+{
+}
+
+class Rva002C6310
+{
+public:
+	int method() const;
+};
+
+int Rva002C6310::method() const
+{
+	return 0;
+}
+
+class Rva002C6320
+{
+public:
+	void method(int);
+};
+
+void Rva002C6320::method(int)
+{
+}
+
+class Rva002C6390
+{
+public:
+	void method(int);
+};
+
+void Rva002C6390::method(int)
+{
+}
+
+class Rva002C63F0
+{
+public:
+	void method(int);
+};
+
+void Rva002C63F0::method(int)
+{
+}
+
+class Rva002C6410
+{
+public:
+	int method() const;
+};
+
+int Rva002C6410::method() const
+{
+	return 0;
+}
+
+class Rva002C6420
+{
+public:
+	void method(int);
+};
+
+void Rva002C6420::method(int)
+{
+}
