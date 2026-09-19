ad64d9f5996a0024e80239d0b27c7418e5bf7e90
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-19 22:53:59 +0200

    feat: recover vector overflow body

---
+++ Code/GameEngine/Source/Common/Rva000E5580VectorInsert.cpp
@@ -0,0 +1,19 @@
+// cl: /DNDEBUG /DWIN32 /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
+
+#include <vector>
+
+struct Gen_t_000e5750_p12cd
+{
+	int a[3];
+	Gen_t_000e5750_p12cd();
+	Gen_t_000e5750_p12cd(const Gen_t_000e5750_p12cd&);
+	~Gen_t_000e5750_p12cd();
+	Gen_t_000e5750_p12cd& operator=(const Gen_t_000e5750_p12cd&);
+};
+
+bool operator==(const Gen_t_000e5750_p12cd&, const Gen_t_000e5750_p12cd&);
+bool operator<(const Gen_t_000e5750_p12cd&, const Gen_t_000e5750_p12cd&);
+
+
+template class _STL::vector<Gen_t_000e5750_p12cd>;
