d44f2aa7d05c2a9de09c7294268e869a23e5355f
Gabe Borges <gbborges@proton.me>
2026-09-19 16:20:15 -0400

    feat(common): convert field accessor at 0x0068db60
    Retail 0x0068DB60 loads a dword from this plus 0x04 and returns it. I placed the clean accessor in Code/GameEngine/Source/Common/Rva0068DB60Field04.cpp because the body proves the field offset but not a semantic owner.

---
+++ Code/GameEngine/Source/Common/Rva0068DB60Field04.cpp
@@ -0,0 +1,16 @@
+typedef unsigned int UnsignedInt;
+
+class Rva0068DB60
+{
+public:
+	UnsignedInt getField04() const;
+
+private:
+	UnsignedInt m_padding000;
+	UnsignedInt m_field04;
+};
+
+UnsignedInt Rva0068DB60::getField04() const
+{
+	return m_field04;
+}
