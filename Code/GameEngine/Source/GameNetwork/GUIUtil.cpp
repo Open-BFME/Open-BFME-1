8207d03b826d3d5bd146c18d9378739dc55d9a31
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-19 22:58:33 +0200

    feat: recover accept control helper

--- Code/GameEngine/Source/GameNetwork/GUIUtil.cpp
+++ Code/GameEngine/Source/GameNetwork/GUIUtil.cpp
@@ -7,8 +7,92 @@
 #include "GameNetwork/GUIUtil.h"
 #include "GameClient/GameWindowManager.h"
 #include "GameClient/MapUtil.h"
+#include "GameClient/GadgetComboBox.h"
 #include "GameNetwork/GameInfo.h"
 #include "Common/NameKeyGenerator.h"
+
+extern int g_Va012F49D4;
+
+class Rva006237C0GameInfoView
+{
+public:
+	virtual void slot00() const = 0;
+	virtual void slot04() const = 0;
+	virtual void slot08() const = 0;
+	virtual void slot0c() const = 0;
+	virtual Bool amIHost() const = 0;
+	virtual Int getLocalSlotNum() const = 0;
+};
+
+// ?EnableAcceptControls@@YAX_NPAVGameInfo@@QAPAVGameWindow@@2222PAV2@2H@Z
+void EnableAcceptControls(Bool Enabled, GameInfo *myGame, GameWindow *comboPlayer[],
+                          GameWindow *comboColor[], GameWindow *comboPlayerTemplate[],
+                          GameWindow *comboTeam[], GameWindow *buttonAccept[], GameWindow *buttonStart,
+                          GameWindow *buttonMapStartPosition[], Int slotNum)
+{
+	if (g_Va012F49D4)
+		return;
+
+	Rva006237C0GameInfoView *game =
+		(Rva006237C0GameInfoView *)myGame;
+
+	if(slotNum == -1 || slotNum >= MAX_SLOTS )
+		slotNum = game->getLocalSlotNum();
+
+	Bool isObserver = myGame->getConstSlot(slotNum)->getPlayerTemplate() == PLAYERTEMPLATE_OBSERVER;
+
+	if( !game->amIHost() && (buttonStart != NULL) )
+		buttonStart->winEnable(Enabled);
+	if(comboColor[slotNum])
+	{
+		if (isObserver)
+		{
+			GadgetComboBoxHideList(comboColor[slotNum]);
+		}
+		comboColor[slotNum]->winEnable(Enabled && !isObserver);
+	}
+	if(comboPlayerTemplate[slotNum])
+		comboPlayerTemplate[slotNum]->winEnable(Enabled);
+	if(comboTeam[slotNum])
+	{
+		if (isObserver)
+		{
+			GadgetComboBoxHideList(comboTeam[slotNum]);
+		}
+		comboTeam[slotNum]->winEnable(Enabled && !isObserver);
+	}
+
+	Bool canChooseStartSpot = FALSE;
+	if (!isObserver)
+		canChooseStartSpot = TRUE;
+	for (Int i=0; i<MAX_SLOTS && !canChooseStartSpot && game->amIHost(); ++i)
+	{
+		if (myGame->getConstSlot(i) && myGame->getConstSlot(i)->isAI())
+			canChooseStartSpot = TRUE;
+	}
+
+	if (slotNum == game->getLocalSlotNum())
+	{
+		if (myGame->getConstSlot(game->getLocalSlotNum())->hasMap())
+		{
+			for (Int i=0; i<MAX_SLOTS; ++i)
+			{
+				if (buttonMapStartPosition[i])
+				{
+					buttonMapStartPosition[i]->winEnable(Enabled && canChooseStartSpot);
+				}
+			}
+		}
+		else
+		{
+			for (Int i=0; i<MAX_SLOTS; ++i)
+			{
+				if (buttonMapStartPosition[i])
+					buttonMapStartPosition[i]->winEnable(FALSE);
+			}
+		}
+	}
+}
 
 // ?ShowUnderlyingGUIElements@@YAX_NPBD1PAPBD2@Z
 // The BFME implementation is shared by the LAN and online map-select menus.
