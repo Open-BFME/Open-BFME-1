aeebf0ee8e2bd016a987708533dfebde94665ef3
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:41:44 +0200

    feat(common): recover 17 tiny bodies from 0x00170E90

---
+++ Code/GameEngine/Source/Common/Rva00170TinyBodies.cpp
@@ -0,0 +1,163 @@
+// Address-derived tiny member bodies from the 0x00170xx region.
+// Their opcodes prove only the return shape and stack cleanup.
+
+struct Rva00170E90Tiny
+{
+	void m( int );
+};
+
+void Rva00170E90Tiny::m( int )
+{
+}
+
+struct Rva00170F20Tiny
+{
+	void m( int );
+};
+
+void Rva00170F20Tiny::m( int )
+{
+}
+
+struct Rva00170F70Tiny
+{
+	void m( int );
+};
+
+void Rva00170F70Tiny::m( int )
+{
+}
+
+struct Rva00170F90Tiny
+{
+	bool m();
+};
+
+bool Rva00170F90Tiny::m()
+{
+	return true;
+}
+
+struct Rva00171000Tiny
+{
+	int m();
+};
+
+int Rva00171000Tiny::m()
+{
+	return 0;
+}
+
+struct Rva00171010Tiny
+{
+	void m( int );
+};
+
+void Rva00171010Tiny::m( int )
+{
+}
+
+struct Rva00171020Tiny
+{
+	int m();
+};
+
+int Rva00171020Tiny::m()
+{
+	return 0;
+}
+
+struct Rva00171050Tiny
+{
+	void m( int );
+};
+
+void Rva00171050Tiny::m( int )
+{
+}
+
+struct Rva001712B0Tiny
+{
+	void m( int );
+};
+
+void Rva001712B0Tiny::m( int )
+{
+}
+
+struct Rva00171320Tiny
+{
+	void m( int );
+};
+
+void Rva00171320Tiny::m( int )
+{
+}
+
+struct Rva00171410Tiny
+{
+	void m( int );
+};
+
+void Rva00171410Tiny::m( int )
+{
+}
+
+struct Rva00171580Tiny
+{
+	bool m();
+};
+
+bool Rva00171580Tiny::m()
+{
+	return true;
+}
+
+struct Rva00171600Tiny
+{
+	void m( int );
+};
+
+void Rva00171600Tiny::m( int )
+{
+}
+
+struct Rva00171720Tiny
+{
+	bool m();
+};
+
+bool Rva00171720Tiny::m()
+{
+	return true;
+}
+
+struct Rva00171730Tiny
+{
+	bool m();
+};
+
+bool Rva00171730Tiny::m()
+{
+	return true;
+}
+
+struct Rva00171800Tiny
+{
+	bool m();
+};
+
+bool Rva00171800Tiny::m()
+{
+	return true;
+}
+
+struct Rva00171880Tiny
+{
+	bool m();
+};
+
+bool Rva00171880Tiny::m()
+{
+	return true;
+}
