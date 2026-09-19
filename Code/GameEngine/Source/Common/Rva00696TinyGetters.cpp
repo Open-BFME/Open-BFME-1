91347c7e45485616bd3d7052a68ec54d01413889
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:03:33 +0200

    feat(common): recover 25 tiny getters from 0x00696290

---
+++ Code/GameEngine/Source/Common/Rva00696TinyGetters.cpp
@@ -0,0 +1,300 @@
+// Address-derived thiscall pointer accessors from carved boundaries.
+// The member layouts below follow the complete retail instruction bodies.
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva00696290
+{
+	char m_pad[0x0C];
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696290::get()
+{
+	return m_value;
+}
+
+class Rva006962E0
+{
+	char m_pad[0x10];
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva006962E0::get()
+{
+	return m_value;
+}
+
+class Rva00696610
+{
+public:
+	void *get();
+};
+
+void *Rva00696610::get()
+{
+	return this;
+}
+
+class Rva006966A0
+{
+public:
+	void *get();
+};
+
+void *Rva006966A0::get()
+{
+	return this;
+}
+
+class Rva006966C0
+{
+public:
+	void *get();
+};
+
+void *Rva006966C0::get()
+{
+	return this;
+}
+
+class Rva00696720
+{
+public:
+	void *get();
+};
+
+void *Rva00696720::get()
+{
+	return this;
+}
+
+class Rva00696750
+{
+public:
+	void *get();
+};
+
+void *Rva00696750::get()
+{
+	return this;
+}
+
+class Rva00696790
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696790::get()
+{
+	return m_value;
+}
+
+class Rva00696900
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696900::get()
+{
+	return m_value;
+}
+
+class Rva00696910
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696910::get()
+{
+	return m_value;
+}
+
+class Rva00696920
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696920::get()
+{
+	return m_value;
+}
+
+class Rva00696980
+{
+public:
+	void *get();
+};
+
+void *Rva00696980::get()
+{
+	return this;
+}
+
+class Rva006969D0
+{
+public:
+	void *get();
+};
+
+void *Rva006969D0::get()
+{
+	return this;
+}
+
+class Rva00696AC0
+{
+public:
+	void *get();
+};
+
+void *Rva00696AC0::get()
+{
+	return this;
+}
+
+class Rva00696AE0
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696AE0::get()
+{
+	return static_cast<char *>(m_value) + 8;
+}
+
+class Rva00696B60
+{
+public:
+	void *get();
+};
+
+void *Rva00696B60::get()
+{
+	return this;
+}
+
+class Rva00696BB0
+{
+public:
+	void *get();
+};
+
+void *Rva00696BB0::get()
+{
+	return this;
+}
+
+class Rva00696BF0
+{
+public:
+	void *get();
+};
+
+void *Rva00696BF0::get()
+{
+	return this;
+}
+
+class Rva00696C50
+{
+public:
+	void *get();
+};
+
+void *Rva00696C50::get()
+{
+	return this;
+}
+
+class Rva00696C70
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696C70::get()
+{
+	return static_cast<char *>(m_value) + 8;
+}
+
+class Rva00696C80
+{
+public:
+	void *get();
+};
+
+void *Rva00696C80::get()
+{
+	return this;
+}
+
+class Rva00696CA0
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696CA0::get()
+{
+	return static_cast<char *>(m_value) + 8;
+}
+
+class Rva00696CB0
+{
+	void *m_value;
+
+public:
+	void *get();
+};
+
+void *Rva00696CB0::get()
+{
+	return m_value;
+}
+
+class Rva00696CC0
+{
+public:
+	void *get();
+};
+
+void *Rva00696CC0::get()
+{
+	return this;
+}
+
+class Rva00696D60
+{
+public:
+	void *get();
+};
+
+void *Rva00696D60::get()
+{
+	return this;
+}
