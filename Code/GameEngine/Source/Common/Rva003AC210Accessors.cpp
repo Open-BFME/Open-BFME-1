ad41d2bda6983fcb73031e027d418580ca9f3679
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 00:28:25 +0200

    feat(common): recover 25 tiny accessors from 0x003AC2B0

--- Code/GameEngine/Source/Common/Rva003AC210Accessors.cpp
+++ Code/GameEngine/Source/Common/Rva003AC210Accessors.cpp
@@ -109,3 +109,278 @@ unsigned char Rva003AC2A0Owner::getByte08() const
 {
 	return *(reinterpret_cast<const unsigned char *>(this) + 8);
 }
+
+class Rva003AC2B0Owner
+{
+public:
+	void *getField08();
+};
+
+void *Rva003AC2B0Owner::getField08()
+{
+	return reinterpret_cast<char *>(this) + 8;
+}
+
+class Rva003AC2C0Owner
+{
+public:
+	int getValue14() const;
+};
+
+int Rva003AC2C0Owner::getValue14() const
+{
+	return *(reinterpret_cast<const int *>(this) + 5);
+}
+
+class Rva003AC2D0Owner
+{
+public:
+	int getValue18() const;
+};
+
+int Rva003AC2D0Owner::getValue18() const
+{
+	return *(reinterpret_cast<const int *>(this) + 6);
+}
+
+class Rva003AC2E0Owner
+{
+public:
+	int getValue1C() const;
+};
+
+int Rva003AC2E0Owner::getValue1C() const
+{
+	return *(reinterpret_cast<const int *>(this) + 7);
+}
+
+class Rva003AC2F0Owner
+{
+public:
+	unsigned char getByte20() const;
+};
+
+unsigned char Rva003AC2F0Owner::getByte20() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0x20);
+}
+
+class Rva003AC300Owner
+{
+public:
+	unsigned char getByte21() const;
+};
+
+unsigned char Rva003AC300Owner::getByte21() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0x21);
+}
+
+class Rva003AC310Owner
+{
+public:
+	unsigned char getByte22() const;
+};
+
+unsigned char Rva003AC310Owner::getByte22() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0x22);
+}
+
+class Rva003AC330Owner
+{
+public:
+	void *getField08();
+};
+
+void *Rva003AC330Owner::getField08()
+{
+	return reinterpret_cast<char *>(this) + 8;
+}
+
+class Rva003AC340Owner
+{
+public:
+	void *getField0C();
+};
+
+void *Rva003AC340Owner::getField0C()
+{
+	return reinterpret_cast<char *>(this) + 0xC;
+}
+
+class Rva003AC350Owner
+{
+public:
+	unsigned char getByte10() const;
+};
+
+unsigned char Rva003AC350Owner::getByte10() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0x10);
+}
+
+class Rva003AC360Owner
+{
+public:
+	int getValue04() const;
+};
+
+int Rva003AC360Owner::getValue04() const
+{
+	return *(reinterpret_cast<const int *>(this) + 1);
+}
+
+class Rva003AC370Owner
+{
+public:
+	void *getField08();
+};
+
+void *Rva003AC370Owner::getField08()
+{
+	return reinterpret_cast<char *>(this) + 8;
+}
+
+class Rva003AC380Owner
+{
+public:
+	float getFloat14() const;
+};
+
+float Rva003AC380Owner::getFloat14() const
+{
+	return *(reinterpret_cast<const float *>(this) + 5);
+}
+
+class Rva003AC390Owner
+{
+public:
+	int getValue18() const;
+};
+
+int Rva003AC390Owner::getValue18() const
+{
+	return *(reinterpret_cast<const int *>(this) + 6);
+}
+
+class Rva003AC3A0Owner
+{
+public:
+	unsigned char getByte1C() const;
+};
+
+unsigned char Rva003AC3A0Owner::getByte1C() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0x1C);
+}
+
+class Rva003AC3B0Owner
+{
+public:
+	int getValue04() const;
+};
+
+int Rva003AC3B0Owner::getValue04() const
+{
+	return *(reinterpret_cast<const int *>(this) + 1);
+}
+
+class Rva003AC3E0Owner
+{
+public:
+	int getValue04() const;
+};
+
+int Rva003AC3E0Owner::getValue04() const
+{
+	return *(reinterpret_cast<const int *>(this) + 1);
+}
+
+class Rva003AC3F0Owner
+{
+public:
+	void *getField0C();
+};
+
+void *Rva003AC3F0Owner::getField0C()
+{
+	return reinterpret_cast<char *>(this) + 0xC;
+}
+
+class Rva003AC400Owner
+{
+public:
+	unsigned char getByte10() const;
+};
+
+unsigned char Rva003AC400Owner::getByte10() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0x10);
+}
+
+class Rva003AC420Owner
+{
+public:
+	void *getField08();
+};
+
+void *Rva003AC420Owner::getField08()
+{
+	return reinterpret_cast<char *>(this) + 8;
+}
+
+class Rva003AC430Owner
+{
+public:
+	unsigned char getByte0C() const;
+};
+
+unsigned char Rva003AC430Owner::getByte0C() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 0xC);
+}
+
+class Rva003AC450Owner
+{
+public:
+	unsigned char getByte08() const;
+};
+
+unsigned char Rva003AC450Owner::getByte08() const
+{
+	return *(reinterpret_cast<const unsigned char *>(this) + 8);
+}
+
+class Rva003AC460Owner
+{
+public:
+	int getValue0C() const;
+};
+
+int Rva003AC460Owner::getValue0C() const
+{
+	return *(reinterpret_cast<const int *>(this) + 3);
+}
+
+class Rva003AC470Owner
+{
+public:
+	Rva003AC470Owner *identity();
+};
+
+Rva003AC470Owner *Rva003AC470Owner::identity()
+{
+	return this;
+}
+
+class Rva003AC480Owner
+{
+public:
+	void *getField04();
+};
+
+void *Rva003AC480Owner::getField04()
+{
+	return reinterpret_cast<char *>(this) + 4;
+}
