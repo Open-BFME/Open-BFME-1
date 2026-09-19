86c391c3c870c0f531052a0abceb7c14b2c011c2
Gabe Borges <gbborges@proton.me>
2026-09-19 09:41:38 -0400

    feat(common): convert carved pointer getter
    The carved boundary at 0x006915C0 contains `mov eax,[ecx+0x3C]; ret`. Those bytes prove a pointer getter at offset 0x3C, but no caller or symbol table proves a semantic owner. I placed `Rva006915C0Owner::get` beside the matching anonymous getter in `Code/GameEngine/Source/Common/Rva006915A0Getter.cpp` and kept the address in the class name.

--- Code/GameEngine/Source/Common/Rva006915A0Getter.cpp
+++ Code/GameEngine/Source/Common/Rva006915A0Getter.cpp
@@ -18,3 +18,19 @@ private:
 
 typedef void *(Rva006915A0Owner::*Rva006915A0Getter)();
 Rva006915A0Getter g_rva006915A0Getter = &Rva006915A0Owner::get;
+
+class Rva006915C0Owner
+{
+public:
+	void *get()
+	{
+		return m_value;
+	}
+
+private:
+	unsigned char m_beforeValue[ 0x3C ];
+	void *m_value;
+};
+
+typedef void *(Rva006915C0Owner::*Rva006915C0Getter)();
+Rva006915C0Getter g_rva006915C0Getter = &Rva006915C0Owner::get;
