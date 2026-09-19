ef9f60fcece4ca7545c46c183e110cac67f7fd3d
dginovker <danielginovker@gmail.com>
2026-09-20 00:17:08 +0900

    Normalize WindowVideoManager include search

--- Code/GameEngine/Source/GameClient/WindowVideoManagerDestructor.cpp
+++ Code/GameEngine/Source/GameClient/WindowVideoManagerDestructor.cpp
@@ -1,4 +1,4 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Source/GameClient /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 // stlport
 // Open-BFME5: byte-exact clean C++ implementation of the retail destructor.
 
