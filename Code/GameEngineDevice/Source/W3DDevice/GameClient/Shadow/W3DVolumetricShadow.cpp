c914f051e646866f56ed925faf4071d7a98ba086
dginovker <danielginovker@gmail.com>
2026-09-19 23:46:41 +0900

    Consolidate two recovered helper functions

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/Shadow/W3DVolumetricShadow.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Shadow/W3DVolumetricShadow.cpp
@@ -66,6 +66,34 @@
 #include "wwshade/shdmesh.h"
 #include "wwshade/shdsubmesh.h"
 
+struct Rva007BDB70Record
+{
+	unsigned char bytes[22];
+};
+
+class Rva007BDB70ArrayOwner
+{
+public:
+	Rva007BDB70Record *lookup(int index);
+	void ensureStorage(void);
+
+private:
+	unsigned char m_prefix[0x24];
+	Rva007BDB70Record *m_records;
+	int m_count;
+};
+
+Rva007BDB70Record *Rva007BDB70ArrayOwner::lookup(int index)
+{
+	if (!m_records)
+		ensureStorage();
+
+	if (index >= 0 && index < m_count)
+		return &m_records[index];
+
+	return 0;
+}
+
 #ifdef _INTERNAL
 // for occasional debugging...
 //#pragma optimize("", off)
