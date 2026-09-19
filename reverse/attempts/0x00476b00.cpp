a68c5bb2fcfd7d95228a674e355df3bc6d228fbb
Jonathan <jonf38@users.noreply.github.com>
2026-09-19 00:23:55 -0400

    Refresh FontLibrary near-match evidence

--- reverse/attempts/0x00476b00.cpp
+++ reverse/attempts/0x00476b00.cpp
@@ -1,5 +1,5 @@
 // ?adjustFont@Rva00476B00FontLibrary@@QAEXPAVRva00476B00FontName@@PAMPAE@Z
-// partial score=0.99 date=2026-09-17
+// partial score=0.996 date=2026-09-19
 // cl: /O2 /Ob1 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /Ireference/shims/asciistring_downloadmanager /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
 // stlport
 //
