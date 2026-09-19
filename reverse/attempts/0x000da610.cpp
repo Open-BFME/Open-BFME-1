8324c71128e2997be94fff8ac4112e8420c29d1e
zivadev1 <331111025+zivadev1@users.noreply.github.com>
2026-09-19 15:17:11 +0900

    Preserve completed Luna rounds and measured partials

--- reverse/attempts/0x000da610.cpp
+++ reverse/attempts/0x000da610.cpp
@@ -1,5 +1,5 @@
 // ?init@Player@@QAEXPBVPlayerTemplate@@@Z
-// partial score=0.15 date=2026-09-17
+// partial score=0.2 date=2026-09-19
 // cl: /O2 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep
 //
 // BFME body conversion for 0x000DA610.  The retail boundary is 2056 bytes:
@@ -40,6 +40,7 @@ class BFMERetailAsciiString
 {
 public:
 	void releaseBuffer();
+	void *m_data;
 };
 
 class UnicodeString
@@ -88,12 +89,16 @@ class Rva000DA590Map
 {
 public:
 	Rva000DA590Map();
+	void *m_vtable;
+	unsigned char m_storage[0x14];
 };
 
 class TeamRelationMap
 {
 public:
 	TeamRelationMap();
+	void *m_vtable;
+	unsigned char m_storage[0x14];
 };
 
 class ScoreKeeper
@@ -128,16 +133,14 @@ class GameSpyGroupRoom
 {
 public:
 	GameSpyGroupRoom(const GameSpyGroupRoom &other);
-	~GameSpyGroupRoom();
-	char m_prefix[8];
 	BFMERetailAsciiString m_name;
-};
-
-class RawTemplateCopy
-{
-public:
-	RawTemplateCopy(const RawTemplateCopy &other);
-	~RawTemplateCopy();
+	void *m_translatedName;
+	Int m_groupID;
+	Int m_numWaiting;
+	Int m_maxWaiting;
+	Int m_numGames;
+	Int m_numPlaying;
+	Int m_bfmeExtra;
 };
 
 // These are the exact ILT names printed by tools/callees.py.  The argument
@@ -238,83 +241,107 @@ static __forceinline void initialiseAllocatedBlock(void **slot,
 class Rva0000CE7DReceiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0000ce7d();
-	}
+	void invoke();
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0000CE7DReceiver@@QAEXXZ=?j_0000ce7d@@YAXXZ")
 
 class Rva0001D4B7Receiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0001d4b7();
-	}
+	void invoke();
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0001D4B7Receiver@@QAEXXZ=?j_0001d4b7@@YAXXZ")
 
 class Rva0002CA16Receiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0002ca16();
-	}
+	void invoke(const void *source);
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0002CA16Receiver@@QAEXPBX@Z=?j_0002ca16@@YAXXZ")
 
 class Rva0000BBF9Receiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0000bbf9();
-	}
+	void invoke();
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0000BBF9Receiver@@QAEXXZ=?j_0000bbf9@@YAXXZ")
 
 class Rva00035DA0Receiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_00035da0();
-	}
+	void invoke(const void *source);
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva00035DA0Receiver@@QAEXPBX@Z=?j_00035da0@@YAXXZ")
 
 class Rva0003481FReceiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0003481f();
-	}
+	void invoke(const void *value);
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0003481FReceiver@@QAEXPBX@Z=?j_0003481f@@YAXXZ")
 
 class Rva00037880Receiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_00037880();
-	}
+	void invoke(const void *value);
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva00037880Receiver@@QAEXPBX@Z=?j_00037880@@YAXXZ")
 
 class Rva0001219DReceiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0001219d();
-	}
+	int *invoke(const int &key);
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0001219DReceiver@@QAEPAHPBH@Z=?j_0001219d@@YAXXZ")
 
 class Rva0002CF16Receiver
 {
 public:
-	__forceinline void invoke()
-	{
-		j_0002cf16();
-	}
+	void invoke();
 };
+#pragma comment(linker, "/alternatename:?invoke@Rva0002CF16Receiver@@QAEXXZ=?j_0002cf16@@YAXXZ")
+
+class Rva000201A3Receiver
+{
+public:
+	void invoke(const void *source);
+};
+#pragma comment(linker, "/alternatename:?invoke@Rva000201A3Receiver@@QAEXPBX@Z=?j_000201a3@@YAXXZ")
+
+class Rva00047203Receiver
+{
+public:
+	void invoke();
+};
+#pragma comment(linker, "/alternatename:?invoke@Rva00047203Receiver@@QAEXXZ=?j_00047203@@YAXXZ")
+
+class Rva00018679Receiver
+{
+public:
+	void invoke(Int field, Bool flag);
+};
+#pragma comment(linker, "/alternatename:?invoke@Rva00018679Receiver@@QAEXH_N@Z=?j_00018679@@YAXXZ")
+
+class Rva000A2F4Receiver
+{
+public:
+	void invoke();
+};
+#pragma comment(linker, "/alternatename:?invoke@Rva000A2F4Receiver@@QAEXXZ=?j_0000a2f4@@YAXXZ")
+
+class Rva00043022Receiver
+{
+public:
+	void invoke();
+};
+#pragma comment(linker, "/alternatename:?invoke@Rva00043022Receiver@@QAEXXZ=?j_00043022@@YAXXZ")
+
+class Rva00043C43Receiver
+{
+public:
+	int *invoke(const void *key);
+};
+#pragma comment(linker, "/alternatename:?invoke@Rva00043C43Receiver@@QAEPAHPBX@Z=?j_00043c43@@YAXXZ")
 
 void Player::init(const PlayerTemplate *pt)
 {
@@ -324,7 +351,6 @@ void Player::init(const PlayerTemplate *pt)
 	// PlayerTeamList is a one-pointer STLport list at +0x288.  Its node is
 	// 0x0c bytes, exactly the allocator contract in the retail body.
 	RawListNode *teamHead = playerField<RawListNode *>(self, 0x288);
-	if (teamHead != 0)
 	{
 		RawListNode *node = teamHead->m_next;
 		while (node != teamHead)
@@ -340,17 +366,15 @@ void Player::init(const PlayerTemplate *pt)
 	playerField<StringBase<unsigned short> >(self, 0x08).releaseBuffer();
 	reinterpret_cast<Rva0000CE7DReceiver *>(self)->invoke();
 
-	RawVirtualDelete *object;
 	playerField<Int>(self, 0x58) = 0;
 	playerField<Int>(self, 0x5c) = 0;
 	playerField<UnsignedByte>(self, 0x60) = 0;
 	playerField<Int>(self, 0x64) = 0;
 	playerField<Int>(self, 0x68) = 0;
 	playerField<Int>(self, 0x6c) = 0;
-	object = playerField<RawVirtualDelete *>(self, 0x70);
-	if (object != 0)
+	if (playerField<RawVirtualDelete *>(self, 0x70) != 0)
 	{
-		releaseVirtual(object);
+		playerField<RawVirtualDelete *>(self, 0x70)->deleteInstance(true);
 		playerField<RawVirtualDelete *>(self, 0x70) = 0;
 	}
 	playerField<Int>(self, 0xa8) = 0;
@@ -358,29 +382,25 @@ void Player::init(const PlayerTemplate *pt)
 	playerField<Player *>(self, 0xb0) = self;
 	reinterpret_cast<Rva000C8440 *>(reinterpret_cast<unsigned char *>(self) + 0xb4)->reset();
 
-	object = playerField<RawVirtualDelete *>(self, 0x1c0);
-	if (object != 0)
+	if (playerField<RawVirtualDelete *>(self, 0x1c0) != 0)
 	{
-		releaseVirtual(object);
+		playerField<RawVirtualDelete *>(self, 0x1c0)->deleteInstance(true);
 		playerField<RawVirtualDelete *>(self, 0x1c0) = 0;
 	}
-	object = playerField<RawVirtualDelete *>(self, 0x220);
-	if (object != 0)
+	if (playerField<RawVirtualDelete *>(self, 0x220) != 0)
 	{
-		releaseVirtual(object);
+		playerField<RawVirtualDelete *>(self, 0x220)->deleteInstance(true);
 		playerField<RawVirtualDelete *>(self, 0x220) = 0;
 	}
-	object = playerField<RawVirtualDelete *>(self, 0x228);
 	playerField<Int>(self, 0x224) = 0;
-	if (object != 0)
+	if (playerField<RawVirtualDelete *>(self, 0x228) != 0)
 	{
-		releaseVirtual(object);
+		playerField<RawVirtualDelete *>(self, 0x228)->deleteInstance(true);
 		playerField<RawVirtualDelete *>(self, 0x228) = 0;
 	}
-	object = playerField<RawVirtualDelete *>(self, 0x22c);
-	if (object != 0)
+	if (playerField<RawVirtualDelete *>(self, 0x22c) != 0)
 	{
-		releaseVirtual(object);
+		playerField<RawVirtualDelete *>(self, 0x22c)->deleteInstance(true);
 		playerField<RawVirtualDelete *>(self, 0x22c) = 0;
 	}
 	playerField<Int>(self, 0x230) = 0;
@@ -396,29 +416,13 @@ void Player::init(const PlayerTemplate *pt)
 	}
 	unidentifiedVector.m_begin = 0;
 
-	object = reinterpret_cast<RawVirtualDelete *>(
-		playerField<void *>(self, 0x28c));
-	if (object != 0)
-		releaseVirtual(object);
-	playerField<void *>(self, 0x28c) = 0;
-	void *relationStorage = operator new(0x18);
-	if (relationStorage != 0)
-	{
-		Rva000DA590Map *relationMap = new (relationStorage) Rva000DA590Map();
-		playerField<void *>(self, 0x28c) = relationMap;
-	}
+	if (playerField<RawVirtualDelete *>(self, 0x28c) != 0)
+		playerField<RawVirtualDelete *>(self, 0x28c)->deleteInstance(true);
+	playerField<void *>(self, 0x28c) = new Rva000DA590Map();
 
-	object = reinterpret_cast<RawVirtualDelete *>(
-		playerField<void *>(self, 0x290));
-	if (object != 0)
-		releaseVirtual(object);
-	playerField<void *>(self, 0x290) = 0;
-	relationStorage = operator new(0x18);
-	if (relationStorage != 0)
-	{
-		TeamRelationMap *relationMap = new (relationStorage) TeamRelationMap();
-		playerField<void *>(self, 0x290) = relationMap;
-	}
+	if (playerField<RawVirtualDelete *>(self, 0x290) != 0)
+		playerField<RawVirtualDelete *>(self, 0x290)->deleteInstance(true);
+	playerField<void *>(self, 0x290) = new TeamRelationMap();
 
 	playerField<UnsignedByte>(self, 0x294) = 1;
 	playerField<UnsignedByte>(self, 0x295) = 1;
@@ -495,25 +499,46 @@ void Player::init(const PlayerTemplate *pt)
 		// ILT is kept address-named until the generated deque body is replaced.
 		Rva0002CA16Receiver *colourMap =
 			reinterpret_cast<Rva0002CA16Receiver *>(playerBytes + 0x1cc);
-		colourMap->invoke();
+		colourMap->invoke(reinterpret_cast<const void *>(templateBytes + 0x60));
 		reinterpret_cast<Rva0001D4B7Receiver *>(playerBytes + 0x1d8)->invoke();
+		reinterpret_cast<Rva000201A3Receiver *>(playerBytes + 0x1d8)->invoke(
+			reinterpret_cast<const void *>(templateBytes + 0x6c));
 		reinterpret_cast<Rva0001D4B7Receiver *>(playerBytes + 0x1ec)->invoke();
+	reinterpret_cast<Rva000201A3Receiver *>(playerBytes + 0x1ec)->invoke(
+			reinterpret_cast<const void *>(playerBytes + 0x1d8));
 		reinterpret_cast<Rva0000BBF9Receiver *>(playerBytes + 0x200)->invoke();
-		reinterpret_cast<Rva00035DA0Receiver *>(playerBytes + 0x214)->invoke();
+		reinterpret_cast<Rva00035DA0Receiver *>(playerBytes + 0x214)->invoke(
+			reinterpret_cast<const void *>(templateBytes + 0x80));
 
 		// The vector contains 4-byte map entries.  Its key/value work is
 		// delegated to the address-named generated helper until that helper
 		// gets a typed row of its own.
 		RawVector &entries = playerField<RawVector>(self, 0x1d8);
-		for (unsigned char *entry = entries.m_begin;
-			entry != entries.m_end; entry += 4)
+		for (unsigned char **bucket = reinterpret_cast<unsigned char **>(entries.m_begin);
+			bucket != reinterpret_cast<unsigned char **>(entries.m_end); ++bucket)
 		{
-			if (*reinterpret_cast<void **>(entry) == 0)
-				continue;
-			playerField<Int>(self, 0x200) = 0;
-			GameSpyGroupRoom room(*reinterpret_cast<const GameSpyGroupRoom *>(entry));
-			reinterpret_cast<BFMERetailAsciiString *>(
-				reinterpret_cast<unsigned char *>(&room) + 8)->releaseBuffer();
+			unsigned char *node = *bucket;
+			while (node != 0)
+			{
+				int *mapped = reinterpret_cast<Rva00043C43Receiver *>(
+					playerBytes + 0x200)->invoke(node + 4);
+				*mapped = 0;
+				GameSpyGroupRoom room(
+					*reinterpret_cast<const GameSpyGroupRoom *>(node + 4));
+				const unsigned char *nameData =
+					*reinterpret_cast<const unsigned char **>(&room);
+				const char *name = nameData != 0 ?
+					reinterpret_cast<const char *>(nameData + 8) :
+					reinterpret_cast<const char *>(0x0107388b);
+				int hash = 0;
+				while (*name != 0)
+				{
+					hash = hash * 5 + static_cast<signed char>(*name);
+					++name;
+				}
+				reinterpret_cast<BFMERetailAsciiString *>(&room)->releaseBuffer();
+				node = *reinterpret_cast<unsigned char **>(node);
+			}
 		}
 		playerField<UnsignedByte>(self, 0x296) =
 			templateBytes[0xbc];
@@ -522,30 +547,53 @@ void Player::init(const PlayerTemplate *pt)
 	}
 	else
 	{
-		static StringBase<unsigned short> emptyWide;
-		static UnicodeString emptyUnicode;
-		static const char emptyText[] = "";
-		playerField<StringBase<unsigned short> >(self, 0x08).set(emptyWide);
+		playerField<StringBase<unsigned short> >(self, 0x08).set(
+			*reinterpret_cast<const StringBase<unsigned short> *>(0x01336e54));
 		reinterpret_cast<Rva000C81E0 *>(reinterpret_cast<unsigned char *>(self) + 0x0c)->reset();
-		playerField<UnicodeString>(self, 0x1c).set(emptyUnicode);
-		playerField<UnsignedInt>(self, 0x20) = 0;
+		playerField<UnicodeString>(self, 0x1c).set(
+			*reinterpret_cast<const UnicodeString *>(0x01336e50));
+		const unsigned char *neutralName =
+			*reinterpret_cast<const unsigned char **>(0x01336e50);
+		const char *neutralText = neutralName != 0 ?
+			reinterpret_cast<const char *>(neutralName + 8) :
+			reinterpret_cast<const char *>(0x0107388b);
+		UnsignedInt neutralKey =
+			(*reinterpret_cast<NameKeyGenerator **>(0x012ed600))->nameToKey(
+				neutralText);
+		playerField<UnsignedInt>(self, 0x20) = neutralKey;
+		reinterpret_cast<RetailLayoutString *>(reinterpret_cast<unsigned char *>(self) + 0x28)->set(
+			reinterpret_cast<const char *>(0x0107301c), 0);
+		playerField<UnsignedInt>(self, 0x50) = playerField<UnsignedInt>(self, 0x24);
 		playerField<UnsignedInt>(self, 0x2c) = 1;
 		playerField<UnsignedInt>(self, 0x4c) = 0;
-		playerField<UnsignedInt>(self, 0x1c4) = 0xffffffffu;
-		playerField<UnsignedInt>(self, 0x1c8) = 0xffffffffu;
-		reinterpret_cast<Rva0003481FReceiver *>(reinterpret_cast<unsigned char *>(self) + 0x1cc)->invoke();
-		reinterpret_cast<Rva00037880Receiver *>(reinterpret_cast<unsigned char *>(self) + 0x214)->invoke();
+		playerField<UnsignedInt>(self, 0x1c4) =
+			*reinterpret_cast<const UnsignedInt *>(0x012a8ba8);
+		playerField<UnsignedInt>(self, 0x1c8) =
+			playerField<UnsignedInt>(self, 0x1c4);
+		reinterpret_cast<Rva0003481FReceiver *>(reinterpret_cast<unsigned char *>(self) + 0x1cc)->invoke(
+			0);
+		reinterpret_cast<Rva00037880Receiver *>(reinterpret_cast<unsigned char *>(self) + 0x214)->invoke(
+			0);
 		reinterpret_cast<Rva0000BBF9Receiver *>(reinterpret_cast<unsigned char *>(self) + 0x200)->invoke();
-		playerField<UnsignedInt>(self, 0x20) = 0;
-		playerField<UnsignedInt>(self, 0x2c) = 1;
-		(void)emptyText;
 	}
 
 	// Neutral-player relation and science-vector cleanup, followed by the
 	// already-landed Player::applyInitialUpgrades body.
-	reinterpret_cast<Rva0001219DReceiver *>(reinterpret_cast<unsigned char *>(self) + 0x28c)->invoke();
+	Int relationKey = playerField<Int>(self, 0x24);
+	Rva0001219DReceiver *playerRelations = reinterpret_cast<Rva0001219DReceiver *>(
+		*reinterpret_cast<unsigned char **>(reinterpret_cast<unsigned char *>(self) + 0x28c) + 4);
+	*playerRelations->invoke(relationKey) = 2;
 	playerField<Int>(self, 0x30) = 2;
 	reinterpret_cast<Rva0002CF16Receiver *>(reinterpret_cast<unsigned char *>(self) + 0x30)->invoke();
+	Rva00018679Receiver *mapState = reinterpret_cast<Rva00018679Receiver *>(
+		reinterpret_cast<unsigned char *>(self) + 0x30);
+	if (pt != 0)
+		mapState->invoke(playerField<Int>(self, 0x24),
+			*reinterpret_cast<const UnsignedByte *>(
+				reinterpret_cast<const unsigned char *>(pt) + 0x118));
+	else
+		mapState->invoke(playerField<Int>(self, 0x24), false);
+	reinterpret_cast<Rva000A2F4Receiver *>(self)->invoke();
 	if (pt != 0)
 		playerField<UnsignedByte>(self, 0x296) =
 			reinterpret_cast<const unsigned char *>(pt)[0x118];
@@ -580,6 +628,7 @@ void Player::init(const PlayerTemplate *pt)
 		}
 		scienceB.m_end = write;
 	}
+	reinterpret_cast<Rva00043022Receiver *>(self)->invoke();
 
 	// The final two list releases are separate in retail: +0x650 has 0x10-byte
 	// nodes and +0x640 has 0x0c-byte nodes.  Keep the two boundaries explicit.
