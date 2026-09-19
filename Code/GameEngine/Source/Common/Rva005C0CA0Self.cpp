11bfd8376b234c501b17b37f5619cd63211f6f8a
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 00:37:02 +0200

    feat(common): recover eighteen tiny accessors from 0x005C0CA0

---
+++ Code/GameEngine/Source/Common/Rva005C0CA0Self.cpp
@@ -0,0 +1,14 @@
+// Retail 0x005C0CA0 returns its receiver unchanged.
+
+class Rva005C0CA0Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C0CA0Owner::get(void)
+{
+	return this;
+}
+
+// ?get@Rva005C0CA0Owner@@QAEPAXXZ
---
+++ Code/GameEngine/Source/Common/Rva005C0TinyAccessors.cpp
@@ -0,0 +1,206 @@
+// Carved tiny accessors with no recovered semantic owner.
+
+class Rva005C0CB0Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C0CB0Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C0CC0Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C0CC0Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C0CD0Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C0CD0Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C0D70Owner
+{
+public:
+	unsigned char getFlag(void) const;
+};
+
+unsigned char Rva005C0D70Owner::getFlag(void) const
+{
+	return *(const unsigned char *)((const char *)this + 4);
+}
+
+class Rva005C1130Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1130Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C1140Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1140Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C1150Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1150Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C1160Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1160Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C1170Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1170Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C1180Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1180Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C1190Owner
+{
+public:
+	void *get(void);
+};
+
+void *Rva005C1190Owner::get(void)
+{
+	return this;
+}
+
+class Rva005C12C0Owner
+{
+public:
+	bool get(void);
+};
+
+bool Rva005C12C0Owner::get(void)
+{
+	return false;
+}
+
+class Rva005C1380Owner
+{
+public:
+	bool get(void);
+};
+
+bool Rva005C1380Owner::get(void)
+{
+	return false;
+}
+
+class Rva005C1500Owner
+{
+public:
+	bool get(void);
+};
+
+bool Rva005C1500Owner::get(void)
+{
+	return false;
+}
+
+class Rva005C1510Owner
+{
+public:
+	bool get(void);
+};
+
+bool Rva005C1510Owner::get(void)
+{
+	return false;
+}
+
+class Rva005C1550Owner
+{
+public:
+	bool get(void);
+};
+
+bool Rva005C1550Owner::get(void)
+{
+	return false;
+}
+
+class Rva005C15A0Owner
+{
+public:
+	bool get(void);
+};
+
+bool Rva005C15A0Owner::get(void)
+{
+	return false;
+}
+
+// ?get@Rva005C0CB0Owner@@QAEPAXXZ
+// ?get@Rva005C0CC0Owner@@QAEPAXXZ
+// ?get@Rva005C0CD0Owner@@QAEPAXXZ
+// ?getFlag@Rva005C0D70Owner@@QBEEXZ
+// ?get@Rva005C1130Owner@@QAEPAXXZ
+// ?get@Rva005C1140Owner@@QAEPAXXZ
+// ?get@Rva005C1150Owner@@QAEPAXXZ
+// ?get@Rva005C1160Owner@@QAEPAXXZ
+// ?get@Rva005C1170Owner@@QAEPAXXZ
+// ?get@Rva005C1180Owner@@QAEPAXXZ
+// ?get@Rva005C1190Owner@@QAEPAXXZ
+// ?get@Rva005C12C0Owner@@QAE_NXZ
+// ?get@Rva005C1380Owner@@QAE_NXZ
+// ?get@Rva005C1500Owner@@QAE_NXZ
+// ?get@Rva005C1510Owner@@QAE_NXZ
+// ?get@Rva005C1550Owner@@QAE_NXZ
+// ?get@Rva005C15A0Owner@@QAE_NXZ
