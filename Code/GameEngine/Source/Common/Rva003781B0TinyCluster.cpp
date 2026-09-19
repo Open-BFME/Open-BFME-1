9cb1ef63e114de3ab783fab2bb4f1bbb5a415a84
Jonathan <jonf38@users.noreply.github.com>
2026-09-19 19:33:13 -0400

    Convert carved tiny cluster near 0x00378250

---
+++ Code/GameEngine/Source/Common/Rva003781B0TinyCluster.cpp
@@ -0,0 +1,56 @@
+// Independently carved tiny bodies reached only through anonymous ILT thunks.
+// Address-qualified method names preserve coverage without claiming an owner.
+
+class Rva003781B0TinyCluster
+{
+public:
+	void call003781B0();
+	Rva003781B0TinyCluster *returnThis00378230();
+	void call00378240();
+	void call00378250();
+	void call003782C0();
+	void call00378620();
+	void call00378C50();
+	Rva003781B0TinyCluster *returnThis00378CA0();
+	void call00378CB0();
+	int getDword00378CC0() const;
+	void call00378CD0();
+	void call00378CE0();
+	void call00378DA0();
+	void call00378DC0();
+	void call00378FF0();
+
+private:
+	int m_dword00;
+};
+
+void Rva003781B0TinyCluster::call003781B0() {}
+
+Rva003781B0TinyCluster *Rva003781B0TinyCluster::returnThis00378230()
+{
+	return this;
+}
+
+void Rva003781B0TinyCluster::call00378240() {}
+void Rva003781B0TinyCluster::call00378250() {}
+void Rva003781B0TinyCluster::call003782C0() {}
+void Rva003781B0TinyCluster::call00378620() {}
+void Rva003781B0TinyCluster::call00378C50() {}
+
+Rva003781B0TinyCluster *Rva003781B0TinyCluster::returnThis00378CA0()
+{
+	return this;
+}
+
+void Rva003781B0TinyCluster::call00378CB0() {}
+
+int Rva003781B0TinyCluster::getDword00378CC0() const
+{
+	return m_dword00;
+}
+
+void Rva003781B0TinyCluster::call00378CD0() {}
+void Rva003781B0TinyCluster::call00378CE0() {}
+void Rva003781B0TinyCluster::call00378DA0() {}
+void Rva003781B0TinyCluster::call00378DC0() {}
+void Rva003781B0TinyCluster::call00378FF0() {}
