9cbb22c9f5a1110be3b2a319fda6711b63df66a7
Gabe Borges <gbborges@proton.me>
2026-09-19 14:55:14 -0400

    feat(fx-particle): match physics header writer
    The `DefaultModuleTemplate<3>::writeINI` caller and ILT `0x00039AB8` prove `writeDefaultPhysicsHeader` at retail RVA `0x005FE060`. The new source uses the category-3 key, separator, stream vtable, and flag update that reproduce all 337 retail bytes.

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/DefaultModuleTemplatePhysicsHeader.cpp
@@ -0,0 +1,189 @@
+// cl: /DNDEBUG /MD /EHsc /O2 /Ob2
+// stlport
+
+namespace _STL
+{
+template <class T> class char_traits
+{
+};
+
+template <class T> class allocator
+{
+};
+
+template <bool threads, int instance>
+class __node_alloc
+{
+public:
+	static void _M_deallocate(void *, unsigned int);
+};
+
+template <class CharT, class Traits, class Allocator>
+class basic_string
+{
+public:
+	~basic_string()
+	{
+		unsigned int bytes =
+			(unsigned int)(m_storageEnd - m_start) * sizeof(CharT);
+		if (m_start) {
+			if (bytes > 128)
+				::operator delete(m_start);
+			else
+				__node_alloc<true, 0>::_M_deallocate(m_start, bytes);
+		}
+	}
+
+	CharT *m_start;
+	CharT *m_finish;
+	CharT *m_storageEnd;
+};
+
+class ios_base
+{
+protected:
+	ios_base();
+
+public:
+	virtual ~ios_base();
+};
+
+template <class CharT, class Traits>
+class basic_streambuf
+{
+public:
+	virtual ~basic_streambuf();
+};
+
+template <class CharT, class Traits>
+class basic_ios : public ios_base
+{
+public:
+	basic_ios();
+	virtual ~basic_ios() {}
+
+protected:
+	void init(basic_streambuf<CharT, Traits> *streambuf);
+
+private:
+	char padding_[0x50];
+	CharT fill_;
+	basic_streambuf<CharT, Traits> *streambuf_;
+	basic_ios<CharT, Traits> *tie_;
+};
+
+template <class CharT, class Traits>
+class basic_ostream : virtual public basic_ios<CharT, Traits>
+{
+public:
+	basic_ostream(basic_streambuf<CharT, Traits> *streambuf);
+	virtual ~basic_ostream();
+};
+
+template <class CharT, class Traits>
+class basic_filebuf : public basic_streambuf<CharT, Traits>
+{
+public:
+	basic_filebuf(int mode);
+	virtual ~basic_filebuf();
+
+private:
+	char padding_[0x68];
+};
+
+template <class CharT, class Traits>
+class basic_ofstream : public basic_ostream<CharT, Traits>
+{
+public:
+	basic_ofstream(int mode);
+	virtual ~basic_ofstream();
+
+private:
+	basic_filebuf<CharT, Traits> buf_;
+};
+}
+
+typedef _STL::basic_string<char, _STL::char_traits<char>, _STL::allocator<char> >
+	StreamText;
+typedef _STL::basic_ofstream<char, _STL::char_traits<char> > OutputStream;
+
+class File
+{
+};
+
+class StreamTextAccessor
+{
+public:
+	StreamText getText();
+};
+
+class StreamWriter
+{
+public:
+	void indent(int character);
+	void append(const char *text);
+	void separate(int character);
+};
+
+class FileWriteShim
+{
+public:
+	virtual ~FileWriteShim();
+	virtual void open();
+	virtual void close();
+	virtual void read();
+	virtual int write(const void *buffer, int bytes);
+};
+
+struct ModuleClassEntry
+{
+	const char *key;
+	const char *name;
+};
+
+class ModuleClassView
+{
+public:
+	virtual ModuleClassEntry *getClass(void) const;
+};
+
+extern const char g_u4Separator[];
+
+namespace FXParticleSystem
+{
+enum ModuleCategory
+{
+	MODULE_CATEGORY_PHYSICS = 3
+};
+
+const char *GetKey(ModuleCategory category);
+}
+
+__forceinline void writeStreamText(File &file, const StreamText &text)
+{
+	reinterpret_cast<FileWriteShim *>(&file)->write(
+		text.m_start, (int)(text.m_finish - text.m_start));
+}
+
+namespace FXParticleSystem
+{
+
+// ?writeDefaultPhysicsHeader@FXParticleSystem@@YAXPBXPAVFile@@PAI@Z
+void writeDefaultPhysicsHeader(const void *self, File *file,
+	unsigned int *flags)
+{
+	OutputStream stream(0x10);
+	const char *name = ((ModuleClassView *)((unsigned char *)self + 4))
+		->getClass()->name;
+	for (unsigned int indent = *flags; indent > 0; --indent)
+		reinterpret_cast<StreamWriter *>(&stream)->indent(' ');
+	reinterpret_cast<StreamWriter *>(&stream)->append(
+		GetKey(MODULE_CATEGORY_PHYSICS));
+	reinterpret_cast<StreamWriter *>(&stream)->append(g_u4Separator);
+	reinterpret_cast<StreamWriter *>(&stream)->append(name);
+	reinterpret_cast<StreamWriter *>(&stream)->separate('\n');
+	writeStreamText(*file,
+		reinterpret_cast<StreamTextAccessor *>(&stream)->getText());
+	*flags += 2;
+}
+}
