7454c424547d164bd1177d02fe16d58c3bda2f25
Gabe Borges <gbborges@proton.me>
2026-09-19 09:06:38 -0400

    feat(common): convert carved byte getter
    The carved 4-byte body at 0x0061E500 reads this+0x19, and adjacent getters at 0x0061E510 and 0x0061E520 read the next fields. I placed the address-derived Rva0061E500::get body in Code/GameEngine/Source/Common/Rva0061E500ByteGet.cpp because no semantic identity is proven.

---
+++ Code/GameEngine/Source/Common/Rva0061E500ByteGet.cpp
@@ -0,0 +1,14 @@
+class Rva0061E500
+{
+	unsigned char m_padding[0x19];
+	unsigned char m_value;
+
+public:
+	unsigned char get() const;
+};
+
+// ?get@Rva0061E500@@QBEEXZ
+unsigned char Rva0061E500::get() const
+{
+	return m_value;
+}
