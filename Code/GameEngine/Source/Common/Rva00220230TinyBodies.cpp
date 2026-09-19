97795ac60350382dbbd7d63df8587b5af35e87f8
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:26:32 +0200

    feat(common): recover 18 tiny bodies from 0x00220230

---
+++ Code/GameEngine/Source/Common/Rva00220230TinyBodies.cpp
@@ -0,0 +1,173 @@
+// cl: /O2
+
+bool Rva00220230Value()
+{
+    return true;
+}
+
+class Rva00220340Owner
+{
+public:
+    int valueAt4() const;
+};
+
+int Rva00220340Owner::valueAt4() const
+{
+    return *(const int *)((const char *)this + 4);
+}
+
+void __stdcall Rva00220650Value(int)
+{
+}
+
+int Rva002206A0Value(int)
+{
+    return 0;
+}
+
+void __stdcall Rva002206B0Value(int)
+{
+}
+
+class Rva002208DOwner
+{
+public:
+    Rva002208DOwner *self();
+};
+
+Rva002208DOwner *Rva002208DOwner::self()
+{
+    return this;
+}
+
+class Rva00220900Owner
+{
+public:
+    char *valueAt10() const;
+};
+
+char *Rva00220900Owner::valueAt10() const
+{
+    return *(char **)this + 16;
+}
+
+class Rva002209A0Owner
+{
+public:
+    char *valueAt10() const;
+};
+
+char *Rva002209A0Owner::valueAt10() const
+{
+    return *(char **)this + 16;
+}
+
+class Rva00220A00Owner
+{
+public:
+    char *valueAt10() const;
+};
+
+char *Rva00220A00Owner::valueAt10() const
+{
+    return *(char **)this + 16;
+}
+
+class Rva00220A40Owner
+{
+public:
+    Rva00220A40Owner *self();
+};
+
+Rva00220A40Owner *Rva00220A40Owner::self()
+{
+    return this;
+}
+
+class Rva00220A60Owner
+{
+public:
+    int valueAt4() const;
+};
+
+int Rva00220A60Owner::valueAt4() const
+{
+    return *(const int *)((const char *)this + 4);
+}
+
+class Rva00220A70Owner
+{
+public:
+    Rva00220A70Owner *self();
+};
+
+Rva00220A70Owner *Rva00220A70Owner::self()
+{
+    return this;
+}
+
+class Rva00220A90Owner
+{
+public:
+    Rva00220A90Owner *self();
+};
+
+Rva00220A90Owner *Rva00220A90Owner::self()
+{
+    return this;
+}
+
+class Rva00220AC0Owner
+{
+public:
+    int valueAt4() const;
+};
+
+int Rva00220AC0Owner::valueAt4() const
+{
+    return *(const int *)((const char *)this + 4);
+}
+
+class Rva00220B00Owner
+{
+public:
+    int valueAt4() const;
+};
+
+int Rva00220B00Owner::valueAt4() const
+{
+    return *(const int *)((const char *)this + 4);
+}
+
+class Rva00220B10Owner
+{
+public:
+    Rva00220B10Owner *self();
+};
+
+Rva00220B10Owner *Rva00220B10Owner::self()
+{
+    return this;
+}
+
+class Rva00220B70Owner
+{
+public:
+    char *valueAt10() const;
+};
+
+char *Rva00220B70Owner::valueAt10() const
+{
+    return *(char **)this + 16;
+}
+
+class Rva00220B90Owner
+{
+public:
+    char *valueAt10() const;
+};
+
+char *Rva00220B90Owner::valueAt10() const
+{
+    return *(char **)this + 16;
+}
