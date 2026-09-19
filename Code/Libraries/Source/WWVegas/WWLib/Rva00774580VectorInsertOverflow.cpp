b367c2a174da7aa27e634b72a355f7b82e829f6b
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 00:35:03 +0200

    feat(wwlib): recover 0x00774580 vector overflow body

---
+++ Code/Libraries/Source/WWVegas/WWLib/Rva00774580VectorInsertOverflow.cpp
@@ -0,0 +1,15 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
+
+#include <vector>
+
+struct Gen_t_00776080_p32cd
+{
+	int a[ 8 ];
+	Gen_t_00776080_p32cd();
+	Gen_t_00776080_p32cd(const Gen_t_00776080_p32cd&);
+	~Gen_t_00776080_p32cd();
+	Gen_t_00776080_p32cd& operator=(const Gen_t_00776080_p32cd&);
+};
+
+template class _STL::vector<Gen_t_00776080_p32cd>;
