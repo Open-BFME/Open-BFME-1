f8dcdd59d80e94e285d3a42250aba14ef3b8f100
Gabe Borges <gbborges@proton.me>
2026-09-19 09:14:28 -0400

    feat(common): convert carved dword getter
    The carved 4-byte body at 0x002954F0 loads this+0x4 and returns, while adjacent rows provide no semantic class or field identity. I placed the address-derived Rva002954F0::get body in Code/GameEngine/Source/Common/Rva002954F0DwordGet.cpp and recorded the blocked Pathfinder slot experiment in reverse/re_attempts.log.

---
+++ Code/GameEngine/Source/Common/Rva002954F0DwordGet.cpp
@@ -0,0 +1,14 @@
+class Rva002954F0
+{
+	unsigned char m_padding[0x4];
+	unsigned int m_value;
+
+public:
+	unsigned int get() const;
+};
+
+// ?get@Rva002954F0@@QBEIXZ
+unsigned int Rva002954F0::get() const
+{
+	return m_value;
+}
