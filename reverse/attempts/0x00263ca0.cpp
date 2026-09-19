525dbf18f7edf5d1c1d06b88aaf5787098d87338
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 00:23:44 +0200

    chore(reverse): bank PlayerHeal special power partial

---
+++ reverse/attempts/0x00263ca0.cpp
@@ -0,0 +1,278 @@
+// ?doSpecialPowerAtObject@PlayerHealSpecialPower@@UAEXPAVObject@@I@Z
+// partial score=0.3 date=2026-09-20
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib
+// PlayerHealSpecialPower::doSpecialPowerAtObject, retail RVA 0x00263CA0.
+// stlport
+
+#define _STLP_USE_STATIC_LIB 1
+#define _STLP_NO_EXCEPTIONS 1
+#define BFME_STLP_NODE_ALLOC 1
+#define __PLACEMENT_VEC_NEW_INLINE
+#include <vector>
+#include "PreRTS.h"
+
+typedef int Int;
+typedef unsigned int UnsignedInt;
+typedef unsigned char UnsignedByte;
+typedef float Real;
+typedef bool Bool;
+
+extern const Real BfmeZeroRange;
+
+class Player;
+class Object;
+class Thing;
+
+struct Rva00263CA0KindOfMask
+{
+	UnsignedInt words[6];
+};
+
+class BodyModuleInterface
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0c() = 0;
+	virtual void slot10() = 0;
+	virtual void slot14() = 0;
+	virtual Real getMaxHealth() const = 0;
+};
+
+class Object
+{
+public:
+	Player *getControllingPlayer() const;
+	Relationship getRelationship(const Object *that) const;
+	Bool isAnyKindOf(const Rva00263CA0KindOfMask &mask) const;
+	Bool isKindOf(KindOfType type) const;
+
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0c() = 0;
+	virtual void slot10() = 0;
+	virtual void slot14() = 0;
+	virtual void slot18() = 0;
+	virtual void slot1c() = 0;
+	virtual void slot20() = 0;
+	virtual void slot24() = 0;
+	virtual void slot28() = 0;
+	virtual void slot2c() = 0;
+	virtual void slot30() = 0;
+	virtual void slot34() = 0;
+	virtual void slot38() = 0;
+	virtual void slot3c() = 0;
+	virtual void attemptHealing(Real amount, const Object *source) = 0;
+
+	unsigned char m_pad00[0x1a0];
+	UnsignedInt m_disabledMask;
+	unsigned char m_pad1a8[0x58];
+	BodyModuleInterface *m_body;
+	unsigned char m_pad204[0x1c];
+	Real m_constructionPercent;
+};
+
+class FXList
+{
+public:
+	static void doFXObj(const FXList *fx, const Object *primary,
+		const Object *secondary);
+};
+
+class ObjectCreationList
+{
+public:
+	void create(const Object *primary, const Object *secondary,
+		Int first, Int second);
+};
+
+class PartitionFilter
+{
+public:
+	PartitionFilter *link(PartitionFilter *next);
+	UnsignedInt m_vtable;
+	PartitionFilter *m_next;
+};
+
+class Rva00263CA0RootFilter : public PartitionFilter
+{
+public:
+	Rva00263CA0RootFilter()
+	{
+		m_next = 0;
+		m_vtable = 0x01083B80;
+	}
+	~Rva00263CA0RootFilter()
+	{
+		m_vtable = 0x01083B5C;
+	}
+};
+
+struct Rva00263CA0Entry
+{
+	Object *object;
+	UnsignedInt unknown04;
+};
+
+struct Rva00263CA0ResultData
+{
+	std::vector<Rva00263CA0Entry> entries;
+	Rva00263CA0Entry *current;
+	Int references;
+};
+
+struct Rva00263CA0WideResult
+{
+	Rva00263CA0ResultData *value;
+
+	Rva00263CA0WideResult(const Rva00263CA0WideResult &other)
+		: value(other.value)
+	{
+		++value->references;
+	}
+	~Rva00263CA0WideResult()
+	{
+		if (--value->references == 0)
+			delete value;
+	}
+
+	Object *next(Object *&object)
+	{
+		if (value->current == value->entries.end())
+			return 0;
+		object = value->current->object;
+		++value->current;
+		return object;
+	}
+};
+
+class BfmeWideForwardC
+{
+public:
+	Rva00263CA0WideResult bfmeForwardWideC(int, int, int, int, int);
+};
+
+extern BfmeWideForwardC *ThePartitionManager;
+
+struct PlayerHealSpecialPowerModuleData
+{
+	unsigned char m_pad00[0x210];
+	Real m_healAmount;
+	Real m_healRadius;
+	Rva00263CA0KindOfMask m_healAffects;
+	FXList *m_healFX;
+	ObjectCreationList *m_healOCL;
+};
+
+struct PlayerHealSpecialPowerBase
+{
+	unsigned char m_pad00[4];
+	PlayerHealSpecialPowerModuleData *m_moduleData;
+	Object *m_object;
+	unsigned char m_pad0c[4];
+};
+
+class Rva00268CB0PlayerHealAction
+{
+public:
+	void run(Object *target);
+};
+
+#pragma comment(linker, "/alternatename:?run@Rva00268CB0PlayerHealAction@@QAEXPAVObject@@@Z=?j_00046c13@@YAXXZ")
+#pragma comment(linker, "/alternatename:?create@ObjectCreationList@@QAEXPBVObject@@0HH@Z=?j_00002a59@@YAXXZ")
+#pragma comment(linker, "/alternatename:?getControllingPlayer@Object@@QBEPAVPlayer@@XZ=?j_00020824@@YAXXZ")
+#pragma comment(linker, "/alternatename:?getRelationship@Object@@QBE?AW4Relationship@@PBV1@@Z=?j_0004a719@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isAnyKindOf@Object@@QBE_NABVRva00263CA0KindOfMask@@@Z=?j_0004250a@@YAXXZ")
+#pragma comment(linker, "/alternatename:?isKindOf@Object@@QBE_NW4KindOfType@@@Z=?j_0003251f@@YAXXZ")
+
+class SpecialPowerModuleInterfaceView
+{
+public:
+	virtual void slot00() = 0;
+	virtual void slot04() = 0;
+	virtual void slot08() = 0;
+	virtual void slot0c() = 0;
+	virtual void slot10() = 0;
+	virtual void slot14() = 0;
+	virtual void slot18() = 0;
+	virtual void slot1c() = 0;
+	virtual void slot20() = 0;
+	virtual void slot24() = 0;
+	virtual void slot28() = 0;
+	virtual void slot2c() = 0;
+	virtual void slot30() = 0;
+	virtual void slot34() = 0;
+	virtual void slot38() = 0;
+	virtual void slot3c() = 0;
+	virtual void startPowerRecharge();
+};
+
+class PlayerHealSpecialPower
+{
+public:
+	virtual void doSpecialPowerAtObject(Object *target, UnsignedInt commandOptions);
+};
+
+void PlayerHealSpecialPower::doSpecialPowerAtObject(Object *target,
+	UnsignedInt commandOptions)
+{
+	Object *owner =
+		((PlayerHealSpecialPowerBase *)((char *)this - 0x10))->m_object;
+	UnsignedInt disabledMask = owner->m_disabledMask;
+	volatile UnsignedInt stackReserve;
+	Object *volatile savedOwner;
+	savedOwner = owner;
+	if (disabledMask != 0)
+		return;
+	if (owner->getControllingPlayer() == 0)
+		return;
+
+	Object *targetObject = target;
+	((Rva00268CB0PlayerHealAction *)((char *)this - 0x10))->run(targetObject);
+	((SpecialPowerModuleInterfaceView *)this)->startPowerRecharge();
+
+	if ((*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc))->m_healOCL != 0)
+		(*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc))->m_healOCL->create(
+			savedOwner, targetObject, 0, 0);
+
+	{
+		Rva00263CA0RootFilter filter;
+		Rva00263CA0WideResult iterator =
+			ThePartitionManager->bfmeForwardWideC(
+			(int)((char *)targetObject + 0x38),
+			*(Int *)&(*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc))->m_healRadius, 0,
+			(int)&filter, 1);
+
+		Object *other;
+		while (iterator.next(other))
+		{
+		if (other->getRelationship(savedOwner) != (Relationship)2)
+			continue;
+		if ((*(UnsignedByte *)((char *)other + 0x344) & 1) != 0)
+			continue;
+		if (!other->isAnyKindOf(
+			*(Rva00263CA0KindOfMask *)((char *)*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc) + 0x218)))
+			continue;
+		if (other->isKindOf((KindOfType)0x9a))
+			continue;
+		if (!other->isKindOf((KindOfType)7))
+			continue;
+		if (other->m_constructionPercent < BfmeZeroRange)
+			continue;
+		if (!(other->m_constructionPercent < *(const Real *)0x010B6554))
+			continue;
+
+		Real amount = other->m_body->getMaxHealth() *
+			(*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc))->m_healAmount;
+		if (!(amount > BfmeZeroRange))
+			continue;
+		other->attemptHealing(amount, 0);
+		if ((*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc))->m_healFX != 0)
+			FXList::doFXObj(
+				(*(PlayerHealSpecialPowerModuleData **)((char *)this - 0xc))->m_healFX,
+				other, 0);
+		}
+	}
+}
