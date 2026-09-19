3195ee6102ce9f6e9e2fdce2fc442d1f37f1d7f5
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:00:45 +0200

    feat(common): recover 25 tiny bodies from 0x005DC8A0

---
+++ Code/GameEngine/Source/Common/Rva005DC8Batch.cpp
@@ -0,0 +1,363 @@
+// Open-BFME: adjacent carved four-byte accessors at 0x005DC8A0-0x005DCBF0.
+// The address-derived owners preserve the unproven semantic identities.
+
+class Rva005DC8A0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC8A0::get() const
+{
+	return m_value;
+}
+
+class Rva005DC8C0
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DC8C0::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DC8D0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC8D0::get() const
+{
+	return m_value;
+}
+
+class Rva005DC900
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC900::get() const
+{
+	return m_value;
+}
+
+class Rva005DC930
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC930::get() const
+{
+	return m_value;
+}
+
+class Rva005DC950
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DC950::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DC960
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC960::get() const
+{
+	return m_value;
+}
+
+class Rva005DC980
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DC980::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DC990
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC990::get() const
+{
+	return m_value;
+}
+
+class Rva005DC9B0
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DC9B0::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DC9C0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DC9C0::get() const
+{
+	return m_value;
+}
+
+class Rva005DCA20
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCA20::get() const
+{
+	return m_value;
+}
+
+class Rva005DCA40
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCA40::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DCA50
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCA50::get() const
+{
+	return m_value;
+}
+
+class Rva005DCA80
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCA80::get() const
+{
+	return m_value;
+}
+
+class Rva005DCAA0
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCAA0::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DCAB0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCAB0::get() const
+{
+	return m_value;
+}
+
+class Rva005DCB00
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCB00::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DCB30
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCB30::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DCB40
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCB40::get() const
+{
+	return m_value;
+}
+
+class Rva005DCB70
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCB70::get() const
+{
+	return m_value;
+}
+
+class Rva005DCB90
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCB90::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DCBA0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva005DCBA0::get() const
+{
+	return m_value;
+}
+
+class Rva005DCBC0
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCBC0::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
+
+class Rva005DCBF0
+{
+public:
+	void clear();
+
+private:
+	volatile unsigned int m_value;
+};
+
+void Rva005DCBF0::clear()
+{
+	unsigned int ignored = m_value;
+	m_value = 0;
+}
