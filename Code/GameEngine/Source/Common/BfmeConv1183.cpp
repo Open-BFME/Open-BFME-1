e16ba453b16606db41bf3ddd0b376548f79943bb
Gabe Borges <gbborges@proton.me>
2026-09-19 09:56:19 -0400

    feat(common): convert BfmeList1183 hash erase
    The ILT at 0x0001DFE3 and the BfmeW1183::bfmeClose1183 caller identify this body as BfmeList1183 removal, while the 0x000421F4 bucket helper identifies the Rva00469D20 AsciiString table. I placed the clean STLport erase instantiation in Code/GameEngine/Source/Common/BfmeConv1183.cpp because that file already owns BfmeW1183 and keeps the list object at the table address.

--- Code/GameEngine/Source/Common/BfmeConv1183.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1183.cpp
@@ -1,4 +1,68 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /D_STLP_USE_STATIC_LIB /EHsc
+// stlport
+//
 // Open-BFME5 conversions.
+
+#define _STLP_NO_EXCEPTIONS 1
+#include <hash_map>
+
+#include "../../../Libraries/Source/WWVegas/WWLib/string_base.h"
+
+extern "C" int __cdecl memcmp(const void *first, const void *second,
+	unsigned int count);
+#pragma intrinsic(memcmp)
+
+class AsciiString
+{
+public:
+	AsciiString(const AsciiString &other) : m_data(other.m_data) {}
+
+	int compare(const AsciiString &other) const
+	{
+		const int length = other.m_data.m_data ? other.m_data.m_data->length : 0;
+		const char *data = other.m_data.m_data ? &other.m_data.m_data->data[0] : "";
+		const int myLength = m_data.m_data ? m_data.m_data->length : 0;
+		const char *myData = m_data.m_data ? &m_data.m_data->data[0] : "";
+		const int result = memcmp(myData, data, myLength < length ? myLength : length);
+		if (result != 0)
+			return result;
+		return myLength - length;
+	}
+
+	StringBase<char> m_data;
+};
+
+inline bool operator==(const AsciiString &left, const AsciiString &right)
+{
+	return left.compare(right) == 0;
+}
+
+namespace rts
+{
+	template <class T>
+	struct hash
+	{
+		unsigned int operator()(T value) const;
+	};
+}
+
+struct Rva00469D20Value
+{
+	AsciiString m_key;
+	int m_mapped;
+};
+
+struct Rva00469D20ExtractKey
+{
+	const AsciiString &operator()(const Rva00469D20Value &entry) const
+	{
+		return entry.m_key;
+	}
+};
+
+typedef _STL::hashtable<Rva00469D20Value, AsciiString, rts::hash<AsciiString>,
+	Rva00469D20ExtractKey, _STL::equal_to<AsciiString>,
+	_STL::allocator<Rva00469D20Value> > BfmeList1183HashTable;
 
 class BfmeStr1183
 {
@@ -20,7 +84,11 @@ struct BfmeSlot1183
 class BfmeList1183
 {
 public:
-	void bfmeRemove1183(BfmeSlot1183 *s);
+	void bfmeRemove1183(BfmeSlot1183 *s)
+	{
+		reinterpret_cast<BfmeList1183HashTable *>(this)->erase(
+			*reinterpret_cast<const AsciiString *>(s));
+	}
 };
 
 class BfmeW1183
