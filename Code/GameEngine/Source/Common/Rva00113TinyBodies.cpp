5775f95ba442f50a5ace84aae592cd0ffb724f2e
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:59:21 +0200

    feat(common): recover 13 tiny bodies from 0x001139F0

---
+++ Code/GameEngine/Source/Common/Rva00113TinyBodies.cpp
@@ -0,0 +1,77 @@
+// cl: /O2 /Ob0 /EHs-c- /GS-
+
+// ?Rva001139F0@@YA_NXZ
+bool Rva001139F0()
+{
+	return false;
+}
+
+// ?Rva00113A00@@YA_NXZ
+bool Rva00113A00()
+{
+	return false;
+}
+
+// ?Rva00113A10@@YA_NXZ
+bool Rva00113A10()
+{
+	return false;
+}
+
+// ?Rva00113A50@@YAHXZ
+int Rva00113A50()
+{
+	return 0;
+}
+
+// ?Rva00113A60@@YAHXZ
+int Rva00113A60()
+{
+	return 0;
+}
+
+// ?Rva00113A70@@YAHXZ
+int Rva00113A70()
+{
+	return 0;
+}
+
+// ?Rva00113A80@@YAHXZ
+int Rva00113A80()
+{
+	return 0;
+}
+
+// ?Rva00113A90@@YAHXZ
+int Rva00113A90()
+{
+	return 0;
+}
+
+// ?Rva00113AA0@@YA_NXZ
+bool Rva00113AA0()
+{
+	return true;
+}
+
+// ?Rva00113AD0@@YGXH@Z
+void __stdcall Rva00113AD0(int)
+{
+}
+
+// ?Rva00113D10@@YGXHH@Z
+void __stdcall Rva00113D10(int, int)
+{
+}
+
+// ?Rva00113F80@@YAHXZ
+int Rva00113F80()
+{
+	return 0;
+}
+
+// ?Rva00113F90@@YAHXZ
+int Rva00113F90()
+{
+	return 0;
+}
