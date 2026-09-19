7bf9397d7652f15f3d790b87a8eb7c4f4d8ca96b
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:23:26 +0200

    feat(common): recover 20 tiny bodies from 0x006CF2D0

---
+++ Code/GameEngine/Source/Common/Rva006CF2TinyBodies.cpp
@@ -0,0 +1,210 @@
+// cl: /O2 /DNDEBUG /MD
+// Address-derived leaves recovered from complete carved boundaries.
+
+class Rva006CF2D0Owner
+{
+public:
+	int value();
+};
+
+int Rva006CF2D0Owner::value()
+{
+	return 0;
+}
+
+class Rva006CF2E0Owner
+{
+public:
+	int value();
+};
+
+int Rva006CF2E0Owner::value()
+{
+	return 0;
+}
+
+class Rva006CF300Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF300Owner::invoke(int)
+{
+}
+
+class Rva006CF320Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF320Owner::invoke(int)
+{
+}
+
+class Rva006CF390Owner
+{
+public:
+	int value();
+};
+
+int Rva006CF390Owner::value()
+{
+	return 0;
+}
+
+class Rva006CF440Owner
+{
+public:
+	void invoke(int, int, int);
+};
+
+void Rva006CF440Owner::invoke(int, int, int)
+{
+}
+
+class Rva006CF450Owner
+{
+public:
+	void invoke(int, int, int, int, int);
+};
+
+void Rva006CF450Owner::invoke(int, int, int, int, int)
+{
+}
+
+class Rva006CF460Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF460Owner::invoke(int)
+{
+}
+
+class Rva006CF470Owner
+{
+public:
+	int value();
+};
+
+int Rva006CF470Owner::value()
+{
+	return 0;
+}
+
+class Rva006CF4C0Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF4C0Owner::invoke(int)
+{
+}
+
+class Rva006CF4D0Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF4D0Owner::invoke(int)
+{
+}
+
+class Rva006CF500Owner
+{
+public:
+	int value();
+};
+
+int Rva006CF500Owner::value()
+{
+	return 0;
+}
+
+class Rva006CF510Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF510Owner::invoke(int)
+{
+}
+
+class Rva006CF5B0Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CF5B0Owner::invoke(int)
+{
+}
+
+class Rva006CF620Owner
+{
+public:
+	int value() const;
+};
+
+int Rva006CF620Owner::value() const
+{
+	return *(const int *)((const char *)this + 0x88);
+}
+
+class Rva006CF630Owner
+{
+public:
+	int value();
+};
+
+int Rva006CF630Owner::value()
+{
+	return 0;
+}
+
+class Rva006CF640Owner
+{
+public:
+	void invoke(int, int);
+};
+
+void Rva006CF640Owner::invoke(int, int)
+{
+}
+
+class Rva006CF9C0Owner
+{
+public:
+	void invoke(int, int);
+};
+
+void Rva006CF9C0Owner::invoke(int, int)
+{
+}
+
+class Rva006CFA10Owner
+{
+public:
+	void invoke(int);
+};
+
+void Rva006CFA10Owner::invoke(int)
+{
+}
+
+class Rva006CFA20Owner
+{
+public:
+	int value();
+};
+
+int Rva006CFA20Owner::value()
+{
+	return 0;
+}
