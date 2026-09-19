6d5d95d6926b031e5d545e00485a338849c03e94
Jonathan <jonf38@users.noreply.github.com>
2026-09-19 16:30:30 -0400

    Convert carved float field getter

--- Code/GameEngine/Source/Common/Rva006E1810FloatGetter.cpp
+++ Code/GameEngine/Source/Common/Rva006E1810FloatGetter.cpp
@@ -1,5 +1,19 @@
 // cl: /O2 /Ob0
 
+class Rva006E17F0FloatField
+{
+	char m_padding[0x50];
+	float m_value;
+
+public:
+	float get() const;
+};
+
+float Rva006E17F0FloatField::get() const
+{
+	return m_value;
+}
+
 class Rva006E1810FloatField
 {
 	char m_padding[0x70];
