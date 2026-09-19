ee56da83f46da8b2a2c2c6c81ffeaaffb3fb6649
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:01:39 +0200

    feat(common): recover 24 tiny accessors from 0x000C8870

---
+++ Code/GameEngine/Source/Common/Rva000C8TinyAccessors.cpp
@@ -0,0 +1,296 @@
+// Address-derived tiny accessors recovered from complete carved retail bodies.
+// The opaque owners preserve only the ABI and field offsets proven by retail.
+// cl: /O2 /MD /EHsc-
+
+class Rva000C8870Byte
+{
+    char m_padding[0x6B];
+    unsigned char m_value;
+public:
+    unsigned char get() const;
+};
+
+unsigned char Rva000C8870Byte::get() const
+{
+    return m_value;
+}
+
+class Rva000C88B0Dword
+{
+    char m_padding[0x78];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C88B0Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8A10Dword
+{
+    char m_padding[0x10];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8A10Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8A20Dword
+{
+    char m_padding[0x14];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8A20Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8A40Dword
+{
+    char m_padding[8];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8A40Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8AA0Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8AA0Address::get() const
+{
+    return (char *)this + 0x1C;
+}
+
+class Rva000C8AB0Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8AB0Address::get() const
+{
+    return (char *)this + 0x28;
+}
+
+class Rva000C8AC0Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8AC0Address::get() const
+{
+    return (char *)this + 0x60;
+}
+
+class Rva000C8AD0Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8AD0Address::get() const
+{
+    return (char *)this + 0x6C;
+}
+
+class Rva000C8BBDword
+{
+    char m_padding[4];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8BBDword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8C10Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8C10Address::get() const
+{
+    return (char *)this + 0x30;
+}
+
+class Rva000C8C20Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8C20Address::get() const
+{
+    return (char *)this + 0x48;
+}
+
+class Rva000C8D10Dword
+{
+    char m_padding[0x14];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8D10Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8E50Identity
+{
+public:
+    void *get();
+};
+
+void *Rva000C8E50Identity::get()
+{
+    return this;
+}
+
+class Rva000C8F80Dword
+{
+    char m_padding[4];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8F80Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8F90Dword
+{
+    char m_padding[8];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8F90Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8FD0Dword
+{
+    char m_padding[0x0C];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8FD0Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8FE0Dword
+{
+    char m_padding[0x10];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8FE0Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C8FF0Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C8FF0Address::get() const
+{
+    return (char *)this + 8;
+}
+
+class Rva000C9000Dword
+{
+    char m_padding[0x20];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C9000Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C9030Float
+{
+    char m_padding[0x44];
+    float m_value;
+public:
+    float get() const;
+};
+
+float Rva000C9030Float::get() const
+{
+    return m_value;
+}
+
+class Rva000C9050Dword
+{
+    char m_padding[0x34];
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C9050Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C9080Dword
+{
+    unsigned int m_value;
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C9080Dword::get() const
+{
+    return m_value;
+}
+
+class Rva000C9150Address
+{
+public:
+    char *get() const;
+};
+
+char *Rva000C9150Address::get() const
+{
+    return (char *)this + 0x14;
+}
