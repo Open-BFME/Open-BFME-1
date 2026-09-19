d0d42b12caabf695ea281e4ed93fbbd426e8e519
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 00:58:13 +0200

    feat(common): recover 25 tiny return bodies from 0x00119EC0

---
+++ Code/GameEngine/Source/Common/Rva00119TinyReturns.cpp
@@ -0,0 +1,125 @@
+// cl: /O2 /MD
+
+int Rva00119EC0ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119EF0ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119F40ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119F50ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119F60ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119F80ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119F90ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119FC0ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119FE0ReturnZero()
+{
+	return 0;
+}
+
+int Rva00119FF0ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A000ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A010ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A020ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A030ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A040ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A060ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A070ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A0A0ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A0B0ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A0C0ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A0E0ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A0F0ReturnZero()
+{
+	return 0;
+}
+
+int Rva0011A100ReturnZero()
+{
+	return 0;
+}
+
+void __stdcall Rva0011A110NoOp(int)
+{
+}
+
+bool Rva0011A190ReturnsFalse()
+{
+	return false;
+}
