5329f1fa62bdfeb9319dfae919f4baa560b1c7c9
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 01:37:11 +0200

    feat(common): recover 18 tiny bodies from 0x0075CF50

---
+++ Code/GameEngine/Source/Common/Rva0075CFAccessors.cpp
@@ -0,0 +1,222 @@
+// Opaque accessors recovered from the adjacent carved bodies.
+// Names preserve the address because callers do not prove semantic ownership.
+
+class Rva0075CF50ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075CF50ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D000DwordAt0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva0075D000DwordAt0::get() const
+{
+	return m_value;
+}
+
+class Rva0075D010ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D010ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D0D0DwordAt0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva0075D0D0DwordAt0::get() const
+{
+	return m_value;
+}
+
+class Rva0075D170ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D170ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D1E0ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D1E0ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D1F0ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D1F0ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D220ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D220ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D240DwordAt0
+{
+public:
+	unsigned int get() const;
+
+private:
+	unsigned int m_value;
+};
+
+unsigned int Rva0075D240DwordAt0::get() const
+{
+	return m_value;
+}
+
+class Rva0075D270DwordAt4
+{
+public:
+	unsigned int get() const;
+
+private:
+	char m_prefix[ 4 ];
+	unsigned int m_value;
+};
+
+unsigned int Rva0075D270DwordAt4::get() const
+{
+	return m_value;
+}
+
+class Rva0075D2F0ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D2F0ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D370ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D370ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D460ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D460ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D4A0PointerPlus8
+{
+public:
+	void *get() const;
+
+private:
+	void *m_base;
+};
+
+void *Rva0075D4A0PointerPlus8::get() const
+{
+	return ( (char *)m_base ) + 8;
+}
+
+class Rva0075D4E0PointerPlus8
+{
+public:
+	void *get() const;
+
+private:
+	void *m_base;
+};
+
+void *Rva0075D4E0PointerPlus8::get() const
+{
+	return ( (char *)m_base ) + 8;
+}
+
+class Rva0075D510PointerPlus8
+{
+public:
+	void *get() const;
+
+private:
+	void *m_base;
+};
+
+void *Rva0075D510PointerPlus8::get() const
+{
+	return ( (char *)m_base ) + 8;
+}
+
+class Rva0075D560ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D560ReturnThis::get()
+{
+	return this;
+}
+
+class Rva0075D5E0ReturnThis
+{
+public:
+	void *get();
+};
+
+void *Rva0075D5E0ReturnThis::get()
+{
+	return this;
+}
