5becbf013b5daff63bc9f02b61d2cbfe4f0836f4
Jonathan <jonf38@users.noreply.github.com>
2026-09-20 03:35:28 -0400

    Bank BYTEmark Huffman driver reconstruction

---
+++ reverse/attempts/0x008782c0.cpp
@@ -0,0 +1,8 @@
+// ?DoHuffman@@YAXXZ
+// partial score=0.562 date=2026-09-20
+// ?DoHuffman@@YAXXZ
+// Authentic BYTEmark nbench DoHuffman reconstruction at retail RVA 0x008782C0.
+// cl: /O2 /GS /MD /GR- /EHsc- -Ivendor/nbench
+#define randnum d_008790b0
+#define abs_randwc bfmeRandomPositive
+#include "../../Code/Libraries/Source/Benchmark/nbench1.c"
