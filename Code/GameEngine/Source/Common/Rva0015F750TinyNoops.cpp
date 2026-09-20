38ceac2e7b5e50d7edcf068e6eeb95eca89f8738
Jonathan <jonf38@users.noreply.github.com>
2026-09-20 13:44:11 -0400

    Convert carved no-ops near 0x0015FE00

---
+++ Code/GameEngine/Source/Common/Rva0015F750TinyNoops.cpp
@@ -0,0 +1,22 @@
+// Address-qualified carved no-ops. Each one-byte RET has a thunk caller, but
+// no caller or data evidence establishes a semantic owner.
+
+void Rva0015F750Noop()
+{
+}
+
+void Rva0015FD40Noop()
+{
+}
+
+void Rva0015FE00Noop()
+{
+}
+
+void Rva0015FE70Noop()
+{
+}
+
+void Rva001605C0Noop()
+{
+}
