8b07c177c9a9cebba5fe5b3c9698e4e08958fac6
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 02:54:08 +0200

    feat(common): recover 25 tiny bodies from 0x0026BFA0

---
+++ Code/GameEngine/Source/Common/Rva0026TinyBodies.cpp
@@ -0,0 +1,358 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva0026BFA0
+{
+public:
+	unsigned char getByte(void) const;
+
+private:
+	char m_prefix[0x2D];
+	unsigned char m_value;
+};
+
+// ?getByte@Rva0026BFA0@@QBEEXZ
+unsigned char Rva0026BFA0::getByte(void) const
+{
+	return m_value;
+}
+
+class Rva0026C5C0
+{
+public:
+	unsigned char getByte(void) const;
+};
+
+// ?getByte@Rva0026C5C0@@QBEEXZ
+unsigned char Rva0026C5C0::getByte(void) const
+{
+	return 0;
+}
+
+class Rva0026C6E0
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026C6E0@@QBEIXZ
+unsigned int Rva0026C6E0::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0026CDF0
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026CDF0@@QBEIXZ
+unsigned int Rva0026CDF0::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0026D480
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026D480@@QBEIXZ
+unsigned int Rva0026D480::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0026DD80
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026DD80@@QBEIXZ
+unsigned int Rva0026DD80::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0026E0E0
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026E0E0@@QBEIXZ
+unsigned int Rva0026E0E0::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0026E480
+{
+public:
+	void set(void);
+
+private:
+	char m_prefix[0x10];
+	unsigned char m_value;
+};
+
+// ?set@Rva0026E480@@QAEXXZ
+void Rva0026E480::set(void)
+{
+	m_value = 1;
+}
+
+class Rva0026E490
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026E490@@QBEIXZ
+unsigned int Rva0026E490::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0026E770
+{
+public:
+	void *self(void);
+};
+
+// ?self@Rva0026E770@@QAEPAXXZ
+void *Rva0026E770::self(void)
+{
+	return this;
+}
+
+class Rva0026F790
+{
+public:
+	void noop(void *first, void *second);
+};
+
+// ?noop@Rva0026F790@@QAEXPAX0@Z
+void Rva0026F790::noop(void *first, void *second)
+{
+}
+
+class Rva0026F7A0
+{
+public:
+	void noop(void *first, void *second);
+};
+
+// ?noop@Rva0026F7A0@@QAEXPAX0@Z
+void Rva0026F7A0::noop(void *first, void *second)
+{
+}
+
+class Rva0026F7B0
+{
+public:
+	void noop(void *first, void *second);
+};
+
+// ?noop@Rva0026F7B0@@QAEXPAX0@Z
+void Rva0026F7B0::noop(void *first, void *second)
+{
+}
+
+class Rva0026F7C0
+{
+public:
+	void noop(void *first, void *second);
+};
+
+// ?noop@Rva0026F7C0@@QAEXPAX0@Z
+void Rva0026F7C0::noop(void *first, void *second)
+{
+}
+
+class Rva0026FEA0
+{
+public:
+	void *self(void);
+};
+
+// ?self@Rva0026FEA0@@QAEPAXXZ
+void *Rva0026FEA0::self(void)
+{
+	return this;
+}
+
+class Rva0026FF20
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0026FF20@@QBEIXZ
+unsigned int Rva0026FF20::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0027FB30
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0027FB30@@QBEIXZ
+unsigned int Rva0027FB30::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva0027FFB0
+{
+public:
+	unsigned char getByte(void) const;
+};
+
+// ?getByte@Rva0027FFB0@@QBEEXZ
+unsigned char Rva0027FFB0::getByte(void) const
+{
+	return 0;
+}
+
+class Rva0027FFD0
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva0027FFD0@@QBEIXZ
+unsigned int Rva0027FFD0::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva00280D80
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva00280D80@@QBEIXZ
+unsigned int Rva00280D80::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva00281420
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva00281420@@QBEIXZ
+unsigned int Rva00281420::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva002818E0
+{
+public:
+	unsigned char getByte(void) const;
+
+private:
+	char m_prefix[9];
+	unsigned char m_value;
+};
+
+// ?getByte@Rva002818E0@@QBEEXZ
+unsigned char Rva002818E0::getByte(void) const
+{
+	return m_value;
+}
+
+class Rva00281930
+{
+public:
+	void *self(void);
+};
+
+// ?self@Rva00281930@@QAEPAXXZ
+void *Rva00281930::self(void)
+{
+	return this;
+}
+
+class Rva00281950
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	unsigned int m_value;
+};
+
+// ?getDword@Rva00281950@@QBEIXZ
+unsigned int Rva00281950::getDword(void) const
+{
+	return m_value;
+}
+
+class Rva00281AA0
+{
+public:
+	unsigned int getDword(void) const;
+
+private:
+	char m_prefix[4];
+	unsigned int m_value;
+};
+
+// ?getDword@Rva00281AA0@@QBEIXZ
+unsigned int Rva00281AA0::getDword(void) const
+{
+	return m_value;
+}
