4b8eaae681625bccc06a7c633dc88ec9b18568eb
Jonathan <jonf38@users.noreply.github.com>
2026-09-20 17:47:12 -0400

    Convert carved no-op cluster near 0x0013D490

---
+++ Code/GameEngine/Source/Common/Rva0013CF80TinyNoops.cpp
@@ -0,0 +1,16 @@
+// Address-qualified carved no-ops. Each one-byte RET has one thunk caller,
+// without evidence establishing a semantic owner.
+
+void Rva0013CF80Noop() {}
+void Rva0013CFA0Noop() {}
+void Rva0013D000Noop() {}
+void Rva0013D1F0Noop() {}
+void Rva0013D240Noop() {}
+void Rva0013D390Noop() {}
+void Rva0013D3A0Noop() {}
+void Rva0013D3C0Noop() {}
+void Rva0013D430Noop() {}
+void Rva0013D480Noop() {}
+void Rva0013D490Noop() {}
+void Rva0013D4B0Noop() {}
+void Rva0013D950Noop() {}
