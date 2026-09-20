a7c1f84ec5a80224e86a218f721d97861468f603
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 05:03:02 +0200

    feat(common): recover 25 tiny bodies from 0x00458180

---
+++ Code/GameEngine/Source/Common/Rva00458180TinyBodies.cpp
@@ -0,0 +1,296 @@
+// cl: /O2 /Ob0 /DNDEBUG /MD
+
+struct Rva00458180Owner
+{
+    char m_padding[0x34];
+    int get() const;
+};
+
+// ?get@Rva00458180Owner@@QBEHXZ
+int Rva00458180Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x34);
+}
+
+struct Rva004581DOwner
+{
+    char m_padding[8];
+    int get() const;
+};
+
+// ?get@Rva004581DOwner@@QBEHXZ
+int Rva004581DOwner::get() const
+{
+    return *(const int *)((const char *)this + 8);
+}
+
+struct Rva004581EOwner
+{
+    char m_padding[0xC];
+    float get() const;
+};
+
+// ?get@Rva004581EOwner@@QBEMXZ
+float Rva004581EOwner::get() const
+{
+    return *(const float *)((const char *)this + 0xC);
+}
+
+struct Rva00458200Owner
+{
+    char m_padding[0x14];
+    float get() const;
+};
+
+// ?get@Rva00458200Owner@@QBEMXZ
+float Rva00458200Owner::get() const
+{
+    return *(const float *)((const char *)this + 0x14);
+}
+
+struct Rva00458210Owner
+{
+    char m_padding[0x18];
+    int get() const;
+};
+
+// ?get@Rva00458210Owner@@QBEHXZ
+int Rva00458210Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x18);
+}
+
+struct Rva00458230Owner
+{
+    char m_padding[0x20];
+    float get() const;
+};
+
+// ?get@Rva00458230Owner@@QBEMXZ
+float Rva00458230Owner::get() const
+{
+    return *(const float *)((const char *)this + 0x20);
+}
+
+struct Rva00458240Owner
+{
+    char m_padding[0x24];
+    float get() const;
+};
+
+// ?get@Rva00458240Owner@@QBEMXZ
+float Rva00458240Owner::get() const
+{
+    return *(const float *)((const char *)this + 0x24);
+}
+
+struct Rva0045BF00Owner
+{
+    void call2(int first, int second);
+};
+
+// ?call2@Rva0045BF00Owner@@QAEXHH@Z
+void Rva0045BF00Owner::call2(int first, int second)
+{
+    (void)first;
+    (void)second;
+}
+
+struct Rva0045BF10Owner
+{
+    char m_padding[0x70];
+    float get() const;
+};
+
+// ?get@Rva0045BF10Owner@@QBEMXZ
+float Rva0045BF10Owner::get() const
+{
+    return *(const float *)((const char *)this + 0x70);
+}
+
+struct Rva0045BF50Owner
+{
+    void call1(int value);
+};
+
+// ?call1@Rva0045BF50Owner@@QAEXH@Z
+void Rva0045BF50Owner::call1(int value)
+{
+    (void)value;
+}
+
+struct Rva0045BF70Owner
+{
+    void call4(int first, int second, int third, int fourth);
+};
+
+// ?call4@Rva0045BF70Owner@@QAEXHHHH@Z
+void Rva0045BF70Owner::call4(int first, int second, int third, int fourth)
+{
+    (void)first;
+    (void)second;
+    (void)third;
+    (void)fourth;
+}
+
+struct Rva0045C050Owner
+{
+    void call1(int value);
+};
+
+// ?call1@Rva0045C050Owner@@QAEXH@Z
+void Rva0045C050Owner::call1(int value)
+{
+    (void)value;
+}
+
+struct Rva0045C120Owner
+{
+    void call1(int value);
+};
+
+// ?call1@Rva0045C120Owner@@QAEXH@Z
+void Rva0045C120Owner::call1(int value)
+{
+    (void)value;
+}
+
+struct Rva0045C1A0Owner
+{
+    char m_padding[4];
+    int get() const;
+};
+
+// ?get@Rva0045C1A0Owner@@QBEHXZ
+int Rva0045C1A0Owner::get() const
+{
+    return *(const int *)((const char *)this + 4);
+}
+
+struct Rva00466090Owner
+{
+    int get() const;
+};
+
+// ?get@Rva00466090Owner@@QBEHXZ
+int Rva00466090Owner::get() const
+{
+    return *(const int *)this;
+}
+
+struct Rva00470850Owner
+{
+    char m_padding[0x18];
+    int get() const;
+};
+
+// ?get@Rva00470850Owner@@QBEHXZ
+int Rva00470850Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x18);
+}
+
+struct Rva00470980Owner
+{
+    int get() const;
+};
+
+// ?get@Rva00470980Owner@@QBEHXZ
+int Rva00470980Owner::get() const
+{
+    return *(const int *)this;
+}
+
+struct Rva00471470Owner
+{
+    char m_padding[0x44];
+    int get() const;
+};
+
+// ?get@Rva00471470Owner@@QBEHXZ
+int Rva00471470Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x44);
+}
+
+struct Rva00471F60Owner
+{
+    char m_padding[0x4C];
+    int get() const;
+};
+
+// ?get@Rva00471F60Owner@@QBEHXZ
+int Rva00471F60Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x4C);
+}
+
+struct Rva00476370Owner
+{
+    void call1(int value);
+};
+
+// ?call1@Rva00476370Owner@@QAEXH@Z
+void Rva00476370Owner::call1(int value)
+{
+    (void)value;
+}
+
+struct Rva00477B00Owner
+{
+    char m_padding[0xC];
+    int get() const;
+};
+
+// ?get@Rva00477B00Owner@@QBEHXZ
+int Rva00477B00Owner::get() const
+{
+    return *(const int *)((const char *)this + 0xC);
+}
+
+struct Rva00477B10Owner
+{
+    char m_padding[0x14];
+    int get() const;
+};
+
+// ?get@Rva00477B10Owner@@QBEHXZ
+int Rva00477B10Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x14);
+}
+
+struct Rva0047A240Owner
+{
+    char m_padding[0x10];
+    int get() const;
+};
+
+// ?get@Rva0047A240Owner@@QBEHXZ
+int Rva0047A240Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x10);
+}
+
+struct Rva0047D080Owner
+{
+    char m_padding[0x28];
+    int get() const;
+};
+
+// ?get@Rva0047D080Owner@@QBEHXZ
+int Rva0047D080Owner::get() const
+{
+    return *(const int *)((const char *)this + 0x28);
+}
+
+struct Rva0047D760Owner
+{
+    void *getThis() const;
+};
+
+// ?getThis@Rva0047D760Owner@@QBEPAXXZ
+void *Rva0047D760Owner::getThis() const
+{
+    return (void *)this;
+}
