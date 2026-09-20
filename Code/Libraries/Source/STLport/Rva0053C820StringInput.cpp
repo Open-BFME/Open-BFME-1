9a79f0d2b5dfc3c2db4dce81f16326783b13fc79
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 23:43:21 +0200

    feat: match STLport string extractor

---
+++ Code/Libraries/Source/STLport/Rva0053C820StringInput.cpp
@@ -0,0 +1,13 @@
+// cl: /MD /D_STLP_USE_STATIC_LIB
+// stlport
+// STLport narrow string extractor at retail 0x0053C820.
+
+#include <istream>
+#include <string>
+
+typedef _STL::basic_istream<char, _STL::char_traits<char> > Rva0053C820Istream;
+typedef _STL::basic_string<char, _STL::char_traits<char>, _STL::allocator<char> > Rva0053C820String;
+
+// ??$?5DV?$char_traits@D@_STL@@V?$allocator@D@1@@_STL@@YAAAV?$basic_istream@DV?$char_traits@D@_STL@@@0@AAV10@AAV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@0@@Z
+template _STL::basic_istream<char, _STL::char_traits<char> > & _STLP_CALL _STL::operator>>(
+	Rva0053C820Istream &, Rva0053C820String &);
