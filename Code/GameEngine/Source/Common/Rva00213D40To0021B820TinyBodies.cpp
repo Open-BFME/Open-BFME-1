2f4d348fb4b999174c8fdf18b49a900f3a4471e6
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 02:42:23 +0200

    feat(common): recover 25 tiny bodies from 0x00213D40

---
+++ Code/GameEngine/Source/Common/Rva00213D40To0021B820TinyBodies.cpp
@@ -0,0 +1,283 @@
+// Address-derived tiny bodies recovered from complete carved extents.
+// The owners remain opaque because no semantic caller names these targets.
+
+// ?Rva00213D40@@YAHXZ
+int __cdecl Rva00213D40()
+{
+	return 0;
+}
+
+// ?Rva002141E0@@YGXPAX@Z
+void __stdcall Rva002141E0( void * )
+{
+}
+
+class Rva00214290
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00214290@@QBEHXZ
+int Rva00214290::get( void ) const
+{
+	return m_value;
+}
+
+class Rva00214760
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00214760@@QBEHXZ
+int Rva00214760::get( void ) const
+{
+	return m_value;
+}
+
+// ?Rva00214F60@@YGXHHHHHHHHHHHHHHHHHHHHHHHH@Z
+void __stdcall Rva00214F60(
+	int, int, int, int, int, int, int, int,
+	int, int, int, int, int, int, int, int,
+	int, int, int, int, int, int, int, int )
+{
+}
+
+// ?Rva00215AB0@@YA_NXZ
+bool __cdecl Rva00215AB0( void )
+{
+	return false;
+}
+
+// ?Rva00215AD0@@YA_NXZ
+bool __cdecl Rva00215AD0( void )
+{
+	return false;
+}
+
+// ?Rva00215AE0@@YA_NXZ
+bool __cdecl Rva00215AE0( void )
+{
+	return false;
+}
+
+class Rva00215B80
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00215B80@@QBEHXZ
+int Rva00215B80::get( void ) const
+{
+	return m_value;
+}
+
+// ?Rva002161E0@@YA_NXZ
+bool __cdecl Rva002161E0( void )
+{
+	return false;
+}
+
+// ?Rva002161F0@@YA_NXZ
+bool __cdecl Rva002161F0( void )
+{
+	return false;
+}
+
+// ?Rva00216200@@YA_NXZ
+bool __cdecl Rva00216200( void )
+{
+	return false;
+}
+
+class Rva00217350
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00217350@@QBEHXZ
+int Rva00217350::get( void ) const
+{
+	return m_value;
+}
+
+class Rva00217650
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00217650@@QBEHXZ
+int Rva00217650::get( void ) const
+{
+	return m_value;
+}
+
+// ?Rva002176D0@@YA_NXZ
+bool __cdecl Rva002176D0( void )
+{
+	return false;
+}
+
+class Rva00217E20
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00217E20@@QBEHXZ
+int Rva00217E20::get( void ) const
+{
+	return m_value;
+}
+
+// ?Rva00218080@@YA_NXZ
+bool __cdecl Rva00218080( void )
+{
+	return true;
+}
+
+class Rva00218180
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00218180@@QBEHXZ
+int Rva00218180::get( void ) const
+{
+	return m_value;
+}
+
+class Rva00218980
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00218980@@QBEHXZ
+int Rva00218980::get( void ) const
+{
+	return m_value;
+}
+
+class Rva00218D30
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00218D30@@QBEHXZ
+int Rva00218D30::get( void ) const
+{
+	return m_value;
+}
+
+class Rva00219B80
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva00219B80@@QBEHXZ
+int Rva00219B80::get( void ) const
+{
+	return m_value;
+}
+
+class Rva0021A230
+{
+public:
+	void *get( void );
+
+private:
+	char m_prefix[ 0x38 ];
+	int m_value;
+};
+
+// ?get@Rva0021A230@@QAEPAXXZ
+void *Rva0021A230::get( void )
+{
+	return &m_value;
+}
+
+class Rva0021A450
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva0021A450@@QBEHXZ
+int Rva0021A450::get( void ) const
+{
+	return m_value;
+}
+
+class Rva0021A760
+{
+public:
+	int get( void ) const;
+
+private:
+	char m_prefix[ 4 ];
+	int m_value;
+};
+
+// ?get@Rva0021A760@@QBEHXZ
+int Rva0021A760::get( void ) const
+{
+	return m_value;
+}
+
+// ?Rva0021B820@@YA_NXZ
+bool __cdecl Rva0021B820( void )
+{
+	return false;
+}
