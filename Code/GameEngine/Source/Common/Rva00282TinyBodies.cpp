4e775c06b01a14a0134103eeb1e6a3a975687438
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 02:55:32 +0200

    feat(common): recover 25 tiny bodies from 0x00282E90

---
+++ Code/GameEngine/Source/Common/Rva00282TinyBodies.cpp
@@ -0,0 +1,311 @@
+// Address-derived tiny bodies with no surviving semantic owner evidence.
+
+// ?Rva00282E90False@@YA_NXZ
+bool Rva00282E90False(void)
+{
+	return false;
+}
+
+class Rva00282F10
+{
+public:
+	Rva00282F10 *self(void);
+};
+
+// ?self@Rva00282F10@@QAEPAV1@XZ
+Rva00282F10 *Rva00282F10::self(void)
+{
+	return this;
+}
+
+class Rva00282F40
+{
+public:
+	Rva00282F40 *self(void);
+};
+
+// ?self@Rva00282F40@@QAEPAV1@XZ
+Rva00282F40 *Rva00282F40::self(void)
+{
+	return this;
+}
+
+class Rva00282F70
+{
+public:
+	Rva00282F70 *self(void);
+};
+
+// ?self@Rva00282F70@@QAEPAV1@XZ
+Rva00282F70 *Rva00282F70::self(void)
+{
+	return this;
+}
+
+class Rva00282FC0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	unsigned int m_word;
+};
+
+// ?word@Rva00282FC0@@QBEIXZ
+unsigned int Rva00282FC0::word(void) const
+{
+	return m_word;
+}
+
+class Rva00282FF0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	unsigned int m_word;
+};
+
+// ?word@Rva00282FF0@@QBEIXZ
+unsigned int Rva00282FF0::word(void) const
+{
+	return m_word;
+}
+
+class Rva00285640
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva00285640@@QBEIXZ
+unsigned int Rva00285640::word(void) const
+{
+	return m_word;
+}
+
+// ?Rva00285750False@@YA_NXZ
+bool Rva00285750False(void)
+{
+	return false;
+}
+
+// ?Rva002857A0Ret4@@YGXPAX@Z
+void __stdcall Rva002857A0Ret4(void *unused)
+{
+}
+
+class Rva00286E10
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva00286E10@@QBEIXZ
+unsigned int Rva00286E10::word(void) const
+{
+	return m_word;
+}
+
+class Rva00286E20
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[8];
+	unsigned int m_word;
+};
+
+// ?word@Rva00286E20@@QBEIXZ
+unsigned int Rva00286E20::word(void) const
+{
+	return m_word;
+}
+
+class Rva00286EB0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva00286EB0@@QBEIXZ
+unsigned int Rva00286EB0::word(void) const
+{
+	return m_word;
+}
+
+class Rva00287890
+{
+public:
+	Rva00287890 *self(void);
+};
+
+// ?self@Rva00287890@@QAEPAV1@XZ
+Rva00287890 *Rva00287890::self(void)
+{
+	return this;
+}
+
+class Rva00287A20
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva00287A20@@QBEIXZ
+unsigned int Rva00287A20::word(void) const
+{
+	return m_word;
+}
+
+class Rva002897F0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva002897F0@@QBEIXZ
+unsigned int Rva002897F0::word(void) const
+{
+	return m_word;
+}
+
+// ?Rva00289A00False@@YA_NXZ
+bool Rva00289A00False(void)
+{
+	return false;
+}
+
+class Rva0028A2C0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva0028A2C0@@QBEIXZ
+unsigned int Rva0028A2C0::word(void) const
+{
+	return m_word;
+}
+
+class Rva0028AAD0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva0028AAD0@@QBEIXZ
+unsigned int Rva0028AAD0::word(void) const
+{
+	return m_word;
+}
+
+// ?Rva0028B650False@@YA_NXZ
+bool Rva0028B650False(void)
+{
+	return false;
+}
+
+// ?Rva0028B660False@@YA_NXZ
+bool Rva0028B660False(void)
+{
+	return false;
+}
+
+class Rva0028B690
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva0028B690@@QBEIXZ
+unsigned int Rva0028B690::word(void) const
+{
+	return m_word;
+}
+
+// ?Rva0028C280Ret4@@YGXPAX@Z
+void __stdcall Rva0028C280Ret4(void *unused)
+{
+}
+
+class Rva0028C510
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva0028C510@@QBEIXZ
+unsigned int Rva0028C510::word(void) const
+{
+	return m_word;
+}
+
+class Rva0028C7E0
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva0028C7E0@@QBEIXZ
+unsigned int Rva0028C7E0::word(void) const
+{
+	return m_word;
+}
+
+class Rva0028CD20
+{
+public:
+	unsigned int word(void) const;
+
+private:
+	char m_pad[4];
+	unsigned int m_word;
+};
+
+// ?word@Rva0028CD20@@QBEIXZ
+unsigned int Rva0028CD20::word(void) const
+{
+	return m_word;
+}
