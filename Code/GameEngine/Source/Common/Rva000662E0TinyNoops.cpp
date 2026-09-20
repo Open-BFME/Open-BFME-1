edd404547d540e8a4b5ee99c1a386091c423d7ec
Jonathan <jonf38@users.noreply.github.com>
2026-09-20 08:39:30 -0400

    Convert carved 0x00066350 no-op cluster

---
+++ Code/GameEngine/Source/Common/Rva000662E0TinyNoops.cpp
@@ -0,0 +1,11 @@
+// Address-qualified carved one-byte RET bodies with no proven semantic owner.
+
+void rva000662E0Noop() {}
+void rva00066300Noop() {}
+void rva00066350Noop() {}
+void rva00066390Noop() {}
+void rva00066590Noop() {}
+void rva00066640Noop() {}
+void rva00066810Noop() {}
+void rva00066A30Noop() {}
+void rva00066D40Noop() {}
