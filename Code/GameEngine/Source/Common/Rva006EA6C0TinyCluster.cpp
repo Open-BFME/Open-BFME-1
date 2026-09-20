adb4164e2543902551671107f0168b1f478ff5d6
Jonathan <jonf38@users.noreply.github.com>
2026-09-19 20:33:54 -0400

    Convert carved tiny cluster near 0x006EACD0

---
+++ Code/GameEngine/Source/Common/Rva006EA6C0TinyCluster.cpp
@@ -0,0 +1,54 @@
+// Independently carved tiny bodies reached only through anonymous ILT thunks.
+// Address-qualified method names preserve coverage without claiming an owner.
+
+class Rva006EA6C0TinyCluster
+{
+public:
+	void call006EA6C0();
+	Rva006EA6C0TinyCluster *returnThis006EABF0();
+	void call006EAC00();
+	int getField10006EAC10() const;
+	void call006EAC80();
+	int getDword006EAC90() const;
+	int getDword006EACA0() const;
+	void call006EACD0();
+	void call006EACE0();
+	void call006EAEC0();
+	void call006EAFF0();
+
+private:
+	int m_dword00;
+	char m_padding04[0x0C];
+	int m_field10;
+};
+
+void Rva006EA6C0TinyCluster::call006EA6C0() {}
+
+Rva006EA6C0TinyCluster *Rva006EA6C0TinyCluster::returnThis006EABF0()
+{
+	return this;
+}
+
+void Rva006EA6C0TinyCluster::call006EAC00() {}
+
+int Rva006EA6C0TinyCluster::getField10006EAC10() const
+{
+	return m_field10;
+}
+
+void Rva006EA6C0TinyCluster::call006EAC80() {}
+
+int Rva006EA6C0TinyCluster::getDword006EAC90() const
+{
+	return m_dword00;
+}
+
+int Rva006EA6C0TinyCluster::getDword006EACA0() const
+{
+	return m_dword00;
+}
+
+void Rva006EA6C0TinyCluster::call006EACD0() {}
+void Rva006EA6C0TinyCluster::call006EACE0() {}
+void Rva006EA6C0TinyCluster::call006EAEC0() {}
+void Rva006EA6C0TinyCluster::call006EAFF0() {}
