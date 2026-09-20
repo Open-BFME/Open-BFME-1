fcfd05130169d4fa231c9b77423001209be7c5bb
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 02:01:30 +0200

    feat(common): recover 15 tiny bodies from 0x0043BC40

---
+++ Code/GameEngine/Source/Common/Rva0043BCTinyAccessors.cpp
@@ -0,0 +1,200 @@
+// Address-derived tiny accessors recovered from carved complete boundaries.
+// cl: /O2 /GR- /EHsc- /MD /DNDEBUG
+
+class Rva0043BC40Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BC60Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BC80Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BCA0Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BCC0Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BF10Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BFB0Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043C110Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043C150Owner
+{
+public:
+	void *getThis( void );
+};
+
+class Rva0043BDA0Owner
+{
+public:
+	void *getInterior( void );
+
+private:
+	void *m_base;
+};
+
+class Rva0043BE00Owner
+{
+public:
+	void *getInterior( void );
+
+private:
+	void *m_base;
+};
+
+class Rva0043BE30Owner
+{
+public:
+	void *getInterior( void );
+
+private:
+	void *m_base;
+};
+
+class Rva0043BEC0Owner
+{
+public:
+	void *getInterior( void );
+
+private:
+	void *m_base;
+};
+
+class Rva0043BEF0Owner
+{
+public:
+	void *getInterior( void );
+
+private:
+	void *m_base;
+};
+
+class Rva0043BF50Owner
+{
+public:
+	void *getInterior( void );
+
+private:
+	void *m_base;
+};
+
+// ?getThis@Rva0043BC40Owner@@QAEPAXXZ
+void *Rva0043BC40Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043BC60Owner@@QAEPAXXZ
+void *Rva0043BC60Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043BC80Owner@@QAEPAXXZ
+void *Rva0043BC80Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043BCA0Owner@@QAEPAXXZ
+void *Rva0043BCA0Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043BCC0Owner@@QAEPAXXZ
+void *Rva0043BCC0Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043BF10Owner@@QAEPAXXZ
+void *Rva0043BF10Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043BFB0Owner@@QAEPAXXZ
+void *Rva0043BFB0Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043C110Owner@@QAEPAXXZ
+void *Rva0043C110Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getThis@Rva0043C150Owner@@QAEPAXXZ
+void *Rva0043C150Owner::getThis( void )
+{
+	return this;
+}
+
+// ?getInterior@Rva0043BDA0Owner@@QAEPAXXZ
+void *Rva0043BDA0Owner::getInterior( void )
+{
+	return ( (char *)m_base ) + 8;
+}
+
+// ?getInterior@Rva0043BE00Owner@@QAEPAXXZ
+void *Rva0043BE00Owner::getInterior( void )
+{
+	return ( (char *)m_base ) + 8;
+}
+
+// ?getInterior@Rva0043BE30Owner@@QAEPAXXZ
+void *Rva0043BE30Owner::getInterior( void )
+{
+	return ( (char *)m_base ) + 8;
+}
+
+// ?getInterior@Rva0043BEC0Owner@@QAEPAXXZ
+void *Rva0043BEC0Owner::getInterior( void )
+{
+	return ( (char *)m_base ) + 8;
+}
+
+// ?getInterior@Rva0043BEF0Owner@@QAEPAXXZ
+void *Rva0043BEF0Owner::getInterior( void )
+{
+	return ( (char *)m_base ) + 8;
+}
+
+// ?getInterior@Rva0043BF50Owner@@QAEPAXXZ
+void *Rva0043BF50Owner::getInterior( void )
+{
+	return ( (char *)m_base ) + 8;
+}
