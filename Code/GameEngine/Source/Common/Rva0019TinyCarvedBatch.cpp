fa667b07e43702c8a307fc9c521f020e5e35c176
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-20 02:30:30 +0200

    feat(common): recover 25 tiny bodies from 0x001913F0

---
+++ Code/GameEngine/Source/Common/Rva0019TinyCarvedBatch.cpp
@@ -0,0 +1,370 @@
+// Opaque tiny carved bodies whose receiver layouts are limited to the witnessed offsets.
+// cl: /O2 /MD /EHsc-
+
+class Rva001913F0Owner
+{
+public:
+    unsigned char *get();
+
+private:
+    unsigned char padding[4];
+    unsigned char value;
+};
+
+// ?get@Rva001913F0Owner@@QAEPAEXZ
+unsigned char *Rva001913F0Owner::get()
+{
+    return &value;
+}
+
+class Rva00191400Owner
+{
+public:
+    unsigned char *get();
+
+private:
+    unsigned char padding[0x0c];
+    unsigned char value;
+};
+
+// ?get@Rva00191400Owner@@QAEPAEXZ
+unsigned char *Rva00191400Owner::get()
+{
+    return &value;
+}
+
+class Rva001914D0Owner
+{
+public:
+    unsigned int get() const;
+
+private:
+    unsigned char padding[0x28];
+    unsigned int value;
+};
+
+// ?get@Rva001914D0Owner@@QBEIXZ
+unsigned int Rva001914D0Owner::get() const
+{
+    return value;
+}
+
+class Rva001914F0Owner
+{
+public:
+    bool get() const;
+
+private:
+    unsigned char padding[0x38];
+    bool value;
+};
+
+// ?get@Rva001914F0Owner@@QBE_NXZ
+bool Rva001914F0Owner::get() const
+{
+    return value;
+}
+
+class Rva00191500Owner
+{
+public:
+    bool get() const;
+
+private:
+    unsigned char padding[0x3a];
+    bool value;
+};
+
+// ?get@Rva00191500Owner@@QBE_NXZ
+bool Rva00191500Owner::get() const
+{
+    return value;
+}
+
+class Rva00192330Owner
+{
+public:
+    unsigned int get() const;
+
+private:
+    unsigned char padding[4];
+    unsigned int value;
+};
+
+// ?get@Rva00192330Owner@@QBEIXZ
+unsigned int Rva00192330Owner::get() const
+{
+    return value;
+}
+
+class Rva00192570Owner
+{
+public:
+    void *get() const;
+
+private:
+    void *value;
+};
+
+// ?get@Rva00192570Owner@@QBEPAXXZ
+void *Rva00192570Owner::get() const
+{
+    return value;
+}
+
+class Rva001A0DE0Owner
+{
+public:
+    Rva001A0DE0Owner *identity();
+};
+
+// ?identity@Rva001A0DE0Owner@@QAEPAV1@XZ
+Rva001A0DE0Owner *Rva001A0DE0Owner::identity()
+{
+    return this;
+}
+
+class Rva001A19E0Owner
+{
+public:
+    unsigned int get() const;
+
+private:
+    unsigned char padding[0x40];
+    unsigned int value;
+};
+
+// ?get@Rva001A19E0Owner@@QBEIXZ
+unsigned int Rva001A19E0Owner::get() const
+{
+    return value;
+}
+
+class Rva001A19F0Owner
+{
+public:
+    bool get() const;
+
+private:
+    unsigned char padding[0x5c];
+    bool value;
+};
+
+// ?get@Rva001A19F0Owner@@QBE_NXZ
+bool Rva001A19F0Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1A00Owner
+{
+public:
+    unsigned int get() const;
+
+private:
+    unsigned char padding[0x60];
+    unsigned int value;
+};
+
+// ?get@Rva001A1A00Owner@@QBEIXZ
+unsigned int Rva001A1A00Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1A80Owner
+{
+public:
+    unsigned int get() const;
+
+private:
+    unsigned char padding[0x14];
+    unsigned int value;
+};
+
+// ?get@Rva001A1A80Owner@@QBEIXZ
+unsigned int Rva001A1A80Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1AA0Owner
+{
+public:
+    bool get() const;
+
+private:
+    unsigned char padding[0x2c];
+    bool value;
+};
+
+// ?get@Rva001A1AA0Owner@@QBE_NXZ
+bool Rva001A1AA0Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1AB0Owner
+{
+public:
+    bool get() const;
+
+private:
+    unsigned char padding[0x2d];
+    bool value;
+};
+
+// ?get@Rva001A1AB0Owner@@QBE_NXZ
+bool Rva001A1AB0Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1B10Owner
+{
+public:
+    unsigned int get() const;
+
+private:
+    unsigned char padding[4];
+    unsigned int value;
+};
+
+// ?get@Rva001A1B10Owner@@QBEIXZ
+unsigned int Rva001A1B10Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1DA0Owner
+{
+public:
+    bool get() const;
+
+private:
+    unsigned char padding[4];
+    bool value;
+};
+
+// ?get@Rva001A1DA0Owner@@QBE_NXZ
+bool Rva001A1DA0Owner::get() const
+{
+    return value;
+}
+
+class Rva001A1DC0Owner
+{
+public:
+    unsigned char *get();
+
+private:
+    unsigned char padding[0x4c];
+    unsigned char value;
+};
+
+// ?get@Rva001A1DC0Owner@@QAEPAEXZ
+unsigned char *Rva001A1DC0Owner::get()
+{
+    return &value;
+}
+
+class Rva001A34A0Owner
+{
+public:
+    void noop(unsigned int);
+};
+
+// ?noop@Rva001A34A0Owner@@QAEXI@Z
+void Rva001A34A0Owner::noop(unsigned int)
+{
+}
+
+class Rva001A34B0Owner
+{
+public:
+    void noop(unsigned int);
+};
+
+// ?noop@Rva001A34B0Owner@@QAEXI@Z
+void Rva001A34B0Owner::noop(unsigned int)
+{
+}
+
+class Rva001A4190Owner
+{
+public:
+    Rva001A4190Owner *identity();
+};
+
+// ?identity@Rva001A4190Owner@@QAEPAV1@XZ
+Rva001A4190Owner *Rva001A4190Owner::identity()
+{
+    return this;
+}
+
+class Rva001A41D0Owner
+{
+public:
+    Rva001A41D0Owner *identity();
+};
+
+// ?identity@Rva001A41D0Owner@@QAEPAV1@XZ
+Rva001A41D0Owner *Rva001A41D0Owner::identity()
+{
+    return this;
+}
+
+class Rva001A4260Owner
+{
+public:
+    Rva001A4260Owner *identity();
+};
+
+// ?identity@Rva001A4260Owner@@QAEPAV1@XZ
+Rva001A4260Owner *Rva001A4260Owner::identity()
+{
+    return this;
+}
+
+class Rva001A42B0Owner
+{
+public:
+    Rva001A42B0Owner *identity();
+};
+
+// ?identity@Rva001A42B0Owner@@QAEPAV1@XZ
+Rva001A42B0Owner *Rva001A42B0Owner::identity()
+{
+    return this;
+}
+
+class Rva001A42E0Owner
+{
+public:
+    void *get() const;
+
+private:
+    void *value;
+};
+
+// ?get@Rva001A42E0Owner@@QBEPAXXZ
+void *Rva001A42E0Owner::get() const
+{
+    return static_cast<unsigned char *>(value) + 0x10;
+}
+
+class Rva001AFBA0Owner
+{
+public:
+    float get() const;
+
+private:
+    unsigned char padding[0x5c];
+    float value;
+};
+
+// ?get@Rva001AFBA0Owner@@QBEMXZ
+float Rva001AFBA0Owner::get() const
+{
+    return value;
+}
