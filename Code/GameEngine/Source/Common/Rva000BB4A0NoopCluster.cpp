a1c049f79a664d753de829dc1df751e25aec2477
Jonathan <jonf38@users.noreply.github.com>
2026-09-20 05:35:22 -0400

    Convert carved 0x000BB4D0 no-op cluster

---
+++ Code/GameEngine/Source/Common/Rva000BB4A0NoopCluster.cpp
@@ -0,0 +1,29 @@
+// Address-qualified identities for carved one-byte no-op bodies.
+
+void Rva000BB4A0Noop()
+{
+}
+
+void Rva000BB4B0Noop()
+{
+}
+
+void Rva000BB4C0Noop()
+{
+}
+
+void Rva000BB4D0Noop()
+{
+}
+
+void Rva000BB590Noop()
+{
+}
+
+void Rva000BB690Noop()
+{
+}
+
+void Rva000BB850Noop()
+{
+}
