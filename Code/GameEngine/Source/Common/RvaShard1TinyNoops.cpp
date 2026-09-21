3eaf5e8b363a2d6c0d7a4e4a2168dc29d2230f82
Jonathan <jonf38@users.noreply.github.com>
2026-09-21 14:04:13 -0400

    Add exact shard-1 fallbacks

--- Code/GameEngine/Source/Common/RvaShard1TinyNoops.cpp
+++ Code/GameEngine/Source/Common/RvaShard1TinyNoops.cpp
@@ -59,3 +59,6 @@ void Rva0008A670Noop() {}
 void Rva00570600Noop() {}
 void Rva005706F0Noop() {}
 void Rva005C0340Noop() {}
+void Rva00652340Noop() {}
+void Rva00748F50Noop() {}
+void Rva0049D090Noop() {}
