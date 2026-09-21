35f3381b90d1c4dc00c7a5c83fdd9b0c88241a6d
Jonathan <jonf38@users.noreply.github.com>
2026-09-21 11:59:39 -0400

    Recover four more shard-0 carved leaves

---
+++ Code/GameEngine/Source/Common/RvaShard0TinyNoops2.cpp
@@ -0,0 +1,8 @@
+// cl: /DNDEBUG /MD /O2 /EHsc
+
+// Anonymous carved leaves in the 0x0022xxxx/0x0023xxxx source cluster.
+// Each retail body is one `ret`; the address tokens avoid owner claims.
+void dup_00221d10() {}
+void dup_0022f020() {}
+void dup_0022fd70() {}
+void dup_00232bb0() {}
