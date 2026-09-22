bf61007fa6e48a8e10a594026b7ac073a410072a
Jonathan <jonf38@users.noreply.github.com>
2026-09-21 23:17:45 -0400

    Record transform ordering and land tail thunks

---
+++ Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
@@ -0,0 +1,28 @@
+// cl: /O2 /MD
+//
+// Address-qualified incremental-link tail thunks.  Their independently
+// matched destinations establish the jump routes; the no-argument forwarding
+// declarations preserve the real receiver and arguments on the caller stack.
+
+extern void rva0000EF5CTarget(void);
+extern void rva0002F095Target(void);
+extern void rva000399A5Target(void);
+
+#pragma comment(linker, "/alternatename:?rva0000EF5CTarget@@YAXXZ=??0PropagandaTowerBehavior@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva0002F095Target@@YAXXZ=?copy@UnsignedPairTreeCopyShim@_STL@@QAEPAU?$_Rb_tree_node@U?$pair@$$CBGE@_STL@@@2@PAU32@0@Z")
+#pragma comment(linker, "/alternatename:?rva000399A5Target@@YAXXZ=?setOrientation@Thing@@QAEXM@Z")
+
+void j_0000EF5C(void)
+{
+	rva0000EF5CTarget();
+}
+
+void j_000399A5(void)
+{
+	rva000399A5Target();
+}
+
+void j_0002F095(void)
+{
+	rva0002F095Target();
+}
