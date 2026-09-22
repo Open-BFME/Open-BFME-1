1e5bf438d5297afb9e1dce7524f20b8603128235
Jonathan <jonf38@users.noreply.github.com>
2026-09-22 04:21:28 -0400

    Match remaining shard 1 tail thunk

--- Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
+++ Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
@@ -18,6 +18,7 @@ extern void rva000119E1Target(void);
 extern void rva00022683Target(void);
 extern void rva0002F095Target(void);
 extern void rva000399A5Target(void);
+extern void rva0003D7B2Target(void);
 
 #pragma comment(linker, "/alternatename:?rva0000EF5CTarget@@YAXXZ=??0PropagandaTowerBehavior@@QAE@PAVThing@@PBVModuleData@@@Z")
 #pragma comment(linker, "/alternatename:?rva000070B3Target@@YAXXZ=?PopulateLobbyPlayerListbox@@YAXXZ")
@@ -33,6 +34,7 @@ extern void rva000399A5Target(void);
 #pragma comment(linker, "/alternatename:?rva00022683Target@@YAXXZ=?readDict@DataChunkInput@@QAE?AVDict@@XZ")
 #pragma comment(linker, "/alternatename:?rva0002F095Target@@YAXXZ=?copy@UnsignedPairTreeCopyShim@_STL@@QAEPAU?$_Rb_tree_node@U?$pair@$$CBGE@_STL@@@2@PAU32@0@Z")
 #pragma comment(linker, "/alternatename:?rva000399A5Target@@YAXXZ=?setOrientation@Thing@@QAEXM@Z")
+#pragma comment(linker, "/alternatename:?rva0003D7B2Target@@YAXXZ=?_M_insert_overflow@?$vector@UWeaponBarrelInfo@ModelConditionInfo@@V?$allocator@UWeaponBarrelInfo@ModelConditionInfo@@@_STL@@@_STL@@IAEXPAUWeaponBarrelInfo@ModelConditionInfo@@ABU34@ABU__false_type@2@I_N@Z")
 
 void j_0000EF5C(void)
 {
@@ -97,6 +99,11 @@ void j_00022683(void)
 void j_000399A5(void)
 {
 	rva000399A5Target();
+}
+
+void j_0003D7B2(void)
+{
+	rva0003D7B2Target();
 }
 
 void j_0002F095(void)
