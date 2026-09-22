57d7d133e498f2087670d565eb1f52b8d3ab5a58
Jonathan <jonf38@users.noreply.github.com>
2026-09-22 02:18:52 -0400

    Match three more shard 1 tail thunks

--- Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
+++ Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
@@ -8,6 +8,9 @@ extern void rva0000EF5CTarget(void);
 extern void rva000070B3Target(void);
 extern void rva000078E7Target(void);
 extern void rva0000CC3ETarget(void);
+extern void rva0000A47ATarget(void);
+extern void rva0000DFBCTarget(void);
+extern void rva0000E43ATarget(void);
 extern void rva000119E1Target(void);
 extern void rva00022683Target(void);
 extern void rva0002F095Target(void);
@@ -17,6 +20,9 @@ extern void rva000399A5Target(void);
 #pragma comment(linker, "/alternatename:?rva000070B3Target@@YAXXZ=?PopulateLobbyPlayerListbox@@YAXXZ")
 #pragma comment(linker, "/alternatename:?rva000078E7Target@@YAXXZ=??$__copy@PBVProductionPrerequisite@@PAV1@H@_STL@@YAPAVProductionPrerequisite@@PBV1@0PAV1@ABUrandom_access_iterator_tag@0@PAH@Z")
 #pragma comment(linker, "/alternatename:?rva0000CC3ETarget@@YAXXZ=??0FlightDeckBehaviorModuleData@@QAE@XZ")
+#pragma comment(linker, "/alternatename:?rva0000A47ATarget@@YAXXZ=?_M_insert_overflow@?$vector@UFlightDeckInfo@FlightDeckBehavior@@V?$allocator@UFlightDeckInfo@FlightDeckBehavior@@@_STL@@@_STL@@IAEXPAUFlightDeckInfo@FlightDeckBehavior@@ABU34@ABU__false_type@2@I_N@Z")
+#pragma comment(linker, "/alternatename:?rva0000DFBCTarget@@YAXXZ=??0StatusBitsUpgrade@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva0000E43ATarget@@YAXXZ=?messageCallback@BuddyThreadClass@@QAEXPAVGPConnection@@PAUGPRecvBuddyMessageArg@@@Z")
 #pragma comment(linker, "/alternatename:?rva000119E1Target@@YAXXZ=?newLocomotor@LocomotorStore@@QBEPAVLocomotor@@PBVLocomotorTemplate@@@Z")
 #pragma comment(linker, "/alternatename:?rva00022683Target@@YAXXZ=?readDict@DataChunkInput@@QAE?AVDict@@XZ")
 #pragma comment(linker, "/alternatename:?rva0002F095Target@@YAXXZ=?copy@UnsignedPairTreeCopyShim@_STL@@QAEPAU?$_Rb_tree_node@U?$pair@$$CBGE@_STL@@@2@PAU32@0@Z")
@@ -40,6 +46,21 @@ void j_000078E7(void)
 void j_0000CC3E(void)
 {
 	rva0000CC3ETarget();
+}
+
+void j_0000A47A(void)
+{
+	rva0000A47ATarget();
+}
+
+void j_0000DFBC(void)
+{
+	rva0000DFBCTarget();
+}
+
+void j_0000E43A(void)
+{
+	rva0000E43ATarget();
 }
 
 void j_000119E1(void)
