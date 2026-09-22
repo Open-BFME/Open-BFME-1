e097e6d6403c31f3c3b7df5c95d6929e760ef850
Dom Clarke <zajad909@mystis.dev>
2026-09-22 08:35:38 +0100

    feat(common): land twelve incremental-link tail thunks
    Land the unclaimed five-byte ILT thunks between 0x000094F3 and 0x0001F717
    as address-qualified forwarding bodies in RvaShard2TailThunks.cpp.
    
    Each thunk decodes to a single jmp rel32 whose destination is an
    independently matched ledger row, so the route is evidence rather than an
    identity claim. The matching symbols.csv rows pin each forwarding
    declaration at the destination it jumps to.

--- Code/GameEngine/Source/Common/RvaShard2TailThunks.cpp
+++ Code/GameEngine/Source/Common/RvaShard2TailThunks.cpp
@@ -7,10 +7,34 @@
 extern void rva0000113BTarget(void);
 extern void rva00006555Target(void);
 extern void rva00007D06Target(void);
+extern void rva000094F3Target(void);
+extern void rva0000A0E2Target(void);
+extern void rva0001418CTarget(void);
+extern void rva00014961Target(void);
+extern void rva000153CATarget(void);
+extern void rva00016C52Target(void);
+extern void rva000178B4Target(void);
+extern void rva0001B3C4Target(void);
+extern void rva0001D5F7Target(void);
+extern void rva0001D7C8Target(void);
+extern void rva0001E8D0Target(void);
+extern void rva0001F717Target(void);
 
 #pragma comment(linker, "/alternatename:?rva0000113BTarget@@YAXXZ=?Rva007849C0Clear@@YAXPAURva007849C0Owner@@@Z")
 #pragma comment(linker, "/alternatename:?rva00006555Target@@YAXXZ=?end@?$list@PAVDrawable@@V?$allocator@PAVDrawable@@@_STL@@@_STL@@QBE?AU?$_List_iterator@PAVDrawable@@U?$_Const_traits@PAVDrawable@@@_STL@@@2@XZ")
 #pragma comment(linker, "/alternatename:?rva00007D06Target@@YAXXZ=?_M_lower_bound@?$_Rb_tree@VAsciiString@@V1@U?$_Identity@VAsciiString@@@_STL@@U?$less@VAsciiString@@@3@V?$allocator@VAsciiString@@@3@@_STL@@ABEPAU?$_Rb_tree_node@VAsciiString@@@2@ABVAsciiString@@@Z")
+#pragma comment(linker, "/alternatename:?rva000094F3Target@@YAXXZ=?buildFieldParse@BroadcastStealthUpdateModuleData@@SAXAAVMultiIniFieldParse@@@Z")
+#pragma comment(linker, "/alternatename:?rva0000A0E2Target@@YAXXZ=??0RainOfFireUpdate@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva0001418CTarget@@YAXXZ=?Rva00784AD0StlAllocate@@YGPAXIPBX@Z")
+#pragma comment(linker, "/alternatename:?rva00014961Target@@YAXXZ=??$_M_allocate_and_copy@PBVProductionPrerequisite@@@?$vector@VProductionPrerequisite@@V?$allocator@VProductionPrerequisite@@@_STL@@@_STL@@IAEPAVProductionPrerequisite@@IPBV2@0@Z")
+#pragma comment(linker, "/alternatename:?rva000153CATarget@@YAXXZ=?d_00784ac0@@YAXXZ")
+#pragma comment(linker, "/alternatename:?rva00016C52Target@@YAXXZ=??0DeletionUpdate@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva000178B4Target@@YAXXZ=??$__uninitialized_copy@PBUPrereqUnitRec@ProductionPrerequisite@@PAU12@@_STL@@YAPAUPrereqUnitRec@ProductionPrerequisite@@PBU12@0PAU12@ABU__false_type@0@@Z")
+#pragma comment(linker, "/alternatename:?rva0001B3C4Target@@YAXXZ=?d_00784ab0@@YAXXZ")
+#pragma comment(linker, "/alternatename:?rva0001D5F7Target@@YAXXZ=??$__uninitialized_copy@PBVW3DAnimationInfo@@PAV1@@_STL@@YAPAVW3DAnimationInfo@@PBV1@0PAV1@ABU__false_type@0@@Z")
+#pragma comment(linker, "/alternatename:?rva0001D7C8Target@@YAXXZ=?computeApproachPosition@DockUpdate@@IAE?AUCoord3D@@HPAVObject@@@Z")
+#pragma comment(linker, "/alternatename:?rva0001E8D0Target@@YAXXZ=??0RepairDockUpdate@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva0001F717Target@@YAXXZ=?begin@?$list@PAVDrawable@@V?$allocator@PAVDrawable@@@_STL@@@_STL@@QBE?AU?$_List_iterator@PAVDrawable@@U?$_Const_traits@PAVDrawable@@@_STL@@@2@XZ")
 
 void j_0000113B(void)
 {
@@ -25,4 +49,64 @@ void j_00006555(void)
 void j_00007D06(void)
 {
 	rva00007D06Target();
+}
+
+void j_000094F3(void)
+{
+	rva000094F3Target();
+}
+
+void j_0000A0E2(void)
+{
+	rva0000A0E2Target();
+}
+
+void j_0001418C(void)
+{
+	rva0001418CTarget();
+}
+
+void j_00014961(void)
+{
+	rva00014961Target();
+}
+
+void j_000153CA(void)
+{
+	rva000153CATarget();
+}
+
+void j_00016C52(void)
+{
+	rva00016C52Target();
+}
+
+void j_000178B4(void)
+{
+	rva000178B4Target();
+}
+
+void j_0001B3C4(void)
+{
+	rva0001B3C4Target();
+}
+
+void j_0001D5F7(void)
+{
+	rva0001D5F7Target();
+}
+
+void j_0001D7C8(void)
+{
+	rva0001D7C8Target();
+}
+
+void j_0001E8D0(void)
+{
+	rva0001E8D0Target();
+}
+
+void j_0001F717(void)
+{
+	rva0001F717Target();
 }
