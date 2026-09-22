d34e34ebe7c495c578edc3323aa6980fc3083e7d
Jonathan <jonf38@users.noreply.github.com>
2026-09-22 01:17:17 -0400

    Match three shard 1 tail thunks

--- Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
+++ Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
@@ -5,12 +5,18 @@
 // declarations preserve the real receiver and arguments on the caller stack.
 
 extern void rva0000EF5CTarget(void);
+extern void rva000070B3Target(void);
+extern void rva000078E7Target(void);
+extern void rva0000CC3ETarget(void);
 extern void rva000119E1Target(void);
 extern void rva00022683Target(void);
 extern void rva0002F095Target(void);
 extern void rva000399A5Target(void);
 
 #pragma comment(linker, "/alternatename:?rva0000EF5CTarget@@YAXXZ=??0PropagandaTowerBehavior@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva000070B3Target@@YAXXZ=?PopulateLobbyPlayerListbox@@YAXXZ")
+#pragma comment(linker, "/alternatename:?rva000078E7Target@@YAXXZ=??$__copy@PBVProductionPrerequisite@@PAV1@H@_STL@@YAPAVProductionPrerequisite@@PBV1@0PAV1@ABUrandom_access_iterator_tag@0@PAH@Z")
+#pragma comment(linker, "/alternatename:?rva0000CC3ETarget@@YAXXZ=??0FlightDeckBehaviorModuleData@@QAE@XZ")
 #pragma comment(linker, "/alternatename:?rva000119E1Target@@YAXXZ=?newLocomotor@LocomotorStore@@QBEPAVLocomotor@@PBVLocomotorTemplate@@@Z")
 #pragma comment(linker, "/alternatename:?rva00022683Target@@YAXXZ=?readDict@DataChunkInput@@QAE?AVDict@@XZ")
 #pragma comment(linker, "/alternatename:?rva0002F095Target@@YAXXZ=?copy@UnsignedPairTreeCopyShim@_STL@@QAEPAU?$_Rb_tree_node@U?$pair@$$CBGE@_STL@@@2@PAU32@0@Z")
@@ -19,6 +25,21 @@ extern void rva000399A5Target(void);
 void j_0000EF5C(void)
 {
 	rva0000EF5CTarget();
+}
+
+void j_000070B3(void)
+{
+	rva000070B3Target();
+}
+
+void j_000078E7(void)
+{
+	rva000078E7Target();
+}
+
+void j_0000CC3E(void)
+{
+	rva0000CC3ETarget();
 }
 
 void j_000119E1(void)
