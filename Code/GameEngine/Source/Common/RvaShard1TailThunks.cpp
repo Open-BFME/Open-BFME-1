c0e5387ce6987c7c48f096223d5cc89ca0a47cd6
Jonathan <jonf38@users.noreply.github.com>
2026-09-22 00:18:19 -0400

    Record opaque flush scheduling and tail thunks

--- Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
+++ Code/GameEngine/Source/Common/RvaShard1TailThunks.cpp
@@ -5,16 +5,30 @@
 // declarations preserve the real receiver and arguments on the caller stack.
 
 extern void rva0000EF5CTarget(void);
+extern void rva000119E1Target(void);
+extern void rva00022683Target(void);
 extern void rva0002F095Target(void);
 extern void rva000399A5Target(void);
 
 #pragma comment(linker, "/alternatename:?rva0000EF5CTarget@@YAXXZ=??0PropagandaTowerBehavior@@QAE@PAVThing@@PBVModuleData@@@Z")
+#pragma comment(linker, "/alternatename:?rva000119E1Target@@YAXXZ=?newLocomotor@LocomotorStore@@QBEPAVLocomotor@@PBVLocomotorTemplate@@@Z")
+#pragma comment(linker, "/alternatename:?rva00022683Target@@YAXXZ=?readDict@DataChunkInput@@QAE?AVDict@@XZ")
 #pragma comment(linker, "/alternatename:?rva0002F095Target@@YAXXZ=?copy@UnsignedPairTreeCopyShim@_STL@@QAEPAU?$_Rb_tree_node@U?$pair@$$CBGE@_STL@@@2@PAU32@0@Z")
 #pragma comment(linker, "/alternatename:?rva000399A5Target@@YAXXZ=?setOrientation@Thing@@QAEXM@Z")
 
 void j_0000EF5C(void)
 {
 	rva0000EF5CTarget();
+}
+
+void j_000119E1(void)
+{
+	rva000119E1Target();
+}
+
+void j_00022683(void)
+{
+	rva00022683Target();
 }
 
 void j_000399A5(void)
