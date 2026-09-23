0d90453ebd4042eef331c8f246c0b6f35150b3d9
TheThag <zackgreco@hotmail.com>
2026-09-23 11:51:30 -0300

    Bank near-exact UpdateSlotList source

---
+++ reverse/attempts/0x00624240.cpp
@@ -0,0 +1,201 @@
+// ?UpdateSlotList@@YAXPAVGameInfo@@QAPAVGameWindow@@1111PAV2@1@Z
+// partial score=0.99 date=2026-09-23
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /D_STLP_USE_STATIC_LIB /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /ICode/Libraries/Source/WWVegas/WWMath
+// stlport
+#include "PreRTS.h"
+#include "GameNetwork/GUIUtil.h"
+#include "GameNetwork/NetworkDefs.h"
+#include "GameClient/GameWindowManager.h"
+#include "GameClient/MapUtil.h"
+#include "Common/NameKeyGenerator.h"
+#include "Common/MultiplayerSettings.h"
+#include "GameClient/GadgetListBox.h"
+#include "GameClient/GadgetComboBox.h"
+#include "GameClient/GadgetTextEntry.h"
+#include "GameClient/GadgetStaticText.h"
+#include "GameClient/GadgetPushButton.h"
+#include "GameClient/GameText.h"
+#include "GameLogic/GameLogic.h"
+#include "GameNetwork/GameInfo.h"
+#include "Common/PlayerTemplate.h"
+#include "GameNetwork/LANAPICallbacks.h"
+#include "GameClient/ChallengeGenerals.h"
+
+extern int g_Va012F49D4;
+
+struct BfmeWolMapMetaData {
+	UnicodeString m_displayName;
+	UnicodeString m_description;
+	unsigned char m_extent[24];
+	int m_numPlayers;
+	bool m_isMultiplayer, m_isScenarioMP, m_isOfficial;
+};
+
+class BfmeUpdateSlotGameInfoView {
+public:
+	virtual Int slot000() = 0;
+	virtual Int slot004() = 0;
+	virtual void reset() = 0;
+	virtual void startGame(Int gameID) = 0;
+	virtual Bool amIHost() const = 0;
+	virtual Int getLocalSlotNum() const = 0;
+};
+
+Bool WouldMapTransfer(GameInfo *game);
+
+static Bool winInitialized = FALSE;
+
+Bool AreSlotListUpdatesEnabled(void)
+{
+	return winInitialized;
+}
+
+void UpdateSlotList( GameInfo *myGame, GameWindow *comboPlayer[],
+										GameWindow *comboColor[], GameWindow *comboPlayerTemplate[],
+										GameWindow *comboTeam[], GameWindow *buttonAccept[], 
+										GameWindow *buttonStart, GameWindow *buttonMapStartPosition[] )
+{
+	if (g_Va012F49D4)
+		return;
+	if(!AreSlotListUpdatesEnabled())
+		return;
+	//LANGameInfo *myGame = TheLAN->GetMyGame();
+
+	const BfmeWolMapMetaData *mapData = (const BfmeWolMapMetaData *)TheMapCache->findMap( myGame->getMap() );
+	Bool willTransfer = TRUE;
+	if (mapData)
+	{
+		willTransfer = !mapData->m_isOfficial;
+	}
+	else
+	{
+		willTransfer = WouldMapTransfer(myGame);
+	}
+
+	if (myGame)
+	{
+		for( int i =0; i < MAX_SLOTS; i++ )
+		{
+			GameSlot * slot = myGame->getSlot(i);
+			// if i'm host, enable the controls for AI
+			if(((BfmeUpdateSlotGameInfoView *)myGame)->amIHost() && slot && slot->isAI())
+			{
+				EnableAcceptControls(TRUE, myGame, comboPlayer, comboColor, comboPlayerTemplate,
+					comboTeam, buttonAccept, buttonStart, buttonMapStartPosition, i);
+			}
+			else if (slot && ((BfmeUpdateSlotGameInfoView *)myGame)->getLocalSlotNum() == i)
+			{
+				if(slot->isAccepted() && !((BfmeUpdateSlotGameInfoView *)myGame)->amIHost())
+				{
+					EnableAcceptControls(FALSE, myGame, comboPlayer, comboColor, comboPlayerTemplate,
+						comboTeam, buttonAccept, buttonStart, buttonMapStartPosition);
+				}
+				else
+				{
+					if (slot->hasMap()) {
+						EnableAcceptControls(TRUE, myGame, comboPlayer, comboColor, comboPlayerTemplate,
+							comboTeam, buttonAccept, buttonStart, buttonMapStartPosition);
+					}
+					else
+					{
+						EnableAcceptControls(willTransfer, myGame, comboPlayer, comboColor, comboPlayerTemplate,
+							comboTeam, buttonAccept, buttonStart, buttonMapStartPosition);
+					}
+				}
+				
+			}
+			else if(((BfmeUpdateSlotGameInfoView *)myGame)->amIHost())
+			{
+				EnableAcceptControls(FALSE, myGame, comboPlayer, comboColor, comboPlayerTemplate,
+					comboTeam, buttonAccept, buttonStart, buttonMapStartPosition, i);
+			}
+			if(slot && slot->isHuman())
+			{
+				UnicodeString newName = slot->getName();
+				UnicodeString oldName = GadgetComboBoxGetText(comboPlayer[i]);
+				if (comboPlayer[i] && newName.compare(oldName))
+				{
+					GadgetComboBoxSetText(comboPlayer[i], newName);
+				}
+				if(i!= 0 && buttonAccept && buttonAccept[i])
+				{
+					buttonAccept[i]->winHide(FALSE);
+				//Color In the little accepted boxes
+					if(slot->isAccepted())
+					{
+						if(BitTest(buttonAccept[i]->winGetStatus(), WIN_STATUS_IMAGE	))
+							buttonAccept[i]->winEnable(TRUE);
+						else
+							GadgetButtonSetEnabledColor(buttonAccept[i], acceptTrueColor );
+					}
+					else
+					{
+						if(BitTest(buttonAccept[i]->winGetStatus(), WIN_STATUS_IMAGE	))
+							buttonAccept[i]->winEnable(FALSE);
+						else
+							GadgetButtonSetEnabledColor(buttonAccept[i], acceptFalseColor );
+					}
+				}
+			}
+			else
+			{				
+				GadgetComboBoxSetSelectedPos(comboPlayer[i], slot->getState(), TRUE);
+        if( buttonAccept &&  buttonAccept[i] )
+				  buttonAccept[i]->winHide(TRUE);
+			}
+/*
+			if (myGame->getLocalSlotNum() == i && i!=0)
+			{
+				if (comboPlayer[i])
+					comboPlayer[i]->winEnable( TRUE );
+			}
+			else*/ if (!((BfmeUpdateSlotGameInfoView *)myGame)->amIHost())
+			{
+				if (comboPlayer[i])
+					comboPlayer[i]->winEnable( FALSE );
+			}
+			//if( i == myGame->getLocalSlotNum())
+      if((comboColor[i] != NULL) && BitTest(comboColor[i]->winGetStatus(), WIN_STATUS_ENABLED))
+				PopulateColorComboBox(i, comboColor, myGame, myGame->getConstSlot(i)->getPlayerTemplate() == PLAYERTEMPLATE_OBSERVER);
+			Int max, idx;
+			if (comboColor[i] != NULL) {
+				max = GadgetComboBoxGetLength(comboColor[i]);
+				for (idx=0; idx<max; ++idx)
+				{
+					Int color = (Int)GadgetComboBoxGetItemData(comboColor[i], idx);
+					if (color == slot->getColor())
+					{
+						GadgetComboBoxSetSelectedPos(comboColor[i], idx, TRUE);
+						break;
+					}
+				}
+			}
+
+			if (comboTeam[i] != NULL) {
+				max = GadgetComboBoxGetLength(comboTeam[i]);
+				for (idx=0; idx<max; ++idx)
+				{
+					Int team = (Int)GadgetComboBoxGetItemData(comboTeam[i], idx);
+					if (team == slot->getTeamNumber())
+					{
+						GadgetComboBoxSetSelectedPos(comboTeam[i], idx, TRUE);
+						break;
+					}
+				}
+			}
+
+			if (comboPlayerTemplate[i] != NULL) {
+				max = GadgetComboBoxGetLength(comboPlayerTemplate[i]);
+				for (idx=0; idx<max; ++idx)
+				{
+					Int playerTemplate = (Int)GadgetComboBoxGetItemData(comboPlayerTemplate[i], idx);
+					if (playerTemplate == slot->getPlayerTemplate())
+					{
+						GadgetComboBoxSetSelectedPos(comboPlayerTemplate[i], idx, TRUE);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
