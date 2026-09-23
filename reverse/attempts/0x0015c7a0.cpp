d2d5a0361cfdb27bfe204e74834664150184b967
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-24 01:00:38 +0200

    reverse: bank measurable 0x0015C7A0 AIGuardOuterState::onEnter

--- reverse/attempts/0x0015c7a0.cpp
+++ reverse/attempts/0x0015c7a0.cpp
@@ -1,211 +1,288 @@
-// ?run@SelfR0015C7A0@@QAEHXZ
-// partial score=0.55 date=2026-09-05
+// ?onEnter@AIGuardOuterState@@UAE?AW4StateReturnType@@XZ
+// partial score=0.71 date=2026-09-24
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: address-derived recovery, retail 0x0015C7A0, 476 bytes.
-//
-// Draft reconstruction: an EHsc frame wraps a `new T(args)` (matches the
-// documented U4NewThenInit shape), several lookups through two global
-// singletons (0x012F0898, 0x012ED810), a three-way choice of Coord3D
-// source, an early-exit guard on a `sub` lookup, a conditional call to a
-// still-unconverted callee at 0x00004C37, and a tail pair of virtual calls.
-// Identity of the owning class and most field types is unresolved; this is
-// an address-derived name pending probe.py convergence.
+// ABI views follow the decoded 0x0015C7A0 body and its complete callees.
 
 typedef int Int;
+typedef unsigned int UnsignedInt;
+typedef bool Bool;
 typedef float Real;
 
-struct Coord0015C7A0
+enum StateReturnType
 {
-	Real x, y, z;
+	STATE_CONTINUE = 0,
+	STATE_SUCCESS = -1,
+	STATE_FAILURE = -2
 };
 
-class BfmeX1011R0015C7A0;
-class TeamR0015C7A0;
+struct Coord3D
+{
+	Real x;
+	Real y;
+	Real z;
+};
 
-class BfmeLook1011R0015C7A0
+class Object
 {
 public:
-	BfmeX1011R0015C7A0 *bfmeFind1011(Int id);
-	char m_pad[0x3C];
-	Int m_field3C;
+	unsigned char m_pad00[0x38];
+	Coord3D m_position;
 };
 
-class BfmeX1011R0015C7A0
+class StateMachine
 {
 public:
-	char m_pad[0x38];
-	Coord0015C7A0 m_coord;
+	virtual void slot00();
+	virtual void slot04();
+	virtual void slot08();
+	virtual void slot0c();
+	virtual void slot10();
+	virtual void slot14();
+	virtual void slot18();
+	virtual void slot1c();
+	virtual void slot20();
+	virtual void slot24();
+	virtual void slot28();
+	virtual void slot2c();
+	virtual void slot30();
+	virtual void slot34();
+	virtual void setGoalObject(const Object *object);
 };
 
-class TeamR0015C7A0
+class Rva0015C7A0GuardMachine : public StateMachine
 {
 public:
-	Coord0015C7A0 *getEstimateTeamPosition_000EDCD0(Coord0015C7A0 *out) const;
+	unsigned char m_body[0x0c];
+	Object *m_owner;
+	unsigned char m_pad14[0x30];
+	Int m_targetID;
+	UnsignedInt m_teamID;
+	class PolygonTrigger *m_area;
+	Coord3D m_position;
+	unsigned char m_pad5c[0x0c];
+	Bool m_field68;
+	unsigned char m_pad69[3];
+	Int m_nemesisID;
+	Int m_field70;
 };
 
-class TeamFactoryR0015C7A0
+class GameLogic
 {
 public:
-	void *find(Int id);
+	Object *findObjectByID(Int id);
+	unsigned char m_pad00[0x3c];
+	UnsignedInt m_frame;
 };
 
-class LayerQueryR0015C7A0
+class Team
 {
 public:
-	Int getLayer(void) const;
+	Coord3D *getEstimateTeamPosition(Coord3D *position) const;
 };
 
-class PolygonTriggerR0015C7A0
+class TeamFactory
 {
 public:
-	void getCenterPoint(Coord0015C7A0 *out) const;
+	Team *findTeamByID(UnsignedInt id);
 };
 
-struct FactorsR0015C7A0
+class PolygonTrigger
 {
-	char m_pad[0x3C];
-	Int m_field3C;
+public:
+	void getCenterPoint(Coord3D *position) const;
 };
 
-struct AIRootR0015C7A0
+class FactorsR0015C7A0
 {
-	char m_pad[0x14];
+public:
+	unsigned char m_pad00[0x3c];
+	UnsignedInt m_field3c;
+};
+
+class AIRootR0015C7A0
+{
+public:
+	unsigned char m_pad00[0x14];
 	FactorsR0015C7A0 *m_factors;
 };
 
-class TailR0015C7A0
+class AttackExitConditionsInterface
 {
 public:
-	virtual void v00();
-	virtual void v04();
-	virtual void v08();
-	virtual void v0C();
-	virtual Int v10Call(void);				// +0x10
+	virtual Bool shouldExit(const StateMachine *machine) const = 0;
 };
 
-class QR0015C7A0
+class QR0015C7A0 : public AttackExitConditionsInterface
 {
 public:
-	char m_pad[0x1C];
-	TailR0015C7A0 *m_tail;					// +0x1C
+	virtual Bool shouldExit(const StateMachine *machine) const
+	{
+		return false;
+	}
+	Int m_conditionsToConsider;
+	Coord3D m_center;
+	Real m_radiusSqr;
+	UnsignedInt m_field18;
 };
-
-class QVtblR0015C7A0
-{
-public:
-	virtual void v00();
-	virtual void v04();
-	virtual void v08();
-	virtual void v0C();
-	virtual void v10();
-	virtual void v14();
-	virtual void v18();
-	virtual void v1C();
-	virtual void v20();
-	virtual void v24();
-	virtual void v28();
-	virtual void v2C();
-	virtual void v30();
-	virtual void v34();
-	virtual void v38Call(void *arg, Int flag);		// +0x38
-	virtual Int v3CReserved();
-};
-
-extern "C" BfmeLook1011R0015C7A0 *g_bfmeClockBK;		// 0x012F0898
-extern TeamFactoryR0015C7A0 *TheTeamFactoryR0015C7A0;		// 0x012ED810
-extern "C" AIRootR0015C7A0 *TheAIParseDefinitionAI;		// 0x012EF214
-
-extern bool bfmeCallENJ(void *a, void *b);
-extern Real aiGetAdjustedVisionRangeForObjectR0015C7A0(void *object, Int flags);
 
 class CtorTargetR0015C7A0
 {
 public:
-	CtorTargetR0015C7A0(void *container, Int a, Int b, Int c, void *out);
-	char m_body[0x54];
+	CtorTargetR0015C7A0(StateMachine *machine, Bool follow,
+		Bool attackingObject, Bool forceAttacking,
+		AttackExitConditionsInterface *conditions);
+	virtual void slot00();
+	virtual void slot04();
+	virtual void slot08();
+	virtual StateReturnType onEnter();
+	unsigned char m_body[0x18];
+	StateMachine *m_machine;
 };
 
-class SelfR0015C7A0
+class AIGuardOuterState
 {
 public:
-	Int run(void);
+	virtual StateReturnType onEnter();
+	unsigned char m_body[0x18];
+	StateMachine *m_machine;
+	unsigned char m_pad20[4];
+	QR0015C7A0 m_exitConditions;
+	CtorTargetR0015C7A0 *m_attackState;
 };
 
-// ?run@SelfR0015C7A0@@QAEHXZ
-Int SelfR0015C7A0::run(void)
+extern void j_0000bf3c();
+extern void j_00004c37();
+extern void j_00007ad6();
+extern void j_0001f253();
+extern void j_000241fe();
+extern void j_0003a391();
+extern void j_00044c2e();
+
+typedef Object *(GameLogic::*FindObjectByIDCall)(Int);
+typedef Team *(TeamFactory::*FindTeamByIDCall)(UnsignedInt);
+typedef Coord3D *(Team::*EstimateTeamPositionCall)(Coord3D *) const;
+typedef Int (Object::*GetLayerCall)() const;
+typedef void (PolygonTrigger::*GetCenterPointCall)(Coord3D *) const;
+typedef Bool (__cdecl *OutOfWeaponRangeCall)(Object *, Object *);
+
+class Rva002BD020AI
 {
-	char *self = reinterpret_cast<char *>(this);
-	char *container = *reinterpret_cast<char **>(self + 0x1C);
+public:
+	static Real getAdjustedVisionRangeForObject(const Object *object, Int flags);
+};
 
-	Int containerFlag = *reinterpret_cast<Int *>(container + 0x70);
-	if (containerFlag == 1)
-		return -1;
+#pragma comment(linker, "/alternatename:??0CtorTargetR0015C7A0@@QAE@PAVStateMachine@@_N11PAVAttackExitConditionsInterface@@@Z=?j_0000bf3c@@YAXXZ")
 
-	BfmeX1011R0015C7A0 *r1 = g_bfmeClockBK->bfmeFind1011(
+StateReturnType AIGuardOuterState::onEnter()
+{
+	unsigned char *self = (unsigned char *)this;
+	unsigned char *container = *reinterpret_cast<unsigned char **>(self + 0x1c);
+	Int guardMode = *reinterpret_cast<Int *>(container + 0x70);
+	if (guardMode == 1)
+		return STATE_SUCCESS;
+
+	Coord3D position;
+	{
+		GameLogic *logic = *(GameLogic **)0x012F0898;
+		union
+		{
+			void *asVoid;
+			FindObjectByIDCall asMember;
+		} findObjectCast;
+		findObjectCast.asVoid = (void *)j_0001f253;
+		Object *target = (logic->*findObjectCast.asMember)(
 			*reinterpret_cast<Int *>(container + 0x44));
-	void *teamRaw = TheTeamFactoryR0015C7A0->find(
-			*reinterpret_cast<Int *>(container + 0x48));
 
-	Coord0015C7A0 tmp;
+		union
+		{
+			void *asVoid;
+			FindTeamByIDCall asMember;
+		} findTeamCast;
+		findTeamCast.asVoid = (void *)j_00044c2e;
+		UnsignedInt teamID = *reinterpret_cast<UnsignedInt *>(container + 0x48);
+		TeamFactory *factory = *(TeamFactory **)0x012ED810;
+		Team *team = (factory->*findTeamCast.asMember)(teamID);
 
-	if (r1)
-	{
-		tmp = r1->m_coord;
-	}
-	else if (teamRaw)
-	{
-		reinterpret_cast<TeamR0015C7A0 *>(teamRaw)->getEstimateTeamPosition_000EDCD0(&tmp);
-	}
-	else
-	{
-		char *container2 = *reinterpret_cast<char **>(self + 0x1C);
-		tmp = *reinterpret_cast<Coord0015C7A0 *>(container2 + 0x50);
-	}
-
-	container = *reinterpret_cast<char **>(self + 0x1C);
-	BfmeX1011R0015C7A0 *sub = g_bfmeClockBK->bfmeFind1011(
-			*reinterpret_cast<Int *>(container + 0x6C));
-	if (!sub)
-		return -1;
-
-	LayerQueryR0015C7A0 *obj = *reinterpret_cast<LayerQueryR0015C7A0 **>(container + 0x10);
-
-	if (obj->getLayer() != 1)
-	{
-		if (!bfmeCallENJ(obj, sub))
-			return -1;
-	}
-
-	Real range = aiGetAdjustedVisionRangeForObjectR0015C7A0(obj, 3);
-
-	char *c2 = *reinterpret_cast<char **>(self + 0x1C);
-	PolygonTriggerR0015C7A0 *pt = *reinterpret_cast<PolygonTriggerR0015C7A0 **>(c2 + 0x4C);
-
-	if (pt)
-	{
-		if (*reinterpret_cast<unsigned char *>(c2 + 0x68))
-			tmp = *reinterpret_cast<Coord0015C7A0 *>(c2 + 0x5C);
+		if (target)
+		{
+			position = target->m_position;
+		}
+		else if (team)
+		{
+			union
+			{
+				void *asVoid;
+				EstimateTeamPositionCall asMember;
+			} positionCast;
+			positionCast.asVoid = (void *)j_000241fe;
+			(team->*positionCast.asMember)(&position);
+		}
 		else
-			pt->getCenterPoint(&tmp);
+		{
+			unsigned char *container2 =
+				*reinterpret_cast<unsigned char **>(self + 0x1c);
+			position = *reinterpret_cast<Coord3D *>(container2 + 0x50);
+		}
 	}
 
-	*reinterpret_cast<Coord0015C7A0 *>(self + 0x2C) = tmp;
-	*reinterpret_cast<Real *>(self + 0x38) = range * tmp.x;
+	container = *reinterpret_cast<unsigned char **>(self + 0x1c);
+	GameLogic *logic = *(GameLogic **)0x012F0898;
+	union
+	{
+		void *asVoid;
+		FindObjectByIDCall asMember;
+	} findObjectCast;
+	findObjectCast.asVoid = (void *)j_0001f253;
+	Object *nemesis = (logic->*findObjectCast.asMember)(
+		*reinterpret_cast<Int *>(container + 0x6c));
+	if (!nemesis)
+		return STATE_SUCCESS;
 
-	*reinterpret_cast<Int *>(self + 0x3C) =
-			g_bfmeClockBK->m_field3C + TheAIParseDefinitionAI->m_factors->m_field3C;
-	*reinterpret_cast<Int *>(self + 0x28) = 7;
+	Object *owner = *reinterpret_cast<Object **>(container + 0x10);
+	union
+	{
+		void *asVoid;
+		GetLayerCall asMember;
+	} layerCast;
+	layerCast.asVoid = (void *)j_0003a391;
+	if ((owner->*layerCast.asMember)() != 1)
+	{
+		OutOfWeaponRangeCall call =
+			(OutOfWeaponRangeCall)j_00004c37;
+		if (!call(owner, nemesis))
+			return STATE_SUCCESS;
+	}
 
-	CtorTargetR0015C7A0 *ctorResult =
-			new CtorTargetR0015C7A0(container, 0, 1, 0, self + 0x24);
-	*reinterpret_cast<void **>(self + 0x40) = ctorResult;
+	Real range = Rva002BD020AI::getAdjustedVisionRangeForObject(owner, 3);
+	unsigned char *container2 = *reinterpret_cast<unsigned char **>(self + 0x1c);
+	PolygonTrigger *area = *reinterpret_cast<PolygonTrigger **>(container2 + 0x4c);
+	if (area)
+	{
+		if (*(unsigned char *)(container2 + 0x68))
+			position = *reinterpret_cast<Coord3D *>(container2 + 0x5c);
+		else
+		{
+			union
+			{
+				void *asVoid;
+				GetCenterPointCall asMember;
+			} centerCast;
+			centerCast.asVoid = (void *)j_00007ad6;
+			(area->*centerCast.asMember)(&position);
+		}
+	}
 
-	QR0015C7A0 *q = reinterpret_cast<QR0015C7A0 *>(ctorResult);
-	TailR0015C7A0 *tail = q->m_tail;
-	reinterpret_cast<QVtblR0015C7A0 *>(tail)->v38Call(sub, -1);
+	m_exitConditions.m_center = position;
+	m_exitConditions.m_radiusSqr = range * range;
+	logic = *(GameLogic **)0x012F0898;
+	AIRootR0015C7A0 *ai = *(AIRootR0015C7A0 **)0x012EF214;
+	m_exitConditions.m_field18 =
+		logic->m_frame + ai->m_factors->m_field3c;
+	m_exitConditions.m_conditionsToConsider = 7;
 
-	TailR0015C7A0 *finalObj = *reinterpret_cast<TailR0015C7A0 **>(self + 0x40);
-	Int callResult = finalObj->v10Call();
-
-	return callResult != 0 ? -1 : 0;
+	m_attackState = new CtorTargetR0015C7A0(
+		m_machine, false, true, false, &m_exitConditions);
+	m_attackState->m_machine->setGoalObject(nemesis);
+	StateReturnType result = m_attackState->onEnter();
+	return result == STATE_CONTINUE ? STATE_CONTINUE : STATE_SUCCESS;
 }
