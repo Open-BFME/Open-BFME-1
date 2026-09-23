afec82337145850a9624050a9d464903a736f175
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-23 10:02:21 +0200

    reverse: bank closer ExpLevelDraw parser

--- reverse/attempts/0x00284e70.cpp
+++ reverse/attempts/0x00284e70.cpp
@@ -1,28 +1,19 @@
-// ?rva00284E70ParseObjectName@BannerCarrierUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
-// partial score=0.36 date=2026-09-12
-// cl: /DNDEBUG /DWIN32 /MD /EHsc
+// ?rva00284E70ParseExpLevelDraw@@YAXPAVINI@@PAX1PBX@Z
+// partial score=0.4 date=2026-09-23
+// ?rva00284E70ParseExpLevelDraw@@YAXPAVINI@@PAX1PBX@Z
+// cl: /DNDEBUG /DWIN32 /MD /EHsc /Oi /D_STLP_USE_STATIC_LIB /Ireference/shims/iniexception
+// stlport
 
-typedef bool Bool;
-typedef unsigned int UnsignedInt;
+#include <vector>
+#include "Common/INIException.h"
 
-#include <new>
-extern "C" void _ReadWriteBarrier();
-#pragma intrinsic(_ReadWriteBarrier)
+typedef unsigned char Bool;
 
-void *__cdecl operator new(UnsignedInt);
 extern "C" int __cdecl strcmp(const char *, const char *);
+extern "C" __declspec(dllimport) int __cdecl _strcmpi(const char *, const char *);
 extern "C" unsigned int __cdecl strlen(const char *);
 #pragma intrinsic(strcmp)
 #pragma intrinsic(strlen)
-
-class RetailLayoutString
-{
-public:
-	RetailLayoutString() : m_data(0) {}
-	~RetailLayoutString();
-	void set(const char *text, int length);
-	void *m_data;
-};
 
 class INI
 {
@@ -34,78 +25,58 @@ public:
 	const char *m_tokenDelimiters;
 };
 
-class INIException
+class RetailLayoutString
 {
 public:
-	INIException(int count, const char *format, ...);
+	RetailLayoutString() : m_data(0) {}
+	void set(const char *text, int length);
+	void *m_data;
+};
+
+class BannerCarrierString
+{
+public:
+	~BannerCarrierString();
+};
+
+class BannerCarrierUpgrade
+{
+public:
+	~BannerCarrierUpgrade();
+	int m_level;
+	std::vector<BannerCarrierString *> m_strings;
 };
 
 struct BannerCarrierSubObject
 {
-	BannerCarrierSubObject() : m_show(false) {}
 	RetailLayoutString m_name;
 	Bool m_show;
-	unsigned char m_pad[3];
 };
 
-struct BannerCarrierObjectNameParse
-{
-	enum MagicEnum { GLUE_NOT_IMPLEMENTED = 0 };
-	inline void *operator new(UnsignedInt size, MagicEnum)
-	{
-		return ::operator new(size);
-	}
-	BannerCarrierObjectNameParse() : m_begin(0), m_end(0), m_storageEnd(0) {}
-	int m_level;
-	BannerCarrierSubObject **m_begin;
-	BannerCarrierSubObject **m_end;
-	BannerCarrierSubObject **m_storageEnd;
-};
-
-class BannerCarrierSubObjectVector
-{
-public:
-	void append(BannerCarrierSubObject *const *value);
-};
-
-class BannerCarrierObjectNameVector
-{
-public:
-	void append(BannerCarrierObjectNameParse *const *value);
-};
-
-class BannerCarrierUpdateModuleData
-{
-public:
-	static void rva00284E70ParseObjectName(INI *ini, void *instance,
-		void *store, const void *userData);
-};
-
-void BannerCarrierUpdateModuleData::rva00284E70ParseObjectName(
+void rva00284E70ParseExpLevelDraw(
 	INI *ini, void *, void *store, const void *)
 {
-	BannerCarrierObjectNameParse *record;
-	record = new (BannerCarrierObjectNameParse::GLUE_NOT_IMPLEMENTED)
-		BannerCarrierObjectNameParse;
+	BannerCarrierUpgrade *record = new BannerCarrierUpgrade;
 	const char *token = ini->getNextTokenOrNull(ini->m_tokenDelimiters);
 	if (token == 0 || strcmp(token, "Level") != 0)
 		throw INIException(3, "'ModelState' expected");
 
-	record->m_level = INI::scanInt(ini->getNextToken(ini->m_tokenDelimiters));
+	record->m_level = INI::scanInt(
+		ini->getNextToken(ini->m_tokenDelimiters));
 	token = ini->getNextTokenOrNull(ini->m_tokenDelimiters);
 	while (token != 0)
 	{
 		if (strcmp(token, "SubObject") != 0)
 			throw INIException(3, "UnitType expected");
 
-		BannerCarrierSubObject *subObject = new BannerCarrierSubObject;
+		BannerCarrierSubObject *entry = new BannerCarrierSubObject;
 		const char *showToken = ini->getNextToken(ini->m_tokenDelimiters);
-		subObject->m_show = strcmp(showToken, "Show") == 0;
+		entry->m_show = _strcmpi(showToken, "Show") == 0;
 		const char *name = ini->getNextToken(ini->m_tokenDelimiters);
-		subObject->m_name.set(name, name ? (int)strlen(name) : 0);
-		((BannerCarrierSubObjectVector *)&record->m_begin)->append(&subObject);
+		entry->m_name.set(name, name ? (int)strlen(name) : 0);
+		record->m_strings.push_back((BannerCarrierString *)entry);
 		token = ini->getNextTokenOrNull(ini->m_tokenDelimiters);
 	}
 
-	((BannerCarrierObjectNameVector *)store)->append(&record);
+	((std::vector<BannerCarrierUpgrade *> *)store)->push_back(record);
 }
