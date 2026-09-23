99b6f01d247075bb7a7c213b5368c631ceba7a4a
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-23 09:21:32 +0200

    fix: recover sound upgrade parser at 0x006087E0

--- Code/GameEngine/Source/GameClient/Drawable/Behavior/Rva00608FE0ElementParse.cpp
+++ Code/GameEngine/Source/GameClient/Drawable/Behavior/Rva00608FE0ElementParse.cpp
@@ -1,17 +1,6 @@
 // ?parse@Rva00608FE0Element@@QAEXPAVINI@@@Z
-// partial score=0.9411764705882353 date=2026-09-22
-// ?parse@Rva00608FE0Element@@QAEXPAVINI@@@Z
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/Libraries/Source/WWVegas/WWLib
 // stlport
-// scratch reconstruction of retail 0x006087E0; not a tracked source claim
-// Retail callee evidence: erase=0x0065960 via ILT 0x00024C17, token=0x008509C0,
-// overflow=0x0063700 via ILT 0x0003827B, releaseBuffer=0x00887940,
-// StringBase<char> copy=0x00887B60 and cstr ctor=0x00888BC0,
-// getVoiceFieldParse=0x0013CE10 via ILT 0x0002698B, add=0x00850920,
-// initFromINIMulti=0x00851910, and buildMasks=0x00608090 via ILT 0x0000F187.
-// The 0x210-byte record lifetime is matched by ctor/copy/dtor at
-// 0x006086E0/0x006089C0/0x00608480 (ILT 0x0002A48C/0x00042339/0x00034158).
-
 #include <new>
 #include <bitset>
 struct Rva006087E0Mask { _STL::bitset<192> bits; void clear() { bits.reset(); } };
@@ -69,11 +58,11 @@ public:
 	void parse(INI *ini);
 
 private:
-	Rva006087E0Mask m_values0;
-	Rva006087E0Mask m_values18;
-	_STL::vector<AsciiString, _STL::allocator<AsciiString> > m_soundNames;
-	_STL::vector<AsciiString, _STL::allocator<AsciiString> > m_otherNames;
-	unsigned char m_tail[0x1c8];
+	Rva006087E0Mask m_mask000;
+	Rva006087E0Mask m_mask018;
+	_STL::vector<AsciiString, _STL::allocator<AsciiString> > m_vector030;
+	_STL::vector<AsciiString, _STL::allocator<AsciiString> > m_vector03c;
+	unsigned char m_tail048[0x1c8];
 };
 
 #pragma comment(linker, "/alternatename:??0Rva00608FE0Element@@QAE@XZ=?j_0002a48c@@YAXXZ")
@@ -82,16 +71,17 @@ private:
 
 void Rva00608FE0Element::parse(INI *ini)
 {
-	m_soundNames.erase(m_soundNames.begin(), m_soundNames.end());
-	m_values0.clear();
-	m_otherNames.erase(m_otherNames.begin(), m_otherNames.end());
-	m_values18.clear();
+	_STL::vector<AsciiString, _STL::allocator<AsciiString> > *firstVector = &m_vector030;
+	firstVector->erase(firstVector->begin(), firstVector->end());
+	m_mask000.clear();
+	m_vector03c.erase(m_vector03c.begin(), m_vector03c.end());
+	m_mask018.clear();
 
 	for (const char *token = ini->getNextTokenOrNull(0); token != 0;
 		token = ini->getNextTokenOrNull(0))
 	{
 		AsciiString value(token);
-		m_soundNames.push_back(value);
+		firstVector->push_back(value);
 	}
 
 	MultiIniFieldParse fields;
