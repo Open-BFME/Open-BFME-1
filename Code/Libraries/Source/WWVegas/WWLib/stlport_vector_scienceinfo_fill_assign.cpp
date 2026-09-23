5d366f8d5ea05ce7beb8863536917b6d41d65b75
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-23 05:17:54 +0200

    convert vector<ScienceInfo*> _M_fill_assign

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_vector_scienceinfo_fill_assign.cpp
@@ -0,0 +1,8 @@
+// cl: /Od /EHsc /Ob2
+// stlport
+#include <vector>
+
+// ?_M_fill_assign@?$vector@PAVScienceInfo@@V?$allocator@PAVScienceInfo@@@_STL@@@_STL@@QAEXIABQAVScienceInfo@@@Z
+// Element type from the landed vector<ScienceInfo *>::erase this body calls at 0x000CF750.
+class ScienceInfo;
+template class _STL::vector<ScienceInfo *>;
