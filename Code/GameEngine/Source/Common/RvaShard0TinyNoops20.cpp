6400f45fba2815a175d086482fb10ae7fb167617
Dom Clarke <zajad909@mystis.dev>
2026-09-23 14:35:01 +0100

    Convert 19 carved one-byte returns to C++

---
+++ Code/GameEngine/Source/Common/RvaShard0TinyNoops20.cpp
@@ -0,0 +1,23 @@
+// cl: /DNDEBUG /MD /O2 /EHsc
+
+// Anonymous carved leaves whose complete retail bodies are one `ret`.
+// Address-qualified names avoid unsupported semantic owner claims.
+void dup_00078cf0() {}
+void dup_000be810() {}
+void dup_000ca6c0() {}
+void dup_000e45c0() {}
+void dup_00122370() {}
+void dup_001a8770() {}
+void dup_001d68c0() {}
+void dup_0022fcf0() {}
+void dup_00385720() {}
+void dup_003cbd70() {}
+void dup_0040f510() {}
+void dup_005706e0() {}
+void dup_006401d0() {}
+void dup_00652250() {}
+void dup_00682ce0() {}
+void dup_0073d9a0() {}
+void dup_00758d90() {}
+void dup_0075d760() {}
+void dup_0075ef80() {}
