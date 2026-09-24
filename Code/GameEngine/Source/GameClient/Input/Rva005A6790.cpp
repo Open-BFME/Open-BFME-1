d2cf9341604906c6613347e1352a66dcb2a377ce
admiraly <249692417+admiraly@users.noreply.github.com>
2026-09-24 19:19:35 +0200

    preserve Mouse global ABI in setter adapter

--- Code/GameEngine/Source/GameClient/Input/Rva005A6790.cpp
+++ Code/GameEngine/Source/GameClient/Input/Rva005A6790.cpp
@@ -9,24 +9,28 @@ extern void j_000423b6(void);
 struct Rva00579160Manager {};
 extern Rva00579160Manager *Rva00579160TheManager;
 
-struct Rva005A63D0Mouse
-{
-	virtual void slot00(void); virtual void slot04(void);
-	virtual void slot08(void); virtual void slot0c(void);
-	virtual void slot10(void); virtual void slot14(void);
-	virtual void slot18(void); virtual void slot1c(void);
-	virtual void slot20(void); virtual void slot24(void);
-	virtual void slot28(void); virtual void slot2c(void);
-	virtual void slot30(void); virtual void slot34(void);
-	virtual void setCursor(int value);
-};
+class Mouse;
+typedef Mouse Rva005A63D0Mouse;
 extern Rva005A63D0Mouse *TheMouse;
 
 typedef void (__fastcall *Rva005A6790Slot38Fn)(Rva005A63D0Mouse *, int, int);
 struct Rva005A6790MouseVtable
 {
-	void *slots[14];
-	Rva005A6790Slot38Fn slot38;
+	void *slot00;
+	void *slot04;
+	void *slot08;
+	void *slot0c;
+	void *slot10;
+	void *slot14;
+	void *slot18;
+	void *slot1c;
+	void *slot20;
+	void *slot24;
+	void *slot28;
+	void *slot2c;
+	void *slot30;
+	void *slot34;
+	Rva005A6790Slot38Fn setCursor;
 };
 
 struct Rva005A6790Object
@@ -47,7 +51,7 @@ int Rva005A6790Object::setValue(int value)
 		int cursorValue = m_value;
 		Rva005A6790MouseVtable *vtable =
 			*(Rva005A6790MouseVtable **)mouse;
-		vtable->slot38(mouse, cursorValue, cursorValue);
+		vtable->setCursor(mouse, cursorValue, cursorValue);
 	}
 	return old;
 }
