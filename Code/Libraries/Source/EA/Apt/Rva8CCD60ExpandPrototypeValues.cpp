ff0fb255c4f4046fe0a2f3bf2da4fbd1309b3376
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-24 03:56:13 +0200

    feat(reverse): recover Apt prototype value expansion

--- Code/Libraries/Source/EA/Apt/Rva8CCD60ExpandPrototypeValues.cpp
+++ Code/Libraries/Source/EA/Apt/Rva8CCD60ExpandPrototypeValues.cpp
@@ -1,6 +1,4 @@
 // ?expandPrototypeValues@Rva8CCD60State@@QAEXPAX0@Z
-// partial score=0.74 date=2026-09-02
-// ?expandPrototypeValues@Rva8CCD60State@@QAEXPAX0@Z
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
 // readable Apt prototype-chain expansion, retail 0x008CCD60 (363 bytes).
 
@@ -15,6 +13,18 @@ struct Rva8CCD60StringBlock
 };
 
 extern void (__cdecl **Rva01337A30ReleaseTable)(void *);
+
+struct BfmeIterator1285;
+
+class BfmeIteratorList1285
+{
+public:
+	BfmeIterator1285 *bfmeFirst1285();
+	BfmeIterator1285 *bfmeNext1285(BfmeIterator1285 *iterator);
+
+	int m_count;
+	BfmeIterator1285 *m_array;
+};
 
 class Rva8CCD60IteratorList;
 
@@ -35,39 +45,64 @@ public:
 	}
 
 	unsigned m_flags;
-	char m_gap[0x18];
+	Rva8CCD60StringBlock *m_string;
+	char m_gap[0x14];
 	Rva8CCD60Value *m_indirect;
 };
 
 struct Rva8CCD60Iterator
 {
 	Rva8CCD60StringBlock *m_name;
+	void *m_extra;
 };
 
-class Rva8CCD60IteratorList
+class Rva8CCD60IteratorList : public BfmeIteratorList1285
 {
 public:
-	Rva8CCD60Iterator *first();
-	Rva8CCD60Iterator *next(Rva8CCD60Iterator *iterator);
+	Rva8CCD60Iterator *first()
+	{
+		return (Rva8CCD60Iterator *)bfmeFirst1285();
+	}
+	Rva8CCD60Iterator *next(Rva8CCD60Iterator *iterator)
+	{
+		return (Rva8CCD60Iterator *)bfmeNext1285((BfmeIterator1285 *)iterator);
+	}
 
-	void *m_unused;
 	unsigned m_nextTagged;
 };
 
 class Rva8CCD60StringValue : public Rva8CCD60Value
 {
-public:
-	Rva8CCD60StringBlock *m_string;
 };
 
-Rva8CCD60StringValue *rva892940CreateStringValue();
+class Rva008B2EA0Node;
+Rva008B2EA0Node *rva008B2EA0Create();
+
+inline Rva8CCD60StringValue *rva892940CreateStringValue()
+{
+	return (Rva8CCD60StringValue *)rva008B2EA0Create();
+}
+
+class Rva00899770;
+class BfmeStrVKI;
+
+class Rva008AE770Stack
+{
+public:
+	Rva00899770 *createString(void *value, int unused, BfmeStrVKI *name,
+		int one, int another, int zero);
+};
 
 class Rva8CCD60State
 {
 public:
 	void expandPrototypeValues(void *first, void *second);
 	Rva8CCD60Value *makeValue(void *first, void *second, void *data,
-		int one1, int one2, int zero);
+		int one1, int one2, int zero)
+	{
+		return (Rva8CCD60Value *)((Rva008AE770Stack *)this)->createString(
+			first, (int)second, (BfmeStrVKI *)data, one1, one2, zero);
+	}
 
 	void pushValue(Rva8CCD60Value *value)
 	{
@@ -81,22 +116,20 @@ public:
 	Rva8CCD60Value **m_stack;
 };
 
-extern Rva8CCD60Value *g_bfmeFallbackDB;
+class AptValue;
+extern AptValue *g_bfmeFallbackDB;
 
 void Rva8CCD60State::expandPrototypeValues(void *first, void *second)
 {
-	Rva8CCD60IteratorList *list;
 	Rva8CCD60Value *value = m_stack[m_count - 1];
 	unsigned flags = value->m_flags;
 	int type = flags & 0x3f;
 	if (type == 1 || type == 42)
 	{
-		flags >>= 15;
-		*(unsigned char *)&flags = (unsigned char)~*(unsigned char *)&flags;
-		if (!(*(unsigned char *)&flags & 1))
+		if (!((unsigned char)~(unsigned char)(flags >> 15) & 1))
 		{
 			Rva8CCD60Value *stringValue = type == 1 ? value : value->m_indirect;
-			value = makeValue(first, second, (char *)stringValue + 8, 1, 1, 0);
+			value = makeValue(first, second, &stringValue->m_string, 1, 1, 0);
 		}
 	}
 
@@ -105,30 +138,31 @@ void Rva8CCD60State::expandPrototypeValues(void *first, void *second)
 	if (!old->maxRefCountHit())
 		old->release();
 	--m_count;
-	pushValue(g_bfmeFallbackDB);
+	pushValue((Rva8CCD60Value *)g_bfmeFallbackDB);
 
-	for (list = value->getIteratorList(); list != 0; )
+	Rva8CCD60IteratorList *list = value->getIteratorList();
+	while (list != 0)
 	{
 		for (Rva8CCD60Iterator *iterator = list->first(); iterator != 0;
 			iterator = list->next(iterator))
 		{
-			const char *name = iterator->m_name->m_text;
-			if (_strcmpi(name, "__proto__") != 0 &&
-				_strcmpi(name, "prototype") != 0)
+			if (_strcmpi(iterator->m_name->m_text, "__proto__") != 0 &&
+				_strcmpi(iterator->m_name->m_text, "prototype") != 0)
 			{
 				Rva8CCD60StringValue *created = rva892940CreateStringValue();
-				Rva8CCD60StringBlock *replacement = iterator->m_name;
-				++replacement->m_refs;
+				++iterator->m_name->m_refs;
 				Rva8CCD60StringBlock *oldString = created->m_string;
 				--oldString->m_refs;
 				if (oldString->m_refs == 0)
 					Rva01337A30ReleaseTable[1](oldString);
-				created->m_string = replacement;
+				created->m_string = iterator->m_name;
 				pushValue(created);
 			}
 		}
-		unsigned next = list->m_nextTagged & ~1u;
-		list = next == 0 ? 0 : ((Rva8CCD60Value *)next)->getIteratorList();
+		Rva8CCD60Value *next = (Rva8CCD60Value *)(list->m_nextTagged & ~1u);
+		if (next == 0)
+			break;
+		list = next->getIteratorList();
 	}
 	value->release();
 }
