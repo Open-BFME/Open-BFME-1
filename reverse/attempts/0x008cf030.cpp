f6727697155a8ecbbd8a9b9c8d358c359008e799
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-24 04:02:18 +0200

    reverse: bank closer 0x008CF030 build array

--- reverse/attempts/0x008cf030.cpp
+++ reverse/attempts/0x008cf030.cpp
@@ -1,37 +1,35 @@
 // ?rva8CF030BuildArray@@YAXPAVRva8CF030State@@PAX@Z
-// partial score=0.82 date=2026-09-02
+// partial score=0.9 date=2026-09-24
 // ?rva8CF030BuildArray@@YAXPAVRva8CF030State@@PAX@Z
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
-// readable Apt array/list builder, retail 0x008CF030 (380 bytes).
+// Apt array builder, retail 0x008CF030 (380 bytes); residue is register allocation only.
 
-struct Rva8CF030StringBlock
+struct BfmeStringData3AF0
 {
-	unsigned short m_refs;
+	unsigned short m_refCount, m_length, m_capacity, m_unknown06;
 };
-
-extern Rva8CF030StringBlock g_bfmeDefaultString1284;
-extern void (__cdecl **Rva01337A30ReleaseTable)(void *);
-extern void *(__cdecl *g_rva8CF030Allocate)(unsigned);
+struct BfmeStringPool3AF0
+{
+	void *m_unknown00;
+	void (__cdecl *free)(void *);
+};
+extern BfmeStringData3AF0 g_bfmeDefaultString1284;
+extern BfmeStringPool3AF0 *g_bfmeStringPool1284;
+extern void *(__cdecl *Rva008C5D70Alloc)(unsigned);
 
 class Rva8CF030String
 {
 public:
-	Rva8CF030String()
-	{
-		m_block = &g_bfmeDefaultString1284;
-		++g_bfmeDefaultString1284.m_refs;
-	}
+	Rva8CF030String() : m_data(&g_bfmeDefaultString1284) { ++m_data->m_refCount; }
 	~Rva8CF030String()
 	{
-		Rva8CF030StringBlock *block = m_block;
-		--block->m_refs;
-		if (block->m_refs == 0)
-			Rva01337A30ReleaseTable[1](block);
+		BfmeStringData3AF0 *old = m_data;
+		if (--old->m_refCount == 0)
+			g_bfmeStringPool1284->free(old);
 	}
 
 private:
-	Rva8CF030StringBlock *m_block;
-	unsigned m_unused;
+	BfmeStringData3AF0 *m_data;
 };
 
 class Rva8CF030List;
@@ -71,6 +69,22 @@ public:
 
 	char m_gap04[8];
 	unsigned m_nextTagged;
+
+	Rva8CF030List *next() const { return (Rva8CF030List *)(m_nextTagged & ~1u); }
+	void setNext(Rva8CF030List *list)
+	{
+		Rva8CF030List *old = next();
+		if (list != 0)
+			list->addRef();
+		if (old != 0)
+			old->release();
+		if (list == 0)
+			m_nextTagged = 0;
+		else if (list->tagged() == 1)
+			m_nextTagged = (unsigned)list | 1;
+		else
+			m_nextTagged = (unsigned)list;
+	}
 };
 
 void rva897300InitializeRefObject(void *object);
@@ -81,12 +95,12 @@ public:
 	Rva8CF030ListNode(Rva8CF030Value *value);
 	static void *operator new(unsigned size)
 	{
-		void *raw = g_rva8CF030Allocate(size + 8);
+		void *raw = Rva008C5D70Alloc(size + 8);
 		void *object = (char *)raw + 8;
 		rva897300InitializeRefObject(object);
 		return object;
 	}
-	static void operator delete(void *);
+	static void operator delete(void *object, unsigned size);
 	char m_gap10[0x0c];
 };
 
@@ -103,36 +117,21 @@ public:
 void rva8CF030BuildArray(Rva8CF030State *state, void *)
 {
 	Rva8CF030String unusedString;
-	register Rva8CF030State *owner = state;
-	Rva8CF030Value *top = owner->m_stack[owner->m_count - 1];
-	Rva8CF030Value *countValue = owner->m_stack[owner->m_count - 2];
-	int count = countValue->toInteger();
-	Rva8CF030List **items = (Rva8CF030List **)g_rva8CF030Allocate(count * 4);
+	Rva8CF030Value *top = state->m_stack[state->m_count - 1];
 	int index = 0;
-
+	int count = state->m_stack[state->m_count - 2]->toInteger();
+	Rva8CF030List **items = (Rva8CF030List **)Rva008C5D70Alloc(count * 4);
 	for (; index < count; ++index)
 	{
-		Rva8CF030Value *value = owner->m_stack[owner->m_count - index - 3];
-		Rva8CF030List *listOwner = value->getList();
-		Rva8CF030List *list = (Rva8CF030List *)(listOwner->m_nextTagged & ~1u);
+		Rva8CF030Value *value = state->m_stack[state->m_count - index - 3];
+		Rva8CF030List *list = value->getList()->next();
 		if (list == 0)
 		{
-			Rva8CF030List *created = new Rva8CF030ListNode(value);
-			listOwner = value->getList();
-			Rva8CF030List *old = (Rva8CF030List *)(listOwner->m_nextTagged & ~1u);
-			if (created != 0)
-				created->addRef();
-			if (old != 0)
-				old->release();
-			if (created == 0)
-				listOwner->m_nextTagged = 0;
-			else
-				listOwner->m_nextTagged = (unsigned)created | (created->tagged() == true ? 1u : 0u);
-			list = created;
+			list = new Rva8CF030ListNode(value);
+			value->getList()->setNext(list);
 		}
 		items[index] = list;
 	}
-
 	top->consumeArray(items, count);
-	owner->popValues(count + 2);
+	state->popValues(count + 2);
 }
