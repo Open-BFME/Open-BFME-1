512a6b8bf16de4033ccfae3456b83c21f6b7d7da
Jonathan <jonf38@users.noreply.github.com>
2026-09-24 06:54:09 -0400

    Improve StackWalk capture homes

--- reverse/attempts/0x0088ce00.cpp
+++ reverse/attempts/0x0088ce00.cpp
@@ -1,5 +1,5 @@
 // ?StackWalk@DebugStackwalk@@SAHAAVSignature@1@PAU_CONTEXT@@_N@Z
-// partial score=0.245 date=2026-09-24
+// partial score=0.27 date=2026-09-24
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
 // stlport
 #define Matrix4x4 Matrix4  // BFME renamed it
@@ -45,10 +45,11 @@ struct BfmeStackFrame : public STACKFRAME
 
 struct BfmeWalkStorage
 {
+	DWORD padding;
+	DWORD regEbp;
+	BfmeStackFrame frame;
 	DWORD regEip;
 	DWORD regEsp;
-	BfmeStackFrame frame;
-	DWORD unused;
 	CONTEXT context;
 };
 
@@ -373,7 +374,6 @@ int DebugStackwalk::StackWalk(Signature &sig, struct _CONTEXT *ctx, bool useFall
   if (!gDbg._StackWalk)
     return 0;
 
-	unsigned long reg_ebp;
 	BfmeWalkStorage storage;
 	// Set up the stack frame structure for the start point of the stack walk (i.e. here).
 	memset(&storage.frame,0,sizeof(storage.frame));
@@ -397,11 +397,11 @@ int DebugStackwalk::StackWalk(Signature &sig, struct _CONTEXT *ctx, bool useFall
     here:
 		  lea	eax,here
 		  mov	storage.regEip,eax
-		  mov	reg_ebp,ebp
+		  mov	storage.regEbp,ebp
 		  mov	storage.regEsp,esp
 	  };
 	  storage.frame.AddrPC.Offset = storage.regEip;
-	  storage.frame.AddrFrame.Offset = reg_ebp;
+	  storage.frame.AddrFrame.Offset = storage.regEbp;
 	  storage.frame.AddrStack.Offset = storage.regEsp;
   }
 
@@ -422,7 +422,7 @@ int DebugStackwalk::StackWalk(Signature &sig, struct _CONTEXT *ctx, bool useFall
 
 	if (sig.m_numAddr < 5 && useFallback && !Rva0088EAF0IsDebuggerPresent())
 	{
-		DWORD *stack = reinterpret_cast<DWORD *>(ctx ? ctx->Esp : reg_ebp);
+		DWORD *stack = reinterpret_cast<DWORD *>(ctx ? ctx->Esp : storage.regEbp);
 		HANDLE process = GetCurrentProcess();
 		sig.m_numAddr = 1;
 		while (sig.m_numAddr < Signature::MAX_ADDR)
