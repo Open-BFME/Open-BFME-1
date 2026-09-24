e36e875f1cf8858fae420efb19aea02c1b30dca0
admiraly <249692417+admiraly@users.noreply.github.com>
2026-09-24 16:34:00 +0200

    Convert CommandLine map-name parser 0x00062D00 to C++
    79 B exact. Placed in the TU that defines the file-static ConvertShortMapPathToLongMapPath so the EDI register call reproduces; GlobalData+0x8 map name added to the TU view.

--- Code/GameEngine/Source/Common/ConvertShortMapPathToLongMapPath.cpp
+++ Code/GameEngine/Source/Common/ConvertShortMapPathToLongMapPath.cpp
@@ -1,4 +1,5 @@
 // cl: /DNDEBUG /MD /EHsc /ICode/Libraries/Source/WWVegas/WWLib
+#include <string.h>
 
 class AsciiString;
 
@@ -33,6 +34,7 @@ public:
     bool nextToken(StringBase<T> *token, const T *delimiters);
     void removeLastChar();
     void set(const StringBase<T> &other);
+    void set(const T *text, int length);
 
 private:
     Header *m_data;
@@ -103,6 +105,11 @@ public:
 		StringBase<char>::set(reinterpret_cast<const StringBase<char> &>(other));
 	}
 
+	void set(const char *text)
+	{
+		StringBase<char>::set(text, text ? strlen(text) : 0);
+	}
+
 	AsciiString &operator=(const char *text);
 };
 
@@ -151,7 +158,9 @@ void ConvertShortMapPathToLongMapPathAnchor(AsciiString &mapName)
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/Common/GlobalData.h
 struct Rva006C9270GlobalData
 {
-	unsigned char m_bfmePad000[0xB80];
+	unsigned char m_bfmePad000[8];
+	AsciiString m_bfmeMapName008;
+	unsigned char m_bfmePad00C[0xB80 - 0xC];
 	AsciiString m_bfmeMapNameB80;
 	unsigned char m_bfmePadB84[4];
 	bool m_bfmeFlagB88;
@@ -183,4 +192,18 @@ int Rva00062C50ParseMapName(char **arguments, int count)
 	}
 
 	return 2;
+}
+
+// Retail 0x00062D00, 79 bytes: the third CommandLine map-name parser. It stores
+// the argument into GlobalData+0x8 and hands that string to the file-static
+// helper, which takes it in EDI (same TU convention as the two parsers above).
+int Rva00062D00ParseMapName(char **arguments, int count)
+{
+	if (TheWritableGlobalData != 0 && count >= 2)
+	{
+		TheWritableGlobalData->m_bfmeMapName008.set(arguments[1]);
+		ConvertShortMapPathToLongMapPath(TheWritableGlobalData->m_bfmeMapName008);
+	}
+
+	return 1;
 }
