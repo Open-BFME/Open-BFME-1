13b83da5d80320403fc358971b8df33f87c00d3b
Jonathan <jonf38@users.noreply.github.com>
2026-09-24 12:52:24 -0400

    Improve StackWalk fallback loop shape

--- reverse/attempts/0x0088ce00.cpp
+++ reverse/attempts/0x0088ce00.cpp
@@ -1,5 +1,5 @@
 // ?StackWalk@DebugStackwalk@@SAHAAVSignature@1@PAU_CONTEXT@@_N@Z
-// partial score=0.36 date=2026-09-24
+// partial score=0.55 date=2026-09-24
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
 // stlport
 #define Matrix4x4 Matrix4  // BFME renamed it
@@ -427,12 +427,11 @@ int DebugStackwalk::StackWalk(Signature &sig, struct _CONTEXT *ctx, bool useFall
 		DWORD *stack = reinterpret_cast<DWORD *>(ctx ? ctx->Esp : storage.regEbp);
 		HANDLE process = GetCurrentProcess();
 		sig.m_numAddr = 1;
-		while (sig.m_numAddr < Signature::MAX_ADDR)
+		for (; sig.m_numAddr < Signature::MAX_ADDR; ++stack)
 		{
 			DWORD address;
 			if (!ReadProcessMemory(process, stack, &address, sizeof(address), NULL))
 				break;
-			++stack;
 			if (IsBadCodePtr(reinterpret_cast<FARPROC>(address)))
 				continue;
 
