33cc998a6a16bbe489dabc2206a3fbd36b40c204
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-24 19:19:10 +0200

    feat(reverse): recover WeaponSet::updateWeaponSet

--- Code/GameEngine/Source/GameLogic/Object/WeaponSet_updateWeaponSet.cpp
+++ Code/GameEngine/Source/GameLogic/Object/WeaponSet_updateWeaponSet.cpp
@@ -1,11 +1,6 @@
-// ?updateWeaponSet@WeaponSet@@QAEXPBVObject@@@Z
-// partial score=0.78 date=2026-09-04
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: WeaponSet::updateWeaponSet, retail 0x001EBBC0 size 379.
-// Focused TU: WeaponSet.cpp keeps ZH WEAPONSLOT_COUNT==3. BFME walks 4 slots,
-// looks up the owner via TheGameLogic when dropping a lock, and stores the
-// owner id at +0x34. Template slots sit at the same +8 as WeaponSet::m_weapons,
-// so the rebuild walk is a pointer-difference induction.
+// Open-BFME5: WeaponSet::updateWeaponSet, retail 0x001EBBC0 size 379; BFME walks 4 slots,
+// re-looks each template up by name key, drops locks via TheGameLogic and stores the owner id at +0x34.
 
 typedef int Int;
 typedef bool Bool;
@@ -52,7 +47,8 @@ public:
 class ThingTemplate
 {
 public:
-	int m_vptr;
+	virtual ~ThingTemplate();
+
 	Overridable *m_nextOverride;
 };
 
@@ -82,7 +78,7 @@ public:
 	int m_vptr;
 	ThingTemplate *m_template;
 	unsigned char m_pad_08[0x74 - 8];
-	Int m_objectId;
+	Int m_id;
 };
 
 class GameLogic
@@ -98,13 +94,14 @@ class WeaponTemplate
 public:
 	int m_vptr;
 	char m_pad_04[0x0C - 4];
+	NameKeyType getNameKey() const { return m_nameKey; }
 	NameKeyType m_nameKey;
 	char m_pad_10[0x4C - 0x10];
 	Int m_damageType;
 	char m_pad_50[0x4D4 - 0x50];
 	Int m_antiMask;
 	char m_pad_4d8[0x4DC - 0x4D8];
-	unsigned char m_isDamageWeapon;
+	Bool m_isDamageWeapon;
 };
 
 class BfmeWeapon
@@ -121,7 +118,7 @@ public:
 	WeaponTemplate *m_template;
 	Int m_ownerId;
 	char m_pad_0c[0x4C - 0x0C];
-	unsigned char m_pitchLimited;
+	Bool m_pitchLimited;
 };
 
 class BfmeWeaponStore
@@ -139,7 +136,7 @@ public:
 	char m_pad_00[8];
 	const WeaponTemplate *m_template[4];
 	char m_pad_18[0xE9 - 0x18];
-	unsigned char m_isWeaponLockSharedAcrossSets;
+	Bool m_isWeaponLockSharedAcrossSets;
 };
 
 class WeaponSet
@@ -156,8 +153,8 @@ private:
 	UnsignedInt m_filledWeaponSlotMask;
 	Int m_totalAntiMask;
 	Int m_totalDamageTypeMask;
-	unsigned char m_hasPitchLimit;
-	unsigned char m_hasDamageWeapon;
+	Bool m_hasPitchLimit;
+	Bool m_hasDamageWeapon;
 	unsigned char m_pad_2e[0x34 - 0x2E];
 	Int m_objectId;
 };
@@ -165,80 +162,64 @@ private:
 // ?updateWeaponSet@WeaponSet@@QAEXPBVObject@@@Z
 void WeaponSet::updateWeaponSet(const Object *obj)
 {
-	Int none = 0;
-	const Object *o = obj;
-	ThingTemplate *tmpl = o->m_template;
+	ThingTemplate *tmpl = obj->m_template;
 	const ThingTemplate *finalTmpl;
-	if (tmpl == (ThingTemplate *)none)
+	if (tmpl == 0)
 		finalTmpl = 0;
 	else
 	{
 		Overridable *next = tmpl->m_nextOverride;
-		if (next != (Overridable *)none)
+		if (next != 0)
 			tmpl = (ThingTemplate *)((BfmeOverridable *)next)->getFinalOverride();
 		finalTmpl = tmpl;
 	}
 
-	const WeaponTemplateSet *set = ((const BfmeThingTemplate *)finalTmpl)->findWeaponTemplateSet(o->getWeaponSetFlags());
-	if (set == (WeaponTemplateSet *)none)
-		return;
-	if (set == m_curWeaponTemplateSet)
-		return;
-
-	if (set->m_isWeaponLockSharedAcrossSets == (unsigned char)none)
+	const WeaponTemplateSet *set = ((const BfmeThingTemplate *)finalTmpl)->findWeaponTemplateSet(obj->getWeaponSetFlags());
+	if (set && set != m_curWeaponTemplateSet)
 	{
-		Object *owner = TheGameLogic->findObjectByID(m_objectId);
-		if (m_curWeaponLockedStatus != none)
+		if (!set->m_isWeaponLockSharedAcrossSets)
 		{
-			m_curWeaponLockedStatus = none;
-			if (owner != (Object *)none)
-				owner->clearModelConditionFlags(ModelConditionFlags(ModelConditionFlags::kInit, 0x88, 0x89, 0x8A));
-		}
-		m_curWeapon = PRIMARY_WEAPON;
-	}
-
-	m_filledWeaponSlotMask = none;
-	m_totalAntiMask = none;
-	m_totalDamageTypeMask = none;
-	m_hasPitchLimit = (unsigned char)none;
-	m_hasDamageWeapon = (unsigned char)none;
-
-	int rel = (int)((const char *)set - (const char *)this);
-	Weapon **slot = &m_weapons[3];
-	int i = 3;
-	do
-	{
-		Weapon *weapon = *slot;
-		if (weapon != (Weapon *)none)
-		{
-			delete weapon;
-			*slot = (Weapon *)none;
-		}
-
-		const WeaponTemplate *wt = *(const WeaponTemplate **)((const char *)slot + rel);
-		if (wt != (const WeaponTemplate *)none)
-		{
-			WeaponTemplate *found = TheWeaponStore->findWeaponTemplatePrivate(wt->m_nameKey);
-			if (found != (WeaponTemplate *)none)
+			Object *owner = TheGameLogic->findObjectByID(m_objectId);
+			if (m_curWeaponLockedStatus != 0)
 			{
-				weapon = TheWeaponStore->allocateNewWeapon(found, (WeaponSlotType)i);
-				*slot = weapon;
-				weapon->m_ownerId = o->m_objectId;
-				((BfmeWeapon *)weapon)->loadAmmoNow(o);
+				m_curWeaponLockedStatus = 0;
+				if (owner)
+					owner->clearModelConditionFlags(ModelConditionFlags(ModelConditionFlags::kInit, 0x88, 0x89, 0x8A));
+			}
+			m_curWeapon = PRIMARY_WEAPON;
+		}
+		m_filledWeaponSlotMask = 0;
+		m_totalAntiMask = 0;
+		m_totalDamageTypeMask = 0;
+		m_hasPitchLimit = false;
+		m_hasDamageWeapon = false;
+		for (Int i = 4 - 1; i >= PRIMARY_WEAPON; --i)
+		{
+			if (m_weapons[i] != 0)
+			{
+				delete m_weapons[i];
+				m_weapons[i] = 0;
+			}
 
-				m_filledWeaponSlotMask |= (1 << i);
-				m_totalAntiMask |= weapon->m_template->m_antiMask;
-				m_totalDamageTypeMask |= (1 << weapon->m_template->m_damageType);
-				if (weapon->m_pitchLimited != (unsigned char)none)
-					m_hasPitchLimit = 1;
-				if (weapon->m_template->m_isDamageWeapon != (unsigned char)none)
-					m_hasDamageWeapon = 1;
+			if (set->m_template[i])
+			{
+				WeaponTemplate *found = TheWeaponStore->findWeaponTemplatePrivate(set->m_template[i]->getNameKey());
+				if (found)
+				{
+					m_weapons[i] = TheWeaponStore->allocateNewWeapon(found, (WeaponSlotType)i);
+					m_weapons[i]->m_ownerId = obj->m_id;
+					((BfmeWeapon *)m_weapons[i])->loadAmmoNow(obj);
+					m_filledWeaponSlotMask |= (1 << i);
+					m_totalAntiMask |= m_weapons[i]->m_template->m_antiMask;
+					m_totalDamageTypeMask |= (1 << m_weapons[i]->m_template->m_damageType);
+					if (m_weapons[i]->m_pitchLimited)
+						m_hasPitchLimit = true;
+					if (m_weapons[i]->m_template->m_isDamageWeapon)
+						m_hasDamageWeapon = true;
+				}
 			}
 		}
-		--i;
-		--slot;
-	} while (i >= none);
-
-	m_curWeaponTemplateSet = set;
-	m_objectId = o->m_objectId;
+		m_curWeaponTemplateSet = set;
+		m_objectId = obj->m_id;
+	}
 }
