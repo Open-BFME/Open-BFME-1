e450f75eee3b67ab896c97f5e19a7c24a522d474
dginovker <danielginovker@gmail.com>
2026-09-25 21:46:44 +0900

    Name target adjustment record values

--- Code/GameEngine/Source/GameLogic/AI/PathfindAdjustDeab0.cpp
+++ Code/GameEngine/Source/GameLogic/AI/PathfindAdjustDeab0.cpp
@@ -25,18 +25,18 @@ public:
 class Weapon
 {
 public:
-	Bool isGoalPosWithinAttackRange( const Object *obj, const Coord3D *pos,
-		const Object *victim, const Coord3D *victimPos, Int flags ) const;
+	Bool isGoalPosWithinAttackRange( const Object *object, const Coord3D *goalPosition,
+		const Object *victim, const Coord3D *victimPosition, Int flags ) const;
 };
 
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIPathfind.h
 class Pathfinder
 {
 public:
-	Bool checkForAdjust( Object *obj, const LocomotorSet *locomotorSet, Bool center,
-		Int cellX, Int cellY, Int layer, Coord3D *pos, Bool isHuman );
-	Bool checkDestination( const Object *obj, Int cellX, Int cellY,
-		PathfindLayerEnum layer, Int iRadius, Bool center );
+	Bool checkForAdjust( Object *object, const LocomotorSet *locomotorSet, Bool centerInCell,
+		Int cellX, Int cellY, Int layer, Coord3D *candidatePosition, Bool isHuman );
+	Bool checkDestination( const Object *object, Int cellX, Int cellY,
+		PathfindLayerEnum layer, Int radius, Bool centerInCell );
 };
 
 class Rva003DEAB0Struct
@@ -45,39 +45,39 @@ public:
 	Bool checkCell( Int cellX, Int cellY );
 
 	Pathfinder *m_pathfinder;			// 0x00
-	Object *m_obj;						// 0x04
+	Object *m_object;						// 0x04
 	const LocomotorSet *m_locomotorSet;	// 0x08
-	Bool m_center;						// 0x0C
+	Bool m_centerInCell;						// 0x0C
 	Int m_layer;						// 0x10
-	Coord3D m_pos;						// 0x14
+	Coord3D m_candidatePosition;						// 0x14
 	Bool m_isHuman;						// 0x20
 	Weapon *m_weapon;					// 0x24
 	Object *m_target;					// 0x28
 	Object *m_victim;					// 0x2C
 	Int m_radius;						// 0x30
-	Coord3D *m_dest;					// 0x34
+	Coord3D *m_destination;					// 0x34
 	Bool m_retryOnTargetLayer;			// 0x38
 };
 
 Bool Rva003DEAB0Struct::checkCell( Int cellX, Int cellY )
 {
-	if( !m_pathfinder->checkForAdjust( m_obj, m_locomotorSet, m_center, cellX, cellY,
-			m_layer, &m_pos, m_isHuman ) )
+	if( !m_pathfinder->checkForAdjust( m_object, m_locomotorSet, m_centerInCell, cellX, cellY,
+			m_layer, &m_candidatePosition, m_isHuman ) )
 	{
 		if( !m_retryOnTargetLayer )
 			return false;
 
-		if( !m_pathfinder->checkForAdjust( m_obj, m_locomotorSet, m_center, cellX, cellY,
-				m_target->getLayer(), &m_pos, m_isHuman ) )
+		if( !m_pathfinder->checkForAdjust( m_object, m_locomotorSet, m_centerInCell, cellX, cellY,
+				m_target->getLayer(), &m_candidatePosition, m_isHuman ) )
 			return false;
 	}
 
-	if( !m_weapon->isGoalPosWithinAttackRange( m_target, &m_pos, m_victim, &m_victim->m_pos, 0 ) )
+	if( !m_weapon->isGoalPosWithinAttackRange( m_target, &m_candidatePosition, m_victim, &m_victim->m_pos, 0 ) )
 		return false;
 
-	if( !m_pathfinder->checkDestination( m_target, cellX, cellY, (PathfindLayerEnum)m_layer, m_radius, m_center ) )
+	if( !m_pathfinder->checkDestination( m_target, cellX, cellY, (PathfindLayerEnum)m_layer, m_radius, m_centerInCell ) )
 		return false;
 
-	*m_dest = m_pos;
+	*m_destination = m_candidatePosition;
 	return true;
 }
