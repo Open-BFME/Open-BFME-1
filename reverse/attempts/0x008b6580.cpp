6a4ea513e16fe1c6da4149a4e64517fece78e9ed
Dom Clarke <zajad909@mystis.dev>
2026-09-26 00:27:32 +0100

    chore(reverse): bank daysInMonth at 0x008B6580 month register near miss

--- reverse/attempts/0x008b6580.cpp
+++ reverse/attempts/0x008b6580.cpp
@@ -1,33 +1,23 @@
+// ?d_008b6580@@YAXXZ
+// partial score=0.93 date=2026-09-26
+// cl: /DNDEBUG /DWIN32 /MD /EHsc /O2 /Ob2
 // ?daysInMonth@@YGHIH@Z
-// partial score=0.7 date=2026-09-02
-// cl: /O2 /DNDEBUG /MD
+bool __stdcall isLeapYear( int year );
 
-bool __stdcall isLeapYear(int year);
-
-int __stdcall daysInMonth(unsigned int month, int year)
+int __stdcall daysInMonth( unsigned int month, int year )
 {
 	int days = 31;
-	unsigned int m = month;
-	if (m > 11)
-		return days;
-	switch (m)
+	switch( month )
 	{
-	case 0:
-	case 2:
-	case 4:
-	case 6:
-	case 7:
-	case 9:
-	case 11:
-		return days;
+	case 0: case 2: case 4: case 6: case 7: case 9: case 11:
+		days = 31;
+		break;
 	case 1:
-		return 28 + isLeapYear(year);
-	case 3:
-	case 5:
-	case 8:
-	case 10:
-		return 30;
-	default:
-		return days;
+		days = isLeapYear( year ) ? 29 : 28;
+		break;
+	case 3: case 5: case 8: case 10:
+		days = 30;
+		break;
 	}
+	return days;
 }
