9b2f6e4512a76d0659c10ac875e1017f44eb4142
dginovker <danielginovker@gmail.com>
2026-09-26 05:30:32 +0900

    Name pathfind cell reset state

--- Code/GameEngine/Source/GameLogic/AI/PathfindCell_reset.cpp
+++ Code/GameEngine/Source/GameLogic/AI/PathfindCell_reset.cpp
@@ -23,17 +23,17 @@ private:
 
 void PathfindCell::reset()
 {
-	PathfindCellInfo *info = m_info;
-	if (info != 0) {
-		info->releaseToPool(g_pathfindCellInfoPool);
+	PathfindCellInfo *cellInfoRecord = m_info;
+	if (cellInfoRecord != 0) {
+		cellInfoRecord->releaseToPool(g_pathfindCellInfoPool);
 		m_info = 0;
 	}
 
-	unsigned int flags = m_pathFlags;
-	flags &= 0xfe000040;
-	flags |= 0x40;
+	unsigned int resetPathFlags = m_pathFlags;
+	resetPathFlags &= 0xfe000040;
+	resetPathFlags |= 0x40;
 	m_zoneAndGoals = 0;
 	m_layerFlags = 0;
 	m_cellTypeAndUnitFlags = 0;
-	m_pathFlags = flags;
+	m_pathFlags = resetPathFlags;
 }
