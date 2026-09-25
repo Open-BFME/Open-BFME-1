a42a31a8c20977932bcfa3d95c00b4176013fd80
StarkillerHDX <128750908+StarkillerHDX@users.noreply.github.com>
2026-09-25 04:19:10 +0200

    Bank closer ObjectCountNearbyEnemies (251 to 196)

--- reverse/attempts/0x002e8130.cpp
+++ reverse/attempts/0x002e8130.cpp
@@ -1,5 +1,7 @@
 // ?ObjectCountNearbyEnemies@@YAHPAUlua_State@@@Z
-// partial score=0.46 date=2026-09-23
+// partial score=0.55 date=2026-09-25
+// ?ObjectCountNearbyEnemies@@YAHPAUlua_State@@@Z
+// scratch experiment: order filter construction before float radius materialization
 // cl: /O2 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib
 // stlport
 #define _STLP_USE_STATIC_LIB 1
@@ -17,15 +19,15 @@ extern "C" void lua_pushnumber(lua_State *, double);
 unsigned Rva00990030Lookup(lua_State *, int);
 
 class Object { public: unsigned char m_pad00[0x38]; Coord3D m_position; };
-class GameLogic { public: Object *bfmeFind1011(int); };
+class GameLogic { public: Object *findObjectByID(int); };
 extern GameLogic *TheGameLogic;
 
 class PartitionFilter {
 public:
   PartitionFilter() : m_next(0) {}
   virtual ~PartitionFilter() {}
-  virtual bool allow(Object *);
-  virtual int getPlayerMask();
+  virtual Bool allow(Object *);
+  virtual Int getPlayerMask();
   PartitionFilter *link(PartitionFilter *next);
   PartitionFilter *m_next;
 };
@@ -33,30 +35,30 @@ class Rva0025ED50ObjectFilter : public PartitionFilter {
 public:
   explicit Rva0025ED50ObjectFilter(Object *object) : m_object(object) {}
   virtual ~Rva0025ED50ObjectFilter() {}
-  virtual bool allow(Object *);
+  virtual Bool allow(Object *);
   Object *m_object;
 };
 class Rva0025ED50RootFilter : public PartitionFilter {
 public:
   Rva0025ED50RootFilter() {}
   virtual ~Rva0025ED50RootFilter() {}
-  virtual bool allow(Object *);
+  virtual Bool allow(Object *);
 };
 class PartitionFilterRelationship : public PartitionFilter {
 public:
-  PartitionFilterRelationship(Object *object, int flags, bool match)
+  PartitionFilterRelationship(Object *object, Int flags, Bool match)
       : m_object(object), m_flags(flags), m_match(match) {}
   virtual ~PartitionFilterRelationship() {}
-  virtual bool allow(Object *);
-  virtual int getPlayerMask();
+  virtual Bool allow(Object *);
+  virtual Int getPlayerMask();
   Object *m_object;
-  int m_flags;
-  bool m_match;
+  Int m_flags;
+  Bool m_match;
 };
 
 struct Rva002E8130Entry { Object *object; unsigned distanceBits; };
 struct Rva009F39F0Payload {
-  std::vector<Rva002E8130Entry> entries;
+  _STL::vector<Rva002E8130Entry> entries;
   Rva002E8130Entry *current;
   int references;
 };
@@ -71,6 +73,14 @@ public: BfmeWideResult bfmeForwardWideC(int, int, int, int, int);
 };
 extern BfmeWideForwardC *ThePartitionManager;
 
+inline BfmeWideResult Rva002E8130Forward(BfmeWideForwardC *manager,
+    int position, int filter, int radius)
+{
+  union { float real; int bits; } radiusWord;
+  return manager->bfmeForwardWideC(
+      position, (radiusWord.real = (float)radius, radiusWord.bits),
+      0, filter, 1);
+}
 
 int ObjectCountNearbyEnemies(lua_State *state) {
   unsigned objectID = Rva00990030Lookup(state, 1);
@@ -79,17 +89,15 @@ int ObjectCountNearbyEnemies(lua_State *state) {
     return 0;
   }
   volatile int radius = (int)lua_tonumber(state, 2);
-  Object *object = TheGameLogic->bfmeFind1011((int)objectID);
+  Object *object = TheGameLogic->findObjectByID((int)objectID);
   if (object != 0) {
-    union { float real; int bits; } radiusWord;
-    BfmeWideResult iterator = ThePartitionManager->bfmeForwardWideC(
+    BfmeWideResult iterator = Rva002E8130Forward(ThePartitionManager,
         (int)((char *)object + 0x38),
-        (radiusWord.real = (float)radius, radiusWord.bits),
-        0,
         (int)PartitionFilterRelationship(object, 1, false).link(
             Rva0025ED50RootFilter().link(&Rva0025ED50ObjectFilter(object))),
-        1);
-    int count = iterator.value->entries.end() - iterator.value->entries.begin();
+        radius);
+    Rva009F39F0Payload *payload = iterator.value;
+    int count = payload->entries.size();
     lua_pushnumber(state, (double)count);
     return 1;
   }
