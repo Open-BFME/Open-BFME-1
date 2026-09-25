d50f02b5295e409bfd499ca2f639f1adc34d6a05
Jonathan <jonf38@users.noreply.github.com>
2026-09-24 20:07:31 -0400

    Correct synthetic base constructor identity

--- Code/GameEngine/Source/Common/Rva006FB500Ctor.cpp
+++ Code/GameEngine/Source/Common/Rva006FB500Ctor.cpp
@@ -1,15 +1,15 @@
 // cl: /O2 /Ob0
 
-class Rva006FB500Base
+class Rva00020004
 {
 public:
-	Rva006FB500Base();
+	Rva00020004();
 	virtual void handle();
 };
 
 int g_rva006FB500_slot;
 
-class Rva006FB500 : public Rva006FB500Base
+class Rva006FB500 : public Rva00020004
 {
 	int m_04;
 	int *m_08;
