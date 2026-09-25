96b9bf7c223326057053f824344cfe2a7df173bc
dginovker <danielginovker@gmail.com>
2026-09-26 00:04:27 +0900

    Name packed line walk inputs

--- Code/GameEngine/Source/GameLogic/AI/PathfinderIterateCellsAlongLineRva003D7010.cpp
+++ Code/GameEngine/Source/GameLogic/AI/PathfinderIterateCellsAlongLineRva003D7010.cpp
@@ -32,7 +32,7 @@ public:
 class PathfindLayer
 {
 public:
-	PathfindCell *getCell( Int x, Int y );
+	PathfindCell *getCell( Int cellX, Int cellY );
 
 private:
 	char m_unreconstructed[0x44];
@@ -43,8 +43,8 @@ struct Rva003D7010Struct;
 class Pathfinder
 {
 public:
-	Int iterateCellsAlongLine( const ICoord2D &start, const ICoord2D &end,
-		PathfindLayerEnum layer, Rva003D7010Struct *userData );
+	Int iterateCellsAlongLine( const ICoord2D &startCell, const ICoord2D &destinationCell,
+		PathfindLayerEnum layer, Rva003D7010Struct *unusedContext );
 
 private:
 	char m_beforeMap[0x10];
@@ -57,29 +57,29 @@ private:
 	char m_beforeLayers[0x85c - 0x24];
 	PathfindLayer m_layers[16];
 
-	__forceinline PathfindCell *getCell( PathfindLayerEnum layer, Int x, Int y )
+	__forceinline PathfindCell *getCell( PathfindLayerEnum layer, Int cellX, Int cellY )
 	{
-		if (x >= m_extent.lo.x && x <= m_extent.hi.x &&
-			y >= m_extent.lo.y && y <= m_extent.hi.y)
+		if (cellX >= m_extent.lo.x && cellX <= m_extent.hi.x &&
+			cellY >= m_extent.lo.y && cellY <= m_extent.hi.y)
 		{
 			if (layer > 1 && layer <= 15)
 			{
-				PathfindCell *cell = m_layers[layer].getCell( x, y );
+				PathfindCell *cell = m_layers[layer].getCell( cellX, cellY );
 				if (cell)
 					return cell;
 			}
-			return &m_map[x][y];
+			return &m_map[cellX][cellY];
 		}
 		return 0;
 	}
 };
 
-Int Pathfinder::iterateCellsAlongLine( const ICoord2D &start,
-	const ICoord2D &end, PathfindLayerEnum layer,
-	Rva003D7010Struct *userData )
+Int Pathfinder::iterateCellsAlongLine( const ICoord2D &startCell,
+	const ICoord2D &destinationCell, PathfindLayerEnum layer,
+	Rva003D7010Struct *unusedContext )
 {
-	const ICoord2D *start_ptr = &start;
-	const ICoord2D *end_ptr = &end;
+	const ICoord2D *start_ptr = &startCell;
+	const ICoord2D *end_ptr = &destinationCell;
 	Int end_x = end_ptr->x;
 	Int x = start_ptr->x;
 	Int delta_x = abs( end_x - x );
@@ -112,12 +112,12 @@ Int Pathfinder::iterateCellsAlongLine( const ICoord2D &start,
 		xinc1 = 1;
 	}
 
-	if (start.x > end_x)
+	if (startCell.x > end_x)
 	{
 		xinc2 = -xinc2;
 		xinc1 = -1;
 	}
-	if (start.y > end_y)
+	if (startCell.y > end_y)
 	{
 		yinc2 = -yinc2;
 		yinc1 = -1;
