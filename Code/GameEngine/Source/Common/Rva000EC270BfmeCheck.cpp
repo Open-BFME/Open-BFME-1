ba7ffcd490f4ca24d282095414bb59ae16439d08
Dom Clarke <zajad909@mystis.dev>
2026-09-25 23:07:22 +0100

    feat(common): convert list membership test at 0x000EC220 and fold the split return tails of 0x000EC270 and 0x000EC440

--- Code/GameEngine/Source/Common/Rva000EC270BfmeCheck.cpp
+++ Code/GameEngine/Source/Common/Rva000EC270BfmeCheck.cpp
@@ -23,3 +23,23 @@ int BfmeHostEC::bfmeCheckEC(BfmeNodeEC *node)
 
 	return 0;
 }
+
+
+struct Rva000EC220List
+{
+	BfmeNodeEC *m_head;
+};
+
+class Rva000EC220Node : public BfmeNodeEC
+{
+public:
+	int isInList(const Rva000EC220List *list) const;
+};
+
+int Rva000EC220Node::isInList(const Rva000EC220List *list) const
+{
+	if (list->m_head == this || m_bfme25CEC != 0 || m_bfme260EC != 0)
+		return 1;
+
+	return 0;
+}
