368057dc80d9c3d2d25fcac2ee63573524315e02
admiraly <249692417+admiraly@users.noreply.github.com>
2026-09-26 13:49:44 +0200

    banked closest-object update 0x002BD940 358B
    (cherry picked from commit f262f1824983249d7af563a06f8a99de4e8792ea)

--- targets/game/reverse/attempts/0x002bd940.cpp
+++ targets/game/reverse/attempts/0x002bd940.cpp
@@ -1,5 +1,5 @@
 // ?bfmeGo002BD940@Rva002BD940Owner@@QAEXXZ
-// partial score=0.35 date=2026-09-21
+// partial score=0.4 date=2026-09-26
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/bfmekindof /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
 // stlport
 #define BFME_STLP_NODE_ALLOC 1
@@ -37,6 +37,7 @@ class PartitionFilter
 public:
 	virtual ~PartitionFilter() {}
 	virtual bool allow(Object *objOther) = 0;
+	PartitionFilter *m_next;
 };
 
 class PartitionFilterAcceptByKindOf : public PartitionFilter
