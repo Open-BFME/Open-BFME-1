8c9b7ea723673339b031bf8932f1a34dd8c1c074
admiraly <249692417+admiraly@users.noreply.github.com>
2026-09-26 11:08:58 +0200

    gen_asm: dump 239 carved anonymous bodies (301418B) in 11 files

---
+++ Code/gen_asm/d_00bf2738.asm
@@ -0,0 +1,241 @@
+.386
+.model flat
+_TEXT SEGMENT
+
+; retail @ 0x00BF2738 size 11
+public ?d_00bf2738@@YAXXZ
+?d_00bf2738@@YAXXZ PROC
+    db 8Bh, 4Dh, 10h, 83h, 0C1h, 10h, 0E9h, 0E5h, 0B0h, 41h, 0FFh
+?d_00bf2738@@YAXXZ ENDP
+
+; retail @ 0x00BF4ED4 size 27
+public ?d_00bf4ed4@@YAXXZ
+?d_00bf4ed4@@YAXXZ PROC
+    db 68h, 28h, 0D8h, 40h, 00h, 6Ah, 04h, 6Ah, 04h, 8Bh, 85h, 0CCh, 0FEh, 0FEh, 0FFh, 05h
+    db 9Ch, 00h, 00h, 00h, 50h, 0E8h, 88h, 1Eh, 0E0h, 0FFh, 0C3h
+?d_00bf4ed4@@YAXXZ ENDP
+
+; retail @ 0x00BF4EEF size 17
+public ?d_00bf4eef@@YAXXZ
+?d_00bf4eef@@YAXXZ PROC
+    db 8Bh, 8Dh, 0CCh, 0FEh, 0FEh, 0FFh, 81h, 0C1h, 00h, 02h, 00h, 00h, 0E9h, 28h, 89h, 41h
+    db 0FFh
+?d_00bf4eef@@YAXXZ ENDP
+
+; retail @ 0x00BF4F62 size 27
+public ?d_00bf4f62@@YAXXZ
+?d_00bf4f62@@YAXXZ PROC
+    db 68h, 4Ch, 36h, 41h, 00h, 6Ah, 03h, 6Ah, 0Ch, 8Bh, 85h, 0CCh, 0FEh, 0FEh, 0FFh, 05h
+    db 04h, 0Ah, 00h, 00h, 50h, 0E8h, 0FAh, 1Dh, 0E0h, 0FFh, 0C3h
+?d_00bf4f62@@YAXXZ ENDP
+
+; retail @ 0x00BFAB57 size 11
+public ?d_00bfab57@@YAXXZ
+?d_00bfab57@@YAXXZ PROC
+    db 8Bh, 4Dh, 04h, 83h, 0C1h, 04h, 0E9h, 0ABh, 0ABh, 41h, 0FFh
+?d_00bfab57@@YAXXZ ENDP
+
+; retail @ 0x00C08598 size 39
+public ?d_00c08598@@YAXXZ
+?d_00c08598@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 04h
+    db 89h, 45h, 0ECh, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0ECh, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0ECh, 0E9h, 81h, 94h, 0D9h, 0FFh
+?d_00c08598@@YAXXZ ENDP
+
+; retail @ 0x00C097A8 size 39
+public ?d_00c097a8@@YAXXZ
+?d_00c097a8@@YAXXZ PROC
+    db 83h, 7Dh, 0ECh, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0ECh, 83h, 0C0h, 60h
+    db 89h, 45h, 0E8h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E8h, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0E8h, 0E9h, 0B1h, 84h, 3Fh, 0FFh
+?d_00c097a8@@YAXXZ ENDP
+
+; retail @ 0x00C1BED8 size 39
+public ?d_00c1bed8@@YAXXZ
+?d_00c1bed8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 08h
+    db 89h, 45h, 0ECh, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0ECh, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0ECh, 0E9h, 81h, 5Dh, 3Eh, 0FFh
+?d_00c1bed8@@YAXXZ ENDP
+
+; retail @ 0x00C209F8 size 39
+public ?d_00c209f8@@YAXXZ
+?d_00c209f8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 60h
+    db 89h, 45h, 0E8h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E8h, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0E8h, 0E9h, 61h, 12h, 3Eh, 0FFh
+?d_00c209f8@@YAXXZ ENDP
+
+; retail @ 0x00C20FF8 size 39
+public ?d_00c20ff8@@YAXXZ
+?d_00c20ff8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 08h
+    db 89h, 45h, 0ECh, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0ECh, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0ECh, 0E9h, 61h, 0Ch, 3Eh, 0FFh
+?d_00c20ff8@@YAXXZ ENDP
+
+; retail @ 0x00C21DA8 size 39
+public ?d_00c21da8@@YAXXZ
+?d_00c21da8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 08h
+    db 89h, 45h, 0ECh, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0ECh, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0ECh, 0E9h, 0B1h, 0FEh, 3Dh, 0FFh
+?d_00c21da8@@YAXXZ ENDP
+
+; retail @ 0x00C2F5C8 size 41
+public ?d_00c2f5c8@@YAXXZ
+?d_00c2f5c8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 10h, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 05h, 58h, 02h
+    db 00h, 00h, 89h, 45h, 0E8h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E8h, 00h, 00h, 00h
+    db 00h, 8Bh, 4Dh, 0E8h, 0E9h, 3Ah, 67h, 3Dh, 0FFh
+?d_00c2f5c8@@YAXXZ ENDP
+
+; retail @ 0x00C35D18 size 41
+public ?d_00c35d18@@YAXXZ
+?d_00c35d18@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 10h, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 05h, 58h, 02h
+    db 00h, 00h, 89h, 45h, 0E8h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E8h, 00h, 00h, 00h
+    db 00h, 8Bh, 4Dh, 0E8h, 0E9h, 0EAh, 0FFh, 3Ch, 0FFh
+?d_00c35d18@@YAXXZ ENDP
+
+; retail @ 0x00C37BB8 size 39
+public ?d_00c37bb8@@YAXXZ
+?d_00c37bb8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 08h
+    db 89h, 45h, 0E8h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E8h, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0E8h, 0E9h, 0A1h, 0A0h, 3Ch, 0FFh
+?d_00c37bb8@@YAXXZ ENDP
+
+; retail @ 0x00C39F58 size 39
+public ?d_00c39f58@@YAXXZ
+?d_00c39f58@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 08h
+    db 89h, 45h, 0E4h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E4h, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0E4h, 0E9h, 01h, 7Dh, 3Ch, 0FFh
+?d_00c39f58@@YAXXZ ENDP
+
+; retail @ 0x00C4D0F0 size 8
+public ?d_00c4d0f0@@YAXXZ
+?d_00c4d0f0@@YAXXZ PROC
+    db 8Bh, 4Dh, 0F0h, 0E9h, 18h, 2Bh, 0CDh, 0FFh
+?d_00c4d0f0@@YAXXZ ENDP
+
+; retail @ 0x00C51008 size 11
+public ?d_00c51008@@YAXXZ
+?d_00c51008@@YAXXZ PROC
+    db 8Bh, 4Dh, 04h, 83h, 0C1h, 04h, 0E9h, 0FAh, 46h, 3Ch, 0FFh
+?d_00c51008@@YAXXZ ENDP
+
+; retail @ 0x00C521F8 size 39
+public ?d_00c521f8@@YAXXZ
+?d_00c521f8@@YAXXZ PROC
+    db 83h, 7Dh, 0F0h, 00h, 0Fh, 84h, 0Eh, 00h, 00h, 00h, 8Bh, 45h, 0F0h, 83h, 0C0h, 04h
+    db 89h, 45h, 0E8h, 0E9h, 07h, 00h, 00h, 00h, 0C7h, 45h, 0E8h, 00h, 00h, 00h, 00h, 8Bh
+    db 4Dh, 0E8h, 0E9h, 0F1h, 0D9h, 0CCh, 0FFh
+?d_00c521f8@@YAXXZ ENDP
+
+; retail @ 0x00C5FD89 size 11
+public ?d_00c5fd89@@YAXXZ
+?d_00c5fd89@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 5Eh, 21h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fd89@@YAXXZ ENDP
+
+; retail @ 0x00C5FD94 size 11
+public ?d_00c5fd94@@YAXXZ
+?d_00c5fd94@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 13h, 21h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fd94@@YAXXZ ENDP
+
+; retail @ 0x00C5FD9F size 8
+public ?d_00c5fd9f@@YAXXZ
+?d_00c5fd9f@@YAXXZ PROC
+    db 8Bh, 4Dh, 04h, 0E9h, 5Eh, 21h, 3Ah, 0FFh
+?d_00c5fd9f@@YAXXZ ENDP
+
+; retail @ 0x00C5FDA7 size 11
+public ?d_00c5fda7@@YAXXZ
+?d_00c5fda7@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 00h, 21h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fda7@@YAXXZ ENDP
+
+; retail @ 0x00C5FDBA size 11
+public ?d_00c5fdba@@YAXXZ
+?d_00c5fdba@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 0EDh, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fdba@@YAXXZ ENDP
+
+; retail @ 0x00C5FDCD size 11
+public ?d_00c5fdcd@@YAXXZ
+?d_00c5fdcd@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 0DAh, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fdcd@@YAXXZ ENDP
+
+; retail @ 0x00C5FDD8 size 8
+public ?d_00c5fdd8@@YAXXZ
+?d_00c5fdd8@@YAXXZ PROC
+    db 8Bh, 4Dh, 04h, 0E9h, 25h, 21h, 3Ah, 0FFh
+?d_00c5fdd8@@YAXXZ ENDP
+
+; retail @ 0x00C5FDE0 size 11
+public ?d_00c5fde0@@YAXXZ
+?d_00c5fde0@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 0C7h, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fde0@@YAXXZ ENDP
+
+; retail @ 0x00C5FDFE size 8
+public ?d_00c5fdfe@@YAXXZ
+?d_00c5fdfe@@YAXXZ PROC
+    db 8Dh, 4Dh, 04h, 0E9h, 4Ch, 08h, 3Dh, 0FFh
+?d_00c5fdfe@@YAXXZ ENDP
+
+; retail @ 0x00C5FE11 size 8
+public ?d_00c5fe11@@YAXXZ
+?d_00c5fe11@@YAXXZ PROC
+    db 8Dh, 4Dh, 0D8h, 0E9h, 39h, 08h, 3Dh, 0FFh
+?d_00c5fe11@@YAXXZ ENDP
+
+; retail @ 0x00C5FE19 size 11
+public ?d_00c5fe19@@YAXXZ
+?d_00c5fe19@@YAXXZ PROC
+    db 8Bh, 45h, 0E8h, 50h, 0E8h, 8Eh, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fe19@@YAXXZ ENDP
+
+; retail @ 0x00C5FE4A size 11
+public ?d_00c5fe4a@@YAXXZ
+?d_00c5fe4a@@YAXXZ PROC
+    db 8Bh, 45h, 0E8h, 50h, 0E8h, 5Dh, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fe4a@@YAXXZ ENDP
+
+; retail @ 0x00C5FE5D size 11
+public ?d_00c5fe5d@@YAXXZ
+?d_00c5fe5d@@YAXXZ PROC
+    db 8Bh, 45h, 0E8h, 50h, 0E8h, 4Ah, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fe5d@@YAXXZ ENDP
+
+; retail @ 0x00C5FE68 size 8
+public ?d_00c5fe68@@YAXXZ
+?d_00c5fe68@@YAXXZ PROC
+    db 8Bh, 4Dh, 0E8h, 0E9h, 95h, 20h, 3Ah, 0FFh
+?d_00c5fe68@@YAXXZ ENDP
+
+; retail @ 0x00C5FE70 size 11
+public ?d_00c5fe70@@YAXXZ
+?d_00c5fe70@@YAXXZ PROC
+    db 8Bh, 45h, 04h, 50h, 0E8h, 37h, 20h, 0C2h, 0FFh, 59h, 0C3h
+?d_00c5fe70@@YAXXZ ENDP
+
+; retail @ 0x00C5FE7B size 8
+public ?d_00c5fe7b@@YAXXZ
+?d_00c5fe7b@@YAXXZ PROC
+    db 8Bh, 4Dh, 04h, 0E9h, 82h, 20h, 3Ah, 0FFh
+?d_00c5fe7b@@YAXXZ ENDP
+
+; retail @ 0x00C5FEB4 size 18
+public ?d_00c5feb4@@YAXXZ
+?d_00c5feb4@@YAXXZ PROC
+    db 8Bh, 4Dh, 04h, 0E9h, 49h, 20h, 3Ah, 0FFh, 0B8h, 9Ch, 0F3h, 24h, 01h, 0E9h, 10h, 6Fh
+    db 0D9h, 0FFh
+?d_00c5feb4@@YAXXZ ENDP
+_TEXT ENDS
+END
