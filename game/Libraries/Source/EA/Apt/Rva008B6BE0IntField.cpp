7c987c835630a8680e5134aea6c7d28c9a7f8708
admiraly <249692417+admiraly@users.noreply.github.com>
2026-09-26 14:30:11 +0200

    reverse: convert Apt integer field wrapper at 0x008B6BE0

---
+++ game/Libraries/Source/EA/Apt/Rva008B6BE0IntField.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD /EHsc
+// RVA 0x008B6BE0: package the integer field at offset 0x34 as an Apt value.
+class AptInteger
+{
+public:
+    static AptInteger *Create(int value);
+};
+
+AptInteger *aptIntField34At008B6BE0(void *self, int argc)
+{
+    return AptInteger::Create(*(int *)((char *)self + 0x34));
+}
